4f46afe4eb57a33a3ba4f227be641f76224e7584
!cake <cakenotfound@googlemail.com>
2014-07-05 12:04:48 +0000

    spelling D:<

--- lua/chatsounds/lists_nosend/TF2ExpirationDate.lua
+++ lua/chatsounds/lists_nosend/TF2ExpirationDate.lua
@@ -3,15 +3,15 @@ L["3 days yes we all have 3 days to live"]={{path="chatsounds/autoadd/TF2Expirat
 L["aaaagh we cannot teleport bread anymore"]={{path="chatsounds/autoadd/TF2ExpirationDate/aaaagh we cannot teleport bread anymore.ogg",length=3.5599092970521542},}
 L["aah what the hell is that"]={{path="chatsounds/autoadd/TF2ExpirationDate/aah what the hell is that.ogg",length=2.2566893424036283},}
 L["asking out that dial tone"]={{path="chatsounds/autoadd/TF2ExpirationDate/asking out that dial tone.ogg",length=1.7197278911564626},}
-L["deargod"]={{path="chatsounds/autoadd/TF2ExpirationDate/deargod.ogg",length=1.545578231292517},}
-L["effiel tower having sexual congress with me"]={{path="chatsounds/autoadd/TF2ExpirationDate/effiel tower having sexual congress with me.ogg",length=2.3118367346938777},}
+L["dear god"]={{path="chatsounds/autoadd/TF2ExpirationDate/dear god.ogg",length=1.545578231292517},}
+L["eiffel tower having sexual congress with me"]={{path="chatsounds/autoadd/TF2ExpirationDate/eiffel tower having sexual congress with me.ogg",length=2.3118367346938777},}
 L["go to hell spy"]={{path="chatsounds/autoadd/TF2ExpirationDate/go to hell spy.ogg",length=0.73142857142857143},}
-L["hello miss pauling we killed everybody and took a breifcase"]={{path="chatsounds/autoadd/TF2ExpirationDate/hello miss pauling we killed everybody and took a breifcase.ogg",length=3.5961904761904764},}
-L["here youve drawn me having sexual congress with the effiel tower"]={{path="chatsounds/autoadd/TF2ExpirationDate/here youve drawn me having sexual congress with the effiel tower.ogg",length=3.6411791383219954},}
+L["hello miss pauling we killed everybody and took a briefcase"]={{path="chatsounds/autoadd/TF2ExpirationDate/hello miss pauling we killed everybody and took a briefcase.ogg",length=3.5961904761904764},}
+L["here youve drawn me having sexual congress with the eiffel tower"]={{path="chatsounds/autoadd/TF2ExpirationDate/here youve drawn me having sexual congress with the eiffel tower.ogg",length=3.6411791383219954},}
 L["hes drawn a picture of me being hit by a car"]={{path="chatsounds/autoadd/TF2ExpirationDate/hes drawn a picture of me being hit by a car.ogg",length=4.7528344671201816},}
 L["oh scout please go f yourself"]={{path="chatsounds/autoadd/TF2ExpirationDate/oh scout please go f yourself.ogg",length=3.4583219954648525},}
 L["our first dying wish is scouts"]={{path="chatsounds/autoadd/TF2ExpirationDate/our first dying wish is scouts.ogg",length=2.4003628117913833},}
-L["sigh does it say you want the buckyes"]={{path="chatsounds/autoadd/TF2ExpirationDate/sigh does it say you want the buckyes.ogg",length=2.3219954648526078},}
+L["sigh does it say you want the buck yes"]={{path="chatsounds/autoadd/TF2ExpirationDate/sigh does it say you want the buck yes.ogg",length=2.3219954648526078},}
 L["soldierno"]={{path="chatsounds/autoadd/TF2ExpirationDate/soldierno.ogg",length=0.79963718820861673},}
 L["the clock is ticking gentlemen"]={{path="chatsounds/autoadd/TF2ExpirationDate/the clock is ticking gentlemen.ogg",length=2.4453514739229023},}
 L["theres more"]={{path="chatsounds/autoadd/TF2ExpirationDate/theres more.ogg",length=0.83591836734693881},}
