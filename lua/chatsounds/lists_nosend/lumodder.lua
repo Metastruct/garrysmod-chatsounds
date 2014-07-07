9ee0fe4ce6c95e0d2a41bce2f25ca00c77dd35fb
LUModder@gmail.com <lumodder@gmail.com>
2014-07-07 01:57:40 +0000

    Suck my balls Doc (From "Why Engineers Shouldn't Defend")

--- lua/chatsounds/lists_nosend/lumodder.lua
+++ lua/chatsounds/lists_nosend/lumodder.lua
@@ -1,3 +1,4 @@
 c.StartList("lumodder")
 L["trainrain"]={{path="chatsounds/autoadd/lumodder/trainrain.ogg",length=3.0040816326530613},}
+L["suck my balls doc"]={{path="chatsounds/autoadd/lumodder/suck my balls doc.ogg",length=1.590540},}
 c.EndList()
