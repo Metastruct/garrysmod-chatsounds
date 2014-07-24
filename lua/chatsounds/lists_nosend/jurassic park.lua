fed6b9db6a21b5a4575828ded4009637dd179c80
verbalsilence@hotmail.com <verbalsilence@hotmail.com>
2014-07-24 12:07:00 +0000

    please! god damn! i hate this hacker crap!

---
+++ lua/chatsounds/lists_nosend/jurassic park.lua
@@ -0,0 +1,5 @@
+c.StartList("jurassic park")
+L["ah ah ah"]={{path="chatsounds/autoadd/jurassic park/ah ah ah.ogg",length=2.971814058956916},}
+L["nuh uh uh"]={{path="chatsounds/autoadd/jurassic park/nuh uh uh.ogg",length=2.971814058956916},}
+L["you didnt say the magic word"]={{path="chatsounds/autoadd/jurassic park/you didnt say the magic word.ogg",length=1.7428571428571429},}
+c.EndList()
