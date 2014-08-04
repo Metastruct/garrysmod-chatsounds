9b41c9a93e27474228607f5191b1f7c2f8b944c7
techbot121@gmail.com <techbot121@users.noreply.github.com>
2014-08-04 00:01:53 +0000

    that's more than i was expecting it would be

--- lua/chatsounds/lists_nosend/thelewa.lua
+++ lua/chatsounds/lists_nosend/thelewa.lua
@@ -1,50 +1,146 @@
 c.StartList("thelewa")
+L["100g or something"]={{path="chatsounds/autoadd/thelewa/100g or something.ogg",length=1.5934693877551021},}
 L["23 minutes"]={{path="chatsounds/autoadd/thelewa/23 minutes.ogg",length=1.1755102040816328},}
+L["420 viewers oh shit"]={{path="chatsounds/autoadd/thelewa/420 viewers oh shit.ogg",length=2.0331972789115644},}
+L["a fucking square"]={{path="chatsounds/autoadd/thelewa/a fucking square.ogg",length=1.0318367346938775},}
+L["a haha"]={{path="chatsounds/autoadd/thelewa/a haha.ogg",length=1.1290702947845805},}
+L["aah"]={{path="chatsounds/autoadd/thelewa/aah.ogg",length=0.71256235827664394},}
+L["absolutely nothing is going on"]={{path="chatsounds/autoadd/thelewa/absolutely nothing is going on.ogg",length=1.4381859410430839},}
 L["ahh"]={{path="chatsounds/autoadd/thelewa/ahh.ogg",length=0.97959183673469385},}
+L["aim training"]={{path="chatsounds/autoadd/thelewa/aim training.ogg",length=0.76625850340136059},}
+L["also this map is definitely fullcomboable"]={{path="chatsounds/autoadd/thelewa/also this map is definitely fullcomboable.ogg",length=2.4192290249433106},}
 L["and as always we do not disable anything"]={{path="chatsounds/autoadd/thelewa/and as always we do not disable anything.ogg",length=2.9765079365079363},}
 L["and as always"]={{path="chatsounds/autoadd/thelewa/and as always.ogg",length=1.4701133786848073},}
+L["ar9point3"]={{path="chatsounds/autoadd/thelewa/ar9point3.ogg",length=1.1247165532879819},}
+L["ar9point5"]={{path="chatsounds/autoadd/thelewa/ar9point5/ar9point5 Copy.ogg",length=1.1726077097505669},{path="chatsounds/autoadd/thelewa/ar9point5/ar9point5.ogg",length=1.1189115646258503},}
 L["bandaria travellers"]={{path="chatsounds/autoadd/thelewa/bandaria travellers.ogg",length=1.4512471655328798},}
 L["bpm 300"]={{path="chatsounds/autoadd/thelewa/bpm 300.ogg",length=1.6108843537414965},}
 L["but now it hurts here"]={{path="chatsounds/autoadd/thelewa/but now it hurts here.ogg",length=0.91138321995464855},}
 L["but very hard for my own standards"]={{path="chatsounds/autoadd/thelewa/but very hard for my own standards.ogg",length=2.3727891156462584},}
 L["but ye i gtg now"]={{path="chatsounds/autoadd/thelewa/but ye i gtg now.ogg",length=1.3365986394557823},}
+L["buy a wacom bamboo"]={{path="chatsounds/autoadd/thelewa/buy a wacom bamboo.ogg",length=1.4236734693877551},}
+L["buy one of these badboys"]={{path="chatsounds/autoadd/thelewa/buy one of these badboys.ogg",length=1.4889795918367348},}
+L["can i play eighto i cant play eighto"]={{path="chatsounds/autoadd/thelewa/can i play eighto i cant play eighto.ogg",length=2.1434920634920633},}
+L["cloudsmasher"]={{path="chatsounds/autoadd/thelewa/cloudsmasher/cloudsmasher.ogg",length=0.86349206349206353},{path="chatsounds/autoadd/thelewa/cloudsmasher/cloudsmasher2.ogg",length=1.0260317460317461},{path="chatsounds/autoadd/thelewa/cloudsmasher/cloudsmasher3.ogg",length=1.9765986394557824},{path="chatsounds/autoadd/thelewa/cloudsmasher/cloudsmasher4.ogg",length=1.1290702947845805},}
 L["cocks"]={{path="chatsounds/autoadd/thelewa/cocks.ogg",length=0.71981859410430837},}
 L["definatly"]={{path="chatsounds/autoadd/thelewa/definatly.ogg",length=0.91573696145124717},}
+L["dont get mad over it its just a one second timeout"]={{path="chatsounds/autoadd/thelewa/dont get mad over it its just a one second timeout.ogg",length=2.9112018140589568},}
+L["dont get mad over it"]={{path="chatsounds/autoadd/thelewa/dont get mad over it.ogg",length=1.1334240362811792},}
 L["dunno"]={{path="chatsounds/autoadd/thelewa/dunno.ogg",length=0.74303854875283448},}
 L["especially my middle finger"]={{path="chatsounds/autoadd/thelewa/especially my middle finger.ogg",length=1.2974149659863945},}
+L["everybody knows how this map goes"]={{path="chatsounds/autoadd/thelewa/everybody knows how this map goes.ogg",length=1.3032199546485261},}
+L["fantastic"]={{path="chatsounds/autoadd/thelewa/fantastic/fantastic.ogg",length=0.6820861678004535},{path="chatsounds/autoadd/thelewa/fantastic/fantastic1.ogg",length=1.0681179138321995},{path="chatsounds/autoadd/thelewa/fantastic/fantastic2.ogg",length=0.80108843537414964},{path="chatsounds/autoadd/thelewa/fantastic/fantastic3.ogg",length=0.77351473922902492},}
+L["first and only time"]={{path="chatsounds/autoadd/thelewa/first and only time.ogg",length=1.4556009070294784},}
 L["first"]={{path="chatsounds/autoadd/thelewa/first.ogg",length=0.79673469387755103},}
+L["get one of these pens too"]={{path="chatsounds/autoadd/thelewa/get one of these pens too.ogg",length=1.3220861678004534},}
 L["good bye"]={{path="chatsounds/autoadd/thelewa/good bye.ogg",length=0.74448979591836739},}
+L["ha hah fantastic"]={{path="chatsounds/autoadd/thelewa/ha hah fantastic.ogg",length=1.629750566893424},}
+L["ha hah"]={{path="chatsounds/autoadd/thelewa/ha hah.ogg",length=0.7691609977324263},}
+L["haha you havent seen my hat"]={{path="chatsounds/autoadd/thelewa/haha you havent seen my hat.ogg",length=1.931609977324263},}
+L["hardrock is really not my specialty"]={{path="chatsounds/autoadd/thelewa/hardrock is really not my specialty.ogg",length=1.7458503401360543},}
 L["have a nice night whatever"]={{path="chatsounds/autoadd/thelewa/have a nice night whatever.ogg",length=1.4338321995464853},}
 L["have a nice night"]={{path="chatsounds/autoadd/thelewa/have a nice night.ogg",length=0.74448979591836739},}
+L["help"]={{path="chatsounds/autoadd/thelewa/help.ogg",length=0.44553287981859413},}
+L["here it goes"]={{path="chatsounds/autoadd/thelewa/here it goes.ogg",length=0.49197278911564624},}
+L["hitlers moustache"]={{path="chatsounds/autoadd/thelewa/hitlers moustache.ogg",length=0.84752834467120186},}
 L["i agree with you"]={{path="chatsounds/autoadd/thelewa/i agree with you.ogg",length=1.0579591836734694},}
+L["i dont really understand people that get mad over 1 second timeouts"]={{path="chatsounds/autoadd/thelewa/i dont really understand people that get mad over 1 second timeouts.ogg",length=3.4263945578231292},}
 L["i dont wanna try that"]={{path="chatsounds/autoadd/thelewa/i dont wanna try that.ogg",length=0.93315192743764175},}
 L["i gotta give this another try"]={{path="chatsounds/autoadd/thelewa/i gotta give this another try.ogg",length=1.545578231292517},}
 L["i have to get that"]={{path="chatsounds/autoadd/thelewa/i have to get that.ogg",length=1.3946485260770976},}
+L["i mean check this out"]={{path="chatsounds/autoadd/thelewa/i mean check this out.ogg",length=0.81414965986394561},}
 L["i mean ehh"]={{path="chatsounds/autoadd/thelewa/i mean ehh.ogg",length=1.6166893424036282},}
+L["i mean"]={{path="chatsounds/autoadd/thelewa/i mean.ogg",length=0.44988662131519275},}
 L["i really love"]={{path="chatsounds/autoadd/thelewa/i really love.ogg",length=1.1073015873015872},}
 L["i think its all that all that very very hard single tapping that i have done"]={{path="chatsounds/autoadd/thelewa/i think its all that all that very very hard single tapping that i have done.ogg",length=4.6149659863945578},}
+L["i was pressing the wrong button"]={{path="chatsounds/autoadd/thelewa/i was pressing the wrong button.ogg",length=2.0680272108843538},}
+L["if thats not fantastic i dont know what is"]={{path="chatsounds/autoadd/thelewa/if thats not fantastic i dont know what is.ogg",length=2.1696145124716555},}
+L["im all real sweaty from playing that thats how intense it was"]={{path="chatsounds/autoadd/thelewa/im all real sweaty from playing that thats how intense it was.ogg",length=2.8487981859410429},}
 L["im gonna finnish up the stream"]={{path="chatsounds/autoadd/thelewa/im gonna finnish up the stream.ogg",length=1.5920181405895693},}
+L["im gonna try alternating"]={{path="chatsounds/autoadd/thelewa/im gonna try alternating.ogg",length=1.4236734693877551},}
+L["im gonna try this again"]={{path="chatsounds/autoadd/thelewa/im gonna try this again.ogg",length=0.92009070294784578},}
+L["im just terrible at the game"]={{path="chatsounds/autoadd/thelewa/im just terrible at the game.ogg",length=1.1174603174603175},}
 L["im not even gonna try that"]={{path="chatsounds/autoadd/thelewa/im not even gonna try that.ogg",length=1.3815873015873017},}
 L["im not im not really concerned about my knuckles"]={{path="chatsounds/autoadd/thelewa/im not im not really concerned about my knuckles.ogg",length=2.4279365079365078},}
+L["is utter shit"]={{path="chatsounds/autoadd/thelewa/is utter shit.ogg",length=1.2175963718820861},}
+L["it is bad"]={{path="chatsounds/autoadd/thelewa/it is bad.ogg",length=0.88235827664399091},}
 L["it is so touching"]={{path="chatsounds/autoadd/thelewa/it is so touching.ogg",length=0.92009070294784578},}
 L["it is the most touching"]={{path="chatsounds/autoadd/thelewa/it is the most touching.ogg",length=2.0012698412698411},}
 L["it saddens me but it is time to part"]={{path="chatsounds/autoadd/thelewa/it saddens me but it is time to part.ogg",length=2.3756916099773244},}
+L["its got triples"]={{path="chatsounds/autoadd/thelewa/its got triples.ogg",length=1.2654875283446712},}
 L["its in wait actually it is in"]={{path="chatsounds/autoadd/thelewa/its in wait actually it is in.ogg",length=1.5281632653061223},}
+L["its just a one second timeout"]={{path="chatsounds/autoadd/thelewa/its just a one second timeout.ogg",length=1.7806802721088435},}
+L["its like a fucking square"]={{path="chatsounds/autoadd/thelewa/its like a fucking square.ogg",length=1.6413605442176871},}
 L["its not in 2 hours you fool"]={{path="chatsounds/autoadd/thelewa/its not in 2 hours you fool.ogg",length=2.0360997732426305},}
 L["its the most touching storyboard i ever played"]={{path="chatsounds/autoadd/thelewa/its the most touching storyboard i ever played.ogg",length=3.0156916099773241},}
+L["jesus christ man"]={{path="chatsounds/autoadd/thelewa/jesus christ man.ogg",length=1.0840816326530611},}
+L["jesus christ"]={{path="chatsounds/autoadd/thelewa/jesus christ.ogg",length=0.75755102040816324},}
+L["lewa annoyed"]={{path="chatsounds/autoadd/thelewa/lewa annoyed/lewa annoyed.ogg",length=0.78512471655328797},{path="chatsounds/autoadd/thelewa/lewa annoyed/lewa annoyed2.ogg",length=1.0086167800453514},{path="chatsounds/autoadd/thelewa/lewa annoyed/lewa annoyed3.ogg",length=0.82140589569160993},}
 L["lewa crying"]={{path="chatsounds/autoadd/thelewa/lewa crying.ogg",length=5.662766439909297},}
-L["lewa singing"]={{path="chatsounds/autoadd/thelewa/lewa singing.ogg",length=1.9983673469387755},}
+L["lewa no"]={{path="chatsounds/autoadd/thelewa/lewa no/lewa no.ogg",length=0.67918367346938779},{path="chatsounds/autoadd/thelewa/lewa no/lewa no1.ogg",length=0.50938775510204082},}
+L["lewa noo"]={{path="chatsounds/autoadd/thelewa/lewa noo.ogg",length=1.0173242630385488},}
+L["lewa nooo"]={{path="chatsounds/autoadd/thelewa/lewa nooo.ogg",length=1.3656235827664398},}
+L["lewa singing"]={{path="chatsounds/autoadd/thelewa/lewa singing/lewa singing.ogg",length=1.9983673469387755},{path="chatsounds/autoadd/thelewa/lewa singing/lewa singing2.ogg",length=2.5759637188208617},}
+L["lewa singing like a pro"]={{path="chatsounds/autoadd/thelewa/lewa singing like a pro.ogg",length=4.0330158730158727},}
+L["lewa wubs"]={{path="chatsounds/autoadd/thelewa/lewa wubs.ogg",length=3.1651700680272108},}
+L["mhm yeh"]={{path="chatsounds/autoadd/thelewa/mhm yeh.ogg",length=1.545578231292517},}
+L["mm"]={{path="chatsounds/autoadd/thelewa/mm.ogg",length=0.64580498866213154},}
 L["my knuckles are really hurtin"]={{path="chatsounds/autoadd/thelewa/my knuckles are really hurtin.ogg",length=1.4715646258503401},}
 L["my knuckles were just fine"]={{path="chatsounds/autoadd/thelewa/my knuckles were just fine.ogg",length=1.0565079365079366},}
 L["neh noes"]={{path="chatsounds/autoadd/thelewa/neh noes.ogg",length=1.5934693877551021},}
+L["nope i dont wanna play that"]={{path="chatsounds/autoadd/thelewa/nope i dont wanna play that.ogg",length=1.2292063492063492},}
+L["not even mad"]={{path="chatsounds/autoadd/thelewa/not even mad.ogg",length=0.88235827664399091},}
+L["obviously"]={{path="chatsounds/autoadd/thelewa/obviously.ogg",length=0.64870748299319725},}
+L["oh boy this is such a good map"]={{path="chatsounds/autoadd/thelewa/oh boy this is such a good map.ogg",length=1.7806802721088435},}
 L["oh my knuckles"]={{path="chatsounds/autoadd/thelewa/oh my knuckles.ogg",length=0.93605442176870746},}
+L["oh shit"]={{path="chatsounds/autoadd/thelewa/oh shit.ogg",length=0.75174603174603172},}
+L["oh this guys in heaven right now"]={{path="chatsounds/autoadd/thelewa/oh this guys in heaven right now.ogg",length=1.4570521541950114},}
+L["oh yeah i need to show you guys the score i got last night"]={{path="chatsounds/autoadd/thelewa/oh yeah i need to show you guys the score i got last night.ogg",length=2.3902040816326529},}
 L["require ridiculous amount of aim"]={{path="chatsounds/autoadd/thelewa/require ridiculous amount of aim.ogg",length=1.8300226757369615},}
+L["so whats going on in the chat"]={{path="chatsounds/autoadd/thelewa/so whats going on in the chat.ogg",length=2.0012698412698411},}
+L["ss"]={{path="chatsounds/autoadd/thelewa/ss.ogg",length=0.50938775510204082},}
+L["that accuracy"]={{path="chatsounds/autoadd/thelewa/that accuracy.ogg",length=1.0492517006802722},}
+L["that part is the hardest part of that map"]={{path="chatsounds/autoadd/thelewa/that part is the hardest part of that map.ogg",length=2.1696145124716555},}
 L["that stream was ridiculous"]={{path="chatsounds/autoadd/thelewa/that stream was ridiculous.ogg",length=1.9112925170068027},}
+L["that was almost as beautiful as last time"]={{path="chatsounds/autoadd/thelewa/that was almost as beautiful as last time.ogg",length=2.0012698412698411},}
+L["that was intense"]={{path="chatsounds/autoadd/thelewa/that was intense.ogg",length=1.0071655328798186},}
+L["that was really bad that was a terrible display"]={{path="chatsounds/autoadd/thelewa/that was really bad that was a terrible display.ogg",length=4.4727437641723355},}
+L["that was very skilled"]={{path="chatsounds/autoadd/thelewa/that was very skilled.ogg",length=1.2930612244897959},}
+L["that went really well actually"]={{path="chatsounds/autoadd/thelewa/that went really well actually.ogg",length=1.2292063492063492},}
+L["thats gay"]={{path="chatsounds/autoadd/thelewa/thats gay.ogg",length=0.57904761904761903},}
+L["thats it thats as far i can get"]={{path="chatsounds/autoadd/thelewa/thats it thats as far i can get.ogg",length=2.0839909297052155},}
+L["thats it"]={{path="chatsounds/autoadd/thelewa/thats it.ogg",length=0.44408163265306122},}
+L["thats not a suprise"]={{path="chatsounds/autoadd/thelewa/thats not a suprise.ogg",length=1.062312925170068},}
+L["the pen weights like 100kg"]={{path="chatsounds/autoadd/thelewa/the pen weights like 100kg.ogg",length=2.6093424036281179},}
+L["the streams are real"]={{path="chatsounds/autoadd/thelewa/the streams are real.ogg",length=1.3467573696145125},}
 L["the tears check out the tears"]={{path="chatsounds/autoadd/thelewa/the tears check out the tears.ogg",length=2.2712018140589567},}
 L["the tears"]={{path="chatsounds/autoadd/thelewa/the tears.ogg",length=0.65596371882086169},}
+L["this accuracy is so orgasmic"]={{path="chatsounds/autoadd/thelewa/this accuracy is so orgasmic.ogg",length=1.8300226757369615},}
+L["this is not doable"]={{path="chatsounds/autoadd/thelewa/this is not doable.ogg",length=0.9026757369614512},}
+L["this is really hard"]={{path="chatsounds/autoadd/thelewa/this is really hard.ogg",length=1.1522902494331067},}
+L["this is the best idea ever"]={{path="chatsounds/autoadd/thelewa/this is the best idea ever.ogg",length=2.01578231292517},}
 L["this seems very hard"]={{path="chatsounds/autoadd/thelewa/this seems very hard.ogg",length=1.242267573696145},}
+L["too hard"]={{path="chatsounds/autoadd/thelewa/too hard.ogg",length=0.47746031746031747},}
 L["uhhh"]={{path="chatsounds/autoadd/thelewa/uhhh.ogg",length=0.82285714285714284},}
+L["was a masterpiece"]={{path="chatsounds/autoadd/thelewa/was a masterpiece.ogg",length=1.3395011337868481},}
 L["we do not disable anything"]={{path="chatsounds/autoadd/thelewa/we do not disable anything.ogg",length=1.5063945578231293},}
+L["well at least im not www"]={{path="chatsounds/autoadd/thelewa/well at least im not www.ogg",length=2.2363718820861678},}
 L["well see"]={{path="chatsounds/autoadd/thelewa/well see.ogg",length=0.74448979591836739},}
+L["were all jewish here"]={{path="chatsounds/autoadd/thelewa/were all jewish here.ogg",length=1.2350113378684808},}
+L["what a fgt"]={{path="chatsounds/autoadd/thelewa/what a fgt.ogg",length=0.67773242630385488},}
+L["what a terrible miss"]={{path="chatsounds/autoadd/thelewa/what a terrible miss.ogg",length=1.1334240362811792},}
+L["whatever its called"]={{path="chatsounds/autoadd/thelewa/whatever its called.ogg",length=1.1871201814058956},}
+L["whats going on"]={{path="chatsounds/autoadd/thelewa/whats going on.ogg",length=0.77931972789115644},}
+L["whatsgoing on haha"]={{path="chatsounds/autoadd/thelewa/whatsgoing on haha.ogg",length=1.3220861678004534},}
 L["when my hand was broken"]={{path="chatsounds/autoadd/thelewa/when my hand was broken.ogg",length=1.3714285714285714},}
+L["whyyy"]={{path="chatsounds/autoadd/thelewa/whyyy.ogg",length=0.98394557823129247},}
+L["woh"]={{path="chatsounds/autoadd/thelewa/woh.ogg",length=0.23219954648526078},}
+L["wonderful"]={{path="chatsounds/autoadd/thelewa/wonderful.ogg",length=0.44117913832199546},}
+L["wow this is really hard"]={{path="chatsounds/autoadd/thelewa/wow this is really hard.ogg",length=2.4061678004535145},}
+L["wow what a miss"]={{path="chatsounds/autoadd/thelewa/wow what a miss.ogg",length=0.6385487528344671},}
+L["wow"]={{path="chatsounds/autoadd/thelewa/wow.ogg",length=0.30621315192743764},}
+L["wuah"]={{path="chatsounds/autoadd/thelewa/wuah.ogg",length=0.37587301587301586},}
+L["yawn"]={{path="chatsounds/autoadd/thelewa/yawn.ogg",length=0.77931972789115644},}
+L["yeah lets try inbachi"]={{path="chatsounds/autoadd/thelewa/yeah lets try inbachi.ogg",length=1.5731519274376418},}
+L["yeh"]={{path="chatsounds/autoadd/thelewa/yeh/yeh.ogg",length=0.44698412698412698},{path="chatsounds/autoadd/thelewa/yeh/yeh2.ogg",length=0.49777777777777776},}
 c.EndList()
