b4c06ff84a7a3e0e954a85038d24a721457662cb
Suchipi Izumi <me@suchipi.com>
2014-08-05 20:32:56 +0000

    a surveillance camera!!!??!?!?

--- lua/chatsounds/lists_nosend/suchipi.lua
+++ lua/chatsounds/lists_nosend/suchipi.lua
@@ -1,5 +1,6 @@
 c.StartList("suchipi")
 L["i wanna take you for a ride"]={{path="chatsounds/autoadd/suchipi/I wanna take you for a ride.ogg",length=15.908639455782312},}
+L["a surveillance camera"]={{path="chatsounds/autoadd/suchipi/a surveillance camera.ogg",length=1.2326757369614512},}
 L["challenge everything"]={{path="chatsounds/autoadd/suchipi/challenge everything.ogg",length=1.2731972789115646},}
 L["cocainum"]={{path="chatsounds/autoadd/suchipi/cocainum.ogg",length=0.82643990929705213},}
 L["ea games"]={{path="chatsounds/autoadd/suchipi/ea games.ogg",length=0.96403628117913831},}
