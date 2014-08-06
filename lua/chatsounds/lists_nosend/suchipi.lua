e9f4c41bd57ebfede8e68fde651a0e2253c0b832
Suchipi Izumi <me@suchipi.com>
2014-08-06 20:47:15 +0000

    ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding yoooooooooooo ding ding ding

--- lua/chatsounds/lists_nosend/suchipi.lua
+++ lua/chatsounds/lists_nosend/suchipi.lua
@@ -3,7 +3,9 @@ L["i wanna take you for a ride"]={{path="chatsounds/autoadd/suchipi/I wanna take
 L["a surveillance camera"]={{path="chatsounds/autoadd/suchipi/a surveillance camera.ogg",length=1.2326757369614512},}
 L["challenge everything"]={{path="chatsounds/autoadd/suchipi/challenge everything.ogg",length=1.2731972789115646},}
 L["cocainum"]={{path="chatsounds/autoadd/suchipi/cocainum.ogg",length=0.82643990929705213},}
+L["ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding yoooooooooooo ding ding ding"]={{path="chatsounds/autoadd/suchipi/ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding yoooooooooooo ding ding ding.ogg",length=9.9476190476190478},}
 L["ea games"]={{path="chatsounds/autoadd/suchipi/ea games.ogg",length=0.96403628117913831},}
+L["gamecubestartup"]={{path="chatsounds/autoadd/suchipi/gamecubestartup/1.ogg",length=4.8181859410430841},{path="chatsounds/autoadd/suchipi/gamecubestartup/2.ogg",length=5.0757369614512475},{path="chatsounds/autoadd/suchipi/gamecubestartup/3.ogg",length=5.5909297052154194},}
 L["swooce1"]={{path="chatsounds/autoadd/suchipi/swooce1.ogg",length=0.66229024943310655},}
 L["swooce2"]={{path="chatsounds/autoadd/suchipi/swooce2.ogg",length=0.67478458049886625},}
 L["watch me swooce right in"]={{path="chatsounds/autoadd/suchipi/watch me swooce right in.ogg",length=1.8993877551020408},}
