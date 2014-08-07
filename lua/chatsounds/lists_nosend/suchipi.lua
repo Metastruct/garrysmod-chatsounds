48ad29ec21d6ab45db08b1f5f78b8527abdeb5bd
Suchipi Izumi <me@suchipi.com>
2014-08-07 00:15:51 +0000

    one iota

--- lua/chatsounds/lists_nosend/suchipi.lua
+++ lua/chatsounds/lists_nosend/suchipi.lua
@@ -6,6 +6,7 @@ L["cocainum"]={{path="chatsounds/autoadd/suchipi/cocainum.ogg",length=0.82643990
 L["ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding yoooooooooooo ding ding ding"]={{path="chatsounds/autoadd/suchipi/ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding yoooooooooooo ding ding ding.ogg",length=9.9476190476190478},}
 L["ea games"]={{path="chatsounds/autoadd/suchipi/ea games.ogg",length=0.96403628117913831},}
 L["gamecubestartup"]={{path="chatsounds/autoadd/suchipi/gamecubestartup/1.ogg",length=4.8181859410430841},{path="chatsounds/autoadd/suchipi/gamecubestartup/2.ogg",length=5.0757369614512475},{path="chatsounds/autoadd/suchipi/gamecubestartup/3.ogg",length=5.5909297052154194},}
+L["llleeetttmmmeeegggeeetttaaalllooooookkkaaatttyyyooouuummmaaannn"]={{path="chatsounds/autoadd/suchipi/llleeetttmmmeeegggeeetttaaalllooooookkkaaatttyyyooouuummmaaannn.ogg",length=4.411428571428571},}
 L["swooce1"]={{path="chatsounds/autoadd/suchipi/swooce1.ogg",length=0.66229024943310655},}
 L["swooce2"]={{path="chatsounds/autoadd/suchipi/swooce2.ogg",length=0.67478458049886625},}
 L["watch me swooce right in"]={{path="chatsounds/autoadd/suchipi/watch me swooce right in.ogg",length=1.8993877551020408},}
