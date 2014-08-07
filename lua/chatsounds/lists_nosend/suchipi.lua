dc107577a60408e664936f3306a9da6b79829583
verbalsilence@hotmail.com <verbalsilence@hotmail.com>
2014-08-07 08:55:26 +0000

    enderbro sounds

--- lua/chatsounds/lists_nosend/suchipi.lua
+++ lua/chatsounds/lists_nosend/suchipi.lua
@@ -4,7 +4,7 @@ L["a surveillance camera"]={{path="chatsounds/autoadd/suchipi/a surveillance cam
 L["challenge everything"]={{path="chatsounds/autoadd/suchipi/challenge everything.ogg",length=1.2731972789115646},}
 L["cocainum"]={{path="chatsounds/autoadd/suchipi/cocainum.ogg",length=0.82643990929705213},}
 L["did you rike it"]={{path="chatsounds/autoadd/suchipi/did you rike it.ogg",length=0.68167800453514737},}
-L["ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding yoooooooooooo ding ding ding"]={{path="chatsounds/autoadd/suchipi/ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding ding yoooooooooooo ding ding ding.ogg",length=9.9476190476190478},}
+L["dingdingding yooo ding ding ding"]={{path="chatsounds/autoadd/suchipi/dingdingding yooo ding ding ding.ogg",length=9.9476190476190478},}
 L["ea games"]={{path="chatsounds/autoadd/suchipi/ea games.ogg",length=0.96403628117913831},}
 L["gamecubestartup"]={{path="chatsounds/autoadd/suchipi/gamecubestartup/1.ogg",length=4.8181859410430841},{path="chatsounds/autoadd/suchipi/gamecubestartup/2.ogg",length=5.0757369614512475},{path="chatsounds/autoadd/suchipi/gamecubestartup/3.ogg",length=5.5909297052154194},}
 L["llleeetttmmmeeegggeeetttaaalllooooookkkaaatttyyyooouuummmaaannn"]={{path="chatsounds/autoadd/suchipi/llleeetttmmmeeegggeeetttaaalllooooookkkaaatttyyyooouuummmaaannn.ogg",length=4.411428571428571},}
