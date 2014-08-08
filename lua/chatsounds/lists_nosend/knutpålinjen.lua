7eca3b7a8299bad0af56a0c69d65a633b79ef11a
mulletbull@gmail.com <mulletbull@gmail.com>
2014-08-08 08:18:16 +0000

    well that was a pain in the ass to translate

---
+++ lua/chatsounds/lists_nosend/knutpålinjen.lua
@@ -0,0 +1,56 @@
+c.StartList("knutpålinjen")
+L["abababa suzenabajanda bubrunbrunbrun abarazeezeezee"]={{path="chatsounds/autoadd/knutpålinjen/abababa suzenabajanda bubrunbrunbrun abarazeezeezee.ogg",length=3.492766439909297},}
+L["aknepasta"]={{path="chatsounds/autoadd/knutpålinjen/aknepasta.ogg",length=1.0509297052154194},}
+L["angloasi bye bye"]={{path="chatsounds/autoadd/knutpålinjen/angloasi bye bye.ogg",length=1.9015873015873015},}
+L["baaahzuraba sheesala bastardkazoda jocka nisse bussah"]={{path="chatsounds/autoadd/knutpålinjen/baaahzuraba sheesala bastardkazoda jocka nisse bussah.ogg",length=4.6704081632653063},}
+L["brrrantjababa zunta zunta zunta pffft"]={{path="chatsounds/autoadd/knutpålinjen/brrrantjababa zunta zunta zunta pffft.ogg",length=3.5025170068027212},}
+L["bye bye"]={{path="chatsounds/autoadd/knutpålinjen/bye bye.ogg",length=0.79278911564625854},}
+L["eeh zee sasa joseph avayendasah ahaaa"]={{path="chatsounds/autoadd/knutpålinjen/eeh zee sasa joseph avayendasah ahaaa.ogg",length=2.3669841269841272},}
+L["el muerte"]={{path="chatsounds/autoadd/knutpålinjen/el muerte.ogg",length=0.85802721088435374},}
+L["elaspita delararerekelopa ela pontapontaponta jutsu ela grasslaba dodo engranda ettabossa"]={{path="chatsounds/autoadd/knutpålinjen/elaspita delararerekelopa ela pontapontaponta jutsu ela grasslaba dodo engranda ettabossa.ogg",length=6.0842630385487526},}
+L["hagasaka"]={{path="chatsounds/autoadd/knutpålinjen/hagasaka.ogg",length=0.79360544217687079},}
+L["havajana protactjone de fettochini"]={{path="chatsounds/autoadd/knutpålinjen/havajana protactjone de fettochini.ogg",length=2.9023129251700679},}
+L["hmmm hmmm hm"]={{path="chatsounds/autoadd/knutpålinjen/hmmm hmmm hm.ogg",length=4.1662811791383216},}
+L["hmmm hmmm"]={{path="chatsounds/autoadd/knutpålinjen/hmmm hmmm.ogg",length=4.2678004535147389},}
+L["hohohaha"]={{path="chatsounds/autoadd/knutpålinjen/hohohaha.ogg",length=0.74238095238095236},}
+L["hoohoo"]={{path="chatsounds/autoadd/knutpålinjen/hoohoo.ogg",length=0.55068027210884352},}
+L["hoonononononononononononononoh"]={{path="chatsounds/autoadd/knutpålinjen/hoonononononononononononononoh.ogg",length=1.7469614512471656},}
+L["jaja"]={{path="chatsounds/autoadd/knutpålinjen/jaja.ogg",length=0.70047619047619047},}
+L["jajajajaja"]={{path="chatsounds/autoadd/knutpålinjen/jajajajaja.ogg",length=1.9939909297052154},}
+L["jombada baahaa grandiose musica"]={{path="chatsounds/autoadd/knutpålinjen/jombada baahaa grandiose musica.ogg",length=2.6444217687074829},}
+L["juubadudalaba zehjazuhzugeegee"]={{path="chatsounds/autoadd/knutpålinjen/juubadudalaba zehjazuhzugeegee.ogg",length=2.0495918367346939},}
+L["knut aha1"]={{path="chatsounds/autoadd/knutpålinjen/knut aha1.ogg",length=1.3902947845804989},}
+L["knut aha2"]={{path="chatsounds/autoadd/knutpålinjen/knut aha2.ogg",length=2.3449886621315192},}
+L["knut aha3"]={{path="chatsounds/autoadd/knutpålinjen/knut aha3.ogg",length=0.61473922902494327},}
+L["knut aha4"]={{path="chatsounds/autoadd/knutpålinjen/knut aha4.ogg",length=1.5173015873015874},}
+L["knut aha5"]={{path="chatsounds/autoadd/knutpålinjen/knut aha5.ogg",length=1.2441496598639457},}
+L["knut aha6"]={{path="chatsounds/autoadd/knutpålinjen/knut aha6.ogg",length=1.1704081632653061},}
+L["knut laugh1"]={{path="chatsounds/autoadd/knutpålinjen/knut laugh1.ogg",length=0.93845804988662129},}
+L["knut laugh2"]={{path="chatsounds/autoadd/knutpålinjen/knut laugh2.ogg",length=3.1106122448979594},}
+L["knut laugh3"]={{path="chatsounds/autoadd/knutpålinjen/knut laugh3.ogg",length=1.4285034013605442},}
+L["knut laugh4"]={{path="chatsounds/autoadd/knutpålinjen/knut laugh4.ogg",length=1.3985487528344671},}
+L["knut laugh5"]={{path="chatsounds/autoadd/knutpålinjen/knut laugh5.ogg",length=1.6315873015873017},}
+L["knut pffft1"]={{path="chatsounds/autoadd/knutpålinjen/knut pffft1.ogg",length=1.1002267573696145},}
+L["knut pffft2"]={{path="chatsounds/autoadd/knutpålinjen/knut pffft2.ogg",length=1.7617006802721089},}
+L["knut pffft3"]={{path="chatsounds/autoadd/knutpålinjen/knut pffft3.ogg",length=1.1352607709750566},}
+L["knut pffft4"]={{path="chatsounds/autoadd/knutpålinjen/knut pffft4.ogg",length=1.2715646258503401},}
+L["knut pffft5"]={{path="chatsounds/autoadd/knutpålinjen/knut pffft5.ogg",length=0.76451247165532876},}
+L["lagrande skott i rumpa"]={{path="chatsounds/autoadd/knutpålinjen/lagrande skott i rumpa.ogg",length=1.9898639455782312},}
+L["macho gub budelanaamacho ra bazeezaba yeeee buzokomonzenezerababara buuuu menerabagh"]={{path="chatsounds/autoadd/knutpålinjen/macho gub budelanaamacho ra bazeezaba yeeee buzokomonzenezerababara buuuu menerabagh.ogg",length=6.1216326530612246},}
+L["myyy myyy myyy delijah"]={{path="chatsounds/autoadd/knutpålinjen/myyy myyy myyy delijah.ogg",length=3.8258049886621315},}
+L["myyy myyy myyy delijakapffft pfft"]={{path="chatsounds/autoadd/knutpålinjen/myyy myyy myyy delijakapffft pfft.ogg",length=6.683741496598639},}
+L["nnnuagh fy bubblan"]={{path="chatsounds/autoadd/knutpålinjen/nnnuagh fy bubblan.ogg",length=2.9589342403628116},}
+L["nuaghnahsuahsaaahaaa"]={{path="chatsounds/autoadd/knutpålinjen/nuaghnahsuahsaaahaaa.ogg",length=3.3991383219954647},}
+L["pffft buah pah eeeow"]={{path="chatsounds/autoadd/knutpålinjen/pffft buah pah eeeow.ogg",length=2.4450113378684808},}
+L["pffft pfft"]={{path="chatsounds/autoadd/knutpålinjen/pffft pfft.ogg",length=3.6317913832199546},}
+L["pft yeeetsa"]={{path="chatsounds/autoadd/knutpålinjen/pft yeeetsa.ogg",length=1.819047619047619},}
+L["pft"]={{path="chatsounds/autoadd/knutpålinjen/pft.ogg",length=0.34217687074829933},}
+L["prostata ingladera pft pft pft aaahehahaha"]={{path="chatsounds/autoadd/knutpålinjen/prostata ingladera pft pft pft aaahehahaha.ogg",length=3.6949659863945579},}
+L["rrranzu zurabazaza"]={{path="chatsounds/autoadd/knutpålinjen/rrranzu zurabazaza.ogg",length=2.6044897959183673},}
+L["satan pissigubbi"]={{path="chatsounds/autoadd/knutpålinjen/satan pissigubbi.ogg",length=2.0110884353741496},}
+L["solarium mellanazee protekta"]={{path="chatsounds/autoadd/knutpålinjen/solarium mellanazee protekta.ogg",length=2.3029251700680273},}
+L["whooo"]={{path="chatsounds/autoadd/knutpålinjen/whooo.ogg",length=0.57108843537414966},}
+L["yee abadoda yoo abadoda yooh hoppsan yooh hoppsan yee sakada yee sakada yee hahahahahahahahahahaha haha"]={{path="chatsounds/autoadd/knutpålinjen/yee abadoda yoo abadoda yooh hoppsan yooh hoppsan yee sakada yee sakada yee hahahahahahahahahahaha haha.ogg",length=10.211473922902494},}
+L["yoohoo"]={{path="chatsounds/autoadd/knutpålinjen/yoohoo.ogg",length=0.7111337868480726},}
+L["zabazibazaazoo zabazibazaazoo zabazibazaazoo zabazibazaazoo"]={{path="chatsounds/autoadd/knutpålinjen/zabazibazaazoo zabazibazaazoo zabazibazaazoo zabazibazaazoo.ogg",length=3.1455782312925171},}
+c.EndList()
