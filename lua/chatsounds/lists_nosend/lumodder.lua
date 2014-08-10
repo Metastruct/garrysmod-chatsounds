e3f1965ce2b8c881aab69bfe9d10908056d38305
LUModder@gmail.com <lumodder@gmail.com>
2014-08-10 20:01:27 +0000

    Added 2 dubstep related chatsounds (just lyrical parts)

--- lua/chatsounds/lists_nosend/lumodder.lua
+++ lua/chatsounds/lists_nosend/lumodder.lua
@@ -3,4 +3,6 @@ L["suck my balls doc"]={{path="chatsounds/autoadd/lumodder/suck my balls doc.ogg
 L["thats racist"]={{path="chatsounds/autoadd/lumodder/thats racist.ogg",length=1.1724263038548752},}
 L["trainrain"]={{path="chatsounds/autoadd/lumodder/trainrain.ogg",length=3.0040816326530613},}
 L["whos been drawing dicks"]={{path="chatsounds/autoadd/lumodder/whos been drawing dicks.ogg",length=1.5914739229024943},}
+L["triple x rated"]={{path="chatsounds/autoadd/lumodder/triple x rated.ogg",length=3.430880},}
+L["but my hope will never die"]={{path="chatsounds/autoadd/lumodder/but my hope will never die.ogg",length=1.558180},}
 c.EndList()
