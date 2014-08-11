40aeb8cd5ecf52ae4b842f41cb93253e58d19ec9
mulletbull@gmail.com <mulletbull@gmail.com>
2014-08-11 15:22:20 +0000

    requested by ryokyon (ps. someone make him a committer so i don't have to upload all of his shit please)

---
+++ lua/chatsounds/lists_nosend/AOTAbridged.lua
@@ -0,0 +1,67 @@
+c.StartList("AOTAbridged")
+L["alright arian coconut whats your name"]={{path="chatsounds/autoadd/AOTAbridged/alright arian coconut whats your name.ogg",length=2.8550113378684809},}
+L["alright you poop nobblers"]={{path="chatsounds/autoadd/AOTAbridged/alright you poop nobblers.ogg",length=2.3580045351473924},}
+L["and it was awkward for everyone"]={{path="chatsounds/autoadd/AOTAbridged/and it was awkward for everyone.ogg",length=3.1429931972789116},}
+L["and you know what happened"]={{path="chatsounds/autoadd/AOTAbridged/and you know what happened.ogg",length=4.0739909297052153},}
+L["are you being sarcastic"]={{path="chatsounds/autoadd/AOTAbridged/are you being sarcastic.ogg",length=3.1270068027210884},}
+L["arrrarrar"]={{path="chatsounds/autoadd/AOTAbridged/arrrarrar.ogg",length=1.2629931972789115},}
+L["balls"]={{path="chatsounds/autoadd/AOTAbridged/balls.ogg",length=0.6729931972789116},}
+L["bitchface mcgee"]={{path="chatsounds/autoadd/AOTAbridged/bitchface mcgee.ogg",length=2.1329931972789113},}
+L["connie laugh"]={{path="chatsounds/autoadd/AOTAbridged/connie laugh.ogg",length=1.4750113378684808},}
+L["crying"]={{path="chatsounds/autoadd/AOTAbridged/crying.ogg",length=3.689002267573696},}
+L["eh"]={{path="chatsounds/autoadd/AOTAbridged/eh.ogg",length=0.36571428571428571},}
+L["ereeeen"]={{path="chatsounds/autoadd/AOTAbridged/ereeeen.ogg",length=2.1170068027210887},}
+L["eren did you kill the colossal titan"]={{path="chatsounds/autoadd/AOTAbridged/eren did you kill the colossal titan.ogg",length=3.4319954648526076},}
+L["eren i love you"]={{path="chatsounds/autoadd/AOTAbridged/eren i love you.ogg",length=4.3300000000000001},}
+L["god dont make it gay armin"]={{path="chatsounds/autoadd/AOTAbridged/god dont make it gay armin.ogg",length=1.3069841269841269},}
+L["god dont make it gay mikasa"]={{path="chatsounds/autoadd/AOTAbridged/god dont make it gay mikasa.ogg",length=1.2109977324263039},}
+L["god help us"]={{path="chatsounds/autoadd/AOTAbridged/god help us.ogg",length=3.8490022675736961},}
+L["going to fuck to sleep"]={{path="chatsounds/autoadd/AOTAbridged/going to fuck to sleep.ogg",length=5.5650113378684809},}
+L["harmonize with me maggot"]={{path="chatsounds/autoadd/AOTAbridged/harmonize with me maggot.ogg",length=5.1439909297052155},}
+L["hes got a good point gibberish"]={{path="chatsounds/autoadd/AOTAbridged/hes got a good point gibberish.ogg",length=6.1900000000000004},}
+L["heugh"]={{path="chatsounds/autoadd/AOTAbridged/heugh.ogg",length=0.43301587301587302},}
+L["how about a nice slice of vengeance"]={{path="chatsounds/autoadd/AOTAbridged/how about a nice slice of vengeance.ogg",length=1.7639909297052154},}
+L["i fucking hate you thomas"]={{path="chatsounds/autoadd/AOTAbridged/i fucking hate you thomas.ogg",length=2.0370068027210886},}
+L["i had stupid dreams too"]={{path="chatsounds/autoadd/AOTAbridged/i had stupid dreams too.ogg",length=3.873356009070295},}
+L["i have the most awkward boner"]={{path="chatsounds/autoadd/AOTAbridged/i have the most awkward boner.ogg",length=2.0370068027210886},}
+L["i think one got me"]={{path="chatsounds/autoadd/AOTAbridged/i think one got me.ogg",length=5.1799999999999997},}
+L["if you cant pull out a protractor from your weener slot and turn"]={{path="chatsounds/autoadd/AOTAbridged/if you cant pull out a protractor from your weener slot and turn.ogg",length=10.872993197278912},}
+L["im a best"]={{path="chatsounds/autoadd/AOTAbridged/im a best.ogg",length=2.8229931972789117},}
+L["im sick and tired of all this shit"]={{path="chatsounds/autoadd/AOTAbridged/im sick and tired of all this shit.ogg",length=2.0770068027210886},}
+L["jaegerbombastic"]={{path="chatsounds/autoadd/AOTAbridged/jaegerbombastic.ogg",length=1.8600000000000001},}
+L["just bertell you what i bertold him"]={{path="chatsounds/autoadd/AOTAbridged/just bertell you what i bertold him.ogg",length=5.7729931972789119},}
+L["listen up you double deckered pig fart"]={{path="chatsounds/autoadd/AOTAbridged/listen up you double deckered pig fart.ogg",length=4.6990022675736958},}
+L["look on the bright side"]={{path="chatsounds/autoadd/AOTAbridged/look on the bright side.ogg",length=2.5759863945578232},}
+L["nope nope nope nope nope"]={{path="chatsounds/autoadd/AOTAbridged/nope nope nope nope nope.ogg",length=1.8285714285714285},}
+L["not getting eaten by titans"]={{path="chatsounds/autoadd/AOTAbridged/not getting eaten by titans.ogg",length=2.4700226757369617},}
+L["oh my god eren your so fucking cool holy shit"]={{path="chatsounds/autoadd/AOTAbridged/oh my god eren your so fucking cool holy shit.ogg",length=2.6619954648526076},}
+L["oh my god jean you suck"]={{path="chatsounds/autoadd/AOTAbridged/oh my god jean you suck.ogg",length=1.5560090702947846},}
+L["oh my god jean your so fucking cool holy shit"]={{path="chatsounds/autoadd/AOTAbridged/oh my god jean your so fucking cool holy shit.ogg",length=2.6470068027210885},}
+L["oh shiiiiiiiiit"]={{path="chatsounds/autoadd/AOTAbridged/oh shiiiiiiiiit.ogg",length=4.9110204081632656},}
+L["ohh"]={{path="chatsounds/autoadd/AOTAbridged/ohh.ogg",length=0.54501133786848077},}
+L["pull your head out of your hamster hole"]={{path="chatsounds/autoadd/AOTAbridged/pull your head out of your hamster hole.ogg",length=8.1909977324263039},}
+L["stick a corn cob up a trainees bunghole"]={{path="chatsounds/autoadd/AOTAbridged/stick a corn cob up a trainees bunghole.ogg",length=6.2390022675736958},}
+L["that bread tasted awful"]={{path="chatsounds/autoadd/AOTAbridged/that bread tasted awful.ogg",length=1.6200000000000001},}
+L["that sounds awesome"]={{path="chatsounds/autoadd/AOTAbridged/that sounds awesome.ogg",length=1.01},}
+L["the fuck did you just say"]={{path="chatsounds/autoadd/AOTAbridged/the fuck did you just say.ogg",length=1.1229931972789116},}
+L["theres nothing we cant accomplish"]={{path="chatsounds/autoadd/AOTAbridged/theres nothing we cant accomplish.ogg",length=4.2219954648526077},}
+L["this is why i called you assholes"]={{path="chatsounds/autoadd/AOTAbridged/this is why i called you assholes.ogg",length=4.5714285714285712},}
+L["thomas you suck"]={{path="chatsounds/autoadd/AOTAbridged/thomas you suck.ogg",length=1.1709977324263039},}
+L["time for hannes"]={{path="chatsounds/autoadd/AOTAbridged/time for hannes.ogg",length=6.2432653061224492},}
+L["titaaaaans"]={{path="chatsounds/autoadd/AOTAbridged/titaaaaans.ogg",length=4.6670068027210885},}
+L["titan killing skills to kill actual titans"]={{path="chatsounds/autoadd/AOTAbridged/titan killing skills to kill actual titans.ogg",length=5.1350113378684803},}
+L["titans are my trigger"]={{path="chatsounds/autoadd/AOTAbridged/titans are my trigger.ogg",length=0.93000000000000005},}
+L["we should look on it"]={{path="chatsounds/autoadd/AOTAbridged/we should look on it.ogg",length=3.2799999999999998},}
+L["well join the military"]={{path="chatsounds/autoadd/AOTAbridged/well join the military.ogg",length=5.1160090702947842},}
+L["well sweet jolly fucking ranchers"]={{path="chatsounds/autoadd/AOTAbridged/well sweet jolly fucking ranchers.ogg",length=5.9819954648526075},}
+L["well sweet mother theresa on the hood of a mercedes benz"]={{path="chatsounds/autoadd/AOTAbridged/well sweet mother theresa on the hood of a mercedes benz.ogg",length=4.8390022675736963},}
+L["well sweet pacific rimjob"]={{path="chatsounds/autoadd/AOTAbridged/well sweet pacific rimjob.ogg",length=4.0899999999999999},}
+L["weve got plenty of openings in the custodial department"]={{path="chatsounds/autoadd/AOTAbridged/weve got plenty of openings in the custodial department.ogg",length=6.5109977324263042},}
+L["what bright side"]={{path="chatsounds/autoadd/AOTAbridged/what bright side.ogg",length=1.0419954648526077},}
+L["what the actual hell is that"]={{path="chatsounds/autoadd/AOTAbridged/what the actual hell is that.ogg",length=4.12734693877551},}
+L["why cant you be more like jaegerbombastic over here"]={{path="chatsounds/autoadd/AOTAbridged/why cant you be more like jaegerbombastic over here.ogg",length=5.5170068027210881},}
+L["why cant you be more like that majestic stallion jean"]={{path="chatsounds/autoadd/AOTAbridged/why cant you be more like that majestic stallion jean.ogg",length=6.2390022675736958},}
+L["you batch of meat tossers"]={{path="chatsounds/autoadd/AOTAbridged/you batch of meat tossers.ogg",length=5.6609977324263037},}
+L["you sound like a majestic fucking eagle"]={{path="chatsounds/autoadd/AOTAbridged/you sound like a majestic fucking eagle.ogg",length=3.5080045351473923},}
+L["you think this is harem of the dead you shrimp dick fuck nugget"]={{path="chatsounds/autoadd/AOTAbridged/you think this is harem of the dead you shrimp dick fuck nugget.ogg",length=4.7109977324263035},}
+c.EndList()
