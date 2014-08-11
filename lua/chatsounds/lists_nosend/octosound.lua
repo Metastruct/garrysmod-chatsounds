f8cd8e0aa659821bbf536e480e416f7d967d9985
Suchipi Izumi <me@suchipi.com>
2014-08-11 00:25:39 +0000

    proper titles for the octosounds (bighead -> oh no thats um uh a big head and its oh no i gotta get away it looks doesnt scary)

--- lua/chatsounds/lists_nosend/octosound.lua
+++ lua/chatsounds/lists_nosend/octosound.lua
@@ -1,48 +1,48 @@
 c.StartList("octosound")
 L["aaaapun"]={{path="chatsounds/autoadd/octosound/aaaapun.ogg",length=2.2035147392290249},}
-L["ahohno"]={{path="chatsounds/autoadd/octosound/ahohno.ogg",length=2.0657823129251702},}
-L["aholewhatsthat"]={{path="chatsounds/autoadd/octosound/aholewhatsthat.ogg",length=3.3397052154195013},}
-L["better try the other one here"]={{path="chatsounds/autoadd/octosound/better try the other one here.ogg",length=2.6166666666666667},}
-L["bighead"]={{path="chatsounds/autoadd/octosound/bighead.ogg",length=6.6105442176870746},}
+L["ah oh no hmm"]={{path="chatsounds/autoadd/octosound/ahohno.ogg",length=2.0657823129251702},}
+L["ah its a hole hm it oh no whats that"]={{path="chatsounds/autoadd/octosound/aholewhatsthat.ogg",length=3.3397052154195013},}
+L["id better try the other one here let me just take"]={{path="chatsounds/autoadd/octosound/better try the other one here.ogg",length=2.6166666666666667},}
+L["oh no thats um uh a big head and its oh no i gotta get away it looks doesnt scary"]={{path="chatsounds/autoadd/octosound/bighead.ogg",length=6.6105442176870746},}
 L["breathing"]={{path="chatsounds/autoadd/octosound/breathing.ogg",length=2.7199773242630387},}
-L["comebackparents"]={{path="chatsounds/autoadd/octosound/comebackparents.ogg",length=3.9250113378684808},}
+L["come back i miss you my parents"]={{path="chatsounds/autoadd/octosound/comebackparents.ogg",length=3.9250113378684808},}
 L["crying"]={{path="chatsounds/autoadd/octosound/crying.ogg",length=2.5822448979591837},}
-L["findingmyparents"]={{path="chatsounds/autoadd/octosound/findingmyparents.ogg",length=3.8905895691609977},}
-L["gettingbiggerohno"]={{path="chatsounds/autoadd/octosound/gettingbiggerohno.ogg",length=2.2723809523809524},}
-L["gosh"]={{path="chatsounds/autoadd/octosound/gosh.ogg",length=3.6495691609977325},}
+L["erm uh blurh finding my parents erm er"]={{path="chatsounds/autoadd/octosound/findingmyparents.ogg",length=3.8905895691609977},}
+L["ah its getting bigger and oh no"]={{path="chatsounds/autoadd/octosound/gettingbiggerohno.ogg",length=2.2723809523809524},}
+L["oh no gosh oh gosh nnrreeeh"]={{path="chatsounds/autoadd/octosound/gosh.ogg",length=3.6495691609977325},}
 L["growling"]={{path="chatsounds/autoadd/octosound/growling.ogg",length=2.1002267573696147},}
-L["havealookatthatplaceagain"]={{path="chatsounds/autoadd/octosound/havealookatthatplaceagain.ogg",length=4.3381632653061226},}
-L["hmmnicecupoftea"]={{path="chatsounds/autoadd/octosound/hmmnicecupoftea.ogg",length=2.1346485260770973},}
-L["hmmwhatsdownhere"]={{path="chatsounds/autoadd/octosound/hmmwhatsdownhere.ogg",length=2.6166666666666667},}
-L["hmmwhatsinthatboxbetteropen"]={{path="chatsounds/autoadd/octosound/hmmwhatsinthatboxbetteropen.ogg",length=2.0658049886621317},}
-L["idohaveparents"]={{path="chatsounds/autoadd/octosound/idohaveparents.ogg",length=12.42920634920635},}
-L["iloveyou"]={{path="chatsounds/autoadd/octosound/iloveyou.ogg",length=6.1973696145124713},}
-L["imgonnagetyou"]={{path="chatsounds/autoadd/octosound/imgonnagetyou.ogg",length=1.9280725623582766},}
-L["ireallymissmyparents"]={{path="chatsounds/autoadd/octosound/ireallymissmyparents.ogg",length=3.7184353741496601},}
-L["iwillfindmyparents"]={{path="chatsounds/autoadd/octosound/iwillfindmyparents.ogg",length=4.5447392290249429},}
-L["kindaweirdalittlebitdifferent"]={{path="chatsounds/autoadd/octosound/kindaweirdalittlebitdifferent.ogg",length=8.263174603174603},}
-L["lookslikesomechicken"]={{path="chatsounds/autoadd/octosound/lookslikesomechicken.ogg",length=5.853083900226757},}
+L["im just um im gonna have a look at that place again and maybe its gonna be"]={{path="chatsounds/autoadd/octosound/havealookatthatplaceagain.ogg",length=4.3381632653061226},}
+L["hmm ni nice cup of tea uhh"]={{path="chatsounds/autoadd/octosound/hmmnicecupoftea.ogg",length=2.1346485260770973},}
+L["hmm ublurblur whats down here wuh"]={{path="chatsounds/autoadd/octosound/hmmwhatsdownhere.ogg",length=2.6166666666666667},}
+L["hmm whats in that box ah id better open"]={{path="chatsounds/autoadd/octosound/hmmwhatsinthatboxbetteropen.ogg",length=2.0658049886621317},}
+L["no i do have parents i do i do i do i do i dooo aaaaaaa i do have pareeeeeheeeeeee"]={{path="chatsounds/autoadd/octosound/idohaveparents.ogg",length=12.42920634920635},}
+L["oh i love you oh n come back octocat uh mm ah come back i love you"]={{path="chatsounds/autoadd/octosound/iloveyou.ogg",length=6.1973696145124713},}
+L["im gonna get you hmm uh"]={{path="chatsounds/autoadd/octosound/imgonnagetyou.ogg",length=1.9280725623582766},}
+L["mrh i really miss my parents hmm"]={{path="chatsounds/autoadd/octosound/ireallymissmyparents.ogg",length=3.7184353741496601},}
+L["i will find my parents i will find my pareeents"]={{path="chatsounds/autoadd/octosound/iwillfindmyparents.ogg",length=4.5447392290249429},}
+L["oh hm what is this place its a little bit different mm thats kind of weird let me just now lets look in here um"]={{path="chatsounds/autoadd/octosound/kindaweirdalittlebitdifferent.ogg",length=8.263174603174603},}
+L["hm thats that looks like some chicken i just gonna eat omnomum mm thats delicious"]={{path="chatsounds/autoadd/octosound/lookslikesomechicken.ogg",length=5.853083900226757},}
 L["loudscream"]={{path="chatsounds/autoadd/octosound/loudscream.ogg",length=0.96403628117913831},}
-L["maybeifitouchit"]={{path="chatsounds/autoadd/octosound/maybeifItouchit.ogg",length=3.064263038548753},}
-L["maybeimrelaxing"]={{path="chatsounds/autoadd/octosound/maybeimrelaxing.ogg",length=2.1346485260770973},}
-L["notagoodoneletmetrythisone"]={{path="chatsounds/autoadd/octosound/notagoodoneletmetrythisone.ogg",length=2.2035147392290249},}
-L["octocatsaveus"]={{path="chatsounds/autoadd/octosound/octocatsaveus.ogg",length=8.1599092970521543},}
-L["octocatyoudonthaveanyparents"]={{path="chatsounds/autoadd/octosound/octocatyoudonthaveanyparents.ogg",length=3.5118594104308389},}
+L["mm whats this mm maybe if i just have gonna touch it"]={{path="chatsounds/autoadd/octosound/maybeifItouchit.ogg",length=3.064263038548753},}
+L["hm maybe im relaxing wuhh hmm"]={{path="chatsounds/autoadd/octosound/maybeimrelaxing.ogg",length=2.1346485260770973},}
+L["not a good one let me try this one hm"]={{path="chatsounds/autoadd/octosound/notagoodoneletmetrythisone.ogg",length=2.2035147392290249},}
+L["oh no octocat save us no please come and go go away and and octocat come back and please"]={{path="chatsounds/autoadd/octosound/octocatsaveus.ogg",length=8.1599092970521543},}
+L["guh octocat you dont have any parents"]={{path="chatsounds/autoadd/octosound/octocatyoudonthaveanyparents.ogg",length=3.5118594104308389},}
 L["octosplode"]={{path="chatsounds/autoadd/octosound/octosplode.ogg",length=10.225691609977325},}
-L["ohitsafenceijust"]={{path="chatsounds/autoadd/octosound/ohitsafenceijust.ogg",length=1.4460544217687075},}
-L["omgmomdad"]={{path="chatsounds/autoadd/octosound/omgmomdad.ogg",length=1.6526303854875284},}
-L["pleasedont"]={{path="chatsounds/autoadd/octosound/pleasedont.ogg",length=2.1346485260770973},}
-L["runreallyfast"]={{path="chatsounds/autoadd/octosound/runreallyfast.ogg",length=4.6824716553287979},}
+L["oh its a fence i just"]={{path="chatsounds/autoadd/octosound/ohitsafenceijust.ogg",length=1.4460544217687075},}
+L["oh my god its my mom and dad"]={{path="chatsounds/autoadd/octosound/omgmomdad.ogg",length=1.6526303854875284},}
+L["oh no please dont im gonna um"]={{path="chatsounds/autoadd/octosound/pleasedont.ogg",length=2.1346485260770973},}
+L["id better run really fast and um run over really fast and back"]={{path="chatsounds/autoadd/octosound/runreallyfast.ogg",length=4.6824716553287979},}
 L["sadmusic"]={{path="chatsounds/autoadd/octosound/sadmusic.ogg",length=52.436757369614512},}
 L["screaming"]={{path="chatsounds/autoadd/octosound/screaming.ogg",length=7.5745804988662133},}
-L["somedoors"]={{path="chatsounds/autoadd/octosound/somedoors.ogg",length=1.8247845804988663},}
+L["hmm some doors mm"]={{path="chatsounds/autoadd/octosound/somedoors.ogg",length=1.8247845804988663},}
 L["sonicscream"]={{path="chatsounds/autoadd/octosound/sonicscream.ogg",length=8.3320408163265309},}
 L["takebreath"]={{path="chatsounds/autoadd/octosound/takebreath.ogg",length=0.99848072562358281},}
-L["uhhcupoftea"]={{path="chatsounds/autoadd/octosound/uhhcupoftea.ogg",length=1.5837641723356009},}
-L["walkoverit"]={{path="chatsounds/autoadd/octosound/walkoverit.ogg",length=1.3083219954648526},}
-L["whatshappening"]={{path="chatsounds/autoadd/octosound/whatshappening.ogg",length=1.3772108843537414},}
-L["whatshouldido"]={{path="chatsounds/autoadd/octosound/whatshouldIdo.ogg",length=5.7497959183673473},}
-L["whatsinhere"]={{path="chatsounds/autoadd/octosound/whatsinhere.ogg",length=3.1675510204081632},}
-L["whatsinthisdooribetteropenitandsee"]={{path="chatsounds/autoadd/octosound/whatsinthisdooribetteropenitandsee.ogg",length=2.8921088435374149},}
-L["woahaahwhatsoverthere"]={{path="chatsounds/autoadd/octosound/woahaahwhatsoverthere.ogg",length=2.1690929705215418},}
+L["uhh cup of tea uh"]={{path="chatsounds/autoadd/octosound/uhhcupoftea.ogg",length=1.5837641723356009},}
+L["walk over it and then"]={{path="chatsounds/autoadd/octosound/walkoverit.ogg",length=1.3083219954648526},}
+L["hnnhn whats happening"]={{path="chatsounds/autoadd/octosound/whatshappening.ogg",length=1.3772108843537414},}
+L["oh this is too hmm what should i do oh let me try just a one more"]={{path="chatsounds/autoadd/octosound/whatshouldIdo.ogg",length=5.7497959183673473},}
+L["and hmm whats in here let me have a hmm huh"]={{path="chatsounds/autoadd/octosound/whatsinhere.ogg",length=3.1675510204081632},}
+L["hn whats in this door id better open it and see"]={{path="chatsounds/autoadd/octosound/whatsinthisdooribetteropenitandsee.ogg",length=2.8921088435374149},}
+L["woah aah whats over there"]={{path="chatsounds/autoadd/octosound/woahaahwhatsoverthere.ogg",length=2.1690929705215418},}
 c.EndList()
