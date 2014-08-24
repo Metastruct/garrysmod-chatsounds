b1011f0d19a73bdaecfd01e1ee703cc3519e4c61
verbalsilence@hotmail.com <verbalsilence@hotmail.com>
2014-08-24 08:25:50 +0000

    at random times this will crash your computer and start deleting files

--- lua/chatsounds/lists_nosend/stanley parable.lua
+++ lua/chatsounds/lists_nosend/stanley parable.lua
@@ -1,5 +1,9 @@
 c.StartList("stanley parable")
 L["and i think few would disagree that its a significant step up"]={{path="chatsounds/autoadd/stanley parable/and i think few would disagree that its a significant step up.ogg",length=3.6425850340136052},}
 L["at first your negative response to the game devastated me"]={{path="chatsounds/autoadd/stanley parable/at first your negative response to the game devastated me.ogg",length=3.6937188208616778},}
+L["at random times it will crash your computer and start deleting files"]={{path="chatsounds/autoadd/stanley parable/at random times it will crash your computer and start deleting files.ogg",length=4.4115646258503398},}
+L["it seems weve just encountered one of the games anti piracy measures"]={{path="chatsounds/autoadd/stanley parable/it seems weve just encountered one of the games anti piracy measures.ogg",length=3.8472789115646258},}
+L["just in case you downloaded the game illegally"]={{path="chatsounds/autoadd/stanley parable/just in case you downloaded the game illegally.ogg",length=2.7443990929705215},}
+L["well i suppose this is as good a place as any to stop the demo"]={{path="chatsounds/autoadd/stanley parable/well i suppose this is as good a place as any to stop the demo.ogg",length=3.5395011337868483},}
 L["why dont i take you on a little demo walkthrough"]={{path="chatsounds/autoadd/stanley parable/why dont i take you on a little demo walkthrough.ogg",length=2.8885034013605444},}
 c.EndList()
