b06d6f2559e3c64efd112c268cdf6ea3ced498ce
verbalsilence@hotmail.com <verbalsilence@hotmail.com>
2014-09-01 00:42:21 +0000

    from part 6

---
+++ lua/chatsounds/lists_nosend/vinesauce ww.lua
@@ -0,0 +1,9 @@
+c.StartList("vinesauce ww")
+L["hey everyone look at the pern i like pern"]={{path="chatsounds/autoadd/vinesauce ww/hey everyone look at the pern i like pern.ogg",length=4.2725850340136056},}
+L["im gonna put it as my spray in ttt so everyone can see pern"]={{path="chatsounds/autoadd/vinesauce ww/im gonna put it as my spray in ttt so everyone can see pern.ogg",length=6.2664852607709749},}
+L["luigi youre facking stupid"]={{path="chatsounds/autoadd/vinesauce ww/luigi youre facking stupid.ogg",length=3.051859410430839},}
+L["pls"]={{path="chatsounds/autoadd/vinesauce ww/pls.ogg",length=0.83417233560090698},}
+L["this is how actual porn addicts speak"]={{path="chatsounds/autoadd/vinesauce ww/this is how actual porn addicts speak.ogg",length=3.3367120181405894},}
+L["top fucking lel mate"]={{path="chatsounds/autoadd/vinesauce ww/top fucking lel mate.ogg",length=1.383514739229025},}
+L["top fucking lel"]={{path="chatsounds/autoadd/vinesauce ww/top fucking lel.ogg",length=1.546281179138322},}
+c.EndList()
