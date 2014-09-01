abdc811bf1137f2ed2922b195520db9628932c00
gigispha@gmail.com <gigispha@gmail.com>
2014-09-01 23:33:36 +0000

    betcha you weren't expecting the potato chips with barbecue flavour

---
+++ lua/chatsounds/lists_nosend/KirbyRightBackAtYa.lua
@@ -0,0 +1,10 @@
+c.StartList("KirbyRightBackAtYa")
+L["barbecue flavoured potato chips"]={{path="chatsounds/autoadd/KirbyRightBackAtYa/barbecue flavoured potato chips.ogg",length=1.5743310657596372},}
+L["buzz off"]={{path="chatsounds/autoadd/KirbyRightBackAtYa/buzz off.ogg",length=0.72290249433106579},}
+L["hand me another bag of dem chips"]={{path="chatsounds/autoadd/KirbyRightBackAtYa/hand me another bag of dem chips.ogg",length=2.4553968253968255},}
+L["if you wanna help me hand me another bag of dem chips"]={{path="chatsounds/autoadd/KirbyRightBackAtYa/if you wanna help me hand me another bag of dem chips.ogg",length=3.6831519274376419},}
+L["laugh"]={{path="chatsounds/autoadd/KirbyRightBackAtYa/laugh.ogg",length=1.2015192743764171},}
+L["man what a bush of dumbscum"]={{path="chatsounds/autoadd/KirbyRightBackAtYa/man what a bush of dumbscum.ogg",length=1.5712471655328799},}
+L["nobody stands between me and a snack"]={{path="chatsounds/autoadd/KirbyRightBackAtYa/nobody stands between me and a snack.ogg",length=1.8333106575963718},}
+L["snack food oh boy"]={{path="chatsounds/autoadd/KirbyRightBackAtYa/snack food oh boy.ogg",length=1.5166439909297051},}
+c.EndList()
