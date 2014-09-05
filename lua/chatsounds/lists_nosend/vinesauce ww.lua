57904d955a598887dec0713b0e682ce0fab6546e
verbalsilence@hotmail.com <verbalsilence@hotmail.com>
2014-09-05 14:14:07 +0000

    shots fired!

--- lua/chatsounds/lists_nosend/vinesauce ww.lua
+++ lua/chatsounds/lists_nosend/vinesauce ww.lua
@@ -1,5 +1,6 @@
 c.StartList("vinesauce ww")
 L["hey everyone look at the pern i like pern"]={{path="chatsounds/autoadd/vinesauce ww/hey everyone look at the pern i like pern.ogg",length=4.2725850340136056},}
+L["hey im cunt im not so cunt and were the game cunts"]={{path="chatsounds/autoadd/vinesauce ww/hey im cunt im not so cunt and were the game cunts.ogg",length=4.223106575963719},}
 L["im gonna put it as my spray in ttt so everyone can see pern"]={{path="chatsounds/autoadd/vinesauce ww/im gonna put it as my spray in ttt so everyone can see pern.ogg",length=6.2664852607709749},}
 L["luigi youre facking stupid"]={{path="chatsounds/autoadd/vinesauce ww/luigi youre facking stupid.ogg",length=3.051859410430839},}
 L["pls"]={{path="chatsounds/autoadd/vinesauce ww/pls.ogg",length=0.83417233560090698},}
