21701a422ce131861d1016fe1f264f4fa1613a9a
gigispha@gmail.com <gigispha@gmail.com>
2014-09-12 22:22:56 +0000

    if you know what i mean

--- lua/chatsounds/lists_nosend/KirbyRightBackAtYa.lua
+++ lua/chatsounds/lists_nosend/KirbyRightBackAtYa.lua
@@ -2,6 +2,7 @@ c.StartList("KirbyRightBackAtYa")
 L["barbecue flavoured potato chips"]={{path="chatsounds/autoadd/KirbyRightBackAtYa/barbecue flavoured potato chips.ogg",length=1.5743310657596372},}
 L["buzz off"]={{path="chatsounds/autoadd/KirbyRightBackAtYa/buzz off.ogg",length=0.72290249433106579},}
 L["hand me another bag of dem chips"]={{path="chatsounds/autoadd/KirbyRightBackAtYa/hand me another bag of dem chips.ogg",length=2.4553968253968255},}
+L["i know i heard the sound of sucking over here somewhere"]={{path="chatsounds/autoadd/KirbyRightBackAtYa/i know i heard the sound of sucking over here somewhere.ogg",length=2.4888662131519275},}
 L["if you wanna help me hand me another bag of dem chips"]={{path="chatsounds/autoadd/KirbyRightBackAtYa/if you wanna help me hand me another bag of dem chips.ogg",length=3.6831519274376419},}
 L["laugh"]={{path="chatsounds/autoadd/KirbyRightBackAtYa/laugh.ogg",length=1.2015192743764171},}
 L["man what a bush of dumbscum"]={{path="chatsounds/autoadd/KirbyRightBackAtYa/man what a bush of dumbscum.ogg",length=1.5712471655328799},}
