50a57cdf4f45d4e02554fea23e312be2da037354
verbalsilence@hotmail.com <verbalsilence@hotmail.com>
2014-09-20 05:43:24 +0000

    NOICE!!!!!!

---
+++ lua/chatsounds/lists_nosend/keyandpeele.lua
@@ -0,0 +1,13 @@
+c.StartList("keyandpeele")
+L["crying noice"]={{path="chatsounds/autoadd/keyandpeele/crying noice.ogg",length=1.105827664399093},}
+L["desperate noice"]={{path="chatsounds/autoadd/keyandpeele/desperate noice.ogg",length=13.040249433106576},}
+L["i just noiced you because of you standing up for yourself and your place in this dance circle"]={{path="chatsounds/autoadd/keyandpeele/i just noiced you because of you standing up for yourself and your place in this dance circle.ogg",length=4.8541950113378682},}
+L["im just noicing your noice"]={{path="chatsounds/autoadd/keyandpeele/im just noicing your noice.ogg",length=1.5510430839002267},}
+L["im kinda the guy that says noice"]={{path="chatsounds/autoadd/keyandpeele/im kinda the guy that says noice.ogg",length=1.7808390022675737},}
+L["its mostly bout the dancers"]={{path="chatsounds/autoadd/keyandpeele/its mostly bout the dancers.ogg",length=1.2638095238095237},}
+L["no more noice from you"]={{path="chatsounds/autoadd/keyandpeele/no more noice from you.ogg",length=1.3787074829931973},}
+L["no no no noice ok"]={{path="chatsounds/autoadd/keyandpeele/no no no noice ok.ogg",length=2.226031746031746},}
+L["noice"]={{path="chatsounds/autoadd/keyandpeele/noice/noice 1.ogg",length=0.59600907029478456},{path="chatsounds/autoadd/keyandpeele/noice/noice 2.ogg",length=0.67498866213151931},{path="chatsounds/autoadd/keyandpeele/noice/noice 3.ogg",length=0.97659863945578229},{path="chatsounds/autoadd/keyandpeele/noice/noice 4.ogg",length=1.0914739229024943},{path="chatsounds/autoadd/keyandpeele/noice/noice 5.ogg",length=1.2207256235827664},{path="chatsounds/autoadd/keyandpeele/noice/noice 6.ogg",length=1.048390022675737},{path="chatsounds/autoadd/keyandpeele/noice/noice 7.ogg",length=0.89040816326530614},{path="chatsounds/autoadd/keyandpeele/noice/noice 8.ogg",length=3.2600680272108842},}
+L["noice is my thing"]={{path="chatsounds/autoadd/keyandpeele/noice is my thing.ogg",length=1.0196598639455783},}
+L["noice noice"]={{path="chatsounds/autoadd/keyandpeele/noice noice.ogg",length=1.0842857142857143},}
+c.EndList()
