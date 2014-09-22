ea21e88a6f68db913d4fdec19bef8c8e08e6c7bf
tw1stal1cky@gmail.com <tw1stal1cky@gmail.com>
2014-09-22 18:06:43 +0000

    nonhuman's

--- lua/chatsounds/lists_nosend/nonhuman.lua
+++ lua/chatsounds/lists_nosend/nonhuman.lua
@@ -1,19 +1,37 @@
 c.StartList("nonhuman")
+L["barett"]={{path="chatsounds/autoadd/nonhuman/barett.OGG",length=1.1541950113378685},}
 L["barrys red cola"]={{path="chatsounds/autoadd/nonhuman/barrys red cola/barrys red cola 1.OGG",length=1.5792970521541949},{path="chatsounds/autoadd/nonhuman/barrys red cola/barrys red cola 2.OGG",length=1.9986167800453514},{path="chatsounds/autoadd/nonhuman/barrys red cola/barrys red cola 3.OGG",length=0.99192743764172331},}
 L["dive into a pool of"]={{path="chatsounds/autoadd/nonhuman/dive into a pool of.OGG",length=1.9986167800453514},}
 L["drink barrys red cola"]={{path="chatsounds/autoadd/nonhuman/drink barrys red cola/drink barrys red cola 2.OGG",length=2},{path="chatsounds/autoadd/nonhuman/drink barrys red cola/drink barrys red cola.OGG",length=2},}
+L["dsr50"]={{path="chatsounds/autoadd/nonhuman/dsr50.OGG",length=2.4222222222222221},}
 L["hitmarker"]={{path="chatsounds/autoadd/nonhuman/hitmarker.OGG",length=0.10038548752834467},}
+L["leave me alooooone"]={{path="chatsounds/autoadd/nonhuman/leave me alooooone.OGG",length=1.5012244897959184},}
+L["mlgbass"]={{path="chatsounds/autoadd/nonhuman/mlgbass/bass1.OGG",length=1.6851020408163266},{path="chatsounds/autoadd/nonhuman/mlgbass/bass2.OGG",length=1.6851020408163266},{path="chatsounds/autoadd/nonhuman/mlgbass/bass3.OGG",length=1.6438548752834468},{path="chatsounds/autoadd/nonhuman/mlgbass/bass4.OGG",length=1.6438548752834468},{path="chatsounds/autoadd/nonhuman/mlgbass/bass6.OGG",length=1.6438548752834468},{path="chatsounds/autoadd/nonhuman/mlgbass/bass7.OGG",length=1.6438548752834468},{path="chatsounds/autoadd/nonhuman/mlgbass/bass8.OGG",length=1.6438548752834468},{path="chatsounds/autoadd/nonhuman/mlgbass/bass9.OGG",length=1.6438548752834468},}
 L["mom get the camera mommy mommy mommy oh my god"]={{path="chatsounds/autoadd/nonhuman/mom get the camera mommy mommy mommy oh my god.OGG",length=6.0123129251700682},}
 L["oh baby a triple"]={{path="chatsounds/autoadd/nonhuman/oh baby a triple.OGG",length=2.6122448979591835},}
 L["oh my fucking god"]={{path="chatsounds/autoadd/nonhuman/oh my fucking god.OGG",length=3.312018140589569},}
+L["rapbattle"]={{path="chatsounds/autoadd/nonhuman/rapbattle.OGG",length=6.7404988662131515},}
+L["shut uuup"]={{path="chatsounds/autoadd/nonhuman/shut uuup.OGG",length=1.3495238095238096},}
 L["thats right get noscoped get noscoped"]={{path="chatsounds/autoadd/nonhuman/thats right get noscoped get noscoped.OGG",length=4.4962131519274378},}
+L["thats right seven hundred freaking points"]={{path="chatsounds/autoadd/nonhuman/thats right seven hundred freaking points.OGG",length=3.6914058956916098},}
 L["trapguncock"]={{path="chatsounds/autoadd/nonhuman/trapguncock.OGG",length=0.72183673469387755},}
 L["trickshot1"]={{path="chatsounds/autoadd/nonhuman/trickshot1.OGG",length=4},}
+L["trickshot10"]={{path="chatsounds/autoadd/nonhuman/trickshot10.OGG",length=8.2674376417233564},}
+L["trickshot11"]={{path="chatsounds/autoadd/nonhuman/trickshot11.OGG",length=6.7404081632653066},}
+L["trickshot12"]={{path="chatsounds/autoadd/nonhuman/trickshot12.OGG",length=15.165918367346938},}
 L["trickshot2"]={{path="chatsounds/autoadd/nonhuman/trickshot2.OGG",length=3.5479138321995465},}
 L["trickshot3"]={{path="chatsounds/autoadd/nonhuman/trickshot3.OGG",length=3.8138775510204082},}
+L["trickshot4"]={{path="chatsounds/autoadd/nonhuman/trickshot4.OGG",length=1.1143990929705216},}
+L["trickshot5"]={{path="chatsounds/autoadd/nonhuman/trickshot5.OGG",length=0.77818594104308392},}
+L["trickshot6"]={{path="chatsounds/autoadd/nonhuman/trickshot6.OGG",length=1.1143990929705216},}
+L["trickshot7"]={{path="chatsounds/autoadd/nonhuman/trickshot7.OGG",length=13.344195011337868},}
+L["trickshot8"]={{path="chatsounds/autoadd/nonhuman/trickshot8.OGG",length=16.491836734693877},}
+L["trickshot9"]={{path="chatsounds/autoadd/nonhuman/trickshot9.OGG",length=12},}
 L["unfortunately the giant also loves barrys red cola"]={{path="chatsounds/autoadd/nonhuman/unfortunately the giant also loves barrys red cola.OGG",length=3.2006122448979593},}
 L["what would you like to do"]={{path="chatsounds/autoadd/nonhuman/what would you like to do.OGG",length=1.3058956916099773},}
 L["wombocombo"]={{path="chatsounds/autoadd/nonhuman/wombocombo.OGG",length=18},}
 L["you drink the delicious barrys red cola"]={{path="chatsounds/autoadd/nonhuman/you drink the delicious barrys red cola.OGG",length=3.0080725623582767},}
+L["you guys suck eggs"]={{path="chatsounds/autoadd/nonhuman/you guys suck eggs.OGG",length=1.9517233560090703},}
+L["you guys suck"]={{path="chatsounds/autoadd/nonhuman/you guys suck.OGG",length=1.3495238095238096},}
 L["you wake on a giants kitchen"]={{path="chatsounds/autoadd/nonhuman/you wake on a giants kitchen.OGG",length=2},}
 c.EndList()
