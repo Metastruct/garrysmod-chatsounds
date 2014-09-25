8fb50954bd97d742e2aee9bbcbd6ffb60502dad5
verbalsilence@hotmail.com <verbalsilence@hotmail.com>
2014-09-25 19:47:57 +0000

    jontron drugs

---
+++ lua/chatsounds/lists_nosend/dane cook.lua
@@ -0,0 +1,19 @@
+c.StartList("dane cook")
+L["alright maam apparently you want some pickles"]={{path="chatsounds/autoadd/dane cook/alright maam apparently you want some pickles.ogg",length=2.0433560090702949},}
+L["alright were dealing with food not missles here governor"]={{path="chatsounds/autoadd/dane cook/alright were dealing with food not missles here governor.ogg",length=2.3452154195011339},}
+L["and the pickles"]={{path="chatsounds/autoadd/dane cook/and the pickles.ogg",length=5.4334693877551024},}
+L["chicken tenders"]={{path="chatsounds/autoadd/dane cook/chicken tenders.ogg",length=1.4164172335600906},}
+L["cmon she wants it her way"]={{path="chatsounds/autoadd/dane cook/cmon she wants it her way.ogg",length=1.9040362811791383},}
+L["helloooo"]={{path="chatsounds/autoadd/dane cook/helloooo.ogg",length=2.2058956916099772},}
+L["hi welcome to burger king may i take your order"]={{path="chatsounds/autoadd/dane cook/hi welcome to burger king may i take your order.ogg",length=1.9853061224489796},}
+L["im bleeding from the ears here bachino lets calm down"]={{path="chatsounds/autoadd/dane cook/im bleeding from the ears here bachino lets calm down.ogg",length=2.8328344671201813},}
+L["large fry"]={{path="chatsounds/autoadd/dane cook/large fry.ogg",length=1.497687074829932},}
+L["maam are you trying to molest me via drivethrough"]={{path="chatsounds/autoadd/dane cook/maam are you trying to molest me via drivethrough.ogg",length=2.8792743764172335},}
+L["now drive around"]={{path="chatsounds/autoadd/dane cook/now drive around.ogg",length=1.1609977324263039},}
+L["okay maam drive around"]={{path="chatsounds/autoadd/dane cook/okay maam drive around.ogg",length=1.1203628117913833},}
+L["scuse me chewbaca uh"]={{path="chatsounds/autoadd/dane cook/scuse me chewbaca uh.ogg",length=1.7298866213151927},}
+L["someone get some sauce now"]={{path="chatsounds/autoadd/dane cook/someone get some sauce now.ogg",length=1.5905668934240362},}
+L["sweet sauce all over my body"]={{path="chatsounds/autoadd/dane cook/sweet sauce all over my body.ogg",length=2.5541950113378684},}
+L["whopper no onion"]={{path="chatsounds/autoadd/dane cook/whopper no onion.ogg",length=1.3815873015873017},}
+L["whopper"]={{path="chatsounds/autoadd/dane cook/whopper.ogg",length=1.0797278911564625},}
+c.EndList()
