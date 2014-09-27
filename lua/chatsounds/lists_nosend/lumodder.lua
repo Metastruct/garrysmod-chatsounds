6136021b4fab1ec974ccb9ff658e86cd09b0f0a3
LUModder@gmail.com <lumodder@gmail.com>
2014-09-27 02:22:36 +0000

    Chatsound dump
    2 Vinesauce Glitch Cricket
    5 Scout's Amazing Adventures

--- lua/chatsounds/lists_nosend/lumodder.lua
+++ lua/chatsounds/lists_nosend/lumodder.lua
@@ -5,4 +5,17 @@ L["thats racist"]={{path="chatsounds/autoadd/lumodder/thats racist.ogg",length=1
 L["trainrain"]={{path="chatsounds/autoadd/lumodder/trainrain.ogg",length=3.0040816326530613},}
 L["triple x rated"]={{path="chatsounds/autoadd/lumodder/triple x rated.ogg",length=3.4309070294784583},}
 L["whos been drawing dicks"]={{path="chatsounds/autoadd/lumodder/whos been drawing dicks.ogg",length=1.5914739229024943},}
+
+--scouts amazing adventures
+L["and you are faggot"]={{path="chatsounds/autoadd/lumodder/scoutsamazingadv/and you are faggot.ogg",length=1.584530},}
+L["no u"]={{path="chatsounds/autoadd/lumodder/scoutsamazingadv/no u.ogg",length=0.938520},}
+
+L["soap"]={{path="chatsounds/autoadd/lumodder/scoutsamazingadv/soap/1.ogg",length=0.231550},}
+L["soap"]={{path="chatsounds/autoadd/lumodder/scoutsamazingadv/soap/2.ogg",length=0.341270},}
+L["soap"]={{path="chatsounds/autoadd/lumodder/scoutsamazingadv/soap/3.ogg",length=0.365640},}
+
+--glitch cricket
+L["there he goes the willy wonga"]={{path="chatsounds/autoadd/lumodder/glitchcricket/there he goes the willy wonga.ogg",length=3.088820},}
+L["there you fn go you cunt bastard"]={{path="chatsounds/autoadd/lumodder/glitchcricket/.ogg",length=5.861920},}
+
 c.EndList()
