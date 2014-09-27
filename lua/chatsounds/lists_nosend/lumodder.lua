20690b93251dd26326a25addce29df9d88e447c4
techbot121@gmail.com <techbot121@users.noreply.github.com>
2014-09-27 23:59:48 +0000

    ok google

--- lua/chatsounds/lists_nosend/lumodder.lua
+++ lua/chatsounds/lists_nosend/lumodder.lua
@@ -1,21 +1,10 @@
 c.StartList("lumodder")
 L["but my hope will never die"]={{path="chatsounds/autoadd/lumodder/but my hope will never die.ogg",length=1.5582086167800453},}
+L["glitchcricket"]={{path="chatsounds/autoadd/lumodder/glitchcricket/there he goes the willy wonga.ogg",length=3.0888435374149661},{path="chatsounds/autoadd/lumodder/glitchcricket/there you fn go you cunt bastard.ogg",length=5.8619501133786844},}
+L["scoutsamazingadv"]={{path="chatsounds/autoadd/lumodder/scoutsamazingadv/and you are faggot.ogg",length=1.5845578231292516},{path="chatsounds/autoadd/lumodder/scoutsamazingadv/no u.ogg",length=0.93854875283446715},}
 L["suck my balls doc"]={{path="chatsounds/autoadd/lumodder/suck my balls doc.ogg",length=1.5905668934240362},}
 L["thats racist"]={{path="chatsounds/autoadd/lumodder/thats racist.ogg",length=1.1724263038548752},}
 L["trainrain"]={{path="chatsounds/autoadd/lumodder/trainrain.ogg",length=3.0040816326530613},}
 L["triple x rated"]={{path="chatsounds/autoadd/lumodder/triple x rated.ogg",length=3.4309070294784583},}
 L["whos been drawing dicks"]={{path="chatsounds/autoadd/lumodder/whos been drawing dicks.ogg",length=1.5914739229024943},}
-
---scouts amazing adventures
-L["and you are faggot"]={{path="chatsounds/autoadd/lumodder/scoutsamazingadv/and you are faggot.ogg",length=1.584530},}
-L["no u"]={{path="chatsounds/autoadd/lumodder/scoutsamazingadv/no u.ogg",length=0.938520},}
-
-L["soap"]={{path="chatsounds/autoadd/lumodder/scoutsamazingadv/soap/1.ogg",length=0.231550},}
-L["soap"]={{path="chatsounds/autoadd/lumodder/scoutsamazingadv/soap/2.ogg",length=0.341270},}
-L["soap"]={{path="chatsounds/autoadd/lumodder/scoutsamazingadv/soap/3.ogg",length=0.365640},}
-
---glitch cricket
-L["there he goes the willy wonga"]={{path="chatsounds/autoadd/lumodder/glitchcricket/there he goes the willy wonga.ogg",length=3.088820},}
-L["there you fn go you cunt bastard"]={{path="chatsounds/autoadd/lumodder/glitchcricket/there you fn go you cunt bastard.ogg",length=5.861920},}
-
 c.EndList()
