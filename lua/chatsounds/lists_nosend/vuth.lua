547bcc51ce9755cf17a50bf2b5e8601dbe88d67b
mulletbull@gmail.com <mulletbull@gmail.com>
2014-10-13 23:07:03 +0000

    requested by vuthakral

--- lua/chatsounds/lists_nosend/vuth.lua
+++ lua/chatsounds/lists_nosend/vuth.lua
@@ -1,7 +1,70 @@
 c.StartList("vuth")
+L["an exclusive home of the meanest sons a bitches in the state of maryland"]={{path="chatsounds/autoadd/vuth/an exclusive home of the meanest sons a bitches in the state of maryland.ogg",length=3.4618140589569162},}
+L["and it better not bounce or youre a dead motherfucker"]={{path="chatsounds/autoadd/vuth/and it better not bounce or youre a dead motherfucker.ogg",length=2.2819954648526077},}
+L["and not get wet"]={{path="chatsounds/autoadd/vuth/and not get wet.ogg",length=0.87598639455782312},}
+L["bad deal"]={{path="chatsounds/autoadd/vuth/bad deal.ogg",length=0.62984126984126987},}
+L["because at big bill hells"]={{path="chatsounds/autoadd/vuth/because at big bill hells.ogg",length=1.1997732426303855},}
+L["big bill hells cars"]={{path="chatsounds/autoadd/vuth/big bill hells cars.ogg",length=1.1443310657596373},}
+L["bring your tile"]={{path="chatsounds/autoadd/vuth/bring your tile.ogg",length=0.66972789115646258},}
+L["bring your trade"]={{path="chatsounds/autoadd/vuth/bring your trade.ogg",length=0.63868480725623578},}
+L["bring your wife"]={{path="chatsounds/autoadd/vuth/bring your wife.ogg",length=0.56551020408163266},}
+L["cars that break down"]={{path="chatsounds/autoadd/vuth/cars that break down.ogg",length=1.2330385487528344},}
+L["cartoonexplosion"]={{path="chatsounds/autoadd/vuth/cartoonexplosion.ogg",length=3.5425396825396827},}
+L["challenge pissing"]={{path="chatsounds/autoadd/vuth/challenge pissing.ogg",length=0.89374149659863944},}
 L["cinemasins alarm"]={{path="chatsounds/autoadd/vuth/cinemasins alarm.ogg",length=3.7872335600907028},}
+L["destroy any enemies fn sh"]={{path="chatsounds/autoadd/vuth/destroy any enemies fn sh.ogg",length=2.3527664399092969},}
+L["do you know who youre dealing with"]={{path="chatsounds/autoadd/vuth/do you know who youre dealing with.ogg",length=1.513310657596372},}
+L["do you smell something burning"]={{path="chatsounds/autoadd/vuth/do you smell something burning.ogg",length=1.5708843537414967},}
+L["dont delay"]={{path="chatsounds/autoadd/vuth/dont delay.ogg",length=0.66752834467120181},}
+L["dont fuck with us"]={{path="chatsounds/autoadd/vuth/dont fuck with us.ogg",length=0.9425170068027211},}
+L["dont wait"]={{path="chatsounds/autoadd/vuth/dont wait.ogg",length=0.58768707482993199},}
+L["fuck you baltimore"]={{path="chatsounds/autoadd/vuth/fuck you baltimore.ogg",length=1.3128798185941044},}
 L["get your pimping shit outta here ya god dang nigger"]={{path="chatsounds/autoadd/vuth/get your pimping shit outta here ya god dang nigger.ogg",length=3.5265306122448981},}
+L["go to hell"]={{path="chatsounds/autoadd/vuth/go to hell.ogg",length=0.72517006802721085},}
+L["guaranteed"]={{path="chatsounds/autoadd/vuth/guaranteed.ogg",length=3.3531519274376418},}
+L["hl1 scientist dialog"]={{path="chatsounds/autoadd/vuth/hl1 scientist dialog/ah gordon here you are we just sent the sample down to the test chamber.ogg",length=4.4969614512471656},{path="chatsounds/autoadd/vuth/hl1 scientist dialog/big day today freeman.ogg",length=1.361360544217687},{path="chatsounds/autoadd/vuth/hl1 scientist dialog/it wasnt meant to do this in the first place.ogg",length=2.5373242630385486},{path="chatsounds/autoadd/vuth/hl1 scientist dialog/quick its about to go critical.ogg",length=2.3977324263038549},{path="chatsounds/autoadd/vuth/hl1 scientist dialog/testing testing clears throat everything seems to be in order.ogg",length=6.7843083900226757},{path="chatsounds/autoadd/vuth/hl1 scientist dialog/theyre waiting for you gordon in the test chamberrr.ogg",length=3.7476643990929706},{path="chatsounds/autoadd/vuth/hl1 scientist dialog/weve boosted the antimass spectrometer to one hundred five percent bit of a gamble but we need the extra resolution.ogg",length=6.4759183673469387},{path="chatsounds/autoadd/vuth/hl1 scientist dialog/what the hell is going on with our equipment.ogg",length=2.5685260770975056},}
+L["home of challenge pissing"]={{path="chatsounds/autoadd/vuth/home of challenge pissing.ogg",length=1.410453514739229},}
+L["hope you rot criminal scum"]={{path="chatsounds/autoadd/vuth/hope you rot criminal scum.ogg",length=2.6122448979591835},}
+L["how does it work"]={{path="chatsounds/autoadd/vuth/how does it work.ogg",length=0.59655328798185936},}
+L["hurry up asshole"]={{path="chatsounds/autoadd/vuth/hurry up asshole.ogg",length=0.95360544217687071},}
 L["i once got busy in a burger king bathroom"]={{path="chatsounds/autoadd/vuth/i once got busy in a burger king bathroom.ogg",length=1.7331292517006802},}
+L["if you can piss six feet in the air straight up"]={{path="chatsounds/autoadd/vuth/if you can piss six feet in the air straight up.ogg",length=2.038049886621315},}
+L["if you think youre gonna find a bargain at big bills"]={{path="chatsounds/autoadd/vuth/if you think youre gonna find a bargain at big bills.ogg",length=2.1821995464852608},}
+L["if youre dumb enough to buy a new car this weekend"]={{path="chatsounds/autoadd/vuth/if youre dumb enough to buy a new car this weekend.ogg",length=2.324126984126984},}
+L["let me answer my f cell phone"]={{path="chatsounds/autoadd/vuth/let me answer my f cell phone.ogg",length=1.8822222222222222},}
 L["nnoo"]={{path="chatsounds/autoadd/vuth/nnoo.ogg",length=0.82678004535147387},}
+L["ohmygod"]={{path="chatsounds/autoadd/vuth/ohmygod.ogg",length=1.0634467120181406},}
+L["only at big bill hell"]={{path="chatsounds/autoadd/vuth/only at big bill hell.ogg",length=1.0334467120181405},}
+L["or well rip your nuts off"]={{path="chatsounds/autoadd/vuth/or well rip your nuts off.ogg",length=1.3372789115646257},}
+L["prices have changed in the black market visit imperial and rebel worlds for shitholes"]={{path="chatsounds/autoadd/vuth/prices have changed in the black market visit imperial and rebel worlds for shitholes.ogg",length=5.496077097505669},}
 L["scene does not include lap dance"]={{path="chatsounds/autoadd/vuth/scene does not include lap dance.ogg",length=1.4442857142857144},}
+L["scientist scream"]={{path="chatsounds/autoadd/vuth/scientist scream/scream01.ogg",length=1.3259863945578232},{path="chatsounds/autoadd/vuth/scientist scream/scream02.ogg",length=1.3934693877551021},{path="chatsounds/autoadd/vuth/scientist scream/scream03.ogg",length=1.3258956916099773},{path="chatsounds/autoadd/vuth/scientist scream/scream04.ogg",length=1.9728798185941043},{path="chatsounds/autoadd/vuth/scientist scream/scream05.ogg",length=2.2499773242630385},{path="chatsounds/autoadd/vuth/scientist scream/scream07.ogg",length=1.9095691609977323},{path="chatsounds/autoadd/vuth/scientist scream/scream08.ogg",length=2.1836734693877551},{path="chatsounds/autoadd/vuth/scientist scream/scream09.ogg",length=0.55183673469387751},{path="chatsounds/autoadd/vuth/scientist scream/scream1.ogg",length=1.9785034013605443},{path="chatsounds/autoadd/vuth/scientist scream/scream10.ogg",length=0.48480725623582765},{path="chatsounds/autoadd/vuth/scientist scream/scream11.ogg",length=0.62956916099773241},{path="chatsounds/autoadd/vuth/scientist scream/scream12.ogg",length=0.51356009070294784},{path="chatsounds/autoadd/vuth/scientist scream/scream13.ogg",length=0.56435374149659867},{path="chatsounds/autoadd/vuth/scientist scream/scream14.ogg",length=0.43328798185941042},{path="chatsounds/autoadd/vuth/scientist scream/scream15.ogg",length=0.27219954648526079},{path="chatsounds/autoadd/vuth/scientist scream/scream16.ogg",length=0.20698412698412699},{path="chatsounds/autoadd/vuth/scientist scream/scream17.ogg",length=0.34367346938775512},{path="chatsounds/autoadd/vuth/scientist scream/scream18.ogg",length=0.22294784580498866},{path="chatsounds/autoadd/vuth/scientist scream/scream2.ogg",length=1.2484353741496599},{path="chatsounds/autoadd/vuth/scientist scream/scream20.ogg",length=1.9286167800453515},{path="chatsounds/autoadd/vuth/scientist scream/scream21.ogg",length=2.7557369614512472},{path="chatsounds/autoadd/vuth/scientist scream/scream22.ogg",length=1.3750566893424037},{path="chatsounds/autoadd/vuth/scientist scream/scream23.ogg",length=1.9902040816326532},{path="chatsounds/autoadd/vuth/scientist scream/scream24.ogg",length=2.0510657596371882},{path="chatsounds/autoadd/vuth/scientist scream/scream25.ogg",length=0.73251700680272114},{path="chatsounds/autoadd/vuth/scientist scream/scream3.ogg",length=1.2698412698412698},{path="chatsounds/autoadd/vuth/scientist scream/scream4.ogg",length=1.4411791383219954},{path="chatsounds/autoadd/vuth/scientist scream/scream6.ogg",length=0.79591836734693877},{path="chatsounds/autoadd/vuth/scientist scream/scream7.ogg",length=0.79591836734693877},}
+L["shitholes"]={{path="chatsounds/autoadd/vuth/shitholes.ogg",length=0.84700680272108841},}
+L["shove it up your ugly ass"]={{path="chatsounds/autoadd/vuth/shove it up your ugly ass.ogg",length=1.2064172335600907},}
+L["shove it up your ugly ass2"]={{path="chatsounds/autoadd/vuth/shove it up your ugly ass2.ogg",length=1.5257596371882085},}
+L["shutup"]={{path="chatsounds/autoadd/vuth/shutup.ogg",length=0.87591836734693873},}
+L["shutup2"]={{path="chatsounds/autoadd/vuth/shutup2.ogg",length=0.7141043083900227},}
+L["stop you violated the law pay the court a fine or serve your sentence your stolen goods are now forfeit"]={{path="chatsounds/autoadd/vuth/stop you violated the law pay the court a fine or serve your sentence your stolen goods are now forfeit.ogg",length=7.5755102040816329},}
+L["take a hike"]={{path="chatsounds/autoadd/vuth/take a hike.ogg",length=0.70743764172335599},}
+L["thats right well fuck your wife"]={{path="chatsounds/autoadd/vuth/thats right well fuck your wife.ogg",length=1.3682993197278912},}
+L["thats right"]={{path="chatsounds/autoadd/vuth/thats right.ogg",length=0.57879818594104304},}
+L["thats too bad i was hoping youd resist arrest"]={{path="chatsounds/autoadd/vuth/thats too bad i was hoping youd resist arrest.ogg",length=4.2057142857142855},}
+L["the f"]={{path="chatsounds/autoadd/vuth/the f.ogg",length=0.76698412698412699},}
+L["the only dealer that tells you to fuck off"]={{path="chatsounds/autoadd/vuth/the only dealer that tells you to fuck off.ogg",length=2.0801814058956918},}
+L["thebutton"]={{path="chatsounds/autoadd/vuth/thebutton.ogg",length=1.5391156462585034},}
+L["then pay with your blood"]={{path="chatsounds/autoadd/vuth/then pay with your blood.ogg",length=1.619591836734694},}
+L["thieves"]={{path="chatsounds/autoadd/vuth/thieves.ogg",length=0.45240362811791385},}
+L["this event ends the minute after you write us a check"]={{path="chatsounds/autoadd/vuth/this event ends the minute after you write us a check.ogg",length=2.3640589569160997},}
+L["to big bell hell"]={{path="chatsounds/autoadd/vuth/to big bell hell.ogg",length=0.97800453514739227},}
+L["well fuck her"]={{path="chatsounds/autoadd/vuth/well fuck her.ogg",length=0.80945578231292514},}
+L["what do you mean you dont agree with me"]={{path="chatsounds/autoadd/vuth/what do you mean you dont agree with me.ogg",length=1.8056689342403629},}
+L["what the f is this"]={{path="chatsounds/autoadd/vuth/what the f is this.ogg",length=1.7692970521541951},}
+L["yes oh hell plot and plan but theres always ways to fuck him up"]={{path="chatsounds/autoadd/vuth/yes oh hell plot and plan but theres always ways to fuck him up.ogg",length=4.0655782312925171},}
+L["yess"]={{path="chatsounds/autoadd/vuth/yess.ogg",length=0.63172335600907026},}
+L["you can kiss my ass"]={{path="chatsounds/autoadd/vuth/you can kiss my ass.ogg",length=1.1709297052154195},}
+L["you get no down payment"]={{path="chatsounds/autoadd/vuth/you get no down payment.ogg",length=1.2574149659863945},}
+L["you heard us right"]={{path="chatsounds/autoadd/vuth/you heard us right.ogg",length=0.80723356009070291},}
+L["youre a big enough shmuck to come to big bell hells cars"]={{path="chatsounds/autoadd/vuth/youre a big enough shmuck to come to big bell hells cars.ogg",length=2.8829931972789113},}
+L["youre fucked six ways from sunday"]={{path="chatsounds/autoadd/vuth/youre fucked six ways from sunday.ogg",length=1.563469387755102},}
+L["youre letting him go"]={{path="chatsounds/autoadd/vuth/youre letting him go.ogg",length=1.1857823129251701},}
 c.EndList()
