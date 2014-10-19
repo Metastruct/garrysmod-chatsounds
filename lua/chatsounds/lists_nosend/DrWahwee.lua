ff63e034a65fa2bb7b2efbcdc34133f3eace5bb9
gigispha@gmail.com <gigispha@gmail.com>
2014-10-19 18:19:15 +0000

    insane close-up

---
+++ lua/chatsounds/lists_nosend/DrWahwee.lua
@@ -0,0 +1,15 @@
+c.StartList("DrWahwee")
+L["but lahve nevah ceen dis type on erth"]={{path="chatsounds/autoadd/DrWahwee/but lahve nevah ceen dis type on erth.ogg",length=1.8527437641723357},}
+L["dats da gewd querstin"]={{path="chatsounds/autoadd/DrWahwee/dats da gewd querstin.ogg",length=1.0707482993197279},}
+L["enahgy ressources"]={{path="chatsounds/autoadd/DrWahwee/enahgy ressources.ogg",length=1.3564625850340135},}
+L["i do no were dis enerrgee cam from butt it cannot let fall intu doctah wahwees hands"]={{path="chatsounds/autoadd/DrWahwee/i do no were dis enerrgee cam from butt it cannot let fall intu doctah wahwees hands.ogg",length=4.8844897959183671},}
+L["megaman"]={{path="chatsounds/autoadd/DrWahwee/megaman.ogg",length=0.72183673469387755},}
+L["seems tah be enahgy ressources"]={{path="chatsounds/autoadd/DrWahwee/seems tah be enahgy ressources.ogg",length=1.9489795918367347},}
+L["theese"]={{path="chatsounds/autoadd/DrWahwee/theese.ogg",length=1.1549659863945578},}
+L["w megaman"]={{path="chatsounds/autoadd/DrWahwee/w megaman.ogg",length=0.7820181405895692},}
+L["we dont know who he is and its dangerous to leave him alone"]={{path="chatsounds/autoadd/DrWahwee/we dont know who he is and its dangerous to leave him alone.ogg",length=3.1520634920634922},}
+L["we may a be abl 2 locat n othah enahgee umission frum duh radahr room"]={{path="chatsounds/autoadd/DrWahwee/we may a be abl 2 locat n othah enahgee umission frum duh radahr room.ogg",length=3.7536054421768705},}
+L["wen we find dat meteah weel find doctah wahwee"]={{path="chatsounds/autoadd/DrWahwee/wen we find dat meteah weel find doctah wahwee.ogg",length=3.0076870748299318},}
+L["ya must recovah all de enahgee imedetly w megaman"]={{path="chatsounds/autoadd/DrWahwee/ya must recovah all de enahgee imedetly w megaman.ogg",length=2.6467800453514738},}
+L["you must go after him"]={{path="chatsounds/autoadd/DrWahwee/you must go after him.ogg",length=1.2512018140589569},}
+c.EndList()
