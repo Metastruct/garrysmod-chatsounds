02aec18433706c04c8fd4419618def4caf7c924a
LUModder@gmail.com <lumodder@gmail.com>
2014-11-12 00:57:45 +0000

    Fixed up a lot of my chatsounds. - Moved Glitch Cricket chatsounds to my main folder. - Moved Scout's Amazing Adventure chatsounds to separate folder. - Added more SAA chatsounds.

--- lua/chatsounds/lists_nosend/lumodder.lua
+++ lua/chatsounds/lists_nosend/lumodder.lua
@@ -1,9 +1,9 @@
 c.StartList("lumodder")
 L["but my hope will never die"]={{path="chatsounds/autoadd/lumodder/but my hope will never die.ogg",length=1.5582086167800453},}
-L["glitchcricket"]={{path="chatsounds/autoadd/lumodder/glitchcricket/there he goes the willy wonga.ogg",length=3.0888435374149661},{path="chatsounds/autoadd/lumodder/glitchcricket/there you fn go you cunt bastard.ogg",length=5.8619501133786844},}
-L["scoutsamazingadv"]={{path="chatsounds/autoadd/lumodder/scoutsamazingadv/and you are faggot.ogg",length=1.5845578231292516},{path="chatsounds/autoadd/lumodder/scoutsamazingadv/no u.ogg",length=0.93854875283446715},}
 L["suck my balls doc"]={{path="chatsounds/autoadd/lumodder/suck my balls doc.ogg",length=1.5905668934240362},}
 L["thats racist"]={{path="chatsounds/autoadd/lumodder/thats racist.ogg",length=1.1724263038548752},}
+L["there he goes the willy wonga"]={{path="chatsounds/autoadd/lumodder/there he goes the willy wonga.ogg",length=3.0888435374149661},}
+L["there you fn go you cunt bastard"]={{path="chatsounds/autoadd/lumodder/there you fn go you cunt bastard.ogg",length=5.8619501133786844},}
 L["trainrain"]={{path="chatsounds/autoadd/lumodder/trainrain.ogg",length=3.0040816326530613},}
 L["triple x rated"]={{path="chatsounds/autoadd/lumodder/triple x rated.ogg",length=3.4309070294784583},}
 L["whos been drawing dicks"]={{path="chatsounds/autoadd/lumodder/whos been drawing dicks.ogg",length=1.5914739229024943},}
