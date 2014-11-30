ee8af799e54cfab9ab4cd8d09eba1f7d96b273ec
verbalsilence@hotmail.com <verbalsilence@hotmail.com>
2014-11-30 20:55:06 +0000

    shh it's all part of the plan

---
+++ lua/chatsounds/lists_nosend/blue danube.lua
@@ -0,0 +1,5 @@
+c.StartList("blue danube")
+L["blue danube 1"]={{path="chatsounds/autoadd/blue danube/blue danube 1.ogg",length=199.80789115646257}}
+L["blue danube 2"]={{path="chatsounds/autoadd/blue danube/blue danube 2.ogg",length=194.53117913832199}}
+L["blue danube 3"]={{path="chatsounds/autoadd/blue danube/blue danube 3.ogg",length=262.16723356009072}}
+c.EndList()
