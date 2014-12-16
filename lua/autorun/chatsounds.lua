501cf7f3d36d70785d91271b4ab940ea7ba4972b
Python1320 <python1320@users.noreply.github.com>
2014-12-16 21:25:17 +0000

    now uses coroutines to slow down processing TODO: cvar/algorithm to adjust slowdown

--- lua/autorun/chatsounds.lua
+++ lua/autorun/chatsounds.lua
@@ -1,4 +1,6 @@
 if CLIENT then
+
+	include("chatsounds/co.lua")
 	include("chatsounds/cl_core.lua")
 	include("chatsounds/capsadmins_font_cache_thing.lua")
 	include("chatsounds/autocomplete.lua")
@@ -12,6 +14,7 @@ if SERVER then
 	AddCSLuaFile("autorun/chatsounds.lua")
 	AddCSLuaFile("chatsounds/capsadmins_font_cache_thing.lua")
 	AddCSLuaFile("chatsounds/autocomplete.lua")
+	AddCSLuaFile("chatsounds/co.lua")
 
 	include("chatsounds/sv_core.lua")
 end
