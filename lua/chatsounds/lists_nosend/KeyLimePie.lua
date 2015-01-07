f3d9e34ec93fe657ce5118b8d686907d279c8fbc
v.k.asakura@gmail.com <v.k.asakura@gmail.com>
2015-01-07 17:59:30 +0000

    msa knackich du bist ein noob

--- lua/chatsounds/lists_nosend/KeyLimePie.lua
+++ lua/chatsounds/lists_nosend/KeyLimePie.lua
@@ -3,11 +3,13 @@ L["aaany second now"]={{path="chatsounds/autoadd/KeyLimePie/aaany second now.ogg
 L["bochum ich haeng an dir"]={{path="chatsounds/autoadd/KeyLimePie/bochum ich haeng an dir.ogg",length=3.0824489795918368}}
 L["bochum ich komm aus dir"]={{path="chatsounds/autoadd/KeyLimePie/bochum ich komm aus dir.ogg",length=3.4946031746031747}}
 L["celebrate"]={{path="chatsounds/autoadd/KeyLimePie/celebrate/celebrate1.ogg",length=4.2376417233560089},{path="chatsounds/autoadd/KeyLimePie/celebrate/celebrate2.ogg",length=4.0010884353741494}}
+L["du bist ein noob"]={{path="chatsounds/autoadd/KeyLimePie/du bist ein noob.ogg",length=19.171836734693876}}
 L["good morning vietnam"]={{path="chatsounds/autoadd/KeyLimePie/good morning vietnam.ogg",length=4.0959637188208617}}
 L["hah gay remix"]={{path="chatsounds/autoadd/KeyLimePie/hah gay remix.ogg",length=15.928888888888888}}
 L["ice cream is a feeling"]={{path="chatsounds/autoadd/KeyLimePie/ice cream is a feeling.ogg",length=2.5135600907029478}}
 L["ich mein versteht mich nicht falsch"]={{path="chatsounds/autoadd/KeyLimePie/ich mein versteht mich nicht falsch.ogg",length=1.223922902494331}}
 L["its a celebration"]={{path="chatsounds/autoadd/KeyLimePie/its a celebration.ogg",length=2.1754195011337867}}
+L["knackich"]={{path="chatsounds/autoadd/KeyLimePie/knackich/knackich1.ogg",length=0.6897278911564626},{path="chatsounds/autoadd/KeyLimePie/knackich/knackich2.ogg",length=0.62249433106575969}}
 L["little babys ice cream"]={{path="chatsounds/autoadd/KeyLimePie/little babys ice cream.ogg",length=1.5615419501133787}}
 L["so ein scheiss"]={{path="chatsounds/autoadd/KeyLimePie/so ein scheiss.ogg",length=1.4237414965986395}}
 L["southpark"]={{path="chatsounds/autoadd/KeyLimePie/southpark.ogg",length=4.7600907029478456}}
