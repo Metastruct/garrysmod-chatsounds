efbca7d0c14f45f3d3c2dab79e6ae7a5f3eb2cf6
v.k.asakura@gmail.com <v.k.asakura@gmail.com>
2015-01-11 03:01:13 +0000

    ARE YOU READY FOR A MIRACLE??!!

---
+++ lua/chatsounds/lists_nosend/miracle.lua
@@ -0,0 +1,8 @@
+c.StartList("miracle")
+L["are you ready"]={{path="chatsounds/autoadd/miracle/are you ready/are you ready1.ogg",length=1.2514965986394557},{path="chatsounds/autoadd/miracle/are you ready/are you ready2.ogg",length=0.69804988662131517},{path="chatsounds/autoadd/miracle/are you ready/are you ready3.ogg",length=1.3519954648526078},{path="chatsounds/autoadd/miracle/are you ready/are you ready4.ogg",length=0.64181405895691612}}
+L["are you ready for a miracle"]={{path="chatsounds/autoadd/miracle/are you ready for a miracle/are you ready for a miracle1.ogg",length=1.9142630385487529},{path="chatsounds/autoadd/miracle/are you ready for a miracle/are you ready for a miracle2.ogg",length=1.809546485260771},{path="chatsounds/autoadd/miracle/are you ready for a miracle/are you ready for a miracle3.ogg",length=1.8117460317460317}}
+L["im ready im ready for a miracle"]={{path="chatsounds/autoadd/miracle/im ready im ready for a miracle.ogg",length=3.209342403628118}}
+L["ready as i can be"]={{path="chatsounds/autoadd/miracle/ready as i can be/ready as i can be1.ogg",length=1.2461224489795919},{path="chatsounds/autoadd/miracle/ready as i can be/ready as i can be2.ogg",length=1.3143310657596372}}
+L["ready"]={{path="chatsounds/autoadd/miracle/ready.ogg",length=0.4260544217687075}}
+L["spirit will set you free"]={{path="chatsounds/autoadd/miracle/spirit will set you free/spirit will set you free1.ogg",length=1.1644671201814059},{path="chatsounds/autoadd/miracle/spirit will set you free/spirit will set you free2.ogg",length=1.4420181405895691}}
+c.EndList()
