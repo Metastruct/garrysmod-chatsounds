02f75d73eed2ea515e81666312d221586d66d765
LUModder@gmail.com <lumodder@gmail.com>
2015-01-19 03:18:05 +0000

    take a closer look at that snout and take a closer look at that meme

--- lua/chatsounds/lists_nosend/lumodder.lua
+++ lua/chatsounds/lists_nosend/lumodder.lua
@@ -1,6 +1,8 @@
 c.StartList("lumodder")
 L["but my hope will never die"]={{path="chatsounds/autoadd/lumodder/but my hope will never die.ogg",length=1.5582086167800453}}
 L["suck my balls doc"]={{path="chatsounds/autoadd/lumodder/suck my balls doc.ogg",length=1.5905668934240362}}
+L["take a closer look at that meme"]={{path="chatsounds/autoadd/lumodder/take a closer look at that meme.ogg",length=2.175873015873016}}
+L["take a closer look at that snout"]={{path="chatsounds/autoadd/lumodder/take a closer look at that snout.ogg",length=2.1247619047619049}}
 L["thats racist"]={{path="chatsounds/autoadd/lumodder/thats racist.ogg",length=1.1724263038548752}}
 L["there he goes the willy wonga"]={{path="chatsounds/autoadd/lumodder/there he goes the willy wonga.ogg",length=3.0888435374149661}}
 L["there you fn go you cunt bastard"]={{path="chatsounds/autoadd/lumodder/there you fn go you cunt bastard.ogg",length=5.8619501133786844}}
