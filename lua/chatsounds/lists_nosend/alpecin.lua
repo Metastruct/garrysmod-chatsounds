2b500de455b8d588758cbfd7d6d4b17663a064d8
v.k.asakura@gmail.com <v.k.asakura@gmail.com>
2015-01-27 00:14:21 +0000

    in der tat dr klenk!

---
+++ lua/chatsounds/lists_nosend/alpecin.lua
@@ -0,0 +1,30 @@
+c.StartList("alpecin")
+L["ab morgen"]={{path="chatsounds/autoadd/alpecin/ab morgen.ogg",length=0.61900226757369614}}
+L["alpecin"]={{path="chatsounds/autoadd/alpecin/alpecin/alpecin 1.ogg",length=0.61492063492063487},{path="chatsounds/autoadd/alpecin/alpecin/alpecin 2.ogg",length=0.72002267573696144},{path="chatsounds/autoadd/alpecin/alpecin/alpecin 3.ogg",length=0.56399092970521547}}
+L["damit hat alpecin viel aufsehen erregt"]={{path="chatsounds/autoadd/alpecin/damit hat alpecin viel aufsehen erregt.ogg",length=2.0726530612244898}}
+L["die wachstumsphasen der haarwurzeln zu verlaengern"]={{path="chatsounds/autoadd/alpecin/die wachstumsphasen der haarwurzeln zu verlaengern.ogg",length=2.0960544217687076}}
+L["doping"]={{path="chatsounds/autoadd/alpecin/doping/doping 1.ogg",length=0.47335600907029479},{path="chatsounds/autoadd/alpecin/doping/doping 2.ogg",length=0.52380952380952384}}
+L["doping fuer die haare"]={{path="chatsounds/autoadd/alpecin/doping fuer die haare.ogg",length=1.2289115646258504}}
+L["dr klenk"]={{path="chatsounds/autoadd/alpecin/dr klenk.ogg",length=0.65215419501133787}}
+L["erblich bedingtem haarausfall"]={{path="chatsounds/autoadd/alpecin/erblich bedingtem haarausfall.ogg",length=1.1452834467120181}}
+L["erblich"]={{path="chatsounds/autoadd/alpecin/erblich.ogg",length=0.35863945578231293}}
+L["erregt"]={{path="chatsounds/autoadd/alpecin/erregt.ogg",length=0.5143083900226757}}
+L["fuer die haare"]={{path="chatsounds/autoadd/alpecin/fuer die haare.ogg",length=0.72126984126984128}}
+L["haarausfall"]={{path="chatsounds/autoadd/alpecin/haarausfall.ogg",length=0.44546485260770974}}
+L["haare"]={{path="chatsounds/autoadd/alpecin/haare/haare 1.ogg",length=0.48732426303854876},{path="chatsounds/autoadd/alpecin/haare/haare 2.ogg",length=0.45276643990929705},{path="chatsounds/autoadd/alpecin/haare/haare 3.ogg",length=0.47278911564625853}}
+L["haarwurzel"]={{path="chatsounds/autoadd/alpecin/haarwurzel.ogg",length=0.45301587301587304}}
+L["in der tat"]={{path="chatsounds/autoadd/alpecin/in der tat.ogg",length=0.75306122448979596}}
+L["jeden morgen"]={{path="chatsounds/autoadd/alpecin/jeden morgen.ogg",length=0.58004535147392289}}
+L["kann alpecin durch koffein wirklich vor erblich bedingtem haarausfall schuetzen"]={{path="chatsounds/autoadd/alpecin/kann alpecin durch koffein wirklich vor erblich bedingtem haarausfall schuetzen.ogg",length=3.7665079365079364}}
+L["koffein"]={{path="chatsounds/autoadd/alpecin/koffein.ogg",length=0.53453514739229024}}
+L["koffeinshampoo"]={{path="chatsounds/autoadd/alpecin/koffeinshampoo.ogg",length=0.92575963718820864}}
+L["laborchef"]={{path="chatsounds/autoadd/alpecin/laborchef.ogg",length=0.55299319727891161}}
+L["liquid"]={{path="chatsounds/autoadd/alpecin/liquid.ogg",length=0.62444444444444447}}
+L["nur fuer die haare"]={{path="chatsounds/autoadd/alpecin/nur fuer die haare.ogg",length=1.0366439909297052}}
+L["schuetzen"]={{path="chatsounds/autoadd/alpecin/schuetzen.ogg",length=0.53281179138321999}}
+L["verlaengern"]={{path="chatsounds/autoadd/alpecin/verlaengern.ogg",length=0.41485260770975058}}
+L["wachstumsphasen"]={{path="chatsounds/autoadd/alpecin/wachstumsphasen.ogg",length=0.75}}
+L["weil es dazu beitraegt"]={{path="chatsounds/autoadd/alpecin/weil es dazu beitraegt.ogg",length=1.209297052154195}}
+L["wir fragten laborchef dr klenk"]={{path="chatsounds/autoadd/alpecin/wir fragten laborchef dr klenk.ogg",length=1.8053514739229024}}
+L["wirklich"]={{path="chatsounds/autoadd/alpecin/wirklich.ogg",length=0.34000000000000002}}
+c.EndList()
