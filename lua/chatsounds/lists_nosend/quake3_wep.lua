cef31005bd60b0c9d97b5b98d9da41ad0c2a3c9a
PotcFdk <PotcFdk@users.noreply.github.com>
2015-03-07 14:49:13 +0000

    Those are sounds for weapons, not for the chat.

--- lua/chatsounds/lists_nosend/quake3_wep.lua
+++
@@ -1,5 +0,0 @@
-c.StartList("quake3_wep")
-L["lightning"]={{path="chatsounds/autoadd/quake3_wep/lightning.ogg",length=2.7684353741496599}}
-L["railgun"]={{path="chatsounds/autoadd/quake3_wep/railgun.ogg",length=1.5505668934240362}}
-L["shotgun"]={{path="chatsounds/autoadd/quake3_wep/shotgun.ogg",length=1.9857596371882087}}
-c.EndList()
