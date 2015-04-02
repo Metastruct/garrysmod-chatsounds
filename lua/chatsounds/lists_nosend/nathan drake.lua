f9f25e49006add38fd7a5a18351d14d6b570d0fe
kristianzxz41@gmail.com <kristianzxz41@gmail.com>
2015-04-02 15:27:42 +0000

    Im bored have some random sounds. -Avast AntiPony 9445

---
+++ lua/chatsounds/lists_nosend/nathan drake.lua
@@ -0,0 +1,13 @@
+c.StartList("nathan drake")
+L["bingo"]={{path="chatsounds/autoadd/nathan drake/bingo/1.ogg",length=0.62693877551020405},{path="chatsounds/autoadd/nathan drake/bingo/2.ogg",length=0.65306122448979587}}
+L["dont mind if i do"]={{path="chatsounds/autoadd/nathan drake/dont mind if i do.ogg",length=1.1755102040816328}}
+L["five finger discount my favorite"]={{path="chatsounds/autoadd/nathan drake/five finger discount my favorite.ogg",length=2.0375510204081633}}
+L["hello"]={{path="chatsounds/autoadd/nathan drake/hello.ogg",length=0.88816326530612244}}
+L["here we go"]={{path="chatsounds/autoadd/nathan drake/here we go.ogg",length=0.75755102040816324}}
+L["i got a clip with your name on it"]={{path="chatsounds/autoadd/nathan drake/i got a clip with your name on it.ogg",length=1.6979591836734693}}
+L["oh yeah"]={{path="chatsounds/autoadd/nathan drake/oh yeah.ogg",length=0.62693877551020405}}
+L["sure"]={{path="chatsounds/autoadd/nathan drake/sure.ogg",length=0.73142857142857143}}
+L["what do we have here"]={{path="chatsounds/autoadd/nathan drake/what do we have here.ogg",length=1.0971428571428572}}
+L["why would someone leave this here"]={{path="chatsounds/autoadd/nathan drake/why would someone leave this here/1.ogg",length=1.3322448979591837},{path="chatsounds/autoadd/nathan drake/why would someone leave this here/2.ogg",length=1.5412244897959184}}
+L["yes please"]={{path="chatsounds/autoadd/nathan drake/yes please.ogg",length=0.70530612244897961}}
+c.EndList()
