5792f5b5f0836d3141ef2c319216c9205dc4ea2e
FailCake <edu444@gmail.com>
2015-04-05 03:20:48 +0100

    Fixed

--- lua/chatsounds/lists_nosend/dergcake.lua
+++ lua/chatsounds/lists_nosend/dergcake.lua
@@ -1,22 +1,22 @@
 c.StartList("dergcake")
-L["darklow"]={{path="chatsounds/autoadd/dergcake/darklow.ogg",length=0.82798185941043079}}
-L["darklow2"]={{path="chatsounds/autoadd/dergcake/darklow2.ogg",length=0.75600907029478459}}
-L["endgame"]={{path="chatsounds/autoadd/dergcake/endgame.ogg",length=54.02122448979592}}
-L["endgame2"]={{path="chatsounds/autoadd/dergcake/endgame2.ogg",length=54.177959183673472}}
-L["endgame3"]={{path="chatsounds/autoadd/dergcake/endgame3.ogg",length=53.342040816326531}}
-L["endgame4"]={{path="chatsounds/autoadd/dergcake/endgame4.ogg",length=53.342040816326531}}
-L["endgame5"]={{path="chatsounds/autoadd/dergcake/endgame5.ogg",length=53.655510204081629}}
-L["endgame6"]={{path="chatsounds/autoadd/dergcake/endgame6.ogg",length=52.479999999999997}}
-L["failcake"]={{path="chatsounds/autoadd/dergcake/failcake.ogg",length=1.0079818594104308}}
-L["failcake2"]={{path="chatsounds/autoadd/dergcake/failcake2.ogg",length=0.79201814058956921}}
+L["aleluia"]={{path="chatsounds/autoadd/dergcake/ALELUIA.ogg",length=2.3219954648526078}}
+L["beepfckingboop"]={{path="chatsounds/autoadd/dergcake/beepfckingboop.ogg",length=1.349750566893424}}
+L["behold"]={{path="chatsounds/autoadd/dergcake/behold.ogg",length=6.3158276643990927}}
+L["crushyou"]={{path="chatsounds/autoadd/dergcake/crushyou.ogg",length=2.1362358276643989}}
+L["darklow"]={{path="chatsounds/autoadd/dergcake/darklow/darklow1.ogg",length=0.82798185941043079},{path="chatsounds/autoadd/dergcake/darklow/darklow2.ogg",length=0.75600907029478459}}
+L["endgame"]={{path="chatsounds/autoadd/dergcake/endgame/endgame1.ogg",length=54.02122448979592},{path="chatsounds/autoadd/dergcake/endgame/endgame2.ogg",length=54.177959183673472},{path="chatsounds/autoadd/dergcake/endgame/endgame3.ogg",length=53.342040816326531},{path="chatsounds/autoadd/dergcake/endgame/endgame4.ogg",length=53.342040816326531},{path="chatsounds/autoadd/dergcake/endgame/endgame5.ogg",length=53.655510204081629},{path="chatsounds/autoadd/dergcake/endgame/endgame6.ogg",length=52.479999999999997}}
+L["failcake"]={{path="chatsounds/autoadd/dergcake/failcake/failcake1.ogg",length=1.0079818594104308},{path="chatsounds/autoadd/dergcake/failcake/failcake2.ogg",length=0.79201814058956921}}
 L["flex"]={{path="chatsounds/autoadd/dergcake/flex.ogg",length=0.90000000000000002}}
+L["fully operational"]={{path="chatsounds/autoadd/dergcake/fully operational.ogg",length=3.6000000000000001}}
 L["henke"]={{path="chatsounds/autoadd/dergcake/henke.ogg",length=0.79199546485260774}}
-L["levelup"]={{path="chatsounds/autoadd/dergcake/levelup.ogg",length=2.4607709750566893}}
-L["levelup2"]={{path="chatsounds/autoadd/dergcake/levelup2.ogg",length=2.4607709750566893}}
+L["i baked a cake just for you"]={{path="chatsounds/autoadd/dergcake/i baked a cake just for you.ogg",length=48.479999999999997}}
+L["levelup"]={{path="chatsounds/autoadd/dergcake/levelup/levelup1.ogg",length=2.4607709750566893},{path="chatsounds/autoadd/dergcake/levelup/levelup2.ogg",length=2.4607709750566893}}
+L["meow"]={{path="chatsounds/autoadd/dergcake/meow/meow1.ogg",length=1.1333106575963718},{path="chatsounds/autoadd/dergcake/meow/meow2.ogg",length=1.0666666666666667},{path="chatsounds/autoadd/dergcake/meow/meow3.ogg",length=1.2333106575963719}}
 L["mulher"]={{path="chatsounds/autoadd/dergcake/mulher.ogg",length=0.68399092970521547}}
 L["notcake"]={{path="chatsounds/autoadd/dergcake/notcake.ogg",length=0.75600907029478459}}
-L["pro_skil"]={{path="chatsounds/autoadd/dergcake/pro_skil.ogg",length=10.031020408163265}}
-L["pro_skill2"]={{path="chatsounds/autoadd/dergcake/pro_skill2.ogg",length=14.706938775510205}}
+L["pro_skill"]={{path="chatsounds/autoadd/dergcake/pro_skill/pro_skill1.ogg",length=10.031020408163265},{path="chatsounds/autoadd/dergcake/pro_skill/pro_skill2.ogg",length=14.706938775510205}}
+L["protect humens"]={{path="chatsounds/autoadd/dergcake/protect humens.ogg",length=2.1973696145124717}}
+L["system failure"]={{path="chatsounds/autoadd/dergcake/system failure.ogg",length=6.5666439909297054}}
 L["version"]={{path="chatsounds/autoadd/dergcake/version.ogg",length=0.79201814058956921}}
 L["yaykids"]={{path="chatsounds/autoadd/dergcake/yaykids.ogg",length=22.780589569160998}}
 c.EndList()
