2d7e80e6d2933e5b04586edccc914f5fa541073a
SpiralP <spiral.palmer@gmail.com>
2015-04-26 15:09:26 -0700

    from @Black-Mage-8

---
+++ lua/chatsounds/lists_nosend/Black_Mage.lua
@@ -0,0 +1,4 @@
+c.StartList("Black_Mage")
+L["dontloseyourway"]={{path="chatsounds/autoadd/Black_Mage/dontloseyourway.ogg",length=35.899999999999999}}
+L["keel over and die"]={{path="chatsounds/autoadd/Black_Mage/keel over and die.ogg",length=3.0563265306122447}}
+c.EndList()
