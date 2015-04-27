a7e278e1b1ff4022aae0a6a3630531194ddd1f98
Foxy Fazbear <sasuke_uchiha13@hotmail.co.uk>
2015-04-27 21:29:16 +0100

    added nightistandsad.ogg to Black_Mage folder.

--- lua/chatsounds/lists_nosend/Black_Mage.lua
+++ lua/chatsounds/lists_nosend/Black_Mage.lua
@@ -1,4 +1,5 @@
 c.StartList("Black_Mage")
 L["dontloseyourway"]={{path="chatsounds/autoadd/Black_Mage/dontloseyourway.ogg",length=35.899999999999999}}
 L["keel over and die"]={{path="chatsounds/autoadd/Black_Mage/keel over and die.ogg",length=3.0563265306122447}}
+L["nightistandsad"]={{path="chatsounds/autoadd/Black_Mage/nightistandsad.ogg",length=97.200000000000003}}
 c.EndList()
