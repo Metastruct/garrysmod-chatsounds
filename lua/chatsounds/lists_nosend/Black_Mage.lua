144067cd75518ff42c9ac76eaa97b930678b652c
Foxy Fazbear <sasuke_uchiha13@hotmail.co.uk>
2015-04-29 21:24:03 +0100

    added chatsounds Kappa

--- lua/chatsounds/lists_nosend/Black_Mage.lua
+++ lua/chatsounds/lists_nosend/Black_Mage.lua
@@ -2,4 +2,6 @@ c.StartList("Black_Mage")
 L["dontloseyourway"]={{path="chatsounds/autoadd/Black_Mage/dontloseyourway.ogg",length=35.899999999999999}}
 L["keel over and die"]={{path="chatsounds/autoadd/Black_Mage/keel over and die.ogg",length=3.0563265306122447}}
 L["nightistandsad"]={{path="chatsounds/autoadd/Black_Mage/nightistandsad.ogg",length=97.200000000000003}}
+L["penis"]={{path="chatsounds/autoadd/Black_Mage/penis.ogg",length=0.90000000000000002}}
+L["that that you let a penis penetrate your friendship penis"]={{path="chatsounds/autoadd/Black_Mage/that that you let a penis penetrate your friendship penis.ogg",length=5.9000000000000004}}
 c.EndList()
