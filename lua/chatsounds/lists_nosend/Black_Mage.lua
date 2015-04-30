80bfca4249d4aee697be5c7073ef8e0eadc99b71
Foxy Fazbear <sasuke_uchiha13@hotmail.co.uk>
2015-04-30 17:33:15 +0100

    added a 'few' things lel

--- lua/chatsounds/lists_nosend/Black_Mage.lua
+++ lua/chatsounds/lists_nosend/Black_Mage.lua
@@ -1,7 +1,13 @@
 c.StartList("Black_Mage")
+L["bayonettacontinueno"]={{path="chatsounds/autoadd/Black_Mage/bayonettacontinueno.ogg",length=5.2395238095238099}}
+L["bayonettacontinuenodeath"]={{path="chatsounds/autoadd/Black_Mage/bayonettacontinuenodeath.ogg",length=4.5848299319727888}}
+L["bayonettacontinueyes"]={{path="chatsounds/autoadd/Black_Mage/bayonettacontinueyes.ogg",length=4.983605442176871}}
+L["bayonettagameover"]={{path="chatsounds/autoadd/Black_Mage/bayonettagameover.ogg",length=8.7903174603174605}}
 L["dontloseyourway"]={{path="chatsounds/autoadd/Black_Mage/dontloseyourway.ogg",length=35.899999999999999}}
+L["jeannecontinueno"]={{path="chatsounds/autoadd/Black_Mage/jeannecontinueno.ogg",length=4.8476190476190473}}
 L["keel over and die"]={{path="chatsounds/autoadd/Black_Mage/keel over and die.ogg",length=3.0563265306122447}}
 L["nightistandsad"]={{path="chatsounds/autoadd/Black_Mage/nightistandsad.ogg",length=97.200000000000003}}
 L["penis"]={{path="chatsounds/autoadd/Black_Mage/penis.ogg",length=0.90000000000000002}}
 L["that that you let a penis penetrate your friendship penis"]={{path="chatsounds/autoadd/Black_Mage/that that you let a penis penetrate your friendship penis.ogg",length=5.9000000000000004}}
+L["wotwtripod"]={{path="chatsounds/autoadd/Black_Mage/wotwtripod.ogg",length=9.06562358276644}}
 c.EndList()
