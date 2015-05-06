3ff43061cf9960caf81579dddc9bd2b8987bd76a
Foxy Fazbear <sasuke_uchiha13@hotmail.co.uk>
2015-05-06 20:27:19 +0100

    Added moar sounds!

--- lua/chatsounds/lists_nosend/Black_Mage.lua
+++ lua/chatsounds/lists_nosend/Black_Mage.lua
@@ -6,6 +6,8 @@ L["bayonettagameover"]={{path="chatsounds/autoadd/Black_Mage/bayonettagameover.o
 L["dontloseyourway"]={{path="chatsounds/autoadd/Black_Mage/dontloseyourway.ogg",length=35.899999999999999}}
 L["jeannecontinueno"]={{path="chatsounds/autoadd/Black_Mage/jeannecontinueno.ogg",length=4.8476190476190473}}
 L["keel over and die"]={{path="chatsounds/autoadd/Black_Mage/keel over and die.ogg",length=3.0563265306122447}}
+L["letsboomboom"]={{path="chatsounds/autoadd/Black_Mage/letsboomboom.ogg",length=2.3250113378684807}}
+L["mindblown"]={{path="chatsounds/autoadd/Black_Mage/mindblown.ogg",length=23.797551020408164}}
 L["nightistandsad"]={{path="chatsounds/autoadd/Black_Mage/nightistandsad.ogg",length=97.200000000000003}}
 L["penis"]={{path="chatsounds/autoadd/Black_Mage/penis.ogg",length=0.90000000000000002}}
 L["that that you let a penis penetrate your friendship penis"]={{path="chatsounds/autoadd/Black_Mage/that that you let a penis penetrate your friendship penis.ogg",length=5.9000000000000004}}
