a2978c4f0b576757f0a1e4dafafe3cb58ef5c9d7
Xerasin <phoenixteam20@gmail.com>
2015-05-07 22:22:25 -0700

    sorry eek

---
+++ lua/chatsounds/lists_nosend/hatintime.lua
@@ -0,0 +1,3 @@
+c.StartList("hatintime")
+L["boop"]={{path="chatsounds/autoadd/hatintime/boop/boop1.ogg",length=0.56492063492063493},{path="chatsounds/autoadd/hatintime/boop/boop2.ogg",length=0.7559410430839002}}
+c.EndList()
