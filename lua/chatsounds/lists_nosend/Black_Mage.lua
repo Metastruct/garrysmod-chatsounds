35c93d16872d5491731787b83b6bb10ab3b45403
Foxy Fazbear <sasuke_uchiha13@hotmail.co.uk>
2015-05-09 00:59:20 +0100

    Some Smashing sounds

--- lua/chatsounds/lists_nosend/Black_Mage.lua
+++ lua/chatsounds/lists_nosend/Black_Mage.lua
@@ -1,16 +1,19 @@
 c.StartList("Black_Mage")
+L["ack ook ich ach bluah"]={{path="chatsounds/autoadd/Black_Mage/ack ook ich ach bluah.ogg",length=2}}
 L["awesome mmm bye"]={{path="chatsounds/autoadd/Black_Mage/awesome mmm bye.ogg",length=6.5009977324263035}}
 L["bayonettacontinueno"]={{path="chatsounds/autoadd/Black_Mage/bayonettacontinueno.ogg",length=5.2395238095238099}}
 L["bayonettacontinuenodeath"]={{path="chatsounds/autoadd/Black_Mage/bayonettacontinuenodeath.ogg",length=4.5848299319727888}}
 L["bayonettacontinueyes"]={{path="chatsounds/autoadd/Black_Mage/bayonettacontinueyes.ogg",length=4.983605442176871}}
 L["bayonettagameover"]={{path="chatsounds/autoadd/Black_Mage/bayonettagameover.ogg",length=8.7903174603174605}}
 L["dontloseyourway"]={{path="chatsounds/autoadd/Black_Mage/dontloseyourway.ogg",length=35.899999999999999}}
+L["ill do this with my hands blargharghargh"]={{path="chatsounds/autoadd/Black_Mage/ill do this with my hands blargharghargh.ogg",length=4.6404308390022679}}
 L["jeannecontinueno"]={{path="chatsounds/autoadd/Black_Mage/jeannecontinueno.ogg",length=4.8476190476190473}}
 L["keel over and die"]={{path="chatsounds/autoadd/Black_Mage/keel over and die.ogg",length=3.0563265306122447}}
 L["letsboomboom"]={{path="chatsounds/autoadd/Black_Mage/letsboomboom.ogg",length=2.3250113378684807}}
 L["mindblown"]={{path="chatsounds/autoadd/Black_Mage/mindblown.ogg",length=23.797551020408164}}
 L["nightistandsad"]={{path="chatsounds/autoadd/Black_Mage/nightistandsad.ogg",length=97.200000000000003}}
 L["penis"]={{path="chatsounds/autoadd/Black_Mage/penis.ogg",length=0.90000000000000002}}
+L["smashing"]={{path="chatsounds/autoadd/Black_Mage/smashing.ogg",length=1.5571428571428572}}
 L["that that you let a penis penetrate your friendship penis"]={{path="chatsounds/autoadd/Black_Mage/that that you let a penis penetrate your friendship penis.ogg",length=5.9000000000000004}}
 L["wotwtripod"]={{path="chatsounds/autoadd/Black_Mage/wotwtripod.ogg",length=9.06562358276644}}
 c.EndList()
