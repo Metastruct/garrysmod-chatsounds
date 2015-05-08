cb051a68eea9996cb6d66cdc459c414f2e8f5a60
Xerasin <phoenixteam20@gmail.com>
2015-05-08 13:28:09 -0700

    Hat in time sounds

--- lua/chatsounds/lists_nosend/hatintime.lua
+++ lua/chatsounds/lists_nosend/hatintime.lua
@@ -1,3 +1,32 @@
 c.StartList("hatintime")
+L["ack"]={{path="chatsounds/autoadd/hatintime/ack/1.ogg",length=0.17478458049886622},{path="chatsounds/autoadd/hatintime/ack/2.ogg",length=0.34750566893424034}}
+L["ah"]={{path="chatsounds/autoadd/hatintime/ah/1.ogg",length=0.3480498866213152},{path="chatsounds/autoadd/hatintime/ah/2.ogg",length=0.33458049886621316},{path="chatsounds/autoadd/hatintime/ah/3.ogg",length=0.19820861678004534},{path="chatsounds/autoadd/hatintime/ah/4.ogg",length=0.13510204081632654}}
+L["all mine"]={{path="chatsounds/autoadd/hatintime/all mine/1.ogg",length=1.1801133786848073},{path="chatsounds/autoadd/hatintime/all mine/2.ogg",length=1.1801133786848073}}
 L["boop"]={{path="chatsounds/autoadd/hatintime/boop/boop1.ogg",length=0.56492063492063493},{path="chatsounds/autoadd/hatintime/boop/boop2.ogg",length=0.7559410430839002}}
+L["five"]={{path="chatsounds/autoadd/hatintime/five/1.ogg",length=0.66326530612244894},{path="chatsounds/autoadd/hatintime/five/2.ogg",length=0.50662131519274378}}
+L["flying boat"]={{path="chatsounds/autoadd/hatintime/flying boat/1.ogg",length=10.211133786848073}}
+L["four"]={{path="chatsounds/autoadd/hatintime/four/1.ogg",length=0.7371428571428571},{path="chatsounds/autoadd/hatintime/four/2.ogg",length=0.55877551020408167}}
+L["gasp"]={{path="chatsounds/autoadd/hatintime/gasp/1.ogg",length=0.52623582766439914},{path="chatsounds/autoadd/hatintime/gasp/2.ogg",length=0.22852607709750566},{path="chatsounds/autoadd/hatintime/gasp/3.ogg",length=0.20995464852607709},{path="chatsounds/autoadd/hatintime/gasp/4.ogg",length=0.20716553287981859},{path="chatsounds/autoadd/hatintime/gasp/5.ogg",length=0.20480725623582766}}
+L["gotem"]={{path="chatsounds/autoadd/hatintime/gotem/1.ogg",length=0.43750566893424037},{path="chatsounds/autoadd/hatintime/gotem/2.ogg",length=0.60675736961451243}}
+L["hah"]={{path="chatsounds/autoadd/hatintime/hah/1.ogg",length=0.15222222222222223},{path="chatsounds/autoadd/hatintime/hah/2.ogg",length=0.20045351473922904}}
+L["hatintimeyell"]={{path="chatsounds/autoadd/hatintime/hatintimeyell/1.ogg",length=4.3552154195011337},{path="chatsounds/autoadd/hatintime/hatintimeyell/2.ogg",length=3.9205442176870746}}
+L["heh"]={{path="chatsounds/autoadd/hatintime/heh/1.ogg",length=0.16158730158730158},{path="chatsounds/autoadd/hatintime/heh/2.ogg",length=0.20226757369614512}}
+L["heyah"]={{path="chatsounds/autoadd/hatintime/heyah/1.ogg",length=0.37936507936507935},{path="chatsounds/autoadd/hatintime/heyah/2.ogg",length=0.2803174603174603}}
+L["hmph"]={{path="chatsounds/autoadd/hatintime/hmph/1.ogg",length=0.48653061224489796},{path="chatsounds/autoadd/hatintime/hmph/2.ogg",length=1.3306349206349206},{path="chatsounds/autoadd/hatintime/hmph/3.ogg",length=1.1861224489795918}}
+L["ho"]={{path="chatsounds/autoadd/hatintime/ho/1.ogg",length=0.27088435374149661},{path="chatsounds/autoadd/hatintime/ho/2.ogg",length=0.19439909297052155}}
+L["how rude"]={{path="chatsounds/autoadd/hatintime/how rude/1.ogg",length=1.4183900226757369}}
+L["huh"]={{path="chatsounds/autoadd/hatintime/huh/1.ogg",length=0.15369614512471655},{path="chatsounds/autoadd/hatintime/huh/2.ogg",length=0.13448979591836735}}
+L["hut"]={{path="chatsounds/autoadd/hatintime/hut/1.ogg",length=0.11965986394557823},{path="chatsounds/autoadd/hatintime/hut/2.ogg",length=0.10281179138321996}}
+L["nooo"]={{path="chatsounds/autoadd/hatintime/nooo/1.ogg",length=4.9355782312925172}}
+L["one"]={{path="chatsounds/autoadd/hatintime/one/1.ogg",length=0.68752834467120183},{path="chatsounds/autoadd/hatintime/one/2.ogg",length=0.56965986394557822}}
+L["ow"]={{path="chatsounds/autoadd/hatintime/ow/1.ogg",length=0.75934240362811789},{path="chatsounds/autoadd/hatintime/ow/2.ogg",length=0.29179138321995463}}
+L["three"]={{path="chatsounds/autoadd/hatintime/three/1.ogg",length=0.9045578231292517},{path="chatsounds/autoadd/hatintime/three/2.ogg",length=0.7146485260770975}}
+L["two"]={{path="chatsounds/autoadd/hatintime/two/1.ogg",length=0.76907029478458044},{path="chatsounds/autoadd/hatintime/two/2.ogg",length=0.57120181405895687}}
+L["uh ok"]={{path="chatsounds/autoadd/hatintime/uh ok/1.ogg",length=0.89249433106575959}}
+L["unf"]={{path="chatsounds/autoadd/hatintime/unf/1.ogg",length=0.34687074829931974},{path="chatsounds/autoadd/hatintime/unf/2.ogg",length=0.28303854875283446}}
+L["whoah"]={{path="chatsounds/autoadd/hatintime/whoah/1.ogg",length=0.34519274376417236},{path="chatsounds/autoadd/hatintime/whoah/2.ogg",length=0.42936507936507934}}
+L["ya"]={{path="chatsounds/autoadd/hatintime/ya/1.ogg",length=0.20573696145124717},{path="chatsounds/autoadd/hatintime/ya/2.ogg",length=0.29226757369614514},{path="chatsounds/autoadd/hatintime/ya/3.ogg",length=0.18870748299319728},{path="chatsounds/autoadd/hatintime/ya/4.ogg",length=0.19743764172335601}}
+L["yawn"]={{path="chatsounds/autoadd/hatintime/yawn/1.ogg",length=2.56562358276644}}
+L["yeah"]={{path="chatsounds/autoadd/hatintime/yeah/1.ogg",length=0.34485260770975057},{path="chatsounds/autoadd/hatintime/yeah/2.ogg",length=0.31002267573696146}}
+L["yikes"]={{path="chatsounds/autoadd/hatintime/yikes/1.ogg",length=0.44412698412698415},{path="chatsounds/autoadd/hatintime/yikes/2.ogg",length=0.43696145124716551}}
 c.EndList()
