2412319ad0f102a2718702b8435ae767cc04eb4b
Roland <mulletbull@hotmail.com>
2015-05-09 03:08:16 +0200

    more gamer poop sounds

--- lua/chatsounds/lists_nosend/Gamer_Poop_LA_Noire.lua
+++ lua/chatsounds/lists_nosend/Gamer_Poop_LA_Noire.lua
@@ -1,21 +1,40 @@
 c.StartList("Gamer_Poop_LA_Noire")
+L["dont do anything i wouldnt do partner"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/dont do anything i wouldnt do partner.ogg",length=2.6469160997732426}}
+L["dont do anything i wouldnt do"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/dont do anything i wouldnt do.ogg",length=1.7764625850340137}}
 L["dont tell me you actually jerk off phelps"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/dont tell me you actually jerk off phelps.ogg",length=1.5438321995464852}}
 L["go flap your cock somewhere else"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/go flap your cock somewhere else.ogg",length=1.7263492063492063}}
 L["heeey i love to jerk off"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/heeey i love to jerk off.ogg",length=1.7644671201814059}}
 L["hell no i like your ass"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/hell no i like your ass.ogg",length=1.8371428571428572}}
+L["i can assure you hes alot less sensitive"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/i can assure you hes alot less sensitive.ogg",length=2.1083900226757368}}
 L["i dont know what youre talking about"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/i dont know what youre talking about.ogg",length=1.2080725623582766}}
 L["i love jesus"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/i love jesus.ogg",length=0.93714285714285717}}
 L["i want my baseball"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/i want my baseball.ogg",length=1.8147165532879819}}
 L["im done with you"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/im done with you.ogg",length=0.74886621315192747}}
 L["im not gonna waste anymore time on this religious shit"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/im not gonna waste anymore time on this religious shit.ogg",length=2.5258956916099775}}
 L["it had nothing to do with me"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/it had nothing to do with me.ogg",length=1.5248752834467121}}
+L["its big enough to do this"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/its big enough to do this.ogg",length=1.2413151927437642}}
+L["its time to act like a grown up"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/its time to act like a grown up.ogg",length=1.345124716553288}}
 L["keep talking"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/keep talking.ogg",length=0.86283446712018141}}
+L["la noire ooooo"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/la noire ooooo.ogg",length=0.70321995464852605}}
 L["lapd motherfucker"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/lapd motherfucker.ogg",length=3.3777324263038548}}
 L["lets change the subject"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/lets change the subject.ogg",length=0.9101360544217687}}
 L["mrs slut"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/mrs slut.ogg",length=1.1885714285714286}}
+L["my stick just got a whole lot harder"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/my stick just got a whole lot harder.ogg",length=1.864671201814059}}
+L["or i can call my partner back over"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/or i can call my partner back over.ogg",length=1.9646485260770976}}
+L["partner"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/partner.ogg",length=0.61444444444444446}}
+L["so what happened"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/so what happened.ogg",length=0.72197278911564622}}
+L["take five minutes"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/take five minutes.ogg",length=0.91272108843537414}}
 L["that girl was ready to fuck"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/that girl was ready to fuck.ogg",length=1.5900680272108842}}
 L["this is america"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/this is america.ogg",length=0.8515646258503401}}
 L["this is germany"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/this is germany.ogg",length=0.78392290249433105}}
+L["to prove that"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/to prove that.ogg",length=1.1196145124716552}}
+L["well hasnt this worked out well for the two of you"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/well hasnt this worked out well for the two of you.ogg",length=2.2658276643990929}}
+L["you can do this the easy way with me"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/you can do this the easy way with me.ogg",length=2.1700226757369614}}
+L["you crazy son of a bitch"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/you crazy son of a bitch.ogg",length=2.6440589569160999}}
 L["you expect me to believe that"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/you expect me to believe that.ogg",length=1.3205215419501133}}
+L["you think your penis frightens me"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/you think your penis frightens me.ogg",length=1.8619727891156463}}
+L["you wanna tell me anything about brains"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/you wanna tell me anything about brains.ogg",length=1.7680272108843538}}
 L["youre lying morgan"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/youre lying morgan.ogg",length=1.0448979591836736}}
+L["youre not being much of a gentleman"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/youre not being much of a gentleman.ogg",length=1.3602267573696145}}
+L["youre not making it any easier"]={{path="chatsounds/autoadd/Gamer_Poop_LA_Noire/youre not making it any easier.ogg",length=1.2603854875283447}}
 c.EndList()
