4f14aac77bb80fb24e122a7a93703fc309876fcd
PotcFdk <PotcFdk@gmx.net>
2015-05-26 03:27:39 +0200

    this yiff fixes #11

---
+++ lua/chatsounds/lists_nosend/wanna-yiff.lua
@@ -0,0 +1,5 @@
+c.StartList("wanna-yiff")
+L["are you ready to yiff"]={{path="chatsounds/autoadd/wanna-yiff/are you ready to yiff.ogg",length=1.4710657596371881}}
+L["welcome to furaffinity"]={{path="chatsounds/autoadd/wanna-yiff/welcome to furaffinity.ogg",length=1.6953287981859411}}
+L["well hey there young man"]={{path="chatsounds/autoadd/wanna-yiff/well hey there young man.ogg",length=1.7670521541950113}}
+c.EndList()
