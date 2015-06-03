fd211427535e56fe07e3520d867b5b2a3f9f2b9d
Foxy Fazbear <sasuke_uchiha13@hotmail.co.uk>
2015-06-03 11:58:06 +0100

    Changed folder name and added a sound.

---
+++ lua/chatsounds/lists_nosend/Beauty_Bomber.lua
@@ -0,0 +1,35 @@
+c.StartList("Beauty_Bomber")
+L["ack ook ich ach bluah"]={{path="chatsounds/autoadd/Beauty_Bomber/ack ook ich ach bluah.ogg",length=2}}
+L["aha wingardium leviosaaa"]={{path="chatsounds/autoadd/Beauty_Bomber/aha wingardium leviosaaa/1.ogg",length=3},{path="chatsounds/autoadd/Beauty_Bomber/aha wingardium leviosaaa/2.ogg",length=3},{path="chatsounds/autoadd/Beauty_Bomber/aha wingardium leviosaaa/3.ogg",length=2.6200000000000001},{path="chatsounds/autoadd/Beauty_Bomber/aha wingardium leviosaaa/4.ogg",length=1.9129931972789116}}
+L["awesome mmm bye"]={{path="chatsounds/autoadd/Beauty_Bomber/awesome mmm bye.ogg",length=6.5009977324263035}}
+L["bayonettacontinueno"]={{path="chatsounds/autoadd/Beauty_Bomber/bayonettacontinueno.ogg",length=5.2395238095238099}}
+L["bayonettacontinuenodeath"]={{path="chatsounds/autoadd/Beauty_Bomber/bayonettacontinuenodeath.ogg",length=4.5848299319727888}}
+L["bayonettacontinueyes"]={{path="chatsounds/autoadd/Beauty_Bomber/bayonettacontinueyes.ogg",length=4.983605442176871}}
+L["bayonettagameover"]={{path="chatsounds/autoadd/Beauty_Bomber/bayonettagameover.ogg",length=8.7903174603174605}}
+L["beauty bomber"]={{path="chatsounds/autoadd/Beauty_Bomber/beauty bomber.ogg",length=0.94040816326530607}}
+L["dontloseyourway"]={{path="chatsounds/autoadd/Beauty_Bomber/dontloseyourway.ogg",length=35.899999999999999}}
+L["ill do this with my hands blargharghargh"]={{path="chatsounds/autoadd/Beauty_Bomber/ill do this with my hands blargharghargh.ogg",length=4.6404308390022679}}
+L["is this the krusty krab"]={{path="chatsounds/autoadd/Beauty_Bomber/is this the krusty krab/1.ogg",length=1.3570068027210884},{path="chatsounds/autoadd/Beauty_Bomber/is this the krusty krab/2.ogg",length=1.1839909297052154},{path="chatsounds/autoadd/Beauty_Bomber/is this the krusty krab/3.ogg",length=1.3319727891156463}}
+L["its leviosaaaa"]={{path="chatsounds/autoadd/Beauty_Bomber/its leviosaaaa.ogg",length=2.1000000000000001}}
+L["jeannecontinueno"]={{path="chatsounds/autoadd/Beauty_Bomber/jeannecontinueno.ogg",length=4.8476190476190473}}
+L["keel over and die"]={{path="chatsounds/autoadd/Beauty_Bomber/keel over and die.ogg",length=3.0563265306122447}}
+L["leedle leedle leedle lee"]={{path="chatsounds/autoadd/Beauty_Bomber/leedle leedle leedle lee.ogg",length=1.75}}
+L["letsboomboom"]={{path="chatsounds/autoadd/Beauty_Bomber/letsboomboom.ogg",length=2.3250113378684807}}
+L["mindblown"]={{path="chatsounds/autoadd/Beauty_Bomber/mindblown.ogg",length=23.797551020408164}}
+L["mmm try again"]={{path="chatsounds/autoadd/Beauty_Bomber/mmm try again.ogg",length=1.5}}
+L["moar"]={{path="chatsounds/autoadd/Beauty_Bomber/moar.ogg",length=0.36401360544217687}}
+L["moonprismpowermakeup"]={{path="chatsounds/autoadd/Beauty_Bomber/moonprismpowermakeup.ogg",length=37.649999999999999}}
+L["mrkrabsviolin"]={{path="chatsounds/autoadd/Beauty_Bomber/mrkrabsviolin.ogg",length=36.784920634920638}}
+L["nightistandsad"]={{path="chatsounds/autoadd/Beauty_Bomber/nightistandsad.ogg",length=97.200000000000003}}
+L["no this is patrick"]={{path="chatsounds/autoadd/Beauty_Bomber/no this is patrick/1.ogg",length=1.46},{path="chatsounds/autoadd/Beauty_Bomber/no this is patrick/2.ogg",length=2.6146258503401358},{path="chatsounds/autoadd/Beauty_Bomber/no this is patrick/3.ogg",length=3.0586167800453516}}
+L["overtime"]={{path="chatsounds/autoadd/Beauty_Bomber/overtime.ogg",length=1.3609750566893424}}
+L["penis"]={{path="chatsounds/autoadd/Beauty_Bomber/penis.ogg",length=0.90000000000000002}}
+L["ronald weasley"]={{path="chatsounds/autoadd/Beauty_Bomber/ronald weasley.ogg",length=2}}
+L["smashing"]={{path="chatsounds/autoadd/Beauty_Bomber/smashing.ogg",length=1.5571428571428572}}
+L["so remember children its wingardium leviosa swish and flick"]={{path="chatsounds/autoadd/Beauty_Bomber/so remember children its wingardium leviosa swish and flick.ogg",length=4.96}}
+L["squiddyg"]={{path="chatsounds/autoadd/Beauty_Bomber/squiddyg.ogg",length=61.085850340136055}}
+L["stop it ron stop"]={{path="chatsounds/autoadd/Beauty_Bomber/stop it ron stop/1.ogg",length=2.1699999999999999},{path="chatsounds/autoadd/Beauty_Bomber/stop it ron stop/2.ogg",length=1.15140589569161},{path="chatsounds/autoadd/Beauty_Bomber/stop it ron stop/3.ogg",length=1.1100000000000001}}
+L["that that you let a penis penetrate your friendship penis"]={{path="chatsounds/autoadd/Beauty_Bomber/that that you let a penis penetrate your friendship penis.ogg",length=5.9000000000000004}}
+L["theatreland"]={{path="chatsounds/autoadd/Beauty_Bomber/theatreland.ogg",length=27.813242630385488}}
+L["wotwtripod"]={{path="chatsounds/autoadd/Beauty_Bomber/wotwtripod.ogg",length=9.06562358276644}}
+c.EndList()
