1e8e193f3e9e4c3b5bc9259ea5323ef3ee1ab4bf
SpiralP <spiral.palmer@gmail.com>
2015-06-10 21:27:21 -0700

    portal sounds!
    (only 60 were able to be played in-game, not sure why, maybe corrupt/old
    version vpk file)

---
+++ lua/chatsounds/lists_send/portal/portal.lua
@@ -0,0 +1,62 @@
+c.StartList("portal")
+L["hello and again welcome to the aperture science computeraided enrichment center"]={{path="sound/vo/aperture_ai/00_part1_entry-1.wav",length=5.905034}}
+L["we hope your brief detention in the relaxation vault has been a pleasant one"]={{path="sound/vo/aperture_ai/00_part1_entry-2.wav",length=4.259048}}
+L["your specimen has been processed and we are now ready to begin the test proper"]={{path="sound/vo/aperture_ai/00_part1_entry-3.wav",length=4.888254}}
+L["before we start however keep in mind that although fun and learning are the primary goals of all enrichment center activities serious injuries may occur"]={{path="sound/vo/aperture_ai/00_part1_entry-4.wav",length=10.194104}}
+L["for your own safety and the safety of others please refrain from touching bzzzzzt"]={{path="sound/vo/aperture_ai/00_part1_entry-5.wav",length=5.274966}}
+L["por favor bordn de fallar muchos gracias de fallar gracias"]={{path="sound/vo/aperture_ai/00_part1_entry-6.wav",length=3.010635}}
+L["stand back the portal will open in three two one"]={{path="sound/vo/aperture_ai/00_part1_entry-7.wav",length=5.741451}}
+L["excellent please proceed into the chamberlock after completing each test"]={{path="sound/vo/aperture_ai/00_part1_success-1.wav",length=5.438005}}
+L["first however note the incandescent particle field across the exit"]={{path="sound/vo/aperture_ai/00_part1_success-2.wav",length=4.661338}}
+L["this aperture science material emancipation grid will vaporize any unauthorized equipment that passes through it for instance the aperture science weighted storage cube"]={{path="sound/vo/aperture_ai/00_part1_success-3.wav",length=12.287596}}
+L["please place the weighted storage cube on the fifteen hundred megawatt aperture science heavy duty supercolliding super button"]={{path="sound/vo/aperture_ai/00_part2_entry-1.wav",length=7.313991}}
+L["perfect please move quickly to the chamberlock as the effects of prolonged exposure to the button are not part of this test"]={{path="sound/vo/aperture_ai/00_part2_success-1.wav",length=10.195896}}
+L["youre doing very well"]={{path="sound/vo/aperture_ai/01_part1_entry-1.wav",length=2.137438}}
+L["please be advised that a noticeable taste of blood is not part of any test protocol but is an unintended side effect of the aperture science material emancipation grill which may in semi rare cases emancipate dental fillings crowns tooth enamel and teeth"]={{path="sound/vo/aperture_ai/01_part1_entry-2.wav",length=18.652948}}
+L["very good you are now in possession of the aperture science handheld portal device"]={{path="sound/vo/aperture_ai/01_part1_get_portal_gun-1.wav",length=7.632018}}
+L["with it you can create your own portals"]={{path="sound/vo/aperture_ai/01_part1_get_portal_gun-2.wav",length=2.894104}}
+L["these intra dimensional gates have proven to be completely safe"]={{path="sound/vo/aperture_ai/01_part1_get_portal_gun-3.wav",length=3.991224}}
+L["the device however has not"]={{path="sound/vo/aperture_ai/01_part1_get_portal_gun-4.wav",length=1.853923}}
+L["do not touch the operational end of the device"]={{path="sound/vo/aperture_ai/01_part1_get_portal_gun-5.wav",length=2.985102}}
+L["do not look directly at the operational end of the device"]={{path="sound/vo/aperture_ai/01_part1_get_portal_gun-6.wav",length=3.692063}}
+L["do not submerge the device in liquid even partially"]={{path="sound/vo/aperture_ai/01_part1_get_portal_gun-7.wav",length=3.510272}}
+L["most importantly under no circumstances should you bzzzpt"]={{path="sound/vo/aperture_ai/01_part1_get_portal_gun-8.wav",length=4.82805}}
+L["please proceed to the chamberlock mind the gap"]={{path="sound/vo/aperture_ai/01_part2_entry-1.wav",length=3.451927}}
+L["well done remember the aperture science bring your daughter to work day is the perfect time to have her tested"]={{path="sound/vo/aperture_ai/01_part2_success-1.wav",length=6.101338}}
+L["welcome to test chamber four"]={{path="sound/vo/aperture_ai/02_part1_entry-1.wav",length=2.027528}}
+L["youre doing quite well"]={{path="sound/vo/aperture_ai/02_part1_entry-2.wav",length=1.545057}}
+L["once again excellent work"]={{path="sound/vo/aperture_ai/02_part1_success-1.wav",length=2.296939}}
+L["as part of a required test protocol we will not monitor the next test chamber you will be entirely on your own good luck"]={{path="sound/vo/aperture_ai/02_part1_success-2.wav",length=8.322766}}
+L["as part of a required test protocol our previous statement suggesting that we would not monitor this chamber was an outright fabrication"]={{path="sound/vo/aperture_ai/02_part2_success-1.wav",length=9.042245}}
+L["good job as part of a required test protocol we will stop enhancing the truth in three two one"]={{path="sound/vo/aperture_ai/02_part2_success-2.wav",length=9.686644}}
+L["while safety is one of many enrichment center goals the aperture science high energy pellet seen to the left of the chamber can and has caused permanent disabilities such as vaporization"]={{path="sound/vo/aperture_ai/03_part1_entry-1.wav",length=13.21127}}
+L["please be careful"]={{path="sound/vo/aperture_ai/03_part1_entry-2.wav",length=1.261406}}
+L["unbelievable you louder subject name herelouder must be the pride of loudersubject hometown herelouder"]={{path="sound/vo/aperture_ai/03_part1_success-1.wav",length=6.918776}}
+L["audible warning devices are required on all mobile equipment however alarms and flashing hazard lights have been found to agitate the high energy pellet and have therefore been disabled for your safety"]={{path="sound/vo/aperture_ai/03_part2_entry-1.wav",length=12.790431}}
+L["good now use the aperture science unstationary scaffold to reach the chamberlock"]={{path="sound/vo/aperture_ai/03_part2_platform_activated-1.wav",length=6.637143}}
+L["please note that we have added a consequence for failure any contact with the chamber floor will result in an unsatisfactory mark on your official testing record followed by death good luck"]={{path="sound/vo/aperture_ai/04_part1_entry-1.wav",length=12.526213}}
+L["very impressive please note that any appearance of danger is merely a device to enhance your testing experience"]={{path="sound/vo/aperture_ai/04_part1_success-1.wav",length=6.848005}}
+L["the enrichment center regrets to inform you that this next test is impossible"]={{path="sound/vo/aperture_ai/05_part1_entry-1.wav",length=5.30424}}
+L["make no attempt to solve it"]={{path="sound/vo/aperture_ai/05_part1_entry-2.wav",length=2.056395}}
+L["the enrichment center apologizes for this clearly broken test chamber"]={{path="sound/vo/aperture_ai/05_part1_nag1-1.wav",length=5.088503}}
+L["once again the enrichment center offers its most sincere apologies on the occasion of this unsolvable test environment"]={{path="sound/vo/aperture_ai/05_part1_nag2-1.wav",length=8.419252}}
+L["frankly this chamber was a mistake if we were you we would quit now"]={{path="sound/vo/aperture_ai/05_part1_nag3-1.wav",length=5.688571}}
+L["no one will blame you for giving up in fact quitting at this point is a perfectly reasonable response"]={{path="sound/vo/aperture_ai/05_part1_nag4-1.wav",length=9.416281}}
+L["quit now and cake will be served immediately"]={{path="sound/vo/aperture_ai/05_part1_nag5-1.wav",length=3.858027}}
+L["fantastic you remained resolute and resourceful in an atmosphere of extreme pessimism"]={{path="sound/vo/aperture_ai/05_part1_success-1.wav",length=7.470998}}
+L["hello again to reiterate our previous warning this test garbled momentum garbled"]={{path="sound/vo/aperture_ai/06_part1_entry-1.wav",length=7.944785}}
+L["spectacular you appear to understand how a portal affects forward momentum or to be more precise how it does not"]={{path="sound/vo/aperture_ai/06_part1_success_1-1.wav",length=8.380952}}
+L["momentum a function of mass and velocity is conserved between portals in laymans terms speedy thing goes in speedy thing comes out"]={{path="sound/vo/aperture_ai/06_part1_success_2-1.wav",length=9.750816}}
+L["ding off"]={{path="sound/vo/aperture_ai/ding_off.wav",length=0.19}}
+L["ding on"]={{path="sound/vo/aperture_ai/ding_on.wav",length=0.19}}
+L["file deleted"]={{path="sound/vo/aperture_ai/file_deleted.wav",length=0.759887}}
+L["weighted storage cube destroyed"]={{path="sound/vo/aperture_ai/generic_crate_lost-1.wav",length=1.941746}}
+L["please proceed to the aperture science vital apparatus vent for a replacement"]={{path="sound/vo/aperture_ai/generic_crate_lost-2.wav",length=5.824195}}
+L["please do not attempt to remove testing apparatus from the testing area"]={{path="sound/vo/aperture_ai/generic_crate_vaporized_in_emancipation_grid-1.wav",length=4.615147}}
+L["a replacement aperture science weighted storage cube will be delivered shortly"]={{path="sound/vo/aperture_ai/generic_crate_vaporized_in_emancipation_grid-2.wav",length=5.175624}}
+L["at the enrichment center we promise never to value your safety above your unique ideas and creativity however do not destroy vital testing apparatus"]={{path="sound/vo/aperture_ai/generic_security_camera_destroyed-1.wav",length=11.140454}}
+L["to ensure the safe performance of all authorized activities do not destroy vital testing apparatus"]={{path="sound/vo/aperture_ai/generic_security_camera_destroyed-2.wav",length=6.467256}}
+L["for your own safety do not destroy vital testing apparatus"]={{path="sound/vo/aperture_ai/generic_security_camera_destroyed-3.wav",length=4.162404}}
+L["certain objects may be vital to your success do not destroy testing apparatus"]={{path="sound/vo/aperture_ai/generic_security_camera_destroyed-4.wav",length=5.242132}}
+L["vital testing apparatus destroyed"]={{path="sound/vo/aperture_ai/generic_security_camera_destroyed-5.wav",length=2.45712}}
+c.EndList()
