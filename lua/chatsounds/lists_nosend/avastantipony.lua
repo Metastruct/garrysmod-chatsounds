969ce68ab19bf7226af9b8e3e55726f2b269cf93
!Avast AntiPony 9445 <kristianzxz41@gmail.com>
2015-06-14 17:52:08 +0200

    Expand Donk
    I certainly hope you know what youre doing

--- lua/chatsounds/lists_nosend/avastantipony.lua
+++ lua/chatsounds/lists_nosend/avastantipony.lua
@@ -1,6 +1,7 @@
 c.StartList("avastantipony")
 L["if youre having girl problems i feel bad for you son i got 99 problems but a bitch aint one"]={{path="chatsounds/autoadd/avastantipony/if youre having girl problems i feel bad for you son i got 99 problems but a bitch aint one.ogg",length=5.1095691609977321}}
 L["interior crocodile alligator i drive a chevrolet movie theater"]={{path="chatsounds/autoadd/avastantipony/interior crocodile alligator i drive a chevrolet movie theater.ogg",length=5.3709297052154197}}
+L["mallgasm"]={{path="chatsounds/autoadd/avastantipony/mallgasm.ogg",length=7.9082539682539679}}
 L["pepsiman"]={{path="chatsounds/autoadd/avastantipony/pepsiman.ogg",length=3.0396371882086166}}
 L["rock solid"]={{path="chatsounds/autoadd/avastantipony/rock solid.ogg",length=3.5566439909297052}}
 c.EndList()
