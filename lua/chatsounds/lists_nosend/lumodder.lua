193db1e5b52831dac7fc4daa6ccfa0535b60d873
Flex <LUModder@gmail.com>
2015-06-18 11:38:34 -0600

    More cosmod/stream cosmo chatsounds -Siglemic -Shaft -Well Shit -I am the penguin -Hey all so uh um uh um um um yeah

--- lua/chatsounds/lists_nosend/lumodder.lua
+++ lua/chatsounds/lists_nosend/lumodder.lua
@@ -1,12 +1,16 @@
 c.StartList("lumodder")
 L["but my hope will never die"]={{path="chatsounds/autoadd/lumodder/but my hope will never die.ogg",length=1.5582086167800453}}
+L["cosmo uh um"]={{path="chatsounds/autoadd/lumodder/cosmo uh um.ogg",length=6.1765079365079369}}
 L["gta wasted"]={{path="chatsounds/autoadd/lumodder/gta wasted.ogg",length=7.5296825396825398}}
+L["i am the penguin"]={{path="chatsounds/autoadd/lumodder/i am the penguin.ogg",length=1.4512471655328798}}
 L["i planted the thing"]={{path="chatsounds/autoadd/lumodder/i planted the thing/1.ogg",length=1.2252834467120182},{path="chatsounds/autoadd/lumodder/i planted the thing/2.ogg",length=1.2455102040816326},{path="chatsounds/autoadd/lumodder/i planted the thing/3.ogg",length=3.8921088435374149}}
 L["its boshy time"]={{path="chatsounds/autoadd/lumodder/its boshy time.ogg",length=1.9873922902494332}}
 L["mu os"]={{path="chatsounds/autoadd/lumodder/mu os.ogg",length=1.1377551020408163}}
 L["mumnumnum"]={{path="chatsounds/autoadd/lumodder/mumnumnum.ogg",length=0.80875283446712021}}
 L["real trap shit"]={{path="chatsounds/autoadd/lumodder/real trap shit.ogg",length=2.3161904761904761}}
+L["shaft"]={{path="chatsounds/autoadd/lumodder/shaft.ogg",length=0.9298412698412698}}
 L["shots fired shots fired"]={{path="chatsounds/autoadd/lumodder/shots fired shots fired.ogg",length=3.9063718820861677}}
+L["siglemic"]={{path="chatsounds/autoadd/lumodder/siglemic.ogg",length=1.4164172335600906}}
 L["so fast"]={{path="chatsounds/autoadd/lumodder/so fast.ogg",length=1.7182766439909296}}
 L["suck my balls doc"]={{path="chatsounds/autoadd/lumodder/suck my balls doc.ogg",length=1.5905668934240362}}
 L["take a closer look at that meme"]={{path="chatsounds/autoadd/lumodder/take a closer look at that meme.ogg",length=2.175873015873016}}
@@ -16,5 +20,6 @@ L["there he goes the willy wonga"]={{path="chatsounds/autoadd/lumodder/there he 
 L["there you fn go you cunt bastard"]={{path="chatsounds/autoadd/lumodder/there you fn go you cunt bastard.ogg",length=5.8619501133786844}}
 L["trainrain"]={{path="chatsounds/autoadd/lumodder/trainrain.ogg",length=3.0040816326530613}}
 L["triple x rated"]={{path="chatsounds/autoadd/lumodder/triple x rated.ogg",length=3.4309070294784583}}
+L["well shit"]={{path="chatsounds/autoadd/lumodder/well shit.ogg",length=1.7531065759637188}}
 L["whos been drawing dicks"]={{path="chatsounds/autoadd/lumodder/whos been drawing dicks.ogg",length=1.5914739229024943}}
 c.EndList()
