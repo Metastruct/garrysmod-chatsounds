f0d64b16fee1bb24ace381f155628f48e0226bf2
TrenchFroast <thesteva256@gmail.com>
2015-06-18 21:19:14 +0100

    jacksfilms sounds
    feel free to move to a new folder if you feel like doing so but for now
    they're in my folder

--- lua/chatsounds/lists_nosend/trenchfroast.lua
+++ lua/chatsounds/lists_nosend/trenchfroast.lua
@@ -1,4 +1,15 @@
 c.StartList("trenchfroast")
 L["peeeeniiissss"]={{path="chatsounds/autoadd/trenchfroast/PEEEENIIISSSS.ogg",length=2.7095464852607711}}
+L["a light switch"]={{path="chatsounds/autoadd/trenchfroast/a light switch.ogg",length=0.65916099773242631}}
 L["baab"]={{path="chatsounds/autoadd/trenchfroast/baab.ogg",length=0.58752834467120185}}
+L["blech"]={{path="chatsounds/autoadd/trenchfroast/blech.ogg",length=0.33643990929705214}}
+L["can you not"]={{path="chatsounds/autoadd/trenchfroast/can you not.ogg",length=0.65170068027210881}}
+L["cool cool"]={{path="chatsounds/autoadd/trenchfroast/cool cool.ogg",length=0.56349206349206349}}
+L["gatorade"]={{path="chatsounds/autoadd/trenchfroast/gatorade.ogg",length=0.41077097505668936}}
+L["getting laid"]={{path="chatsounds/autoadd/trenchfroast/getting laid.ogg",length=0.67274376417233561}}
+L["getting paid getting laid and gatorade"]={{path="chatsounds/autoadd/trenchfroast/getting paid getting laid and gatorade.ogg",length=2.0679591836734694}}
+L["getting paid"]={{path="chatsounds/autoadd/trenchfroast/getting paid.ogg",length=0.68888888888888888}}
+L["jackask"]={{path="chatsounds/autoadd/trenchfroast/jackask.ogg",length=1.9887528344671201}}
+L["liurelly"]={{path="chatsounds/autoadd/trenchfroast/liurelly.ogg",length=0.86482993197278912}}
+L["sampepperbreathing"]={{path="chatsounds/autoadd/trenchfroast/sampepperbreathing.ogg",length=5.3351473922902493}}
 c.EndList()
