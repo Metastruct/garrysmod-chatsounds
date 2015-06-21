5aa0622269883eb3e628406c17431fe55571d564
TrenchFroast <thesteva256@gmail.com>
2015-06-21 23:10:33 +0100

    Moar jacksfilms
    Now in its own folder!

---
+++ lua/chatsounds/lists_nosend/jacksfilms.lua
@@ -0,0 +1,16 @@
+c.StartList("jacksfilms")
+L["a light switch"]={{path="chatsounds/autoadd/jacksfilms/a light switch.ogg",length=0.65916099773242631}}
+L["bad day"]={{path="chatsounds/autoadd/jacksfilms/bad day.ogg",length=0.55725623582766437}}
+L["blech"]={{path="chatsounds/autoadd/jacksfilms/blech.ogg",length=0.33643990929705214}}
+L["can you not"]={{path="chatsounds/autoadd/jacksfilms/can you not.ogg",length=0.65170068027210881}}
+L["cool cool"]={{path="chatsounds/autoadd/jacksfilms/cool cool/cool cool.ogg",length=0.56349206349206349},{path="chatsounds/autoadd/jacksfilms/cool cool/cool cool2.ogg",length=0.47959183673469385},{path="chatsounds/autoadd/jacksfilms/cool cool/cool cool3.ogg",length=0.7097959183673469}}
+L["gatorade"]={{path="chatsounds/autoadd/jacksfilms/gatorade.ogg",length=0.41077097505668936}}
+L["getting laid"]={{path="chatsounds/autoadd/jacksfilms/getting laid.ogg",length=0.67274376417233561}}
+L["getting paid getting laid and gatorade"]={{path="chatsounds/autoadd/jacksfilms/getting paid getting laid and gatorade.ogg",length=2.0679591836734694}}
+L["getting paid"]={{path="chatsounds/autoadd/jacksfilms/getting paid.ogg",length=0.68888888888888888}}
+L["jackask"]={{path="chatsounds/autoadd/jacksfilms/jackask.ogg",length=1.9887528344671201}}
+L["liurelly"]={{path="chatsounds/autoadd/jacksfilms/liurelly.ogg",length=0.86482993197278912}}
+L["sampepperbreathing"]={{path="chatsounds/autoadd/jacksfilms/sampepperbreathing.ogg",length=5.3351473922902493}}
+L["thanks dad"]={{path="chatsounds/autoadd/jacksfilms/thanks dad.ogg",length=0.72412698412698417}}
+L["what the fck"]={{path="chatsounds/autoadd/jacksfilms/what the fck.ogg",length=0.85857142857142854}}
+c.EndList()
