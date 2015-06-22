0af17f55bc4917a9ba2d16f17906d459e4468b33
TrenchFroast <thesteva256@gmail.com>
2015-06-22 10:44:21 +0100

    jacksfilms sound dump
    you can probably tell im kinda obsessed with him

--- lua/chatsounds/lists_nosend/jacksfilms.lua
+++ lua/chatsounds/lists_nosend/jacksfilms.lua
@@ -1,10 +1,15 @@
 c.StartList("jacksfilms")
 L["a light switch"]={{path="chatsounds/autoadd/jacksfilms/a light switch.ogg",length=0.65916099773242631}}
+L["a whata fucke"]={{path="chatsounds/autoadd/jacksfilms/a whata fucke.ogg",length=0.86095238095238091}}
+L["aauummmmm"]={{path="chatsounds/autoadd/jacksfilms/aauummmmm.ogg",length=1.599092970521542}}
+L["awkwardlaugh"]={{path="chatsounds/autoadd/jacksfilms/awkwardlaugh.ogg",length=1.129092970521542}}
 L["bad day"]={{path="chatsounds/autoadd/jacksfilms/bad day.ogg",length=0.55725623582766437}}
+L["biches"]={{path="chatsounds/autoadd/jacksfilms/biches.ogg",length=0.61485260770975059}}
 L["blech"]={{path="chatsounds/autoadd/jacksfilms/blech.ogg",length=0.33643990929705214}}
 L["can you not"]={{path="chatsounds/autoadd/jacksfilms/can you not.ogg",length=0.65170068027210881}}
 L["cool cool"]={{path="chatsounds/autoadd/jacksfilms/cool cool/cool cool.ogg",length=0.56349206349206349},{path="chatsounds/autoadd/jacksfilms/cool cool/cool cool2.ogg",length=0.47959183673469385},{path="chatsounds/autoadd/jacksfilms/cool cool/cool cool3.ogg",length=0.7097959183673469}}
 L["fck all of you"]={{path="chatsounds/autoadd/jacksfilms/fck all of you.ogg",length=0.7777097505668934}}
+L["fut yeah"]={{path="chatsounds/autoadd/jacksfilms/fut yeah.ogg",length=0.53326530612244893}}
 L["gatorade"]={{path="chatsounds/autoadd/jacksfilms/gatorade.ogg",length=0.41077097505668936}}
 L["getting laid"]={{path="chatsounds/autoadd/jacksfilms/getting laid.ogg",length=0.67274376417233561}}
 L["getting paid getting laid and gatorade"]={{path="chatsounds/autoadd/jacksfilms/getting paid getting laid and gatorade.ogg",length=2.0679591836734694}}
@@ -14,6 +19,9 @@ L["jackask"]={{path="chatsounds/autoadd/jacksfilms/jackask.ogg",length=1.9887528
 L["liurelly"]={{path="chatsounds/autoadd/jacksfilms/liurelly.ogg",length=0.86482993197278912}}
 L["redllwhhhaaach"]={{path="chatsounds/autoadd/jacksfilms/redllwhhhaaach.ogg",length=1.1603854875283446}}
 L["sampepperbreathing"]={{path="chatsounds/autoadd/jacksfilms/sampepperbreathing.ogg",length=5.3351473922902493}}
+L["tatatatatatatata"]={{path="chatsounds/autoadd/jacksfilms/tatatatatatatata.ogg",length=1.6602267573696146}}
 L["thanks dad"]={{path="chatsounds/autoadd/jacksfilms/thanks dad.ogg",length=0.72412698412698417}}
 L["what the fck"]={{path="chatsounds/autoadd/jacksfilms/what the fck.ogg",length=0.85857142857142854}}
+L["you fcking bich"]={{path="chatsounds/autoadd/jacksfilms/you fcking bich.ogg",length=0.75609977324263034}}
+L["you mad jellystrated buttdoctor"]={{path="chatsounds/autoadd/jacksfilms/you mad jellystrated buttdoctor.ogg",length=2.9195464852607711}}
 c.EndList()
