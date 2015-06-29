b4a980861f4ad0f6df1499cf1f25471312a80157
GigiSpahz <r-type-pasfer@live.fr>
2015-06-29 21:29:11 +0200

    more fof sounds

--- lua/chatsounds/lists_nosend/nonhuman.lua
+++ lua/chatsounds/lists_nosend/nonhuman.lua
@@ -1,21 +1,28 @@
 c.StartList("nonhuman")
 L["barett"]={{path="chatsounds/autoadd/nonhuman/barett.ogg",length=1.1541950113378685}}
 L["barrys red cola"]={{path="chatsounds/autoadd/nonhuman/barrys red cola/barrys red cola 1.ogg",length=1.5792970521541949},{path="chatsounds/autoadd/nonhuman/barrys red cola/barrys red cola 2.ogg",length=1.9986167800453514},{path="chatsounds/autoadd/nonhuman/barrys red cola/barrys red cola 3.ogg",length=0.99192743764172331}}
+L["come on bitch shoot me"]={{path="chatsounds/autoadd/nonhuman/come on bitch shoot me.OGG",length=1.6718367346938776}}
+L["coward"]={{path="chatsounds/autoadd/nonhuman/coward.OGG",length=0.70530612244897961}}
 L["dive into a pool of"]={{path="chatsounds/autoadd/nonhuman/dive into a pool of.ogg",length=1.9986167800453514}}
 L["drink barrys red cola"]={{path="chatsounds/autoadd/nonhuman/drink barrys red cola/drink barrys red cola 2.ogg",length=2},{path="chatsounds/autoadd/nonhuman/drink barrys red cola/drink barrys red cola.ogg",length=2}}
 L["dsr50"]={{path="chatsounds/autoadd/nonhuman/dsr50.ogg",length=2.4222222222222221}}
+L["here have a drink"]={{path="chatsounds/autoadd/nonhuman/here have a drink.OGG",length=1.1232653061224489}}
 L["hitmarker"]={{path="chatsounds/autoadd/nonhuman/hitmarker.ogg",length=0.10038548752834467}}
+L["i see him"]={{path="chatsounds/autoadd/nonhuman/i see him.OGG",length=0.67918367346938779}}
 L["illuminati"]={{path="chatsounds/autoadd/nonhuman/illuminati.ogg",length=8.0403628117913826}}
 L["leave me alooooone"]={{path="chatsounds/autoadd/nonhuman/leave me alooooone.ogg",length=1.5012244897959184}}
 L["mlgbass"]={{path="chatsounds/autoadd/nonhuman/mlgbass/bass1.ogg",length=1.6851020408163266},{path="chatsounds/autoadd/nonhuman/mlgbass/bass2.ogg",length=1.6851020408163266},{path="chatsounds/autoadd/nonhuman/mlgbass/bass3.ogg",length=1.6438548752834468},{path="chatsounds/autoadd/nonhuman/mlgbass/bass4.ogg",length=1.6438548752834468},{path="chatsounds/autoadd/nonhuman/mlgbass/bass6.ogg",length=1.6438548752834468},{path="chatsounds/autoadd/nonhuman/mlgbass/bass7.ogg",length=1.6438548752834468},{path="chatsounds/autoadd/nonhuman/mlgbass/bass8.ogg",length=1.6438548752834468},{path="chatsounds/autoadd/nonhuman/mlgbass/bass9.ogg",length=1.6438548752834468}}
 L["mom get the camera mommy mommy mommy oh my god"]={{path="chatsounds/autoadd/nonhuman/mom get the camera mommy mommy mommy oh my god.ogg",length=6.0123129251700682}}
+L["move it"]={{path="chatsounds/autoadd/nonhuman/move it.OGG",length=0.73142857142857143}}
 L["oh baby a triple"]={{path="chatsounds/autoadd/nonhuman/oh baby a triple.ogg",length=2.6122448979591835}}
 L["oh my fucking god"]={{path="chatsounds/autoadd/nonhuman/oh my fucking god.ogg",length=3.312018140589569}}
 L["pass the whiskey"]={{path="chatsounds/autoadd/nonhuman/pass the whiskey/passthewhiskey1.ogg",length=1.4338775510204083},{path="chatsounds/autoadd/nonhuman/pass the whiskey/passthewhiskey2.ogg",length=1.2324036281179138}}
 L["rapbattle"]={{path="chatsounds/autoadd/nonhuman/rapbattle.ogg",length=6.7404988662131515}}
+L["shoot these dogs"]={{path="chatsounds/autoadd/nonhuman/shoot these dogs.OGG",length=1.28}}
 L["shut uuup"]={{path="chatsounds/autoadd/nonhuman/shut uuup.ogg",length=1.3495238095238096}}
 L["thats right get noscoped get noscoped"]={{path="chatsounds/autoadd/nonhuman/thats right get noscoped get noscoped.ogg",length=4.4962131519274378}}
 L["thats right seven hundred freaking points"]={{path="chatsounds/autoadd/nonhuman/thats right seven hundred freaking points.ogg",length=3.6914058956916098}}
+L["theyre coming"]={{path="chatsounds/autoadd/nonhuman/theyre coming.OGG",length=0.67918367346938779}}
 L["trapguncock"]={{path="chatsounds/autoadd/nonhuman/trapguncock.ogg",length=0.72183673469387755}}
 L["trickshot1"]={{path="chatsounds/autoadd/nonhuman/trickshot1.ogg",length=4}}
 L["trickshot10"]={{path="chatsounds/autoadd/nonhuman/trickshot10.ogg",length=8.2674376417233564}}
@@ -35,8 +42,12 @@ L["wombocombo"]={{path="chatsounds/autoadd/nonhuman/wombocombo.ogg",length=18}}
 L["xddddd"]={{path="chatsounds/autoadd/nonhuman/xddddd.ogg",length=1.6588435374149659}}
 L["xdddddddddddddd"]={{path="chatsounds/autoadd/nonhuman/xdddddddddddddd.ogg",length=3.0524716553287981}}
 L["xddddddistorted"]={{path="chatsounds/autoadd/nonhuman/xddddddistorted.ogg",length=1.6588435374149659}}
+L["yeeehaaaa"]={{path="chatsounds/autoadd/nonhuman/yeeehaaaa.OGG",length=1.5412244897959184}}
+L["yeehaww"]={{path="chatsounds/autoadd/nonhuman/yeehaww.OGG",length=1.5934693877551021}}
 L["you drink the delicious barrys red cola"]={{path="chatsounds/autoadd/nonhuman/you drink the delicious barrys red cola.ogg",length=3.0080725623582767}}
 L["you guys suck eggs"]={{path="chatsounds/autoadd/nonhuman/you guys suck eggs.ogg",length=1.9517233560090703}}
 L["you guys suck"]={{path="chatsounds/autoadd/nonhuman/you guys suck.ogg",length=1.3495238095238096}}
+L["you need a whiskey"]={{path="chatsounds/autoadd/nonhuman/you need a whiskey.OGG",length=0.80979591836734699}}
 L["you wake on a giants kitchen"]={{path="chatsounds/autoadd/nonhuman/you wake on a giants kitchen.ogg",length=2}}
+L["you yella bella coward"]={{path="chatsounds/autoadd/nonhuman/you yella bella coward.OGG",length=1.6457142857142857}}
 c.EndList()
