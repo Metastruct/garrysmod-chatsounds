fe93991008af4dec547e5e0f768d3b1d78dc610e
TrenchFroast <thesteva256@gmail.com>
2015-07-27 01:42:02 +0100

    i put them into the wrong folder
    nights = bad times to commit

--- lua/chatsounds/lists_nosend/trenchfroast.lua
+++ lua/chatsounds/lists_nosend/trenchfroast.lua
@@ -16,24 +16,16 @@ L["go to the church and ask god to fug you"]={{path="chatsounds/autoadd/trenchfr
 L["haaaaah"]={{path="chatsounds/autoadd/trenchfroast/haaaaah.ogg",length=3.7846485260770977}}
 L["hah"]={{path="chatsounds/autoadd/trenchfroast/hah/hah 1.ogg",length=0.18954648526077097},{path="chatsounds/autoadd/trenchfroast/hah/hah 2.ogg",length=0.39852607709750565},{path="chatsounds/autoadd/trenchfroast/hah/hah 3.ogg",length=0.32190476190476192},{path="chatsounds/autoadd/trenchfroast/hah/hah 4.ogg",length=0.50392290249433103}}
 L["ipponmanzoku"]={{path="chatsounds/autoadd/trenchfroast/ipponmanzoku/ippon manzoku song 1.ogg",length=3.294920634920635},{path="chatsounds/autoadd/trenchfroast/ipponmanzoku/ippon manzoku song 2.ogg",length=3.3324263038548754},{path="chatsounds/autoadd/trenchfroast/ipponmanzoku/ippon manzoku song 3.ogg",length=3.2747392290249433},{path="chatsounds/autoadd/trenchfroast/ipponmanzoku/ippon manzoku song 4.ogg",length=3.2735147392290251}}
-L["no no im not c im not cool with that"]={{path="chatsounds/autoadd/trenchfroast/no no im not c im not cool with that.ogg",length=3.0651020408163263}}
-L["no"]={{path="chatsounds/autoadd/trenchfroast/no.ogg",length=0.6492290249433107}}
-L["okey"]={{path="chatsounds/autoadd/trenchfroast/okey.ogg",length=0.49814058956916102}}
 L["packagerip"]={{path="chatsounds/autoadd/trenchfroast/packagerip.ogg",length=0.84424036281179138}}
 L["peeeeniiissss"]={{path="chatsounds/autoadd/trenchfroast/PEEEENIIISSSS.ogg",length=2.7095464852607711}}
 L["plug"]={{path="chatsounds/autoadd/trenchfroast/plug.ogg",length=0.83192743764172339}}
 L["pyuchhhhhhhh"]={{path="chatsounds/autoadd/trenchfroast/pyuchhhhhhhh.ogg",length=2.0062585034013605}}
 L["sambaloop"]={{path="chatsounds/autoadd/trenchfroast/sambaloop.ogg",length=4.7993197278911568}}
-L["screw this no"]={{path="chatsounds/autoadd/trenchfroast/screw this no.ogg",length=1.4764172335600907}}
-L["screw this"]={{path="chatsounds/autoadd/trenchfroast/screw this.ogg",length=0.85040816326530611}}
 L["shut up granshush"]={{path="chatsounds/autoadd/trenchfroast/shut up granshush.ogg",length=1.7208843537414966}}
 L["stranth"]={{path="chatsounds/autoadd/trenchfroast/stranth.ogg",length=0.68614512471655331}}
 L["take your prune juuj"]={{path="chatsounds/autoadd/trenchfroast/take your prune juuj.ogg",length=1.2013378684807257}}
 L["thats it"]={{path="chatsounds/autoadd/trenchfroast/thats it.ogg",length=0.57108843537414966}}
-L["traaash"]={{path="chatsounds/autoadd/trenchfroast/traaash/1.ogg",length=0.96943310657596371},{path="chatsounds/autoadd/trenchfroast/traaash/2.ogg",length=0.78115646258503402},{path="chatsounds/autoadd/trenchfroast/traaash/3.ogg",length=0.98870748299319733}}
 L["tribal"]={{path="chatsounds/autoadd/trenchfroast/tribal.ogg",length=4.7508390022675737}}
 L["tribalrush"]={{path="chatsounds/autoadd/trenchfroast/tribalrush.ogg",length=5.7766439909297054}}
-L["what no no im not c im not cool with that screw this screw this no im done"]={{path="chatsounds/autoadd/trenchfroast/what no no im not c im not cool with that screw this screw this no im done.ogg",length=6.8545804988662136}}
-L["what"]={{path="chatsounds/autoadd/trenchfroast/what.ogg",length=0.74419501133786847}}
 L["worthles"]={{path="chatsounds/autoadd/trenchfroast/worthles.ogg",length=0.88136054421768706}}
 c.EndList()
