37b05cd45675b6f6779dca02eb01706c0b79f12b
TrenchFroast <thesteva256@gmail.com>
2015-07-29 21:19:44 +0100

    probably fix for the dead chatsounds
    kill me if they dont

--- lua/chatsounds/lists_nosend/just do it ytp.lua
+++
@@ -1,9 +0,0 @@
-c.StartList("just do it ytp")
-L["jooj"]={{path="chatsounds/autoadd/just do it ytp/jooj.ogg",length=0.41315192743764173}}
-L["noon"]={{path="chatsounds/autoadd/just do it ytp/noon.ogg",length=0.36086167800453517}}
-L["soos"]={{path="chatsounds/autoadd/just do it ytp/soos.ogg",length=0.3295238095238095}}
-L["sss s s stop"]={{path="chatsounds/autoadd/just do it ytp/sss s s stop.ogg",length=1.1426530612244898}}
-L["stoots"]={{path="chatsounds/autoadd/just do it ytp/stoots.ogg",length=0.8673922902494331}}
-L["yeey"]={{path="chatsounds/autoadd/just do it ytp/yeey.ogg",length=0.26140589569160999}}
-L["yuuy"]={{path="chatsounds/autoadd/just do it ytp/yuuy.ogg",length=0.28922902494331065}}
-c.EndList()
--- lua/chatsounds/lists_nosend/real human bean.lua
+++
@@ -1,4 +0,0 @@
-c.StartList("real human bean")
-L["and a real hero"]={{path="chatsounds/autoadd/real human bean/and a real hero.ogg",length=8.6070748299319728}}
-L["real human bean"]={{path="chatsounds/autoadd/real human bean/real human bean.ogg",length=8.7026984126984122}}
-c.EndList()
