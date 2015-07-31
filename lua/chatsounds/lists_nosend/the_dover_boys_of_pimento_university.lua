9f5253f2c38ca7e5d8de5730243ac51d4b4fe345
VerbalSilence <verbalsilence@hotmail.com>
2015-07-31 23:32:36 +1000

    added underscores to folder names with spaces

---
+++ lua/chatsounds/lists_nosend/the_dover_boys_of_pimento_university.lua
@@ -0,0 +1,7 @@
+c.StartList("the_dover_boys_of_pimento_university")
+L["a runabout ill steal it no one will ever know"]={{path="chatsounds/autoadd/the_dover_boys_of_pimento_university/a runabout ill steal it no one will ever know.ogg",length=4.9654195011337867}}
+L["a runabout"]={{path="chatsounds/autoadd/the_dover_boys_of_pimento_university/a runabout.ogg",length=1.2836281179138322}}
+L["i hate dick"]={{path="chatsounds/autoadd/the_dover_boys_of_pimento_university/i hate dick.ogg",length=1.181904761904762}}
+L["ill steal it"]={{path="chatsounds/autoadd/the_dover_boys_of_pimento_university/ill steal it.ogg",length=1.2964399092970522}}
+L["no one will ever know"]={{path="chatsounds/autoadd/the_dover_boys_of_pimento_university/no one will ever know.ogg",length=2.3853514739229027}}
+c.EndList()
