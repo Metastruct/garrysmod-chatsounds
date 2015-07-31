fad365d1cf7b4b21aa2885e4502a2748a7def250
VerbalSilence <verbalsilence@hotmail.com>
2015-08-01 09:27:40 +1000

    ran preprocessor

--- lua/chatsounds/lists_nosend/mitterdoo.lua
+++ lua/chatsounds/lists_nosend/mitterdoo.lua
@@ -1,7 +1,9 @@
 c.StartList("mitterdoo")
+L["and his name is john cena"]={{path="chatsounds/autoadd/mitterdoo/and his name is john cena.ogg",length=12.995283446712017}}
 L["fuck you"]={{path="chatsounds/autoadd/mitterdoo/fuck you.ogg",length=0.8990476190476191}}
 L["i swear to christ ill hook you in the gabba"]={{path="chatsounds/autoadd/mitterdoo/i swear to christ ill hook you in the gabba.ogg",length=3.0882539682539685}}
 L["i swear u are one cheeky cunt mate"]={{path="chatsounds/autoadd/mitterdoo/i swear u are one cheeky cunt mate.ogg",length=2.5658049886621317}}
+L["john cena"]={{path="chatsounds/autoadd/mitterdoo/john cena.ogg",length=11.97562358276644}}
 L["mitterdoo"]={{path="chatsounds/autoadd/mitterdoo/mitterdoo/mitterdoo1.ogg",length=0.48036281179138324},{path="chatsounds/autoadd/mitterdoo/mitterdoo/mitterdoo2.ogg",length=0.55727891156462583}}
 L["say it to my face and not online and well see what happens"]={{path="chatsounds/autoadd/mitterdoo/say it to my face and not online and well see what happens.ogg",length=3.3785034013605442}}
 L["u wot m8"]={{path="chatsounds/autoadd/mitterdoo/u wot m8.ogg",length=0.99265306122448982}}
