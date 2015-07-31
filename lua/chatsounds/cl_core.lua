2403162c2ec142b7e28e3e05b6e40de81135a704
PotcFdk <PotcFdk@gmx.net>
2015-07-31 16:16:45 +0200

    Replace two gsub calls with a single one.

--- lua/chatsounds/cl_core.lua
+++ lua/chatsounds/cl_core.lua
@@ -1110,7 +1110,7 @@ function chatsounds.GetScriptFromText(text)
 		-- no need to search for words if none are left
 		-- could probably be a lot better with a pattern but I suck (capsadmin) at patterns so fuck that
 
-		if #text:gsub("_", ""):gsub(" ", "") == 0 then stop = true return end
+		if #text:gsub("[_%s]+", "") == 0 then stop = true return end
 
 		local found = false
 		repeat
@@ -1130,7 +1130,7 @@ function chatsounds.GetScriptFromText(text)
 				found = true
 
 				text = text:sub(1, fstart - 1) .. ("_"):rep(key:len() + offset) .. text:sub(fend + offset + 1)
-				--print(key, text, ({text:gsub("_", ""):gsub(" ", "")})[1])
+				--print(key, text, ({text:gsub("[_%s]+", "")})[1])
 			end
 		until not fstart
 
