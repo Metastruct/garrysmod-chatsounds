7e1f28f7e3b1e417e8c3f82c8e03114e6c86735b
Ornstein0941 <dannyblackguy@outlook.com>
2015-08-02 20:44:33 -0400

    i suck dick for dev

--- lua/chatsounds/lists_nosend/dannyblackguy.lua
+++ lua/chatsounds/lists_nosend/dannyblackguy.lua
@@ -2,16 +2,27 @@ c.StartList("dannyblackguy")
 L["911"]={{path="chatsounds/autoadd/dannyblackguy/911.ogg",length=0.66662131519274381}}
 L["barack obama"]={{path="chatsounds/autoadd/dannyblackguy/barack obama.ogg",length=0.95863945578231291}}
 L["but you must be some kind of retard"]={{path="chatsounds/autoadd/dannyblackguy/but you must be some kind of retard.ogg",length=1.8999999999999999}}
+L["clarence shut the fuck up"]={{path="chatsounds/autoadd/dannyblackguy/clarence shut the fuck up.ogg",length=1.4259410430839001}}
 L["cyka"]={{path="chatsounds/autoadd/dannyblackguy/cyka.ogg",length=0.55000000000000004}}
 L["danny"]={{path="chatsounds/autoadd/dannyblackguy/danny.ogg",length=0.31480725623582767}}
+L["donnys ya man"]={{path="chatsounds/autoadd/dannyblackguy/donnys ya man.ogg",length=1}}
+L["fuckin pussie"]={{path="chatsounds/autoadd/dannyblackguy/fuckin pussie.ogg",length=2.1268253968253967}}
+L["i ant going back"]={{path="chatsounds/autoadd/dannyblackguy/i ant going back.ogg",length=1.8713151927437641}}
+L["if its not made of paper i cant help"]={{path="chatsounds/autoadd/dannyblackguy/if its not made of paper i cant help.ogg",length=2.356281179138322}}
+L["im donny the dealer"]={{path="chatsounds/autoadd/dannyblackguy/im donny the dealer.ogg",length=1.513764172335601}}
+L["its called the stealthy fucka"]={{path="chatsounds/autoadd/dannyblackguy/its called the stealthy fucka.ogg",length=1.7202947845804988}}
 L["japanese people can fly"]={{path="chatsounds/autoadd/dannyblackguy/japanese people can fly.ogg",length=1.8106349206349206}}
 L["look at my fucking car"]={{path="chatsounds/autoadd/dannyblackguy/look at my fucking car.ogg",length=1.8808163265306121}}
 L["rated rp"]={{path="chatsounds/autoadd/dannyblackguy/rated rp.ogg",length=1.9060997732426304}}
-L["senator armstrong"]={{path="chatsounds/autoadd/dannyblackguy/senator armstrong/checked the internet lately.ogg",length=1.2971882086167801},{path="chatsounds/autoadd/dannyblackguy/senator armstrong/dont fuck with this senitor.ogg",length=1.7596825396825397},{path="chatsounds/autoadd/dannyblackguy/senator armstrong/fuck the media.ogg",length=1.3500000000000001},{path="chatsounds/autoadd/dannyblackguy/senator armstrong/making the mother of all omelets here jack.ogg",length=4.1502267573696141},{path="chatsounds/autoadd/dannyblackguy/senator armstrong/played college ball ya know.ogg",length=1.9961451247165534},{path="chatsounds/autoadd/dannyblackguy/senator armstrong/they will make america great again.ogg",length=3.0231519274376417},{path="chatsounds/autoadd/dannyblackguy/senator armstrong/well if it isnt saucy jack.ogg",length=2.5522902494331068}}
+L["save your breath you feathered fuck"]={{path="chatsounds/autoadd/dannyblackguy/save your breath you feathered fuck.ogg",length=1.9846485260770974}}
 L["steamnames"]={{path="chatsounds/autoadd/dannyblackguy/steamnames/bazzinga xd.ogg",length=2.7477777777777779},{path="chatsounds/autoadd/dannyblackguy/steamnames/beating off in church with dad.ogg",length=3.2065306122448978},{path="chatsounds/autoadd/dannyblackguy/steamnames/congratulations future cumlords.ogg",length=3.2402040816326529},{path="chatsounds/autoadd/dannyblackguy/steamnames/crying to radiohead isnt lame.ogg",length=3.6575963718820863},{path="chatsounds/autoadd/dannyblackguy/steamnames/dont call it a cumback.ogg",length=2.2999999999999998},{path="chatsounds/autoadd/dannyblackguy/steamnames/famous dog porn.ogg",length=2.8464172335600906},{path="chatsounds/autoadd/dannyblackguy/steamnames/first the kiss then the cum.ogg",length=3},{path="chatsounds/autoadd/dannyblackguy/steamnames/get paid money to worship satan.ogg",length=3.3557823129251703},{path="chatsounds/autoadd/dannyblackguy/steamnames/i cum slamed ya mum.ogg",length=2.4777777777777779},{path="chatsounds/autoadd/dannyblackguy/steamnames/illegal dubstep mixtape.ogg",length=2.6368934240362814},{path="chatsounds/autoadd/dannyblackguy/steamnames/the fatal cum thought.ogg",length=2},{path="chatsounds/autoadd/dannyblackguy/steamnames/true badass anime dad.ogg",length=3.1370521541950112},{path="chatsounds/autoadd/dannyblackguy/steamnames/we jedi sniper.ogg",length=2.2000000000000002}}
 L["this stuff is your history"]={{path="chatsounds/autoadd/dannyblackguy/this stuff is your history.ogg",length=1.524625850340136}}
 L["welcome home"]={{path="chatsounds/autoadd/dannyblackguy/welcome home.ogg",length=0.98575963718820858}}
+L["what are ya talkin to me"]={{path="chatsounds/autoadd/dannyblackguy/what are ya talkin to me.ogg",length=0.73036281179138318}}
 L["what the fuck"]={{path="chatsounds/autoadd/dannyblackguy/what the fuck.ogg",length=1.1185034013605442}}
+L["yeah shut the fuck up clarence"]={{path="chatsounds/autoadd/dannyblackguy/yeah shut the fuck up clarence.ogg",length=1.9582312925170069}}
 L["you son of a bitch"]={{path="chatsounds/autoadd/dannyblackguy/you son of a bitch.ogg",length=1.073015873015873}}
+L["your fucking going straight to prision"]={{path="chatsounds/autoadd/dannyblackguy/your fucking going straight to prision.ogg",length=1.78}}
+L["yourfuckinjoken"]={{path="chatsounds/autoadd/dannyblackguy/yourfuckinjoken.ogg",length=1.8710430839002268}}
 L["zap zap mother fucker"]={{path="chatsounds/autoadd/dannyblackguy/zap zap mother fucker.ogg",length=1.5893424036281179}}
 c.EndList()
