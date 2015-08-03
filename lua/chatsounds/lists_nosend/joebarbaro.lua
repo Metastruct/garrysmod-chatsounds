f506f84ea21e43f57d47abd4268ac90227fade70
Ornstein0941 <dannyblackguy@outlook.com>
2015-08-03 10:21:59 -0400

    sounds
    i moved some of my sounds around and added a few joe barbaro sounds

--- lua/chatsounds/lists_nosend/joebarbaro.lua
+++ lua/chatsounds/lists_nosend/joebarbaro.lua
@@ -1,8 +1,11 @@
 c.StartList("joebarbaro")
 L["back me up vito"]={{path="chatsounds/autoadd/joebarbaro/back me up vito.ogg",length=0.966530612244898}}
 L["back me up"]={{path="chatsounds/autoadd/joebarbaro/back me up.ogg",length=0.56374149659863948}}
+L["fuck you"]={{path="chatsounds/autoadd/joebarbaro/fuck you.ogg",length=0.65306122448979587}}
 L["im going in"]={{path="chatsounds/autoadd/joebarbaro/im going in.ogg",length=0.78367346938775506}}
+L["jesus you drive like my fucking grandmotha"]={{path="chatsounds/autoadd/joebarbaro/jesus you drive like my fucking grandmotha.ogg",length=2.6122448979591835}}
 L["take your fucking foot off the accelerator"]={{path="chatsounds/autoadd/joebarbaro/take your fucking foot off the accelerator.ogg",length=2.3248979591836734}}
 L["vito"]={{path="chatsounds/autoadd/joebarbaro/vito.ogg",length=0.38834467120181404}}
+L["what the fuck are you doing"]={{path="chatsounds/autoadd/joebarbaro/what the fuck are you doing.ogg",length=1.1232653061224489}}
 L["you drive like my fucking grandmother you know that"]={{path="chatsounds/autoadd/joebarbaro/you drive like my fucking grandmother you know that.ogg",length=2.0897959183673471}}
 c.EndList()
