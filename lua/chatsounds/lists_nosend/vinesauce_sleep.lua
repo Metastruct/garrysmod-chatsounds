0be61cc25774fafc7c9edab8b95848c387334ebe
VerbalSilence <verbalsilence@hotmail.com>
2015-08-11 09:24:43 +1000

    pls pls stay sleep

---
+++ lua/chatsounds/lists_nosend/vinesauce_sleep.lua
@@ -0,0 +1,9 @@
+c.StartList("vinesauce_sleep")
+L["go back to sleep little friend go to sleep and ill rp with you"]={{path="chatsounds/autoadd/vinesauce_sleep/go back to sleep little friend go to sleep and ill rp with you.ogg",length=9.0812018140589572}}
+L["go to bed little man pls do not rp anymore"]={{path="chatsounds/autoadd/vinesauce_sleep/go to bed little man pls do not rp anymore.ogg",length=7.4027664399092972}}
+L["go to sleep let me spit let my lizard spit on your face"]={{path="chatsounds/autoadd/vinesauce_sleep/go to sleep let me spit let my lizard spit on your face.ogg",length=7.7031292517006804}}
+L["go to sleep my friend go baaaack to sleep"]={{path="chatsounds/autoadd/vinesauce_sleep/go to sleep my friend go baaaack to sleep.ogg",length=8.321473922902495}}
+L["go to sleep my friend"]={{path="chatsounds/autoadd/vinesauce_sleep/go to sleep my friend.ogg",length=2.2614739229024945}}
+L["pls pls stay sleep"]={{path="chatsounds/autoadd/vinesauce_sleep/pls pls stay sleep.ogg",length=2.5441269841269842}}
+L["stay sleep my friend i will unzip your lizard"]={{path="chatsounds/autoadd/vinesauce_sleep/stay sleep my friend i will unzip your lizard.ogg",length=7.6147845804988661}}
+c.EndList()
