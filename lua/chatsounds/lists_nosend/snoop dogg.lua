3e5e0e7b2ff0ea980c71a382c1d5363824cd6729
!Avast AntiPony 9445 <kristianzxz41@gmail.com>
2015-08-18 20:08:29 +0200

    Snoop Dogg in da hause
    I swear he don't say "hey"
    
    HOLD UP
    WAAAAAAAIT

---
+++ lua/chatsounds/lists_nosend/snoop dogg.lua
@@ -0,0 +1,5 @@
+c.StartList("snoop dogg")
+L["hold up wait"]={{path="chatsounds/autoadd/snoop dogg/hold up wait.ogg",length=2.8369614512471655}}
+L["hold up"]={{path="chatsounds/autoadd/snoop dogg/hold up.ogg",length=0.84999999999999998}}
+L["wait"]={{path="chatsounds/autoadd/snoop dogg/wait.ogg",length=1.569705215419501}}
+c.EndList()
