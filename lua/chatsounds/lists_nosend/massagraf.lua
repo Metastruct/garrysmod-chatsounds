69cf4dee093b7719ddd4fc69da36e533568499ec
Flippylosaurus <hillebrand@flippylosaurus.eu>
2015-08-30 11:31:37 +0200

    changed samson_and_gert to massagraf, and added more sounds
    As almost all of the sounds from samson_and_gert are sentencemixed by
    Massagraf, I decided to move it to another directory named massagraf. I
    also added some more massagraf sounds and music.

---
+++ lua/chatsounds/lists_nosend/massagraf.lua
@@ -0,0 +1,61 @@
+c.StartList("massagraf")
+L["alehop"]={{path="chatsounds/autoadd/massagraf/alehop.ogg",length=0.49183673469387756}}
+L["als dat maar goed afloopt"]={{path="chatsounds/autoadd/massagraf/als dat maar goed afloopt.ogg",length=1.4097278911564626}}
+L["bosdwergsqueak"]={{path="chatsounds/autoadd/massagraf/bosdwergsqueak.ogg",length=2.5934920634920635}}
+L["dag lieve ploppertjes allemaal welkom op youtube"]={{path="chatsounds/autoadd/massagraf/dag lieve ploppertjes allemaal welkom op youtube.ogg",length=4.5177097505668931}}
+L["dag poepertjes allemaal"]={{path="chatsounds/autoadd/massagraf/dag poepertjes allemaal.ogg",length=1.8787301587301588}}
+L["dat heeft klusje weer eens mooi geregeld zie"]={{path="chatsounds/autoadd/massagraf/dat heeft klusje weer eens mooi geregeld zie.ogg",length=1.9921768707482994}}
+L["dat was weer spanned he kuthond"]={{path="chatsounds/autoadd/massagraf/dat was weer spanned he kuthond.ogg",length=1.4709977324263039}}
+L["draai maar he dan wordt je mischien wat minder dik"]={{path="chatsounds/autoadd/massagraf/draai maar he dan wordt je mischien wat minder dik.ogg",length=2.6651473922902493}}
+L["eh ja goed dan"]={{path="chatsounds/autoadd/massagraf/eh ja goed dan.ogg",length=1.5890022675736961}}
+L["ehge zeg jij het maar van leemhuyzen"]={{path="chatsounds/autoadd/massagraf/ehge zeg jij het maar van leemhuyzen.ogg",length=1.8496371882086169}}
+L["en jij"]={{path="chatsounds/autoadd/massagraf/en jij.ogg",length=0.39095238095238094}}
+L["en jullie weten het je moeder is een plopkoek"]={{path="chatsounds/autoadd/massagraf/en jullie weten het je moeder is een plopkoek.ogg",length=3.0431746031746032}}
+L["er klopt iemand op de deur"]={{path="chatsounds/autoadd/massagraf/er klopt iemand op de deur.ogg",length=1.0089115646258504}}
+L["evil klus"]={{path="chatsounds/autoadd/massagraf/evil klus.ogg",length=1.1503174603174604}}
+L["ga jij naar van leemhuyzen en zeg maar dat ik eraan kom"]={{path="chatsounds/autoadd/massagraf/ga jij naar van leemhuyzen en zeg maar dat ik eraan kom.ogg",length=1.9505215419501134}}
+L["gert is mijn bitch fietswash is de shit begrepen"]={{path="chatsounds/autoadd/massagraf/gert is mijn bitch fietswash is de shit begrepen.ogg",length=3.413424036281179}}
+L["gertje gertje gertje gertje"]={{path="chatsounds/autoadd/massagraf/gertje gertje gertje gertje.ogg",length=1.5259637188208617}}
+L["gertje gertje nee niet doen"]={{path="chatsounds/autoadd/massagraf/gertje gertje nee niet doen.ogg",length=1.7008616780045351}}
+L["gertje zit nog altijd te"]={{path="chatsounds/autoadd/massagraf/gertje zit nog altijd te.ogg",length=1.8160090702947846}}
+L["gertjeeee"]={{path="chatsounds/autoadd/massagraf/gertjeeee.ogg",length=0.95845804988662131}}
+L["gertson ik ga braken"]={{path="chatsounds/autoadd/massagraf/gertson ik ga braken.ogg",length=1.9673469387755103}}
+L["gertson1"]={{path="chatsounds/autoadd/massagraf/gertson1.ogg",length=0.95845804988662131}}
+L["goed zo"]={{path="chatsounds/autoadd/massagraf/goed zo.ogg",length=0.42904761904761907}}
+L["he wat was dat"]={{path="chatsounds/autoadd/massagraf/he wat was dat.ogg",length=0.87340136054421769}}
+L["hebben jullie zin in een dansje"]={{path="chatsounds/autoadd/massagraf/hebben jullie zin in een dansje.ogg",length=1.3120181405895692}}
+L["hehehe nu zullen jullie wel denken wat de fuck is dat nu weer"]={{path="chatsounds/autoadd/massagraf/hehehe nu zullen jullie wel denken wat de fuck is dat nu weer.ogg",length=3.5178231292517008}}
+L["het lijden van de vlaamse bosdwerg theme"]={{path="chatsounds/autoadd/massagraf/het lijden van de vlaamse bosdwerg theme.ogg",length=14.013219954648527}}
+L["hey gertson"]={{path="chatsounds/autoadd/massagraf/hey gertson.ogg",length=1.3031519274376417}}
+L["hij is worst"]={{path="chatsounds/autoadd/massagraf/hij is worst.ogg",length=1.3115646258503402}}
+L["hou jij maar op met je flauwe kut"]={{path="chatsounds/autoadd/massagraf/hou jij maar op met je flauwe kut.ogg",length=1.1287074829931973}}
+L["ik ben een dief"]={{path="chatsounds/autoadd/massagraf/ik ben een dief.ogg",length=9.5002721088435376}}
+L["ik ben hier voor hem"]={{path="chatsounds/autoadd/massagraf/ik ben hier voor hem.ogg",length=1.2258503401360543}}
+L["ik ben sexy"]={{path="chatsounds/autoadd/massagraf/ik ben sexy.ogg",length=0.78188208616780042}}
+L["ik ben woest"]={{path="chatsounds/autoadd/massagraf/ik ben woest.ogg",length=0.70097505668934246}}
+L["ik merkte niet dat klui en lus mijn melkherberg binnenkwamen"]={{path="chatsounds/autoadd/massagraf/ik merkte niet dat klui en lus mijn melkherberg binnenkwamen.ogg",length=4.3236281179138318}}
+L["ik wil ook graag rijk worden"]={{path="chatsounds/autoadd/massagraf/ik wil ook graag rijk worden.ogg",length=1.8664625850340135}}
+L["is er iets"]={{path="chatsounds/autoadd/massagraf/is er iets.ogg",length=0.64317460317460318}}
+L["ja het is waar"]={{path="chatsounds/autoadd/massagraf/ja het is waar.ogg",length=1.2863492063492064}}
+L["ja ja zeg dat was me wat"]={{path="chatsounds/autoadd/massagraf/ja ja zeg dat was me wat.ogg",length=1.6523129251700681}}
+L["ja zeg als jij niet draait dan krijg jij geen 5 taarten he"]={{path="chatsounds/autoadd/massagraf/ja zeg als jij niet draait dan krijg jij geen 5 taarten he.ogg",length=3.5647619047619048}}
+L["je moeder is een plopkoek"]={{path="chatsounds/autoadd/massagraf/je moeder is een plopkoek.ogg",length=1.4379138321995464}}
+L["jij zo dom gertje"]={{path="chatsounds/autoadd/massagraf/jij zo dom gertje.ogg",length=1.2190929705215419}}
+L["kabouter p in da house theme"]={{path="chatsounds/autoadd/massagraf/kabouter p in da house theme.ogg",length=26.765328798185941}}
+L["maar ik zij het nog tegen hem wat een slecht idee om"]={{path="chatsounds/autoadd/massagraf/maar ik zij het nog tegen hem wat een slecht idee om.ogg",length=2.2984353741496597}}
+L["maar nu zullen jullie wel denken waarom ben ik mislukt"]={{path="chatsounds/autoadd/massagraf/maar nu zullen jullie wel denken waarom ben ik mislukt.ogg",length=4.1692063492063491}}
+L["mag ik mee doen"]={{path="chatsounds/autoadd/massagraf/mag ik mee doen.ogg",length=1.5301587301587301}}
+L["mah ik zal het zeggen meneer van veelhuyzen"]={{path="chatsounds/autoadd/massagraf/mah ik zal het zeggen meneer van veelhuyzen.ogg",length=3.190612244897959}}
+L["mijn geld"]={{path="chatsounds/autoadd/massagraf/mijn geld.ogg",length=0.68521541950113374}}
+L["moh gertje die zit daar zo een beetje heel veel te"]={{path="chatsounds/autoadd/massagraf/moh gertje die zit daar zo een beetje heel veel te.ogg",length=3.257891156462585}}
+L["moh gertje"]={{path="chatsounds/autoadd/massagraf/moh gertje.ogg",length=0.82743764172335599}}
+L["oooh"]={{path="chatsounds/autoadd/massagraf/oooh.ogg",length=0.79029478458049884}}
+L["plop lahal"]={{path="chatsounds/autoadd/massagraf/plop lahal.ogg",length=0.9265079365079365}}
+L["plop scream"]={{path="chatsounds/autoadd/massagraf/plop scream.ogg",length=2.0706122448979594}}
+L["smal ja die die was dood"]={{path="chatsounds/autoadd/massagraf/smal ja die die was dood.ogg",length=2.086281179138322}}
+L["van leemhuyzen nu maak je het nog erger dan het al was"]={{path="chatsounds/autoadd/massagraf/van Leemhuyzen nu maak je het nog erger dan het al was.ogg",length=2.2831292517006805}}
+L["wat is dat eigenlijk zo"]={{path="chatsounds/autoadd/massagraf/wat is dat eigenlijk zo.ogg",length=1.6982993197278911}}
+L["wie kan dat zijn"]={{path="chatsounds/autoadd/massagraf/wie kan dat zijn.ogg",length=5.9146938775510201}}
+L["zeg dik ik ben niet dik"]={{path="chatsounds/autoadd/massagraf/zeg dik ik ben niet dik.ogg",length=2.6063038548752835}}
+L["zeg ik heb bijna geen geld meer"]={{path="chatsounds/autoadd/massagraf/zeg ik heb bijna geen geld meer.ogg",length=2.2069614512471656}}
+c.EndList()
