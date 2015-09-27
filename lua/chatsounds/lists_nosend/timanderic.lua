258927315c6105452cf55cddf43015892e392e40
notcake <cakenotfound@gmail.com>
2015-09-27 15:11:49 +0100

    TODO: SCHEDULE DELETION OF ALL SOUND FOLDERS NAMED AFTER PLAYERS.

---
+++ lua/chatsounds/lists_nosend/timanderic.lua
@@ -0,0 +1,5 @@
+c.StartList("timanderic")
+L["right on your lap"]={{path="chatsounds/autoadd/timanderic/right on your lap.ogg",length=1.6905215419501134}}
+L["sit sit sit on you"]={{path="chatsounds/autoadd/timanderic/sit sit sit on you.ogg",length=1.8816099773242629}}
+L["this is what i do i sit on you"]={{path="chatsounds/autoadd/timanderic/this is what I do I sit on you.ogg",length=1.9404081632653061}}
+c.EndList()
