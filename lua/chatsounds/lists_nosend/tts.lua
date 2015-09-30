79646b4941e0844c403cad68f10f97106137e1fd
PotcFdk <PotcFdk@gmx.net>
2015-09-30 17:13:09 +0200

    Move around some files.

---
+++ lua/chatsounds/lists_nosend/tts.lua
@@ -0,0 +1,4 @@
+c.StartList("tts")
+L["barbecue"]={{path="chatsounds/autoadd/tts/barbecue.ogg",length=0.80979591836734699}}
+L["the hook died"]={{path="chatsounds/autoadd/tts/the hook died.ogg",length=0.94222222222222218}}
+c.EndList()
---
+++ lua/chatsounds/lists_nosend/valve.lua
@@ -0,0 +1,3 @@
+c.StartList("valve")
+L["these things they take time"]={{path="chatsounds/autoadd/valve/these things they take time.ogg",length=1.6961451247165533}}
+c.EndList()
