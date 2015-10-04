bc1a7721b418599f5fcf4a280efb7c3ac77fd54b
VerbalSilence <verbalsilence@hotmail.com>
2015-10-04 17:07:07 +1000

    hold you down

---
+++ lua/chatsounds/lists_nosend/djkhaled.lua
@@ -0,0 +1,14 @@
+c.StartList("djkhaled")
+L["come back and ask for more"]={{path="chatsounds/autoadd/djkhaled/come back and ask for more.ogg",length=1.6737641723356009}}
+L["go buy your mom a house"]={{path="chatsounds/autoadd/djkhaled/go buy your mom a house.ogg",length=1.7152607709750567}}
+L["go buy your whole family houses"]={{path="chatsounds/autoadd/djkhaled/go buy your whole family houses.ogg",length=1.9089342403628118}}
+L["go spend some money for no reason"]={{path="chatsounds/autoadd/djkhaled/go spend some money for no reason.ogg",length=2.0749206349206348}}
+L["hold on say my name"]={{path="chatsounds/autoadd/djkhaled/hold on say my name.ogg",length=1.6046031746031746}}
+L["i appreciate that"]={{path="chatsounds/autoadd/djkhaled/i appreciate that.ogg",length=1.7982539682539682}}
+L["put this money in your savings account"]={{path="chatsounds/autoadd/djkhaled/put this money in your savings account.ogg",length=1.9227437641723355}}
+L["say my name baby"]={{path="chatsounds/autoadd/djkhaled/say my name baby/say my name baby 1.ogg",length=1.576938775510204},{path="chatsounds/autoadd/djkhaled/say my name baby/say my name baby 2.ogg",length=1.7844444444444445}}
+L["thats right baby you remember that"]={{path="chatsounds/autoadd/djkhaled/thats right baby you remember that.ogg",length=2.5037414965986393}}
+L["you grateful"]={{path="chatsounds/autoadd/djkhaled/you grateful.ogg",length=1.3417687074829932}}
+L["you loyal"]={{path="chatsounds/autoadd/djkhaled/you loyal.ogg",length=0.85764172335600908}}
+L["you smart"]={{path="chatsounds/autoadd/djkhaled/you smart.ogg",length=1.4247845804988661}}
+c.EndList()
