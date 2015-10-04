67ff07d0074e5ec3c92356d4482a6c18ed589ae7
TrenchFroast <thesteva256@gmail.com>
2015-10-04 23:28:34 +0100

    more shut up/stfu sounds
    conveniently placed in the shut_up folder

---
+++ lua/chatsounds/lists_nosend/shut_up.lua
@@ -0,0 +1,12 @@
+c.StartList("shut_up")
+L["and shut the fuck up"]={{path="chatsounds/autoadd/shut_up/and shut the fuck up.ogg",length=1.2397732426303856}}
+L["drink your fucking milk"]={{path="chatsounds/autoadd/shut_up/drink your fucking milk.ogg",length=1.3119274376417234}}
+L["henry shut up"]={{path="chatsounds/autoadd/shut_up/henry shut up.ogg",length=1.502766439909297}}
+L["please just drink your fucking milk and shut the fuck up"]={{path="chatsounds/autoadd/shut_up/please just drink your fucking milk and shut the fuck up.ogg",length=3.0609070294784582}}
+L["shaddefakkup"]={{path="chatsounds/autoadd/shut_up/shaddefakkup.ogg",length=1.0396825396825398}}
+L["shut up henry"]={{path="chatsounds/autoadd/shut_up/shut up henry/shut up henry1.ogg",length=0.77981859410430843},{path="chatsounds/autoadd/shut_up/shut up henry/shut up henry2.ogg",length=1.0472562358276645}}
+L["shut up shut uuup"]={{path="chatsounds/autoadd/shut_up/shut up shut uuup.ogg",length=2.3798185941043082}}
+L["shut uuup"]={{path="chatsounds/autoadd/shut_up/shut uuup.ogg",length=1.5187301587301587}}
+L["shuttup"]={{path="chatsounds/autoadd/shut_up/shuttup.ogg",length=0.54689342403628116}}
+L["stfu7"]={{path="chatsounds/autoadd/shut_up/stfu7.ogg",length=1.6896145124716553}}
+c.EndList()
