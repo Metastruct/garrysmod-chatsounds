f9dfc31e89789bb4e05748c49fd4cd8ca33b8e63
igniscitrinus <igniscitrinus@users.noreply.github.com>
2015-10-31 16:06:53 +0100

    WITNESS ME SENPAI - closes #37

--- lua/chatsounds/lists_nosend/misc_youtube.lua
+++ lua/chatsounds/lists_nosend/misc_youtube.lua
@@ -1,3 +1,4 @@
 c.StartList("misc_youtube")
+L["notice me senpai"]={{path="chatsounds/autoadd/misc_youtube/notice me senpai.ogg",length=2.5720408163265307}}
 L["oh yeah mister crabs"]={{path="chatsounds/autoadd/misc_youtube/oh yeah mister crabs.ogg",length=2.0172335600907028}}
 c.EndList()
