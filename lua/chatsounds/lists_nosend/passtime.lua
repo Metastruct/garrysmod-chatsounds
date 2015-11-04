7a22c530f8e3fecf4e989bafe5913d25d1837408
!Avast AntiPony 9445 <kristianzxz41@gmail.com>
2015-11-04 21:16:16 +0100

    More Team-Fortress 2 sounds
    Map compiled sounds from pd_watergate, "SCORE!!" from Passtime (Orginal
    "Score" sound removed for some reason) and tf2music.lua

---
+++ lua/chatsounds/lists_nosend/passtime.lua
@@ -0,0 +1,3 @@
+c.StartList("passtime")
+L["score"]={{path="chatsounds/autoadd/passtime/score.ogg",length=4.0756462585034017}}
+c.EndList()
---
+++ lua/chatsounds/lists_nosend/pd_watergate.lua
@@ -0,0 +1,9 @@
+c.StartList("pd_watergate")
+L["pickup beer"]={{path="chatsounds/autoadd/pd_watergate/pickup_beer.ogg",length=1.4106122448979592}}
+L["the enemy is in the beam"]={{path="chatsounds/autoadd/pd_watergate/temp_inbeam_enemy.ogg",length=2.8734693877551019}}
+L["we are in the beam"]={{path="chatsounds/autoadd/pd_watergate/temp_inbeam_own.ogg",length=2.27265306122449}}
+L["collect beer and deposit it into the ufo beam to score points"]={{path="chatsounds/autoadd/pd_watergate/temp_intro.ogg",length=6.1387755102040815}}
+L["theyve almost made the ufo totally drunk"]={{path="chatsounds/autoadd/pd_watergate/temp_theyvealmostmadeufodrunk.ogg",length=4.4146938775510201}}
+L["we have almost made the ufo totally drunk"]={{path="chatsounds/autoadd/pd_watergate/temp_wevealmostmadetheufodrunk.ogg",length=4.963265306122449}}
+L["we have almost made the ufo to tally drunk"]={{path="chatsounds/autoadd/pd_watergate/temp_wevealmostmadetheufodrunk.ogg",length=4.963265306122449}}
+c.EndList()
