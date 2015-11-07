ad6f3549161700e42090c755fd6699ee42010dfe
VerbalSilence <verbalsilence@hotmail.com>
2015-11-08 01:01:31 +1000

    dunkey sounds

---
+++ lua/chatsounds/lists_nosend/dunkey.lua
@@ -0,0 +1,8 @@
+c.StartList("dunkey")
+L["and he ran he ran so far away"]={{path="chatsounds/autoadd/dunkey/and he ran he ran so far away.ogg",length=6.5753061224489793}}
+L["and i ran i ran so far away"]={{path="chatsounds/autoadd/dunkey/and i ran i ran so far away.ogg",length=12.508458049886622}}
+L["dunkey rage"]={{path="chatsounds/autoadd/dunkey/dunkey rage.ogg",length=6.0607936507936504}}
+L["eww"]={{path="chatsounds/autoadd/dunkey/eww/eww1.ogg",length=0.88145124716553291},{path="chatsounds/autoadd/dunkey/eww/eww2.ogg",length=1.0708390022675738}}
+L["oh not even close baby woo"]={{path="chatsounds/autoadd/dunkey/oh not even close baby woo.ogg",length=6.8578231292517007}}
+L["stfu8"]={{path="chatsounds/autoadd/dunkey/stfu8.ogg",length=2.7317233560090703}}
+c.EndList()
--- lua/chatsounds/lists_nosend/pd_watergate.lua
+++ lua/chatsounds/lists_nosend/pd_watergate.lua
@@ -1,9 +1,8 @@
 c.StartList("pd_watergate")
-L["pickup beer"]={{path="chatsounds/autoadd/pd_watergate/pickup_beer.ogg",length=1.4106122448979592}}
-L["the enemy is in the beam"]={{path="chatsounds/autoadd/pd_watergate/temp_inbeam_enemy.ogg",length=2.8734693877551019}}
-L["we are in the beam"]={{path="chatsounds/autoadd/pd_watergate/temp_inbeam_own.ogg",length=2.27265306122449}}
-L["collect beer and deposit it into the ufo beam to score points"]={{path="chatsounds/autoadd/pd_watergate/temp_intro.ogg",length=6.1387755102040815}}
-L["theyve almost made the ufo totally drunk"]={{path="chatsounds/autoadd/pd_watergate/temp_theyvealmostmadeufodrunk.ogg",length=4.4146938775510201}}
-L["we have almost made the ufo totally drunk"]={{path="chatsounds/autoadd/pd_watergate/temp_wevealmostmadetheufodrunk.ogg",length=4.963265306122449}}
-L["we have almost made the ufo to tally drunk"]={{path="chatsounds/autoadd/pd_watergate/temp_wevealmostmadetheufodrunk.ogg",length=4.963265306122449}}
+L["pickup_beer"]={{path="chatsounds/autoadd/pd_watergate/pickup_beer.ogg",length=1.4106122448979592}}
+L["temp_inbeam_enemy"]={{path="chatsounds/autoadd/pd_watergate/temp_inbeam_enemy.ogg",length=2.8734693877551019}}
+L["temp_inbeam_own"]={{path="chatsounds/autoadd/pd_watergate/temp_inbeam_own.ogg",length=2.27265306122449}}
+L["temp_intro"]={{path="chatsounds/autoadd/pd_watergate/temp_intro.ogg",length=6.1387755102040815}}
+L["temp_theyvealmostmadeufodrunk"]={{path="chatsounds/autoadd/pd_watergate/temp_theyvealmostmadeufodrunk.ogg",length=4.4146938775510201}}
+L["temp_wevealmostmadetheufodrunk"]={{path="chatsounds/autoadd/pd_watergate/temp_wevealmostmadetheufodrunk.ogg",length=4.963265306122449}}
 c.EndList()
