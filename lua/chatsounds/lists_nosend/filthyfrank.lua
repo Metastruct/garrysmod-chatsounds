feee6123418c36d1832f58626a27a0531548f1f1
igniscitrinus <igniscitrinus@users.noreply.github.com>
2015-11-26 17:36:08 +0100

    Added filthy sounds + YOOOOOOOOOOO

--- lua/chatsounds/lists_nosend/filthyfrank.lua
+++ lua/chatsounds/lists_nosend/filthyfrank.lua
@@ -1,16 +1,28 @@
 c.StartList("filthyfrank")
 L["are you gay"]={{path="chatsounds/autoadd/filthyfrank/are you gay.ogg",length=0.89396825396825397}}
+L["balls"]={{path="chatsounds/autoadd/filthyfrank/balls.ogg",length=0.70528344671201815}}
 L["bbbbboss"]={{path="chatsounds/autoadd/filthyfrank/bbbbboss.ogg",length=2.8633333333333333}}
 L["boss"]={{path="chatsounds/autoadd/filthyfrank/boss/boss1.ogg",length=0.76997732426303855},{path="chatsounds/autoadd/filthyfrank/boss/boss2.ogg",length=0.86621315192743764}}
 L["boss i have cancer"]={{path="chatsounds/autoadd/filthyfrank/Boss i have cancer.ogg",length=2.2377324263038547}}
 L["boss pls"]={{path="chatsounds/autoadd/filthyfrank/boss pls.ogg",length=0.82430839002267575}}
+L["boss pls gibe de pusi boss"]={{path="chatsounds/autoadd/filthyfrank/boss pls gibe de pusi boss.ogg",length=2.4473696145124717}}
 L["can i have pizza pls"]={{path="chatsounds/autoadd/filthyfrank/can i have pizza pls/Can i have a pizza pls1.ogg",length=1.3474603174603175},{path="chatsounds/autoadd/filthyfrank/can i have pizza pls/Can i have a pizza pls2.ogg",length=1.6722902494331067}}
 L["can i have pussy"]={{path="chatsounds/autoadd/filthyfrank/can i have pussy.ogg",length=1.3631972789115647}}
+L["existentialmacbookgroaning"]={{path="chatsounds/autoadd/filthyfrank/existentialmacbookgroaning.ogg",length=6.3685034013605444}}
+L["frank throw me in the river so that i can be at peace"]={{path="chatsounds/autoadd/filthyfrank/frank throw me in the river so that i can be at peace.ogg",length=3.8580498866213153}}
 L["franksegue"]={{path="chatsounds/autoadd/filthyfrank/franksegue/franksegue1.ogg",length=14.341224489795918},{path="chatsounds/autoadd/filthyfrank/franksegue/franksegue2.ogg",length=9.6342630385487524}}
+L["give me the suc"]={{path="chatsounds/autoadd/filthyfrank/give me the suc/give me the suc1.ogg",length=1.1301814058956916},{path="chatsounds/autoadd/filthyfrank/give me the suc/give me the suc2.ogg",length=1.6340136054421768}}
 L["hey boss"]={{path="chatsounds/autoadd/filthyfrank/hey boss/Hey boss1.ogg",length=0.40904761904761905},{path="chatsounds/autoadd/filthyfrank/hey boss/Hey boss2.ogg",length=0.66170068027210882},{path="chatsounds/autoadd/filthyfrank/hey boss/Hey boss3.ogg",length=0.8180952380952381}}
 L["hey boss can i have a pizza pls"]={{path="chatsounds/autoadd/filthyfrank/Hey boss can i have a pizza pls.ogg",length=2.6467800453514738}}
 L["hey what the hell do you want"]={{path="chatsounds/autoadd/filthyfrank/hey what the hell do you want.ogg",length=1.1609977324263039}}
+L["i envy death"]={{path="chatsounds/autoadd/filthyfrank/i envy death.ogg",length=1.2982993197278911}}
 L["i have cancer boss"]={{path="chatsounds/autoadd/filthyfrank/i have cancer boss.ogg",length=1.612562358276644}}
+L["i knew everything in the universe yet i could not fall in love"]={{path="chatsounds/autoadd/filthyfrank/i knew everything in the universe yet i could not fall in love.ogg",length=4.5336054421768708}}
+L["i let the beat drop like old people with polio"]={{path="chatsounds/autoadd/filthyfrank/i let the beat drop like old people with polio.ogg",length=4.3328117913832198}}
+L["i was first born in a laboratory unknowingly falling into a pit of immortality"]={{path="chatsounds/autoadd/filthyfrank/i was first born in a laboratory unknowingly falling into a pit of immortality.ogg",length=5.308412698412698}}
+L["i was forever trapped in an infinite timeline of pain and suffering"]={{path="chatsounds/autoadd/filthyfrank/i was forever trapped in an infinite timeline of pain and suffering.ogg",length=4.3870294784580501}}
+L["i will be your friend you fat asian fuck"]={{path="chatsounds/autoadd/filthyfrank/i will be your friend you fat asian fuck.ogg",length=2.8155102040816327}}
+L["its filthy frank motherfucker its filthy frank bitch"]={{path="chatsounds/autoadd/filthyfrank/its filthy frank motherfucker its filthy frank bitch.ogg",length=5.0197732426303858}}
 L["lets get some pussy tonight"]={{path="chatsounds/autoadd/filthyfrank/lets get some pussy tonight.ogg",length=2.6354648526077096}}
 L["looks like the hardest thing about this case is my cock"]={{path="chatsounds/autoadd/filthyfrank/looks like the hardest thing about this case is my cock.ogg",length=4.2724716553287978}}
 L["moshi moshi motherfucker"]={{path="chatsounds/autoadd/filthyfrank/moshi moshi motherfucker.ogg",length=1.799546485260771}}
@@ -18,6 +30,8 @@ L["nnnggg"]={{path="chatsounds/autoadd/filthyfrank/nnnggg.ogg",length=1.49623582
 L["nnnooo"]={{path="chatsounds/autoadd/filthyfrank/nnnooo.ogg",length=2.0549659863945577}}
 L["nyes"]={{path="chatsounds/autoadd/filthyfrank/nyes/nyes1.ogg",length=1.364172335600907},{path="chatsounds/autoadd/filthyfrank/nyes/nyes2.ogg",length=2.1362358276643989}}
 L["roasted"]={{path="chatsounds/autoadd/filthyfrank/roasted.ogg",length=11.363265306122448}}
+L["these bitches wanna suck my balls"]={{path="chatsounds/autoadd/filthyfrank/these bitches wanna suck my balls.ogg",length=2.9654875283446711}}
+L["throw me in the river and let me sink to the bottom"]={{path="chatsounds/autoadd/filthyfrank/throw me in the river and let me sink to the bottom.ogg",length=3.2032426303854877}}
 L["why did you leave me"]={{path="chatsounds/autoadd/filthyfrank/why did you leave me.ogg",length=1.7099773242630385}}
 L["whyyy"]={{path="chatsounds/autoadd/filthyfrank/whyyy.ogg",length=1.4160770975056689}}
 L["yes boss"]={{path="chatsounds/autoadd/filthyfrank/yes boss/yes boss1.ogg",length=1.3611791383219956},{path="chatsounds/autoadd/filthyfrank/yes boss/yes boss2.ogg",length=0.78199546485260774}}
--- lua/chatsounds/lists_nosend/misc_youtube.lua
+++ lua/chatsounds/lists_nosend/misc_youtube.lua
@@ -1,4 +1,5 @@
 c.StartList("misc_youtube")
 L["notice me senpai"]={{path="chatsounds/autoadd/misc_youtube/notice me senpai.ogg",length=2.5720408163265307}}
 L["oh yeah mister crabs"]={{path="chatsounds/autoadd/misc_youtube/oh yeah mister crabs.ogg",length=2.0172335600907028}}
+L["yooooooooooo"]={{path="chatsounds/autoadd/misc_youtube/yooooooooooo.ogg",length=2.5658049886621317}}
 c.EndList()
