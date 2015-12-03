f1dcf82a98aa6d53fa0cc5708c96e34910fd261f
PotcFdk <PotcFdk@gmx.net>
2015-12-03 23:34:48 +0100

    Fix priviledgy typo

--- lua/chatsounds/lists_nosend/internet comment etiquette.lua
+++ lua/chatsounds/lists_nosend/internet comment etiquette.lua
@@ -4,6 +4,6 @@ L["l o l"]={{path="chatsounds/autoadd/internet comment etiquette/l o l.ogg",leng
 L["let me show you"]={{path="chatsounds/autoadd/internet comment etiquette/let me show you.ogg",length=1.2319954648526077}}
 L["nothing"]={{path="chatsounds/autoadd/internet comment etiquette/nothing.ogg",length=0.85303854875283447}}
 L["oh shit no no no no"]={{path="chatsounds/autoadd/internet comment etiquette/oh shit no no no no.ogg",length=1.8833786848072562}}
-L["priviledge check"]={{path="chatsounds/autoadd/internet comment etiquette/priviledge check/priviledge check 2.ogg",length=1.3875510204081634},{path="chatsounds/autoadd/internet comment etiquette/priviledge check/priviledge check.ogg",length=2.865170068027211}}
+L["privilege check"]={{path="chatsounds/autoadd/internet comment etiquette/privilege check/privilege check 2.ogg",length=1.3875510204081634},{path="chatsounds/autoadd/internet comment etiquette/privilege check/privilege check.ogg",length=2.865170068027211}}
 L["you do not have the permission to get that upset"]={{path="chatsounds/autoadd/internet comment etiquette/you do not have the permission to get that upset.ogg",length=2.2245578231292518}}
 c.EndList()
