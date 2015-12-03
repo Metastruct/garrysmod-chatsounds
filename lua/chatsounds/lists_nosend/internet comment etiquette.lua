99313cacfa46adda95459fe3b40c2f31630d341b
PotcFdk <PotcFdk@gmx.net>
2015-12-03 19:56:33 +0100

    *rings bell* closes #38

---
+++ lua/chatsounds/lists_nosend/internet comment etiquette.lua
@@ -0,0 +1,9 @@
+c.StartList("internet comment etiquette")
+L["hashtag antisemitism"]={{path="chatsounds/autoadd/internet comment etiquette/hashtag antisemitism.ogg",length=1.5316326530612245}}
+L["l o l"]={{path="chatsounds/autoadd/internet comment etiquette/l o l.ogg",length=0.78090702947845803}}
+L["let me show you"]={{path="chatsounds/autoadd/internet comment etiquette/let me show you.ogg",length=1.2319954648526077}}
+L["nothing"]={{path="chatsounds/autoadd/internet comment etiquette/nothing.ogg",length=0.85303854875283447}}
+L["oh shit no no no no"]={{path="chatsounds/autoadd/internet comment etiquette/oh shit no no no no.ogg",length=1.8833786848072562}}
+L["priviledge check"]={{path="chatsounds/autoadd/internet comment etiquette/priviledge check/priviledge check 2.ogg",length=1.3875510204081634},{path="chatsounds/autoadd/internet comment etiquette/priviledge check/priviledge check.ogg",length=2.865170068027211}}
+L["you do not have the permission to get that upset"]={{path="chatsounds/autoadd/internet comment etiquette/you do not have the permission to get that upset.ogg",length=2.2245578231292518}}
+c.EndList()
