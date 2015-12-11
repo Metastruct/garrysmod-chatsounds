64e1880cba2ba09d3c485599eac7ff291f992b97
Beauty-Bomber <sasuke_uchiha13@hotmail.co.uk>
2015-12-11 17:41:53 +0000

    here you go midori kek

--- lua/chatsounds/lists_nosend/Beauty_Bomber.lua
+++ lua/chatsounds/lists_nosend/Beauty_Bomber.lua
@@ -49,6 +49,7 @@ L["that that you let a penis penetrate your friendship penis"]={{path="chatsound
 L["theatreland"]={{path="chatsounds/autoadd/Beauty_Bomber/theatreland.ogg",length=27.813242630385488}}
 L["who put you on the planet"]={{path="chatsounds/autoadd/Beauty_Bomber/who put you on the planet/1.ogg",length=2.4715873015873018},{path="chatsounds/autoadd/Beauty_Bomber/who put you on the planet/2.ogg",length=2.8089569160997732}}
 L["wilsooooon"]={{path="chatsounds/autoadd/Beauty_Bomber/wilsooooon/1.ogg",length=4.2000000000000002},{path="chatsounds/autoadd/Beauty_Bomber/wilsooooon/2.ogg",length=1.5248526077097506}}
+L["woodyscream"]={{path="chatsounds/autoadd/Beauty_Bomber/woodyscream.ogg",length=0.47854875283446713}}
 L["wotwtripod"]={{path="chatsounds/autoadd/Beauty_Bomber/wotwtripod.ogg",length=9.06562358276644}}
 L["youre an inanimate fucking object"]={{path="chatsounds/autoadd/Beauty_Bomber/youre an inanimate fucking object.ogg",length=2.1220181405895691}}
 c.EndList()
