a32cc5eb8c7c3570588dd8672b614420c53a901c
techbot <techbot121@gmail.com>
2015-12-29 00:51:41 +0100

    yes

--- lua/chatsounds/lists_nosend/jet_set_radio.lua
+++ lua/chatsounds/lists_nosend/jet_set_radio.lua
@@ -7,4 +7,6 @@ L["im gonna"]={{path="chatsounds/autoadd/jet_set_radio/im gonna.ogg",length=1.22
 L["oh get outta my way"]={{path="chatsounds/autoadd/jet_set_radio/oh get outta my way.ogg",length=1.1467120181405897}}
 L["oh jeez my knee"]={{path="chatsounds/autoadd/jet_set_radio/oh jeez my knee.ogg",length=1.7657596371882087}}
 L["oh oh my toe my toe"]={{path="chatsounds/autoadd/jet_set_radio/oh oh my toe my toe.ogg",length=1.6419501133786849}}
+L["sneakman1"]={{path="chatsounds/autoadd/jet_set_radio/sneakman1.ogg",length=15.370453514739228}}
+L["sneakman2"]={{path="chatsounds/autoadd/jet_set_radio/sneakman2.ogg",length=9.1202494331065758}}
 c.EndList()
