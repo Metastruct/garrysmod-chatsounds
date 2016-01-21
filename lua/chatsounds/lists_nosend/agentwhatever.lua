1165765e83c0fd311d17ed17e204d4a8ffaf7eb9
AgentWhatever <psycoclownagent@gmail.com>
2016-01-21 22:41:52 +0100

    Personal bs folder and stuff from https://www.youtube.com/watch?v=vFfzI8IUgE8

---
+++ lua/chatsounds/lists_nosend/agentwhatever.lua
@@ -0,0 +1,4 @@
+c.StartList("agentwhatever")
+L["pulsejet"]={{path="chatsounds/autoadd/agentwhatever/pulsejet.ogg",length=30.04111111111111}}
+L["toomanycooks"]={{path="chatsounds/autoadd/agentwhatever/toomanycooks.ogg",length=669.36945578231291}}
+c.EndList()
