01b70750335b49092edb6dc400337b1f2f223760
SpiralP <spiral.palmer@gmail.com>
2016-01-20 17:54:25 -0800

    lists

--- lua/chatsounds/lists_nosend/amin_peck.lua
+++ lua/chatsounds/lists_nosend/amin_peck.lua
@@ -1,3 +1,5 @@
 c.StartList("amin_peck")
 L["bowlingpins"]={{path="chatsounds/autoadd/amin_peck/bowlingpins.ogg",length=1.1070748299319728}}
+L["doctor breerb"]={{path="chatsounds/autoadd/amin_peck/doctor breerb.ogg",length=1.1493877551020408}}
+L["ile"]={{path="chatsounds/autoadd/amin_peck/ile.wav",length=0.80764172335600903}}
 c.EndList()
