325dde3cb5d1e34f980965381f5d3814e7ed45b5
Lixquid <Lixquid@users.noreply.github.com>
2016-01-24 18:20:22 +0000

    Added splatoon noises

--- lua/chatsounds/lists_nosend/halloween.lua
+++ lua/chatsounds/lists_nosend/halloween.lua
@@ -10,7 +10,6 @@ L["sssending"]={{path="chatsounds/autoadd/halloween/sssending.ogg",length=12.587
 L["sssintro"]={{path="chatsounds/autoadd/halloween/sssintro.ogg",length=12.258458049886622}}
 L["thats the trash can"]={{path="chatsounds/autoadd/halloween/thats the trash can.ogg",length=1.7848752834467121}}
 L["were so sorry skeletons youre so misunderstood"]={{path="chatsounds/autoadd/halloween/were so sorry skeletons youre so misunderstood.ogg",length=6.1918820861678006}}
-L["woomy"]={{path="chatsounds/autoadd/halloween/woomy.ogg",length=0.37587301587301586}}
 L["you only want to socialize but i dont think we should"]={{path="chatsounds/autoadd/halloween/you only want to socialize but i dont think we should.ogg",length=5.9615646258503405}}
 L["youll shake and shudder in surprise when you hear the zombies shriek"]={{path="chatsounds/autoadd/halloween/youll shake and shudder in surprise when you hear the zombies shriek.ogg",length=6.5577097505668931}}
 c.EndList()
