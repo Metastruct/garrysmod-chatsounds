a1415755b601ed1d078f3b18308a081faf84e207
CamoBunny <camobunny101@sky.com>
2016-01-26 20:57:54 +0000

    one small step for camo!

---
+++ lua/chatsounds/lists_nosend/camobunny.lua
@@ -0,0 +1,13 @@
+c.StartList("camobunny")
+L["deez nuts"]={{path="chatsounds/autoadd/camobunny/deez nuts.ogg",length=0.83353741496598643}}
+L["ha got eem"]={{path="chatsounds/autoadd/camobunny/ha got eem.ogg",length=1.8146938775510204}}
+L["its hip to fuck bees"]={{path="chatsounds/autoadd/camobunny/its hip to fuck bees.ogg",length=1.5057369614512472}}
+L["mmmmmmohmygod"]={{path="chatsounds/autoadd/camobunny/mmmmmmohmygod.ogg",length=1.685873015873016}}
+L["my father gave me a small loan of a million dollars"]={{path="chatsounds/autoadd/camobunny/my father gave me a small loan of a million dollars.ogg",length=2.3236507936507937}}
+L["small loan of a million dollars"]={{path="chatsounds/autoadd/camobunny/small loan of a million dollars.ogg",length=1.442562358276644}}
+L["stop fuckin lying"]={{path="chatsounds/autoadd/camobunny/stop fuckin lying.ogg",length=2.1288888888888891}}
+L["thuglife"]={{path="chatsounds/autoadd/camobunny/thuglife.ogg",length=9.5401133786848078}}
+L["why the fuck you lying"]={{path="chatsounds/autoadd/camobunny/why the fuck you lying.ogg",length=2.3957142857142859}}
+L["why you always lying"]={{path="chatsounds/autoadd/camobunny/why you always lying.ogg",length=2.057437641723356}}
+L["you know i i started off in brooklyn"]={{path="chatsounds/autoadd/camobunny/you know i i started off in brooklyn.ogg",length=1.7017006802721089}}
+c.EndList()
