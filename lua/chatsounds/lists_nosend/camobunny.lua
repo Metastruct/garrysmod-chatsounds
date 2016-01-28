5762cd78b7d0b7cbcf72ef03e6e60824c86d4ab1
CamoBunny <camobunny101@sky.com>
2016-01-28 20:04:11 +0000

    requested sound by mike, plus some other stuff

--- lua/chatsounds/lists_nosend/camobunny.lua
+++ lua/chatsounds/lists_nosend/camobunny.lua
@@ -1,13 +1,34 @@
 c.StartList("camobunny")
+L["caillou was making a fucking mess"]={{path="chatsounds/autoadd/camobunny/caillou was making a fucking mess.ogg",length=2.026281179138322}}
+L["caillous big furry finger up the butt show"]={{path="chatsounds/autoadd/camobunny/caillous big furry finger up the butt show.ogg",length=3.643015873015873}}
+L["cuz sum came in the mail today"]={{path="chatsounds/autoadd/camobunny/cuz sum came in the mail today.ogg",length=1.7799546485260771}}
 L["deez nuts"]={{path="chatsounds/autoadd/camobunny/deez nuts.ogg",length=0.83353741496598643}}
+L["dinadle"]={{path="chatsounds/autoadd/camobunny/dinadle.ogg",length=0.78650793650793649}}
+L["electricsquidsisters"]={{path="chatsounds/autoadd/camobunny/electricsquidsisters.ogg",length=67.849614512471661}}
+L["fuckbeessong"]={{path="chatsounds/autoadd/camobunny/fuckbeessong.ogg",length=73.781043083900229}}
+L["fuuck off"]={{path="chatsounds/autoadd/camobunny/fuuck off.ogg",length=1.1744671201814059}}
 L["ha got eem"]={{path="chatsounds/autoadd/camobunny/ha got eem.ogg",length=1.8146938775510204}}
+L["he thought it would be fun to be xavior"]={{path="chatsounds/autoadd/camobunny/he thought it would be fun to be xavior.ogg",length=2.8238548752834469}}
+L["i got one question for you"]={{path="chatsounds/autoadd/camobunny/i got one question for you.ogg",length=1.342517006802721}}
 L["its hip to fuck bees"]={{path="chatsounds/autoadd/camobunny/its hip to fuck bees.ogg",length=1.5057369614512472}}
+L["lal"]={{path="chatsounds/autoadd/camobunny/lal.ogg",length=0.2856235827664399}}
+L["mingebag in the bin"]={{path="chatsounds/autoadd/camobunny/mingebag in the bin.ogg",length=1.3960090702947845}}
 L["mmmmmmohmygod"]={{path="chatsounds/autoadd/camobunny/mmmmmmohmygod.ogg",length=1.685873015873016}}
+L["monkey on the car"]={{path="chatsounds/autoadd/camobunny/monkey on the car/1.ogg",length=1.3053968253968253},{path="chatsounds/autoadd/camobunny/monkey on the car/2.ogg",length=1.4979365079365079}}
 L["my father gave me a small loan of a million dollars"]={{path="chatsounds/autoadd/camobunny/my father gave me a small loan of a million dollars.ogg",length=2.3236507936507937}}
+L["my name is patback"]={{path="chatsounds/autoadd/camobunny/my name is patback.ogg",length=3.4355102040816328}}
+L["noooo me toastaaa"]={{path="chatsounds/autoadd/camobunny/noooo me toastaaa.ogg",length=3.6314739229024942}}
+L["officer i got one question for you"]={{path="chatsounds/autoadd/camobunny/officer i got one question for you.ogg",length=1.7023356009070294}}
+L["or some shit"]={{path="chatsounds/autoadd/camobunny/or some shit.ogg",length=1.1856009070294784}}
+L["patback"]={{path="chatsounds/autoadd/camobunny/patback.ogg",length=1.6525850340136055}}
+L["please pass the milk please"]={{path="chatsounds/autoadd/camobunny/please pass the milk please.ogg",length=7.7273015873015876}}
 L["small loan of a million dollars"]={{path="chatsounds/autoadd/camobunny/small loan of a million dollars.ogg",length=1.442562358276644}}
 L["stop fuckin lying"]={{path="chatsounds/autoadd/camobunny/stop fuckin lying.ogg",length=2.1288888888888891}}
 L["thuglife"]={{path="chatsounds/autoadd/camobunny/thuglife.ogg",length=9.5401133786848078}}
+L["what are thooooooose"]={{path="chatsounds/autoadd/camobunny/what are thooooooose.ogg",length=2.5572335600907028}}
+L["whatarethosejurassicpark"]={{path="chatsounds/autoadd/camobunny/whatarethosejurassicpark.ogg",length=6.5029251700680275}}
 L["why the fuck you lying"]={{path="chatsounds/autoadd/camobunny/why the fuck you lying.ogg",length=2.3957142857142859}}
 L["why you always lying"]={{path="chatsounds/autoadd/camobunny/why you always lying.ogg",length=2.057437641723356}}
+L["you forgot the pickle"]={{path="chatsounds/autoadd/camobunny/you forgot the pickle.ogg",length=3.656190476190476}}
 L["you know i i started off in brooklyn"]={{path="chatsounds/autoadd/camobunny/you know i i started off in brooklyn.ogg",length=1.7017006802721089}}
 c.EndList()
