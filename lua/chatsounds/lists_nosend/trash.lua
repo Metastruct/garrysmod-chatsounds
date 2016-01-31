ccc4c5b9485c3b607c91a67033af0312feceaab5
Mikeai <mgamkrel@gmail.com>
2016-02-01 00:49:13 +0200

    ITS TRASH

---
+++ lua/chatsounds/lists_nosend/trash.lua
@@ -0,0 +1,11 @@
+c.StartList("trash")
+L["and then i pick up the trash can and bash the guy on the head"]={{path="chatsounds/autoadd/trash/and then i pick up the trash can and bash the guy on the head.ogg",length=4.5858276643990932}}
+L["and then i start eating garbage"]={{path="chatsounds/autoadd/trash/and then i start eating garbage.ogg",length=2.2608390022675735}}
+L["frank what the hell are you doing man"]={{path="chatsounds/autoadd/trash/frank what the hell are you doing man.ogg",length=1.3709297052154195}}
+L["i come out i throw trash all over the ring"]={{path="chatsounds/autoadd/trash/i come out i throw trash all over the ring.ogg",length=3.9845351473922901}}
+L["im telling you its like you said its all fake"]={{path="chatsounds/autoadd/trash/im telling you its like you said its all fake.ogg",length=2.728344671201814}}
+L["im the trash man"]={{path="chatsounds/autoadd/trash/im the trash man.ogg",length=1.4430839002267575}}
+L["its my character"]={{path="chatsounds/autoadd/trash/its my character.ogg",length=1.2025850340136055}}
+L["nah no nobodys gonna get hurt"]={{path="chatsounds/autoadd/trash/nah no nobodys gonna get hurt.ogg",length=1.5713832199546485}}
+L["thats gonna be a disaster"]={{path="chatsounds/autoadd/trash/thats gonna be a disaster.ogg",length=1.7196825396825397}}
+c.EndList()
