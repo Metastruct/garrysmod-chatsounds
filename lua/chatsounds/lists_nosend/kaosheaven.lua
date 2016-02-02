57b12ff20abf062e01c42f7727cc820e2b60a5d3
KaosHeaven <redmagus8@gmail.com>
2016-02-02 00:45:40 -0600

    more chatsounds

--- lua/chatsounds/lists_nosend/kaosheaven.lua
+++ lua/chatsounds/lists_nosend/kaosheaven.lua
@@ -1,4 +1,5 @@
 c.StartList("kaosheaven")
 L["ohoh"]={{path="chatsounds/autoadd/kaosheaven/ohoh.ogg",length=1.1681179138321995}}
 L["trials"]={{path="chatsounds/autoadd/kaosheaven/trials.ogg",length=79.226485260770971}}
+L["yeah dude"]={{path="chatsounds/autoadd/kaosheaven/yeah dude.ogg",length=1.2402494331065759}}
 c.EndList()
