765a9572e1b74c9a4cab2a0a6a9bf8b78b60bc37
KaosHeaven <redmagus8@gmail.com>
2016-02-02 00:08:43 -0600

    weird al yankovich in the drive thru ohoh

--- lua/chatsounds/lists_nosend/kaosheaven.lua
+++ lua/chatsounds/lists_nosend/kaosheaven.lua
@@ -1,3 +1,4 @@
 c.StartList("kaosheaven")
+L["ohoh"]={{path="chatsounds/autoadd/kaosheaven/ohoh.ogg",length=1.1681179138321995}}
 L["trials"]={{path="chatsounds/autoadd/kaosheaven/trials.ogg",length=79.226485260770971}}
 c.EndList()
