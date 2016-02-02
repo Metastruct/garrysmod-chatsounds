6e263d17f84e82ba8b091ead956563221c7e4a81
KaosHeaven <redmagus8@gmail.com>
2016-02-02 15:14:58 -0600

    home made chatsound :3

--- lua/chatsounds/lists_nosend/kaosheaven.lua
+++ lua/chatsounds/lists_nosend/kaosheaven.lua
@@ -1,4 +1,5 @@
 c.StartList("kaosheaven")
+L["has anyone heard of a website called meatspin"]={{path="chatsounds/autoadd/kaosheaven/has anyone heard of a website called meatspin.ogg",length=5.7324263038548757}}
 L["ohoh"]={{path="chatsounds/autoadd/kaosheaven/ohoh.ogg",length=1.1681179138321995}}
 L["trials"]={{path="chatsounds/autoadd/kaosheaven/trials.ogg",length=79.226485260770971}}
 L["yeah dude"]={{path="chatsounds/autoadd/kaosheaven/yeah dude.ogg",length=1.2402494331065759}}
