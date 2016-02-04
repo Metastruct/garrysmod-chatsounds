c0d0dcceb977c554e7acf4f54c7f2d20a067eac3
KaosHeaven <redmagus8@gmail.com>
2016-02-03 19:46:09 -0600

    amplified a file and added a couple of jingles that were missing

--- lua/chatsounds/lists_nosend/kaosheaven.lua
+++ lua/chatsounds/lists_nosend/kaosheaven.lua
@@ -1,6 +1,8 @@
 c.StartList("kaosheaven")
 L["has anyone heard of a website called meatspin"]={{path="chatsounds/autoadd/kaosheaven/has anyone heard of a website called meatspin.ogg",length=5.7324263038548757}}
+L["nokia"]={{path="chatsounds/autoadd/kaosheaven/nokia.ogg",length=5.0619501133786846}}
 L["ohoh"]={{path="chatsounds/autoadd/kaosheaven/ohoh.ogg",length=1.1681179138321995}}
+L["the more you know"]={{path="chatsounds/autoadd/kaosheaven/the more you know.ogg",length=6.2693877551020405}}
 L["trials"]={{path="chatsounds/autoadd/kaosheaven/trials.ogg",length=79.226485260770971}}
 L["yeah dude"]={{path="chatsounds/autoadd/kaosheaven/yeah dude.ogg",length=1.2402494331065759}}
 c.EndList()
