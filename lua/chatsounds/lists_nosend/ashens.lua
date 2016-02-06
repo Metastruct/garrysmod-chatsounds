532a1aac38733249d2253fa6b86d33ab4951524b
TrenchFroast <thesteva256@gmail.com>
2016-02-06 01:44:58 +0000

    woow missing ashens sounds

--- lua/chatsounds/lists_nosend/ashens.lua
+++ lua/chatsounds/lists_nosend/ashens.lua
@@ -1,7 +1,10 @@
 c.StartList("ashens")
+L["ashenshello"]={{path="chatsounds/autoadd/ashens/ashenshello.ogg",length=0.84739229024943308}}
+L["ashensjingle"]={{path="chatsounds/autoadd/ashens/ashensjingle.ogg",length=2.116780045351474}}
 L["c c christmas baboon hippo monkey thats shitting out a blue pipe cleaner"]={{path="chatsounds/autoadd/ashens/c c christmas baboon hippo monkey thats shitting out a blue pipe cleaner.ogg",length=6.4216099773242634}}
 L["i am an angry god"]={{path="chatsounds/autoadd/ashens/i am an angry god.ogg",length=2.1928344671201816}}
 L["merry fucking christmas"]={{path="chatsounds/autoadd/ashens/merry fucking christmas.ogg",length=2.5574603174603174}}
 L["no bubbles for you"]={{path="chatsounds/autoadd/ashens/no bubbles for you.ogg",length=1.8977551020408163}}
 L["welcome"]={{path="chatsounds/autoadd/ashens/welcome.ogg",length=0.6469160997732426}}
+L["what a load of old shit"]={{path="chatsounds/autoadd/ashens/what a load of old shit.ogg",length=1.5047165532879818}}
 c.EndList()
