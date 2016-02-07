89650f95a98841484e798d16bf6c2a751e5e2cc0
Mikeai <mgamkrel@gmail.com>
2016-02-07 19:02:36 +0200

    Memes, Regret and Dogsong

---
+++ lua/chatsounds/lists_nosend/mememachine.lua
@@ -0,0 +1,22 @@
+c.StartList("mememachine")
+L["a fresh new pair of memes"]={{path="chatsounds/autoadd/mememachine/a fresh new pair of memes.ogg",length=3.0448299319727892}}
+L["bad memes"]={{path="chatsounds/autoadd/mememachine/bad memes.ogg",length=1.2352607709750567}}
+L["give me the memes"]={{path="chatsounds/autoadd/mememachine/give me the memes.ogg",length=2.0587755102040814}}
+L["i breathe the memes"]={{path="chatsounds/autoadd/mememachine/i breathe the memes.ogg",length=2.0262585034013605}}
+L["i catch the memes i spread the memes"]={{path="chatsounds/autoadd/mememachine/i catch the memes i spread the memes.ogg",length=3.0773242630385487}}
+L["i eat the memes i shit the memes"]={{path="chatsounds/autoadd/mememachine/i eat the memes i shit the memes.ogg",length=3.1531746031746031}}
+L["i need all the memes the memes to survive"]={{path="chatsounds/autoadd/mememachine/i need all the memes the memes to survive/i need all the memes the memes to survive.ogg",length=3.1639909297052156},{path="chatsounds/autoadd/mememachine/i need all the memes the memes to survive/i need all the memes the memes to survive2.ogg",length=3.1206802721088436}}
+L["i need the memes"]={{path="chatsounds/autoadd/mememachine/i need the memes/i need the memes1.ogg",length=2.0262585034013605},{path="chatsounds/autoadd/mememachine/i need the memes/i need the memes2.ogg",length=1.993764172335601}}
+L["i smell the memes"]={{path="chatsounds/autoadd/mememachine/i smell the memes/i smell the memes1.ogg",length=2.0370975056689344},{path="chatsounds/autoadd/mememachine/i smell the memes/i smell the memes2.ogg",length=2.1887981859410433}}
+L["if you borrowed my memes then you owe me"]={{path="chatsounds/autoadd/mememachine/if you borrowed my memes then you owe me.ogg",length=3.0556462585034012}}
+L["im a motherfucking meme machine"]={{path="chatsounds/autoadd/mememachine/im a motherfucking meme machine.ogg",length=2.7522675736961451}}
+L["inhale the memes exhale the memes"]={{path="chatsounds/autoadd/mememachine/inhale the memes exhale the memes.ogg",length=2.8714512471655329}}
+L["inject the memes into my blood stream"]={{path="chatsounds/autoadd/mememachine/inject the memes into my blood stream.ogg",length=2.8606122448979594}}
+L["meme machine"]={{path="chatsounds/autoadd/mememachine/meme machine/meme machine1.ogg",length=3.0339909297052152},{path="chatsounds/autoadd/mememachine/meme machine/meme machine2.ogg",length=3.1369160997732428}}
+L["mememachine"]={{path="chatsounds/autoadd/mememachine/mememachine.ogg",length=130.72140589569162}}
+L["smoke memes snort the memes"]={{path="chatsounds/autoadd/mememachine/smoke memes snort the memes.ogg",length=3.196530612244898}}
+L["there are good memes and there are bad memes"]={{path="chatsounds/autoadd/mememachine/there are good memes and there are bad memes.ogg",length=3.2831972789115644}}
+L["they call me the meme machine"]={{path="chatsounds/autoadd/mememachine/they call me the meme machine.ogg",length=2.1237868480725623}}
+L["why has god abandoned us"]={{path="chatsounds/autoadd/mememachine/why has god abandoned us.ogg",length=2.7522675736961451}}
+L["without memes i will die"]={{path="chatsounds/autoadd/mememachine/without memes i will die.ogg",length=2.5138775510204083}}
+c.EndList()
--- lua/chatsounds/lists_nosend/undertale.lua
+++ lua/chatsounds/lists_nosend/undertale.lua
@@ -1,3 +1,4 @@
 c.StartList("undertale")
+L["dogsong"]={{path="chatsounds/autoadd/undertale/dogsong.ogg",length=72.517891156462582}}
 L["tts laugh"]={{path="chatsounds/autoadd/undertale/tts laugh.ogg",length=2.6956235827664399}}
 c.EndList()
