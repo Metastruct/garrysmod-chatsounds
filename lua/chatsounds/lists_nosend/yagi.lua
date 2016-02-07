cb9c168ed757b25fdf54e5410cd3be9d35f91588
Yagira <yagiraa@gmail.com>
2016-02-07 03:34:32 +0100

    no idea what im doing

---
+++ lua/chatsounds/lists_nosend/yagi.lua
@@ -0,0 +1,6 @@
+c.StartList("yagi")
+L["hey buddy what happened to you"]={{path="chatsounds/autoadd/yagi/hey buddy what happened to you.ogg",length=2.2523356009070294}}
+L["oh great traps"]={{path="chatsounds/autoadd/yagi/oh great traps.ogg",length=1.4164172335600906}}
+L["the fuck is wrong with these people"]={{path="chatsounds/autoadd/yagi/the fuck is wrong with these people.ogg",length=2.1130158730158732}}
+L["this song"]={{path="chatsounds/autoadd/yagi/this song.ogg",length=1.195827664399093}}
+c.EndList()
