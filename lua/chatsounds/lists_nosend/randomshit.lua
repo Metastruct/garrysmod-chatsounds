555297a25c47298bfe8c68fe3709555f9b8c457c
Mike <mgamkrel@gmail.com>
2016-02-29 19:37:41 +0200

    Random Shit, why not

---
+++ lua/chatsounds/lists_nosend/randomshit.lua
@@ -0,0 +1,22 @@
+c.StartList("randomshit")
+L["10 10 10 10 100 100 best game best game"]={{path="chatsounds/autoadd/randomshit/10 10 10 10 100 100 best game best game.ogg",length=3.1704308390022677}}
+L["bazinga"]={{path="chatsounds/autoadd/randomshit/bazinga/bazinga1.ogg",length=0.8990476190476191},{path="chatsounds/autoadd/randomshit/bazinga/bazinga2.ogg",length=0.78802721088435379},{path="chatsounds/autoadd/randomshit/bazinga/bazinga3.ogg",length=1.0876417233560092},{path="chatsounds/autoadd/randomshit/bazinga/bazinga4.ogg",length=1.0414285714285714},{path="chatsounds/autoadd/randomshit/bazinga/bazinga5.ogg",length=0.90002267573696149},{path="chatsounds/autoadd/randomshit/bazinga/bazinga6.ogg",length=0.60907029478458052},{path="chatsounds/autoadd/randomshit/bazinga/bazinga7.ogg",length=0.87555555555555553},{path="chatsounds/autoadd/randomshit/bazinga/bazinga8.ogg",length=0.83206349206349206},{path="chatsounds/autoadd/randomshit/bazinga/bazinga9.ogg",length=0.92448979591836733}}
+L["can i get a kappa"]={{path="chatsounds/autoadd/randomshit/can i get a kappa.ogg",length=1.5209070294784581}}
+L["dont listen to the old fucker"]={{path="chatsounds/autoadd/randomshit/dont listen to the old fucker.ogg",length=1.74281179138322}}
+L["dumblecunts gonna say a few words to you now so you all better pay some attention now ye"]={{path="chatsounds/autoadd/randomshit/dumblecunts gonna say a few words to you now so you all better pay some attention now ye.ogg",length=6.5958956916099769}}
+L["fuck you mom kappa"]={{path="chatsounds/autoadd/randomshit/fuck you mom kappa.ogg",length=1.8741269841269841}}
+L["hello from the other siiiiiiide"]={{path="chatsounds/autoadd/randomshit/hello from the other siiiiiiide.ogg",length=4.8404308390022672}}
+L["i cant play in these conditions"]={{path="chatsounds/autoadd/randomshit/i cant play in these conditions.ogg",length=1.7251927437641723}}
+L["is it because i turn into a cat youre a fucking cat"]={{path="chatsounds/autoadd/randomshit/is it because i turn into a cat youre a fucking cat.ogg",length=3.4963492063492065}}
+L["kappa"]={{path="chatsounds/autoadd/randomshit/kappa/kappa1.ogg",length=0.88122448979591839},{path="chatsounds/autoadd/randomshit/kappa/kappa2.ogg",length=0.7695238095238095}}
+L["man i fuckin shit on your nan"]={{path="chatsounds/autoadd/randomshit/man i fuckin shit on your nan.ogg",length=2.544331065759637}}
+L["mom youre an idiot kappa"]={{path="chatsounds/autoadd/randomshit/mom youre an idiot kappa.ogg",length=2.2340589569160998}}
+L["shut it you stupid woman"]={{path="chatsounds/autoadd/randomshit/shut it you stupid woman.ogg",length=2.1450113378684805}}
+L["they have twelve penises"]={{path="chatsounds/autoadd/randomshit/they have twelve penises.ogg",length=2.7885034013605443}}
+L["this is ban roulette i love it"]={{path="chatsounds/autoadd/randomshit/this is ban roulette i love it.ogg",length=1.9858276643990929}}
+L["very evil indeed"]={{path="chatsounds/autoadd/randomshit/very evil indeed.ogg",length=2.1826757369614511}}
+L["why are you not in a cage you fuckin animal"]={{path="chatsounds/autoadd/randomshit/why are you not in a cage you fuckin animal.ogg",length=2.7885034013605443}}
+L["woody the woodpecker"]={{path="chatsounds/autoadd/randomshit/woody the woodpecker.ogg",length=3.5744897959183675}}
+L["yeah i know im a cat you fucken bastard"]={{path="chatsounds/autoadd/randomshit/yeah i know im a cat you fucken bastard.ogg",length=2.9011111111111112}}
+L["youre a gay old bastard man"]={{path="chatsounds/autoadd/randomshit/youre a gay old bastard man.ogg",length=2.3809750566893424}}
+c.EndList()
