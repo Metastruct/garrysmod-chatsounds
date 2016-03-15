c169f4e1bb387e39c9e02542f7f7981f34919aaa
chris (KaosHeaven) <redmagus8@gmail.com>
2016-03-14 22:08:50 -0500

    Dr. House chatsounds

--- lua/chatsounds/lists_nosend/kaosheaven.lua
+++ lua/chatsounds/lists_nosend/kaosheaven.lua
@@ -1,6 +1,13 @@
 c.StartList("kaosheaven")
+L["are you insane"]={{path="chatsounds/autoadd/kaosheaven/are you insane.ogg",length=3}}
+L["bitchin"]={{path="chatsounds/autoadd/kaosheaven/bitchin.ogg",length=2.332018140589569}}
+L["blasphemer"]={{path="chatsounds/autoadd/kaosheaven/blasphemer.ogg",length=0.80000000000000004}}
+L["could an eight year old do this"]={{path="chatsounds/autoadd/kaosheaven/could an eight year old do this.ogg",length=3}}
 L["did you know that"]={{path="chatsounds/autoadd/kaosheaven/did you know that.ogg",length=1.5250566893424036}}
+L["dude im cripple"]={{path="chatsounds/autoadd/kaosheaven/dude im cripple.ogg",length=3.9900000000000002}}
+L["dude she is in a coma who are you trying to impress"]={{path="chatsounds/autoadd/kaosheaven/dude she is in a coma who are you trying to impress.ogg",length=3.4849886621315194}}
 L["good god run away he is gonna murder us all with that glass"]={{path="chatsounds/autoadd/kaosheaven/good god run away he is gonna murder us all with that glass.ogg",length=4.702040816326531}}
+L["hahahahahahah"]={{path="chatsounds/autoadd/kaosheaven/hahahahahahah.ogg",length=2}}
 L["has anyone heard of a website called meatspin"]={{path="chatsounds/autoadd/kaosheaven/has anyone heard of a website called meatspin.ogg",length=5.7324263038548757}}
 L["hello from the other side"]={{path="chatsounds/autoadd/kaosheaven/hello from the other side.ogg",length=7}}
 L["hello from the outside"]={{path="chatsounds/autoadd/kaosheaven/hello from the outside.ogg",length=5.5}}
@@ -13,18 +20,22 @@ L["i want two cheeseburgers"]={{path="chatsounds/autoadd/kaosheaven/i want two c
 L["just standing there"]={{path="chatsounds/autoadd/kaosheaven/just standing there.ogg",length=1.9040362811791383}}
 L["leme tell you about a porcupines balls"]={{path="chatsounds/autoadd/kaosheaven/leme tell you about a porcupines balls.ogg",length=7.4672108843537419}}
 L["mkay"]={{path="chatsounds/autoadd/kaosheaven/mkay.ogg",length=0.49716553287981857}}
+L["muhahahaha"]={{path="chatsounds/autoadd/kaosheaven/muhahahaha.ogg",length=1.6749886621315193}}
 L["nokia"]={{path="chatsounds/autoadd/kaosheaven/nokia.ogg",length=5.0619501133786846}}
 L["nyeh"]={{path="chatsounds/autoadd/kaosheaven/nyeh.ogg",length=0.47682539682539682}}
 L["oh my god"]={{path="chatsounds/autoadd/kaosheaven/oh my god.ogg",length=1.951111111111111}}
 L["oh my god hes got a glass"]={{path="chatsounds/autoadd/kaosheaven/oh my god hes got a glass.ogg",length=3.8893424036281181}}
 L["ohoh"]={{path="chatsounds/autoadd/kaosheaven/ohoh.ogg",length=1.1681179138321995}}
+L["ooo helps me concentrate pop"]={{path="chatsounds/autoadd/kaosheaven/ooo helps me concentrate pop.ogg",length=5.5}}
 L["pissing out the window and shitting out the window are two different things"]={{path="chatsounds/autoadd/kaosheaven/pissing out the window and shitting out the window are two different things.ogg",length=15.307755102040817}}
 L["some shit to drink"]={{path="chatsounds/autoadd/kaosheaven/some shit to drink.ogg",length=5.600408163265306}}
 L["steve-o"]={{path="chatsounds/autoadd/kaosheaven/steve-o.ogg",length=1.2402494331065759}}
 L["the more you know"]={{path="chatsounds/autoadd/kaosheaven/the more you know.ogg",length=6.2693877551020405}}
 L["the part where he kills you"]={{path="chatsounds/autoadd/kaosheaven/the part where he kills you.ogg",length=38}}
 L["tom delaways used to be a chef on tv"]={{path="chatsounds/autoadd/kaosheaven/tom delaways used to be a chef on tv.ogg",length=8.4005895691609975}}
+L["totally dude"]={{path="chatsounds/autoadd/kaosheaven/totally dude.ogg",length=1.3999999999999999}}
 L["trials"]={{path="chatsounds/autoadd/kaosheaven/trials.ogg",length=79.226485260770971}}
 L["uh huh uh huh are you shitting me"]={{path="chatsounds/autoadd/kaosheaven/uh huh uh huh are you shitting me.ogg",length=13.067596371882086}}
 L["you must wear condom when browsing internet"]={{path="chatsounds/autoadd/kaosheaven/you must wear condom when browsing internet.ogg",length=4.7600907029478456}}
+L["your insane im an insane genius"]={{path="chatsounds/autoadd/kaosheaven/your insane im an insane genius.ogg",length=2.6000000000000001}}
 c.EndList()
