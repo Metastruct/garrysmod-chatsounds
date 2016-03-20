a7d7967d69919d652b3252064b34b08c27266b71
techbot <techbot121@gmail.com>
2016-03-20 13:19:17 +0100

    oooo and ran preprocessor

--- lua/chatsounds/lists_nosend/toastyengineer.lua
+++ lua/chatsounds/lists_nosend/toastyengineer.lua
@@ -1,4 +1,2 @@
-c.StartList("toastyengineer")
-L["banned for life"]={{path="chatsounds/autoadd/toastyengineer/banned for life.ogg",length=4.6820408163265306}}
-L["get out of here stalker"]={{path="chatsounds/autoadd/toastyengineer/get out of here stalker.ogg",length=1.4559183673469387}}
+c.StartList("ToastyEngineer")
 c.EndList()
--- lua/chatsounds/lists_nosend/zelda_mm.lua
+++ lua/chatsounds/lists_nosend/zelda_mm.lua
@@ -1,27 +1,2 @@
-c.StartList("zelda_mm")
-L["ah"]={{path="chatsounds/autoadd/zelda_mm/ah.ogg",length=0.5144897959183673}}
-L["auh"]={{path="chatsounds/autoadd/zelda_mm/auh.ogg",length=0.38630385487528346}}
-L["aww"]={{path="chatsounds/autoadd/zelda_mm/aww.ogg",length=1.2635600907029478}}
-L["baby"]={{path="chatsounds/autoadd/zelda_mm/baby.ogg",length=1.192562358276644}}
-L["eeneyahnnyun"]={{path="chatsounds/autoadd/zelda_mm/eeneyahnnyun.ogg",length=2.2468934240362812}}
-L["eheheh"]={{path="chatsounds/autoadd/zelda_mm/eheheh.ogg",length=1.4823582766439909}}
-L["ehhh"]={{path="chatsounds/autoadd/zelda_mm/ehhh.ogg",length=1.262312925170068}}
-L["embarrassed"]={{path="chatsounds/autoadd/zelda_mm/embarrassed.ogg",length=1.6836961451247165}}
-L["giggle"]={{path="chatsounds/autoadd/zelda_mm/giggle.ogg",length=1.1093877551020408}}
-L["groan"]={{path="chatsounds/autoadd/zelda_mm/groan.ogg",length=1.7106802721088434}}
-L["haa phm"]={{path="chatsounds/autoadd/zelda_mm/haa phm.ogg",length=1.7912471655328799}}
-L["heh"]={{path="chatsounds/autoadd/zelda_mm/heh.ogg",length=0.7626757369614513}}
-L["hehheh"]={{path="chatsounds/autoadd/zelda_mm/hehheh.ogg",length=1.231156462585034}}
-L["hoi"]={{path="chatsounds/autoadd/zelda_mm/hoi.ogg",length=1.1096145124716554}}
-L["huh"]={{path="chatsounds/autoadd/zelda_mm/huh.ogg",length=0.32800453514739231}}
-L["impressed"]={{path="chatsounds/autoadd/zelda_mm/impressed.ogg",length=1.6469387755102041}}
-L["mmhmm"]={{path="chatsounds/autoadd/zelda_mm/mmhmm.ogg",length=1.1905668934240363}}
-L["mmm"]={{path="chatsounds/autoadd/zelda_mm/mmm.ogg",length=1.2066213151927438}}
-L["nanananana"]={{path="chatsounds/autoadd/zelda_mm/nanananana.ogg",length=1.5199319727891156}}
-L["oop"]={{path="chatsounds/autoadd/zelda_mm/oop.ogg",length=0.27519274376417235}}
-L["oww"]={{path="chatsounds/autoadd/zelda_mm/oww.ogg",length=1.1365079365079365}}
-L["tingle fall"]={{path="chatsounds/autoadd/zelda_mm/tingle fall.ogg",length=2.9348299319727893}}
-L["tingle magic"]={{path="chatsounds/autoadd/zelda_mm/tingle magic.ogg",length=3.0265079365079366}}
-L["yah"]={{path="chatsounds/autoadd/zelda_mm/yah.ogg",length=0.51473922902494329}}
-L["yay"]={{path="chatsounds/autoadd/zelda_mm/yay.ogg",length=1.3282539682539682}}
+c.StartList("Zelda_MM")
 c.EndList()
