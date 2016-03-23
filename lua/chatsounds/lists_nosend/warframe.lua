d9466921f374e4cdb86a82eb47f7e1de73f257e4
Mikeai <mgamkrel@gmail.com>
2016-03-23 21:45:37 +0200

    TENNO SKOOM
    im so sorry

---
+++ lua/chatsounds/lists_nosend/warframe.lua
@@ -0,0 +1,4 @@
+c.StartList("warframe")
+L["tenno skoom"]={{path="chatsounds/autoadd/warframe/tenno skoom.ogg",length=1.2901814058956915}}
+L["tenno skooom"]={{path="chatsounds/autoadd/warframe/tenno skooom.ogg",length=2.3068934240362813}}
+c.EndList()
