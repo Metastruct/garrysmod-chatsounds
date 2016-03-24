2d5d675f73ab0829bdd70fd175d1793c487dfb40
Mikeai <mgamkrel@gmail.com>
2016-03-24 14:36:11 +0200

    Added some more trash
    Because who didn't want more Austin Powers shatsounds

---
+++ lua/chatsounds/lists_nosend/austinpowers.lua
@@ -0,0 +1,74 @@
+c.StartList("austinpowers")
+L["a trillion is more than a billion numbnuts"]={{path="chatsounds/autoadd/austinpowers/a trillion is more than a billion numbnuts.ogg",length=2.3295238095238093}}
+L["alright zip it"]={{path="chatsounds/autoadd/austinpowers/alright zip it.ogg",length=0.79099773242630389}}
+L["and that is to have sharks with frickin laser beams attached to their heads"]={{path="chatsounds/autoadd/austinpowers/and that is to have sharks with frickin laser beams attached to their heads.ogg",length=4.9546031746031742}}
+L["at the age of fourteen a rostrian named vilma"]={{path="chatsounds/autoadd/austinpowers/at the age of fourteen a rostrian named vilma.ogg",length=2.7989115646258504}}
+L["at the age of twelve i recieved my first scribe"]={{path="chatsounds/autoadd/austinpowers/at the age of twelve i recieved my first scribe.ogg",length=2.4164399092970523}}
+L["austin powers1"]={{path="chatsounds/autoadd/austinpowers/austin powers1.ogg",length=1.5124489795918368}}
+L["because youre quite not evil enough"]={{path="chatsounds/autoadd/austinpowers/because youre quite not evil enough.ogg",length=2.1382993197278912}}
+L["but i do know this"]={{path="chatsounds/autoadd/austinpowers/but i do know this.ogg",length=1.9644444444444444}}
+L["din din"]={{path="chatsounds/autoadd/austinpowers/din din.ogg",length=7.988185941043084}}
+L["doctor evil i didnt spend six years of evil medical school to be called mister thank you very much"]={{path="chatsounds/autoadd/austinpowers/doctor evil i didnt spend six years of evil medical school to be called mister thank you very much.ogg",length=6.0672108843537416}}
+L["dont mess with me im one crazy mofo"]={{path="chatsounds/autoadd/austinpowers/dont mess with me im one crazy mofo.ogg",length=2.2252154195011338}}
+L["dramaticgrouplaugh"]={{path="chatsounds/autoadd/austinpowers/dramaticgrouplaugh.ogg",length=34.734285714285711}}
+L["either way its bad"]={{path="chatsounds/autoadd/austinpowers/either way its bad.ogg",length=2.1035147392290248}}
+L["even after they paid me the money im still going to melt every city on the planet with liquid hot magma"]={{path="chatsounds/autoadd/austinpowers/even after they paid me the money im still going to melt every city on the planet with liquid hot magma.ogg",length=5.841201814058957}}
+L["even before you start that was a preemptive shh"]={{path="chatsounds/autoadd/austinpowers/even before you start that was a preemptive shh.ogg",length=3.6507482993197278}}
+L["fat bastard could i have my mojo please"]={{path="chatsounds/autoadd/austinpowers/fat bastard could i have my mojo please.ogg",length=3.2507936507936508}}
+L["fire the laser"]={{path="chatsounds/autoadd/austinpowers/fire the laser.ogg",length=1.4142403628117914}}
+L["he would make outrageous claims like he invented the question mark"]={{path="chatsounds/autoadd/austinpowers/he would make outrageous claims like he invented the question mark.ogg",length=4.2418367346938775}}
+L["hes the snake to my mongoose or the mongoose to my snake"]={{path="chatsounds/autoadd/austinpowers/hes the snake to my mongoose or the mongoose to my snake.ogg",length=5.4935147392290249}}
+L["however isnt it ironic that the very things that you stand for"]={{path="chatsounds/autoadd/austinpowers/however isnt it ironic that the very things that you stand for.ogg",length=5.2012698412698413}}
+L["i already had someone created in my image hes evil he wants to take over the world and he fits easily into most overhead storage bins"]={{path="chatsounds/autoadd/austinpowers/i already had someone created in my image hes evil he wants to take over the world and he fits easily into most overhead storage bins.ogg",length=8.3793424036281188}}
+L["i dont know animals"]={{path="chatsounds/autoadd/austinpowers/i dont know animals.ogg",length=1.6341496598639456}}
+L["i have one simple request"]={{path="chatsounds/autoadd/austinpowers/i have one simple request.ogg",length=2.4859863945578233}}
+L["im going to place him in an easily escapable situation involving an overly elaborate and exotic death"]={{path="chatsounds/autoadd/austinpowers/im going to place him in an easily escapable situation involving an overly elaborate and exotic death.ogg",length=5.841201814058957}}
+L["in the spring wed make meat helmets"]={{path="chatsounds/autoadd/austinpowers/in the spring wed make meat helmets.ogg",length=2.7641496598639455}}
+L["its okay its alright come on"]={{path="chatsounds/autoadd/austinpowers/its okay its alright come on.ogg",length=2.7467573696145124}}
+L["just know i have a whole bag of shh with your name on it"]={{path="chatsounds/autoadd/austinpowers/just know i have a whole bag of shh with your name on it.ogg",length=3.4247619047619047}}
+L["just one calorie not evil enough"]={{path="chatsounds/autoadd/austinpowers/just one calorie not evil enough.ogg",length=2.3642857142857143}}
+L["knock knock whos there shh"]={{path="chatsounds/autoadd/austinpowers/knock knock whos there shh.ogg",length=2.0339909297052152}}
+L["know this austin i am your father"]={{path="chatsounds/autoadd/austinpowers/know this austin i am your father.ogg",length=14.535691609977324}}
+L["ladies and gentlemen the jury exzippit a"]={{path="chatsounds/autoadd/austinpowers/ladies and gentlemen the jury exzippit a.ogg",length=3.1205215419501133}}
+L["let me tell you a little story about a man named shh"]={{path="chatsounds/autoadd/austinpowers/let me tell you a little story about a man named shh.ogg",length=2.0861451247165532}}
+L["meat helmets"]={{path="chatsounds/autoadd/austinpowers/meat helmets.ogg",length=1.0448979591836736}}
+L["my childhood was typical"]={{path="chatsounds/autoadd/austinpowers/my childhood was typical.ogg",length=2.2599999999999998}}
+L["my father was a relentlessly self improving blongerie owner with low grade narcolepsy and a penchant for buggery"]={{path="chatsounds/autoadd/austinpowers/my father was a relentlessly self improving blongerie owner with low grade narcolepsy and a penchant for buggery.ogg",length=7.7013378684807252}}
+L["my father would womanize he would drink"]={{path="chatsounds/autoadd/austinpowers/my father would womanize he would drink.ogg",length=2.920612244897959}}
+L["my mother was a 15 year old french prostitute named chloe with webbed feet"]={{path="chatsounds/autoadd/austinpowers/my mother was a 15 year old french prostitute named chloe with webbed feet.ogg",length=5.0241269841269842}}
+L["no not really i cant back that up"]={{path="chatsounds/autoadd/austinpowers/no not really i cant back that up.ogg",length=3.5294331065759637}}
+L["now can you remind me what i pay you people for honestly throw me a bone here"]={{path="chatsounds/autoadd/austinpowers/now can you remind me what i pay you people for honestly throw me a bone here.ogg",length=5.2675056689342403}}
+L["now evidently my cycloptic colleague informs me that that cant be done"]={{path="chatsounds/autoadd/austinpowers/now evidently my cycloptic colleague informs me that that cant be done.ogg",length=4.8850566893424032}}
+L["now mister evil"]={{path="chatsounds/autoadd/austinpowers/now mister evil.ogg",length=2.8163038548752835}}
+L["oh hell lets just do what we always do hijack some nuclear weapons and hold the world hostage"]={{path="chatsounds/autoadd/austinpowers/oh hell lets just do what we always do hijack some nuclear weapons and hold the world hostage.ogg",length=4.8502947845804991}}
+L["ow you shot me you a hole"]={{path="chatsounds/autoadd/austinpowers/ow you shot me you a hole.ogg",length=4.4582312925170067}}
+L["really"]={{path="chatsounds/autoadd/austinpowers/really.ogg",length=1.2538775510204081}}
+L["right idiot yes"]={{path="chatsounds/autoadd/austinpowers/right idiot yes.ogg",length=2.4380952380952383}}
+L["rright"]={{path="chatsounds/autoadd/austinpowers/rright.ogg",length=2.3452154195011339}}
+L["scott you had your chance okay"]={{path="chatsounds/autoadd/austinpowers/scott you had your chance okay.ogg",length=4.363514739229025}}
+L["see you in hell powers"]={{path="chatsounds/autoadd/austinpowers/see you in hell powers.ogg",length=4.1970068027210887}}
+L["shaved my testicles"]={{path="chatsounds/autoadd/austinpowers/shaved my testicles.ogg",length=3.5116780045351472}}
+L["shitt"]={{path="chatsounds/autoadd/austinpowers/shitt.ogg",length=0.93875283446712021}}
+L["sometimes he would accuse chesnuts of being lazy"]={{path="chatsounds/autoadd/austinpowers/sometimes he would accuse chesnuts of being lazy.ogg",length=3.9289115646258503}}
+L["summers in rangoon luge lessons"]={{path="chatsounds/autoadd/austinpowers/summers in rangoon luge lessons.ogg",length=3.8767573696145123}}
+L["the sort of general malaise that only the genius possess and the insane lament"]={{path="chatsounds/autoadd/austinpowers/the sort of general malaise that only the genius possess and the insane lament.ogg",length=6.7278231292517008}}
+L["there really is nothing like a shorn scrotum its breathtaking i suggest you try it"]={{path="chatsounds/autoadd/austinpowers/there really is nothing like a shorn scrotum its breathtaking i suggest you try it.ogg",length=5.8585941043083904}}
+L["this time its personal"]={{path="chatsounds/autoadd/austinpowers/this time its personal.ogg",length=2.0433560090702949}}
+L["this time its personal kill him"]={{path="chatsounds/autoadd/austinpowers/this time its personal kill him.ogg",length=5.875963718820862}}
+L["very well where do i begin"]={{path="chatsounds/autoadd/austinpowers/very well where do i begin.ogg",length=2.4859863945578233}}
+L["well actually that was just footage from the movie independence day but the real laser would be alot like that"]={{path="chatsounds/autoadd/austinpowers/well actually that was just footage from the movie independence day but the real laser would be alot like that.ogg",length=5.771655328798186}}
+L["well done mr powers"]={{path="chatsounds/autoadd/austinpowers/well done mr powers.ogg",length=2.6586848072562357}}
+L["well its true"]={{path="chatsounds/autoadd/austinpowers/well its true.ogg",length=3.5985941043083902}}
+L["were not so different you and i"]={{path="chatsounds/autoadd/austinpowers/were not so different you and i.ogg",length=2.8212244897959184}}
+L["when a problem comes along you must zip it"]={{path="chatsounds/autoadd/austinpowers/when a problem comes along you must zip it.ogg",length=4.8329024943310657}}
+L["when i was insolent i was placed in a bag and beaten with reeds pretty standard really"]={{path="chatsounds/autoadd/austinpowers/when i was insolent i was placed in a bag and beaten with reeds pretty standard really.ogg",length=6.5539682539682538}}
+L["why make trillions when we could make billions"]={{path="chatsounds/autoadd/austinpowers/why make trillions when we could make billions.ogg",length=4.8937414965986399}}
+L["whyd you run out on me"]={{path="chatsounds/autoadd/austinpowers/whyd you run out on me.ogg",length=1.3386167800453515}}
+L["you know scott ive been a frickin evil doctor for thirty frickin years o k cut me some frickin slack"]={{path="chatsounds/autoadd/austinpowers/you know scott ive been a frickin evil doctor for thirty frickin years o k cut me some frickin slack.ogg",length=10.795804988662132}}
+L["youre like a child"]={{path="chatsounds/autoadd/austinpowers/youre like a child.ogg",length=1.7123809523809523}}
+L["youre quasi evil youre semi evil youre the margarine of evil"]={{path="chatsounds/autoadd/austinpowers/youre quasi evil youre semi evil youre the margarine of evil.ogg",length=5.4065986394557823}}
+L["youre the diet coke of evil"]={{path="chatsounds/autoadd/austinpowers/youre the diet coke of evil.ogg",length=2.2078458049886622}}
+L["zip it"]={{path="chatsounds/autoadd/austinpowers/zip it.ogg",length=0.39984126984126983}}
+L["zipit"]={{path="chatsounds/autoadd/austinpowers/zipit.ogg",length=0.51283446712018144}}
+L["zzip"]={{path="chatsounds/autoadd/austinpowers/zzip.ogg",length=0.66929705215419499}}
+c.EndList()
