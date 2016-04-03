df7045abbf3b87e1e6ae2a2c2f46916d57287139
mitterdoo <connorashcroft@live.com>
2016-04-03 08:21:36 -0700

    warm it up

--- lua/chatsounds/lists_nosend/h3h3.lua
+++ lua/chatsounds/lists_nosend/h3h3.lua
@@ -1,4 +1,6 @@
 c.StartList("h3h3")
+L["aerate it"]={{path="chatsounds/autoadd/h3h3/aerate it.ogg",length=0.53544217687074835}}
+L["aerating"]={{path="chatsounds/autoadd/h3h3/aerating/aerating1.ogg",length=5.1398639455782309},{path="chatsounds/autoadd/h3h3/aerating/aerating2.ogg",length=1.3509523809523809},{path="chatsounds/autoadd/h3h3/aerating/aerating3.ogg",length=0.61782312925170069},{path="chatsounds/autoadd/h3h3/aerating/aerating4.ogg",length=0.58487528344671202},{path="chatsounds/autoadd/h3h3/aerating/aerating5.ogg",length=0.88965986394557828},{path="chatsounds/autoadd/h3h3/aerating/aerating6.ogg",length=0.92263038548752829}}
 L["all they care about is bracelets dude"]={{path="chatsounds/autoadd/h3h3/all they care about is bracelets dude.ogg",length=2.2960090702947844}}
 L["and thats how you use gamer gunk cleaner ginger to clean your gamer gunk"]={{path="chatsounds/autoadd/h3h3/and thats how you use gamer gunk cleaner ginger to clean your gamer gunk.ogg",length=4.5160090702947846}}
 L["are you afraid of enough stuff in your life well not nearly enough"]={{path="chatsounds/autoadd/h3h3/are you afraid of enough stuff in your life well not nearly enough.ogg",length=3.2856916099773241}}
@@ -6,6 +8,8 @@ L["are you horny"]={{path="chatsounds/autoadd/h3h3/are you horny.ogg",length=1.0
 L["as a gamer you have to be a little bit insulted by just how dumb they think we are"]={{path="chatsounds/autoadd/h3h3/as a gamer you have to be a little bit insulted by just how dumb they think we are.ogg",length=4.1399999999999997}}
 L["boy you could tell these people are pretty insecure about their product"]={{path="chatsounds/autoadd/h3h3/boy you could tell these people are pretty insecure about their product.ogg",length=3.2013378684807257}}
 L["comic sans warning"]={{path="chatsounds/autoadd/h3h3/comic sans warning.ogg",length=1.4653287981859411}}
+L["cover all 9000 taste buds"]={{path="chatsounds/autoadd/h3h3/cover all 9000 taste buds.ogg",length=1.8370068027210884}}
+L["driving up"]={{path="chatsounds/autoadd/h3h3/driving up.ogg",length=0.56015873015873019}}
 L["dude can you believe how much of a prankster i am"]={{path="chatsounds/autoadd/h3h3/dude can you believe how much of a prankster i am.ogg",length=3.0319954648526077}}
 L["ethan dude"]={{path="chatsounds/autoadd/h3h3/ethan dude/1.ogg",length=0.79131519274376416},{path="chatsounds/autoadd/h3h3/ethan dude/10.ogg",length=0.33034013605442175},{path="chatsounds/autoadd/h3h3/ethan dude/2.ogg",length=0.32970521541950115},{path="chatsounds/autoadd/h3h3/ethan dude/3.ogg",length=0.5165532879818594},{path="chatsounds/autoadd/h3h3/ethan dude/4.ogg",length=0.61546485260770978},{path="chatsounds/autoadd/h3h3/ethan dude/5.ogg",length=0.2637641723356009},{path="chatsounds/autoadd/h3h3/ethan dude/6.ogg",length=0.56052154195011339},{path="chatsounds/autoadd/h3h3/ethan dude/7.ogg",length=0.39564625850340135},{path="chatsounds/autoadd/h3h3/ethan dude/8.ogg",length=0.58249433106575965},{path="chatsounds/autoadd/h3h3/ethan dude/9.ogg",length=0.32970521541950115}}
 L["ethancough"]={{path="chatsounds/autoadd/h3h3/ethancough/ethan cough 1.ogg",length=2.7226757369614512},{path="chatsounds/autoadd/h3h3/ethancough/ethan cough 2.ogg",length=4.1493424036281183},{path="chatsounds/autoadd/h3h3/ethancough/ethan cough 3.ogg",length=2.4293424036281177}}
@@ -42,18 +46,23 @@ L["kissing prank expletives"]={{path="chatsounds/autoadd/h3h3/kissing prank expl
 L["kissing prank gone sexual"]={{path="chatsounds/autoadd/h3h3/kissing prank gone sexual.ogg",length=3.5413378684807255}}
 L["man these guys must really love horses they must be cowboys or something"]={{path="chatsounds/autoadd/h3h3/man these guys must really love horses they must be cowboys or something.ogg",length=4.3741950113378687}}
 L["maregasms"]={{path="chatsounds/autoadd/h3h3/maregasms.ogg",length=1.3298412698412698}}
+L["mmm thats a ten"]={{path="chatsounds/autoadd/h3h3/mmm thats a ten.ogg",length=1.6928344671201814}}
 L["mom would be so proud"]={{path="chatsounds/autoadd/h3h3/mom would be so proud.ogg",length=1.4839909297052154}}
 L["mommy and daddy disappear at night to go fuck their horses"]={{path="chatsounds/autoadd/h3h3/mommy and daddy disappear at night to go fuck their horses.ogg",length=3.6817913832199545}}
 L["oh your keyboard isnt full of chip dip and splooge oh well youre not a real gamer"]={{path="chatsounds/autoadd/h3h3/oh your keyboard isnt full of chip dip and splooge oh well youre not a real gamer.ogg",length=6.9346712018140586}}
 L["ooh sounds like quite a prank there buddy"]={{path="chatsounds/autoadd/h3h3/ooh sounds like quite a prank there buddy.ogg",length=2.8826757369614513}}
 L["parent in room"]={{path="chatsounds/autoadd/h3h3/parent in room.ogg",length=1.2817687074829931}}
 L["pir"]={{path="chatsounds/autoadd/h3h3/pir.ogg",length=0.93875283446712021}}
+L["pure vanilla"]={{path="chatsounds/autoadd/h3h3/pure vanilla.ogg",length=0.6301814058956916}}
 L["ruh"]={{path="chatsounds/autoadd/h3h3/ruh.ogg",length=1.065124716553288}}
 L["sdpp"]={{path="chatsounds/autoadd/h3h3/sdpp.ogg",length=1.3178684807256236}}
 L["send the dick pics please"]={{path="chatsounds/autoadd/h3h3/send the dick pics please.ogg",length=1.5706349206349206}}
 L["since when did being a prick become a prank"]={{path="chatsounds/autoadd/h3h3/since when did being a prick become a prank.ogg",length=2.8199999999999998}}
 L["sorry dude im sorry im just willing to do anything to get famous"]={{path="chatsounds/autoadd/h3h3/sorry dude im sorry im just willing to do anything to get famous.ogg",length=3.4519954648526077}}
+L["sweeteners"]={{path="chatsounds/autoadd/h3h3/sweeteners.ogg",length=0.58074829931972793}}
 L["thanks mr on the spectrum"]={{path="chatsounds/autoadd/h3h3/thanks mr on the spectrum.ogg",length=1.6173242630385487}}
+L["that cream"]={{path="chatsounds/autoadd/h3h3/that cream.ogg",length=0.64664399092970526}}
+L["thats a ten"]={{path="chatsounds/autoadd/h3h3/thats a ten.ogg",length=0.59310657596371885}}
 L["thats cool dawg i respect that"]={{path="chatsounds/autoadd/h3h3/thats cool dawg i respect that.ogg",length=1.6626757369614513}}
 L["this guy nails the gamer"]={{path="chatsounds/autoadd/h3h3/this guy nails the gamer.ogg",length=2.2466666666666666}}
 L["this guys not pranking anyone hes just being an asshole dude"]={{path="chatsounds/autoadd/h3h3/this guys not pranking anyone hes just being an asshole dude.ogg",length=2.5960090702947847}}
@@ -61,6 +70,7 @@ L["this is bobby from news six"]={{path="chatsounds/autoadd/h3h3/this is bobby f
 L["this is comedy this is where you come to laugh"]={{path="chatsounds/autoadd/h3h3/this is comedy this is where you come to laugh.ogg",length=3.0506575963718823}}
 L["this isnt for you stop being a sleuth"]={{path="chatsounds/autoadd/h3h3/this isnt for you stop being a sleuth.ogg",length=2.2024943310657594}}
 L["top 5 kissing pranks 2014 grabbing booooooooooobs"]={{path="chatsounds/autoadd/h3h3/top 5 kissing pranks 2014 grabbing booooooooooobs.ogg",length=4.1946712018140593}}
+L["warm it up"]={{path="chatsounds/autoadd/h3h3/warm it up.ogg",length=0.50043083900226759}}
 L["what a prank"]={{path="chatsounds/autoadd/h3h3/what a prank.ogg",length=0.98800453514739228}}
 L["what if his ponies were faking orgasms"]={{path="chatsounds/autoadd/h3h3/what if his ponies were faking orgasms.ogg",length=1.9782766439909296}}
 L["woah his asshole is a dimension to another plane of existence"]={{path="chatsounds/autoadd/h3h3/woah his asshole is a dimension to another plane of existence.ogg",length=4.9121088435374149}}
