f706c6d78da3a09e19e612767a8221e47cf922c2
TrenchFroast <thesteva256@gmail.com>
2016-04-09 14:24:22 +0100

    [request] russian kid screaming
    i died inside while doing this

---
+++ lua/chatsounds/lists_nosend/unpleasant.lua
@@ -0,0 +1,23 @@
+c.StartList("unpleasant")
+L["aaaaa aa blyad"]={{path="chatsounds/autoadd/unpleasant/aaaaa aa blyad.ogg",length=1.4512471655328798}}
+L["aeeee"]={{path="chatsounds/autoadd/unpleasant/aeeee.ogg",length=0.93170068027210884}}
+L["aiiiiiiaeea"]={{path="chatsounds/autoadd/unpleasant/aiiiiiiaeea.ogg",length=1.5553287981859409}}
+L["aiiiiiieaeih"]={{path="chatsounds/autoadd/unpleasant/aiiiiiieaeih.ogg",length=2.4932426303854873}}
+L["aiiiiiiiiiieah"]={{path="chatsounds/autoadd/unpleasant/aiiiiiiiiiieah.ogg",length=2.1159183673469388}}
+L["baa"]={{path="chatsounds/autoadd/unpleasant/baa.ogg",length=0.91428571428571426}}
+L["blyad"]={{path="chatsounds/autoadd/unpleasant/blyad.ogg",length=0.62693877551020405}}
+L["caaack"]={{path="chatsounds/autoadd/unpleasant/caaack.ogg",length=0.73142857142857143}}
+L["cyyka"]={{path="chatsounds/autoadd/unpleasant/cyyka.ogg",length=1.0565079365079366}}
+L["deathpainscreams"]={{path="chatsounds/autoadd/unpleasant/deathpainscreams.ogg",length=4.3176870748299319}}
+L["demonscream"]={{path="chatsounds/autoadd/unpleasant/demonscream.ogg",length=3.3607256235827663}}
+L["gaaaaaah"]={{path="chatsounds/autoadd/unpleasant/gaaaaaah.ogg",length=0.78077097505668935}}
+L["gaaaah"]={{path="chatsounds/autoadd/unpleasant/gaaaah.ogg",length=0.74013605442176866}}
+L["gaah"]={{path="chatsounds/autoadd/unpleasant/gaah.ogg",length=0.55727891156462583}}
+L["nahuy"]={{path="chatsounds/autoadd/unpleasant/nahuy.ogg",length=0.77786848072562353}}
+L["pasholnahuy"]={{path="chatsounds/autoadd/unpleasant/pasholnahuy.ogg",length=2.2465306122448978}}
+L["piiizdet"]={{path="chatsounds/autoadd/unpleasant/piiizdet.ogg",length=1.4396371882086167}}
+L["pizd"]={{path="chatsounds/autoadd/unpleasant/pizd.ogg",length=0.89396825396825397}}
+L["pizdet"]={{path="chatsounds/autoadd/unpleasant/pizdet/pizdet 1.ogg",length=0.71981859410430837},{path="chatsounds/autoadd/unpleasant/pizdet/pizdet 2.ogg",length=0.87655328798185939}}
+L["you have been ip bann aaaiiiiiiiiiiieeeaaa"]={{path="chatsounds/autoadd/unpleasant/you have been ip bann aaaiiiiiiiiiiieeeaaa.ogg",length=4.8892517006802718}}
+L["yourmomscreams"]={{path="chatsounds/autoadd/unpleasant/yourmomscreams.ogg",length=2.0448072562358277}}
+c.EndList()
