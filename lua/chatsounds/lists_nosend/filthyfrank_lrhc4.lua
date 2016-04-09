a7cb22ce882ad2296241a125859f318834e1daf3
TrenchFroast <thesteva256@gmail.com>
2016-04-09 01:42:41 +0100

    filthy frank - loser reads hater comments 4*
    (incomplete - will complete some other time if no one else does)

---
+++ lua/chatsounds/lists_nosend/filthyfrank_lrhc4.lua
@@ -0,0 +1,42 @@
+c.StartList("filthyfrank_lrhc4")
+L["alright lets relax man"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/alright lets relax man.ogg",length=1.4190022675736962}}
+L["are you gay"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/are you gay.ogg",length=0.59394557823129257}}
+L["are you gay or straightup nonhuman because you know no chicks will dig you"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/are you gay or straightup nonhuman because you know no chicks will dig you.ogg",length=3.5963038548752833}}
+L["call of duty ghost"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/call of duty ghost.ogg",length=1.5615419501133787}}
+L["clapslap"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/clapslap.ogg",length=0.33612244897959181}}
+L["come back here where the fuck you goin nigga"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/come back here where the fuck you goin nigga.ogg",length=1.9590249433106577}}
+L["fam"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/fam.ogg",length=0.50204081632653064}}
+L["fanfiction"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/fanfiction.ogg",length=0.86637188208616778}}
+L["fine"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/fine.ogg",length=0.54539682539682544}}
+L["frankpanting"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/frankpanting.ogg",length=4.6431746031746028}}
+L["fucking filthy ass nigga"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/fucking filthy ass nigga.ogg",length=1.4248979591836735}}
+L["gimme the tims"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/gimme the tims/gimme the tims 1.ogg",length=0.85643990929705216},{path="chatsounds/autoadd/filthyfrank_lrhc4/gimme the tims/gimme the tims 2.ogg",length=0.88335600907029477}}
+L["gimme the tims right now"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/gimme the tims right now.ogg",length=1.6560997732426304}}
+L["how is that not homo"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/how is that not homo.ogg",length=1.2758049886621314}}
+L["i believe i can fly"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/i believe i can fly.ogg",length=15.23374149659864}}
+L["i got you fam"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/i got you fam.ogg",length=1.2702947845804988}}
+L["i have fun with my dog and i love video games"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/i have fun with my dog and i love video games.ogg",length=3.2504535147392288}}
+L["id rather bake my vegetable state mother on my room with the sun blazing ass off"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/id rather bake my vegetable state mother on my room with the sun blazing ass off.ogg",length=5.5625623582766437}}
+L["im clearly busy right now"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/im clearly busy right now.ogg",length=1.3578004535147392}}
+L["kys"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/kys.ogg",length=0.69950113378684808}}
+L["nah i can smell you man"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/nah i can smell you man.ogg",length=1.2436054421768707}}
+L["not the tims"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/not the tims.ogg",length=1.2518820861678004}}
+L["now its not a game huh cuz papa got a brand new toy"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/now its not a game huh cuz papa got a brand new toy.ogg",length=2.4287755102040816}}
+L["piece of shit"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/piece of shit.ogg",length=0.96362811791383218}}
+L["shut the fuck up"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/shut the fuck up.ogg",length=0.9153514739229025}}
+L["shut the fuck up and get yo ass home man"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/shut the fuck up and get yo ass home man.ogg",length=2.5705215419501135}}
+L["shut the fuck up no one cares about your robot fanfiction"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/shut the fuck up no one cares about your robot fanfiction.ogg",length=3.2733560090702949}}
+L["stupid cunt with down syndrome"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/stupid cunt with down syndrome.ogg",length=1.9119501133786847}}
+L["then watch another piece of shit video of yours"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/then watch another piece of shit video of yours.ogg",length=3.564263038548753}}
+L["this show taint funny"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/this show taint funny.ogg",length=1.520498866213152}}
+L["video games"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/video games.ogg",length=0.93047619047619046}}
+L["whooooagah"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/whooooagah.ogg",length=0.9992743764172336}}
+L["wtf is this bullshit man"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/wtf is this bullshit man.ogg",length=2.2214058956916101}}
+L["yeeah"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/yeeah.ogg",length=1.1327210884353742}}
+L["yo frank"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/yo frank.ogg",length=0.82684807256235826}}
+L["you dont fuck with me"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/you dont fuck with me.ogg",length=1.2706349206349206}}
+L["you fucking jaah"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/you fucking jaah.ogg",length=1.3873922902494331}}
+L["you know papas gon come back you know papas always looking out for everybody"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/you know papas gon come back you know papas always looking out for everybody.ogg",length=3.0156689342403626}}
+L["you look like a rapist"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/you look like a rapist.ogg",length=1.3095918367346939}}
+L["youre a retard"]={{path="chatsounds/autoadd/filthyfrank_lrhc4/youre a retard.ogg",length=1.1318367346938776}}
+c.EndList()
