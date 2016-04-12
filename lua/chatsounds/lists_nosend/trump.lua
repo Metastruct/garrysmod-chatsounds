d55e171086d1d31426f2e59a03762c295c94e693
Yagira <yagiraa@gmail.com>
2016-04-12 12:30:16 +0200

    Don't even ask.

---
+++ lua/chatsounds/lists_nosend/trump.lua
@@ -0,0 +1,9 @@
+c.StartList("trump")
+L["are you feeling it naoo donald truuuuump"]={{path="chatsounds/autoadd/trump/are you feeling it naoo donald truuuuump.ogg",length=3.030204081632653}}
+L["are you feeling it now donald trump"]={{path="chatsounds/autoadd/trump/are you feeling it now donald trump.ogg",length=2.8444444444444446}}
+L["donald trump you know you want it"]={{path="chatsounds/autoadd/trump/donald trump you know you want it.ogg",length=2.6935147392290251}}
+L["i love you so much donald trump"]={{path="chatsounds/autoadd/trump/i love you so much donald trump.ogg",length=2.0897959183673471}}
+L["im not mexican so you wont have to deport me"]={{path="chatsounds/autoadd/trump/im not mexican so you wont have to deport me.ogg",length=4.2028117913832199}}
+L["im such a narwhal for you donald"]={{path="chatsounds/autoadd/trump/im such a narwhal for you donald.ogg",length=2.5658049886621317}}
+L["look donald im a narwhal"]={{path="chatsounds/autoadd/trump/look donald im a narwhal.ogg",length=3.4481632653061225}}
+c.EndList()
