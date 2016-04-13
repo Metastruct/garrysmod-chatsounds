a656a0e84175e6ad7a45a7940fe52865ec3423ce
Morten <animorten@gmail.com>
2016-04-14 01:13:40 +0200

    that must be an ancient entry
    Former-commit-id: 1e95146e9e3c96df4c2580c3fd155b0030c852c3

--- lua/chatsounds/lists_send/portal2/misc.lua
+++
@@ -1,4 +0,0 @@
-c.StartList("portal 2");
-L["we are currently experiencing technical difficulties due to circumstances of potentically apocalyptic significance beyond our control"] =
-	{{path = "chatsounds/autoadd/portal 2/prehub08.wav", length = 1}};
-c.EndList();
