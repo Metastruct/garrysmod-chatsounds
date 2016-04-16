9e70846aa710405f9fb2368916c45314022e1214
techbot <techbot121@gmail.com>
2016-04-16 23:56:25 +0200

    look at this fucking thing j

---
+++ lua/chatsounds/lists_nosend/holyshitj.lua
@@ -0,0 +1,35 @@
+c.StartList("holyshitj")
+L["and well be on the fucking news man"]={{path="chatsounds/autoadd/holyshitj/and well be on the fucking news man.ogg",length=2.0921768707482995}}
+L["cmon man lets get that thing"]={{path="chatsounds/autoadd/holyshitj/cmon man lets get that thing.ogg",length=2.014126984126984}}
+L["holy shit"]={{path="chatsounds/autoadd/holyshitj/holy shit/holy shit-2.ogg",length=1.1709977324263039},{path="chatsounds/autoadd/holyshitj/holy shit/holy shit-3.ogg",length=1.1553968253968254},{path="chatsounds/autoadd/holyshitj/holy shit/holy shit-4.ogg",length=1.233469387755102},{path="chatsounds/autoadd/holyshitj/holy shit/holy shit-5.ogg",length=1.2022222222222223},{path="chatsounds/autoadd/holyshitj/holy shit/holy shit-6.ogg",length=1.2022222222222223},{path="chatsounds/autoadd/holyshitj/holy shit/holy shit-7.ogg",length=1.2490702947845804},{path="chatsounds/autoadd/holyshitj/holy shit/holy shit.ogg",length=1.0773242630385487}}
+L["holy shit j"]={{path="chatsounds/autoadd/holyshitj/holy shit j.ogg",length=1.5144897959183674}}
+L["holy shit look at this fucking thing"]={{path="chatsounds/autoadd/holyshitj/holy shit look at this fucking thing.ogg",length=2.466893424036281}}
+L["holy shit we are witnessing a baby fucking whale right here"]={{path="chatsounds/autoadd/holyshitj/holy shit we are witnessing a baby fucking whale right here.ogg",length=3.801859410430839}}
+L["it needs help whatever it is dude its dying"]={{path="chatsounds/autoadd/holyshitj/it needs help whatever it is dude its dying.ogg",length=2.7791609977324261}}
+L["its a baby fucking whale man"]={{path="chatsounds/autoadd/holyshitj/its a baby fucking whale man.ogg",length=2.466893424036281}}
+L["j"]={{path="chatsounds/autoadd/holyshitj/j.ogg",length=0.62453514739229021}}
+L["j what the fuck is that"]={{path="chatsounds/autoadd/holyshitj/j what the fuck is that.ogg",length=2.1234240362811789}}
+L["lets fucking hook that shit"]={{path="chatsounds/autoadd/holyshitj/lets fucking hook that shit.ogg",length=1.6393877551020408}}
+L["lets hook it"]={{path="chatsounds/autoadd/holyshitj/lets hook it.ogg",length=0.71820861678004533}}
+L["look at that"]={{path="chatsounds/autoadd/holyshitj/look at that.ogg",length=0.70258503401360539}}
+L["oh man"]={{path="chatsounds/autoadd/holyshitj/oh man.ogg",length=0.95242630385487526}}
+L["oh man look at this fucking thing"]={{path="chatsounds/autoadd/holyshitj/oh man look at this fucking thing/oh man look at this fucking thing-2.ogg",length=2.0921768707482995},{path="chatsounds/autoadd/holyshitj/oh man look at this fucking thing/oh man look at this fucking thing.ogg",length=2.1234240362811789}}
+L["oh man that things just dead"]={{path="chatsounds/autoadd/holyshitj/oh man that things just dead.ogg",length=1.8736054421768706}}
+L["oh my fucking god"]={{path="chatsounds/autoadd/holyshitj/oh my fucking god.ogg",length=1.483265306122449}}
+L["oh my god"]={{path="chatsounds/autoadd/holyshitj/oh my god/oh my god-2.ogg",length=1.3739682539682541},{path="chatsounds/autoadd/holyshitj/oh my god/oh my god.ogg",length=1.3739682539682541}}
+L["oh my god look at this thing"]={{path="chatsounds/autoadd/holyshitj/oh my god look at this thing.ogg",length=2.435668934240363}}
+L["oh my god man were seeing some shit we aint never seen before"]={{path="chatsounds/autoadd/holyshitj/oh my god man were seeing some shit we aint never seen before.ogg",length=4.2000000000000002}}
+L["oh my god that thing looks dead man"]={{path="chatsounds/autoadd/holyshitj/oh my god that thing looks dead man.ogg",length=2.4825170068027211}}
+L["oh my god what the fuck is that bro"]={{path="chatsounds/autoadd/holyshitj/oh my god what the fuck is that bro.ogg",length=3.0446031746031745}}
+L["oh my look at this fucking thing"]={{path="chatsounds/autoadd/holyshitj/oh my look at this fucking thing.ogg",length=2.7635600907029478}}
+L["oh shit j"]={{path="chatsounds/autoadd/holyshitj/oh shit j.ogg",length=1.4364399092970521}}
+L["pull up right next to that shit j"]={{path="chatsounds/autoadd/holyshitj/pull up right next to that shit j.ogg",length=2.1234240362811789}}
+L["that is still some good meat on that fucking fish"]={{path="chatsounds/autoadd/holyshitj/that is still some good meat on that fucking fish.ogg",length=3.1851247165532879}}
+L["that thing is big j"]={{path="chatsounds/autoadd/holyshitj/that thing is big j.ogg",length=1.4520408163265306}}
+L["we gotta call the aquarium or something dude"]={{path="chatsounds/autoadd/holyshitj/we gotta call the aquarium or something dude.ogg",length=2.3888435374149659}}
+L["we seeing some shit we aint seen before kid"]={{path="chatsounds/autoadd/holyshitj/we seeing some shit we aint seen before kid.ogg",length=2.9977777777777779}}
+L["what is that fucking thing"]={{path="chatsounds/autoadd/holyshitj/what is that fucking thing.ogg",length=2.2483219954648526}}
+L["what is that thing"]={{path="chatsounds/autoadd/holyshitj/what is that thing.ogg",length=1.7330839002267573}}
+L["what the fuck is that"]={{path="chatsounds/autoadd/holyshitj/what the fuck is that.ogg",length=1.6550113378684808}}
+L["will you look at this shit"]={{path="chatsounds/autoadd/holyshitj/will you look at this shit.ogg",length=1.5925623582766439}}
+c.EndList()
