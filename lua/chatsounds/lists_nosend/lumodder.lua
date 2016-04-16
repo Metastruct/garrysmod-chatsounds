4a9d5c927e6dbdb082bf24a1e8930fffd5a71f8b
Flex <lumodder@gmail.com>
2016-04-16 14:59:00 -0700

    Sound request dump from Impulse more useless shit from me

--- lua/chatsounds/lists_nosend/lumodder.lua
+++ lua/chatsounds/lists_nosend/lumodder.lua
@@ -2,15 +2,18 @@ c.StartList("lumodder")
 L["aero chord scream"]={{path="chatsounds/autoadd/lumodder/aero chord scream.ogg",length=7.2992970521541949}}
 L["but my hope will never die"]={{path="chatsounds/autoadd/lumodder/but my hope will never die.ogg",length=1.5582086167800453}}
 L["cosmo uh um"]={{path="chatsounds/autoadd/lumodder/cosmo uh um.ogg",length=6.1765079365079369}}
+L["fuckbeesremastered"]={{path="chatsounds/autoadd/lumodder/fuckbeesremastered.ogg",length=201.3257142857143}}
 L["get the fuck out of my life distorted"]={{path="chatsounds/autoadd/lumodder/get the fuck out of my life distorted.ogg",length=18.672312925170068}}
 L["gta wasted"]={{path="chatsounds/autoadd/lumodder/gta wasted.ogg",length=7.5296825396825398}}
 L["hhyyoooooonngghh"]={{path="chatsounds/autoadd/lumodder/hhyyoooooonngghh.ogg",length=2.2821088435374151}}
 L["i am the penguin"]={{path="chatsounds/autoadd/lumodder/i am the penguin.ogg",length=1.4512471655328798}}
 L["i planted the thing"]={{path="chatsounds/autoadd/lumodder/i planted the thing/1.ogg",length=1.2252834467120182},{path="chatsounds/autoadd/lumodder/i planted the thing/2.ogg",length=1.2455102040816326},{path="chatsounds/autoadd/lumodder/i planted the thing/3.ogg",length=3.8921088435374149}}
 L["its boshy time"]={{path="chatsounds/autoadd/lumodder/its boshy time.ogg",length=1.9873922902494332}}
+L["let the fucking bass"]={{path="chatsounds/autoadd/lumodder/let the fucking bass.ogg",length=1.4802040816326532}}
 L["moonthemefull"]={{path="chatsounds/autoadd/lumodder/moonthemefull.ogg",length=92.499591836734695}}
 L["mu os"]={{path="chatsounds/autoadd/lumodder/mu os.ogg",length=1.1377551020408163}}
 L["mumnumnum"]={{path="chatsounds/autoadd/lumodder/mumnumnum.ogg",length=0.80875283446712021}}
+L["nbgskrill"]={{path="chatsounds/autoadd/lumodder/nbgskrill.ogg",length=56.293877551020408}}
 L["real trap shit"]={{path="chatsounds/autoadd/lumodder/real trap shit.ogg",length=2.3161904761904761}}
 L["shaft"]={{path="chatsounds/autoadd/lumodder/shaft.ogg",length=0.9298412698412698}}
 L["shots fired shots fired"]={{path="chatsounds/autoadd/lumodder/shots fired shots fired.ogg",length=3.9063718820861677}}
@@ -28,4 +31,5 @@ L["well shit"]={{path="chatsounds/autoadd/lumodder/well shit.ogg",length=1.75310
 L["whos been drawing dicks"]={{path="chatsounds/autoadd/lumodder/whos been drawing dicks.ogg",length=1.5914739229024943}}
 L["why are you nine"]={{path="chatsounds/autoadd/lumodder/why are you nine.ogg",length=1.4790022675736962}}
 L["why you such a fag face"]={{path="chatsounds/autoadd/lumodder/why you such a fag face.ogg",length=2.0114285714285716}}
+L["yo skrill drop it hard"]={{path="chatsounds/autoadd/lumodder/yo skrill drop it hard.ogg",length=1.2926303854875283}}
 c.EndList()
