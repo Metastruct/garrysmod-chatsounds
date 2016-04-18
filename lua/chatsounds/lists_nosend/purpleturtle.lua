9a67f817cffe7cf2c54734aa5b7372c239fef277
Flex <lumodder@gmail.com>
2016-04-17 19:32:56 -0700

    higher quality op sound and name fix

--- lua/chatsounds/lists_nosend/purpleturtle.lua
+++ lua/chatsounds/lists_nosend/purpleturtle.lua
@@ -1,7 +1,7 @@
 c.StartList("purpleturtle")
 L["fukkin degenerate"]={{path="chatsounds/autoadd/purpleturtle/fukkin degenerate.ogg",length=1.9075056689342405}}
+L["mglvfull"]={{path="chatsounds/autoadd/purpleturtle/mglvfull.ogg",length=156.16}}
 L["mglvhalf"]={{path="chatsounds/autoadd/purpleturtle/mglvhalf.ogg",length=23.897823129251702}}
-L["mlgvfull"]={{path="chatsounds/autoadd/purpleturtle/mlgvfull.ogg",length=156.16}}
 L["reeeee"]={{path="chatsounds/autoadd/purpleturtle/reeeee.ogg",length=3.0810657596371884}}
 L["ukrane banshee scream"]={{path="chatsounds/autoadd/purpleturtle/ukrane banshee scream.ogg",length=7.5180272108843536}}
 c.EndList()
