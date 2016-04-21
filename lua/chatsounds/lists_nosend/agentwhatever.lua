b0b7b1a48fe469d9881948b4cd6883c16d808bdc
Toasty Engineer <timrace01@gmail.com>
2016-04-21 22:11:13 +0200

    Duped chatsound + changed size
    Im not entirely sure why you made dupes. Im assuming you wanted 2 different chatsound names having the same sound, if you did then I changed it to that with only one file. Otherwise i dont know why you duped it.

--- lua/chatsounds/lists_nosend/agentwhatever.lua
+++ lua/chatsounds/lists_nosend/agentwhatever.lua
@@ -1,6 +1,6 @@
 c.StartList("agentwhatever")
 L["pulse jet"]={{path="chatsounds/autoadd/agentwhatever/pulse jet.ogg",length=30.04111111111111}}
-L["pulsejet"]={{path="chatsounds/autoadd/agentwhatever/pulsejet.ogg",length=30.04111111111111}}
+L["pulsejet"]={{path="chatsounds/autoadd/agentwhatever/pulse jet.ogg",length=30.04111111111111}}
 L["too many cooks"]={{path="chatsounds/autoadd/agentwhatever/too many cooks.ogg",length=669.36945578231291}}
-L["toomanycooks"]={{path="chatsounds/autoadd/agentwhatever/toomanycooks.ogg",length=669.36945578231291}}
+L["toomanycooks"]={{path="chatsounds/autoadd/agentwhatever/too many cooks.ogg",length=669.36945578231291}}
 c.EndList()
--- lua/chatsounds/lists_nosend/toastyengineer.lua
+++ lua/chatsounds/lists_nosend/toastyengineer.lua
@@ -1,4 +1,5 @@
 c.StartList("toastyengineer")
 L["banned for life"]={{path="chatsounds/autoadd/toastyengineer/banned for life.ogg",length=4.6820408163265306}}
 L["get out of here stalker"]={{path="chatsounds/autoadd/toastyengineer/get out of here stalker.ogg",length=1.4559183673469387}}
+L["reeeeeeeeeeeeeeee"]={{path="chatsounds/autoadd/toastyengineer/reeeeeeeeeeeeeeee.ogg",length=2.6188435374149659}}
 c.EndList()
