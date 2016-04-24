67bb5e303b4032417234a0aae54859aeefa1ad4a
TrenchFroast <thesteva256@gmail.com>
2016-04-24 01:15:24 +0100

    things said with miscellaneous accents
    dmca inbound probably maybe possibly

---
+++ lua/chatsounds/lists_nosend/theaccents.lua
@@ -0,0 +1,50 @@
+c.StartList("theaccents")
+L["absolutely nothing"]={{path="chatsounds/autoadd/theaccents/absolutely nothing.ogg",length=2.1478458049886622}}
+L["ahem"]={{path="chatsounds/autoadd/theaccents/ahem.ogg",length=0.48471655328798186}}
+L["ai there"]={{path="chatsounds/autoadd/theaccents/ai there.ogg",length=0.81328798185941042}}
+L["alalalala"]={{path="chatsounds/autoadd/theaccents/alalalala.ogg",length=0.65727891156462581}}
+L["alright thats it fuck it"]={{path="chatsounds/autoadd/theaccents/alright thats it fuck it.ogg",length=2.1884807256235828}}
+L["aw fwuck"]={{path="chatsounds/autoadd/theaccents/aw fwuck.ogg",length=0.84462585034013604}}
+L["aww noo"]={{path="chatsounds/autoadd/theaccents/aww noo.ogg",length=0.85260770975056688}}
+L["aww shit"]={{path="chatsounds/autoadd/theaccents/aww shit.ogg",length=0.61251700680272114}}
+L["bloody hell"]={{path="chatsounds/autoadd/theaccents/bloody hell.ogg",length=0.53188208616780042}}
+L["blyad"]={{path="chatsounds/autoadd/theaccents/blyad.ogg",length=0.3512018140589569}}
+L["ciao"]={{path="chatsounds/autoadd/theaccents/ciao.ogg",length=0.5072789115646259}}
+L["cmon sit down have a cup of coffee"]={{path="chatsounds/autoadd/theaccents/cmon sit down have a cup of coffee.ogg",length=1.8924263038548752}}
+L["deutschland deutschland deutschland"]={{path="chatsounds/autoadd/theaccents/deutschland deutschland deutschland.ogg",length=1.9646485260770976}}
+L["deutschland uber alles"]={{path="chatsounds/autoadd/theaccents/deutschland uber alles.ogg",length=1.7821315192743765}}
+L["focking"]={{path="chatsounds/autoadd/theaccents/focking.ogg",length=0.77496598639455783}}
+L["fuck it"]={{path="chatsounds/autoadd/theaccents/fuck it.ogg",length=0.25480725623582767}}
+L["fucking bastard"]={{path="chatsounds/autoadd/theaccents/fucking bastard.ogg",length=1.1508390022675736}}
+L["fucking cunt"]={{path="chatsounds/autoadd/theaccents/fucking cunt.ogg",length=0.73197278911564623}}
+L["fucking douchebag"]={{path="chatsounds/autoadd/theaccents/fucking douchebag/fucking douchebag 1.ogg",length=0.77931972789115644},{path="chatsounds/autoadd/theaccents/fucking douchebag/fucking douchebag 2.ogg",length=1.5775056689342404}}
+L["fucking hell man"]={{path="chatsounds/autoadd/theaccents/fucking hell man.ogg",length=0.91859410430839006}}
+L["fucking pause house"]={{path="chatsounds/autoadd/theaccents/fucking pause house.ogg",length=1.3772335600907029}}
+L["fucking up already"]={{path="chatsounds/autoadd/theaccents/fucking up already.ogg",length=0.86349206349206353}}
+L["goddamnit"]={{path="chatsounds/autoadd/theaccents/goddamnit/goddamnit 1.ogg",length=0.59607709750566895},{path="chatsounds/autoadd/theaccents/goddamnit/goddamnit 2.ogg",length=0.52027210884353736}}
+L["hurra"]={{path="chatsounds/autoadd/theaccents/hurra.ogg",length=1.0014058956916099}}
+L["i know youre there"]={{path="chatsounds/autoadd/theaccents/i know youre there.ogg",length=0.79528344671201812}}
+L["little mousey come out"]={{path="chatsounds/autoadd/theaccents/little mousey come out.ogg",length=2.4468027210884356}}
+L["mamma mia"]={{path="chatsounds/autoadd/theaccents/mamma mia.ogg",length=1.0492517006802722}}
+L["noo"]={{path="chatsounds/autoadd/theaccents/noo.ogg",length=0.6468027210884354}}
+L["oh fuck you cunt"]={{path="chatsounds/autoadd/theaccents/oh fuck you cunt.ogg",length=1.4746258503401362}}
+L["owwow"]={{path="chatsounds/autoadd/theaccents/owwow.ogg",length=0.44292517006802723}}
+L["pwick"]={{path="chatsounds/autoadd/theaccents/pwick.ogg",length=0.62258503401360543}}
+L["scheisse"]={{path="chatsounds/autoadd/theaccents/scheisse.ogg",length=0.55984126984126981}}
+L["shut up man"]={{path="chatsounds/autoadd/theaccents/shut up man.ogg",length=1.4265759637188209}}
+L["so i just gave up and went home"]={{path="chatsounds/autoadd/theaccents/so i just gave up and went home.ogg",length=2.1652607709750566}}
+L["stop shouting"]={{path="chatsounds/autoadd/theaccents/stop shouting.ogg",length=0.9462131519274376}}
+L["that was shit"]={{path="chatsounds/autoadd/theaccents/that was shit.ogg",length=0.95770975056689345}}
+L["we have deposited the cunt"]={{path="chatsounds/autoadd/theaccents/we have deposited the cunt.ogg",length=1.6587755102040815}}
+L["weirdmouthnoises"]={{path="chatsounds/autoadd/theaccents/weirdmouthnoises.ogg",length=2.9808616780045352}}
+L["whacked"]={{path="chatsounds/autoadd/theaccents/whacked.ogg",length=0.37854875283446709}}
+L["what am i doing"]={{path="chatsounds/autoadd/theaccents/what am i doing.ogg",length=0.84614512471655334}}
+L["what is this eh"]={{path="chatsounds/autoadd/theaccents/what is this eh.ogg",length=0.8801814058956916}}
+L["what you think im funny"]={{path="chatsounds/autoadd/theaccents/what you think im funny.ogg",length=0.950702947845805}}
+L["why"]={{path="chatsounds/autoadd/theaccents/why.ogg",length=0.3395918367346939}}
+L["yes yes fucking waste of time"]={{path="chatsounds/autoadd/theaccents/yes yes fucking waste of time.ogg",length=1.8401814058956916}}
+L["you fucking"]={{path="chatsounds/autoadd/theaccents/you fucking.ogg",length=0.76335600907029477}}
+L["you fucking douchebag"]={{path="chatsounds/autoadd/theaccents/you fucking douchebag.ogg",length=1.1472108843537414}}
+L["you fucking fuck"]={{path="chatsounds/autoadd/theaccents/you fucking fuck/you fucking fuck 1.ogg",length=1.6457142857142857},{path="chatsounds/autoadd/theaccents/you fucking fuck/you fucking fuck 2.ogg",length=1.1014965986394558}}
+L["you may have won the battle but you have not won the war"]={{path="chatsounds/autoadd/theaccents/you may have won the battle but you have not won the war.ogg",length=1.9185487528344671}}
+c.EndList()
