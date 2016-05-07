a21315d04247a0d62dfd6fa60350b15aec582b71
GoogleOS <developeros@live.de>
2016-05-07 21:28:16 +0200

    Added defcon sound

--- lua/chatsounds/lists_nosend/google.lua
+++ lua/chatsounds/lists_nosend/google.lua
@@ -1,5 +1,6 @@
 c.StartList("google")
 L["and his name is john cena"]={{path="chatsounds/autoadd/google/and his name is john cena.ogg",length=5.0416326530612245}}
 L["piertotum locomotor"]={{path="chatsounds/autoadd/google/piertotum locomotor.ogg",length=2.8097732426303854}}
+L["presidential authority granted, server is now in defcon one"]={{path="chatsounds/autoadd/google/presidential authority granted, server is now in defcon one.ogg",length=4.6149659863945578}}
 L["the sergeant at arms is hereby instructed to compel the attendance of absent sentors"]={{path="chatsounds/autoadd/google/the sergeant at arms is hereby instructed to compel the attendance of absent sentors.ogg",length=5.4741043083900225}}
 c.EndList()
