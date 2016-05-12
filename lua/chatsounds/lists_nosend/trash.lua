5b51525776474763e938a44eeb92a804bdbaec9d
Mikeai <mgamkrel@gmail.com>
2016-05-12 19:58:51 +0300

    garbage
    when im dead just throw me in the trash

--- lua/chatsounds/lists_nosend/trash.lua
+++ lua/chatsounds/lists_nosend/trash.lua
@@ -2,10 +2,17 @@ c.StartList("trash")
 L["and then i pick up the trash can and bash the guy on the head"]={{path="chatsounds/autoadd/trash/and then i pick up the trash can and bash the guy on the head.ogg",length=4.5858276643990932}}
 L["and then i start eating garbage"]={{path="chatsounds/autoadd/trash/and then i start eating garbage.ogg",length=2.2608390022675735}}
 L["frank what the hell are you doing man"]={{path="chatsounds/autoadd/trash/frank what the hell are you doing man.ogg",length=1.3709297052154195}}
+L["garbage"]={{path="chatsounds/autoadd/trash/garbage.ogg",length=0.7853514739229025}}
 L["i come out i throw trash all over the ring"]={{path="chatsounds/autoadd/trash/i come out i throw trash all over the ring.ogg",length=3.9845351473922901}}
+L["im not gonna be buried in a grave"]={{path="chatsounds/autoadd/trash/im not gonna be buried in a grave.ogg",length=1.8499773242630386}}
 L["im telling you its like you said its all fake"]={{path="chatsounds/autoadd/trash/im telling you its like you said its all fake.ogg",length=2.728344671201814}}
 L["im the trash man"]={{path="chatsounds/autoadd/trash/im the trash man.ogg",length=1.4430839002267575}}
 L["its my character"]={{path="chatsounds/autoadd/trash/its my character.ogg",length=1.2025850340136055}}
 L["nah no nobodys gonna get hurt"]={{path="chatsounds/autoadd/trash/nah no nobodys gonna get hurt.ogg",length=1.5713832199546485}}
+L["that that is not my future"]={{path="chatsounds/autoadd/trash/that that is not my future.ogg",length=1.8740136054421768}}
 L["thats gonna be a disaster"]={{path="chatsounds/autoadd/trash/thats gonna be a disaster.ogg",length=1.7196825396825397}}
+L["the trash business is a gold mine"]={{path="chatsounds/autoadd/trash/the trash business is a gold mine.ogg",length=2.9651473922902496}}
+L["trash"]={{path="chatsounds/autoadd/trash/trash/trash1.ogg",length=0.62},{path="chatsounds/autoadd/trash/trash/trash2.ogg",length=0.57956916099773248},{path="chatsounds/autoadd/trash/trash/trash5.ogg",length=0.4212018140589569},{path="chatsounds/autoadd/trash/trash/tresh3.ogg",length=0.49868480725623582},{path="chatsounds/autoadd/trash/trash/tresh4.ogg",length=0.64693877551020407}}
+L["whaddya mean because its trash"]={{path="chatsounds/autoadd/trash/whaddya mean because its trash.ogg",length=1.846485260770975}}
+L["when im dead just throw me in the trash"]={{path="chatsounds/autoadd/trash/when im dead just throw me in the trash.ogg",length=1.9861224489795919}}
 c.EndList()
