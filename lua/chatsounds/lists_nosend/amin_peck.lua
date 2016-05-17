a7262a3b749a34a608c210f866292209bcef7e65
SpiralP <spiral.palmer@gmail.com>
2016-05-16 18:58:16 -0700

    lists

--- lua/chatsounds/lists_nosend/amin_peck.lua
+++ lua/chatsounds/lists_nosend/amin_peck.lua
@@ -2,4 +2,5 @@ c.StartList("amin_peck")
 L["bowlingpins"]={{path="chatsounds/autoadd/amin_peck/bowlingpins.ogg",length=1.1070748299319728}}
 L["doctor breerb"]={{path="chatsounds/autoadd/amin_peck/doctor breerb.ogg",length=1.1493877551020408}}
 L["ile"]={{path="chatsounds/autoadd/amin_peck/ile.ogg",length=0.80764172335600903}}
+L["this is a huge waste of my time"]={{path="chatsounds/autoadd/amin_peck/this is a huge waste of my time.ogg",length=3.4784807256235828}}
 c.EndList()
