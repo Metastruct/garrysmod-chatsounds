7aee01dd799ca713ac0dec19f1a847aa731a3caa
!Avast AntiPony 9445 <kristianzxz41@gmail.com>
2016-05-25 19:48:41 +0200

    Anime Pimp Tyrone in the house
    Damn. That shit good. :ok_hand:

---
+++ lua/chatsounds/lists_nosend/anime_pimp_tyrone.lua
@@ -0,0 +1,8 @@
+c.StartList("anime_pimp_tyrone")
+L["bitch"]={{path="chatsounds/autoadd/anime_pimp_tyrone/bitch.ogg",length=0.60965986394557825}}
+L["give me my tea bitch"]={{path="chatsounds/autoadd/anime_pimp_tyrone/give me my tea bitch.ogg",length=1.4863945578231292}}
+L["i dont care if you sick"]={{path="chatsounds/autoadd/anime_pimp_tyrone/i dont care if you sick.ogg",length=1.2854648526077097}}
+L["mmm damn dat shit good"]={{path="chatsounds/autoadd/anime_pimp_tyrone/mmm damn that shit good.ogg",length=3.1661224489795918}}
+L["mmm damn that shit good"]={{path="chatsounds/autoadd/anime_pimp_tyrone/mmm damn that shit good.ogg",length=3.1661224489795918}}
+L["wake the fuck up"]={{path="chatsounds/autoadd/anime_pimp_tyrone/wake the fuck up.ogg",length=1.3881859410430839}}
+c.EndList()
