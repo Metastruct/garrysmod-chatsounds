64f53e9b18aaff34e8ea5bcc9f0424e85dcc7c94
Toasty Engineer <timrace01@gmail.com>
2016-05-26 22:11:39 +0200

    I just knew I forgot something
    Failed Push No. 1523523#

---
+++ lua/chatsounds/lists_nosend/payday2.lua
@@ -0,0 +1,5 @@
+c.StartList("payday2")
+L["donacdum"]={{path="chatsounds/autoadd/payday2/donacdum/donacdum1.ogg",length=0.88993197278911562},{path="chatsounds/autoadd/payday2/donacdum/donacdum2.ogg",length=0.91952380952380952}}
+L["get the fuck up"]={{path="chatsounds/autoadd/payday2/get the fuck up.ogg",length=1.1798639455782314}}
+L["guys the thermal drill go get it"]={{path="chatsounds/autoadd/payday2/guys the thermal drill go get it.ogg",length=2.3803174603174604}}
+c.EndList()
