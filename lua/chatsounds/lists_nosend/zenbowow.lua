2a0c96fb7ca846cb6015259dad2c243a5997f7e6
TrenchFroast <thesteva256@gmail.com>
2016-06-11 12:38:17 +0100

    HEY ZENBO
    WOW! [fixes #84]

---
+++ lua/chatsounds/lists_nosend/zenbowow.lua
@@ -0,0 +1,23 @@
+c.StartList("zenbowow")
+L["beep"]={{path="chatsounds/autoadd/zenbowow/beep.ogg",length=0.34736961451247167}}
+L["come here"]={{path="chatsounds/autoadd/zenbowow/come here.ogg",length=0.69374149659863948}}
+L["five nintynine"]={{path="chatsounds/autoadd/zenbowow/five nintynine.ogg",length=0.78798185941043086}}
+L["follow me"]={{path="chatsounds/autoadd/zenbowow/follow me.ogg",length=0.51689342403628113}}
+L["hey zenbo"]={{path="chatsounds/autoadd/zenbowow/hey zenbo/hey zenbo 1.ogg",length=0.57614512471655333},{path="chatsounds/autoadd/zenbowow/hey zenbo/hey zenbo 2.ogg",length=0.90256235827664399}}
+L["hey zenbo come here"]={{path="chatsounds/autoadd/zenbowow/hey zenbo come here.ogg",length=1.4817233560090703}}
+L["hey zenbo follow me"]={{path="chatsounds/autoadd/zenbowow/hey zenbo follow me.ogg",length=1.096984126984127}}
+L["i am a robot of many talents"]={{path="chatsounds/autoadd/zenbowow/i am a robot of many talents.ogg",length=2.4767800453514739}}
+L["i can sing and dance"]={{path="chatsounds/autoadd/zenbowow/i can sing and dance.ogg",length=1.5744444444444445}}
+L["in reality"]={{path="chatsounds/autoadd/zenbowow/in reality.ogg",length=0.85360544217687073}}
+L["of your dream"]={{path="chatsounds/autoadd/zenbowow/of your dream.ogg",length=0.85741496598639455}}
+L["sure"]={{path="chatsounds/autoadd/zenbowow/sure.ogg",length=0.79986394557823126}}
+L["take a photo"]={{path="chatsounds/autoadd/zenbowow/take a photo.ogg",length=0.8989795918367347}}
+L["turn on the lights"]={{path="chatsounds/autoadd/zenbowow/turn on the lights.ogg",length=1.1370068027210884}}
+L["turning on the lights"]={{path="chatsounds/autoadd/zenbowow/turning on the lights.ogg",length=1.6156916099773242}}
+L["we can make the robot"]={{path="chatsounds/autoadd/zenbowow/we can make the robot.ogg",length=1.4222675736961452}}
+L["woohoo"]={{path="chatsounds/autoadd/zenbowow/woohoo.ogg",length=0.75326530612244902}}
+L["wow"]={{path="chatsounds/autoadd/zenbowow/wow.ogg",length=0.57197278911564631}}
+L["wowdistorted"]={{path="chatsounds/autoadd/zenbowow/wowdistorted.ogg",length=0.53410430839002265}}
+L["zenbo"]={{path="chatsounds/autoadd/zenbowow/zenbo.ogg",length=0.44276643990929704}}
+L["zenbo five ninetynine"]={{path="chatsounds/autoadd/zenbowow/zenbo five ninetynine.ogg",length=1.3492063492063493}}
+c.EndList()
