39fd8a4be87a671f39fd29c2290ecccc38e1181d
KaosHeaven <redmagus8@gmail.com>
2016-06-17 18:30:01 -0500

    fix
    oops

---
+++ lua/chatsounds/lists_nosend/chrysler_eva.lua
@@ -0,0 +1,19 @@
+c.StartList("chrysler_eva")
+L["please check your brake fluid level"]={{path="chatsounds/autoadd/chrysler_eva/please check your brake fluid level.ogg",length=2.6842857142857142}}
+L["please check your brakelamps"]={{path="chatsounds/autoadd/chrysler_eva/please check your brakelamps.ogg",length=2.2604308390022676}}
+L["please check your disc brake pads"]={{path="chatsounds/autoadd/chrysler_eva/please check your disc brake pads.ogg",length=2.5429931972789115}}
+L["please check your engine coolant level"]={{path="chatsounds/autoadd/chrysler_eva/please check your engine coolant level.ogg",length=2.966825396825397}}
+L["please check your fuel levels"]={{path="chatsounds/autoadd/chrysler_eva/please check your fuel levels.ogg",length=2.4017006802721088}}
+L["please check your headlamps"]={{path="chatsounds/autoadd/chrysler_eva/please check your headlamps.ogg",length=2.2604308390022676}}
+L["please check your taillamps"]={{path="chatsounds/autoadd/chrysler_eva/please check your taillamps.ogg",length=2.4017233560090703}}
+L["please close your driver door"]={{path="chatsounds/autoadd/chrysler_eva/please close your driver door.ogg",length=2.543015873015873}}
+L["please close your passenger door"]={{path="chatsounds/autoadd/chrysler_eva/please close your passenger door.ogg",length=2.5429931972789115}}
+L["prompt service is required"]={{path="chatsounds/autoadd/chrysler_eva/prompt service is required.ogg",length=2.4017233560090703}}
+L["your charging system is malfunctioning"]={{path="chatsounds/autoadd/chrysler_eva/your charging system is malfunctioning.ogg",length=2.8255555555555554}}
+L["your engine oil levels are critical engine damage may occur"]={{path="chatsounds/autoadd/chrysler_eva/your engine oil levels are critical engine damage may occur.ogg",length=5.6511111111111108}}
+L["your engine temperature is above normal"]={{path="chatsounds/autoadd/chrysler_eva/your engine temperature is above normal.ogg",length=3.1081179138321997}}
+L["your headlamps are on"]={{path="chatsounds/autoadd/chrysler_eva/your headlamps are on.ogg",length=2.5429931972789115}}
+L["your keys are in the ignition"]={{path="chatsounds/autoadd/chrysler_eva/your keys are in the ignition.ogg",length=2.4017233560090703}}
+L["your parking brake is on"]={{path="chatsounds/autoadd/chrysler_eva/your parking brake is on.ogg",length=2.2604308390022676}}
+L["your washer fluid is low"]={{path="chatsounds/autoadd/chrysler_eva/your washer fluid is low.ogg",length=2.5429931972789115}}
+c.EndList()
