5a17cdc962c0b5e8851f4264827ef54bee2991de
!Avast AntiPony 9445 <kristianzxz41@gmail.com>
2016-06-22 00:00:16 +0200

    Forgot to run chatsounds-preprocessor
    Darn. I need the bed.

--- lua/chatsounds/lists_nosend/blood.lua
+++ lua/chatsounds/lists_nosend/blood.lua
@@ -14,9 +14,11 @@ L["cultdie"]={{path="chatsounds/autoadd/blood/cultdie/cultdie1.ogg",length=1.255
 L["cultfir"]={{path="chatsounds/autoadd/blood/cultfir/cultfir1.ogg",length=3.2034467120181405},{path="chatsounds/autoadd/blood/cultfir/cultfir2.ogg",length=4.6198639455782313},{path="chatsounds/autoadd/blood/cultfir/cultfir3.ogg",length=3.62140589569161}}
 L["cultglo"]={{path="chatsounds/autoadd/blood/cultglo/cultglo1.ogg",length=3.4677551020408162},{path="chatsounds/autoadd/blood/cultglo/cultglo2.ogg",length=3.2143310657596373},{path="chatsounds/autoadd/blood/cultglo/cultglo3.ogg",length=2.2770975056689342},{path="chatsounds/autoadd/blood/cultglo/cultglo4.ogg",length=2.2688435374149658}}
 L["cultrom"]={{path="chatsounds/autoadd/blood/cultrom/cultrom1.ogg",length=2.5609070294784582},{path="chatsounds/autoadd/blood/cultrom/cultrom2.ogg",length=1.9825850340136055},{path="chatsounds/autoadd/blood/cultrom/cultrom3.ogg",length=1.835283446712018},{path="chatsounds/autoadd/blood/cultrom/cultrom4.ogg",length=1.1212698412698412}}
+L["cultrom3"]={{path="chatsounds/autoadd/blood/cultrom3.ogg",length=1.835283446712018}}
 L["cultspt"]={{path="chatsounds/autoadd/blood/cultspt/cultspt1.ogg",length=2.5609070294784582},{path="chatsounds/autoadd/blood/cultspt/cultspt2.ogg",length=1.9825850340136055},{path="chatsounds/autoadd/blood/cultspt/cultspt3.ogg",length=2.2534240362811793},{path="chatsounds/autoadd/blood/cultspt/cultspt4.ogg",length=1.1212698412698412},{path="chatsounds/autoadd/blood/cultspt/vorox esco marana.ogg",length=1.2071655328798185}}
+L["cultspt3"]={{path="chatsounds/autoadd/blood/cultspt3.ogg",length=2.2534240362811793}}
 L["dargh"]={{path="chatsounds/autoadd/blood/cultpan1.ogg",length=0.3834013605442177}}
-L["geroshay cruo"]={{path="chatsounds/autoadd/blood/cultspt3.ogg",length=2.2534240362811793}}
+L["geroshay cruo"]={{path="chatsounds/autoadd/blood/geroshay cruo/cultrom3.ogg",length=1.835283446712018},{path="chatsounds/autoadd/blood/geroshay cruo/cultspt3.ogg",length=2.2534240362811793}}
 L["huargh"]={{path="chatsounds/autoadd/blood/cultpan3.ogg",length=0.40634920634920635}}
 L["i live again"]={{path="chatsounds/autoadd/blood/i live again.ogg",length=2.6561451247165535}}
 L["in marana domusbhaava crunatus"]={{path="chatsounds/autoadd/blood/in marana domusbhaava crunatus.ogg",length=3.2143310657596373}}
