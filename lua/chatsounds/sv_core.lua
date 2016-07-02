24b1813470a20f276f02df69c5f1272c00bfd6ef
notcake <cakenotfound@googlemail.com>
2016-07-02 20:22:48 +0100

    Dead code removal.

--- lua/chatsounds/sv_core.lua
+++ lua/chatsounds/sv_core.lua
@@ -61,50 +61,22 @@ function chatsounds.GetRecipientFilter(pos)
 	local i=1
 	
 	for key, ply in next,player.GetAll() do
-		
 		local enabled = ply:GetInfoNum("chatsounds_enabled",0)
 		if enabled and enabled >= 1 then
-		
 			if not pos or (pos and ply:VisibleVec(pos)) then
 				plys[i] = ply
 				i = i + 1
 			end
-			
 		end
-		
 	end
 	
 	while i<257 do
-	
 		if plys[i]==nil then break end
 		plys[i]=nil
 		i=i+1
-		
 	end
 
 	return plys
-end
-
-function chatsounds.IsLuaCommand(text)
-	-- Matt: Maybe use LuaDev module to see if its one of the commands?
-	text = text:TrimLeft()
-
-	if ( text:sub(1, 2) == "!l" or
-		text:sub(1, 3) == "!lc" or
-		text:sub(1, 3) == "!cl" or
-		text:sub(1, 3) == "!lc" or
-		text:sub(1, 3) == "!ls" or
-		text:sub(1, 3) == "!lm" or
-		text:sub(1, 4) == "!cmd" or
-		text:sub(1, 6) == "!print" or
-		text:sub(1, 6) == "!printm" or
-		text:sub(1, 6) == "!printc" or
-		text:sub(1, 6) == "!table" )
-	then
-		return true
-	end
-
-	return false
 end
 
 local offground=Vector(0,0,16)
@@ -115,30 +87,19 @@ function chatsounds.Say(ply, text)
 	if hook.Call("PreChatSoundsSay", nil, ply, text) == false then return end
 
 	local seed = chatsounds.Seed - 127
-	--if( #text > 220 ) then
-		net.Start("chatsounds")
-			net.WriteEntity(ply)
-			-- cut to 32KB
-			net.WriteString( text:sub(1,64000-32-32000) )
-			net.WriteInt(seed,32)
+	net.Start("chatsounds")
+		net.WriteEntity(ply)
+		-- cut to 32KB
+		net.WriteString( text:sub(1,64000-32-32000) )
+		net.WriteInt(seed,32)
 		local pos = ply:GetPos()
-			  pos:Add(offground)
-		net.Send(chatsounds.GetRecipientFilter(pos))
-	--else
-	--	umsg.Start("chatsounds", chatsounds.GetRecipientFilter())
-	--		umsg.Entity(ply)
-	--		umsg.Char(seed)
-	--		-- todo: make this clientside, maybe store clientside chat history and match them with CRC?
-	--		-- I'll leave it at this for now
-	--		umsg.String(text)
-	--	umsg.End()
-	--end
+		pos:Add(offground)
+	net.Send(chatsounds.GetRecipientFilter(pos))
+	
 	chatsounds.GenerateNewSeed()
 end
 
 function chatsounds.PlayerSay(ply, text)
-	if chatsounds.IsLuaCommand(text) then return end
-	
 	if string.sub(text, 1, 2) == "##" then
 		text = string.sub(text, 3)
 		chatsounds.Say(ply, text)
