48b632ad50d42594fa59be7ab6975bd68e1fc48a
Slade Xanthas <sladexanthas@gmail.com>
2016-07-23 02:32:22 -0400

    Updated sounds on behalf of Impulse

--- lua/chatsounds/lists_nosend/impulse.lua
+++ lua/chatsounds/lists_nosend/impulse.lua
@@ -4,19 +4,26 @@ L["alientaunt"]={{path="chatsounds/autoadd/impulse/alientaunt/alientaunt1.ogg",l
 L["are you fucking serious"]={{path="chatsounds/autoadd/impulse/are you fucking serious.ogg",length=2.1720181405895693}}
 L["barrelroll"]={{path="chatsounds/autoadd/impulse/barrelroll.ogg",length=13.716802721088435}}
 L["cheekibreekiradio"]={{path="chatsounds/autoadd/impulse/cheekibreekiradio.ogg",length=65.175510204081633}}
+L["chimandolin"]={{path="chatsounds/autoadd/impulse/chimandolin.ogg",length=12.151836734693877}}
 L["do a barrel roll"]={{path="chatsounds/autoadd/impulse/do a barrel roll.ogg",length=1.0158956916099773}}
 L["get outta here"]={{path="chatsounds/autoadd/impulse/get outta here.ogg",length=1.6303628117913833}}
+L["healing spring"]={{path="chatsounds/autoadd/impulse/healing spring.ogg",length=20.062086167800455}}
 L["i wonder if theres any toucans here like i just want to kill an endangered species"]={{path="chatsounds/autoadd/impulse/i wonder if theres any toucans here like i just want to kill an endangered species.ogg",length=4.4865759637188205}}
 L["im gonna shove my cock down your throat"]={{path="chatsounds/autoadd/impulse/im gonna shove my cock down your throat.ogg",length=3.1928117913832201}}
 L["imma firin mah lazer"]={{path="chatsounds/autoadd/impulse/imma firin mah lazer.ogg",length=3.298888888888889}}
-L["is that wrong slash does that make me a bad person for wanting to kill an endangered species just because i could"]={{path="chatsounds/autoadd/impulse/is that wrong slash does that make me a bad person for wanting to kill an endangered species just because i could.ogg",length=6.5055328798185945}}
+L["is that wrong slash does that make me a bad person"]={{path="chatsounds/autoadd/impulse/is that wrong slash does that make me a bad person.ogg",length=6.5055328798185945}}
 L["its the future what are you gonna do about it"]={{path="chatsounds/autoadd/impulse/its the future what are you gonna do about it.ogg",length=1.9698866213151927}}
-L["kid noises"]={{path="chatsounds/autoadd/impulse/kid noises.ogg",length=10.597346938775511}}
+L["kid noises"]={{path="chatsounds/autoadd/impulse/kid noises.ogg",length=10.20702947845805}}
 L["kid scream"]={{path="chatsounds/autoadd/impulse/kid scream.ogg",length=1.1798639455782314}}
+L["lord macintosh plus"]={{path="chatsounds/autoadd/impulse/lord macintosh plus/1.ogg",length=64.837732426303859},{path="chatsounds/autoadd/impulse/lord macintosh plus/2.ogg",length=60.147981859410429},{path="chatsounds/autoadd/impulse/lord macintosh plus/3.ogg",length=60.603922902494332},{path="chatsounds/autoadd/impulse/lord macintosh plus/4.ogg",length=60.669047619047618},{path="chatsounds/autoadd/impulse/lord macintosh plus/5.ogg",length=77.831678004535149}}
+L["megaddeth"]={{path="chatsounds/autoadd/impulse/megaddeth.ogg",length=6.1670294784580495}}
 L["mom i want a sandwich"]={{path="chatsounds/autoadd/impulse/mom i want a sandwich.ogg",length=2.0736961451247167}}
 L["my human instinct is telling me to just ruin it and like burn it down"]={{path="chatsounds/autoadd/impulse/my human instinct is telling me to just ruin it and like burn it down.ogg",length=10.206961451247166}}
+L["nomble"]={{path="chatsounds/autoadd/impulse/nomble.ogg",length=7.6039455782312926}}
 L["offlimits"]={{path="chatsounds/autoadd/impulse/offlimits.ogg",length=113.17460317460318}}
 L["oww my spine"]={{path="chatsounds/autoadd/impulse/oww my spine.ogg",length=1.9487074829931972}}
+L["pixies"]={{path="chatsounds/autoadd/impulse/pixies.ogg",length=8}}
+L["sandman"]={{path="chatsounds/autoadd/impulse/sandman.ogg",length=15.482267573696145}}
 L["sounds like im in the amazon rainforest"]={{path="chatsounds/autoadd/impulse/sounds like im in the amazon rainforest.ogg",length=2.5797732426303854}}
 L["wanna fight about it"]={{path="chatsounds/autoadd/impulse/wanna fight about it.ogg",length=1.794625850340136}}
 L["yeah ill check my ammo in the fucking doorway"]={{path="chatsounds/autoadd/impulse/yeah ill check my ammo in the fucking doorway.ogg",length=2.5797732426303854}}
