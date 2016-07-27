da3388a95c882bfe2f416f9b3a21fd0ad7af9c40
KabusIblis <kabusiblis@gmail.com>
2016-07-27 23:41:44 +0100

    more
    missed some file directories

--- lua/chatsounds/lists_nosend/fullmetaljacket.lua
+++ lua/chatsounds/lists_nosend/fullmetaljacket.lua
@@ -1,4 +1,5 @@
 c.StartList("fullmetaljacket")
 L["dicks"]={{path="chatsounds/autoadd/fullmetaljacket/dicks.ogg",length=0.57136054421768712}}
 L["do you suck dicks"]={{path="chatsounds/autoadd/fullmetaljacket/do you suck dicks.ogg",length=1.3322448979591837}}
+L["you had best unfuck yourself or i will unscrew your head and shit down your neck"]={{path="chatsounds/autoadd/fullmetaljacket/you had best unfuck yourself or i will unscrew your head and shit down your neck.ogg",length=4.9234467120181407}}
 c.EndList()
--- lua/chatsounds/lists_nosend/terrorism.lua
+++ lua/chatsounds/lists_nosend/terrorism.lua
@@ -1,5 +1,3 @@
-c.StartList("snipaaz")
-L["allahuakbar"]={{path="chatsounds/autoadd/snipaaz/allahuakbar.ogg",length=5.6598639455782314}}
-L["allahuakbar2"]={{path="chatsounds/autoadd/snipaaz/allahuakbar2.ogg",length=2.4200907029478458}}
-L["allahuakbar3"]={{path="chatsounds/autoadd/snipaaz/allahuakbar3.ogg",length=1.2807936507936508}}
+c.StartList("terrorism")
+L["allahuakbar"]={{path="chatsounds/autoadd/terrorism/1.ogg",length=5.6598639455782314},{path="chatsounds/autoadd/terrorism/2.ogg",length=2.4200907029478458},["allahuakbar3"]={{path="chatsounds/autoadd/terrorism/3.ogg",length=1.2807936507936508}}
 c.EndList()
