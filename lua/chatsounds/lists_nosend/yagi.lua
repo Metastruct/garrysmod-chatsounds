d2af9f24be071094ceb84ae97433ce9fb47f7b96
Yagira <yagiraa@gmail.com>
2016-08-27 18:17:54 +0200

    Some more chatsounds.

--- lua/chatsounds/lists_nosend/yagi.lua
+++ lua/chatsounds/lists_nosend/yagi.lua
@@ -1,18 +1,35 @@
 c.StartList("yagi")
+L["all it takes is focus and a steady hand"]={{path="chatsounds/autoadd/yagi/all it takes is focus and a steady hand.ogg",length=3.1093877551020408}}
 L["bitch wake up"]={{path="chatsounds/autoadd/yagi/bitch wake up.ogg",length=1.3704761904761904}}
 L["by the way your car sucks freak"]={{path="chatsounds/autoadd/yagi/by the way your car sucks freak.ogg",length=2.6819047619047618}}
+L["come on barbie lets go party"]={{path="chatsounds/autoadd/yagi/come on barbie lets go party.ogg",length=8.7205668934240368}}
+L["excuse me sir i need you to turn around"]={{path="chatsounds/autoadd/yagi/excuse me sir i need you to turn around.ogg",length=2.8014739229024945}}
+L["fuck my life"]={{path="chatsounds/autoadd/yagi/fuck my life.ogg",length=1.2973696145124716}}
+L["fucking obi wan shit"]={{path="chatsounds/autoadd/yagi/fucking obi wan shit.ogg",length=1.5844671201814058}}
 L["get out"]={{path="chatsounds/autoadd/yagi/get out.ogg",length=0.88235827664399091}}
+L["hes just like a jedi"]={{path="chatsounds/autoadd/yagi/hes just like a jedi.ogg",length=1.7512698412698413}}
 L["hey bitch can i borrow some sugar"]={{path="chatsounds/autoadd/yagi/hey bitch can i borrow some sugar.ogg",length=2.9373242630385485}}
 L["hey buddy what happened to you"]={{path="chatsounds/autoadd/yagi/hey buddy what happened to you.ogg",length=2.2523356009070294}}
+L["hopefully on the other side of this door is a quick and painless death"]={{path="chatsounds/autoadd/yagi/hopefully on the other side of this door is a quick and painless death.ogg",length=3.5062131519274375}}
+L["i really hate my life"]={{path="chatsounds/autoadd/yagi/i really hate my life.ogg",length=1.6797732426303855}}
 L["im gonna get some coffee"]={{path="chatsounds/autoadd/yagi/im gonna get some coffee.ogg",length=1.195827664399093}}
+L["im gonna say being a piece of shit in life brought you down this road"]={{path="chatsounds/autoadd/yagi/im gonna say being a piece of shit in life brought you down this road.ogg",length=3.2523356009070294}}
+L["im in my own personal hell"]={{path="chatsounds/autoadd/yagi/im in my own personal hell.ogg",length=1.8227437641723356}}
 L["im so out of here"]={{path="chatsounds/autoadd/yagi/im so out of here.ogg",length=1.4164172335600906}}
 L["im surrounded by idiots"]={{path="chatsounds/autoadd/yagi/im surrounded by idiots.ogg",length=2.9257142857142857}}
+L["it feels like ive been in that room for six fucking years"]={{path="chatsounds/autoadd/yagi/it feels like ive been in that room for six fucking years.ogg",length=3.6754421768707481}}
+L["just what i need more trash in my life"]={{path="chatsounds/autoadd/yagi/just what i need more trash in my life.ogg",length=2.7758049886621317}}
 L["oh great traps"]={{path="chatsounds/autoadd/yagi/oh great traps.ogg",length=1.4164172335600906}}
+L["oh my god can you just holy shit"]={{path="chatsounds/autoadd/yagi/oh my god can you just holy shit.ogg",length=3.1927891156462587}}
 L["open up its the police"]={{path="chatsounds/autoadd/yagi/open up its the police.ogg",length=1.9156462585034013}}
 L["thank god i put on my anti explosion cream i got of ebay"]={{path="chatsounds/autoadd/yagi/thank god i put on my anti explosion cream i got of ebay.ogg",length=3.6720634920634923}}
+L["thats better"]={{path="chatsounds/autoadd/yagi/thats better.ogg",length=0.92185941043083897}}
 L["the fuck is wrong with these people"]={{path="chatsounds/autoadd/yagi/the fuck is wrong with these people.ogg",length=2.1130158730158732}}
+L["this is my favourite song"]={{path="chatsounds/autoadd/yagi/this is my favourite song.ogg",length=1.7035827664399092}}
 L["this song"]={{path="chatsounds/autoadd/yagi/this song.ogg",length=1.195827664399093}}
 L["well sir if you are having problems with our customer support you can call 1-800-eat-a-dick"]={{path="chatsounds/autoadd/yagi/well sir if you are having problems with our customer support you can call 1-800-eat-a-dick.ogg",length=4.1447619047619044}}
-L["what about the negro noodle"]={{path="chatsounds/autoadd/yagi/what about the negro noodle.ogg",length=1.7182766439909296}}
+L["what the fuck"]={{path="chatsounds/autoadd/yagi/what the fuck.ogg",length=1.5324263038548753}}
+L["when did i turn into indiana jones"]={{path="chatsounds/autoadd/yagi/when did i turn into indiana jones.ogg",length=2.4899092970521544}}
+L["why the fuck is this room randomly filled with lava anyway"]={{path="chatsounds/autoadd/yagi/why the fuck is this room randomly filled with lava anyway.ogg",length=3.8122675736961451}}
 L["you said you love me"]={{path="chatsounds/autoadd/yagi/you said you love me.ogg",length=1.4628571428571429}}
 c.EndList()
