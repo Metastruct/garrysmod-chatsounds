8281e488d7068aa15affbd7f70bd3ab9ffcfe09e
!Avast AntiPony 9445 <kristianzxz41@gmail.com>
2016-10-15 00:53:18 +0200

    Haven't made anything here in a while.
    So basicly:
    Added wrrrrrrreeeeeeooooow from "An Idiot Abroad"
    (https://youtu.be/gx0zTdDpuX0)
    Added EAT THE MUFFIN sounds (https://youtu.be/i_7PRDPa7I4)
    Added kue kue kue chocoball(u?) sounds (https://youtu.be/lDUOmY80GhU)
    Added RIP RIP BRITAIN - Cool Dudes sounds (https://youtu.be/xgHwaYvzuvg)
    Added original and higher quality "aww fuck i cant believe youve done
    this" (https://youtu.be/wKbU8B-QVZk) and moved them from tw1sta to a new
    folder. Sorry TW1ST.
    
    I really hope someone appreciate the links to source material.
    
    Also I bet Trenboy suck alota d***

---
+++ lua/chatsounds/lists_nosend/an_idiot_abroad.lua
@@ -0,0 +1,3 @@
+c.StartList("an_idiot_abroad")
+L["wrrrrrrrrrrreeeeeeeeeeoooooooowwwww"]={{path="chatsounds/autoadd/an_idiot_abroad/wrrrrrrrrrrreeeeeeeeeeoooooooowwwww.ogg",length=24.450340136054422}}
+c.EndList()
---
+++ lua/chatsounds/lists_nosend/kue_kue_kue_chocoball.lua
@@ -0,0 +1,5 @@
+c.StartList("kue_kue_kue_chocoball")
+L["chocoball"]={{path="chatsounds/autoadd/kue_kue_kue_chocoball/chocoball.ogg",length=1.5454195011337868}}
+L["kue kue kue"]={{path="chatsounds/autoadd/kue_kue_kue_chocoball/kue kue kue.ogg",length=1.8635827664399094}}
+L["kue kue kue chocoball"]={{path="chatsounds/autoadd/kue_kue_kue_chocoball/kue kue kue chocoball.ogg",length=4.6617913832199545}}
+c.EndList()
