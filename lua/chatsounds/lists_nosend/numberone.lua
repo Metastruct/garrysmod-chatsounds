2701f61c5495f9cbc65b237e81e3a1d90dc41ef2
Mikeai <mgamkrel@gmail.com>
2016-11-06 15:53:31 +0200

    that i just found
    Fixed a smol misspelling

--- lua/chatsounds/lists_nosend/numberone.lua
+++ lua/chatsounds/lists_nosend/numberone.lua
@@ -9,7 +9,7 @@ L["now listen closely"]={{path="chatsounds/autoadd/numberone/now listen closely.
 L["now look at this net"]={{path="chatsounds/autoadd/numberone/now look at this net.ogg",length=1.3021995464852607}}
 L["now watch and learn heres the deal"]={{path="chatsounds/autoadd/numberone/now watch and learn heres the deal.ogg",length=2.9859637188208619}}
 L["oh lets try something else"]={{path="chatsounds/autoadd/numberone/oh lets try something else.ogg",length=1.8342857142857143}}
-L["that i just foun"]={{path="chatsounds/autoadd/numberone/that i just foun.ogg",length=1.48421768707483}}
+L["that i just found"]={{path="chatsounds/autoadd/numberone/that i just found.ogg",length=1.48421768707483}}
 L["this is going down in history"]={{path="chatsounds/autoadd/numberone/this is going down in history.ogg",length=2.9264399092970521}}
 L["throw it at him not me"]={{path="chatsounds/autoadd/numberone/throw it at him not me.ogg",length=2.2263492063492065}}
 L["what are you doing"]={{path="chatsounds/autoadd/numberone/what are you doing.ogg",length=2.0618140589569163}}
