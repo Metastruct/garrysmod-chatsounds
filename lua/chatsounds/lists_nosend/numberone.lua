1cfaa24deb85059558fd8ee68bb775c9ca15b090
Mikeai <mgamkrel@gmail.com>
2016-11-06 15:50:37 +0200

    We are number one
    now look at this net

---
+++ lua/chatsounds/lists_nosend/numberone.lua
@@ -0,0 +1,17 @@
+c.StartList("numberone")
+L["be careful not to make a sound"]={{path="chatsounds/autoadd/numberone/be careful not to make a sound.ogg",length=2.436371882086168}}
+L["be ready to throw"]={{path="chatsounds/autoadd/numberone/be ready to throw.ogg",length=1.2951927437641724}}
+L["hell slip and slide on this banana peel"]={{path="chatsounds/autoadd/numberone/hell slip and slide on this banana peel.ogg",length=2.9544444444444444}}
+L["heres a little lesson in trickery"]={{path="chatsounds/autoadd/numberone/heres a little lesson in trickery.ogg",length=2.744421768707483}}
+L["hgo"]={{path="chatsounds/autoadd/numberone/hgo.ogg",length=0.94514739229024947}}
+L["no dont touch that"]={{path="chatsounds/autoadd/numberone/no dont touch that.ogg",length=1.4562131519274377}}
+L["now listen closely"]={{path="chatsounds/autoadd/numberone/now listen closely.ogg",length=1.8482766439909297}}
+L["now look at this net"]={{path="chatsounds/autoadd/numberone/now look at this net.ogg",length=1.3021995464852607}}
+L["now watch and learn heres the deal"]={{path="chatsounds/autoadd/numberone/now watch and learn heres the deal.ogg",length=2.9859637188208619}}
+L["oh lets try something else"]={{path="chatsounds/autoadd/numberone/oh lets try something else.ogg",length=1.8342857142857143}}
+L["that i just foun"]={{path="chatsounds/autoadd/numberone/that i just foun.ogg",length=1.48421768707483}}
+L["this is going down in history"]={{path="chatsounds/autoadd/numberone/this is going down in history.ogg",length=2.9264399092970521}}
+L["throw it at him not me"]={{path="chatsounds/autoadd/numberone/throw it at him not me.ogg",length=2.2263492063492065}}
+L["what are you doing"]={{path="chatsounds/autoadd/numberone/what are you doing.ogg",length=2.0618140589569163}}
+L["when i say go"]={{path="chatsounds/autoadd/numberone/when i say go.ogg",length=1.3336961451247165}}
+c.EndList()
