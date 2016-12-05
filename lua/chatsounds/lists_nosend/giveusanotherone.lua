43d6e03d85360018447cfdf72381ad3afb4bd7de
Mikeai <mgamkrel@gmail.com>
2016-12-05 16:52:49 +0200

    give us another one
    aaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ lua/chatsounds/lists_nosend/giveusanotherone.lua
@@ -0,0 +1,9 @@
+c.StartList("giveusanotherone")
+L["aaaaaaaaaaa"]={{path="chatsounds/autoadd/giveusanotherone/aaaaaaaaaaa.ogg",length=1.4699773242630385}}
+L["ffuck yeah"]={{path="chatsounds/autoadd/giveusanotherone/ffuck yeah.ogg",length=1.6333106575963718}}
+L["give us another one"]={{path="chatsounds/autoadd/giveusanotherone/give us another one/giveusanotherone1.ogg",length=0.9095918367346939},{path="chatsounds/autoadd/giveusanotherone/give us another one/giveusanotherone2.ogg",length=1.2559637188208617}}
+L["give us another one aaaaaaaa"]={{path="chatsounds/autoadd/giveusanotherone/give us another one aaaaaaaa.ogg",length=2.790702947845805}}
+L["hahaa"]={{path="chatsounds/autoadd/giveusanotherone/hahaa.ogg",length=2.1064172335600908}}
+L["ooohahoo"]={{path="chatsounds/autoadd/giveusanotherone/ooohahoo.ogg",length=2.6696145124716555}}
+L["yeaheheh"]={{path="chatsounds/autoadd/giveusanotherone/yeaheheh.ogg",length=1.9712244897959184}}
+c.EndList()
