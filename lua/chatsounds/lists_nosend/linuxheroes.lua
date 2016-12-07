fdb1e8e7efba3736e6e72942bd15e89a5bd7e426
Hillebrand van de Groep <hillebrand@T420s.T420s.local>
2016-12-07 20:38:31 +0100

    eek, forgot the lua shit

---
+++ lua/chatsounds/lists_nosend/linuxheroes.lua
@@ -0,0 +1,9 @@
+c.StartList("linuxheroes")
+L["and im very happy to say that its the exception rather than the rule"]={{path="chatsounds/autoadd/linuxheroes/and im very happy to say that its the exception rather than the rule.ogg",length=9.2004535147392286}}
+L["and nvidia has been the single worst company weve ever dealt with"]={{path="chatsounds/autoadd/linuxheroes/and nvidia has been the single worst company weve ever dealt with.ogg",length=4.1014058956916104}}
+L["and that is really sad beacause nvidia tries to sell chips a lot of chips into the android market"]={{path="chatsounds/autoadd/linuxheroes/and that is really sad beacause nvidia tries to sell chips a lot of chips into the android market.ogg",length=7.1774603174603175}}
+L["fuck you"]={{path="chatsounds/autoadd/linuxheroes/fuck you.ogg",length=1.0565079365079366}}
+L["id just like to interject for a moment what youre referring to as linux"]={{path="chatsounds/autoadd/linuxheroes/id just like to interject for a moment what youre referring to as linux.ogg",length=94.119183673469394}}
+L["nvidia has been one of the worst troublespots weve had with hardware manufacturers"]={{path="chatsounds/autoadd/linuxheroes/nvidia has been one of the worst troublespots weve had with hardware manufacturers.ogg",length=8.5630612244897968}}
+L["so nvidia fuck you"]={{path="chatsounds/autoadd/linuxheroes/so nvidia fuck you.ogg",length=2.2871655328798184}}
+c.EndList()
