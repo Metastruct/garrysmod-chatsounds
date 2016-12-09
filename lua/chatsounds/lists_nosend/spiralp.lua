42149fefb42193fcc806e0aee9bceeb7604d6af1
Mikeai <mgamkrel@gmail.com>
2016-12-09 19:35:58 +0200

    Fix thing
    >mfw they forget to run the processor

--- lua/chatsounds/lists_nosend/spiralp.lua
+++ lua/chatsounds/lists_nosend/spiralp.lua
@@ -1,3 +1,4 @@
 c.StartList("spiralp")
 L["go and smell the roses"]={{path="chatsounds/autoadd/spiralp/go and smell the roses/1.ogg",length=2.2474829931972788},{path="chatsounds/autoadd/spiralp/go and smell the roses/2.ogg",length=2.1003628117913831},{path="chatsounds/autoadd/spiralp/go and smell the roses/3.ogg",length=1.8884126984126983},{path="chatsounds/autoadd/spiralp/go and smell the roses/4.ogg",length=2.060045351473923}}
+L["i like turtles"]={{path="chatsounds/autoadd/spiralp/i like turtles.ogg",length=0.94684807256235826}}
 c.EndList()
