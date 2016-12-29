5b99cd84dc7693369fb560b9225aaa76cccd257c
KabusIblis <kabusiblis@gmail.com>
2016-12-29 13:02:42 +0000

    lemmie smash
    ben is a hoe

---
+++ lua/chatsounds/lists_nosend/lemmiesmash.lua
@@ -0,0 +1,23 @@
+c.StartList("lemmiesmash")
+L["becky thought my tail was big"]={{path="chatsounds/autoadd/lemmiesmash/becky thought my tail was big.ogg",length=2.1671201814058958}}
+L["becky used to let me smash"]={{path="chatsounds/autoadd/lemmiesmash/becky used to let me smash.ogg",length=2.1671201814058958}}
+L["ben is a hoe"]={{path="chatsounds/autoadd/lemmiesmash/ben is a hoe.ogg",length=1.1306802721088436}}
+L["blue and yellow no"]={{path="chatsounds/autoadd/lemmiesmash/blue and yellow no.ogg",length=4.2400226757369612}}
+L["but becky is smashing ben"]={{path="chatsounds/autoadd/lemmiesmash/but becky is smashing ben.ogg",length=1.7431065759637188}}
+L["heck"]={{path="chatsounds/autoadd/lemmiesmash/heck.ogg",length=0.75376417233560089}}
+L["hey gurl you want some tail flip ugh flip"]={{path="chatsounds/autoadd/lemmiesmash/hey gurl you want some tail flip ugh flip.ogg",length=5.1351473922902491}}
+L["i like sticks"]={{path="chatsounds/autoadd/lemmiesmash/i like sticks.ogg",length=3.1564625850340136}}
+L["lemmie smash"]={{path="chatsounds/autoadd/lemmiesmash/lemmie smash.ogg",length=1.6017913832199546}}
+L["no ron go find becky"]={{path="chatsounds/autoadd/lemmiesmash/no ron go find becky.ogg",length=2.920907029478458}}
+L["no ron i dont want some fuk i got u blue"]={{path="chatsounds/autoadd/lemmiesmash/no ron i dont want some fuk i got u blue.ogg",length=2.6382312925170068}}
+L["no wait lemmie smash"]={{path="chatsounds/autoadd/lemmiesmash/no wait lemmie smash.ogg",length=1.8844671201814058}}
+L["pplease"]={{path="chatsounds/autoadd/lemmiesmash/pplease.ogg",length=1.0364399092970522}}
+L["rom im leaving"]={{path="chatsounds/autoadd/lemmiesmash/rom im leaving.ogg",length=2.3555782312925171}}
+L["ron your tail is small"]={{path="chatsounds/autoadd/lemmiesmash/ron your tail is small.ogg",length=3.9102494331065758}}
+L["swiggity swooty"]={{path="chatsounds/autoadd/lemmiesmash/swiggity swooty.ogg",length=1.5546712018140589}}
+L["this is a nice stick"]={{path="chatsounds/autoadd/lemmiesmash/this is a nice stick.ogg",length=2.7324716553287982}}
+L["what has my life come to"]={{path="chatsounds/autoadd/lemmiesmash/what has my life come to.ogg",length=1.8844671201814058}}
+L["wut"]={{path="chatsounds/autoadd/lemmiesmash/wut.ogg",length=0.61244897959183675}}
+L["you want a yellow she doesnt want yellow"]={{path="chatsounds/autoadd/lemmiesmash/you want a yellow she doesnt want yellow.ogg",length=4.2400226757369612}}
+L["you want sum fuk"]={{path="chatsounds/autoadd/lemmiesmash/you want sum fuk.ogg",length=1.7902267573696145}}
+c.EndList()
