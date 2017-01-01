4762846f0c472e5aeef00fa8320842eb5f60e346
Datamats <datamatsfilms@gmail.com>
2017-01-01 12:14:25 +0100

    cookies!!!

---
+++ lua/chatsounds/lists_nosend/cookie_monster.lua
@@ -0,0 +1,25 @@
+c.StartList("cookie_monster")
+L["10 cookies yeah"]={{path="chatsounds/autoadd/cookie_monster/10 cookies yeah.ogg",length=4.4582312925170067}}
+L["ahm nomnomnom"]={{path="chatsounds/autoadd/cookie_monster/ahm nomnomnom.ogg",length=3.3436734693877552}}
+L["can me uh have a cookie"]={{path="chatsounds/autoadd/cookie_monster/can me uh have a cookie.ogg",length=3.5294331065759637}}
+L["cookie has question"]={{path="chatsounds/autoadd/cookie_monster/cookie has question.ogg",length=2.1362358276643989}}
+L["cookie monster speech"]={{path="chatsounds/autoadd/cookie_monster/cookie monster speech.ogg",length=33.994013605442177}}
+L["cookies yeah"]={{path="chatsounds/autoadd/cookie_monster/cookies yeah.ogg",length=2.6470748299319729}}
+L["do you have cookies"]={{path="chatsounds/autoadd/cookie_monster/do you have cookies.ogg",length=3.4365532879818592}}
+L["does a toaster eat cookie"]={{path="chatsounds/autoadd/cookie_monster/does a toaster eat cookie.ogg",length=3.5294331065759637}}
+L["gasp cookies"]={{path="chatsounds/autoadd/cookie_monster/gasp cookies.ogg",length=2.9257142857142857}}
+L["give that best friend a cookie"]={{path="chatsounds/autoadd/cookie_monster/give that best friend a cookie.ogg",length=3.0650340136054424}}
+L["guess what me thinking about today"]={{path="chatsounds/autoadd/cookie_monster/guess what me thinking about today.ogg",length=3.4829931972789114}}
+L["hello there"]={{path="chatsounds/autoadd/cookie_monster/hello there.ogg",length=1.1609977324263039}}
+L["is it cookie"]={{path="chatsounds/autoadd/cookie_monster/is it cookie.ogg",length=1.0216780045351475}}
+L["its a cookie"]={{path="chatsounds/autoadd/cookie_monster/its a cookie.ogg",length=0.97523809523809524}}
+L["m i k e"]={{path="chatsounds/autoadd/cookie_monster/m i k e.ogg",length=2.9721541950113379}}
+L["me going to ask most important question of all"]={{path="chatsounds/autoadd/cookie_monster/me going to ask most important question of all.ogg",length=3.3901133786848074}}
+L["me have question for you"]={{path="chatsounds/autoadd/cookie_monster/me have question for you.ogg",length=1.5789569160997732}}
+L["meee cookie monster"]={{path="chatsounds/autoadd/cookie_monster/meee cookie monster.ogg",length=1.486077097505669}}
+L["mike"]={{path="chatsounds/autoadd/cookie_monster/mike.ogg",length=0.88235827664399091}}
+L["noooo"]={{path="chatsounds/autoadd/cookie_monster/noooo.ogg",length=0.88235827664399091}}
+L["something to eat that is round and orange"]={{path="chatsounds/autoadd/cookie_monster/something to eat that is round and orange.ogg",length=4.0867120181405898}}
+L["why you why"]={{path="chatsounds/autoadd/cookie_monster/why you why.ogg",length=1.0681179138321995}}
+L["you know coookieess"]={{path="chatsounds/autoadd/cookie_monster/you know coookieess.ogg",length=2.6935147392290251}}
+c.EndList()
