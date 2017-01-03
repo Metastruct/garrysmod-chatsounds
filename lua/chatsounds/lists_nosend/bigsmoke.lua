7623c9664da714d683c922a62e3a81183973bb54
Mikeai <mgamkrel@gmail.com>
2017-01-03 04:55:56 +0200

    fat boy
    extra dip

---
+++ lua/chatsounds/lists_nosend/bigsmoke.lua
@@ -0,0 +1,16 @@
+c.StartList("bigsmoke")
+L["a number nine large"]={{path="chatsounds/autoadd/bigsmoke/a number nine large.ogg",length=2.2446712018140591}}
+L["a number seven"]={{path="chatsounds/autoadd/bigsmoke/a number seven.ogg",length=1.7778231292517006}}
+L["a number six with extra dip"]={{path="chatsounds/autoadd/bigsmoke/a number six with extra dip.ogg",length=2.5196825396825395}}
+L["and a large soda"]={{path="chatsounds/autoadd/bigsmoke/and a large soda.ogg",length=1.9568934240362812}}
+L["can i take your order please"]={{path="chatsounds/autoadd/bigsmoke/can i take your order please.ogg",length=1.4772789115646259}}
+L["extra dip"]={{path="chatsounds/autoadd/bigsmoke/extra dip.ogg",length=1.2086848072562357}}
+L["fat boy"]={{path="chatsounds/autoadd/bigsmoke/fat boy.ogg",length=1.5092290249433107}}
+L["get me a number nine just like his"]={{path="chatsounds/autoadd/bigsmoke/get me a number nine just like his.ogg",length=2.2382766439909298}}
+L["hey ill take a number nine"]={{path="chatsounds/autoadd/bigsmoke/hey ill take a number nine.ogg",length=1.7074829931972788}}
+L["ill have two number nines"]={{path="chatsounds/autoadd/bigsmoke/ill have two number nines.ogg",length=2.5452380952380951}}
+L["lemme get a number six with extra dip"]={{path="chatsounds/autoadd/bigsmoke/lemme get a number six with extra dip.ogg",length=2.3469841269841272}}
+L["one with cheese"]={{path="chatsounds/autoadd/bigsmoke/one with cheese.ogg",length=1.4197052154195011}}
+L["two number fourty fives"]={{path="chatsounds/autoadd/bigsmoke/two number fourty fives.ogg",length=2.0975736961451248}}
+L["youve gotta eat to keep your strength up man"]={{path="chatsounds/autoadd/bigsmoke/youve gotta eat to keep your strength up man.ogg",length=2.0400226757369615}}
+c.EndList()
