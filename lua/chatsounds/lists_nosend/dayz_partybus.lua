c5a51f837e963685ca9d0239913b2173b33f2443
!Avast AntiPony 9445 <kristianzxz41@gmail.com>
2017-01-07 00:47:22 +0100

    You saw the Vinesauce party bus being commited
    I didn't

---
+++ lua/chatsounds/lists_nosend/dayz_partybus.lua
@@ -0,0 +1,41 @@
+c.StartList("dayz_partybus")
+L["aaaaaaaaaaaaaaaay"]={{path="chatsounds/autoadd/dayz_partybus/aaaaaaaaaaaaaaaay.ogg",length=4.8945804988662127}}
+L["everything you say is like verbal diarrhea"]={{path="chatsounds/autoadd/dayz_partybus/everything you say is like verbal diarrhea.ogg",length=2.1354421768707481}}
+L["everything you utter is just like pft pft pft pft pffft pft pft pft pft pft pft pft pft"]={{path="chatsounds/autoadd/dayz_partybus/everything you utter is just like pft pft pft pft pffft pft pft pft pft pft pft pft pft.ogg",length=4.0442403628117916}}
+L["get the fuck over here"]={{path="chatsounds/autoadd/dayz_partybus/get the fuck over here.ogg",length=0.95528344671201815}}
+L["hello zombie"]={{path="chatsounds/autoadd/dayz_partybus/hello zombie.ogg",length=0.74845804988662135}}
+L["i gotta pee"]={{path="chatsounds/autoadd/dayz_partybus/i gotta pee.ogg",length=0.69866213151927437}}
+L["i havent heard about japanese busses what are you talking about"]={{path="chatsounds/autoadd/dayz_partybus/i havent heard about japanese busses what are you talking about.ogg",length=2.7990476190476192}}
+L["i saved his fucking anus from being annihilated"]={{path="chatsounds/autoadd/dayz_partybus/i saved his fucking anus from being annihilated.ogg",length=3.4576190476190476}}
+L["i see you in the distance"]={{path="chatsounds/autoadd/dayz_partybus/i see you in the distance.ogg",length=1.4632199546485261}}
+L["japanese busses"]={{path="chatsounds/autoadd/dayz_partybus/japanese busses.ogg",length=1.3946938775510205}}
+L["joel"]={{path="chatsounds/autoadd/dayz_partybus/joel.ogg",length=0.64925170068027216}}
+L["joellaugh"]={{path="chatsounds/autoadd/dayz_partybus/joellaugh/1.ogg",length=1.4008616780045351},{path="chatsounds/autoadd/dayz_partybus/joellaugh/2.ogg",length=2.0177097505668935},{path="chatsounds/autoadd/dayz_partybus/joellaugh/3.ogg",length=0.69650793650793652}}
+L["joelscream"]={{path="chatsounds/autoadd/dayz_partybus/joelscream.ogg",length=0.98070294784580503}}
+L["ku pa paa"]={{path="chatsounds/autoadd/dayz_partybus/ku pa paa.ogg",length=0.75145124716553291}}
+L["later fags"]={{path="chatsounds/autoadd/dayz_partybus/later fags.ogg",length=1.4244444444444444}}
+L["no come on dont be lame"]={{path="chatsounds/autoadd/dayz_partybus/no come on dont be lame.ogg",length=1.6360544217687074}}
+L["no no no no no no no come on come on guys"]={{path="chatsounds/autoadd/dayz_partybus/no no no no no no no come on come on guys.ogg",length=1.8155782312925171}}
+L["no no no wait for me man"]={{path="chatsounds/autoadd/dayz_partybus/no no no wait for me man.ogg",length=1.1298185941043084}}
+L["no nuh no i uh"]={{path="chatsounds/autoadd/dayz_partybus/no nuh no i uh.ogg",length=0.86188208616780049}}
+L["nothing is substantial of value or anything"]={{path="chatsounds/autoadd/dayz_partybus/nothing is substantial of value or anything.ogg",length=4.5572335600907028}}
+L["ohey ohey"]={{path="chatsounds/autoadd/dayz_partybus/ohey ohey.ogg",length=2.1719274376417235}}
+L["parteh bus"]={{path="chatsounds/autoadd/dayz_partybus/parteh bus.ogg",length=0.82895691609977329}}
+L["party bus party bus"]={{path="chatsounds/autoadd/dayz_partybus/party bus party bus/1.ogg",length=1.5473696145124716},{path="chatsounds/autoadd/dayz_partybus/party bus party bus/2.ogg",length=1.4323582766439908}}
+L["party bus party bus woo woo woo"]={{path="chatsounds/autoadd/dayz_partybus/party bus party bus woo woo woo.ogg",length=2.8832879818594104}}
+L["reeeeeeeev"]={{path="chatsounds/autoadd/dayz_partybus/reeeeeeeev.ogg",length=2.1462811791383221}}
+L["rev"]={{path="chatsounds/autoadd/dayz_partybus/rev/1.ogg",length=0.45396825396825397},{path="chatsounds/autoadd/dayz_partybus/rev/2.ogg",length=0.3776643990929705}}
+L["run"]={{path="chatsounds/autoadd/dayz_partybus/run.ogg",length=0.39448979591836736}}
+L["tedada dot dot"]={{path="chatsounds/autoadd/dayz_partybus/tedada dot dot.ogg",length=0.97043083900226756}}
+L["wat"]={{path="chatsounds/autoadd/dayz_partybus/wat.ogg",length=0.63714285714285712}}
+L["we dont have time for this"]={{path="chatsounds/autoadd/dayz_partybus/we dont have time for this.ogg",length=1.3836281179138321}}
+L["we salute the goat"]={{path="chatsounds/autoadd/dayz_partybus/we salute the goat.ogg",length=1.3878911564625851}}
+L["why"]={{path="chatsounds/autoadd/dayz_partybus/why.ogg",length=0.53047619047619043}}
+L["woo"]={{path="chatsounds/autoadd/dayz_partybus/woo.ogg",length=0.76458049886621315}}
+L["yeah im good just bleeding its good oh god"]={{path="chatsounds/autoadd/dayz_partybus/yeah im good just bleeding its good oh god.ogg",length=4.0274376417233562}}
+L["yeah ok i got urinary tract infection problems ok"]={{path="chatsounds/autoadd/dayz_partybus/yeah ok i got urinary tract infection problems ok.ogg",length=4.3165986394557825}}
+L["yeah thats where you belong"]={{path="chatsounds/autoadd/dayz_partybus/yeah thats where you belong.ogg",length=1.1062585034013606}}
+L["you heard about those japanese busses man"]={{path="chatsounds/autoadd/dayz_partybus/you heard about those japanese busses man.ogg",length=2.531451247165533}}
+L["you want some tampons"]={{path="chatsounds/autoadd/dayz_partybus/you want some tampons.ogg",length=1.60859410430839}}
+L["youre peeing blood"]={{path="chatsounds/autoadd/dayz_partybus/youre peeing blood.ogg",length=1.2079138321995464}}
+c.EndList()
---
+++ lua/chatsounds/lists_nosend/kliksphilip.lua
@@ -0,0 +1,3 @@
+c.StartList("kliksphilip")
+L["i didnt"]={{path="chatsounds/autoadd/kliksphilip/i didnt.ogg",length=0.60900226757369613}}
+c.EndList()
