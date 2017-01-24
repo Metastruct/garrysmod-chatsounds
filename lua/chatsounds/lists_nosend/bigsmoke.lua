85dae70364319be162b4adcb79d56fda2d8e653d
Mikeai <mgamkrel@gmail.com>
2017-01-24 18:31:27 +0200

    you picked the wrong house fool
    oooooooo

--- lua/chatsounds/lists_nosend/bigsmoke.lua
+++ lua/chatsounds/lists_nosend/bigsmoke.lua
@@ -3,14 +3,24 @@ L["a number nine large"]={{path="chatsounds/autoadd/bigsmoke/a number nine large
 L["a number seven"]={{path="chatsounds/autoadd/bigsmoke/a number seven.ogg",length=1.7778231292517006}}
 L["a number six with extra dip"]={{path="chatsounds/autoadd/bigsmoke/a number six with extra dip.ogg",length=2.5196825396825395}}
 L["and a large soda"]={{path="chatsounds/autoadd/bigsmoke/and a large soda.ogg",length=1.9568934240362812}}
+L["big smoke"]={{path="chatsounds/autoadd/bigsmoke/big smoke.ogg",length=0.77664399092970526}}
 L["can i take your order please"]={{path="chatsounds/autoadd/bigsmoke/can i take your order please.ogg",length=1.4772789115646259}}
+L["chill chill"]={{path="chatsounds/autoadd/bigsmoke/chill chill.ogg",length=1.1191383219954649}}
+L["cj"]={{path="chatsounds/autoadd/bigsmoke/cj.ogg",length=1.0371428571428571}}
 L["extra dip"]={{path="chatsounds/autoadd/bigsmoke/extra dip.ogg",length=1.2086848072562357}}
 L["fat boy"]={{path="chatsounds/autoadd/bigsmoke/fat boy.ogg",length=1.5092290249433107}}
 L["get me a number nine just like his"]={{path="chatsounds/autoadd/bigsmoke/get me a number nine just like his.ogg",length=2.2382766439909298}}
 L["hey ill take a number nine"]={{path="chatsounds/autoadd/bigsmoke/hey ill take a number nine.ogg",length=1.7074829931972788}}
 L["ill have two number nines"]={{path="chatsounds/autoadd/bigsmoke/ill have two number nines.ogg",length=2.5452380952380951}}
+L["its me carl"]={{path="chatsounds/autoadd/bigsmoke/its me carl.ogg",length=0.99371882086167795}}
 L["lemme get a number six with extra dip"]={{path="chatsounds/autoadd/bigsmoke/lemme get a number six with extra dip.ogg",length=2.3469841269841272}}
+L["make us cry"]={{path="chatsounds/autoadd/bigsmoke/make us cry.ogg",length=1.2493877551020409}}
 L["one with cheese"]={{path="chatsounds/autoadd/bigsmoke/one with cheese.ogg",length=1.4197052154195011}}
+L["oooo"]={{path="chatsounds/autoadd/bigsmoke/oooo.ogg",length=1.3603401360544218}}
+L["same things make us laugh"]={{path="chatsounds/autoadd/bigsmoke/same things make us laugh.ogg",length=1.9151020408163266}}
 L["two number fourty fives"]={{path="chatsounds/autoadd/bigsmoke/two number fourty fives.ogg",length=2.0975736961451248}}
+L["what fuckin book"]={{path="chatsounds/autoadd/bigsmoke/what fuckin book.ogg",length=1.234920634920635}}
+L["you okay man"]={{path="chatsounds/autoadd/bigsmoke/you okay man.ogg",length=0.9454875283446712}}
+L["you picked the wrong house fool"]={{path="chatsounds/autoadd/bigsmoke/you picked the wrong house fool.ogg",length=1.6594331065759638}}
 L["youve gotta eat to keep your strength up man"]={{path="chatsounds/autoadd/bigsmoke/youve gotta eat to keep your strength up man.ogg",length=2.0400226757369615}}
 c.EndList()
