644a3842ce9fb2cdfd1608b94aaf343cfbd00d68
Mikeai <mgamkrel@gmail.com>
2017-02-02 15:49:55 +0200

    more smash
    nice guys finish last

--- lua/chatsounds/lists_nosend/u want sum fuk.lua
+++ lua/chatsounds/lists_nosend/u want sum fuk.lua
@@ -1,4 +1,6 @@
 c.StartList("u want sum fuk")
+L["are you peckin kidding me"]={{path="chatsounds/autoadd/u want sum fuk/are you peckin kidding me.ogg",length=1.1280272108843536}}
+L["aw shit"]={{path="chatsounds/autoadd/u want sum fuk/aw shit.ogg",length=0.61335600907029475}}
 L["becky thought my tail was big"]={{path="chatsounds/autoadd/u want sum fuk/becky thought my tail was big.ogg",length=1.8595691609977325}}
 L["becky used to let me smash"]={{path="chatsounds/autoadd/u want sum fuk/becky used to let me smash.ogg",length=1.9249433106575964}}
 L["ben is a hoe"]={{path="chatsounds/autoadd/u want sum fuk/ben is a hoe.ogg",length=0.73773242630385483}}
@@ -7,29 +9,52 @@ L["blue and yellow no"]={{path="chatsounds/autoadd/u want sum fuk/blue and yello
 L["but becky is smashin ben"]={{path="chatsounds/autoadd/u want sum fuk/but becky is smashin ben.ogg",length=1.6213378684807256}}
 L["flick"]={{path="chatsounds/autoadd/u want sum fuk/flick.ogg",length=1.7774149659863945}}
 L["fuck this nest fuck ben"]={{path="chatsounds/autoadd/u want sum fuk/fuck this nest fuck ben.ogg",length=3.2179138321995464}}
+L["get that beak ready becky"]={{path="chatsounds/autoadd/u want sum fuk/get that beak ready becky.ogg",length=1.6426757369614513}}
 L["go find becky"]={{path="chatsounds/autoadd/u want sum fuk/go find becky.ogg",length=1.4646712018140589}}
+L["guys im learning to jump for becky"]={{path="chatsounds/autoadd/u want sum fuk/guys im learning to jump for becky.ogg",length=2.7495691609977326}}
+L["he had a hat"]={{path="chatsounds/autoadd/u want sum fuk/he had a hat/hat1.ogg",length=1.2408163265306122},{path="chatsounds/autoadd/u want sum fuk/he had a hat/hat2.ogg",length=2.6579138321995464}}
 L["heck"]={{path="chatsounds/autoadd/u want sum fuk/heck.ogg",length=0.24281179138321995}}
+L["help me"]={{path="chatsounds/autoadd/u want sum fuk/help me.ogg",length=0.56401360544217682}}
 L["hey girl you want sum tail"]={{path="chatsounds/autoadd/u want sum fuk/hey girl you want sum tail.ogg",length=3.2916099773242631}}
+L["i cant walk up sophie"]={{path="chatsounds/autoadd/u want sum fuk/i cant walk up sophie.ogg",length=1.607437641723356}}
 L["i got ya blue"]={{path="chatsounds/autoadd/u want sum fuk/i got ya blue.ogg",length=1.0714058956916099}}
 L["i like sticks"]={{path="chatsounds/autoadd/u want sum fuk/i like sticks.ogg",length=2.8550113378684809}}
 L["i need you becky becky lemme smash"]={{path="chatsounds/autoadd/u want sum fuk/i need you becky becky lemme smash.ogg",length=3.2983446712018138}}
+L["im comin for you becky"]={{path="chatsounds/autoadd/u want sum fuk/im comin for you becky.ogg",length=1.51578231292517}}
+L["im not like the other guys"]={{path="chatsounds/autoadd/u want sum fuk/im not like the other guys.ogg",length=1.7977777777777777}}
 L["ima get that bitch a stick"]={{path="chatsounds/autoadd/u want sum fuk/ima get that bitch a stick.ogg",length=1.9462131519274377}}
+L["jumpjumpjump"]={{path="chatsounds/autoadd/u want sum fuk/jumpjumpjump.ogg",length=4.4098639455782314}}
+L["just go with it ron"]={{path="chatsounds/autoadd/u want sum fuk/just go with it ron.ogg",length=1.8189342403628117}}
+L["just walk but like up"]={{path="chatsounds/autoadd/u want sum fuk/just walk but like up.ogg",length=2.4675510204081634}}
 L["lemme smash"]={{path="chatsounds/autoadd/u want sum fuk/lemme smash.ogg",length=1.5358730158730158}}
 L["lemme smash please"]={{path="chatsounds/autoadd/u want sum fuk/lemme smash please.ogg",length=3.201814058956916}}
+L["ma lady"]={{path="chatsounds/autoadd/u want sum fuk/ma lady.ogg",length=1.0434240362811791}}
 L["need stick got stick becky i got stick lemme smash"]={{path="chatsounds/autoadd/u want sum fuk/need stick got stick becky i got stick lemme smash.ogg",length=2.7603854875283447}}
+L["nice guys finish last"]={{path="chatsounds/autoadd/u want sum fuk/nice guys finish last.ogg",length=1.9740362811791383}}
 L["no ron"]={{path="chatsounds/autoadd/u want sum fuk/no ron.ogg",length=1.3321768707482993}}
 L["no ron i dont want sum fu"]={{path="chatsounds/autoadd/u want sum fuk/no ron i dont want sum fu.ogg",length=1.5916099773242631}}
 L["no wait lemme smash"]={{path="chatsounds/autoadd/u want sum fuk/no wait lemme smash.ogg",length=1.8644217687074829}}
+L["oh my bird hes retarded"]={{path="chatsounds/autoadd/u want sum fuk/oh my bird hes retarded.ogg",length=1.7343310657596371}}
 L["peck peck"]={{path="chatsounds/autoadd/u want sum fuk/peck peck.ogg",length=1.6736734693877551}}
+L["pretty good right guys"]={{path="chatsounds/autoadd/u want sum fuk/pretty good right guys.ogg",length=1.6215419501133788}}
 L["ron im leaving"]={{path="chatsounds/autoadd/u want sum fuk/ron im leaving.ogg",length=1.9678684807256235}}
 L["ron ur tail is smol"]={{path="chatsounds/autoadd/u want sum fuk/ron ur tail is smol.ogg",length=1.8540589569160997}}
 L["she doesnt want yellow"]={{path="chatsounds/autoadd/u want sum fuk/she doesnt want yellow.ogg",length=1.7033333333333334}}
+L["sophie"]={{path="chatsounds/autoadd/u want sum fuk/sophie.ogg",length=0.9376643990929705}}
+L["sspiders"]={{path="chatsounds/autoadd/u want sum fuk/sspiders.ogg",length=0.83192743764172339}}
 L["stick stick stick stick stick stick stick stick"]={{path="chatsounds/autoadd/u want sum fuk/stick stick stick stick stick stick stick stick.ogg",length=5.0937188208616782}}
+L["susan watch"]={{path="chatsounds/autoadd/u want sum fuk/susan watch.ogg",length=0.85571428571428576}}
+L["swag"]={{path="chatsounds/autoadd/u want sum fuk/swag.ogg",length=0.76845804988662136}}
 L["swigity swooty"]={{path="chatsounds/autoadd/u want sum fuk/swigity swooty.ogg",length=1.8247619047619048}}
+L["thats not how it works"]={{path="chatsounds/autoadd/u want sum fuk/thats not how it works.ogg",length=1.5792290249433107}}
+L["theyre everywhere"]={{path="chatsounds/autoadd/u want sum fuk/theyre everywhere.ogg",length=0.84954648526077092}}
 L["this is a nice stick"]={{path="chatsounds/autoadd/u want sum fuk/this is a nice stick.ogg",length=3.0593650793650795}}
 L["u wan sum fuk"]={{path="chatsounds/autoadd/u want sum fuk/u wan sum fuk.ogg",length=1.7959863945578232}}
+L["wait wait wait wait wait"]={{path="chatsounds/autoadd/u want sum fuk/wait wait wait wait wait.ogg",length=2.4076190476190478}}
+L["what the fuck susan are you seeing this"]={{path="chatsounds/autoadd/u want sum fuk/what the fuck susan are you seeing this.ogg",length=2.2560544217687073}}
 L["whats my life come to"]={{path="chatsounds/autoadd/u want sum fuk/whats my life come to.ogg",length=1.6460770975056689}}
 L["whut"]={{path="chatsounds/autoadd/u want sum fuk/whut/1.ogg",length=1.9616780045351474},{path="chatsounds/autoadd/u want sum fuk/whut/2.ogg",length=0.95918367346938771}}
 L["wrong stick"]={{path="chatsounds/autoadd/u want sum fuk/wrong stick.ogg",length=1.0763718820861679}}
+L["you got this ronald"]={{path="chatsounds/autoadd/u want sum fuk/you got this ronald.ogg",length=1.2408163265306122}}
 L["you want yellow"]={{path="chatsounds/autoadd/u want sum fuk/you want yellow.ogg",length=2.3020408163265307}}
 c.EndList()
