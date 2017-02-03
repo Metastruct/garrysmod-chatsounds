bb5a9aa7b0e41b8e11526eea4074ee83e25eb767
DengoanXI <natanielxi@hotmail.no>
2017-02-03 19:07:45 +0100

    so much dust and cobwebs up in this bitch

--- lua/chatsounds/lists_nosend/dengoan.lua
+++ lua/chatsounds/lists_nosend/dengoan.lua
@@ -1,5 +1,5 @@
 c.StartList("dengoan")
-L["angelintro"]={{path="chatsounds/autoadd/dengoan/angelintro.ogg",length=16.822857142857142}}
+L["angelintro"]={{path="chatsounds/autoadd/dengoan/angelintro.ogg",length=18.17596371882086}}
 L["ari ari ari ari ari"]={{path="chatsounds/autoadd/dengoan/ari ari ari ari ari.ogg",length=5.6985487528344674}}
 L["arrigato gyro"]={{path="chatsounds/autoadd/dengoan/arrigato gyro.ogg",length=2.8389795918367349}}
 L["arriva derci"]={{path="chatsounds/autoadd/dengoan/arriva derci.ogg",length=1.5749206349206348}}
@@ -8,13 +8,14 @@ L["chie im gonna get so fat"]={{path="chatsounds/autoadd/dengoan/chie im gonna g
 L["crawling in my skin"]={{path="chatsounds/autoadd/dengoan/crawling in my skin/1.ogg",length=5.3872108843537418},{path="chatsounds/autoadd/dengoan/crawling in my skin/2.ogg",length=20.485646258503401}}
 L["destroyed"]={{path="chatsounds/autoadd/dengoan/destroyed.ogg",length=2.0695011337868481}}
 L["devil may cry"]={{path="chatsounds/autoadd/dengoan/devil may cry.ogg",length=2.3999999999999999}}
+L["do that"]={{path="chatsounds/autoadd/dengoan/do that.ogg",length=0.9672562358276644}}
+L["do we just lay down and fuck"]={{path="chatsounds/autoadd/dengoan/do we just lay down and fuck.ogg",length=1.7195691609977324}}
 L["dominatrix banana head"]={{path="chatsounds/autoadd/dengoan/dominatrix banana head.ogg",length=3.5004081632653059}}
 L["dont touch me"]={{path="chatsounds/autoadd/dengoan/dont touch me.ogg",length=2.5413151927437641}}
 L["dorarararara"]={{path="chatsounds/autoadd/dengoan/dorarararara.ogg",length=5.9820408163265304}}
 L["fuck off"]={{path="chatsounds/autoadd/dengoan/fuck off.ogg",length=0.83455782312925175}}
 L["garuda"]={{path="chatsounds/autoadd/dengoan/garuda.ogg",length=29.117596371882087}}
 L["get me some sausages"]={{path="chatsounds/autoadd/dengoan/get me some sausages.ogg",length=3.3697959183673469}}
-L["ggversus"]={{path="chatsounds/autoadd/dengoan/ggversus.ogg",length=8.0176643990929701}}
 L["go fuck yourself"]={{path="chatsounds/autoadd/dengoan/go fuck yourself.ogg",length=1.1591836734693877}}
 L["go kill yourself"]={{path="chatsounds/autoadd/dengoan/go kill yourself.ogg",length=1.506235827664399}}
 L["hadouken"]={{path="chatsounds/autoadd/dengoan/hadouken.ogg",length=1.0766439909297052}}
@@ -22,6 +23,8 @@ L["heaven or hell"]={{path="chatsounds/autoadd/dengoan/heaven or hell.ogg",lengt
 L["how about go fuck yourself"]={{path="chatsounds/autoadd/dengoan/how about go fuck yourself.ogg",length=1.7836734693877552}}
 L["how dare you youre not me"]={{path="chatsounds/autoadd/dengoan/how dare you youre not me.ogg",length=3.0039909297052154}}
 L["i dunno i dont speak skanky whore"]={{path="chatsounds/autoadd/dengoan/i dunno i dont speak skanky whore.ogg",length=2.7167346938775512}}
+L["i feel like a fucking celebrity in this town"]={{path="chatsounds/autoadd/dengoan/i feel like a fucking celebrity in this town.ogg",length=3.5129478458049888}}
+L["i should do you up the ass"]={{path="chatsounds/autoadd/dengoan/i should do you up the ass.ogg",length=1.6926984126984126}}
 L["i want you"]={{path="chatsounds/autoadd/dengoan/i want you.ogg",length=14.538095238095238}}
 L["im wearing a kimono because im filling in for my mom at the in we run"]={{path="chatsounds/autoadd/dengoan/im wearing a kimono because im filling in for my mom at the in we run.ogg",length=4.0228571428571431}}
 L["it all comes tumbling down"]={{path="chatsounds/autoadd/dengoan/it all comes tumbling down.ogg",length=6.6541043083900231}}
@@ -29,19 +32,17 @@ L["its not gay"]={{path="chatsounds/autoadd/dengoan/its not gay.ogg",length=0.78
 L["its symbolic"]={{path="chatsounds/autoadd/dengoan/its symbolic.ogg",length=3.1085714285714285}}
 L["its time to split"]={{path="chatsounds/autoadd/dengoan/its time to split.ogg",length=1.4682539682539681}}
 L["knockout"]={{path="chatsounds/autoadd/dengoan/knockout.ogg",length=1.289546485260771}}
-L["let it out"]={{path="chatsounds/autoadd/dengoan/let it out.ogg",length=1.8695238095238096}}
 L["lets get high"]={{path="chatsounds/autoadd/dengoan/lets get high.ogg",length=0.98798185941043082}}
 L["lets rock"]={{path="chatsounds/autoadd/dengoan/lets rock.ogg",length=1.2974829931972789}}
 L["listen up bitch"]={{path="chatsounds/autoadd/dengoan/listen up bitch.ogg",length=1.086938775510204}}
 L["meeeee"]={{path="chatsounds/autoadd/dengoan/meeeee.ogg",length=3.5525623582766439}}
 L["messatsu"]={{path="chatsounds/autoadd/dengoan/messatsu.ogg",length=1.3699773242630386}}
 L["muda muda muda muda"]={{path="chatsounds/autoadd/dengoan/muda muda muda muda/muda muda muda muda1.ogg",length=6.8830385487528343},{path="chatsounds/autoadd/dengoan/muda muda muda muda/muda muda muda muda2.ogg",length=6.2393877551020411}}
+L["no"]={{path="chatsounds/autoadd/dengoan/no.ogg",length=0.42988662131519273}}
 L["no means no"]={{path="chatsounds/autoadd/dengoan/no means no.ogg",length=1.5383446712018141}}
 L["oh god"]={{path="chatsounds/autoadd/dengoan/oh god.ogg",length=1.6265759637188208}}
 L["okai"]={{path="chatsounds/autoadd/dengoan/okai.ogg",length=0.5061224489795918}}
 L["oraoraoraora"]={{path="chatsounds/autoadd/dengoan/oraoraoraora/oraoraoraora1.ogg",length=6.5110204081632652},{path="chatsounds/autoadd/dengoan/oraoraoraora/oraoraoraora2.ogg",length=3.2005895691609978},{path="chatsounds/autoadd/dengoan/oraoraoraora/oraoraoraora3.ogg",length=7.6643990929705215},{path="chatsounds/autoadd/dengoan/oraoraoraora/oraoraoraora4.ogg",length=7.1527664399092972}}
-L["p4afinisher"]={{path="chatsounds/autoadd/dengoan/p4afinisher.ogg",length=25.17170068027211}}
-L["poemforthesoulsofeverybody"]={{path="chatsounds/autoadd/dengoan/poemforthesoulsofeverybody.ogg",length=28.459773242630387}}
 L["ruru"]={{path="chatsounds/autoadd/dengoan/ruru.ogg",length=0.83154195011337873}}
 L["ruru2"]={{path="chatsounds/autoadd/dengoan/ruru2.ogg",length=1.0636281179138323}}
 L["ruru3"]={{path="chatsounds/autoadd/dengoan/ruru3.ogg",length=0.78276643990929706}}
@@ -54,16 +55,16 @@ L["strange isnt it"]={{path="chatsounds/autoadd/dengoan/strange isnt it.ogg",len
 L["tatsumaki senpuukyaku"]={{path="chatsounds/autoadd/dengoan/tatsumaki senpuukyaku.ogg",length=1.3799773242630387}}
 L["tentacles"]={{path="chatsounds/autoadd/dengoan/tentacles.ogg",length=0.82036281179138326}}
 L["that was gay"]={{path="chatsounds/autoadd/dengoan/that was gay.ogg",length=0.62390022675736967}}
-L["thou art to stick your hand in the tv do it"]={{path="chatsounds/autoadd/dengoan/thou art to stick your hand in the tv do it.ogg",length=4.2607029478458047}}
+L["thats right"]={{path="chatsounds/autoadd/dengoan/thats right.ogg",length=0.6045351473922902}}
+L["thou art thee art thou stick your hand in the tv do it do it do it"]={{path="chatsounds/autoadd/dengoan/thou art thee art thou stick your hand in the tv do it do it do it.ogg",length=4.2607029478458047}}
 L["trial of the dragon"]={{path="chatsounds/autoadd/dengoan/trial of the dragon.ogg",length=3.4210657596371883}}
 L["unforgivable"]={{path="chatsounds/autoadd/dengoan/unforgivable.ogg",length=1.0194104308390022}}
 L["uryyy"]={{path="chatsounds/autoadd/dengoan/uryyy.ogg",length=6.6117460317460317}}
-L["victorymvc3"]={{path="chatsounds/autoadd/dengoan/victorymvc3.ogg",length=44.872925170068029}}
-L["victoryumvc3"]={{path="chatsounds/autoadd/dengoan/victoryumvc3.ogg",length=43.941405895691609}}
 L["vola vola vola vola"]={{path="chatsounds/autoadd/dengoan/vola vola vola vola.ogg",length=4.4446485260770974}}
 L["well as much as i value career advice from the slut that killed my father"]={{path="chatsounds/autoadd/dengoan/well as much as i value career advice from the slut that killed my father.ogg",length=4.0312471655328794}}
 L["what are you doing"]={{path="chatsounds/autoadd/dengoan/what are you doing.ogg",length=1.5467120181405896}}
 L["why did it have to be tentacles"]={{path="chatsounds/autoadd/dengoan/why did it have to be tentacles.ogg",length=1.88140589569161}}
+L["you dont do that"]={{path="chatsounds/autoadd/dengoan/you dont do that.ogg",length=1.2090702947845804}}
 L["you guys are jerks"]={{path="chatsounds/autoadd/dengoan/you guys are jerks.ogg",length=5.0416326530612245}}
 L["youre so sexy kawaii"]={{path="chatsounds/autoadd/dengoan/youre so sexy kawaii.ogg",length=2.1016326530612246}}
 c.EndList()
