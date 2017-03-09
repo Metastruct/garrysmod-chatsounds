e25e7a56b3ca9963dd0a5988912c2a5612ab947f
EngineMachiner <mfpc98@gmail.com>
2017-03-09 16:22:31 -0300

    Add files via upload

--- lua/chatsounds/lists_nosend/weeb.lua
+++ lua/chatsounds/lists_nosend/weeb.lua
@@ -1,12 +1,19 @@
 c.StartList("weeb")
 L["better"]={{path="chatsounds/autoadd/weeb/better.ogg",length=0.51659863945578233}}
 L["cheater"]={{path="chatsounds/autoadd/weeb/cheater.ogg",length=0.80818594104308394}}
+L["dont leave me now"]={{path="chatsounds/autoadd/weeb/dont leave me now.ogg",length=1.5052834467120182}}
+L["hsw8bit"]={{path="chatsounds/autoadd/weeb/hsw8bit.ogg",length=15.000090702947846}}
 L["imagination"]={{path="chatsounds/autoadd/weeb/imagination.ogg",length=1.9399773242630385}}
 L["love revolution"]={{path="chatsounds/autoadd/weeb/love revolution.ogg",length=5.685736961451247}}
+L["love revolution remix"]={{path="chatsounds/autoadd/weeb/love revolution remix.ogg",length=7.7492517006802721}}
 L["no no no"]={{path="chatsounds/autoadd/weeb/no no no.ogg",length=3.6078684807256236}}
 L["ponponpon"]={{path="chatsounds/autoadd/weeb/ponponpon.ogg",length=3.7659183673469387}}
 L["sexy boy"]={{path="chatsounds/autoadd/weeb/sexy boy.ogg",length=1.0754195011337868}}
+L["shdb"]={{path="chatsounds/autoadd/weeb/shdb2.ogg",length=9.4134013605442171}}
+L["shdb8bit"]={{path="chatsounds/autoadd/weeb/shdb8bit.ogg",length=9.5434013605442178}}
 L["touch me please"]={{path="chatsounds/autoadd/weeb/touch me please.ogg",length=1.7213378684807257}}
 L["wagamama ki no mama ai no joke"]={{path="chatsounds/autoadd/weeb/wagamama ki no mama ai no joke.ogg",length=3.0870975056689343}}
+L["wowowowow"]={{path="chatsounds/autoadd/weeb/wowowowow.ogg",length=1.7222448979591836}}
+L["yeayeayea"]={{path="chatsounds/autoadd/weeb/yeayeayea.ogg",length=1.583673469387755}}
 L["youre such a cheater"]={{path="chatsounds/autoadd/weeb/youre such a cheater.ogg",length=2.4460544217687077}}
 c.EndList()
