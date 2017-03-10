27465a1e25e4d50b0bf34c165ea9ac0fcd85da20
PotcFdk <PotcFdk@gmx.net>
2017-03-10 02:52:37 +0100

    Squashed commit of the following:
    commit 9f47d3ce51ccc94a4decb3ea48ead684c3df1d39
    Merge: 05c4abee eefa808e
    Author: PotcFdk <PotcFdk@gmx.net>
    Date:   Fri Mar 10 02:49:04 2017 +0100
    
        Merge branch 'master' of https://github.com/EngineMachiner/garrysmod-chatsounds into EngineMachiner-master
    
        # Conflicts:
        #	lua/chatsounds/lists_nosend/weeb.lua
    
    commit eefa808e15c949b28c5f428f73db8a31ad566f3f
    Author: EngineMachiner <mfpc98@gmail.com>
    Date:   Thu Mar 9 19:59:59 2017 -0300
    
        Add files via upload
    
    commit eeb5ece530656ef552ff39adde0c3bfaab21bb2c
    Author: EngineMachiner <mfpc98@gmail.com>
    Date:   Thu Mar 9 19:58:15 2017 -0300
    
        Delete shdb.ogg
    
    commit 5cde4f00dbaf96fff6baa813a1d942a675a11baa
    Author: EngineMachiner <mfpc98@gmail.com>
    Date:   Thu Mar 9 19:57:04 2017 -0300
    
        Add files via upload
    
    commit e25e7a56b3ca9963dd0a5988912c2a5612ab947f
    Author: EngineMachiner <mfpc98@gmail.com>
    Date:   Thu Mar 9 16:22:31 2017 -0300
    
        Add files via upload
    
    commit f3ca75355e3228a2314c9624bf571826e3757d33
    Author: EngineMachiner <mfpc98@gmail.com>
    Date:   Thu Mar 9 16:21:30 2017 -0300
    
        Add files via upload
    
    commit 6934314f41bafc2fbdc6435cd403e6a91d5e537f
    Author: EngineMachiner <mfpc98@gmail.com>
    Date:   Wed Jan 11 13:19:59 2017 -0300
    
        Add files via upload
    
    commit fa61ca6084e3a47200eb43b17b3a04c5ee2a48cd
    Author: EngineMachiner <mfpc98@gmail.com>
    Date:   Wed Jan 11 13:19:12 2017 -0300
    
        Add files via upload
    
    commit 39e4b9fb02e1a2a0a4d56f196024251325038cee
    Author: EngineMachiner <mfpc98@gmail.com>
    Date:   Wed Jan 11 13:07:36 2017 -0300
    
        Add files via upload
    
    commit b088fb14d2a8eaba5171a79505802e5421093132
    Author: EngineMachiner <mfpc98@gmail.com>
    Date:   Wed Jan 11 13:07:00 2017 -0300
    
        Add files via upload

--- lua/chatsounds/lists_nosend/weeb.lua
+++ lua/chatsounds/lists_nosend/weeb.lua
@@ -1,12 +1,26 @@
 c.StartList("weeb")
 L["better"]={{path="chatsounds/autoadd/weeb/better.ogg",length=0.51659863945578233}}
 L["cheater"]={{path="chatsounds/autoadd/weeb/cheater.ogg",length=0.80818594104308394}}
+L["chu! natsu party"]={{path="chatsounds/autoadd/weeb/chu! natsu party/1.ogg",length=8.5681632653061222}}
+L["dont leave me now"]={{path="chatsounds/autoadd/weeb/dont leave me now.ogg",length=1.5052834467120182}}
+L["good"]={{path="chatsounds/autoadd/weeb/good.ogg",length=0.82430839002267575}}
+L["hey"]={{path="chatsounds/autoadd/weeb/hey.ogg",length=0.90557823129251702}}
+L["hsw8bit"]={{path="chatsounds/autoadd/weeb/hsw8bit.ogg",length=15.000090702947846}}
 L["imagination"]={{path="chatsounds/autoadd/weeb/imagination.ogg",length=1.9399773242630385}}
+L["koko ni iruzee"]={{path="chatsounds/autoadd/weeb/koko ni iruzee/1.ogg",length=10.135510204081633},{path="chatsounds/autoadd/weeb/koko ni iruzee/2.ogg",length=6.8731065759637184},{path="chatsounds/autoadd/weeb/koko ni iruzee/3.ogg",length=9.1370521541950112}}
 L["love revolution"]={{path="chatsounds/autoadd/weeb/love revolution.ogg",length=5.685736961451247}}
+L["love revolution remix"]={{path="chatsounds/autoadd/weeb/love revolution remix.ogg",length=7.7492517006802721}}
+L["manpower"]={{path="chatsounds/autoadd/weeb/manpower.ogg",length=7.8599546485260774}}
 L["no no no"]={{path="chatsounds/autoadd/weeb/no no no.ogg",length=3.6078684807256236}}
+L["otome pasta ni kandou"]={{path="chatsounds/autoadd/weeb/otome pasta ni kandou/1.ogg",length=13.699773242630386}}
 L["ponponpon"]={{path="chatsounds/autoadd/weeb/ponponpon.ogg",length=3.7659183673469387}}
+L["repeat after me"]={{path="chatsounds/autoadd/weeb/repeat after me.ogg",length=1.5325170068027212}}
 L["sexy boy"]={{path="chatsounds/autoadd/weeb/sexy boy.ogg",length=1.0754195011337868}}
+L["shabadabadoodoodoo"]={{path="chatsounds/autoadd/weeb/shabadabadoodoodoo/1.ogg",length=8.9861224489795912},{path="chatsounds/autoadd/weeb/shabadabadoodoodoo/2.ogg",length=9.4134013605442171}}
+L["shdb8bit"]={{path="chatsounds/autoadd/weeb/shdb8bit.ogg",length=9.5434013605442178}}
 L["touch me please"]={{path="chatsounds/autoadd/weeb/touch me please.ogg",length=1.7213378684807257}}
 L["wagamama ki no mama ai no joke"]={{path="chatsounds/autoadd/weeb/wagamama ki no mama ai no joke.ogg",length=3.0870975056689343}}
+L["wowowowow"]={{path="chatsounds/autoadd/weeb/wowowowow.ogg",length=1.7222448979591836}}
+L["yeayeayea"]={{path="chatsounds/autoadd/weeb/yeayeayea.ogg",length=1.583673469387755}}
 L["youre such a cheater"]={{path="chatsounds/autoadd/weeb/youre such a cheater.ogg",length=2.4460544217687077}}
 c.EndList()
