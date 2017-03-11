bc4d7def26e6fef2050ffd12534413ec14bb4095
PotcFdk <PotcFdk@gmx.net>
2017-03-11 15:29:55 +0100

    Regenerate lists.

--- lua/chatsounds/lists_nosend/weeb.lua
+++ lua/chatsounds/lists_nosend/weeb.lua
@@ -5,7 +5,6 @@ L["chu! natsu party"]={{path="chatsounds/autoadd/weeb/chu! natsu party/1.ogg",le
 L["dont leave me now"]={{path="chatsounds/autoadd/weeb/dont leave me now.ogg",length=1.5052834467120182}}
 L["good"]={{path="chatsounds/autoadd/weeb/good.ogg",length=0.82430839002267575}}
 L["hey"]={{path="chatsounds/autoadd/weeb/hey.ogg",length=0.90557823129251702}}
-L["hsw8bit"]={{path="chatsounds/autoadd/weeb/hsw8bit.ogg",length=15.000090702947846}}
 L["imagination"]={{path="chatsounds/autoadd/weeb/imagination.ogg",length=1.9399773242630385}}
 L["koko ni iruzee"]={{path="chatsounds/autoadd/weeb/koko ni iruzee/1.ogg",length=10.135510204081633},{path="chatsounds/autoadd/weeb/koko ni iruzee/2.ogg",length=6.8731065759637184},{path="chatsounds/autoadd/weeb/koko ni iruzee/3.ogg",length=9.1370521541950112}}
 L["love revolution"]={{path="chatsounds/autoadd/weeb/love revolution.ogg",length=5.685736961451247}}
@@ -17,7 +16,6 @@ L["ponponpon"]={{path="chatsounds/autoadd/weeb/ponponpon.ogg",length=3.765918367
 L["repeat after me"]={{path="chatsounds/autoadd/weeb/repeat after me.ogg",length=1.5325170068027212}}
 L["sexy boy"]={{path="chatsounds/autoadd/weeb/sexy boy.ogg",length=1.0754195011337868}}
 L["shabadabadoodoodoo"]={{path="chatsounds/autoadd/weeb/shabadabadoodoodoo/1.ogg",length=8.9861224489795912},{path="chatsounds/autoadd/weeb/shabadabadoodoodoo/2.ogg",length=9.4134013605442171}}
-L["shdb8bit"]={{path="chatsounds/autoadd/weeb/shdb8bit.ogg",length=9.5434013605442178}}
 L["touch me please"]={{path="chatsounds/autoadd/weeb/touch me please.ogg",length=1.7213378684807257}}
 L["wagamama ki no mama ai no joke"]={{path="chatsounds/autoadd/weeb/wagamama ki no mama ai no joke.ogg",length=3.0870975056689343}}
 L["wowowowow"]={{path="chatsounds/autoadd/weeb/wowowowow.ogg",length=1.7222448979591836}}
