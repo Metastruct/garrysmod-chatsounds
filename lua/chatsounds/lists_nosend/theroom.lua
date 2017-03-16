0d8512c6f57c45d32a2d7b2daa18e7cad61d7a84
Mikeai <mgamkrel@gmail.com>
2017-03-16 23:14:50 +0200

    The room chatsounds reworks and additions
    im fed up with this world

--- lua/chatsounds/lists_nosend/theroom.lua
+++ lua/chatsounds/lists_nosend/theroom.lua
@@ -1,14 +1,17 @@
 c.StartList("theroom")
 L["and the bank"]={{path="chatsounds/autoadd/theroom/and the bank.ogg",length=0.74013605442176866}}
 L["anyway"]={{path="chatsounds/autoadd/theroom/anyway.ogg",length=0.68353741496598641}}
+L["are you okay denny"]={{path="chatsounds/autoadd/theroom/are you okay denny.ogg",length=1.0710884353741497}}
 L["bye"]={{path="chatsounds/autoadd/theroom/bye.ogg",length=0.62693877551020405}}
 L["bye bye"]={{path="chatsounds/autoadd/theroom/bye bye.ogg",length=0.65306122448979587}}
-L["can i have a dozen red roses please"]={{path="chatsounds/autoadd/theroom/can i have a dozen red roses please.ogg",length=2.0114285714285716}}
+L["can i have a dozen red roses please"]={{path="chatsounds/autoadd/theroom/can i have a dozen red roses please.ogg",length=2.4185714285714286}}
 L["can i help you"]={{path="chatsounds/autoadd/theroom/can i help you.ogg",length=0.71401360544217685}}
 L["can i kiss you"]={{path="chatsounds/autoadd/theroom/can i kiss you.ogg",length=0.74884353741496601}}
 L["chicken"]={{path="chatsounds/autoadd/theroom/chicken.ogg",length=3.4917006802721087}}
 L["danny"]={{path="chatsounds/autoadd/theroom/danny.ogg",length=0.42666666666666669}}
+L["denny dont worry about that"]={{path="chatsounds/autoadd/theroom/denny dont worry about that.ogg",length=3.2247619047619049}}
 L["do you have something else to do"]={{path="chatsounds/autoadd/theroom/do you have something else to do.ogg",length=1.3757823129251701}}
+L["do you understand life"]={{path="chatsounds/autoadd/theroom/do you understand life.ogg",length=2.1191383219954649}}
 L["dont touch me motherfucker"]={{path="chatsounds/autoadd/theroom/dont touch me motherfucker.ogg",length=2.063673469387755}}
 L["dramatic"]={{path="chatsounds/autoadd/theroom/dramatic.ogg",length=17.536870748299318}}
 L["everybody betrayed me"]={{path="chatsounds/autoadd/theroom/everybody betrayed me.ogg",length=1.3844897959183673}}
@@ -23,6 +26,7 @@ L["hey everybody"]={{path="chatsounds/autoadd/theroom/hey everybody.ogg",length=
 L["hi"]={{path="chatsounds/autoadd/theroom/hi.ogg",length=0.67918367346938779}}
 L["hi babe"]={{path="chatsounds/autoadd/theroom/hi babe.ogg",length=0.69659863945578226}}
 L["hi doggy"]={{path="chatsounds/autoadd/theroom/hi doggy.ogg",length=0.74013605442176866}}
+L["how could you do this to me"]={{path="chatsounds/autoadd/theroom/how could you do this to me.ogg",length=1.9578911564625849}}
 L["how is your sex life"]={{path="chatsounds/autoadd/theroom/how is your sex life.ogg",length=1.1755102040816328}}
 L["how much is it"]={{path="chatsounds/autoadd/theroom/how much is it.ogg",length=0.62693877551020405}}
 L["i can not tell you its confidential"]={{path="chatsounds/autoadd/theroom/i can not tell you its confidential.ogg",length=1.7676190476190476}}
@@ -33,29 +37,37 @@ L["i dont think i will ever get it"]={{path="chatsounds/autoadd/theroom/i dont t
 L["i got the results of the test back"]={{path="chatsounds/autoadd/theroom/i got the results of the test back.ogg",length=1.9853061224489796}}
 L["i just like to watch you guys"]={{path="chatsounds/autoadd/theroom/i just like to watch you guys.ogg",length=1.3322448979591837}}
 L["i saved them bontos"]={{path="chatsounds/autoadd/theroom/i saved them bontos.ogg",length=1.2625850340136053}}
+L["i think you should leave right now mark"]={{path="chatsounds/autoadd/theroom/i think you should leave right now mark.ogg",length=1.8427210884353742}}
 L["i would do anything for my girl"]={{path="chatsounds/autoadd/theroom/i would do anything for my girl.ogg",length=1.8459863945578232}}
-L["im fed up with this world"]={{path="chatsounds/autoadd/theroom/im fed up with this world.ogg",length=1.4715646258503401}}
-L["in a few minutes bitch"]={{path="chatsounds/autoadd/theroom/in a few minutes bitch.ogg",length=1.4541496598639456}}
+L["im fed up with this world"]={{path="chatsounds/autoadd/theroom/im fed up with this world.ogg",length=1.8600000000000001}}
+L["in a few minutes bitch"]={{path="chatsounds/autoadd/theroom/in a few minutes bitch.ogg",length=1.6411791383219954}}
 L["is he dead"]={{path="chatsounds/autoadd/theroom/is he dead.ogg",length=0.62693877551020405}}
+L["it seems to me like youre the expert mark"]={{path="chatsounds/autoadd/theroom/it seems to me like youre the expert mark.ogg",length=3.2017460317460316}}
 L["its about lisa"]={{path="chatsounds/autoadd/theroom/its about lisa.ogg",length=0.61823129251700681}}
 L["its bullshit"]={{path="chatsounds/autoadd/theroom/its bullshit.ogg",length=0.76190476190476186}}
+L["its bullshit i did not hit her"]={{path="chatsounds/autoadd/theroom/its bullshit i did not hit her.ogg",length=2.0845804988662131}}
 L["its not over"]={{path="chatsounds/autoadd/theroom/its not over.ogg",length=1.0100680272108844}}
 L["its not true"]={{path="chatsounds/autoadd/theroom/its not true.ogg",length=0.90993197278911564}}
 L["just a chicken"]={{path="chatsounds/autoadd/theroom/just a chicken.ogg",length=0.85333333333333339}}
+L["keep in moind"]={{path="chatsounds/autoadd/theroom/keep in moind.ogg",length=0.90120181405895694}}
 L["keep the change"]={{path="chatsounds/autoadd/theroom/keep the change.ogg",length=0.77496598639455783}}
 L["laughter"]={{path="chatsounds/autoadd/theroom/laughter.ogg",length=5.093877551020408}}
 L["lets go inside and eat some cake"]={{path="chatsounds/autoadd/theroom/lets go inside and eat some cake.ogg",length=1.7240816326530612}}
 L["lets go outside"]={{path="chatsounds/autoadd/theroom/lets go outside.ogg",length=0.76625850340136059}}
+L["maybe i know more than you think i do mark"]={{path="chatsounds/autoadd/theroom/maybe i know more than you think i do mark.ogg",length=2.441609977324263}}
 L["no i can"]={{path="chatsounds/autoadd/theroom/no i can.ogg",length=1.0536054421768708}}
+L["no its not true dont even ask"]={{path="chatsounds/autoadd/theroom/no its not true dont even ask.ogg",length=2.2227891156462585}}
 L["not hit her"]={{path="chatsounds/autoadd/theroom/not hit her.ogg",length=0.77496598639455783}}
 L["oh come on why not"]={{path="chatsounds/autoadd/theroom/oh come on why not.ogg",length=1.3409523809523809}}
-L["oh hi johnny i didnt know it was you"]={{path="chatsounds/autoadd/theroom/oh hi johnny i didnt know it was you.ogg",length=1.3844897959183673}}
+L["oh hi johnny i didnt know it was you"]={{path="chatsounds/autoadd/theroom/oh hi johnny i didnt know it was you.ogg",length=1.5778458049886621}}
 L["oh hi maarc"]={{path="chatsounds/autoadd/theroom/oh hi maarc.ogg",length=0.70965986394557823}}
 L["oh hi mark"]={{path="chatsounds/autoadd/theroom/oh hi mark.ogg",length=0.70965986394557823}}
+L["okay folks everything is fine"]={{path="chatsounds/autoadd/theroom/okay folks everything is fine.ogg",length=1.8484807256235827}}
 L["ouch"]={{path="chatsounds/autoadd/theroom/ouch.ogg",length=0.5050340136054422}}
 L["people are very strange these days"]={{path="chatsounds/autoadd/theroom/people are very strange these days.ogg",length=1.5934693877551021}}
 L["shes gone"]={{path="chatsounds/autoadd/theroom/shes gone.ogg",length=0.67918367346938779}}
 L["shoot danny"]={{path="chatsounds/autoadd/theroom/shoot danny.ogg",length=0.88816326530612244}}
+L["somebody had better do something around here"]={{path="chatsounds/autoadd/theroom/somebody had better do something around here.ogg",length=3.1786848072562357}}
 L["stop ganging up on me"]={{path="chatsounds/autoadd/theroom/stop ganging up on me.ogg",length=1.6457142857142857}}
 L["thanks a lot"]={{path="chatsounds/autoadd/theroom/thanks a lot.ogg",length=0.55727891156462583}}
 L["that will be eighteen dollars"]={{path="chatsounds/autoadd/theroom/that will be eighteen dollars.ogg",length=1.18421768707483}}
@@ -64,15 +76,22 @@ L["they betrayed me"]={{path="chatsounds/autoadd/theroom/they betrayed me.ogg",l
 L["they didnt keep their promise"]={{path="chatsounds/autoadd/theroom/they didnt keep their promise.ogg",length=1.3931972789115645}}
 L["they tricked me and i dont care anymore"]={{path="chatsounds/autoadd/theroom/they tricked me and i dont care anymore.ogg",length=1.7414965986394557}}
 L["theyre crazy"]={{path="chatsounds/autoadd/theroom/theyre crazy.ogg",length=0.88816326530612244}}
+L["tommyscream"]={{path="chatsounds/autoadd/theroom/tommyscream.ogg",length=0.98471655328798191}}
 L["wake up johnny come on"]={{path="chatsounds/autoadd/theroom/wake up johnny come on.ogg",length=1.3148299319727892}}
 L["well make a lot of money"]={{path="chatsounds/autoadd/theroom/well make a lot of money.ogg",length=1.1755102040816328}}
 L["what a story mark"]={{path="chatsounds/autoadd/theroom/what a story mark.ogg",length=1.0405442176870747}}
 L["what are you nuts"]={{path="chatsounds/autoadd/theroom/what are you nuts.ogg",length=1.0448979591836736}}
+L["whats new with you"]={{path="chatsounds/autoadd/theroom/whats new with you.ogg",length=1.2899092970521542}}
+L["why did you do this you know better right"]={{path="chatsounds/autoadd/theroom/why did you do this you know better right.ogg",length=2.5452607709750565}}
 L["why lisa why lisa please talk to me please"]={{path="chatsounds/autoadd/theroom/why lisa why lisa please talk to me please.ogg",length=3.5961904761904764}}
 L["why lisa why why"]={{path="chatsounds/autoadd/theroom/why lisa why why.ogg",length=2.4293877551020406}}
-L["you are tearing me apart lisa"]={{path="chatsounds/autoadd/theroom/you are tearing me apart lisa.ogg",length=2.4903401360544217}}
+L["yeah you can say that again"]={{path="chatsounds/autoadd/theroom/yeah you can say that again.ogg",length=1.3705215419501133}}
+L["you are tearing me apart lisa"]={{path="chatsounds/autoadd/theroom/you are tearing me apart lisa.ogg",length=2.7410430839002267}}
 L["you can come out now johnny"]={{path="chatsounds/autoadd/theroom/you can come out now johnny.ogg",length=1.1929251700680272}}
+L["you little tramp"]={{path="chatsounds/autoadd/theroom/you little tramp.ogg",length=1.4511564625850339}}
 L["you look beautiful today"]={{path="chatsounds/autoadd/theroom/you look beautiful today.ogg",length=1.2887074829931973}}
+L["you must be kidding arent you"]={{path="chatsounds/autoadd/theroom/you must be kidding arent you.ogg",length=2.0270068027210884}}
 L["youre just a little chicken"]={{path="chatsounds/autoadd/theroom/youre just a little chicken.ogg",length=1.7066666666666668}}
+L["youre lying i never hit you"]={{path="chatsounds/autoadd/theroom/youre lying i never hit you.ogg",length=2.2803628117913832}}
 L["youre my favorite customer"]={{path="chatsounds/autoadd/theroom/youre my favorite customer.ogg",length=1.062312925170068}}
 c.EndList()
