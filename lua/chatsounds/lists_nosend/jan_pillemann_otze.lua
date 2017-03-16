da97ab98b1ed1b45240ac8f53169ea0c98ed1ac7
PotcFdk <PotcFdk@gmx.net>
2017-03-17 00:10:38 +0100

    typo fix

--- lua/chatsounds/lists_nosend/jan_pillemann_otze.lua
+++ lua/chatsounds/lists_nosend/jan_pillemann_otze.lua
@@ -1,6 +1,6 @@
 c.StartList("jan_pillemann_otze")
 L["den hat man jan pillemann otze arsch genannt"]={{path="chatsounds/autoadd/jan_pillemann_otze/den hat man jan pillemann otze arsch genannt.ogg",length=3}}
-L["jan pillemann jan pilleman otze arsch"]={{path="chatsounds/autoadd/jan_pillemann_otze/jan pillemann jan pilleman otze arsch.ogg",length=2.4485260770975055}}
+L["jan pillemann jan pillemann otze arsch"]={{path="chatsounds/autoadd/jan_pillemann_otze/jan pillemann jan pillemann otze arsch.ogg",length=2.4485260770975055}}
 L["jan pillemann otze"]={{path="chatsounds/autoadd/jan_pillemann_otze/jan pillemann otze/jan pillemann otze 1.ogg",length=1.4932879818594105},{path="chatsounds/autoadd/jan_pillemann_otze/jan pillemann otze/jan pillemann otze 2.ogg",length=1.44140589569161}}
 L["jan pillemann otze arsch"]={{path="chatsounds/autoadd/jan_pillemann_otze/jan pillemann otze arsch.ogg",length=1.35859410430839}}
 L["otze arsch"]={{path="chatsounds/autoadd/jan_pillemann_otze/otze arsch.ogg",length=1.0236734693877552}}
