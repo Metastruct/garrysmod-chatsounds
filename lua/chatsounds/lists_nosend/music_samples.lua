1bd63fa2fc321b7466ca24281686f803cb6c6910
PotcFdk <PotcFdk@gmx.net>
2017-03-18 10:36:00 +0100

    Rename files

--- lua/chatsounds/lists_nosend/music_samples.lua
+++ lua/chatsounds/lists_nosend/music_samples.lua
@@ -1,5 +1,5 @@
 c.StartList("music_samples")
-L["d-d-d-d-drop-the-bass"]={{path="chatsounds/autoadd/music_samples/d-d-d-d-drop-the-bass.mp3",length=1.6979591836734693}}
+L["d-d-d-d-drop the bass"]={{path="chatsounds/autoadd/music_samples/d-d-d-d-drop the bass.mp3",length=1.6979591836734693}}
+L["drop the bass"]={{path="chatsounds/autoadd/music_samples/drop the bass.mp3",length=0.86204081632653062}}
 L["drop the bass 2"]={{path="chatsounds/autoadd/music_samples/drop the bass 2.mp3",length=0.94022675736961447}}
-L["drop-the-bass"]={{path="chatsounds/autoadd/music_samples/drop-the-bass.mp3",length=0.86204081632653062}}
 c.EndList()
