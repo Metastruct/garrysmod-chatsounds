8b6b180db59f743dd07fe3c99c76ce911e25ff95
Nyaako <kawaii.as.fk.m8@gmail.com>
2017-05-10 18:03:56 +0100

    random stuff idek
    yeah

---
+++ lua/chatsounds/lists_nosend/syzygy.lua
@@ -0,0 +1,15 @@
+c.StartList("syzygy")
+L["can you like shut up"]={{path="chatsounds/autoadd/syzygy/can you like shut up.ogg",length=1.8404535147392291}}
+L["cocaine pancakes"]={{path="chatsounds/autoadd/syzygy/cocaine pancakes.ogg",length=1.7574603174603174}}
+L["gameboygirl"]={{path="chatsounds/autoadd/syzygy/gameboygirl.ogg",length=3.2222902494331067}}
+L["get the fuck out of my room im playing minecraft"]={{path="chatsounds/autoadd/syzygy/get the fuck out of my room im playing minecraft.ogg",length=3.1319501133786849}}
+L["hey whats up depressed faggot here"]={{path="chatsounds/autoadd/syzygy/hey whats up depressed faggot here.ogg",length=2.2973242630385489}}
+L["i need"]={{path="chatsounds/autoadd/syzygy/i need.ogg",length=1.3}}
+L["luigi"]={{path="chatsounds/autoadd/syzygy/luigi.ogg",length=0.90482993197278916}}
+L["my breath does not smell of meth"]={{path="chatsounds/autoadd/syzygy/my breath does not smell of meth.ogg",length=2.0578911564625852}}
+L["nice meme"]={{path="chatsounds/autoadd/syzygy/nice meme.ogg",length=0.91696145124716555}}
+L["shut yer fucking face"]={{path="chatsounds/autoadd/syzygy/shut yer fucking face.ogg",length=1.3577097505668934}}
+L["stick some sausages into this frying pan im fucking starving"]={{path="chatsounds/autoadd/syzygy/stick some sausages into this frying pan im fucking starving.ogg",length=3.1840816326530614}}
+L["wheres my breakfast"]={{path="chatsounds/autoadd/syzygy/wheres my breakfast.ogg",length=1.2957142857142858}}
+L["you have been banned from the mickey mouse club for inappropriate behaviour"]={{path="chatsounds/autoadd/syzygy/you have been banned from the mickey mouse club for inappropriate behaviour.ogg",length=6.5567346938775506}}
+c.EndList()
