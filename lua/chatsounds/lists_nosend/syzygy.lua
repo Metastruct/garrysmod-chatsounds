abe6654287f67be34a9755c2aa44ef657a30de66
Nyaako <kawaii.as.fk.m8@gmail.com>
2017-05-10 20:43:01 +0100

    everyone
    everyone!! (np berb)

--- lua/chatsounds/lists_nosend/syzygy.lua
+++ lua/chatsounds/lists_nosend/syzygy.lua
@@ -1,6 +1,7 @@
 c.StartList("syzygy")
 L["can you like shut up"]={{path="chatsounds/autoadd/syzygy/can you like shut up.ogg",length=1.8404535147392291}}
 L["cocaine pancakes"]={{path="chatsounds/autoadd/syzygy/cocaine pancakes.ogg",length=1.7574603174603174}}
+L["everyone"]={{path="chatsounds/autoadd/syzygy/everyone.ogg",length=1.3357596371882086}}
 L["gameboygirl"]={{path="chatsounds/autoadd/syzygy/gameboygirl.ogg",length=3.2222902494331067}}
 L["get the fuck out of my room im playing minecraft"]={{path="chatsounds/autoadd/syzygy/get the fuck out of my room im playing minecraft.ogg",length=3.1319501133786849}}
 L["hey whats up depressed faggot here"]={{path="chatsounds/autoadd/syzygy/hey whats up depressed faggot here.ogg",length=2.2973242630385489}}
