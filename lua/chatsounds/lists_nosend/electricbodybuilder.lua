187bf332d87073b9502e23255c9dde2c3e8419ba
HighGuard <tiger2001@mail.ru>
2017-05-24 22:11:42 +0300

    Add files via upload (#114)

---
+++ lua/chatsounds/lists_nosend/electricbodybuilder.lua
@@ -0,0 +1,5 @@
+c.StartList("electricbodybuilder")
+L["hi"]={{path="chatsounds/autoadd/electricbodybuilder/hi.ogg",length=0.75464852607709754}}
+L["hi im electric bodybuilder from russia"]={{path="chatsounds/autoadd/electricbodybuilder/hi im electric bodybuilder from russia.ogg",length=5.7375056689342401}}
+L["russia"]={{path="chatsounds/autoadd/electricbodybuilder/russia.ogg",length=1.3699773242630386}}
+c.EndList()
