8515a4c4f1238e732935fb7cf36ed8c896626e5c
W1C0P3E <mineev.0807@gmail.com>
2017-07-17 00:51:51 +0300

    seperated - lua file

--- lua/chatsounds/lists_nosend/wicopee.lua
+++ lua/chatsounds/lists_nosend/wicopee.lua
@@ -1,3 +1,12 @@
 c.StartList("wicopee")
-L["do you ever feel angry are you paralyzed by your anger"]={{path="chatsounds/autoadd/wicopee/do you ever feel angry are you paralyzed by your anger.ogg",length=33.866984126984129}}
+L["all of this behaviour is a symptom of the fact that youre not expressing your anger"]={{path="chatsounds/autoadd/wicopee/all of this behaviour is a symptom of the fact that youre not expressing your anger.ogg",length=5.8058049886621319}}
+L["and you are afraid that if you blow up youre gonna blow away all your friends and all your co workers"]={{path="chatsounds/autoadd/wicopee/and you are afraid that if you blow up youre gonna blow away all your friends and all your co workers.ogg",length=6.2729251700680271}}
+L["are you paralyzed by your anger"]={{path="chatsounds/autoadd/wicopee/are you paralyzed by your anger.ogg",length=2.7694331065759639}}
+L["are you really frustrated"]={{path="chatsounds/autoadd/wicopee/are you really frustrated.ogg",length=2.0353741496598641}}
+L["chances are"]={{path="chatsounds/autoadd/wicopee/chances are.ogg",length=1.4347619047619047}}
+L["do you ever feel angry"]={{path="chatsounds/autoadd/wicopee/do you ever feel angry.ogg",length=1.7150566893424035}}
+L["do you ever get to the point where you gonna blow up"]={{path="chatsounds/autoadd/wicopee/do you ever get to the point where you gonna blow up.ogg",length=2.80281179138322}}
+L["do you feel yourself with butterflies in your stomach a lot"]={{path="chatsounds/autoadd/wicopee/do you feel yourself with butterflies in your stomach a lot.ogg",length=3.7036734693877551}}
+L["we can certainly express our anger if we handle our anger assertivley"]={{path="chatsounds/autoadd/wicopee/we can certainly express our anger if we handle our anger assertivley.ogg",length=5.9726303854875287}}
+L["you get headaches"]={{path="chatsounds/autoadd/wicopee/you get headaches.ogg",length=1.3680272108843536}}
 c.EndList()
