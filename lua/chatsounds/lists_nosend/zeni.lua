0e5d5f04db7263c0395fd3a973dba3de7dff0b78
Toasty Engineer <timrace01@gmail.com>
2017-07-22 19:53:26 +0200

    BEGONE THOT

---
+++ lua/chatsounds/lists_nosend/zeni.lua
@@ -0,0 +1,5 @@
+c.StartList("zeni")
+L["banned for life"]={{path="chatsounds/autoadd/zeni/banned for life.ogg",length=4.6820408163265306}}
+L["begone thot"]={{path="chatsounds/autoadd/zeni/begone thot.ogg",length=2.0002494331065761}}
+L["get out of here stalker"]={{path="chatsounds/autoadd/zeni/get out of here stalker.ogg",length=1.4559183673469387}}
+c.EndList()
