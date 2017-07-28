30070c260de580bffd9cbff806950ff92c8cbc2b
PotcFdk <PotcFdk@gmx.net>
2017-07-28 14:36:28 +0200

    Revert bad commit / regen list. This reverts commit 518f846361751e3aff68382397c2bfb7d01a8cf0.

---
+++ lua/chatsounds/lists_nosend/8bit.lua
@@ -0,0 +1,4 @@
+c.StartList("8bit")
+L["ducktales"]={{path="chatsounds/autoadd/8bit/ducktales.ogg",length=1.530}}
+L["supermariobros3"]={{path="chatsounds/autoadd/8bit/supermariobros3.ogg",length=3.647}}
+c.EndList()
--- lua/chatsounds/lists_nosend/barking dogs.lua
+++
@@ -1,4 +0,0 @@
-c.StartList("barking dogs")
-L["ducktales"]={{path="chatsounds/autoadd/barking dogs/ducktales.ogg",length=1.530}}
-L["supermariobros3"]={{path="chatsounds/autoadd/barking dogs/supermariobros3.ogg",length=3.647}}
-c.EndList()
