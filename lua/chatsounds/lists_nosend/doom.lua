442dce803b7e2b73189b14bc3d6ddf86659f37c4
SpiralP <spiral.palmer@gmail.com>
2017-07-29 20:22:56 -0700

    doom 2016

---
+++ lua/chatsounds/lists_nosend/doom.lua
@@ -0,0 +1,47 @@
+c.StartList("doom")
+L["authorization denied"]={{path="chatsounds/autoadd/doom/authorization denied.ogg",length=7.1040136054421765}}
+L["auxiliary neural security network offline"]={{path="chatsounds/autoadd/doom/auxiliary neural security network offline.ogg",length=8.2773469387755103}}
+L["bfg testing sequence initiated"]={{path="chatsounds/autoadd/doom/bfg testing sequence initiated.ogg",length=11.818684807256236}}
+L["biometric mismatch detected scanning unauthorized personnel confirmed expulsion to commence in 5"]={{path="chatsounds/autoadd/doom/biometric mismatch detected scanning unauthorized personnel confirmed expulsion to commence in 5.ogg",length=19.669319727891157}}
+L["cannot determine the cause of the demonic invasion"]={{path="chatsounds/autoadd/doom/cannot determine the cause of the demonic invasion.ogg",length=10.922653061224489}}
+L["category 3 dimensional event"]={{path="chatsounds/autoadd/doom/category 3 dimensional event.ogg",length=17.066666666666666}}
+L["category 4 dimensional event"]={{path="chatsounds/autoadd/doom/category 4 dimensional event.ogg",length=17.728004535147392}}
+L["demonic breach detected"]={{path="chatsounds/autoadd/doom/demonic breach detected.ogg",length=6.9973469387755101}}
+L["demonic presence at critical levels"]={{path="chatsounds/autoadd/doom/demonic presence at critical levels.ogg",length=7.3813378684807258}}
+L["demonic presence at nominal levels"]={{path="chatsounds/autoadd/doom/demonic presence at nominal levels.ogg",length=7.5733333333333333}}
+L["demonic presence at unsafe levels"]={{path="chatsounds/autoadd/doom/demonic presence at unsafe levels.ogg",length=9.6853514739229016}}
+L["demonic presence detected"]={{path="chatsounds/autoadd/doom/demonic presence detected.ogg",length=6.9546712018140591}}
+L["demonic presence eliminated"]={{path="chatsounds/autoadd/doom/demonic presence eliminated.ogg",length=9.5146712018140587}}
+L["demonic presence reduced to minimum safe levels"]={{path="chatsounds/autoadd/doom/demonic presence reduced to minimum safe levels.ogg",length=10.026666666666667}}
+L["discharging your firearms in this facility is a serious offence"]={{path="chatsounds/autoadd/doom/discharging your firearms in this facility is a serious offence.ogg",length=9.7706802721088444}}
+L["do not be alarmed by the system update"]={{path="chatsounds/autoadd/doom/do not be alarmed by the system update.ogg",length=17.941337868480726}}
+L["do not be alarmed by the system update it is a prototype of our interdimensional tether a means to pull you back from hell im sorry for the jolt but i didnt think youd allow its installation voluntarily"]={{path="chatsounds/autoadd/doom/do not be alarmed by the system update.ogg",length=17.941337868480726}}
+L["entry denied"]={{path="chatsounds/autoadd/doom/entry denied.ogg",length=6.9333333333333336}}
+L["hey this is for your benefit"]={{path="chatsounds/autoadd/doom/hey this is for your benefit.ogg",length=3.1786848072562357}}
+L["i am not the villain in this story i do what i do because there is no choice"]={{path="chatsounds/autoadd/doom/i am not the villain in this story i do what i do because there is no choice.ogg",length=9.8133333333333326}}
+L["i cant kill you but i wont have you standing in our way"]={{path="chatsounds/autoadd/doom/i cant kill you but i wont have you standing in our way.ogg",length=6.1013378684807256}}
+L["if stopping our energy production is what you want"]={{path="chatsounds/autoadd/doom/if stopping our energy production is what you want.ogg",length=16.55467120181406}}
+L["if stopping our energy production is what you want then you need only to destroy this last filter and argent energy will no longer exist in this solar system we will be back at square one"]={{path="chatsounds/autoadd/doom/if stopping our energy production is what you want.ogg",length=16.55467120181406}}
+L["if youre going to win youll need this"]={{path="chatsounds/autoadd/doom/if youre going to win youll need this.ogg",length=3.3933333333333335}}
+L["lost soul scream"]={{path="chatsounds/autoadd/doom/lost soul scream.ogg",length=6.6133333333333333}}
+L["pay attention im not teaching you this again"]={{path="chatsounds/autoadd/doom/pay attention im not teaching you this again.ogg",length=3.3493197278911566}}
+L["please remember to clean up all toxic spills"]={{path="chatsounds/autoadd/doom/please remember to clean up all toxic spills.ogg",length=8.1920181405895693}}
+L["retinal scan failed access denied"]={{path="chatsounds/autoadd/doom/retinal scan failed access denied.ogg",length=10.218684807256237}}
+L["save your amunition"]={{path="chatsounds/autoadd/doom/save your amunition.ogg",length=3.8399999999999999}}
+L["splashyell"]={{path="chatsounds/autoadd/doom/splashyell.ogg",length=1.48}}
+L["systems reinitializing main power online"]={{path="chatsounds/autoadd/doom/systems reinitializing main power online.ogg",length=9.7706802721088444}}
+L["the demons will often attack one another theyre natural cannibals when left among themselves"]={{path="chatsounds/autoadd/doom/the demons will often attack one another theyre natural cannibals when left among themselves.ogg",length=7.5733333333333333}}
+L["the door will not open until the threat is removed"]={{path="chatsounds/autoadd/doom/the door will not open until the threat is removed.ogg",length=4.2680045351473925}}
+L["they are rage brutal without mercy but you you will be worse rip and tear until it is done"]={{path="chatsounds/autoadd/doom/they are rage brutal without mercy but you you will be worse rip and tear until it is done.ogg",length=14.988004535147393}}
+L["warning core blast doors are open extreme danger"]={{path="chatsounds/autoadd/doom/warning core blast doors are open extreme danger.ogg",length=9.5999999999999996}}
+L["warning core temperature reaching critical levels"]={{path="chatsounds/autoadd/doom/warning core temperature reaching critical levels.ogg",length=9.4506802721088441}}
+L["warning core temperature rising"]={{path="chatsounds/autoadd/doom/warning core temperature rising.ogg",length=7.6159863945578232}}
+L["warning energy contamination at critical levels"]={{path="chatsounds/autoadd/doom/warning energy contamination at critical levels.ogg",length=9.4293197278911567}}
+L["warning terminal breach detected"]={{path="chatsounds/autoadd/doom/warning terminal breach detected.ogg",length=10.944013605442176}}
+L["well thats it then theres nothing else to be done"]={{path="chatsounds/autoadd/doom/well thats it then theres nothing else to be done.ogg",length=6.4213378684807259}}
+L["what you are destroying is more valuable than you can possibly imagine"]={{path="chatsounds/autoadd/doom/what you are destroying is more valuable than you can possibly imagine.ogg",length=11.47734693877551}}
+L["what you are destroying is more valuable than you can possibly imagine it is a perfect and unlimited energy resource"]={{path="chatsounds/autoadd/doom/what you are destroying is more valuable than you can possibly imagine.ogg",length=11.47734693877551}}
+L["you think the only way is to kill them all leave nothing behind and you may be right"]={{path="chatsounds/autoadd/doom/you think the only way is to kill them all leave nothing behind and you may be right.ogg",length=17.365351473922903}}
+L["you think the only way is to kill them all leave nothing behind and you may be right but we cant just shut it all down without argent energy it will be worse i dont expect you to agree"]={{path="chatsounds/autoadd/doom/you think the only way is to kill them all leave nothing behind and you may be right.ogg",length=17.365351473922903}}
+L["youre going to have to remove it"]={{path="chatsounds/autoadd/doom/youre going to have to remove it.ogg",length=5.3546712018140585}}
+c.EndList()
