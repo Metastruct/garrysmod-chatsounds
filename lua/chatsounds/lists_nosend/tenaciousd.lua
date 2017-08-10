b1da7a245b311bd13496f7ec1e3758677168f5ae
Meta Automator <MetaAutomator@users.noreply.github.com>
2017-08-10 18:07:05 +0000

    chatsounds preprocessor (10d6333c591c804dc9aed21e35d89876def71df3)
    [ci skip]

--- lua/chatsounds/lists_nosend/tenaciousd.lua
+++ lua/chatsounds/lists_nosend/tenaciousd.lua
@@ -1,10 +1,16 @@
 c.StartList("tenaciousd")
 L["aaaaaahh"]={{path="chatsounds/autoadd/tenaciousd/aaaaaahh.ogg",length=1.473}}
+L["check this riff its fucking tasty"]={{path="chatsounds/autoadd/tenaciousd/check this riff its fucking tasty.ogg",length=7.482}}
 L["dramaticscream"]={{path="chatsounds/autoadd/tenaciousd/dramaticscream/dramaticscream 1.ogg",length=1.649},{path="chatsounds/autoadd/tenaciousd/dramaticscream/dramaticscream 2.ogg",length=1.315}}
 L["dun what"]={{path="chatsounds/autoadd/tenaciousd/dun what.ogg",length=1.297}}
 L["fart"]={{path="chatsounds/autoadd/tenaciousd/fart.ogg",length=0.322}}
+L["fuck"]={{path="chatsounds/autoadd/tenaciousd/fuck/fuck1.ogg",length=1.487},{path="chatsounds/autoadd/tenaciousd/fuck/fuck2.ogg",length=1.756}}
+L["fuuuuck"]={{path="chatsounds/autoadd/tenaciousd/fuuuuck.ogg",length=3.852}}
+L["i am complete"]={{path="chatsounds/autoadd/tenaciousd/i am complete.ogg",length=3.464}}
 L["longlaugh"]={{path="chatsounds/autoadd/tenaciousd/longlaugh.ogg",length=7.594}}
 L["pshit"]={{path="chatsounds/autoadd/tenaciousd/pshit.ogg",length=0.923}}
+L["sexslave"]={{path="chatsounds/autoadd/tenaciousd/sexslave.ogg",length=1.362}}
 L["what the fuck"]={{path="chatsounds/autoadd/tenaciousd/what the fuck.ogg",length=1.446}}
 L["you dont have any money"]={{path="chatsounds/autoadd/tenaciousd/you dont have any money.ogg",length=1.709}}
+L["you guys are fucking lame"]={{path="chatsounds/autoadd/tenaciousd/you guys are fucking lame.ogg",length=1.656}}
 c.EndList()
