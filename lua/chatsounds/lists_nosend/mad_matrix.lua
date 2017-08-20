a4096a001183e98eb51f81d60fbe2732f750580e
Brian Davis <kjones1999@outlook.com>
2017-08-20 13:08:30 -0400

    gasp

---
+++ lua/chatsounds/lists_nosend/mad_matrix.lua
@@ -0,0 +1,4 @@
+c.StartList("mad_matrix")
+L["aaa blyad"]={{path="chatsounds/autoadd/mad_matrix/aaa blyad.ogg",length=1.802}}
+L["oddgasp"]={{path="chatsounds/autoadd/mad_matrix/oddgasp.ogg",length=0.418}}
+c.EndList()
