0f1ca4f8793b2bb430266177807765c4f08649ea
Brian Davis <kjones1999@outlook.com>
2017-08-20 12:37:27 -0400

    raar and shaash mister raar

--- lua/chatsounds/lists_nosend/hl2_edits.lua
+++ lua/chatsounds/lists_nosend/hl2_edits.lua
@@ -7,7 +7,11 @@ L["dr breerb"]={{path="chatsounds/autoadd/hl2_edits/dr breerb.ogg",length=1.149}
 L["eeeuuuggghhh"]={{path="chatsounds/autoadd/hl2_edits/eeeuuuggghhh.ogg",length=6.120}}
 L["gordon you must get out of here runrunrunrunrunrunrunrun"]={{path="chatsounds/autoadd/hl2_edits/gordon you must get out of here runrunrunrunrunrunrunrun.ogg",length=5.300}}
 L["i must experience a crowbar in the butt"]={{path="chatsounds/autoadd/hl2_edits/i must experience a crowbar in the butt.ogg",length=3.639}}
+L["i realize that im not cumming"]={{path="chatsounds/autoadd/hl2_edits/i realize that im not cumming.ogg",length=1.938}}
 L["ile"]={{path="chatsounds/autoadd/hl2_edits/ile.ogg",length=0.808}}
+L["no one is more deserving of a sex"]={{path="chatsounds/autoadd/hl2_edits/no one is more deserving of a sex.ogg",length=3.065}}
 L["pick up the trash can"]={{path="chatsounds/autoadd/hl2_edits/pick up the trash can/1.ogg",length=2.059},{path="chatsounds/autoadd/hl2_edits/pick up the trash can/2.ogg",length=1.560}}
+L["raar and shaash mister raar"]={{path="chatsounds/autoadd/hl2_edits/raar and shaash mister raar.ogg",length=2.909}}
 L["the boreaerob"]={{path="chatsounds/autoadd/hl2_edits/the boreaerob.ogg",length=3.170}}
+L["the right lays in the wrong place can make soos"]={{path="chatsounds/autoadd/hl2_edits/the right lays in the wrong place can make soos.ogg",length=3.306}}
 c.EndList()
