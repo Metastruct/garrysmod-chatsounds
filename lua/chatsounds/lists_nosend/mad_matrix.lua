5778712af678c78429b20a6bd7f740a4a12df112
Brian Davis <kjones1999@outlook.com>
2017-08-31 21:09:51 -0400

    ooh yeeaahh

--- lua/chatsounds/lists_nosend/mad_matrix.lua
+++ lua/chatsounds/lists_nosend/mad_matrix.lua
@@ -3,10 +3,13 @@ L["aaa blyad"]={{path="chatsounds/autoadd/mad_matrix/aaa blyad.ogg",length=1.802
 L["bug flyby"]={{path="chatsounds/autoadd/mad_matrix/bug flyby.ogg",length=2.728}}
 L["eeree"]={{path="chatsounds/autoadd/mad_matrix/eeree.ogg",length=0.636}}
 L["emit eht ta kool"]={{path="chatsounds/autoadd/mad_matrix/emit eht ta kool.ogg",length=2.229}}
+L["hey all you suckers out there"]={{path="chatsounds/autoadd/mad_matrix/hey all you suckers out there.ogg",length=1.232}}
 L["how bout for instance"]={{path="chatsounds/autoadd/mad_matrix/how bout for instance.ogg",length=1.234}}
 L["huh huh huh huh huh we was robbed"]={{path="chatsounds/autoadd/mad_matrix/huh huh huh huh huh we was robbed.ogg",length=2.815}}
+L["its gonna be on like sucking dicc"]={{path="chatsounds/autoadd/mad_matrix/its gonna be on like sucking dicc.ogg",length=1.799}}
 L["nani"]={{path="chatsounds/autoadd/mad_matrix/nani.ogg",length=0.538}}
 L["oddgasp"]={{path="chatsounds/autoadd/mad_matrix/oddgasp.ogg",length=0.418}}
+L["ooh yeeaahh"]={{path="chatsounds/autoadd/mad_matrix/ooh yeeaahh.ogg",length=1.337}}
 L["slimespawn"]={{path="chatsounds/autoadd/mad_matrix/slimespawn/1.ogg",length=0.379},{path="chatsounds/autoadd/mad_matrix/slimespawn/2.ogg",length=0.488},{path="chatsounds/autoadd/mad_matrix/slimespawn/3.ogg",length=0.508},{path="chatsounds/autoadd/mad_matrix/slimespawn/4.ogg",length=0.564}}
 L["slimesplat"]={{path="chatsounds/autoadd/mad_matrix/slimesplat/1.ogg",length=0.342},{path="chatsounds/autoadd/mad_matrix/slimesplat/2.ogg",length=0.422}}
 L["slimetalk"]={{path="chatsounds/autoadd/mad_matrix/slimetalk/1.ogg",length=0.157},{path="chatsounds/autoadd/mad_matrix/slimetalk/10.ogg",length=0.253},{path="chatsounds/autoadd/mad_matrix/slimetalk/11.ogg",length=0.761},{path="chatsounds/autoadd/mad_matrix/slimetalk/12.ogg",length=0.620},{path="chatsounds/autoadd/mad_matrix/slimetalk/13.ogg",length=0.902},{path="chatsounds/autoadd/mad_matrix/slimetalk/14.ogg",length=1.349},{path="chatsounds/autoadd/mad_matrix/slimetalk/15.ogg",length=1.676},{path="chatsounds/autoadd/mad_matrix/slimetalk/16.ogg",length=0.854},{path="chatsounds/autoadd/mad_matrix/slimetalk/2.ogg",length=0.311},{path="chatsounds/autoadd/mad_matrix/slimetalk/3.ogg",length=0.447},{path="chatsounds/autoadd/mad_matrix/slimetalk/4.ogg",length=0.456},{path="chatsounds/autoadd/mad_matrix/slimetalk/5.ogg",length=0.303},{path="chatsounds/autoadd/mad_matrix/slimetalk/6.ogg",length=0.522},{path="chatsounds/autoadd/mad_matrix/slimetalk/7.ogg",length=0.629},{path="chatsounds/autoadd/mad_matrix/slimetalk/8.ogg",length=0.658},{path="chatsounds/autoadd/mad_matrix/slimetalk/9.ogg",length=0.602}}
@@ -18,4 +21,7 @@ L["snivyshortyawn"]={{path="chatsounds/autoadd/mad_matrix/snivyshortyawn.ogg",le
 L["snivytalk"]={{path="chatsounds/autoadd/mad_matrix/snivytalk/1.ogg",length=0.783},{path="chatsounds/autoadd/mad_matrix/snivytalk/2.ogg",length=0.431}}
 L["snivytired"]={{path="chatsounds/autoadd/mad_matrix/snivytired/1.ogg",length=1.503},{path="chatsounds/autoadd/mad_matrix/snivytired/2.ogg",length=2.172},{path="chatsounds/autoadd/mad_matrix/snivytired/3.ogg",length=1.453}}
 L["snivytwister"]={{path="chatsounds/autoadd/mad_matrix/snivytwister.ogg",length=1.683}}
+L["wweww"]={{path="chatsounds/autoadd/mad_matrix/wweww.ogg",length=0.727}}
+L["you wanna play the game you wanna play the game you wanna play with the big boys"]={{path="chatsounds/autoadd/mad_matrix/you wanna play the game you wanna play the game you wanna play with the big boys.ogg",length=2.604}}
+L["yuuy"]={{path="chatsounds/autoadd/mad_matrix/yuuy.ogg",length=0.880}}
 c.EndList()
