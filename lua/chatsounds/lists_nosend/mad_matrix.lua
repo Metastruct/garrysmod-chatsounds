bc35785e2c0408dcc9676b6d72735fea91f524a0
Brian Davis <kjones1999@outlook.com>
2017-09-10 00:40:10 -0400

    the raar manmanma

--- lua/chatsounds/lists_nosend/mad_matrix.lua
+++ lua/chatsounds/lists_nosend/mad_matrix.lua
@@ -1,6 +1,7 @@
 c.StartList("mad_matrix")
 L["aaa blyad"]={{path="chatsounds/autoadd/mad_matrix/aaa blyad.ogg",length=1.802}}
 L["bug flyby"]={{path="chatsounds/autoadd/mad_matrix/bug flyby.ogg",length=2.728}}
+L["cunt"]={{path="chatsounds/autoadd/mad_matrix/cunt/1.ogg",length=0.746},{path="chatsounds/autoadd/mad_matrix/cunt/2.ogg",length=0.526}}
 L["double soos power"]={{path="chatsounds/autoadd/mad_matrix/double soos power.ogg",length=3.685}}
 L["eeree"]={{path="chatsounds/autoadd/mad_matrix/eeree.ogg",length=0.636}}
 L["emit eht ta kool"]={{path="chatsounds/autoadd/mad_matrix/emit eht ta kool.ogg",length=2.229}}
@@ -14,6 +15,7 @@ L["nani"]={{path="chatsounds/autoadd/mad_matrix/nani.ogg",length=0.538}}
 L["nothing is working distorted"]={{path="chatsounds/autoadd/mad_matrix/nothing is working distorted.ogg",length=1.797}}
 L["oddgasp"]={{path="chatsounds/autoadd/mad_matrix/oddgasp.ogg",length=0.418}}
 L["ooh yeeaahh"]={{path="chatsounds/autoadd/mad_matrix/ooh yeeaahh.ogg",length=1.337}}
+L["put your finger on the button"]={{path="chatsounds/autoadd/mad_matrix/put your finger on the button.ogg",length=1.115}}
 L["slimespawn"]={{path="chatsounds/autoadd/mad_matrix/slimespawn/1.ogg",length=0.379},{path="chatsounds/autoadd/mad_matrix/slimespawn/2.ogg",length=0.488},{path="chatsounds/autoadd/mad_matrix/slimespawn/3.ogg",length=0.508},{path="chatsounds/autoadd/mad_matrix/slimespawn/4.ogg",length=0.564}}
 L["slimesplat"]={{path="chatsounds/autoadd/mad_matrix/slimesplat/1.ogg",length=0.342},{path="chatsounds/autoadd/mad_matrix/slimesplat/2.ogg",length=0.422}}
 L["slimetalk"]={{path="chatsounds/autoadd/mad_matrix/slimetalk/1.ogg",length=0.157},{path="chatsounds/autoadd/mad_matrix/slimetalk/10.ogg",length=0.253},{path="chatsounds/autoadd/mad_matrix/slimetalk/11.ogg",length=0.761},{path="chatsounds/autoadd/mad_matrix/slimetalk/12.ogg",length=0.620},{path="chatsounds/autoadd/mad_matrix/slimetalk/13.ogg",length=0.902},{path="chatsounds/autoadd/mad_matrix/slimetalk/14.ogg",length=1.349},{path="chatsounds/autoadd/mad_matrix/slimetalk/15.ogg",length=1.676},{path="chatsounds/autoadd/mad_matrix/slimetalk/16.ogg",length=0.854},{path="chatsounds/autoadd/mad_matrix/slimetalk/2.ogg",length=0.311},{path="chatsounds/autoadd/mad_matrix/slimetalk/3.ogg",length=0.447},{path="chatsounds/autoadd/mad_matrix/slimetalk/4.ogg",length=0.456},{path="chatsounds/autoadd/mad_matrix/slimetalk/5.ogg",length=0.303},{path="chatsounds/autoadd/mad_matrix/slimetalk/6.ogg",length=0.522},{path="chatsounds/autoadd/mad_matrix/slimetalk/7.ogg",length=0.629},{path="chatsounds/autoadd/mad_matrix/slimetalk/8.ogg",length=0.658},{path="chatsounds/autoadd/mad_matrix/slimetalk/9.ogg",length=0.602}}
