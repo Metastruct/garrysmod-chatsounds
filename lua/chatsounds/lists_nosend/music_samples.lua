d5552d418f0911bdce7b616fd9a7f7c6f1f6d5c3
PotcFdk <PotcFdk@gmx.net>
2017-09-13 23:06:11 +0200

    put your hands up for meta

--- lua/chatsounds/lists_nosend/music_samples.lua
+++ lua/chatsounds/lists_nosend/music_samples.lua
@@ -1,6 +1,25 @@
 c.StartList("music_samples")
+L["ba bam the bass bass"]={{path="chatsounds/autoadd/music_samples/ba bam the bass bass.ogg",length=1.555}}
+L["bam"]={{path="chatsounds/autoadd/music_samples/bam.ogg",length=0.347}}
+L["bam the bass bass"]={{path="chatsounds/autoadd/music_samples/bam the bass bass.ogg",length=1.555}}
+L["bass"]={{path="chatsounds/autoadd/music_samples/bass.ogg",length=0.482}}
 L["d-d-d-d-drop the bass"]={{path="chatsounds/autoadd/music_samples/d-d-d-d-drop the bass.mp3",length=1.700}}
+L["deeeeeeerb"]={{path="chatsounds/autoadd/music_samples/deeeeeeerb.ogg",length=5.853}}
+L["derb"]={{path="chatsounds/autoadd/music_samples/derb/derb.ogg",length=0.489},{path="chatsounds/autoadd/music_samples/derb/derb2.ogg",length=0.744}}
 L["drop the bass"]={{path="chatsounds/autoadd/music_samples/drop the bass.mp3",length=0.865}}
 L["drop the bass 2"]={{path="chatsounds/autoadd/music_samples/drop the bass 2.mp3",length=0.967}}
+L["go"]={{path="chatsounds/autoadd/music_samples/go.ogg",length=0.296}}
+L["go yes go"]={{path="chatsounds/autoadd/music_samples/go yes go.ogg",length=0.989}}
+L["i love this city"]={{path="chatsounds/autoadd/music_samples/i love this city.ogg",length=1.204}}
 L["ow wah ah ah ah"]={{path="chatsounds/autoadd/music_samples/ow wah ah ah ah.ogg",length=1.039}}
+L["pump"]={{path="chatsounds/autoadd/music_samples/pump.ogg",length=0.355}}
+L["pump it up"]={{path="chatsounds/autoadd/music_samples/pump it up.ogg",length=0.778}}
+L["pump it up it up it up"]={{path="chatsounds/autoadd/music_samples/pump it up it up it up.ogg",length=1.268}}
+L["put your hands up for detroit"]={{path="chatsounds/autoadd/music_samples/put your hands up for detroit.ogg",length=2.079}}
+L["rock rock rock the house"]={{path="chatsounds/autoadd/music_samples/rock rock rock the house.ogg",length=1.538}}
+L["rock rock the house the house the house"]={{path="chatsounds/autoadd/music_samples/rock rock the house the house the house.ogg",length=1.868}}
+L["rock the house"]={{path="chatsounds/autoadd/music_samples/rock the house.ogg",length=0.904}}
+L["the bass"]={{path="chatsounds/autoadd/music_samples/the bass.ogg",length=0.685}}
+L["yes go"]={{path="chatsounds/autoadd/music_samples/yes go.ogg",length=0.862}}
+L["yes to make the crowd yes go"]={{path="chatsounds/autoadd/music_samples/yes to make the crowd yes go.ogg",length=1.420}}
 c.EndList()
