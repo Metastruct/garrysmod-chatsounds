060819c494db9748f1de14a524d5e856d2abfd51
Brian Davis <kjones1999@outlook.com>
2017-10-03 20:40:34 -0400

    Minor fix of bombinomicon.lua
    Former-commit-id: a4fd39c2c1ff053394d2a5d43bb6ac741846117c

--- lua/chatsounds/lists_send/tf/bombinomicon.lua
+++ lua/chatsounds/lists_send/tf/bombinomicon.lua
@@ -3,11 +3,11 @@ L["ah ha thats right kill each other aw man it makes me feel good to see you guy
 L["everybody take a knee dear god thank you for making me the devil and putting all these great guys in hell pow zoop"]={{path="vo/halloween_bombinomicon/sf13_bcon_misc06.mp3",length=2.1399166584015}}
 L["get up here steal anything you like youre in hell how much worse could it get"]={{path="vo/halloween_bombinomicon/sf13_bcon_end01.mp3",length=1.3040000200272}}
 L["guys guys guys guys let me get real with you for a minute this is why i got into the devil business you you crazy nuts guys tearing each other apart like animals i think i got a burning cinder in my eye or something get some gifts and get out of here you nuts"]={{path="vo/halloween_bombinomicon/sf13_bcon_end12.mp3",length=6.1627707481384}}
-L["happy halloween you knuckleheads"]={{path="vo/halloween_bombinomicon/sf13_bcon_end03",length=0.75541669130325}}
+L["happy halloween you knuckleheads"]={{path="vo/halloween_bombinomicon/sf13_bcon_end03.mp3",length=0.75541669130325}}
 L["hey kill each other oh you guys are the best not you sniper"]={{path="vo/halloween_bombinomicon/sf13_bcon_misc03.mp3",length=1.2952916622162}}
 L["hey there you are heres a gift"]={{path="vo/halloween_bombinomicon/sf13_bcon_give_gift01.mp3",length=0.78154164552689}}
 L["hey look at us we all die together in hell yeah its pretty good"]={{path="vo/halloween_bombinomicon/sf13_bcon_misc10.mp3",length=1.5390833616257}}
-L["hey you brought souls goods stuff you guys are the best"]={{path="vo/halloween_bombinomicon/sf13_bcon_misc01.mp3",length=1.364937543869}}
+L["hey you brought souls good stuff you guys are the best"]={{path="vo/halloween_bombinomicon/sf13_bcon_misc01.mp3",length=1.364937543869}}
 L["hey you did it get up here grab some free crap you earned it"]={{path="vo/halloween_bombinomicon/sf13_bcon_end04.mp3",length=1.2778749465942}}
 L["hey you made it"]={{path="vo/halloween_bombinomicon/sf13_bcon_give_gift02.mp3",length=0.58127081394196}}
 L["hey you won grab some stuff its free"]={{path="vo/halloween_bombinomicon/sf13_bcon_give_gift04.mp3",length=0.89472913742065}}
