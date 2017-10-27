2148848648d1a21fc0b9b0c0fb504cd0a23c8163
V 1 N <32311306+V-1-N@users.noreply.github.com>
2017-10-27 18:56:14 -0400

    Hopefully i did not fuck this up (moar chatsounds)
    New to github, so chances of something not right is about 75%.
    
    My guess of what may be wrong is where the lua file is placed

---
+++ sound/chatsounds/autoadd/vin/vin.lua
@@ -0,0 +1,13 @@
+c.StartList("vin")
+L["fleelf"]={{path="chatsounds/autoadd/vin/fleelf.ogg",length=0.358}}
+L["flextapeglitch"]={{path="chatsounds/autoadd/vin/flextapeglitch.ogg",length=1.606}}
+L["i soos"]={{path="chatsounds/autoadd/vin/i soos.ogg",length=0.857}}
+L["mariogibberish"]={{path="chatsounds/autoadd/vin/mariogibberish.ogg",length=2.377}}
+L["mortis longdick"]={{path="chatsounds/autoadd/vin/mortis longdick.ogg",length=2.137}}
+L["now you can do the dishes"]={{path="chatsounds/autoadd/vin/now you can do the dishes.ogg",length=2.205}}
+L["oh"]={{path="chatsounds/autoadd/vin/oh.ogg",length=0.763}}
+L["u wot"]={{path="chatsounds/autoadd/vin/u wot.ogg",length=0.844}}
+L["wait wait what"]={{path="chatsounds/autoadd/vin/wait wait what.ogg",length=1.242}}
+L["what the actual fuck"]={{path="chatsounds/autoadd/vin/what the actual fuck.ogg",length=1.762}}
+L["yeeeeeeeee"]={{path="chatsounds/autoadd/vin/yeeeeeeeee.ogg",length=1.310}}
+c.EndList()
