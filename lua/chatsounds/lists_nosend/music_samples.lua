7575586d3961a33070a147a0ae5e8a1d69f52084
PotcFdk <PotcFdk@gmx.net>
2017-11-21 01:35:53 +0100

    Rename, move around and re-encode some sounds.

--- lua/chatsounds/lists_nosend/music_samples.lua
+++ lua/chatsounds/lists_nosend/music_samples.lua
@@ -3,11 +3,10 @@ L["ba bam the bass bass"]={{path="chatsounds/autoadd/music_samples/ba bam the ba
 L["bam"]={{path="chatsounds/autoadd/music_samples/bam.ogg",length=0.347}}
 L["bam the bass bass"]={{path="chatsounds/autoadd/music_samples/bam the bass bass.ogg",length=1.555}}
 L["bass"]={{path="chatsounds/autoadd/music_samples/bass.ogg",length=0.482}}
-L["d-d-d-d-drop the bass"]={{path="chatsounds/autoadd/music_samples/d-d-d-d-drop the bass.mp3",length=1.700}}
+L["dddddrop the bass"]={{path="chatsounds/autoadd/music_samples/dddddrop the bass.ogg",length=1.698}}
 L["deeeeeeerb"]={{path="chatsounds/autoadd/music_samples/deeeeeeerb.ogg",length=5.853}}
 L["derb"]={{path="chatsounds/autoadd/music_samples/derb/derb.ogg",length=0.489},{path="chatsounds/autoadd/music_samples/derb/derb2.ogg",length=0.744}}
-L["drop the bass"]={{path="chatsounds/autoadd/music_samples/drop the bass.mp3",length=0.865}}
-L["drop the bass 2"]={{path="chatsounds/autoadd/music_samples/drop the bass 2.mp3",length=0.967}}
+L["drop the bass"]={{path="chatsounds/autoadd/music_samples/drop the bass/drop the bass 1.ogg",length=0.862},{path="chatsounds/autoadd/music_samples/drop the bass/drop the bass 2.ogg",length=0.940}}
 L["go"]={{path="chatsounds/autoadd/music_samples/go.ogg",length=0.296}}
 L["go yes go"]={{path="chatsounds/autoadd/music_samples/go yes go.ogg",length=0.989}}
 L["i love this city"]={{path="chatsounds/autoadd/music_samples/i love this city.ogg",length=1.204}}
