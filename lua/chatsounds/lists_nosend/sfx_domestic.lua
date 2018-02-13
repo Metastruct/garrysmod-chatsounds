94ca9bff72f614718b3845e3a803d2ef0a5d5519
SpiralP <spiral.palmer@gmail.com>
2018-02-12 16:56:19 -0800

    people laughing

--- lua/chatsounds/lists_nosend/sfx_domestic.lua
+++ lua/chatsounds/lists_nosend/sfx_domestic.lua
@@ -1,73 +1,41 @@
 c.StartList("sfx_domestic")
-L["bathtub lock 1"]={{path="chatsounds/autoadd/sfx_domestic/bathtub lock 1.ogg",length=0.876}}
-L["brushing carpet 1"]={{path="chatsounds/autoadd/sfx_domestic/brushing carpet 1.ogg",length=3.537}}
-L["can open 1"]={{path="chatsounds/autoadd/sfx_domestic/can open 1.ogg",length=1.130}}
-L["can open 2"]={{path="chatsounds/autoadd/sfx_domestic/can open 2.ogg",length=1.101}}
-L["can to table 1"]={{path="chatsounds/autoadd/sfx_domestic/can to table 1.ogg",length=0.434}}
-L["can to table 2"]={{path="chatsounds/autoadd/sfx_domestic/can to table 2.ogg",length=0.367}}
-L["chair screech 1"]={{path="chatsounds/autoadd/sfx_domestic/chair screech 1.ogg",length=1.267}}
-L["cigarette lighter 1"]={{path="chatsounds/autoadd/sfx_domestic/cigarette lighter 1.ogg",length=0.467}}
-L["door close 1"]={{path="chatsounds/autoadd/sfx_domestic/door close 1.ogg",length=1.067}}
-L["door close 10"]={{path="chatsounds/autoadd/sfx_domestic/door close 10.ogg",length=1.242}}
-L["door close 11"]={{path="chatsounds/autoadd/sfx_domestic/door close 11.ogg",length=2.208}}
-L["door close 2"]={{path="chatsounds/autoadd/sfx_domestic/door close 2.ogg",length=1.663}}
-L["door close 3"]={{path="chatsounds/autoadd/sfx_domestic/door close 3.ogg",length=0.960}}
-L["door close 4"]={{path="chatsounds/autoadd/sfx_domestic/door close 4.ogg",length=1.668}}
-L["door close 5"]={{path="chatsounds/autoadd/sfx_domestic/door close 5.ogg",length=0.933}}
-L["door close 6"]={{path="chatsounds/autoadd/sfx_domestic/door close 6.ogg",length=1.367}}
-L["door close 7"]={{path="chatsounds/autoadd/sfx_domestic/door close 7.ogg",length=1.002}}
-L["door close 8"]={{path="chatsounds/autoadd/sfx_domestic/door close 8.ogg",length=1.493}}
-L["door close 9"]={{path="chatsounds/autoadd/sfx_domestic/door close 9.ogg",length=0.534}}
-L["door lock 1"]={{path="chatsounds/autoadd/sfx_domestic/door lock 1.ogg",length=0.968}}
-L["door lock 2"]={{path="chatsounds/autoadd/sfx_domestic/door lock 2.ogg",length=1.168}}
-L["door open 1"]={{path="chatsounds/autoadd/sfx_domestic/door open 1.ogg",length=0.788}}
-L["door open 2"]={{path="chatsounds/autoadd/sfx_domestic/door open 2.ogg",length=2.068}}
-L["door open 3"]={{path="chatsounds/autoadd/sfx_domestic/door open 3.ogg",length=0.895}}
-L["door open 4"]={{path="chatsounds/autoadd/sfx_domestic/door open 4.ogg",length=1.193}}
-L["door open 5"]={{path="chatsounds/autoadd/sfx_domestic/door open 5.ogg",length=1.000}}
-L["door open 6"]={{path="chatsounds/autoadd/sfx_domestic/door open 6.ogg",length=1.600}}
-L["door open 7"]={{path="chatsounds/autoadd/sfx_domestic/door open 7.ogg",length=1.235}}
+L["bathtub lock"]={{path="chatsounds/autoadd/sfx_domestic/bathtub lock.ogg",length=0.876}}
+L["brushing carpet"]={{path="chatsounds/autoadd/sfx_domestic/brushing carpet.ogg",length=3.537}}
+L["can open"]={{path="chatsounds/autoadd/sfx_domestic/can open/can open 1.ogg",length=1.130},{path="chatsounds/autoadd/sfx_domestic/can open/can open 2.ogg",length=1.101}}
+L["can to table"]={{path="chatsounds/autoadd/sfx_domestic/can to table/can to table 1.ogg",length=0.434},{path="chatsounds/autoadd/sfx_domestic/can to table/can to table 2.ogg",length=0.367}}
+L["chair screech"]={{path="chatsounds/autoadd/sfx_domestic/chair screech.ogg",length=1.267}}
+L["cigarette lighter"]={{path="chatsounds/autoadd/sfx_domestic/cigarette lighter.ogg",length=0.467}}
+L["door close"]={{path="chatsounds/autoadd/sfx_domestic/door close/door close 1.ogg",length=1.067},{path="chatsounds/autoadd/sfx_domestic/door close/door close 10.ogg",length=1.242},{path="chatsounds/autoadd/sfx_domestic/door close/door close 11.ogg",length=2.208},{path="chatsounds/autoadd/sfx_domestic/door close/door close 2.ogg",length=1.663},{path="chatsounds/autoadd/sfx_domestic/door close/door close 3.ogg",length=0.960},{path="chatsounds/autoadd/sfx_domestic/door close/door close 4.ogg",length=1.668},{path="chatsounds/autoadd/sfx_domestic/door close/door close 5.ogg",length=0.933},{path="chatsounds/autoadd/sfx_domestic/door close/door close 6.ogg",length=1.367},{path="chatsounds/autoadd/sfx_domestic/door close/door close 7.ogg",length=1.002},{path="chatsounds/autoadd/sfx_domestic/door close/door close 8.ogg",length=1.493},{path="chatsounds/autoadd/sfx_domestic/door close/door close 9.ogg",length=0.534}}
+L["door lock"]={{path="chatsounds/autoadd/sfx_domestic/door lock/door lock 1.ogg",length=0.968},{path="chatsounds/autoadd/sfx_domestic/door lock/door lock 2.ogg",length=1.168}}
+L["door open"]={{path="chatsounds/autoadd/sfx_domestic/door open/door open 1.ogg",length=0.788},{path="chatsounds/autoadd/sfx_domestic/door open/door open 2.ogg",length=2.068},{path="chatsounds/autoadd/sfx_domestic/door open/door open 3.ogg",length=0.895},{path="chatsounds/autoadd/sfx_domestic/door open/door open 4.ogg",length=1.193},{path="chatsounds/autoadd/sfx_domestic/door open/door open 5.ogg",length=1.000},{path="chatsounds/autoadd/sfx_domestic/door open/door open 6.ogg",length=1.600},{path="chatsounds/autoadd/sfx_domestic/door open/door open 7.ogg",length=1.235}}
 L["door sliding"]={{path="chatsounds/autoadd/sfx_domestic/door sliding.ogg",length=2.536}}
-L["door squeak 2"]={{path="chatsounds/autoadd/sfx_domestic/door squeak 2.ogg",length=2.090}}
-L["door squeak 3"]={{path="chatsounds/autoadd/sfx_domestic/door squeak 3.ogg",length=1.557}}
-L["floor squeak 1"]={{path="chatsounds/autoadd/sfx_domestic/floor squeak 1.ogg",length=1.557}}
-L["floor squeak 2"]={{path="chatsounds/autoadd/sfx_domestic/floor squeak 2.ogg",length=1.450}}
-L["keys catch 1"]={{path="chatsounds/autoadd/sfx_domestic/keys catch 1.ogg",length=1.533}}
-L["keys drop 1"]={{path="chatsounds/autoadd/sfx_domestic/keys drop 1.ogg",length=0.700}}
-L["keys drop 2"]={{path="chatsounds/autoadd/sfx_domestic/keys drop 2.ogg",length=0.700}}
-L["light switch 1"]={{path="chatsounds/autoadd/sfx_domestic/light switch 1.ogg",length=0.490}}
-L["light switch 2"]={{path="chatsounds/autoadd/sfx_domestic/light switch 2.ogg",length=0.853}}
-L["light switch 3"]={{path="chatsounds/autoadd/sfx_domestic/light switch 3.ogg",length=0.362}}
-L["matches 1"]={{path="chatsounds/autoadd/sfx_domestic/matches 1.ogg",length=1.173}}
-L["matches scratch 1"]={{path="chatsounds/autoadd/sfx_domestic/matches scratch 1.ogg",length=0.660}}
-L["microwave oven close 1"]={{path="chatsounds/autoadd/sfx_domestic/microwave oven close 1.ogg",length=1.400}}
-L["microwave oven open 1"]={{path="chatsounds/autoadd/sfx_domestic/microwave oven open 1.ogg",length=1.267}}
-L["microwave oven timer 1"]={{path="chatsounds/autoadd/sfx_domestic/microwave oven timer 1.ogg",length=1.500}}
-L["oven door 1"]={{path="chatsounds/autoadd/sfx_domestic/oven door 1.ogg",length=1.529}}
-L["phone dial 2"]={{path="chatsounds/autoadd/sfx_domestic/phone dial 2.ogg",length=1.868}}
-L["phone dial 3"]={{path="chatsounds/autoadd/sfx_domestic/phone dial 3.ogg",length=1.100}}
-L["phone hang up 1"]={{path="chatsounds/autoadd/sfx_domestic/phone hang up 1.ogg",length=0.537}}
-L["phone hang up 2"]={{path="chatsounds/autoadd/sfx_domestic/phone hang up 2.ogg",length=0.406}}
-L["phone hang up 3"]={{path="chatsounds/autoadd/sfx_domestic/phone hang up 3.ogg",length=1.000}}
-L["phone hang up 4"]={{path="chatsounds/autoadd/sfx_domestic/phone hang up 4.ogg",length=0.700}}
-L["phone pick up 1"]={{path="chatsounds/autoadd/sfx_domestic/phone pick up 1.ogg",length=0.800}}
-L["pot lid close 1"]={{path="chatsounds/autoadd/sfx_domestic/pot lid close 1.ogg",length=0.512}}
-L["pot lid close 2"]={{path="chatsounds/autoadd/sfx_domestic/pot lid close 2.ogg",length=2.218}}
-L["pouring liquid 2"]={{path="chatsounds/autoadd/sfx_domestic/pouring liquid 2.ogg",length=2.202}}
-L["scissors 2"]={{path="chatsounds/autoadd/sfx_domestic/scissors 2.ogg",length=1.130}}
-L["scissors 3"]={{path="chatsounds/autoadd/sfx_domestic/scissors 3.ogg",length=1.833}}
+L["door squeak"]={{path="chatsounds/autoadd/sfx_domestic/door squeak/door squeak 2.ogg",length=2.090},{path="chatsounds/autoadd/sfx_domestic/door squeak/door squeak 3.ogg",length=1.557}}
+L["floor squeak"]={{path="chatsounds/autoadd/sfx_domestic/floor squeak/floor squeak 1.ogg",length=1.557},{path="chatsounds/autoadd/sfx_domestic/floor squeak/floor squeak 2.ogg",length=1.450}}
+L["keys catch"]={{path="chatsounds/autoadd/sfx_domestic/keys catch.ogg",length=1.533}}
+L["keys drop"]={{path="chatsounds/autoadd/sfx_domestic/keys drop/keys drop 1.ogg",length=0.700},{path="chatsounds/autoadd/sfx_domestic/keys drop/keys drop 2.ogg",length=0.700}}
+L["light switch"]={{path="chatsounds/autoadd/sfx_domestic/light switch/light switch 1.ogg",length=0.490},{path="chatsounds/autoadd/sfx_domestic/light switch/light switch 2.ogg",length=0.853},{path="chatsounds/autoadd/sfx_domestic/light switch/light switch 3.ogg",length=0.362}}
+L["matches"]={{path="chatsounds/autoadd/sfx_domestic/matches.ogg",length=1.173}}
+L["matches scratch"]={{path="chatsounds/autoadd/sfx_domestic/matches scratch.ogg",length=0.660}}
+L["microwave oven close"]={{path="chatsounds/autoadd/sfx_domestic/microwave oven close.ogg",length=1.400}}
+L["microwave oven open"]={{path="chatsounds/autoadd/sfx_domestic/microwave oven open.ogg",length=1.267}}
+L["microwave oven timer"]={{path="chatsounds/autoadd/sfx_domestic/microwave oven timer.ogg",length=1.500}}
+L["oven door"]={{path="chatsounds/autoadd/sfx_domestic/oven door.ogg",length=1.529}}
+L["phone dial"]={{path="chatsounds/autoadd/sfx_domestic/phone dial/phone dial 2.ogg",length=1.868},{path="chatsounds/autoadd/sfx_domestic/phone dial/phone dial 3.ogg",length=1.100}}
+L["phone hang up"]={{path="chatsounds/autoadd/sfx_domestic/phone hang up/phone hang up 1.ogg",length=0.537},{path="chatsounds/autoadd/sfx_domestic/phone hang up/phone hang up 2.ogg",length=0.406},{path="chatsounds/autoadd/sfx_domestic/phone hang up/phone hang up 3.ogg",length=1.000},{path="chatsounds/autoadd/sfx_domestic/phone hang up/phone hang up 4.ogg",length=0.700}}
+L["phone pick up"]={{path="chatsounds/autoadd/sfx_domestic/phone pick up.ogg",length=0.800}}
+L["pot lid close"]={{path="chatsounds/autoadd/sfx_domestic/pot lid close/pot lid close 1.ogg",length=0.512},{path="chatsounds/autoadd/sfx_domestic/pot lid close/pot lid close 2.ogg",length=2.218}}
+L["pouring liquid"]={{path="chatsounds/autoadd/sfx_domestic/pouring liquid.ogg",length=2.202}}
+L["scissors"]={{path="chatsounds/autoadd/sfx_domestic/scissors/scissors 2.ogg",length=1.130},{path="chatsounds/autoadd/sfx_domestic/scissors/scissors 3.ogg",length=1.833}}
 L["shaker"]={{path="chatsounds/autoadd/sfx_domestic/shaker.ogg",length=3.570}}
-L["shaking can 2"]={{path="chatsounds/autoadd/sfx_domestic/shaking can 2.ogg",length=2.156}}
-L["sharpening knife 1"]={{path="chatsounds/autoadd/sfx_domestic/sharpening knife 1.ogg",length=2.260}}
-L["shaving cream 1"]={{path="chatsounds/autoadd/sfx_domestic/shaving cream 1.ogg",length=3.671}}
+L["shaking can"]={{path="chatsounds/autoadd/sfx_domestic/shaking can.ogg",length=2.156}}
+L["sharpening knife"]={{path="chatsounds/autoadd/sfx_domestic/sharpening knife.ogg",length=2.260}}
+L["shaving cream"]={{path="chatsounds/autoadd/sfx_domestic/shaving cream.ogg",length=3.671}}
 L["shower curtain"]={{path="chatsounds/autoadd/sfx_domestic/shower curtain.ogg",length=1.168}}
 L["shower door"]={{path="chatsounds/autoadd/sfx_domestic/shower door.ogg",length=2.836}}
-L["stapler 1"]={{path="chatsounds/autoadd/sfx_domestic/stapler 1.ogg",length=0.567}}
-L["toaster 1"]={{path="chatsounds/autoadd/sfx_domestic/toaster 1.ogg",length=0.767}}
-L["toaster 2"]={{path="chatsounds/autoadd/sfx_domestic/toaster 2.ogg",length=0.934}}
-L["washing hands 3"]={{path="chatsounds/autoadd/sfx_domestic/washing hands 3.ogg",length=5.799}}
-L["water drop 1"]={{path="chatsounds/autoadd/sfx_domestic/water drop 1.ogg",length=0.767}}
+L["stapler"]={{path="chatsounds/autoadd/sfx_domestic/stapler.ogg",length=0.567}}
+L["toaster"]={{path="chatsounds/autoadd/sfx_domestic/toaster/toaster 1.ogg",length=0.767},{path="chatsounds/autoadd/sfx_domestic/toaster/toaster 2.ogg",length=0.934}}
+L["washing hands"]={{path="chatsounds/autoadd/sfx_domestic/washing hands.ogg",length=5.799}}
+L["water drop"]={{path="chatsounds/autoadd/sfx_domestic/water drop.ogg",length=0.767}}
 L["whipping cream"]={{path="chatsounds/autoadd/sfx_domestic/whipping cream.ogg",length=1.969}}
-L["wine glass clink 1"]={{path="chatsounds/autoadd/sfx_domestic/wine glass clink 1.ogg",length=3.605}}
-L["wine glass clink 2"]={{path="chatsounds/autoadd/sfx_domestic/wine glass clink 2.ogg",length=1.600}}
+L["wine glass clink"]={{path="chatsounds/autoadd/sfx_domestic/wine glass clink/wine glass clink 1.ogg",length=3.605},{path="chatsounds/autoadd/sfx_domestic/wine glass clink/wine glass clink 2.ogg",length=1.600}}
 c.EndList()
