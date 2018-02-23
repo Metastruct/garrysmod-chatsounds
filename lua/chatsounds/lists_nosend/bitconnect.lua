2492d7ac08818e14a9e7af6e25ff09e7a77798a0
Brian Davis <kjones1999@outlook.com>
2018-02-23 18:55:43 -0500

    wasowasowasowassuuuup bitconneeect

---
+++ lua/chatsounds/lists_nosend/bitconnect.lua
@@ -0,0 +1,18 @@
+c.StartList("bitconnect")
+L["bitconnaaaaaaaaaaaact"]={{path="chatsounds/autoadd/bitconnect/bitconnaaaaaaaaaaaact.ogg",length=4.886}}
+L["bitconnect"]={{path="chatsounds/autoadd/bitconnect/bitconnect.ogg",length=1.709}}
+L["bitconneeeeeeeeeeeect"]={{path="chatsounds/autoadd/bitconnect/bitconneeeeeeeeeeeect/1.ogg",length=4.585},{path="chatsounds/autoadd/bitconnect/bitconneeeeeeeeeeeect/2.ogg",length=5.214}}
+L["hahahaha"]={{path="chatsounds/autoadd/bitconnect/hahahaha.ogg",length=1.812}}
+L["i loooooooooooooooooove"]={{path="chatsounds/autoadd/bitconnect/i loooooooooooooooooove.ogg",length=7.687}}
+L["nononono thats a scam"]={{path="chatsounds/autoadd/bitconnect/nononono thats a scam.ogg",length=2.672}}
+L["then i said to em i say to them you know what"]={{path="chatsounds/autoadd/bitconnect/then i said to em i say to them you know what.ogg",length=1.801}}
+L["then shes gonna say"]={{path="chatsounds/autoadd/bitconnect/then shes gonna say.ogg",length=0.982}}
+L["wasowasowasowassuuuup bitconneeect"]={{path="chatsounds/autoadd/bitconnect/wasowasowasowassuuuup bitconneeect.ogg",length=6.431}}
+L["what am i gonna do"]={{path="chatsounds/autoadd/bitconnect/what am i gonna do.ogg",length=2.122}}
+L["when i yun started to put ten thousand dos a dayum in hur right on her you know on her table"]={{path="chatsounds/autoadd/bitconnect/when i yun started to put ten thousand dos a dayum in hur right on her you know on her table.ogg",length=6.245}}
+L["whooaa"]={{path="chatsounds/autoadd/bitconnect/whooaa.ogg",length=1.036}}
+L["whooaa hahahaha"]={{path="chatsounds/autoadd/bitconnect/whooaa hahahaha.ogg",length=2.993}}
+L["woow"]={{path="chatsounds/autoadd/bitconnect/woow.ogg",length=1.363}}
+L["wowowowo whassup"]={{path="chatsounds/autoadd/bitconnect/wowowowo whassup.ogg",length=2.308}}
+L["wwhatt"]={{path="chatsounds/autoadd/bitconnect/wwhatt.ogg",length=1.395}}
+c.EndList()
