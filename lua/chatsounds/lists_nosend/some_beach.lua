16e5aba2c84527b49e11387085b03288d1183cc6
ajloveslily14 <ajloveslily@gmail.com>
2018-02-27 01:25:46 -0600

    is anyone there? oh...hi!

---
+++ lua/chatsounds/lists_nosend/some_beach.lua
@@ -0,0 +1,5 @@
+c.StartList("some_beach")
+L["hi"]={{path="chatsounds/autoadd/some_beach/hi.ogg",length=0.471}}
+L["is anyone there"]={{path="chatsounds/autoadd/some_beach/is anyone there.ogg",length=1.694}}
+L["oh"]={{path="chatsounds/autoadd/some_beach/oh.ogg",length=0.483}}
+c.EndList()
