7a9c3db4e87d7dc572c76b24dad631e626f2f9d8
PotcFdk <PotcFdk@gmx.net>
2018-03-01 11:04:42 +0100

    The world is not anymore the way it used to be! Mmhmmhmmh, no no no!

--- lua/chatsounds/lists_nosend/bitconnect.lua
+++ lua/chatsounds/lists_nosend/bitconnect.lua
@@ -5,10 +5,14 @@ L["bitconneeeeeeeeeeeect"]={{path="chatsounds/autoadd/bitconnect/bitconneeeeeeee
 L["hahahaha"]={{path="chatsounds/autoadd/bitconnect/hahahaha.ogg",length=1.812}}
 L["heheheeeeyyyyyy"]={{path="chatsounds/autoadd/bitconnect/heheheeeeyyyyyy/1.ogg",length=2.879},{path="chatsounds/autoadd/bitconnect/heheheeeeyyyyyy/2.ogg",length=2.914},{path="chatsounds/autoadd/bitconnect/heheheeeeyyyyyy/3.ogg",length=2.895}}
 L["i loooooooooooooooooove"]={{path="chatsounds/autoadd/bitconnect/i loooooooooooooooooove.ogg",length=7.687}}
+L["let me tell you"]={{path="chatsounds/autoadd/bitconnect/let me tell you.ogg",length=0.968}}
+L["mmm no no no"]={{path="chatsounds/autoadd/bitconnect/mmm no no no.ogg",length=1.591}}
 L["nononono thats a scam"]={{path="chatsounds/autoadd/bitconnect/nononono thats a scam.ogg",length=2.672}}
+L["the world is not anymore the way it used to be"]={{path="chatsounds/autoadd/bitconnect/the world is not anymore the way it used to be.ogg",length=2.848}}
 L["then i said to em i say to them you know what"]={{path="chatsounds/autoadd/bitconnect/then i said to em i say to them you know what.ogg",length=1.801}}
 L["then shes gonna say"]={{path="chatsounds/autoadd/bitconnect/then shes gonna say.ogg",length=0.982}}
 L["wasowasowasowassuuuup bitconneeect"]={{path="chatsounds/autoadd/bitconnect/wasowasowasowassuuuup bitconneeect.ogg",length=6.431}}
+L["we are really changing the world as we know it"]={{path="chatsounds/autoadd/bitconnect/we are really changing the world as we know it.ogg",length=3.160}}
 L["what am i gonna do"]={{path="chatsounds/autoadd/bitconnect/what am i gonna do.ogg",length=2.122}}
 L["when i yun started to put ten thousand dos a dayum in hur right on her you know on her table"]={{path="chatsounds/autoadd/bitconnect/when i yun started to put ten thousand dos a dayum in hur right on her you know on her table.ogg",length=6.245}}
 L["whooaa"]={{path="chatsounds/autoadd/bitconnect/whooaa.ogg",length=1.036}}
