6d54fa0e1264122273af9d1b3cdaac34abd513a6
SpiralP <spiral.palmer@gmail.com>
2018-03-07 14:50:20 -0800

    move files to correct locations

--- lua/chatsounds/lists_nosend/ahamudo.lua
+++ lua/chatsounds/lists_nosend/ahamudo.lua
@@ -1,7 +1,5 @@
 c.StartList("ahamudo")
-L["doug demuro"]={{path="chatsounds/autoadd/ahamudo/doug demuro/this bumper that bumper its all covered under warranty.ogg",length=3.529},{path="chatsounds/autoadd/ahamudo/doug demuro/unlimited mileage bumper to bumper warranty.ogg",length=5.181}}
 L["sore wa osesugiru haha desu"]={{path="chatsounds/autoadd/ahamudo/sore wa osesugiru haha desu.ogg",length=2.756}}
 L["squaaw"]={{path="chatsounds/autoadd/ahamudo/squaaw.ogg",length=1.646}}
-L["touhou"]={{path="chatsounds/autoadd/ahamudo/touhou/nuclearfusion.ogg",length=40.046},{path="chatsounds/autoadd/ahamudo/touhou/touhou death.ogg",length=1.985}}
 L["watashi wa sube te mi takimashta"]={{path="chatsounds/autoadd/ahamudo/watashi wa sube te mi takimashta.ogg",length=3.164}}
 c.EndList()
