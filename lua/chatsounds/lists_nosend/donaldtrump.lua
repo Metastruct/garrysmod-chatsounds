3c3d0799ea0df2aaaccbf64ac5a8f4914122b168
Brian Davis <kjones1999@outlook.com>
2018-03-08 00:27:30 -0500

    im watching the debate and she disappeared

--- lua/chatsounds/lists_nosend/donaldtrump.lua
+++ lua/chatsounds/lists_nosend/donaldtrump.lua
@@ -1,7 +1,16 @@
 c.StartList("donaldtrump")
+L["i i know where she went"]={{path="chatsounds/autoadd/donaldtrump/i i know where she went.ogg",length=2.179}}
+L["i thought she quit i thought she gave up"]={{path="chatsounds/autoadd/donaldtrump/i thought she quit i thought she gave up.ogg",length=2.573}}
 L["im donald trump reminding you to fuck yourself you got it"]={{path="chatsounds/autoadd/donaldtrump/im donald trump reminding you to fuck yourself you got it.ogg",length=4.246}}
+L["im watching the debate and she disappeared"]={{path="chatsounds/autoadd/donaldtrump/im watching the debate and she disappeared.ogg",length=3.132}}
+L["its disgusting i dont want to talk about"]={{path="chatsounds/autoadd/donaldtrump/its disgusting i dont want to talk about.ogg",length=2.066}}
 L["my father gave me a small loan of a million dollars"]={{path="chatsounds/autoadd/donaldtrump/my father gave me a small loan of a million dollars.ogg",length=2.324}}
+L["no its too disgusting"]={{path="chatsounds/autoadd/donaldtrump/no its too disgusting.ogg",length=1.421}}
 L["small loan of a million dollars"]={{path="chatsounds/autoadd/donaldtrump/small loan of a million dollars.ogg",length=1.443}}
+L["so hilarys going to get beaten but i havent started with hilary yet"]={{path="chatsounds/autoadd/donaldtrump/so hilarys going to get beaten but i havent started with hilary yet.ogg",length=3.356}}
 L["ssmmaall llooaann ooff aa mmiilliioonn ddoollaarrss"]={{path="chatsounds/autoadd/donaldtrump/ssmmaall llooaann ooff aa mmiilliioonn ddoollaarrss.ogg",length=2.387}}
+L["what happened to her"]={{path="chatsounds/autoadd/donaldtrump/what happened to her.ogg",length=1.255}}
+L["where did she go"]={{path="chatsounds/autoadd/donaldtrump/where did she go/1.ogg",length=1.085},{path="chatsounds/autoadd/donaldtrump/where did she go/2.ogg",length=1.430}}
+L["where did she go where did hilary go"]={{path="chatsounds/autoadd/donaldtrump/where did she go where did hilary go.ogg",length=2.532}}
 L["you know i i started off in brooklyn"]={{path="chatsounds/autoadd/donaldtrump/you know i i started off in brooklyn.ogg",length=1.702}}
 c.EndList()
