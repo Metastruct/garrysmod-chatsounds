471ac2eb6a16ddd69386e615c86640b0e18b41c8
Ahmad <theu8ahmad@gmail.com>
2018-03-08 10:45:28 +0200

    Revert "preprocessed"
    This reverts commit 9db76ced5f3aa5a2b17bcb4a53d1705840c6b3ae.

--- lua/chatsounds/lists_nosend/ahamudo.lua
+++ lua/chatsounds/lists_nosend/ahamudo.lua
@@ -1,20 +1,5 @@
 c.StartList("ahamudo")
-L["awwh theres everything inside of here noooo"]={{path="chatsounds/autoadd/ahamudo/awwh theres everything inside of here noooo.ogg",length=4.238}}
-L["bhd sobbing"]={{path="chatsounds/autoadd/ahamudo/bhd sobbing.ogg",length=7.709}}
-L["bitch you guessed it hoo"]={{path="chatsounds/autoadd/ahamudo/bitch you guessed it hoo.ogg",length=2.473}}
-L["e e e e e e e e eh"]={{path="chatsounds/autoadd/ahamudo/e e e e e e e e eh.ogg",length=1.474}}
-L["hah heha hah"]={{path="chatsounds/autoadd/ahamudo/hah heha hah.ogg",length=1.161}}
-L["huhaa"]={{path="chatsounds/autoadd/ahamudo/huhaa.ogg",length=0.665}}
-L["i know all about dick and balls"]={{path="chatsounds/autoadd/ahamudo/i know all about dick and balls.ogg",length=3.367}}
-L["n n nipplefuck"]={{path="chatsounds/autoadd/ahamudo/n n nipplefuck.ogg",length=1.718}}
-L["nigga no i cant show this on camera"]={{path="chatsounds/autoadd/ahamudo/nigga no i cant show this on camera.ogg",length=5.306}}
-L["nigga what am i doin"]={{path="chatsounds/autoadd/ahamudo/nigga what am i doin.ogg",length=1.393}}
-L["ohh hohohoo oh thats terrible"]={{path="chatsounds/autoadd/ahamudo/ohh hohohoo oh thats terrible.ogg",length=3.442}}
-L["pull out your titties"]={{path="chatsounds/autoadd/ahamudo/pull out your titties.ogg",length=0.804}}
 L["sore wa osesugiru haha desu"]={{path="chatsounds/autoadd/ahamudo/sore wa osesugiru haha desu.ogg",length=2.756}}
 L["squaaw"]={{path="chatsounds/autoadd/ahamudo/squaaw.ogg",length=1.646}}
-L["use your titties"]={{path="chatsounds/autoadd/ahamudo/use your titties.ogg",length=0.722}}
-L["wait wtf no noo nooo"]={{path="chatsounds/autoadd/ahamudo/wait wtf no noo nooo.ogg",length=3.297}}
 L["watashi wa sube te mi takimashta"]={{path="chatsounds/autoadd/ahamudo/watashi wa sube te mi takimashta.ogg",length=3.164}}
-L["you is right"]={{path="chatsounds/autoadd/ahamudo/you is right.ogg",length=1.718}}
 c.EndList()
