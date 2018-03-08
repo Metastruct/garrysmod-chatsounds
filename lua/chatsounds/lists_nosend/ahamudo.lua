fe77d613d91fb73a6eeb29b059fe25001a9287ef
Ahmad <theu8ahmad@gmail.com>
2018-03-08 07:59:09 +0200

    a pinch of chatsounds (#149)
    * that really rattles my bumpers
    
    * a bit of deletion
    
    * move files to correct locations
    
    * add bf3 sounds

---
+++ lua/chatsounds/lists_nosend/ahamudo.lua
@@ -0,0 +1,5 @@
+c.StartList("ahamudo")
+L["sore wa osesugiru haha desu"]={{path="chatsounds/autoadd/ahamudo/sore wa osesugiru haha desu.ogg",length=2.756}}
+L["squaaw"]={{path="chatsounds/autoadd/ahamudo/squaaw.ogg",length=1.646}}
+L["watashi wa sube te mi takimashta"]={{path="chatsounds/autoadd/ahamudo/watashi wa sube te mi takimashta.ogg",length=3.164}}
+c.EndList()
