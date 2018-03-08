4d51b8f2873105ea910ef8b64b1402ccec3458ba
SpiralP <spiral.palmer@gmail.com>
2018-03-07 21:56:20 -0800

    add bf3 sounds

---
+++ lua/chatsounds/lists_nosend/bf3.lua
@@ -0,0 +1,7 @@
+c.StartList("bf3")
+L["backup where the fuck is it"]={{path="chatsounds/autoadd/bf3/backup where the fuck is it.ogg",length=2.775}}
+L["give me some fucking backup"]={{path="chatsounds/autoadd/bf3/give me some fucking backup.ogg",length=2.289}}
+L["give me some fucking backup now"]={{path="chatsounds/autoadd/bf3/give me some fucking backup now.ogg",length=2.476}}
+L["somebody give me some fucking backup please"]={{path="chatsounds/autoadd/bf3/somebody give me some fucking backup please.ogg",length=3.447}}
+L["wheres the fucking backup man"]={{path="chatsounds/autoadd/bf3/wheres the fucking backup man.ogg",length=2.577}}
+c.EndList()
