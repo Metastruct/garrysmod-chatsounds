927fa82cd721a76da957129dee9f417bce531a4e
SpiralP <spiral.palmer@gmail.com>
2018-03-10 18:39:20 -0800

    my wife still doesnt believe in me

--- lua/chatsounds/lists_nosend/bitconnect.lua
+++ lua/chatsounds/lists_nosend/bitconnect.lua
@@ -4,14 +4,17 @@ L["bitconnect"]={{path="chatsounds/autoadd/bitconnect/bitconnect.ogg",length=1.7
 L["bitconneeeeeeeeeeeect"]={{path="chatsounds/autoadd/bitconnect/bitconneeeeeeeeeeeect/1.ogg",length=4.585},{path="chatsounds/autoadd/bitconnect/bitconneeeeeeeeeeeect/2.ogg",length=5.214},{path="chatsounds/autoadd/bitconnect/bitconneeeeeeeeeeeect/3.ogg",length=17.043}}
 L["hahahaha"]={{path="chatsounds/autoadd/bitconnect/hahahaha.ogg",length=1.812}}
 L["heheheeeeyyyyyy"]={{path="chatsounds/autoadd/bitconnect/heheheeeeyyyyyy/1.ogg",length=2.879},{path="chatsounds/autoadd/bitconnect/heheheeeeyyyyyy/2.ogg",length=2.914},{path="chatsounds/autoadd/bitconnect/heheheeeeyyyyyy/3.ogg",length=2.895}}
+L["i am right now independently financially independently"]={{path="chatsounds/autoadd/bitconnect/i am right now independently financially independently.ogg",length=5.800}}
 L["i loooooooooooooooooove"]={{path="chatsounds/autoadd/bitconnect/i loooooooooooooooooove.ogg",length=7.687}}
 L["let me tell you"]={{path="chatsounds/autoadd/bitconnect/let me tell you.ogg",length=0.968}}
 L["mmm no no no"]={{path="chatsounds/autoadd/bitconnect/mmm no no no.ogg",length=1.591}}
+L["my wife still doesnt believe in me"]={{path="chatsounds/autoadd/bitconnect/my wife still doesnt believe in me.ogg",length=2.674}}
 L["nononono thats a scam"]={{path="chatsounds/autoadd/bitconnect/nononono thats a scam.ogg",length=2.672}}
 L["the world is not anymore the way it used to be"]={{path="chatsounds/autoadd/bitconnect/the world is not anymore the way it used to be.ogg",length=2.848}}
 L["then i said to em i say to them you know what"]={{path="chatsounds/autoadd/bitconnect/then i said to em i say to them you know what.ogg",length=1.801}}
 L["then shes gonna say"]={{path="chatsounds/autoadd/bitconnect/then shes gonna say.ogg",length=0.982}}
 L["wasowasowasowassuuuup bitconneeect"]={{path="chatsounds/autoadd/bitconnect/wasowasowasowassuuuup bitconneeect.ogg",length=6.431}}
+L["we are coming and we are coming in waves"]={{path="chatsounds/autoadd/bitconnect/we are coming and we are coming in waves.ogg",length=4.696}}
 L["we are really changing the world as we know it"]={{path="chatsounds/autoadd/bitconnect/we are really changing the world as we know it.ogg",length=3.160}}
 L["what am i gonna do"]={{path="chatsounds/autoadd/bitconnect/what am i gonna do.ogg",length=2.122}}
 L["when i yun started to put ten thousand dos a dayum in hur right on her you know on her table"]={{path="chatsounds/autoadd/bitconnect/when i yun started to put ten thousand dos a dayum in hur right on her you know on her table.ogg",length=6.245}}
