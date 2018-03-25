acd4a5ef09877693b5555c065d977e9c99ac5bdd
Meta Automator <MetaAutomator@users.noreply.github.com>
2018-03-25 15:31:39 +0000

    chatsounds preprocessor (691414f2348e72b9e953d7de09ced3aeb5bc43ec)
    [ci skip]

--- lua/chatsounds/lists_nosend/loser.lua
+++ lua/chatsounds/lists_nosend/loser.lua
@@ -1,13 +1,25 @@
 c.StartList("loser")
+L["ah shit here comes pacman"]={{path="chatsounds/autoadd/loser/ah shit here comes pacman.ogg",length=3.029}}
+L["hey pacman whats up"]={{path="chatsounds/autoadd/loser/hey pacman whats up.ogg",length=1.769}}
 L["hi welcome to chilis"]={{path="chatsounds/autoadd/loser/hi welcome to chilis.ogg",length=1.739}}
 L["im a im a frog now"]={{path="chatsounds/autoadd/loser/im a im a frog now.ogg",length=2.109}}
+L["kfc"]={{path="chatsounds/autoadd/loser/kfc.ogg",length=0.625}}
+L["kfc and watermelon"]={{path="chatsounds/autoadd/loser/kfc and watermelon.ogg",length=1.243}}
+L["me you bitches im high on crack"]={{path="chatsounds/autoadd/loser/me you bitches im high on crack.ogg",length=3.049}}
+L["no pacman drugs are bad"]={{path="chatsounds/autoadd/loser/no pacman drugs are bad.ogg",length=2.501}}
 L["no respect for anything except your potions"]={{path="chatsounds/autoadd/loser/no respect for anything except your potions.ogg",length=4.211}}
 L["no way my wife is a frog"]={{path="chatsounds/autoadd/loser/no way my wife is a frog.ogg",length=1.440}}
+L["nope cant help you maam"]={{path="chatsounds/autoadd/loser/nope cant help you maam.ogg",length=1.620}}
+L["pussies"]={{path="chatsounds/autoadd/loser/pussies.ogg",length=1.372}}
 L["thats fucking impossible"]={{path="chatsounds/autoadd/loser/thats fucking impossible.ogg",length=1.391}}
+L["wanna freebase"]={{path="chatsounds/autoadd/loser/wanna freebase.ogg",length=2.040}}
+L["watermelon"]={{path="chatsounds/autoadd/loser/watermelon.ogg",length=0.531}}
 L["when my potions can do anything that you cant"]={{path="chatsounds/autoadd/loser/when my potions can do anything that you cant.ogg",length=3.204}}
 L["where is i wanna i would like to see her"]={{path="chatsounds/autoadd/loser/where is i wanna i would like to see her.ogg",length=1.872}}
+L["whoa holy shit"]={{path="chatsounds/autoadd/loser/whoa holy shit.ogg",length=3.056}}
 L["why respect knights"]={{path="chatsounds/autoadd/loser/why respect knights.ogg",length=1.324}}
 L["yeah i ban him i ban him"]={{path="chatsounds/autoadd/loser/yeah i ban him i ban him.ogg",length=3.006}}
+L["yo yo yo yo what it is motherfuckers"]={{path="chatsounds/autoadd/loser/yo yo yo yo what it is motherfuckers.ogg",length=3.544}}
 L["you seem like a fucking quack"]={{path="chatsounds/autoadd/loser/you seem like a fucking quack.ogg",length=1.507}}
 L["your wife is indeed a frog"]={{path="chatsounds/autoadd/loser/your wife is indeed a frog.ogg",length=2.125}}
 L["youre telling me that my wife is now a frog thats what youre telling me"]={{path="chatsounds/autoadd/loser/youre telling me that my wife is now a frog thats what youre telling me.ogg",length=5.724}}
