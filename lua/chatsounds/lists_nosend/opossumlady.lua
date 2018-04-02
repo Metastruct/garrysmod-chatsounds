4c1bdd19b4f790aafe10fdf70091e7206e8d835e
Brian Davis <kjones1999@outlook.com>
2018-04-02 04:21:14 -0400

    bong bongdrop

---
+++ lua/chatsounds/lists_nosend/opossumlady.lua
@@ -0,0 +1,22 @@
+c.StartList("opossumlady")
+L["bong bongdrop"]={{path="chatsounds/autoadd/opossumlady/bong bongdrop.ogg",length=3.657}}
+L["clear the deck and prepare to compress"]={{path="chatsounds/autoadd/opossumlady/clear the deck and prepare to compress.ogg",length=4.007}}
+L["doesnt that look good well forget it thud dont do it no"]={{path="chatsounds/autoadd/opossumlady/doesnt that look good well forget it thud dont do it no.ogg",length=9.613}}
+L["duckfilled fattypuss tablesmack"]={{path="chatsounds/autoadd/opossumlady/duckfilled fattypuss tablesmack.ogg",length=3.349}}
+L["elephantmaddening"]={{path="chatsounds/autoadd/opossumlady/elephantmaddening.ogg",length=41.902}}
+L["elephantnoise okay"]={{path="chatsounds/autoadd/opossumlady/elephantnoise okay.ogg",length=1.823}}
+L["elephantnoise well we have an elephant in the room"]={{path="chatsounds/autoadd/opossumlady/elephantnoise well we have an elephant in the room.ogg",length=5.635}}
+L["elephantnoise what elephantnoise yknow mr elephant has been acting up alot lately elephantnoise"]={{path="chatsounds/autoadd/opossumlady/elephantnoise what elephantnoise yknow mr elephant has been acting up alot lately elephantnoise.ogg",length=13.168}}
+L["im meant to be green"]={{path="chatsounds/autoadd/opossumlady/im meant to be green.ogg",length=3.321}}
+L["in my heart i know its true"]={{path="chatsounds/autoadd/opossumlady/in my heart i know its true.ogg",length=3.451}}
+L["now if after all this thud"]={{path="chatsounds/autoadd/opossumlady/now if after all this thud.ogg",length=5.344}}
+L["ohhh opossumchoking"]={{path="chatsounds/autoadd/opossumlady/ohhh opossumchoking.ogg",length=1.087}}
+L["opossum opossum"]={{path="chatsounds/autoadd/opossumlady/opossum opossum.ogg",length=2.120}}
+L["opossumchoking yes they are opossumchoking"]={{path="chatsounds/autoadd/opossumlady/opossumchoking yes they are opossumchoking.ogg",length=2.308}}
+L["the crow came up aaaaaaah aaaaaah"]={{path="chatsounds/autoadd/opossumlady/the crow came up aaaaaaah aaaaaah.ogg",length=4.592}}
+L["thiis littlepiggywent wewewewe all the way home hahahaha oh she just loves that"]={{path="chatsounds/autoadd/opossumlady/thiis littlepiggywent wewewewe all the way home hahahaha oh she just loves that.ogg",length=7.334}}
+L["thud and now friends"]={{path="chatsounds/autoadd/opossumlady/thud and now friends.ogg",length=2.180}}
+L["trashseeking"]={{path="chatsounds/autoadd/opossumlady/trashseeking.ogg",length=3.616}}
+L["wallsmack"]={{path="chatsounds/autoadd/opossumlady/wallsmack.ogg",length=1.580}}
+L["who besides your dead loved one is likely to be on the roof"]={{path="chatsounds/autoadd/opossumlady/who besides your dead loved one is likely to be on the roof.ogg",length=5.677}}
+c.EndList()
