627a1ae75a43fb8fe162e7919ff720a4bd7a1903
Sera ❤ <devsera.phend@gmail.com>
2018-04-14 01:13:56 -0500

    Some BFB Soundclips

---
+++ lua/chatsounds/lists_nosend/bfb.lua
@@ -0,0 +1,38 @@
+c.StartList("bfb")
+L["bfb intro"]={{path="chatsounds/autoadd/bfb/bfb intro.ogg",length=18.290}}
+L["but why"]={{path="chatsounds/autoadd/bfb/but why.ogg",length=0.941}}
+L["did someone say killing"]={{path="chatsounds/autoadd/bfb/did someone say killing.ogg",length=1.289}}
+L["dont you ticking dare touch my clock hands"]={{path="chatsounds/autoadd/bfb/dont you ticking dare touch my clock hands.ogg",length=2.910}}
+L["dont you want to battle to win a prize"]={{path="chatsounds/autoadd/bfb/dont you want to battle to win a prize.ogg",length=2.547}}
+L["dorasong"]={{path="chatsounds/autoadd/bfb/dorasong.ogg",length=6.222}}
+L["four screech"]={{path="chatsounds/autoadd/bfb/four screech/four screech 1.ogg",length=20.766},{path="chatsounds/autoadd/bfb/four screech/four screech 2.ogg",length=1.022},{path="chatsounds/autoadd/bfb/four screech/four screech 3.ogg",length=0.292}}
+L["four squish"]={{path="chatsounds/autoadd/bfb/four squish.ogg",length=2.045}}
+L["here is a fork"]={{path="chatsounds/autoadd/bfb/here is a fork.ogg",length=0.810}}
+L["hmm i dunno that sounds like a bad idea"]={{path="chatsounds/autoadd/bfb/hmm i dunno that sounds like a bad idea.ogg",length=2.553}}
+L["i hate you"]={{path="chatsounds/autoadd/bfb/i hate you.ogg",length=0.771}}
+L["i honestly think it might be impossible"]={{path="chatsounds/autoadd/bfb/i honestly think it might be impossible.ogg",length=2.365}}
+L["i want to stop"]={{path="chatsounds/autoadd/bfb/i want to stop.ogg",length=1.041}}
+L["ive done some research and i believe it means that the person cares so little that the last droplets of care spirit are being sucked out of their body through their arms and out of the tips of their fingers and into the outer reaches of interstellar space to be lost forever a true indicator that person doesnt give a fluff"]={{path="chatsounds/autoadd/bfb/someresearch.ogg",length=16.214}}
+L["just wanted  to let you know i dont have a favorite number"]={{path="chatsounds/autoadd/bfb/just wanted  to let you know i dont have a favorite number.ogg",length=2.910}}
+L["lets play a game"]={{path="chatsounds/autoadd/bfb/lets play a game.ogg",length=1.239}}
+L["liars must be punished"]={{path="chatsounds/autoadd/bfb/liars must be punished.ogg",length=1.148}}
+L["look the fluttery arms she does not care"]={{path="chatsounds/autoadd/bfb/look the fluttery arms she does not care.ogg",length=2.547}}
+L["my fork"]={{path="chatsounds/autoadd/bfb/my fork.ogg",length=0.677}}
+L["no"]={{path="chatsounds/autoadd/bfb/no.ogg",length=0.489}}
+L["no more screechy please"]={{path="chatsounds/autoadd/bfb/no more screechy please.ogg",length=1.145}}
+L["no thanks im good"]={{path="chatsounds/autoadd/bfb/no thanks im good.ogg",length=1.882}}
+L["oh my gosh i won"]={{path="chatsounds/autoadd/bfb/oh my gosh i won.ogg",length=1.518}}
+L["pilot song"]={{path="chatsounds/autoadd/bfb/pilot song.ogg",length=25.986}}
+L["pretty cool dont you think"]={{path="chatsounds/autoadd/bfb/pretty cool dont you think.ogg",length=1.957}}
+L["sounds like a lie to me"]={{path="chatsounds/autoadd/bfb/sounds like a lie to me.ogg",length=1.436}}
+L["staaaaaaaaap"]={{path="chatsounds/autoadd/bfb/staaaaaaaaap.ogg",length=2.760}}
+L["thank you for opening my eyes"]={{path="chatsounds/autoadd/bfb/thank you for opening my eyes.ogg",length=2.296}}
+L["thats it im leafing"]={{path="chatsounds/autoadd/bfb/thats it im leafing.ogg",length=1.280}}
+L["too bad"]={{path="chatsounds/autoadd/bfb/too bad.ogg",length=0.881}}
+L["were gonna demote you from member to bember"]={{path="chatsounds/autoadd/bfb/were gonna demote you from member to bember.ogg",length=2.829}}
+L["whatever you win some you lose some"]={{path="chatsounds/autoadd/bfb/whatever you win some you lose some.ogg",length=2.057}}
+L["wow thats really thought provoking"]={{path="chatsounds/autoadd/bfb/wow thats really thought provoking.ogg",length=2.459}}
+L["yeah sounds totally reasonable"]={{path="chatsounds/autoadd/bfb/yeah sounds totally reasonable.ogg",length=1.875}}
+L["yeah too bad"]={{path="chatsounds/autoadd/bfb/yeah too bad.ogg",length=1.217}}
+L["youre welcome"]={{path="chatsounds/autoadd/bfb/youre welcome.ogg",length=1.192}}
+c.EndList()
