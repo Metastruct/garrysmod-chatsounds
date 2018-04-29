4a1761008e3a5f2d03eb008896639c46d46b68e8
Meta Automator <MetaAutomator@users.noreply.github.com>
2018-04-29 00:13:54 +0000

    chatsounds preprocessor (58a14cb66b86667d74fe66c5abe6b8a9f7b764f7)
    [ci skip]

--- lua/chatsounds/lists_nosend/vin.lua
+++ lua/chatsounds/lists_nosend/vin.lua
@@ -2,13 +2,22 @@ c.StartList("vin")
 L["fleelf"]={{path="chatsounds/autoadd/vin/fleelf.ogg",length=0.358}}
 L["flextapeglitch"]={{path="chatsounds/autoadd/vin/flextapeglitch.ogg",length=1.606}}
 L["i soos"]={{path="chatsounds/autoadd/vin/i soos.ogg",length=0.857}}
+L["kaboom distorted"]={{path="chatsounds/autoadd/vin/kaboom distorted.ogg",length=15.479}}
 L["mariogibberish"]={{path="chatsounds/autoadd/vin/mariogibberish.ogg",length=2.377}}
+L["marx1"]={{path="chatsounds/autoadd/vin/marx1.ogg",length=3.286}}
+L["marx2"]={{path="chatsounds/autoadd/vin/marx2.ogg",length=2.264}}
+L["marx3"]={{path="chatsounds/autoadd/vin/marx3.ogg",length=2.113}}
+L["marx4"]={{path="chatsounds/autoadd/vin/marx4.ogg",length=1.707}}
+L["marxscream"]={{path="chatsounds/autoadd/vin/marxscream.ogg",length=4.342}}
 L["mortis longdick"]={{path="chatsounds/autoadd/vin/mortis longdick.ogg",length=2.137}}
+L["notbigsusgibton"]={{path="chatsounds/autoadd/vin/notbigsusgibton.ogg",length=1.414}}
 L["now you can do the dishes"]={{path="chatsounds/autoadd/vin/now you can do the dishes.ogg",length=2.205}}
 L["oh"]={{path="chatsounds/autoadd/vin/oh.ogg",length=0.763}}
+L["steamed hams"]={{path="chatsounds/autoadd/vin/steamed hams.ogg",length=1.202}}
 L["u wot"]={{path="chatsounds/autoadd/vin/u wot.ogg",length=0.844}}
 L["wait wait what"]={{path="chatsounds/autoadd/vin/wait wait what.ogg",length=1.242}}
 L["what the actual fuck"]={{path="chatsounds/autoadd/vin/what the actual fuck.ogg",length=1.762}}
+L["what was that"]={{path="chatsounds/autoadd/vin/what was that.ogg",length=0.568}}
 L["yeeeeeeeee"]={{path="chatsounds/autoadd/vin/yeeeeeeeee.ogg",length=1.310}}
 L["you dont even care do you"]={{path="chatsounds/autoadd/vin/you dont even care do you.ogg",length=1.950}}
 c.EndList()
