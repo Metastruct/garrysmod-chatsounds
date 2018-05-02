627ad2205d36f86d9afbd060d87782dbf3bb0d13
Meta Automator <MetaAutomator@users.noreply.github.com>
2018-05-02 22:44:13 +0000

    chatsounds preprocessor (411d76595d7d50d8d7f7da526d12c4f4a723a357)
    [ci skip]

--- lua/chatsounds/lists_nosend/vin.lua
+++ lua/chatsounds/lists_nosend/vin.lua
@@ -9,10 +9,14 @@ L["left channel"]={{path="chatsounds/autoadd/vin/left channel.ogg",length=0.832}
 L["mariogibberish"]={{path="chatsounds/autoadd/vin/mariogibberish.ogg",length=2.377}}
 L["marx"]={{path="chatsounds/autoadd/vin/marx/1.ogg",length=3.286},{path="chatsounds/autoadd/vin/marx/2.ogg",length=2.264},{path="chatsounds/autoadd/vin/marx/3.ogg",length=2.113},{path="chatsounds/autoadd/vin/marx/4.ogg",length=1.707}}
 L["marxscream"]={{path="chatsounds/autoadd/vin/marxscream.ogg",length=4.342}}
+L["no keep going"]={{path="chatsounds/autoadd/vin/no keep going.ogg",length=1.376}}
 L["now you can do the dishes"]={{path="chatsounds/autoadd/vin/now you can do the dishes.ogg",length=2.205}}
 L["oh"]={{path="chatsounds/autoadd/vin/oh.ogg",length=0.763}}
 L["oh shit"]={{path="chatsounds/autoadd/vin/oh shit.ogg",length=0.880}}
+L["parrotscream"]={{path="chatsounds/autoadd/vin/parrotscream.ogg",length=1.432}}
+L["parrotscreamdistorted"]={{path="chatsounds/autoadd/vin/parrotscreamdistorted.ogg",length=3.124}}
 L["pills here pppppppppppppppppills here"]={{path="chatsounds/autoadd/vin/pills here pppppppppppppppppills here.ogg",length=9.919}}
+L["shut up"]={{path="chatsounds/autoadd/vin/shut up.ogg",length=0.643}}
 L["snans"]={{path="chatsounds/autoadd/vin/snans.ogg",length=0.696}}
 L["soup time"]={{path="chatsounds/autoadd/vin/soup time.ogg",length=6.010}}
 L["steamed hams"]={{path="chatsounds/autoadd/vin/steamed hams.ogg",length=1.202}}
