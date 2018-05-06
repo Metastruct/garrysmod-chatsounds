da4cfcb9dca921ce5a2c082db1c95164f115747d
Zeni <timrace01@gmail.com>
2018-05-06 15:53:09 +0200

    welcome to mcdawnalds

--- lua/chatsounds/lists_nosend/zeni.lua
+++ lua/chatsounds/lists_nosend/zeni.lua
@@ -1,6 +1,10 @@
 c.StartList("zeni")
 L["banned for life"]={{path="chatsounds/autoadd/zeni/banned for life.ogg",length=4.682}}
 L["begone thot"]={{path="chatsounds/autoadd/zeni/begone thot.ogg",length=2.000}}
+L["chinken nunget"]={{path="chatsounds/autoadd/zeni/chinken nunget.ogg",length=2.206}}
+L["do you wanna phucking beesechurger"]={{path="chatsounds/autoadd/zeni/do you wanna phucking beesechurger.ogg",length=2.895}}
 L["get out of here stalker"]={{path="chatsounds/autoadd/zeni/get out of here stalker.ogg",length=1.456}}
+L["please i just want to see my wife again"]={{path="chatsounds/autoadd/zeni/please i just want to see my wife again.ogg",length=3.021}}
 L["spidermanpizzatheme"]={{path="chatsounds/autoadd/zeni/spidermanpizzatheme.ogg",length=87.290}}
+L["welcome to mcdawnalds"]={{path="chatsounds/autoadd/zeni/welcome to mcdawnalds.ogg",length=3.493}}
 c.EndList()
