16ec2c9c00e35837b957debed443f32de8d59c4f
PotcFdk <PotcFdk@gmx.net>
2018-05-19 15:56:07 +0200

    Rename and combine some sounds in dasmirkolp

--- lua/chatsounds/lists_nosend/dasmirkolp.lua
+++ lua/chatsounds/lists_nosend/dasmirkolp.lua
@@ -1,11 +1,9 @@
 c.StartList("dasmirkolp")
-L["alter"]={{path="chatsounds/autoadd/dasmirkolp/alter.ogg",length=1.115}}
+L["alter"]={{path="chatsounds/autoadd/dasmirkolp/alter/alter.ogg",length=1.115},{path="chatsounds/autoadd/dasmirkolp/alter/alter1.ogg",length=1.416}}
 L["alter falter"]={{path="chatsounds/autoadd/dasmirkolp/alter falter.ogg",length=1.231}}
-L["alter1"]={{path="chatsounds/autoadd/dasmirkolp/alter1.ogg",length=1.416}}
-L["anliebsten aus frischen japanern geschnetzelt"]={{path="chatsounds/autoadd/dasmirkolp/anliebsten aus frischen japanern geschnetzelt.ogg",length=3.638}}
+L["am liebsten aus frischen japanern geschnetzelt"]={{path="chatsounds/autoadd/dasmirkolp/am liebsten aus frischen japanern geschnetzelt.ogg",length=3.638}}
 L["baaaaaaaaaaahhh"]={{path="chatsounds/autoadd/dasmirkolp/baaaaaaaaaaahhh.ogg",length=1.477}}
 L["blub und bla bla"]={{path="chatsounds/autoadd/dasmirkolp/blub und bla bla.ogg",length=2.392}}
-L["boah ich mag sushi alter"]={{path="chatsounds/autoadd/dasmirkolp/boah ich mag sushi alter.ogg",length=2.090}}
 L["boah nein"]={{path="chatsounds/autoadd/dasmirkolp/boah nein.ogg",length=0.906}}
 L["das kann nicht angehn"]={{path="chatsounds/autoadd/dasmirkolp/das kann nicht angehn.ogg",length=1.248}}
 L["dieses spiel ist fucking fuck"]={{path="chatsounds/autoadd/dasmirkolp/dieses spiel ist fucking fuck.ogg",length=3.413}}
@@ -15,7 +13,7 @@ L["du homosexueller hetero"]={{path="chatsounds/autoadd/dasmirkolp/du homosexuel
 L["du scheiss kack"]={{path="chatsounds/autoadd/dasmirkolp/du scheiss kack.ogg",length=1.884}}
 L["ein spass leck mich alter hund"]={{path="chatsounds/autoadd/dasmirkolp/ein spass leck mich alter hund.ogg",length=3.669}}
 L["ey manno"]={{path="chatsounds/autoadd/dasmirkolp/ey manno.ogg",length=1.974}}
-L["fantastisch boah ich darf nicht bruellen"]={{path="chatsounds/autoadd/dasmirkolp/fantastisch boah ich darf nicht bruellen.ogg",length=7.077}}
+L["fantastisch boah mann ich darf nicht bruellen"]={{path="chatsounds/autoadd/dasmirkolp/fantastisch boah mann ich darf nicht bruellen.ogg",length=7.077}}
 L["fick die huehner"]={{path="chatsounds/autoadd/dasmirkolp/fick die huehner.ogg",length=1.695}}
 L["ficken"]={{path="chatsounds/autoadd/dasmirkolp/ficken.ogg",length=1.207}}
 L["fotze"]={{path="chatsounds/autoadd/dasmirkolp/fotze.ogg",length=0.766}}
@@ -37,13 +35,13 @@ L["nein du nutte"]={{path="chatsounds/autoadd/dasmirkolp/nein du nutte.ogg",leng
 L["nein ich reg mich doch nicht auf"]={{path="chatsounds/autoadd/dasmirkolp/nein ich reg mich doch nicht auf.ogg",length=4.347}}
 L["nein what the fuck da ist auch einer"]={{path="chatsounds/autoadd/dasmirkolp/nein what the fuck da ist auch einer.ogg",length=3.557}}
 L["nnneeeiiinnn"]={{path="chatsounds/autoadd/dasmirkolp/nnneeeiiinnn.ogg",length=1.565}}
+L["oah ich mag sushi alter"]={{path="chatsounds/autoadd/dasmirkolp/oah ich mag sushi alter.ogg",length=2.090}}
 L["oh du verdammter japaner"]={{path="chatsounds/autoadd/dasmirkolp/oh du verdammter japaner.ogg",length=3.204}}
 L["oh mein gott ist das ein scheiss level"]={{path="chatsounds/autoadd/dasmirkolp/oh mein gott ist das ein scheiss level.ogg",length=3.576}}
 L["ohhh man"]={{path="chatsounds/autoadd/dasmirkolp/ohhh man.ogg",length=1.676}}
 L["ohhh verdammt"]={{path="chatsounds/autoadd/dasmirkolp/ohhh verdammt.ogg",length=2.043}}
 L["scheiss dreck man"]={{path="chatsounds/autoadd/dasmirkolp/scheiss dreck man.ogg",length=1.314}}
-L["verdammt"]={{path="chatsounds/autoadd/dasmirkolp/verdammt.ogg",length=1.355}}
-L["verdammt1"]={{path="chatsounds/autoadd/dasmirkolp/verdammt1.ogg",length=1.858}}
+L["verdammt"]={{path="chatsounds/autoadd/dasmirkolp/verdammt/verdammt.ogg",length=1.355},{path="chatsounds/autoadd/dasmirkolp/verdammt/verdammt1.ogg",length=1.858}}
 L["was ist denn das"]={{path="chatsounds/autoadd/dasmirkolp/was ist denn das.ogg",length=1.930}}
 L["was war das denn oh mein gott"]={{path="chatsounds/autoadd/dasmirkolp/was war das denn oh mein gott.ogg",length=4.707}}
 L["was zum"]={{path="chatsounds/autoadd/dasmirkolp/was zum.ogg",length=1.024}}
