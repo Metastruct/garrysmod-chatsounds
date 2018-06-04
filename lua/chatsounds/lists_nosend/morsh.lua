cba721f90b4466da4bb5965e8a91e6b314aa2e2d
PotcFdk <PotcFdk@gmx.net>
2018-06-04 23:22:23 +0200

    That's a burp. Disgusting.

--- lua/chatsounds/lists_nosend/morsh.lua
+++ lua/chatsounds/lists_nosend/morsh.lua
@@ -2,13 +2,13 @@ c.StartList("morsh")
 L["a man"]={{path="chatsounds/autoadd/morsh/a man.ogg",length=0.575}}
 L["airhorn"]={{path="chatsounds/autoadd/morsh/airhorn.ogg",length=1.712}}
 L["airhorn2"]={{path="chatsounds/autoadd/morsh/airhorn2.ogg",length=2.837}}
+L["burp"]={{path="chatsounds/autoadd/morsh/burp.ogg",length=1.518}}
 L["everyday"]={{path="chatsounds/autoadd/morsh/everyday.ogg",length=0.834}}
 L["for his"]={{path="chatsounds/autoadd/morsh/for his.ogg",length=0.366}}
 L["hours later"]={{path="chatsounds/autoadd/morsh/hours later.ogg",length=1.294}}
 L["i just"]={{path="chatsounds/autoadd/morsh/i just.ogg",length=0.522}}
 L["killed"]={{path="chatsounds/autoadd/morsh/killed.ogg",length=0.731}}
 L["lacucaracha"]={{path="chatsounds/autoadd/morsh/lacucaracha.ogg",length=3.097}}
-L["oioioi"]={{path="chatsounds/autoadd/morsh/oioioi.ogg",length=1.518}}
 L["potato wedges"]={{path="chatsounds/autoadd/morsh/potato wedges.ogg",length=1.176}}
 L["ravioli ravioli give me the formuoli"]={{path="chatsounds/autoadd/morsh/ravioli ravioli give me the formuoli.ogg",length=3.279}}
 L["rupe te"]={{path="chatsounds/autoadd/morsh/rupe te.ogg",length=6.153}}
