60924d9b9bac8be01650ce70c6623f5d5b85202b
Meta Automator <MetaAutomator@users.noreply.github.com>
2018-07-15 22:42:33 +0000

    chatsounds preprocessor (4656b5953b1379ee65fc8f3b34c1e6f97f7a6801)
    [ci skip]

---
+++ lua/chatsounds/lists_nosend/lp - shin megami tensei.lua
@@ -0,0 +1,19 @@
+c.StartList("lp - shin megami tensei")
+L["dagda laugh"]={{path="chatsounds/autoadd/lp - shin megami tensei/dagda laugh.ogg",length=2.183}}
+L["dammit"]={{path="chatsounds/autoadd/lp - shin megami tensei/dammit.ogg",length=0.438}}
+L["dammit you monster"]={{path="chatsounds/autoadd/lp - shin megami tensei/dammit you monster.ogg",length=1.952}}
+L["didnt your parents teach you any manners you censored"]={{path="chatsounds/autoadd/lp - shin megami tensei/didnt your parents teach you any manners you censored.ogg",length=5.920}}
+L["dont get in my way you little shit"]={{path="chatsounds/autoadd/lp - shin megami tensei/dont get in my way you little shit.ogg",length=3.136}}
+L["encounter"]={{path="chatsounds/autoadd/lp - shin megami tensei/encounter.ogg",length=6.664}}
+L["gasp"]={{path="chatsounds/autoadd/lp - shin megami tensei/gasp.ogg",length=0.246}}
+L["hurry kill it kill it now"]={{path="chatsounds/autoadd/lp - shin megami tensei/hurry kill it kill it now.ogg",length=2.610}}
+L["im the king"]={{path="chatsounds/autoadd/lp - shin megami tensei/im the king.ogg",length=1.180}}
+L["it came out of nowhere"]={{path="chatsounds/autoadd/lp - shin megami tensei/it came out of nowhere.ogg",length=1.280}}
+L["its all you kid finish him off"]={{path="chatsounds/autoadd/lp - shin megami tensei/its all you kid finish him off.ogg",length=2.944}}
+L["okay you got it bro"]={{path="chatsounds/autoadd/lp - shin megami tensei/okay you got it bro.ogg",length=1.747}}
+L["one more god rejected"]={{path="chatsounds/autoadd/lp - shin megami tensei/one more god rejected.ogg",length=2.368}}
+L["stop your incessant whinning and keep fighting"]={{path="chatsounds/autoadd/lp - shin megami tensei/stop your incessant whinning and keep fighting.ogg",length=2.464}}
+L["thats exactly what a godslayer should say"]={{path="chatsounds/autoadd/lp - shin megami tensei/thats exactly what a godslayer should say.ogg",length=3.459}}
+L["woah what the"]={{path="chatsounds/autoadd/lp - shin megami tensei/woah what the.ogg",length=0.800}}
+L["yhvh"]={{path="chatsounds/autoadd/lp - shin megami tensei/yhvh.ogg",length=0.648}}
+c.EndList()
---
+++ lua/chatsounds/lists_nosend/unreal.lua
@@ -0,0 +1,16 @@
+c.StartList("unreal")
+L["access denied"]={{path="chatsounds/autoadd/unreal/access denied.ogg",length=1.686}}
+L["access permitted"]={{path="chatsounds/autoadd/unreal/access permitted.ogg",length=1.599}}
+L["bell"]={{path="chatsounds/autoadd/unreal/bell.ogg",length=2.659}}
+L["electrical arcing"]={{path="chatsounds/autoadd/unreal/electrical arcing.ogg",length=1.921}}
+L["force field deactivated"]={{path="chatsounds/autoadd/unreal/force field deactivated.ogg",length=2.519}}
+L["light5a"]={{path="chatsounds/autoadd/unreal/light5a.ogg",length=3.379}}
+L["multiple security breaches detected"]={{path="chatsounds/autoadd/unreal/multiple security breaches detected.ogg",length=3.007}}
+L["prisoner 327 escaping"]={{path="chatsounds/autoadd/unreal/prisoner 327 escaping.ogg",length=3.127}}
+L["prisoner 472 escaping"]={{path="chatsounds/autoadd/unreal/prisoner 472 escaping.ogg",length=3.987}}
+L["prisoner 849 escaping"]={{path="chatsounds/autoadd/unreal/prisoner 849 escaping.ogg",length=3.544}}
+L["scream"]={{path="chatsounds/autoadd/unreal/scream/scream10.ogg",length=4.644},{path="chatsounds/autoadd/unreal/scream/scream12.ogg",length=2.484},{path="chatsounds/autoadd/unreal/scream/scream13.ogg",length=2.979}}
+L["security team to sector 5 a"]={{path="chatsounds/autoadd/unreal/security team to sector 5 a.ogg",length=3.274}}
+L["wind01"]={{path="chatsounds/autoadd/unreal/wind01.ogg",length=5.174}}
+L["you have entered a restricted area"]={{path="chatsounds/autoadd/unreal/you have entered a restricted area.ogg",length=2.889}}
+c.EndList()
