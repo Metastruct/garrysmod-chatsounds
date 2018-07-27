5be79719c9d1bf80fb7b5607cfd932daf42bef86
!Avast AntiPony 9445 <kristianzxz41@gmail.com>
2018-07-27 02:04:39 +0200

    Duyu 64
    Real high quality rips.

--- lua/chatsounds/lists_nosend/jamesbond.lua
+++ lua/chatsounds/lists_nosend/jamesbond.lua
@@ -9,6 +9,7 @@ L["give me the codes natalya give them to me"]={{path="chatsounds/autoadd/jamesb
 L["i am invincible"]={{path="chatsounds/autoadd/jamesbond/i am invincible.ogg",length=1.679}}
 L["i can break her codes"]={{path="chatsounds/autoadd/jamesbond/i can break her codes.ogg",length=1.354}}
 L["i can do it"]={{path="chatsounds/autoadd/jamesbond/i can do it.ogg",length=0.887}}
+L["i must protest"]={{path="chatsounds/autoadd/jamesbond/i must protest.ogg",length=0.938}}
 L["james bond"]={{path="chatsounds/autoadd/jamesbond/james bond.ogg",length=0.865}}
 L["jamesbondtheme1"]={{path="chatsounds/autoadd/jamesbond/jamesbondtheme1.ogg",length=6.750}}
 L["jamesbondtheme2"]={{path="chatsounds/autoadd/jamesbond/jamesbondtheme2.ogg",length=9.885}}
@@ -20,4 +21,6 @@ L["shes a moron"]={{path="chatsounds/autoadd/jamesbond/shes a moron.ogg",length=
 L["the name is bond"]={{path="chatsounds/autoadd/jamesbond/the name is bond.ogg",length=0.786}}
 L["then she can fix it"]={{path="chatsounds/autoadd/jamesbond/then she can fix it.ogg",length=1.447}}
 L["yes i am invincible"]={{path="chatsounds/autoadd/jamesbond/yes i am invincible/1.ogg",length=2.744},{path="chatsounds/autoadd/jamesbond/yes i am invincible/2.ogg",length=2.690}}
+L["youll blow the gas tanks"]={{path="chatsounds/autoadd/jamesbond/youll blow the gas tanks.ogg",length=1.490}}
+L["youre out of order"]={{path="chatsounds/autoadd/jamesbond/youre out of order.ogg",length=0.761}}
 c.EndList()
