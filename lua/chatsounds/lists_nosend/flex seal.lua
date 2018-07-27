d843027b6c1c8f80b3b81a5300cb971c263ee6cb
SpiralP <spiral.palmer@gmail.com>
2018-07-26 22:09:36 -0700

    flex seal now thats a lotta damage

---
+++ lua/chatsounds/lists_nosend/flex seal.lua
@@ -0,0 +1,8 @@
+c.StartList("flex seal")
+L["how bout a little more"]={{path="chatsounds/autoadd/flex seal/how bout a little more.ogg",length=0.960}}
+L["maniacal laughing"]={{path="chatsounds/autoadd/flex seal/maniacal laughing.ogg",length=0.830}}
+L["maniacal laughing chainsaw"]={{path="chatsounds/autoadd/flex seal/maniacal laughing chainsaw.ogg",length=3.181}}
+L["now thats a lotta damage"]={{path="chatsounds/autoadd/flex seal/now thats a lotta damage.ogg",length=1.604}}
+L["thats a lotta damage"]={{path="chatsounds/autoadd/flex seal/thats a lotta damage.ogg",length=1.589}}
+L["you may have seen a few people test flex seal on a bucket"]={{path="chatsounds/autoadd/flex seal/you may have seen a few people test flex seal on a bucket.ogg",length=3.775}}
+c.EndList()
