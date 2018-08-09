a344d6215449cb86a8213f85fcff155f3672b60a
Meta Automator <MetaAutomator@users.noreply.github.com>
2018-08-09 19:11:49 +0000

    chatsounds preprocessor (0dba48425fcdb1bcb8e1db8a6a053c4bf89fdf8e)
    [ci skip]

---
+++ lua/chatsounds/lists_nosend/flyingkitty.lua
@@ -0,0 +1,14 @@
+c.StartList("flyingkitty")
+L["caught a stiffy in my ass cheeks i grinded for my sauce"]={{path="chatsounds/autoadd/flyingkitty/caught a stiffy in my ass cheeks i grinded for my sauce.ogg",length=3.093}}
+L["grills on my grills on my grills on my grills"]={{path="chatsounds/autoadd/flyingkitty/grills on my grills on my grills on my grills.ogg",length=2.255}}
+L["i dont wanna feed the horse its gonna shit some more"]={{path="chatsounds/autoadd/flyingkitty/i dont wanna feed the horse its gonna shit some more.ogg",length=2.725}}
+L["i see ice"]={{path="chatsounds/autoadd/flyingkitty/i see ice.ogg",length=0.837}}
+L["i shoot my cents like buck buck buckbuckbuckbuck"]={{path="chatsounds/autoadd/flyingkitty/i shoot my cents like buck buck buckbuckbuckbuck.ogg",length=2.466}}
+L["lost my guns just a week ago boom"]={{path="chatsounds/autoadd/flyingkitty/lost my guns just a week ago boom.ogg",length=3.477}}
+L["man thats really all i care about clothes clothes"]={{path="chatsounds/autoadd/flyingkitty/man thats really all i care about clothes clothes.ogg",length=2.998}}
+L["moo moo"]={{path="chatsounds/autoadd/flyingkitty/moo moo.ogg",length=0.763}}
+L["pop these pop these pop you let that shit pop"]={{path="chatsounds/autoadd/flyingkitty/pop these pop these pop you let that shit pop.ogg",length=3.230}}
+L["put your fucking clothes on i only want the clothes"]={{path="chatsounds/autoadd/flyingkitty/put your fucking clothes on i only want the clothes.ogg",length=2.766}}
+L["rreegguullaarr"]={{path="chatsounds/autoadd/flyingkitty/rreegguullaarr.ogg",length=2.426}}
+L["whoopity scoop whoop poop poop"]={{path="chatsounds/autoadd/flyingkitty/whoopity scoop whoop poop poop.ogg",length=2.862}}
+c.EndList()
