823bd832fa4e9ce537eebe3283d0788fd24acf8d
KabusIblis <kabusiblis@gmail.com>
2018-08-25 22:55:43 +0100

    whoaaaaaaaaooooooooooooooooooo

--- lua/chatsounds/lists_nosend/mgsv.lua
+++ lua/chatsounds/lists_nosend/mgsv.lua
@@ -2,5 +2,6 @@ c.StartList("mgsv")
 L["idroid function unavailable"]={{path="chatsounds/autoadd/mgsv/idroid function unavailable.ogg",length=1.540}}
 L["phantomcigar"]={{path="chatsounds/autoadd/mgsv/phantomcigar.ogg",length=9.641}}
 L["such a lust for revenge"]={{path="chatsounds/autoadd/mgsv/such a lust for revenge.ogg",length=1.765}}
+L["whoaaaaaaaaooooooooooooooooooo"]={{path="chatsounds/autoadd/mgsv/whoaaaaaaaaooooooooooooooooooo.ogg",length=8.268}}
 L["whooooo"]={{path="chatsounds/autoadd/mgsv/whooooo.ogg",length=1.708}}
 c.EndList()
