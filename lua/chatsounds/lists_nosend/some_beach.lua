f237c3a94882cf34b8f99807a1ab663e16924fda
ajloveslily14 <ajloveslily@gmail.com>
2018-09-19 18:15:51 -0700

    KAUUUSSS

--- lua/chatsounds/lists_nosend/some_beach.lua
+++ lua/chatsounds/lists_nosend/some_beach.lua
@@ -1,5 +1,6 @@
 c.StartList("some_beach")
 L["hi"]={{path="chatsounds/autoadd/some_beach/hi.ogg",length=0.478}}
 L["is anyone there"]={{path="chatsounds/autoadd/some_beach/is anyone there.ogg",length=1.695}}
+L["kabus"]={{path="chatsounds/autoadd/some_beach/kabus/kabus.ogg",length=1.625},{path="chatsounds/autoadd/some_beach/kabus/kabus2.ogg",length=1.160}}
 L["oh"]={{path="chatsounds/autoadd/some_beach/oh.ogg",length=0.486}}
 c.EndList()
