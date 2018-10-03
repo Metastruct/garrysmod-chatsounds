36e2e0470fe369b88d3c78c9f0f4aec683450414
Techbot <techbot121@gmail.com>
2018-10-03 11:41:20 +0200

    manual listgeneration since metautomator isn't working atm

---
+++ lua/chatsounds/lists_nosend/merasmus.lua
@@ -0,0 +1,17 @@
+c.StartList("merasmus")
+L["are now on sale mortals"]={{path="chatsounds/autoadd/merasmus/are now on sale mortals.ogg",length=2.813}}
+L["bonus fucks"]={{path="chatsounds/autoadd/merasmus/bonus fucks.ogg",length=1.080}}
+L["but i wanted to rape them"]={{path="chatsounds/autoadd/merasmus/but i wanted to rape them.ogg",length=2.632}}
+L["do you feel it tightening around your cock"]={{path="chatsounds/autoadd/merasmus/do you feel it tightening around your cock.ogg",length=2.513}}
+L["heblelb"]={{path="chatsounds/autoadd/merasmus/heblelb.ogg",length=0.854}}
+L["hes going to fuck that cock"]={{path="chatsounds/autoadd/merasmus/hes going to fuck that cock.ogg",length=2.814}}
+L["his cock got crushed"]={{path="chatsounds/autoadd/merasmus/his cock got crushed.ogg",length=2.175}}
+L["holy fuck"]={{path="chatsounds/autoadd/merasmus/holy fuck.ogg",length=1.182}}
+L["it took a while to get sexual again"]={{path="chatsounds/autoadd/merasmus/it took a while to get sexual again.ogg",length=2.374}}
+L["remember when i got bonah"]={{path="chatsounds/autoadd/merasmus/remember when i got bonah.ogg",length=1.837}}
+L["this cunt is binding mortal"]={{path="chatsounds/autoadd/merasmus/this cunt is binding mortal.ogg",length=3.652}}
+L["tits"]={{path="chatsounds/autoadd/merasmus/tits.ogg",length=0.365}}
+L["welcome mortals to my sex kingdom"]={{path="chatsounds/autoadd/merasmus/welcome mortals to my sex kingdom.ogg",length=3.695}}
+L["you dare to fuck merasmus"]={{path="chatsounds/autoadd/merasmus/you dare to fuck merasmus.ogg",length=2.524}}
+L["your just a big stupid pile of shit"]={{path="chatsounds/autoadd/merasmus/your just a big stupid pile of shit.ogg",length=2.240}}
+c.EndList()
