b72cb76c1202b6afd8255a68e6b1ffcd1eced935
Kody Wright <kodyman@att.net>
2018-12-29 14:39:55 -0500

    epic

--- lua/chatsounds/lists_nosend/midori.lua
+++ lua/chatsounds/lists_nosend/midori.lua
@@ -8,6 +8,7 @@ L["give me the cigarettes"]={{path="chatsounds/autoadd/midori/give me the cigare
 L["give me the water"]={{path="chatsounds/autoadd/midori/give me the water.ogg",length=6.614}}
 L["i am very hungry"]={{path="chatsounds/autoadd/midori/i am very hungry.ogg",length=2.970}}
 L["im never comin here again"]={{path="chatsounds/autoadd/midori/im never comin here again.ogg",length=3.097}}
+L["mlg xfiles"]={{path="chatsounds/autoadd/midori/mlg xfiles.ogg",length=38.582}}
 L["no i dont want that"]={{path="chatsounds/autoadd/midori/no i dont want that.ogg",length=2.541}}
 L["that was terrible"]={{path="chatsounds/autoadd/midori/that was terrible.ogg",length=2.029}}
 L["that wasnt bad"]={{path="chatsounds/autoadd/midori/that wasnt bad.ogg",length=2.871}}
