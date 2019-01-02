d5d08026c9dd0cfcc51a32b9502a0153fbb7ef2b
Kody Wright <kodyman@att.net>
2019-01-01 19:59:55 -0500

    thirsty clown and fixed banana

--- lua/chatsounds/lists_nosend/midori.lua
+++ lua/chatsounds/lists_nosend/midori.lua
@@ -1,4 +1,11 @@
 c.StartList("midori")
+L["balloon fellatio"]={{path="chatsounds/autoadd/midori/balloon fellatio.ogg",length=2.508}}
+L["banana screech"]={{path="chatsounds/autoadd/midori/banana screech.ogg",length=1.695}}
+L["daddy"]={{path="chatsounds/autoadd/midori/daddy.ogg",length=0.708}}
+L["demented clown horn"]={{path="chatsounds/autoadd/midori/demented clown horn/1.ogg",length=1.347},{path="chatsounds/autoadd/midori/demented clown horn/2.ogg",length=0.998}}
+L["do i look pretty for you"]={{path="chatsounds/autoadd/midori/do i look pretty for you.ogg",length=2.171}}
+L["do you like my hair"]={{path="chatsounds/autoadd/midori/do you like my hair.ogg",length=1.312}}
+L["do you want a balloon animal"]={{path="chatsounds/autoadd/midori/do you want a balloon animal.ogg",length=2.090}}
 L["eating sound"]={{path="chatsounds/autoadd/midori/eating sound/1.ogg",length=2.396},{path="chatsounds/autoadd/midori/eating sound/2.ogg",length=2.812},{path="chatsounds/autoadd/midori/eating sound/3.ogg",length=2.396},{path="chatsounds/autoadd/midori/eating sound/4.ogg",length=4.207}}
 L["give me french fry"]={{path="chatsounds/autoadd/midori/give me french fry.ogg",length=5.083}}
 L["give me the butter"]={{path="chatsounds/autoadd/midori/give me the butter.ogg",length=2.812}}
@@ -6,10 +13,23 @@ L["give me the cheeese"]={{path="chatsounds/autoadd/midori/give me the cheeese.o
 L["give me the cheese"]={{path="chatsounds/autoadd/midori/give me the cheese.ogg",length=3.208}}
 L["give me the cigarettes"]={{path="chatsounds/autoadd/midori/give me the cigarettes.ogg",length=4.079}}
 L["give me the water"]={{path="chatsounds/autoadd/midori/give me the water.ogg",length=6.614}}
+L["hey daddy"]={{path="chatsounds/autoadd/midori/hey daddy.ogg",length=1.347}}
+L["hey daddy i know what you wanna do"]={{path="chatsounds/autoadd/midori/hey daddy i know what you wanna do.ogg",length=2.496}}
+L["hows this one"]={{path="chatsounds/autoadd/midori/hows this one.ogg",length=1.533}}
 L["i am very hungry"]={{path="chatsounds/autoadd/midori/i am very hungry.ogg",length=2.970}}
+L["i just wanted to remind you how beautiful you are"]={{path="chatsounds/autoadd/midori/i just wanted to remind you how beautiful you are.ogg",length=3.460}}
+L["i wanna be your boyfriend"]={{path="chatsounds/autoadd/midori/i wanna be your boyfriend.ogg",length=2.159}}
 L["im never comin here again"]={{path="chatsounds/autoadd/midori/im never comin here again.ogg",length=3.097}}
+L["is my hair pretty"]={{path="chatsounds/autoadd/midori/is my hair pretty.ogg",length=1.997}}
+L["its a duck"]={{path="chatsounds/autoadd/midori/its a duck.ogg",length=1.637}}
+L["ive missed you"]={{path="chatsounds/autoadd/midori/ive missed you.ogg",length=1.393}}
 L["mlg xfiles"]={{path="chatsounds/autoadd/midori/mlg xfiles.ogg",length=38.582}}
 L["no i dont want that"]={{path="chatsounds/autoadd/midori/no i dont want that.ogg",length=2.541}}
+L["oh these are pretty cool bananas"]={{path="chatsounds/autoadd/midori/oh these are pretty cool bananas.ogg",length=1.820}}
+L["pumping balloon"]={{path="chatsounds/autoadd/midori/pumping balloon.ogg",length=5.979}}
 L["that was terrible"]={{path="chatsounds/autoadd/midori/that was terrible.ogg",length=2.029}}
 L["that wasnt bad"]={{path="chatsounds/autoadd/midori/that wasnt bad.ogg",length=2.871}}
+L["where have you been"]={{path="chatsounds/autoadd/midori/where have you been.ogg",length=1.335}}
+L["yeah"]={{path="chatsounds/autoadd/midori/yeah.ogg",length=0.813}}
+L["you know what they say about big feet right"]={{path="chatsounds/autoadd/midori/you know what they say about big feet right.ogg",length=3.274}}
 c.EndList()
