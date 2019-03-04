1510e648e63968bc677af849b0a6297f8ce97480
Meta Automator <MetaAutomator@users.noreply.github.com>
2019-03-04 05:35:34 +0000

    chatsounds preprocessor (e2a28f049a33a1316cbb2e4c6f12b2d7fad83cd7)
    [ci skip]

--- lua/chatsounds/lists_nosend/scooter.lua
+++ lua/chatsounds/lists_nosend/scooter.lua
@@ -1,10 +1,10 @@
 c.StartList("scooter")
-L["amateur hour goes on and on"]={{path="chatsounds/autoadd/scooter/amateur hour.ogg",length=6.840}}
-L["encore hardcore rock you down to the floor"]={{path="chatsounds/autoadd/scooter/encore hardcore.ogg",length=6.400}}
-L["how do you do i thought why not na na na na just me and you"]={{path="chatsounds/autoadd/scooter/how do you do.ogg",length=13.490}}
-L["how much is the fish"]={{path="chatsounds/autoadd/scooter/how much is the fish.ogg",length=3.100}}
-L["alright crew its weekend"]={{path="chatsounds/autoadd/scooter/its weekend.ogg",length=5.710}}
-L["my name is mc h"]={{path="chatsounds/autoadd/scooter/my name is mc h.ogg",length=5.710}}
-L["jumping is an ability"]={{path="chatsounds/autoadd/scooter/wikipediajumping.ogg",length=5.710}}
-L["yeaahh"]={{path="chatsounds/autoadd/scooter/yeaahh.ogg",length=5.710}}
+L["amateur hour"]={{path="chatsounds/autoadd/scooter/amateur hour.ogg",length=6.843}}
+L["encore hardcore"]={{path="chatsounds/autoadd/scooter/encore hardcore.ogg",length=6.400}}
+L["how do you do"]={{path="chatsounds/autoadd/scooter/how do you do.ogg",length=13.491}}
+L["how much is the fish"]={{path="chatsounds/autoadd/scooter/how much is the fish.ogg",length=3.101}}
+L["its weekend"]={{path="chatsounds/autoadd/scooter/its weekend.ogg",length=5.708}}
+L["my name is mc h"]={{path="chatsounds/autoadd/scooter/my name is mc h.ogg",length=15.191}}
+L["wikipediajumping"]={{path="chatsounds/autoadd/scooter/wikipediajumping.ogg",length=47.247}}
+L["yeaahh"]={{path="chatsounds/autoadd/scooter/yeaahh.ogg",length=2.545}}
 c.EndList()
