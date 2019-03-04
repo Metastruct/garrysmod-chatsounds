1619897d8810bd695c2c857adfce03d6f99b8c0d
pancakeportal99 <doofevil10@gmail.com>
2019-03-04 00:16:57 -0500

    Add files via upload

---
+++ lua/chatsounds/lists_nosend/scooter.lua
@@ -0,0 +1,10 @@
+c.StartList("scooter")
+L["amateur hour goes on and on"]={{path="chatsounds/autoadd/scooter/amateur hour.ogg",length=6.840}}
+L["encore hardcore rock you down to the floor"]={{path="chatsounds/autoadd/scooter/encore hardcore.ogg",length=6.400}}
+L["how do you do i thought why not na na na na just me and you"]={{path="chatsounds/autoadd/scooter/how do you do.ogg",length=13.490}}
+L["how much is the fish"]={{path="chatsounds/autoadd/scooter/how much is the fish.ogg",length=3.100}}
+L["alright crew its weekend"]={{path="chatsounds/autoadd/scooter/its weekend.ogg",length=5.710}}
+L["my name is mc h"]={{path="chatsounds/autoadd/scooter/my name is mc h.ogg",length=5.710}}
+L["jumping is an ability"]={{path="chatsounds/autoadd/scooter/wikipediajumping.ogg",length=5.710}}
+L["yeaahh"]={{path="chatsounds/autoadd/scooter/yeaahh.ogg",length=5.710}}
+c.EndList()
