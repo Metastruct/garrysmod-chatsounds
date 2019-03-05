f236c63cd85aad227a630556200cfaee0ce6c263
Meta Automator <MetaAutomator@users.noreply.github.com>
2019-03-05 08:38:53 +0000

    chatsounds preprocessor (08c03de930d12c4f76209ff0651add7b17ec6bf3)
    [ci skip]

--- lua/chatsounds/lists_nosend/scooter.lua
+++ lua/chatsounds/lists_nosend/scooter.lua
@@ -1,10 +1,18 @@
 c.StartList("scooter")
 L["amateur hour"]={{path="chatsounds/autoadd/scooter/amateur hour.ogg",length=6.843}}
+L["chillybow"]={{path="chatsounds/autoadd/scooter/chillybow.ogg",length=2.425}}
+L["doop doop"]={{path="chatsounds/autoadd/scooter/doop doop.ogg",length=6.691}}
 L["encore hardcore"]={{path="chatsounds/autoadd/scooter/encore hardcore.ogg",length=6.400}}
+L["faster harder scooter"]={{path="chatsounds/autoadd/scooter/faster harder scooter.ogg",length=6.052}}
 L["how do you do"]={{path="chatsounds/autoadd/scooter/how do you do.ogg",length=13.491}}
 L["how much is the fish"]={{path="chatsounds/autoadd/scooter/how much is the fish.ogg",length=3.101}}
+L["hyper hyper"]={{path="chatsounds/autoadd/scooter/hyper hyper.ogg",length=3.251}}
 L["its weekend"]={{path="chatsounds/autoadd/scooter/its weekend.ogg",length=5.708}}
+L["jadore hardcore"]={{path="chatsounds/autoadd/scooter/jadore hardcore.ogg",length=2.492}}
+L["maria believe me i like it loud"]={{path="chatsounds/autoadd/scooter/maria believe me i like it loud.ogg",length=3.156}}
+L["move your ass"]={{path="chatsounds/autoadd/scooter/move your ass.ogg",length=3.093}}
 L["my name is mc h"]={{path="chatsounds/autoadd/scooter/my name is mc h.ogg",length=15.191}}
+L["what can i do do do"]={{path="chatsounds/autoadd/scooter/what can i do do do.ogg",length=13.181}}
 L["wikipediajumping"]={{path="chatsounds/autoadd/scooter/wikipediajumping.ogg",length=47.247}}
 L["yeaahh"]={{path="chatsounds/autoadd/scooter/yeaahh.ogg",length=2.545}}
 c.EndList()
