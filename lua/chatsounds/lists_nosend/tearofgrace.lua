d03ea7343a9efde107d7fb78989ec3ee626823e8
Brian Davis <kjones1999@outlook.com>
2019-03-17 19:24:51 -0400

    imma fight it

--- lua/chatsounds/lists_nosend/tearofgrace.lua
+++ lua/chatsounds/lists_nosend/tearofgrace.lua
@@ -1,9 +1,19 @@
 c.StartList("tearofgrace")
+L["3 ill gain 1 and ill go again"]={{path="chatsounds/autoadd/tearofgrace/3 ill gain 1 and ill go again.ogg",length=4.413}}
+L["4 ill gain 1 and ill go again"]={{path="chatsounds/autoadd/tearofgrace/4 ill gain 1 and ill go again.ogg",length=4.325}}
+L["bastaaard"]={{path="chatsounds/autoadd/tearofgrace/bastaaard.ogg",length=1.413}}
 L["cunt"]={{path="chatsounds/autoadd/tearofgrace/cunt/1.ogg",length=0.746},{path="chatsounds/autoadd/tearofgrace/cunt/2.ogg",length=0.526}}
+L["ehwaz"]={{path="chatsounds/autoadd/tearofgrace/ehwaz.ogg",length=0.813}}
 L["fucking muffins"]={{path="chatsounds/autoadd/tearofgrace/fucking muffins.ogg",length=1.113}}
+L["gangbang of brett part 2 no cunts involved boogaloo"]={{path="chatsounds/autoadd/tearofgrace/gangbang of brett part 2 no cunts involved boogaloo.ogg",length=6.523}}
 L["i dont want bobs rotten fucking head"]={{path="chatsounds/autoadd/tearofgrace/i dont want bobs rotten fucking head.ogg",length=4.385}}
 L["i dont want that"]={{path="chatsounds/autoadd/tearofgrace/i dont want that.ogg",length=2.575}}
+L["imma fight it"]={{path="chatsounds/autoadd/tearofgrace/imma fight it.ogg",length=1.340}}
+L["its a 3 which is gain 4 so ill just take 1 and well go again"]={{path="chatsounds/autoadd/tearofgrace/its a 3 which is gain 4 so ill just take 1 and well go again.ogg",length=6.067}}
 L["just gotta get right out of here"]={{path="chatsounds/autoadd/tearofgrace/just gotta get right out of here.ogg",length=4.494}}
+L["mother"]={{path="chatsounds/autoadd/tearofgrace/mother/1.ogg",length=1.479},{path="chatsounds/autoadd/tearofgrace/mother/2.ogg",length=1.965},{path="chatsounds/autoadd/tearofgrace/mother/3.ogg",length=3.010}}
 L["nothing gained nothing ventured"]={{path="chatsounds/autoadd/tearofgrace/nothing gained nothing ventured.ogg",length=3.284}}
 L["oh its good i can go where ive already been laughter"]={{path="chatsounds/autoadd/tearofgrace/oh its good i can go where ive already been laughter.ogg",length=6.247}}
+L["yay"]={{path="chatsounds/autoadd/tearofgrace/yay.ogg",length=0.879}}
+L["yay wait"]={{path="chatsounds/autoadd/tearofgrace/yay wait.ogg",length=1.024}}
 c.EndList()
