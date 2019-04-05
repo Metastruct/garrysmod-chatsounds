4f814aba446949d4b8b6a6be5052a599af863785
Brian Davis <kjones1999@outlook.com>
2019-04-04 22:09:20 -0400

    ive got the need to seed

--- lua/chatsounds/lists_nosend/borderlands2.lua
+++ lua/chatsounds/lists_nosend/borderlands2.lua
@@ -1,33 +1,58 @@
 c.StartList("borderlands2")
+L["a flower for me"]={{path="chatsounds/autoadd/borderlands2/a flower for me.ogg",length=2.906}}
+L["a new meat puppet to wear"]={{path="chatsounds/autoadd/borderlands2/a new meat puppet to wear.ogg",length=1.980}}
 L["angry im so goddamn angry"]={{path="chatsounds/autoadd/borderlands2/angry im so goddamn angry.ogg",length=5.248}}
+L["another victim"]={{path="chatsounds/autoadd/borderlands2/another victim.ogg",length=1.902}}
+L["another walking meat puppet"]={{path="chatsounds/autoadd/borderlands2/another walking meat puppet.ogg",length=2.368}}
 L["boom time"]={{path="chatsounds/autoadd/borderlands2/boom time.ogg",length=1.137}}
 L["boooom"]={{path="chatsounds/autoadd/borderlands2/boooom.ogg",length=1.855}}
+L["but youll miss teatime"]={{path="chatsounds/autoadd/borderlands2/but youll miss teatime.ogg",length=2.424}}
 L["cant hear you"]={{path="chatsounds/autoadd/borderlands2/cant hear you.ogg",length=1.912}}
+L["come closer"]={{path="chatsounds/autoadd/borderlands2/come closer.ogg",length=1.315}}
 L["corpse robbing"]={{path="chatsounds/autoadd/borderlands2/corpse robbing.ogg",length=1.553}}
 L["die die die die die die die die die"]={{path="chatsounds/autoadd/borderlands2/die die die die die die die die die.ogg",length=5.659}}
+L["dont look at me"]={{path="chatsounds/autoadd/borderlands2/dont look at me.ogg",length=1.287}}
+L["drink it"]={{path="chatsounds/autoadd/borderlands2/drink it.ogg",length=0.937}}
+L["dropkick your babies"]={{path="chatsounds/autoadd/borderlands2/dropkick your babies.ogg",length=2.289}}
 L["e e e"]={{path="chatsounds/autoadd/borderlands2/e e e.ogg",length=1.548}}
 L["explode"]={{path="chatsounds/autoadd/borderlands2/explode.ogg",length=2.334}}
+L["eyes like grapes delicious"]={{path="chatsounds/autoadd/borderlands2/eyes like grapes delicious.ogg",length=2.879}}
+L["facehump time"]={{path="chatsounds/autoadd/borderlands2/facehump time.ogg",length=1.591}}
 L["for france"]={{path="chatsounds/autoadd/borderlands2/for france.ogg",length=4.410}}
+L["french me mommy"]={{path="chatsounds/autoadd/borderlands2/french me mommy.ogg",length=1.793}}
+L["get ready for bad touch"]={{path="chatsounds/autoadd/borderlands2/get ready for bad touch.ogg",length=2.433}}
 L["gimme"]={{path="chatsounds/autoadd/borderlands2/gimme.ogg",length=0.552}}
+L["gimme my hitpoints back"]={{path="chatsounds/autoadd/borderlands2/gimme my hitpoints back.ogg",length=1.349}}
 L["give it give it give it"]={{path="chatsounds/autoadd/borderlands2/give it give it give it.ogg",length=2.131}}
 L["goliathnoises"]={{path="chatsounds/autoadd/borderlands2/goliathnoises/1.ogg",length=4.204},{path="chatsounds/autoadd/borderlands2/goliathnoises/2.ogg",length=3.607},{path="chatsounds/autoadd/borderlands2/goliathnoises/3.ogg",length=4.555},{path="chatsounds/autoadd/borderlands2/goliathnoises/4.ogg",length=4.256}}
 L["gonna floss with your spine"]={{path="chatsounds/autoadd/borderlands2/gonna floss with your spine.ogg",length=4.511}}
 L["gotta kill gotta kill gotta kill"]={{path="chatsounds/autoadd/borderlands2/gotta kill gotta kill gotta kill.ogg",length=6.645}}
 L["gotta scavenge"]={{path="chatsounds/autoadd/borderlands2/gotta scavenge.ogg",length=1.732}}
 L["hate hate hate hate hate"]={{path="chatsounds/autoadd/borderlands2/hate hate hate hate hate.ogg",length=6.671}}
+L["hate your face hate your face"]={{path="chatsounds/autoadd/borderlands2/hate your face hate your face.ogg",length=2.523}}
 L["hatesplosion"]={{path="chatsounds/autoadd/borderlands2/hatesplosion.ogg",length=2.811}}
 L["here it comes aahahaha"]={{path="chatsounds/autoadd/borderlands2/here it comes aahahaha.ogg",length=8.521}}
 L["here it comes big boy"]={{path="chatsounds/autoadd/borderlands2/here it comes big boy.ogg",length=4.876}}
+L["high five oh youre dead sorry"]={{path="chatsounds/autoadd/borderlands2/high five oh youre dead sorry.ogg",length=2.175}}
+L["i am immortal"]={{path="chatsounds/autoadd/borderlands2/i am immortal.ogg",length=2.247}}
 L["i got da bomb"]={{path="chatsounds/autoadd/borderlands2/i got da bomb/1.ogg",length=0.878},{path="chatsounds/autoadd/borderlands2/i got da bomb/2.ogg",length=1.663}}
+L["i like my victims how i like my coffee in the butt"]={{path="chatsounds/autoadd/borderlands2/i like my victims how i like my coffee in the butt.ogg",length=4.040}}
+L["i love you"]={{path="chatsounds/autoadd/borderlands2/i love you.ogg",length=1.380}}
 L["i swear its a cupcake"]={{path="chatsounds/autoadd/borderlands2/i swear its a cupcake.ogg",length=1.675}}
+L["i wanna play just play with me"]={{path="chatsounds/autoadd/borderlands2/i wanna play just play with me.ogg",length=3.695}}
 L["i want it gimme gimme gimme"]={{path="chatsounds/autoadd/borderlands2/i want it gimme gimme gimme.ogg",length=2.209}}
+L["ill miss you"]={{path="chatsounds/autoadd/borderlands2/ill miss you.ogg",length=1.346}}
+L["im free"]={{path="chatsounds/autoadd/borderlands2/im free.ogg",length=2.427}}
 L["im rich bitch"]={{path="chatsounds/autoadd/borderlands2/im rich bitch.ogg",length=1.760}}
 L["im starving"]={{path="chatsounds/autoadd/borderlands2/im starving.ogg",length=2.817}}
 L["in the stash"]={{path="chatsounds/autoadd/borderlands2/in the stash.ogg",length=1.405}}
 L["its going in my stash"]={{path="chatsounds/autoadd/borderlands2/its going in my stash.ogg",length=2.183}}
+L["its inside me its inside me"]={{path="chatsounds/autoadd/borderlands2/its inside me its inside me.ogg",length=3.045}}
+L["its meat puppet master theatre"]={{path="chatsounds/autoadd/borderlands2/its meat puppet master theatre.ogg",length=2.990}}
 L["its mine"]={{path="chatsounds/autoadd/borderlands2/its mine.ogg",length=1.817}}
 L["its mine now"]={{path="chatsounds/autoadd/borderlands2/its mine now.ogg",length=2.089}}
 L["its oh its serious now"]={{path="chatsounds/autoadd/borderlands2/its oh its serious now.ogg",length=5.462}}
+L["ive got the need to seed"]={{path="chatsounds/autoadd/borderlands2/ive got the need to seed.ogg",length=3.033}}
 L["just gotta explode"]={{path="chatsounds/autoadd/borderlands2/just gotta explode.ogg",length=2.135}}
 L["kaboom baby"]={{path="chatsounds/autoadd/borderlands2/kaboom baby.ogg",length=6.086}}
 L["kamikaze"]={{path="chatsounds/autoadd/borderlands2/kamikaze.ogg",length=1.408}}
@@ -40,25 +65,36 @@ L["no need to shout"]={{path="chatsounds/autoadd/borderlands2/no need to shout.o
 L["now"]={{path="chatsounds/autoadd/borderlands2/now.ogg",length=0.538}}
 L["omnomnomnumnumnum"]={{path="chatsounds/autoadd/borderlands2/omnomnomnumnumnum.ogg",length=2.255}}
 L["ooooohoohoo"]={{path="chatsounds/autoadd/borderlands2/ooooohoohoo.ogg",length=2.262}}
+L["ouch"]={{path="chatsounds/autoadd/borderlands2/ouch.ogg",length=0.587}}
+L["owwie"]={{path="chatsounds/autoadd/borderlands2/owwie.ogg",length=1.103}}
 L["playtime is over"]={{path="chatsounds/autoadd/borderlands2/playtime is over.ogg",length=3.854}}
 L["psychonoises"]={{path="chatsounds/autoadd/borderlands2/psychonoises.ogg",length=4.918}}
+L["psychopain"]={{path="chatsounds/autoadd/borderlands2/psychopain/1.ogg",length=0.469},{path="chatsounds/autoadd/borderlands2/psychopain/2.ogg",length=0.339},{path="chatsounds/autoadd/borderlands2/psychopain/3.ogg",length=0.485},{path="chatsounds/autoadd/borderlands2/psychopain/4.ogg",length=0.249},{path="chatsounds/autoadd/borderlands2/psychopain/5.ogg",length=0.561}}
 L["pull the pin feel the pain"]={{path="chatsounds/autoadd/borderlands2/pull the pin feel the pain.ogg",length=2.224}}
 L["ratnoises"]={{path="chatsounds/autoadd/borderlands2/ratnoises.ogg",length=2.262}}
 L["seeeeeemeeennnn"]={{path="chatsounds/autoadd/borderlands2/seeeeeemeeennnn.ogg",length=3.490}}
 L["should not you really shouldnt have done that"]={{path="chatsounds/autoadd/borderlands2/should not you really shouldnt have done that.ogg",length=5.717}}
+L["slurp it drink it eat it sniff smell it"]={{path="chatsounds/autoadd/borderlands2/slurp it drink it eat it sniff smell it.ogg",length=4.164}}
 L["slurp it up"]={{path="chatsounds/autoadd/borderlands2/slurp it up.ogg",length=1.530}}
 L["so hungry"]={{path="chatsounds/autoadd/borderlands2/so hungry.ogg",length=2.706}}
 L["sscream"]={{path="chatsounds/autoadd/borderlands2/sscream.ogg",length=1.798}}
 L["stop looking at me"]={{path="chatsounds/autoadd/borderlands2/stop looking at me.ogg",length=2.253}}
 L["suck it"]={{path="chatsounds/autoadd/borderlands2/suck it.ogg",length=1.164}}
+L["suck the marrow"]={{path="chatsounds/autoadd/borderlands2/suck the marrow.ogg",length=2.126}}
 L["thank you for the loot brother"]={{path="chatsounds/autoadd/borderlands2/thank you for the loot brother.ogg",length=1.830}}
 L["thanks brother"]={{path="chatsounds/autoadd/borderlands2/thanks brother.ogg",length=2.023}}
+L["the worst"]={{path="chatsounds/autoadd/borderlands2/the worst.ogg",length=0.917}}
 L["tick tick tick tick"]={{path="chatsounds/autoadd/borderlands2/tick tick tick tick.ogg",length=1.137}}
 L["touching"]={{path="chatsounds/autoadd/borderlands2/touching.ogg",length=1.059}}
 L["underground"]={{path="chatsounds/autoadd/borderlands2/underground.ogg",length=1.843}}
+L["unforgivable unforgivable"]={{path="chatsounds/autoadd/borderlands2/unforgivable unforgivable.ogg",length=3.111}}
+L["wear your face like a condom"]={{path="chatsounds/autoadd/borderlands2/wear your face like a condom.ogg",length=1.790}}
+L["what does it all mean"]={{path="chatsounds/autoadd/borderlands2/what does it all mean.ogg",length=2.520}}
 L["why are you yelling"]={{path="chatsounds/autoadd/borderlands2/why are you yelling.ogg",length=3.333}}
+L["why will no one play with me"]={{path="chatsounds/autoadd/borderlands2/why will no one play with me.ogg",length=1.566}}
 L["wooohooo"]={{path="chatsounds/autoadd/borderlands2/wooohooo.ogg",length=1.268}}
 L["yes"]={{path="chatsounds/autoadd/borderlands2/yes.ogg",length=1.402}}
 L["yoink"]={{path="chatsounds/autoadd/borderlands2/yoink.ogg",length=0.539}}
 L["you dont need this anymore"]={{path="chatsounds/autoadd/borderlands2/you dont need this anymore.ogg",length=2.520}}
+L["you smell like my little girl"]={{path="chatsounds/autoadd/borderlands2/you smell like my little girl.ogg",length=2.508}}
 c.EndList()
