58c2cb4c79c4631b523f3b2f5ac05c43f70da2df
Meta Automator <MetaAutomator@users.noreply.github.com>
2019-04-14 22:21:28 +0000

    chatsounds preprocessor (e93ba739bf1a88cc7c7f15903511340d69e47cda)
    [ci skip]

--- lua/chatsounds/lists_nosend/spiderbite.lua
+++ lua/chatsounds/lists_nosend/spiderbite.lua
@@ -1,6 +1,7 @@
 c.StartList("spiderbite")
 L["cus thats just messy"]={{path="chatsounds/autoadd/spiderbite/cus thats just messy.ogg",length=1.579}}
 L["dont let it bite ya"]={{path="chatsounds/autoadd/spiderbite/dont let it bite ya.ogg",length=1.491}}
+L["god forbid it bite ya"]={{path="chatsounds/autoadd/spiderbite/god forbid it bite ya.ogg",length=1.723}}
 L["hurt"]={{path="chatsounds/autoadd/spiderbite/hurt.ogg",length=1.270}}
 L["hurt spider bite"]={{path="chatsounds/autoadd/spiderbite/hurt spider bite.ogg",length=1.822}}
 L["just dont let it happen"]={{path="chatsounds/autoadd/spiderbite/just dont let it happen.ogg",length=1.185}}
@@ -14,5 +15,9 @@ L["spider bite"]={{path="chatsounds/autoadd/spiderbite/spider bite/spiderbite1.o
 L["spider bite boy"]={{path="chatsounds/autoadd/spiderbite/spider bite boy.ogg",length=2.021}}
 L["spider bite hurt"]={{path="chatsounds/autoadd/spiderbite/spider bite hurt.ogg",length=2.043}}
 L["that spider bit me"]={{path="chatsounds/autoadd/spiderbite/that spider bit me.ogg",length=1.646}}
+L["that spider bit me where the good lord split me"]={{path="chatsounds/autoadd/spiderbite/that spider bit me where the good lord split me.ogg",length=3.385}}
+L["that spider bite cha"]={{path="chatsounds/autoadd/spiderbite/that spider bite cha.ogg",length=1.601}}
+L["that spider bite hurt"]={{path="chatsounds/autoadd/spiderbite/that spider bite hurt.ogg",length=1.844}}
 L["what happen"]={{path="chatsounds/autoadd/spiderbite/what happen.ogg",length=0.833}}
+L["you definately dont want that spider to bit you where the good lord split you"]={{path="chatsounds/autoadd/spiderbite/you definately dont want that spider to bit you where the good lord split you.ogg",length=3.527}}
 c.EndList()
