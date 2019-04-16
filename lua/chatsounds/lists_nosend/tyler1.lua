9308da9ddcf04a2858fe6018b3f920f0dca1cf07
WicopeeDot <mineev.0807@gmail.com>
2019-04-16 22:57:09 +0300

    tyler1 chatsounds

---
+++ lua/chatsounds/lists_nosend/tyler1.lua
@@ -0,0 +1,24 @@
+c.StartList("tyler1")
+L["ah get this beta shit of me"]={{path="chatsounds/autoadd/tyler1/ah get this beta shit of me.ogg",length=2.813}}
+L["autismrage sugoi sugoi sugoyo"]={{path="chatsounds/autoadd/tyler1/autismrage sugoi sugoi sugoyo.ogg",length=3.612}}
+L["ayyyy help heeelp"]={{path="chatsounds/autoadd/tyler1/ayyyy help heeelp.ogg",length=6.444}}
+L["carsounds"]={{path="chatsounds/autoadd/tyler1/carsounds.ogg",length=11.363}}
+L["cowsounds"]={{path="chatsounds/autoadd/tyler1/cowsounds.ogg",length=2.269}}
+L["dogsounds"]={{path="chatsounds/autoadd/tyler1/dogsounds.ogg",length=3.667}}
+L["fave"]={{path="chatsounds/autoadd/tyler1/fave.ogg",length=0.363}}
+L["get out of my fucking head please"]={{path="chatsounds/autoadd/tyler1/get out of my fucking head please.ogg",length=3.322}}
+L["get this shit of me"]={{path="chatsounds/autoadd/tyler1/get this shit of me.ogg",length=2.323}}
+L["holy shit dude"]={{path="chatsounds/autoadd/tyler1/holy shit dude.ogg",length=1.652}}
+L["i get it its loud shut up"]={{path="chatsounds/autoadd/tyler1/i get it its loud shut up.ogg",length=2.106}}
+L["im a scream hold on"]={{path="chatsounds/autoadd/tyler1/im a scream hold on.ogg",length=2.287}}
+L["im ice poseidon long live ice windowsxpshutdown"]={{path="chatsounds/autoadd/tyler1/im ice poseidon long live ice windowsxpshutdown.ogg",length=6.734}}
+L["milk biscuits"]={{path="chatsounds/autoadd/tyler1/milk biscuits.ogg",length=1.724}}
+L["sniffing intensifies"]={{path="chatsounds/autoadd/tyler1/sniffing intensifies.ogg",length=1.743}}
+L["tyler1 teleports"]={{path="chatsounds/autoadd/tyler1/tyler1 teleports.ogg",length=2.904}}
+L["tyler1autism"]={{path="chatsounds/autoadd/tyler1/tyler1autism/1.ogg",length=4.792},{path="chatsounds/autoadd/tyler1/tyler1autism/10.ogg",length=3.503},{path="chatsounds/autoadd/tyler1/tyler1autism/11.ogg",length=6.335},{path="chatsounds/autoadd/tyler1/tyler1autism/12.ogg",length=5.246},{path="chatsounds/autoadd/tyler1/tyler1autism/13.ogg",length=1.089},{path="chatsounds/autoadd/tyler1/tyler1autism/2.ogg",length=2.650},{path="chatsounds/autoadd/tyler1/tyler1autism/3.ogg",length=3.231},{path="chatsounds/autoadd/tyler1/tyler1autism/4.ogg",length=4.247},{path="chatsounds/autoadd/tyler1/tyler1autism/5.ogg",length=2.596},{path="chatsounds/autoadd/tyler1/tyler1autism/6.ogg",length=3.249},{path="chatsounds/autoadd/tyler1/tyler1autism/7.ogg",length=4.374},{path="chatsounds/autoadd/tyler1/tyler1autism/8.ogg",length=5.827},{path="chatsounds/autoadd/tyler1/tyler1autism/9.ogg",length=4.592}}
+L["tyler1rage"]={{path="chatsounds/autoadd/tyler1/tyler1rage.ogg",length=11.508}}
+L["tyler1scream"]={{path="chatsounds/autoadd/tyler1/tyler1scream/1.ogg",length=0.944},{path="chatsounds/autoadd/tyler1/tyler1scream/10.ogg",length=4.247},{path="chatsounds/autoadd/tyler1/tyler1scream/2.ogg",length=3.031},{path="chatsounds/autoadd/tyler1/tyler1scream/3.ogg",length=4.429},{path="chatsounds/autoadd/tyler1/tyler1scream/4.ogg",length=1.779},{path="chatsounds/autoadd/tyler1/tyler1scream/5.ogg",length=2.087},{path="chatsounds/autoadd/tyler1/tyler1scream/6.ogg",length=2.396},{path="chatsounds/autoadd/tyler1/tyler1scream/7.ogg",length=1.851},{path="chatsounds/autoadd/tyler1/tyler1scream/8.ogg",length=3.158},{path="chatsounds/autoadd/tyler1/tyler1scream/9.ogg",length=4.374}}
+L["uhh"]={{path="chatsounds/autoadd/tyler1/uhh.ogg",length=0.508}}
+L["wait am i live oh"]={{path="chatsounds/autoadd/tyler1/wait am i live oh.ogg",length=0.980}}
+L["you wanna see loud"]={{path="chatsounds/autoadd/tyler1/you wanna see loud.ogg",length=1.307}}
+c.EndList()
