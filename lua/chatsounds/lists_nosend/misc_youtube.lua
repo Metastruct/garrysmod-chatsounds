36583ab0661d2dcd0e55929970b549bf4fbeb21c
etothepowerof26 <jdhale2004@gmail.com>
2019-04-27 18:03:51 -0400

    fix yeet.ogg's sample rate
    doesn't make preprocessor angry

--- lua/chatsounds/lists_nosend/misc_youtube.lua
+++ lua/chatsounds/lists_nosend/misc_youtube.lua
@@ -1,5 +1,6 @@
 c.StartList("misc_youtube")
 L["notice me senpai"]={{path="chatsounds/autoadd/misc_youtube/notice me senpai.ogg",length=2.572}}
 L["oh yeah mister crabs"]={{path="chatsounds/autoadd/misc_youtube/oh yeah mister crabs.ogg",length=2.017}}
+L["yeet"]={{path="chatsounds/autoadd/misc_youtube/yeet.ogg",length=0.634}}
 L["yooooooooooo"]={{path="chatsounds/autoadd/misc_youtube/yooooooooooo.ogg",length=2.566}}
 c.EndList()
