efc5287293348376634d08498feffccec417da60
redv-2 <50215718+redv-2@users.noreply.github.com>
2019-05-02 20:08:47 +0100

    more jojo chatsounds (#196)
    * Add files via upload
    
    * Ran preprocessor

---
+++ lua/chatsounds/lists_nosend/jojo.lua
@@ -0,0 +1,7 @@
+c.StartList("jojo")
+L["caesarrr"]={{path="chatsounds/autoadd/jojo/caesarrr.ogg",length=5.715}}
+L["doppio ring"]={{path="chatsounds/autoadd/jojo/doppio ring.ogg",length=11.434}}
+L["jotaro shut up"]={{path="chatsounds/autoadd/jojo/jotaro shut up.ogg",length=4.016}}
+L["nice"]={{path="chatsounds/autoadd/jojo/nice.ogg",length=2.252}}
+L["wryyy"]={{path="chatsounds/autoadd/jojo/wryyy.ogg",length=3.020}}
+c.EndList()
