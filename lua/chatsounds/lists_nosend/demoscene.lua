ca4bfa1c02e6f70d8743b6bbccb3895ec29f932b
PotcFdk <PotcFdk@gmx.net>
2019-05-07 02:04:28 +0200

    Add stuff to the demoscene set.

--- lua/chatsounds/lists_nosend/demoscene.lua
+++ lua/chatsounds/lists_nosend/demoscene.lua
@@ -1,8 +1,15 @@
 c.StartList("demoscene")
 L["atomic playboy"]={{path="chatsounds/autoadd/demoscene/atomic playboy.ogg",length=1.802}}
+L["geiler hydrant"]={{path="chatsounds/autoadd/demoscene/geiler hydrant.ogg",length=1.256}}
 L["get down"]={{path="chatsounds/autoadd/demoscene/get down.ogg",length=0.816}}
+L["hello kevin"]={{path="chatsounds/autoadd/demoscene/hello kevin.ogg",length=0.669}}
+L["ja"]={{path="chatsounds/autoadd/demoscene/ja/01.ogg",length=0.551},{path="chatsounds/autoadd/demoscene/ja/02.ogg",length=0.532},{path="chatsounds/autoadd/demoscene/ja/03.ogg",length=0.454},{path="chatsounds/autoadd/demoscene/ja/04.ogg",length=0.435},{path="chatsounds/autoadd/demoscene/ja/05.ogg",length=0.435},{path="chatsounds/autoadd/demoscene/ja/06.ogg",length=0.396},{path="chatsounds/autoadd/demoscene/ja/07.ogg",length=0.358},{path="chatsounds/autoadd/demoscene/ja/08.ogg",length=0.367},{path="chatsounds/autoadd/demoscene/ja/09.ogg",length=0.367},{path="chatsounds/autoadd/demoscene/ja/10.ogg",length=0.425},{path="chatsounds/autoadd/demoscene/ja/11.ogg",length=0.358},{path="chatsounds/autoadd/demoscene/ja/12.ogg",length=0.425},{path="chatsounds/autoadd/demoscene/ja/13.ogg",length=0.348},{path="chatsounds/autoadd/demoscene/ja/14.ogg",length=0.367},{path="chatsounds/autoadd/demoscene/ja/15.ogg",length=0.454}}
+L["ladies and gentlemen"]={{path="chatsounds/autoadd/demoscene/ladies and gentlemen.ogg",length=1.682}}
+L["marimba"]={{path="chatsounds/autoadd/demoscene/marimba/01.ogg",length=2.510},{path="chatsounds/autoadd/demoscene/marimba/02.ogg",length=5.780}}
 L["razor"]={{path="chatsounds/autoadd/demoscene/razor.ogg",length=7.296}}
 L["second reality"]={{path="chatsounds/autoadd/demoscene/second reality.ogg",length=3.715}}
+L["soccer sport"]={{path="chatsounds/autoadd/demoscene/soccer sport/soccer sport.ogg",length=1.388},{path="chatsounds/autoadd/demoscene/soccer sport/soccer sport2.ogg",length=1.523}}
 L["ten seconds to transmission"]={{path="chatsounds/autoadd/demoscene/ten seconds to transmission.ogg",length=1.501}}
+L["this is the captain speaking"]={{path="chatsounds/autoadd/demoscene/this is the captain speaking.ogg",length=1.548}}
 L["transmission"]={{path="chatsounds/autoadd/demoscene/transmission.ogg",length=0.772}}
 c.EndList()
