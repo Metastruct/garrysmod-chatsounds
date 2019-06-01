625cf2b3472994ea732f775c40c32a409cce06b2
mitterdoo <connorashcroft@live.com>
2019-06-01 15:51:03 -0700

    fixing eric andre sounds

---
+++ lua/chatsounds/lists_nosend/ericandre.lua
@@ -0,0 +1,5 @@
+c.StartList("ericandre")
+L["let me iiiin"]={{path="chatsounds/autoadd/ericandre/let me iiiin.ogg",length=1.091}}
+L["let me in"]={{path="chatsounds/autoadd/ericandre/let me in.ogg",length=1.015}}
+L["let me in let me iiiin"]={{path="chatsounds/autoadd/ericandre/let me in let me iiiin.ogg",length=2.435}}
+c.EndList()
