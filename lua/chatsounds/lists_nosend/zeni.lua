aecc1cdaffeb52e1ede4024bc3a57541f5a69de3
Zeni <timrace01@gmail.com>
2019-06-12 03:45:35 +0200

    thanossnap

--- lua/chatsounds/lists_nosend/zeni.lua
+++ lua/chatsounds/lists_nosend/zeni.lua
@@ -10,16 +10,20 @@ L["flextapechainsaw"]={{path="chatsounds/autoadd/zeni/flextapechainsaw.ogg",leng
 L["flextapesawing"]={{path="chatsounds/autoadd/zeni/flextapesawing.ogg",length=6.313}}
 L["get out of here stalker"]={{path="chatsounds/autoadd/zeni/get out of here stalker.ogg",length=1.456}}
 L["how about a little more"]={{path="chatsounds/autoadd/zeni/how about a little more.ogg",length=1.039}}
+L["i am inevitable"]={{path="chatsounds/autoadd/zeni/i am inevitable.ogg",length=2.668}}
 L["i sawed this boat in half"]={{path="chatsounds/autoadd/zeni/i sawed this boat in half.ogg",length=2.070}}
 L["ladies and gentlemen we got him"]={{path="chatsounds/autoadd/zeni/ladies and gentlemen we got him.ogg",length=2.899}}
 L["now thats a lotta damage"]={{path="chatsounds/autoadd/zeni/now thats a lotta damage.ogg",length=1.750}}
+L["perfectly balanced as all things should be"]={{path="chatsounds/autoadd/zeni/perfectly balanced as all things should be.ogg",length=3.292}}
 L["please i just want to see my wife again"]={{path="chatsounds/autoadd/zeni/please i just want to see my wife again.ogg",length=3.021}}
 L["risitadoom"]={{path="chatsounds/autoadd/zeni/risitadoom.ogg",length=17.329}}
 L["risitalaugh"]={{path="chatsounds/autoadd/zeni/risitalaugh/1.ogg",length=11.353},{path="chatsounds/autoadd/zeni/risitalaugh/2.ogg",length=7.872},{path="chatsounds/autoadd/zeni/risitalaugh/3.ogg",length=4.342},{path="chatsounds/autoadd/zeni/risitalaugh/4.ogg",length=3.087},{path="chatsounds/autoadd/zeni/risitalaugh/5.ogg",length=4.389},{path="chatsounds/autoadd/zeni/risitalaugh/6.ogg",length=5.248}}
 L["spidermanpizzatheme"]={{path="chatsounds/autoadd/zeni/spidermanpizzatheme.ogg",length=87.290}}
+L["thanossnap"]={{path="chatsounds/autoadd/zeni/thanossnap/1.ogg",length=4.435},{path="chatsounds/autoadd/zeni/thanossnap/2.ogg",length=1.043}}
 L["thats a lotta damage"]={{path="chatsounds/autoadd/zeni/thats a lotta damage.ogg",length=1.615}}
 L["to show you the power of flex tape"]={{path="chatsounds/autoadd/zeni/to show you the power of flex tape.ogg",length=2.948}}
 L["welcome to mcdawnalds"]={{path="chatsounds/autoadd/zeni/welcome to mcdawnalds.ogg",length=3.493}}
+L["you should have gone for the head"]={{path="chatsounds/autoadd/zeni/you should have gone for the head.ogg",length=3.366}}
 L["zombiescream"]={{path="chatsounds/autoadd/zeni/zombiescream.ogg",length=3.982}}
 L["zombiescream distorted"]={{path="chatsounds/autoadd/zeni/zombiescream distorted.ogg",length=3.982}}
 c.EndList()
