f3eb68bbc2bab6209b956a56cd9aab7d038fa362
etothepowerof26 <jdhale2004@gmail.com>
2019-06-24 18:56:54 -0400

    Chatsounds for Ahamudo

--- lua/chatsounds/lists_nosend/ahamudo.lua
+++ lua/chatsounds/lists_nosend/ahamudo.lua
@@ -1,20 +1,46 @@
 c.StartList("ahamudo")
+L["achkmed"]={{path="chatsounds/autoadd/ahamudo/achkmed.ogg",length=0.852}}
+L["ahmad"]={{path="chatsounds/autoadd/ahamudo/ahmad.ogg",length=0.659}}
 L["awwh theres everything inside of here noooo"]={{path="chatsounds/autoadd/ahamudo/awwh theres everything inside of here noooo.ogg",length=4.238}}
+L["baka mitai"]={{path="chatsounds/autoadd/ahamudo/baka mitai.ogg",length=291.318}}
+L["banzaiii"]={{path="chatsounds/autoadd/ahamudo/banzaiii/1.ogg",length=0.894},{path="chatsounds/autoadd/ahamudo/banzaiii/2.ogg",length=1.115}}
 L["bhd sobbing"]={{path="chatsounds/autoadd/ahamudo/bhd sobbing.ogg",length=7.709}}
 L["bitch you guessed it hoo"]={{path="chatsounds/autoadd/ahamudo/bitch you guessed it hoo.ogg",length=2.473}}
+L["deiyya"]={{path="chatsounds/autoadd/ahamudo/deiyya.ogg",length=0.604}}
 L["e e e e e e e e eh"]={{path="chatsounds/autoadd/ahamudo/e e e e e e e e eh.ogg",length=1.474}}
+L["fate buffmajor"]={{path="chatsounds/autoadd/ahamudo/fate buffmajor.ogg",length=1.405}}
+L["fate buffminor"]={{path="chatsounds/autoadd/ahamudo/fate buffminor.ogg",length=1.080}}
+L["fate buffremoval"]={{path="chatsounds/autoadd/ahamudo/fate buffremoval.ogg",length=1.248}}
+L["fate death"]={{path="chatsounds/autoadd/ahamudo/fate death.ogg",length=1.521}}
+L["fate debuffmajor"]={{path="chatsounds/autoadd/ahamudo/fate debuffmajor.ogg",length=0.650}}
+L["fate debuffminor"]={{path="chatsounds/autoadd/ahamudo/fate debuffminor.ogg",length=0.610}}
+L["fate foundationrestored"]={{path="chatsounds/autoadd/ahamudo/fate foundationrestored.ogg",length=5.912}}
+L["fate healaverage"]={{path="chatsounds/autoadd/ahamudo/fate healaverage.ogg",length=1.096}}
+L["fate healmajor"]={{path="chatsounds/autoadd/ahamudo/fate healmajor.ogg",length=1.468}}
+L["fate healminor"]={{path="chatsounds/autoadd/ahamudo/fate healminor.ogg",length=1.135}}
+L["fate npgain up"]={{path="chatsounds/autoadd/ahamudo/fate npgain up.ogg",length=1.213}}
+L["fate revive"]={{path="chatsounds/autoadd/ahamudo/fate revive.ogg",length=1.591}}
 L["hah heha hah"]={{path="chatsounds/autoadd/ahamudo/hah heha hah.ogg",length=1.161}}
+L["hamood habibi"]={{path="chatsounds/autoadd/ahamudo/hamood habibi.ogg",length=16.816}}
 L["huhaa"]={{path="chatsounds/autoadd/ahamudo/huhaa.ogg",length=0.665}}
 L["i know all about dick and balls"]={{path="chatsounds/autoadd/ahamudo/i know all about dick and balls.ogg",length=3.367}}
+L["ikuzeeee"]={{path="chatsounds/autoadd/ahamudo/ikuzeeee.ogg",length=1.683}}
+L["jingulaugh"]={{path="chatsounds/autoadd/ahamudo/jingulaugh/1.ogg",length=2.374},{path="chatsounds/autoadd/ahamudo/jingulaugh/2.ogg",length=2.110}}
+L["kuso"]={{path="chatsounds/autoadd/ahamudo/kuso.ogg",length=1.146}}
+L["majimalaugh"]={{path="chatsounds/autoadd/ahamudo/majimalaugh.ogg",length=1.493}}
 L["n n nipplefuck"]={{path="chatsounds/autoadd/ahamudo/n n nipplefuck.ogg",length=1.718}}
 L["nigga no i cant show this on camera"]={{path="chatsounds/autoadd/ahamudo/nigga no i cant show this on camera.ogg",length=5.306}}
 L["nigga what am i doin"]={{path="chatsounds/autoadd/ahamudo/nigga what am i doin.ogg",length=1.393}}
 L["ohh hohohoo oh thats terrible"]={{path="chatsounds/autoadd/ahamudo/ohh hohohoo oh thats terrible.ogg",length=3.442}}
 L["pull out your titties"]={{path="chatsounds/autoadd/ahamudo/pull out your titties.ogg",length=0.804}}
+L["rrrrets get to da top"]={{path="chatsounds/autoadd/ahamudo/rrrrets get to da top.ogg",length=8.428}}
 L["sore wa osesugiru haha desu"]={{path="chatsounds/autoadd/ahamudo/sore wa osesugiru haha desu.ogg",length=2.756}}
 L["squaaw"]={{path="chatsounds/autoadd/ahamudo/squaaw.ogg",length=1.646}}
+L["tch"]={{path="chatsounds/autoadd/ahamudo/tch.ogg",length=0.436}}
 L["use your titties"]={{path="chatsounds/autoadd/ahamudo/use your titties.ogg",length=0.722}}
 L["wait wtf no noo nooo"]={{path="chatsounds/autoadd/ahamudo/wait wtf no noo nooo.ogg",length=3.297}}
 L["watashi wa sube te mi takimashta"]={{path="chatsounds/autoadd/ahamudo/watashi wa sube te mi takimashta.ogg",length=3.164}}
+L["yooooh"]={{path="chatsounds/autoadd/ahamudo/yooooh.ogg",length=1.387}}
+L["yorokobe shounen"]={{path="chatsounds/autoadd/ahamudo/yorokobe shounen/1.ogg",length=1.663},{path="chatsounds/autoadd/ahamudo/yorokobe shounen/2.ogg",length=5.307}}
 L["you is right"]={{path="chatsounds/autoadd/ahamudo/you is right.ogg",length=1.718}}
 c.EndList()
