3cfc72ebd28bfc07a065fe74db8fb91055e83b7d
etothepowerof26 <jdhale2004@gmail.com>
2019-07-04 10:35:11 -0400

    more rising revengeance chatsounds

--- lua/chatsounds/lists_nosend/mgrr_ytp.lua
+++ lua/chatsounds/lists_nosend/mgrr_ytp.lua
@@ -1,15 +1,24 @@
 c.StartList("mgrr_ytp")
 L["ahh too much candy loud vomit"]={{path="chatsounds/autoadd/mgrr_ytp/ahh too much candy loud vomit.ogg",length=3.793}}
 L["blow me no"]={{path="chatsounds/autoadd/mgrr_ytp/blow me no.ogg",length=1.692}}
+L["could have gone pro if i hadnt joined the university of texas"]={{path="chatsounds/autoadd/mgrr_ytp/could have gone pro if i hadnt joined the university of texas.ogg",length=3.000}}
+L["end of stoots"]={{path="chatsounds/autoadd/mgrr_ytp/end of stoots.ogg",length=1.350}}
 L["fak u distorted"]={{path="chatsounds/autoadd/mgrr_ytp/fak u distorted.ogg",length=1.700}}
+L["feasting on the insides of your enimeem enemies"]={{path="chatsounds/autoadd/mgrr_ytp/feasting on the insides of your enimeem enemies.ogg",length=4.200}}
 L["forget it weve both heard enough speeches about bigger cock by now"]={{path="chatsounds/autoadd/mgrr_ytp/forget it weve both heard enough speeches about bigger cock by now.ogg",length=4.089}}
 L["fuuf"]={{path="chatsounds/autoadd/mgrr_ytp/fuuf.ogg",length=0.578}}
+L["get saas"]={{path="chatsounds/autoadd/mgrr_ytp/get saas.ogg",length=0.746}}
 L["hell be lucky if he can carry his box of candy out of the fucking building"]={{path="chatsounds/autoadd/mgrr_ytp/hell be lucky if he can carry his box of candy out of the fucking building.ogg",length=4.651}}
 L["hm cherry soos i take it youre a fan"]={{path="chatsounds/autoadd/mgrr_ytp/hm cherry soos i take it youre a fan.ogg",length=2.796}}
+L["i could fuck the president with my bare cock"]={{path="chatsounds/autoadd/mgrr_ytp/i could fuck the president with my bare cock.ogg",length=2.800}}
+L["i fuck coc you know"]={{path="chatsounds/autoadd/mgrr_ytp/i fuck coc you know.ogg",length=1.350}}
+L["i was born to eat coc h hhh h"]={{path="chatsounds/autoadd/mgrr_ytp/i was born to eat coc h hhh h.ogg",length=4.000}}
 L["i wont be needing my little cock any longer"]={{path="chatsounds/autoadd/mgrr_ytp/i wont be needing my little cock any longer.ogg",length=2.129}}
 L["im here for my kan deez"]={{path="chatsounds/autoadd/mgrr_ytp/im here for my kan deez.ogg",length=1.889}}
+L["im not talking about money jack im talking about money"]={{path="chatsounds/autoadd/mgrr_ytp/im not talking about money jack im talking about money.ogg",length=4.750}}
 L["its ogre"]={{path="chatsounds/autoadd/mgrr_ytp/its ogre.ogg",length=1.438}}
 L["lets go to sees candies"]={{path="chatsounds/autoadd/mgrr_ytp/lets go to sees candies.ogg",length=2.566}}
+L["nanomachines suus"]={{path="chatsounds/autoadd/mgrr_ytp/nanomachines suus.ogg",length=1.700}}
 L["now i sees you deny your cock its purpose"]={{path="chatsounds/autoadd/mgrr_ytp/now i sees you deny your cock its purpose.ogg",length=4.780}}
 L["oh fuck"]={{path="chatsounds/autoadd/mgrr_ytp/oh fuck.ogg",length=0.874}}
 L["oh im here for my ses"]={{path="chatsounds/autoadd/mgrr_ytp/oh im here for my ses.ogg",length=2.555}}
@@ -28,6 +37,9 @@ L["stoots"]={{path="chatsounds/autoadd/mgrr_ytp/stoots.ogg",length=0.465}}
 L["suuus"]={{path="chatsounds/autoadd/mgrr_ytp/suuus.ogg",length=1.086}}
 L["this is what happens when you bring a tool to a toolfight"]={{path="chatsounds/autoadd/mgrr_ytp/this is what happens when you bring a tool to a toolfight.ogg",length=4.061}}
 L["two bogeys two bogeys two bogeys two distorted"]={{path="chatsounds/autoadd/mgrr_ytp/two bogeys two bogeys two bogeys two distorted.ogg",length=2.538}}
+L["what are you saas"]={{path="chatsounds/autoadd/mgrr_ytp/what are you saas.ogg",length=1.340}}
+L["what are you saying im saying garble youve lost your mind"]={{path="chatsounds/autoadd/mgrr_ytp/what are you saying im saying garble youve lost your mind.ogg",length=8.000}}
 L["why dont we ever hear speeches about a common cock"]={{path="chatsounds/autoadd/mgrr_ytp/why dont we ever hear speeches about a common cock.ogg",length=2.989}}
 L["you aint lisssstenin"]={{path="chatsounds/autoadd/mgrr_ytp/you aint lisssstenin.ogg",length=2.622}}
+L["youre the sos soos sooos soooos"]={{path="chatsounds/autoadd/mgrr_ytp/youre the sos soos sooos soooos.ogg",length=2.700}}
 c.EndList()
