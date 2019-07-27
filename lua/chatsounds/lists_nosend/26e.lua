8f7fea4e49c717feff1aa770a51036c579e7a549
etothepowerof26 <jdhale2004@gmail.com>
2019-07-26 22:04:51 -0400

    bat strike

--- lua/chatsounds/lists_nosend/26e.lua
+++ lua/chatsounds/lists_nosend/26e.lua
@@ -1,6 +1,7 @@
 c.StartList("26e")
 L["ahh stop i couldve dropped my croissant"]={{path="chatsounds/autoadd/26e/ahh stop i couldve dropped my croissant.ogg",length=3.237}}
 L["amenbreak full"]={{path="chatsounds/autoadd/26e/amenbreak full.ogg",length=6.807}}
+L["bat strike"]={{path="chatsounds/autoadd/26e/bat strike.ogg",length=1.668}}
 L["nestetrismusic"]={{path="chatsounds/autoadd/26e/nestetrismusic/1.ogg",length=54.817},{path="chatsounds/autoadd/26e/nestetrismusic/2.ogg",length=35.410},{path="chatsounds/autoadd/26e/nestetrismusic/3.ogg",length=24.231}}
 L["she say do you love me i tell her dddddddd"]={{path="chatsounds/autoadd/26e/she say do you love me i tell her dddddddd.ogg",length=8.475}}
 L["windowsxpsong"]={{path="chatsounds/autoadd/26e/windowsxpsong/1.ogg",length=43.378},{path="chatsounds/autoadd/26e/windowsxpsong/2.ogg",length=4.457},{path="chatsounds/autoadd/26e/windowsxpsong/3.ogg",length=38.378}}
