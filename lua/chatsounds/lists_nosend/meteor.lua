211d5e3188a5475299425a6ff66642daa14ea905
Yerchance <54071335+Yerchance@users.noreply.github.com>
2019-08-21 19:05:52 +0200

    Add files via upload

--- lua/chatsounds/lists_nosend/meteor.lua
+++ lua/chatsounds/lists_nosend/meteor.lua
@@ -1,39 +1,39 @@
 c.StartList("meteor")
-L["360 ooga booga booga"]={{path="chatsounds/autoadd/meteor/360oogaboogabooga.ogg",length=3.350}}
-L["alter was alter"]={{path="chatsounds/autoadd/meteor/alterwasalter.ogg",length=1.324}}
-L["violent burp"]={{path="chatsounds/autoadd/meteor/burp.ogg",length=1.224}}
-L["how about no you crazy dutch bastard on crack"]={{path="chatsounds/autoadd/meteor/crazydutchbastardoncrack.ogg",length=3.664}}
-L["deine mudda"]={{path="chatsounds/autoadd/meteor/deinemudda.ogg",length=1.265}}
-L["die dumme sau hat facebook"]={{path="chatsounds/autoadd/meteor/diedummesauhatfacebook.ogg",length=1.916}}
-L["es kann der schoenste nicht kuhl sein wenn es den scheiss drucker nicht gefaellt"]={{path="chatsounds/autoadd/meteor/eskannderschoenstenichtkuhlsein.ogg",length=3.940}}
+L["360 ooga booga booga"]={{path="chatsounds/autoadd/meteor/360 ooga booga booga.ogg",length=3.350}}
+L["alter was alter"]={{path="chatsounds/autoadd/meteor/alter was alter.ogg",length=1.324}}
+L["violent burp"]={{path="chatsounds/autoadd/meteor/violent burp.ogg",length=1.224}}
+L["how about no you crazy dutch bastard on crack"]={{path="chatsounds/autoadd/meteor/how about no you crazy dutch bastard on crack.ogg",length=3.664}}
+L["deine mudda"]={{path="chatsounds/autoadd/meteor/deine mudda.ogg",length=1.265}}
+L["die dumme sau hat facebook"]={{path="chatsounds/autoadd/meteor/die dumme sau hat facebook.ogg",length=1.916}}
+L["es kann der schoenste nicht kuhl sein wenn es den scheiss drucker nicht gefaellt"]={{path="chatsounds/autoadd/meteor/es kann der schoenste nicht kuhl sein wenn es den scheiss drucker nicht gefaellt.ogg",length=3.940}}
 L["flex"]={{path="chatsounds/autoadd/meteor/flex.ogg",length=1.126}}
-L["fuck off"]={{path="chatsounds/autoadd/meteor/fuckoff.ogg",length=0.865}}
-L["herb laugh spl"]={{path="chatsounds/autoadd/meteor/herblaughspl.ogg",length=20.950}}
-L["hey yall got a cigarette"]={{path="chatsounds/autoadd/meteor/heyyallgotacigarrete.ogg",length=1.626}}
-L["hey yall got a cigarette um we dont smoke fuck off"]={{path="chatsounds/autoadd/meteor/heyyallgotacigarreteumwedontsmokefuckoff.ogg",length=4.284}}
-L["hiv ass fuck"]={{path="chatsounds/autoadd/meteor/hivassfuck.ogg",length=1.567}}
-L["hiv cum acquired"]={{path="chatsounds/autoadd/meteor/hivcumacquired.ogg",length=1.228}}
-L["hiv painis cok cock spastic sounds outaten sisteen"]={{path="chatsounds/autoadd/meteor/hivpainiscokcockspazzingoutatensisteen.ogg",length=7.184}}
+L["fuck off"]={{path="chatsounds/autoadd/meteor/fuck off.ogg",length=0.865}}
+L["herb laugh spl"]={{path="chatsounds/autoadd/meteor/herb laugh spl.ogg",length=20.950}}
+L["hey yall got a cigarette"]={{path="chatsounds/autoadd/meteor/hey yall got a cigarette.ogg",length=1.626}}
+L["hey yall got a cigarette um we dont smoke fuck off"]={{path="chatsounds/autoadd/meteor/hey yall got a cigarette um we dont smoke fuck off.ogg",length=4.284}}
+L["hiv ass fuck"]={{path="chatsounds/autoadd/meteor/hiv ass fuck.ogg",length=1.567}}
+L["hiv cum acquired"]={{path="chatsounds/autoadd/meteor/hiv cum acquired.ogg",length=1.228}}
+L["hiv painis cok cock spastic sounds outaten sisteen"]={{path="chatsounds/autoadd/meteor/hiv painis cok cock spastic sounds outaten sisteen.ogg",length=7.184}}
 L["kobe"]={{path="chatsounds/autoadd/meteor/kobe.ogg",length=0.454}}
 L["loki1"]={{path="chatsounds/autoadd/meteor/loki1.ogg",length=3.291}}
 L["loki2"]={{path="chatsounds/autoadd/meteor/loki2.ogg",length=4.389}}
-L["behind you"]={{path="chatsounds/autoadd/meteor/lokibehindyou.ogg",length=1.646}}
-L["mario jump"]={{path="chatsounds/autoadd/meteor/mariojump.ogg",length=0.543}}
-L["ey voll krass korrekt was willst du hier junge alle aufmucken hol ich brudah komm ich zu dir hau den knast kaputt junge"]={{path="chatsounds/autoadd/meteor/muratknastkaputt.ogg",length=6.322}}
+L["behind you"]={{path="chatsounds/autoadd/meteor/behind you.ogg",length=1.646}}
+L["mario jump"]={{path="chatsounds/autoadd/meteor/mario jump.ogg",length=0.543}}
+L["ey voll krass korrekt was willst du hier junge alle aufmucken hol ich brudah komm ich zu dir hau den knast kaputt junge"]={{path="chatsounds/autoadd/meteor/ey voll krass korrekt was willst du hier junge alle aufmucken hol ich brudah komm ich zu dir hau den knast kaputt junge.ogg",length=6.322}}
 L["ninja"]={{path="chatsounds/autoadd/meteor/ninja.ogg",length=0.653}}
-L["ninja flex"]={{path="chatsounds/autoadd/meteor/ninjaflex.ogg",length=1.829}}
-L["oh hell no"]={{path="chatsounds/autoadd/meteor/ohhellno.ogg",length=2.496}}
-L["may i have your attention please may i have your attention please"]={{path="chatsounds/autoadd/meteor/slimshady1.ogg",length=6.604}}
-L["will the real slim shady please stand up i repeat will the real slim shady please stand up were gonna have a problem here ag you all act like youve never seen a white person before"]={{path="chatsounds/autoadd/meteor/slimshady2.ogg",length=20.788}}
-L["because yes im the real slim shady im slim shady"]={{path="chatsounds/autoadd/meteor/slimshady3.ogg",length=6.163}}
-L["so wont the real slim shady please stand up please stand up"]={{path="chatsounds/autoadd/meteor/slimshady4.ogg",length=7.208}}
-L["the fuck are you looking at nigger"]={{path="chatsounds/autoadd/meteor/thefuckareyoulookingatnigger.ogg",length=2.220}}
-L["volvo anti cock"]={{path="chatsounds/autoadd/meteor/volvoanticock.ogg",length=1.424}}
-L["the fuck are you looking at nigger keep scrolling"]={{path="chatsounds/autoadd/meteor/whatareyoullokingatniggerkeepscrolling.ogg",length=3.213}}
-L["im going to punch your face in the face"]={{path="chatsounds/autoadd/meteor/punchyourfaceintheface.ogg",length=7.728}}
-L["radical laugh"]={{path="chatsounds/autoadd/meteor/extremelaugh.ogg",length=2.109}}
-L["ouu edgy edgy boy alert"]={{path="chatsounds/autoadd/meteor/edgyboyalert.ogg",length=8.758}}
-L["violent laugh"]={{path="chatsounds/autoadd/meteor/violentlaugh.ogg",length=4.413}}
-L["you fucking mongoloid"]={{path="chatsounds/autoadd/meteor/youfuckingmongoloid.ogg",length=1.463}}
+L["ninja flex"]={{path="chatsounds/autoadd/meteor/ninja flex.ogg",length=1.829}}
+L["oh hell no"]={{path="chatsounds/autoadd/meteor/oh hell no.ogg",length=2.496}}
+L["may i have your attention please may i have your attention please"]={{path="chatsounds/autoadd/meteor/may i have your attention please may i have your attention please.ogg",length=6.604}}
+L["will the real slim shady please stand up i repeat will the real slim shady please stand up were gonna have a problem here ag you all act like youve never seen a white person before"]={{path="chatsounds/autoadd/meteor/will the real slim shady please stand up i repeat will the real slim shady please stand up were gonna have a problem here.ogg",length=20.788}}
+L["because yes im the real slim shady im slim shady"]={{path="chatsounds/autoadd/meteor/because yes im the real slim shady im slim shady.ogg",length=6.163}}
+L["so wont the real slim shady please stand up please stand up"]={{path="chatsounds/autoadd/meteor/so wont the real slim shady please stand up please stand up.ogg",length=7.208}}
+L["the fuck are you looking at nigger"]={{path="chatsounds/autoadd/meteor/the fuck are you looking at nigger.ogg",length=2.220}}
+L["volvo anti cock"]={{path="chatsounds/autoadd/meteor/volvo anti cock.ogg",length=1.424}}
+L["the fuck are you looking at nigger keep scrolling"]={{path="chatsounds/autoadd/meteor/the fuck are you looking at nigger keep scrolling.ogg",length=3.213}}
+L["im going to punch your face in the face"]={{path="chatsounds/autoadd/meteor/im going to punch your face in the face.ogg",length=7.728}}
+L["radical laugh"]={{path="chatsounds/autoadd/meteor/radical laugh.ogg",length=2.109}}
+L["ouu edgy edgy boy alert"]={{path="chatsounds/autoadd/meteor/ouu edgy edgy boy alert.ogg",length=8.758}}
+L["violent laugh"]={{path="chatsounds/autoadd/meteor/violent laugh.ogg",length=4.413}}
+L["you fucking mongoloid"]={{path="chatsounds/autoadd/meteor/you fucking mongoloid.ogg",length=1.463}}
 L["mongoloid"]={{path="chatsounds/autoadd/meteor/mongoloid.ogg",length=0.717}
 c.EndList()
