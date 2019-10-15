7e7c789ddbf70d316fd66ed8d035ae02c2a10eda
Goodlyay <zackxv7@yahoo.com>
2019-10-15 02:01:19 -0700

    Chatsounds batch 1

---
+++ lua/chatsounds/lists_nosend/steve_moustache_show.lua
@@ -0,0 +1,4 @@
+c.StartList("steve_moustache_show")
+L["what the fuck did you do to me holy fucking shit"]={{path="chatsounds/autoadd/steve_moustache_show/what the fuck did you do to me holy fucking shit.ogg",length=3.739}}
+L["you sons of bitches butchered me"]={{path="chatsounds/autoadd/steve_moustache_show/you sons of bitches butchered me.ogg",length=2.629}}
+c.EndList()
