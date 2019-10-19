a3c25fd0ac140b50504508c0cf086234785144bb
Meta Automator <MetaAutomator@users.noreply.github.com>
2019-10-19 03:18:31 +0000

    chatsounds preprocessor (65cd320c9649ac1b7e1d1de2fdc7147732ea52f5)
    [ci skip]

---
+++ lua/chatsounds/lists_nosend/idubbbz_gr_scr.lua
@@ -0,0 +1,36 @@
+c.StartList("idubbbz_gr_scr")
+L["alright fellas put your hands behind your back where i can sees em"]={{path="chatsounds/autoadd/idubbbz_gr_scr/alright fellas put your hands behind your back where i can sees em.ogg",length=5.509}}
+L["are you serious this is torture"]={{path="chatsounds/autoadd/idubbbz_gr_scr/are you serious this is torture.ogg",length=3.641}}
+L["borrriiing"]={{path="chatsounds/autoadd/idubbbz_gr_scr/borrriiing.ogg",length=2.056}}
+L["damn girl you grew up"]={{path="chatsounds/autoadd/idubbbz_gr_scr/damn girl you grew up.ogg",length=4.843}}
+L["fine you leave me no choice prepare to be epically owned noob"]={{path="chatsounds/autoadd/idubbbz_gr_scr/fine you leave me no choice prepare to be epically owned noob.ogg",length=6.503}}
+L["get on the ground get on the ground right now"]={{path="chatsounds/autoadd/idubbbz_gr_scr/get on the ground get on the ground right now.ogg",length=3.722}}
+L["i dont know it says its a some sort of doesnt identify or identify as a man im not sure"]={{path="chatsounds/autoadd/idubbbz_gr_scr/i dont know it says its a some sort of doesnt identify or identify as a man im not sure.ogg",length=6.814}}
+L["i dont know what this is but its kinda hot"]={{path="chatsounds/autoadd/idubbbz_gr_scr/i dont know what this is but its kinda hot.ogg",length=4.410}}
+L["i like this this is cute i want this inside of me"]={{path="chatsounds/autoadd/idubbbz_gr_scr/i like this this is cute i want this inside of me.ogg",length=4.892}}
+L["idubbz cringe"]={{path="chatsounds/autoadd/idubbbz_gr_scr/idubbz cringe.ogg",length=9.925}}
+L["if you dont get on the ground right now im gonna make a video about you"]={{path="chatsounds/autoadd/idubbbz_gr_scr/if you dont get on the ground right now im gonna make a video about you.ogg",length=3.775}}
+L["its i dont know its acting sporadic and i dont even know what it is"]={{path="chatsounds/autoadd/idubbbz_gr_scr/its i dont know its acting sporadic and i dont even know what it is.ogg",length=6.072}}
+L["just admit that youre a whore and a slut and a harlot"]={{path="chatsounds/autoadd/idubbbz_gr_scr/just admit that youre a whore and a slut and a harlot.ogg",length=3.937}}
+L["mission control we have a belligerent dumbass here that wont cooperate"]={{path="chatsounds/autoadd/idubbbz_gr_scr/mission control we have a belligerent dumbass here that wont cooperate.ogg",length=4.193}}
+L["oh yeah thats good kinda funny aftertaste though"]={{path="chatsounds/autoadd/idubbbz_gr_scr/oh yeah thats good kinda funny aftertaste though.ogg",length=9.536}}
+L["oooh thats kinda small yikes"]={{path="chatsounds/autoadd/idubbbz_gr_scr/oooh thats kinda small yikes.ogg",length=9.170}}
+L["put a sock in it no one cares"]={{path="chatsounds/autoadd/idubbbz_gr_scr/put a sock in it no one cares.ogg",length=3.467}}
+L["sir maam"]={{path="chatsounds/autoadd/idubbbz_gr_scr/sir maam.ogg",length=2.416}}
+L["sorry i think im just too stupid to understand this"]={{path="chatsounds/autoadd/idubbbz_gr_scr/sorry i think im just too stupid to understand this.ogg",length=3.203}}
+L["suspect is fleeing on foot suspect is acting extremely erratic"]={{path="chatsounds/autoadd/idubbbz_gr_scr/suspect is fleeing on foot suspect is acting extremely erratic.ogg",length=10.269}}
+L["thats quite big impressive"]={{path="chatsounds/autoadd/idubbbz_gr_scr/thats quite big impressive.ogg",length=7.419}}
+L["this feels a lot like hell"]={{path="chatsounds/autoadd/idubbbz_gr_scr/this feels a lot like hell.ogg",length=2.181}}
+L["this is a damn ugly world we live in"]={{path="chatsounds/autoadd/idubbbz_gr_scr/this is a damn ugly world we live in.ogg",length=2.265}}
+L["this is fucking annoying"]={{path="chatsounds/autoadd/idubbbz_gr_scr/this is fucking annoying.ogg",length=2.271}}
+L["this is one of those moments where i thank my lucky stars that im white"]={{path="chatsounds/autoadd/idubbbz_gr_scr/this is one of those moments where i thank my lucky stars that im white.ogg",length=4.303}}
+L["this is really boring you really need to up the energy level im getting sick of this"]={{path="chatsounds/autoadd/idubbbz_gr_scr/this is really boring you really need to up the energy level im getting sick of this.ogg",length=5.458}}
+L["ugh got a fucking itchy asshole"]={{path="chatsounds/autoadd/idubbbz_gr_scr/ugh got a fucking itchy asshole.ogg",length=3.032}}
+L["what the fuck was that taser agh"]={{path="chatsounds/autoadd/idubbbz_gr_scr/what the fuck was that taser agh.ogg",length=3.882}}
+L["whoa thats a genuinely shocking fact"]={{path="chatsounds/autoadd/idubbbz_gr_scr/whoa thats a genuinely shocking fact.ogg",length=3.087}}
+L["yeah uh big hoss looks like we got a 69 in progress im gonna need you to send me in some good ass backup"]={{path="chatsounds/autoadd/idubbbz_gr_scr/yeah uh big hoss looks like we got a 69 in progress im gonna need you to send me in some good ass backup.ogg",length=9.275}}
+L["yeah yeah bring in the good ass backup"]={{path="chatsounds/autoadd/idubbbz_gr_scr/yeah yeah bring in the good ass backup.ogg",length=4.100}}
+L["you a stupid ho you a you a stupid ho"]={{path="chatsounds/autoadd/idubbbz_gr_scr/you a stupid ho you a you a stupid ho.ogg",length=7.249}}
+L["youre torturing me and everyone else watching"]={{path="chatsounds/autoadd/idubbbz_gr_scr/youre torturing me and everyone else watching.ogg",length=3.952}}
+L["youre under arrest nimrod"]={{path="chatsounds/autoadd/idubbbz_gr_scr/youre under arrest nimrod.ogg",length=4.140}}
+c.EndList()
