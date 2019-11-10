54f5a19dbfa921b64dbb6a2475018a187030250e
!Avast AntiPony 9445 <kristianzxz41@gmail.com>
2019-11-10 06:31:37 +0100

    typo

--- lua/chatsounds/lists_nosend/doom jaguar.lua
+++ lua/chatsounds/lists_nosend/doom jaguar.lua
@@ -4,7 +4,7 @@ L["and i say to you my brothers and sisters"]={{path="chatsounds/autoadd/doom ja
 L["and i say to you my brothers and sisters hell is a deep dark foul smelling prison of the damned filled with fire and brimstone lost souls and demons and how do you escape this eternal damnation you blast the nasty pink demon right between their beady little eyes"]={{path="chatsounds/autoadd/doom jaguar/doom jaguar commercial.ogg",length=23.599}}
 L["filled with fire and brimstone"]={{path="chatsounds/autoadd/doom jaguar/filled with fire and brimstone.ogg",length=2.686}}
 L["filled with fire and brimstone lost souls and demons"]={{path="chatsounds/autoadd/doom jaguar/filled with fire and brimstone lost souls and demons.ogg",length=4.895}}
-L["hell is a deep dark foul smelling place of the damned"]={{path="chatsounds/autoadd/doom jaguar/hell is a deep dark foul smelling place of the damned.ogg",length=4.247}}
+L["hell is a deep dark foul smelling prison of the damned"]={{path="chatsounds/autoadd/doom jaguar/hell is a deep dark foul smelling prison of the damned.ogg",length=4.247}}
 L["lost souls and demons"]={{path="chatsounds/autoadd/doom jaguar/lost souls and demons.ogg",length=2.218}}
 L["nasty pink demon"]={{path="chatsounds/autoadd/doom jaguar/nasty pink demon.ogg",length=1.298}}
 L["right between thier beady little eyes"]={{path="chatsounds/autoadd/doom jaguar/right between thier beady little eyes.ogg",length=3.559}}
