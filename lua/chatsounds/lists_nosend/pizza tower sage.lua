0c8286fa80f3593c68ab31d9b5b5ae25964a2e71
!Avast AntiPony 9445 <kristianzxz41@gmail.com>
2019-11-10 06:17:03 +0100

    Nasty Pink Demon
    And I say to you my brothers and sisters, Hell is a deep, dark, foul-smelling prison of the damned. Filled with fire and brimstone, lost souls and demons. And how do you escape this eternal damnation? You blast the nasty pink demon right between their beady little eyes.

---
+++ lua/chatsounds/lists_nosend/pizza tower sage.lua
@@ -0,0 +1,13 @@
+c.StartList("pizza tower sage")
+L["collect giant pizza"]={{path="chatsounds/autoadd/pizza tower sage/collect giant pizza.ogg",length=2.092}}
+L["collect pizza"]={{path="chatsounds/autoadd/pizza tower sage/collect pizza.ogg",length=2.400}}
+L["collect toppin"]={{path="chatsounds/autoadd/pizza tower sage/collect toppin.ogg",length=2.400}}
+L["dungeon depth"]={{path="chatsounds/autoadd/pizza tower sage/dungeon depth.ogg",length=51.480}}
+L["noise woah"]={{path="chatsounds/autoadd/pizza tower sage/woah.ogg",length=0.607}}
+L["pizza tower rank a"]={{path="chatsounds/autoadd/pizza tower sage/pizza tower rank a.ogg",length=15.576}}
+L["pizza tower rank b"]={{path="chatsounds/autoadd/pizza tower sage/pizza tower rank b.ogg",length=15.637}}
+L["pizza tower rank c"]={{path="chatsounds/autoadd/pizza tower sage/pizza tower rank b.ogg",length=15.637}}
+L["pizza tower rank d"]={{path="chatsounds/autoadd/pizza tower sage/pizza tower rank d.ogg",length=14.837}}
+L["pizza tower rank s"]={{path="chatsounds/autoadd/pizza tower sage/pizza tower rank s.ogg",length=14.652}}
+L["woah"]={{path="chatsounds/autoadd/pizza tower sage/woah.ogg",length=0.607}}
+c.EndList()
