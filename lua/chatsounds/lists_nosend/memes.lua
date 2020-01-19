42559fdc88fbc49859f152cd8f37582e609c3eab
mitterdoo <connorashcroft@live.com>
2020-01-19 14:27:48 -0800

    do you think god stays in heaven because He too lives in fear of what he's created

--- lua/chatsounds/lists_nosend/memes.lua
+++ lua/chatsounds/lists_nosend/memes.lua
@@ -1,5 +1,6 @@
 c.StartList("memes")
 L["360 ooga booga booga"]={{path="chatsounds/autoadd/memes/360 ooga booga booga.ogg",length=3.350}}
+L["do you think god stays in heaven because he too lives in fear of what hes created"]={{path="chatsounds/autoadd/memes/do you think god stays in heaven because he too lives in fear of what hes created.ogg",length=6.430}}
 L["im going to punch your face in the face"]={{path="chatsounds/autoadd/memes/im going to punch your face in the face.ogg",length=7.728}}
 L["kobe"]={{path="chatsounds/autoadd/memes/kobe.ogg",length=0.454}}
 L["le monke"]={{path="chatsounds/autoadd/memes/le monke.ogg",length=59.507}}
