bca4c4f40ed65c607561658c6644d23aea55f59a
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-02-01 21:13:28 +0000

    chatsounds preprocessor (270b752e1dccf4aab270c9dc7140c09499e4cb29)
    [ci skip]

--- lua/chatsounds/lists_nosend/memes.lua
+++ lua/chatsounds/lists_nosend/memes.lua
@@ -1,8 +1,11 @@
 c.StartList("memes")
 L["360 ooga booga booga"]={{path="chatsounds/autoadd/memes/360 ooga booga booga.ogg",length=3.350}}
+L["cringe"]={{path="chatsounds/autoadd/memes/cringe.ogg",length=0.986}}
+L["cringeriff"]={{path="chatsounds/autoadd/memes/cringeriff.ogg",length=5.548}}
 L["do you think god stays in heaven because he too lives in fear of what hes created"]={{path="chatsounds/autoadd/memes/do you think god stays in heaven because he too lives in fear of what hes created.ogg",length=6.430}}
 L["im going to punch your face in the face"]={{path="chatsounds/autoadd/memes/im going to punch your face in the face.ogg",length=7.728}}
 L["kobe"]={{path="chatsounds/autoadd/memes/kobe.ogg",length=0.454}}
 L["le monke"]={{path="chatsounds/autoadd/memes/le monke.ogg",length=59.507}}
+L["mixed race couple"]={{path="chatsounds/autoadd/memes/mixed race couple.ogg",length=1.710}}
 L["oh hell no"]={{path="chatsounds/autoadd/memes/oh hell no.ogg",length=2.188}}
 c.EndList()
