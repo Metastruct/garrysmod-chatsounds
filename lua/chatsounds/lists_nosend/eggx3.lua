febb3c52428f2c7f61d2be457b03023a5e24b541
Niccep <43934118+0x098@users.noreply.github.com>
2020-02-09 20:11:50 +0200

    eggx3
    epic

---
+++ lua/chatsounds/lists_nosend/eggx3.lua
@@ -0,0 +1,22 @@
+c.StartList("eggx3")
+L["aah"]={{path="chatsounds/autoadd/eggx3/aah.ogg",length=0.530}}
+L["am i the only one who when i see a small kid just wants to whomp it in the head"]={{path="chatsounds/autoadd/eggx3/am i the only one who when i see a small kid just wants to whomp it in the head.ogg",length=4.926}}
+L["are you underage"]={{path="chatsounds/autoadd/eggx3/are you underage.ogg",length=1.437}}
+L["bruh"]={{path="chatsounds/autoadd/eggx3/bruh.ogg",length=0.515}}
+L["but do you realise how brave it is to shit in the school bathrooms"]={{path="chatsounds/autoadd/eggx3/but do you realise how brave it is to shit in the school bathrooms.ogg",length=3.716}}
+L["can you renegade"]={{path="chatsounds/autoadd/eggx3/can you renegade.ogg",length=1.078}}
+L["huh"]={{path="chatsounds/autoadd/eggx3/huh.ogg",length=1.030}}
+L["i am not happy"]={{path="chatsounds/autoadd/eggx3/i am not happy.ogg",length=1.193}}
+L["i didnt realize they were an actual agency"]={{path="chatsounds/autoadd/eggx3/i didnt realize they were an actual agency.ogg",length=2.166}}
+L["just want to slap it"]={{path="chatsounds/autoadd/eggx3/just want to slap it.ogg",length=0.799}}
+L["knock it over"]={{path="chatsounds/autoadd/eggx3/knock it over.ogg",length=0.879}}
+L["now this ones kinda mean"]={{path="chatsounds/autoadd/eggx3/now this ones kinda mean.ogg",length=1.519}}
+L["please ignore the fact that im dressed like a cheesestring"]={{path="chatsounds/autoadd/eggx3/please ignore the fact that im dressed like a cheesestring.ogg",length=3.065}}
+L["raid shadow legends"]={{path="chatsounds/autoadd/eggx3/raid shadow legends.ogg",length=0.980}}
+L["shut up"]={{path="chatsounds/autoadd/eggx3/shut up.ogg",length=0.729}}
+L["so i was uh takin a piss"]={{path="chatsounds/autoadd/eggx3/so i was uh takin a piss.ogg",length=1.636}}
+L["so whats your opinions on it"]={{path="chatsounds/autoadd/eggx3/so whats your opinions on it.ogg",length=1.768}}
+L["this is kind of a shit joke"]={{path="chatsounds/autoadd/eggx3/this is kind of a shit joke.ogg",length=1.366}}
+L["you guys will not believe what just happened"]={{path="chatsounds/autoadd/eggx3/you guys will not believe what just happened.ogg",length=2.872}}
+L["yuh"]={{path="chatsounds/autoadd/eggx3/yuh.ogg",length=0.292}}
+c.EndList()
