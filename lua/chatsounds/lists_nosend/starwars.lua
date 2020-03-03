69d9b83afd4cfba6a0e0a03603a69fb5673fa94f
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-03-03 15:10:24 +0000

    chatsounds preprocessor (3f68d2897e3a7c0dbb87738cf3f4c1ea18c638e3)
    [ci skip]

--- lua/chatsounds/lists_nosend/starwars.lua
+++ lua/chatsounds/lists_nosend/starwars.lua
@@ -1,5 +1,7 @@
 c.StartList("starwars")
 L["cantina"]={{path="chatsounds/autoadd/starwars/cantina.ogg",length=43.677}}
+L["general kenobi"]={{path="chatsounds/autoadd/starwars/general kenobi.ogg",length=1.567}}
+L["hello there"]={{path="chatsounds/autoadd/starwars/hello there.ogg",length=0.765}}
 L["telein"]={{path="chatsounds/autoadd/starwars/telein.ogg",length=2.926}}
 L["teleout"]={{path="chatsounds/autoadd/starwars/teleout.ogg",length=3.866}}
 c.EndList()
