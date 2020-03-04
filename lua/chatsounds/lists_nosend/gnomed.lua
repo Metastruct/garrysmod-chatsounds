b9a3635e0bf538f371eee02051073feae5017008
Mike <mgamkrel@gmail.com>
2020-03-04 00:55:32 +0000

    Whoops

--- lua/chatsounds/lists_nosend/gnomed.lua
+++ lua/chatsounds/lists_nosend/gnomed.lua
@@ -7,5 +7,5 @@ L["hoo hoo hoo ha ha"]={{path="chatsounds/autoadd/gnomed/hoo hoo hoo ha ha.ogg",
 L["hullo me old chum"]={{path="chatsounds/autoadd/gnomed/hullo me old chum.ogg",length=1.578}}
 L["im a gnome"]={{path="chatsounds/autoadd/gnomed/im a gnome.ogg",length=1.186}}
 L["im gnot a gnelf"]={{path="chatsounds/autoadd/gnomed/im gnot a gnelf.ogg",length=1.700}}
-L["im gnot a gnoblin"]={{path="chatsounds/autoadd/gnomed/im gnot a gnoblin.ogg",length=7.458}}
+L["im gnot a gnoblin"]={{path="chatsounds/autoadd/gnomed/im gnot a gnoblin.ogg",length=1.866}}
 c.EndList()
