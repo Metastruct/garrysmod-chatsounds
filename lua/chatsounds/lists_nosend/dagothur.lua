8f804d65338facda23d80486b8bed3ffb2043435
Mike <mgamkrel@gmail.com>
2020-03-04 00:41:54 +0000

    How can you kill a god
    What a grand and intoxicating innocence.

---
+++ lua/chatsounds/lists_nosend/dagothur.lua
@@ -0,0 +1,20 @@
+c.StartList("dagothur")
+L["better luck on your next incarnation"]={{path="chatsounds/autoadd/dagothur/better luck on your next incarnation.ogg",length=2.628}}
+L["come on"]={{path="chatsounds/autoadd/dagothur/come on.ogg",length=1.276}}
+L["come to me through fire and war"]={{path="chatsounds/autoadd/dagothur/come to me through fire and war.ogg",length=2.731}}
+L["forgive me but i am enjoying this"]={{path="chatsounds/autoadd/dagothur/forgive me but i am enjoying this.ogg",length=3.088}}
+L["how can you kill a god"]={{path="chatsounds/autoadd/dagothur/how can you kill a god.ogg",length=2.037}}
+L["how sweet it is to be a god"]={{path="chatsounds/autoadd/dagothur/how sweet it is to be a god.ogg",length=3.538}}
+L["im a god"]={{path="chatsounds/autoadd/dagothur/im a god.ogg",length=1.276}}
+L["im a god how can you kill a god"]={{path="chatsounds/autoadd/dagothur/im a god how can you kill a god.ogg",length=3.398}}
+L["it is not too late for my mercy"]={{path="chatsounds/autoadd/dagothur/it is not too late for my mercy.ogg",length=2.281}}
+L["lay down your weapons"]={{path="chatsounds/autoadd/dagothur/lay down your weapons.ogg",length=1.783}}
+L["no recall or intervention can work in this place"]={{path="chatsounds/autoadd/dagothur/no recall or intervention can work in this place.ogg",length=3.266}}
+L["persistent arent you"]={{path="chatsounds/autoadd/dagothur/persistent arent you.ogg",length=2.206}}
+L["there is no escape"]={{path="chatsounds/autoadd/dagothur/there is no escape.ogg",length=1.539}}
+L["this is getting tiresome"]={{path="chatsounds/autoadd/dagothur/this is getting tiresome.ogg",length=2.459}}
+L["this is taking too long"]={{path="chatsounds/autoadd/dagothur/this is taking too long.ogg",length=1.811}}
+L["what a fool you are"]={{path="chatsounds/autoadd/dagothur/what a fool you are.ogg",length=1.699}}
+L["what a grand and intoxicating innocence"]={{path="chatsounds/autoadd/dagothur/what a grand and intoxicating innocence.ogg",length=2.853}}
+L["you are a stubborn thing nerevar"]={{path="chatsounds/autoadd/dagothur/you are a stubborn thing nerevar.ogg",length=3.003}}
+c.EndList()
