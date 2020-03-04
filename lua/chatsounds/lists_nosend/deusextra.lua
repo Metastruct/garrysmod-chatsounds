8ea2d894f08fcfef41ab3ee4857fcae761655e53
Mike <mgamkrel@gmail.com>
2020-03-04 00:48:54 +0000

    How about some soy food

---
+++ lua/chatsounds/lists_nosend/deusextra.lua
@@ -0,0 +1,6 @@
+c.StartList("deusextra")
+L["how about some soy food"]={{path="chatsounds/autoadd/deusextra/how about some soy food.ogg",length=1.598}}
+L["i just wanted to say you can have this soy food"]={{path="chatsounds/autoadd/deusextra/i just wanted to say you can have this soy food.ogg",length=2.424}}
+L["no not yet"]={{path="chatsounds/autoadd/deusextra/no not yet.ogg",length=4.105}}
+L["youve got ten seconds to have this soy food before i add you to the list of nsf casualties"]={{path="chatsounds/autoadd/deusextra/youve got ten seconds to have this soy food before i add you to the list of nsf casualties.ogg",length=5.551}}
+c.EndList()
