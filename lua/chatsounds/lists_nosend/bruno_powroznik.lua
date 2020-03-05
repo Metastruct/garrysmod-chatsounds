c05282e2c8d7f18cd664a2f93263b3bb8c877710
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-03-05 00:12:35 +0000

    chatsounds preprocessor (16123d501c84f045167619066faa93d6e5dd2e09)
    [ci skip]

---
+++ lua/chatsounds/lists_nosend/bruno_powroznik.lua
@@ -0,0 +1,21 @@
+c.StartList("bruno_powroznik")
+L["banana"]={{path="chatsounds/autoadd/bruno_powroznik/banana.ogg",length=1.304}}
+L["broomstick handle"]={{path="chatsounds/autoadd/bruno_powroznik/broomstick handle.ogg",length=1.739}}
+L["cigar"]={{path="chatsounds/autoadd/bruno_powroznik/cigar.ogg",length=1.208}}
+L["coins"]={{path="chatsounds/autoadd/bruno_powroznik/coins.ogg",length=1.594}}
+L["dildos"]={{path="chatsounds/autoadd/bruno_powroznik/dildos.ogg",length=1.546}}
+L["fingers"]={{path="chatsounds/autoadd/bruno_powroznik/fingers.ogg",length=1.498}}
+L["fishing rod handle"]={{path="chatsounds/autoadd/bruno_powroznik/fishing rod handle.ogg",length=1.763}}
+L["hockey stick handle"]={{path="chatsounds/autoadd/bruno_powroznik/hockey stick handle.ogg",length=2.005}}
+L["objects that ive shoved up my arse"]={{path="chatsounds/autoadd/bruno_powroznik/objects that ive shoved up my arse.ogg",length=2.730}}
+L["pebbles"]={{path="chatsounds/autoadd/bruno_powroznik/pebbles.ogg",length=1.498}}
+L["pencils"]={{path="chatsounds/autoadd/bruno_powroznik/pencils.ogg",length=1.643}}
+L["pens"]={{path="chatsounds/autoadd/bruno_powroznik/pens.ogg",length=1.232}}
+L["screwdriver handle"]={{path="chatsounds/autoadd/bruno_powroznik/screwdriver handle.ogg",length=1.981}}
+L["small glass jar"]={{path="chatsounds/autoadd/bruno_powroznik/small glass jar.ogg",length=1.981}}
+L["stiff cocks"]={{path="chatsounds/autoadd/bruno_powroznik/stiff cocks.ogg",length=1.836}}
+L["test tube"]={{path="chatsounds/autoadd/bruno_powroznik/test tube.ogg",length=1.232}}
+L["toothbrush handle"]={{path="chatsounds/autoadd/bruno_powroznik/toothbrush handle.ogg",length=1.908}}
+L["umbrella handle"]={{path="chatsounds/autoadd/bruno_powroznik/umbrella handle.ogg",length=1.667}}
+L["vibrators"]={{path="chatsounds/autoadd/bruno_powroznik/vibrators.ogg",length=1.449}}
+c.EndList()
