daa7a68b474347548f9342b460034356ac68e7ac
Mike <mgamkrel@gmail.com>
2020-03-09 13:54:05 +0000

    They're playing tic tac toe on my fucking forehead
    This is so fucked up

---
+++ lua/chatsounds/lists_nosend/foreheadtictactoe.lua
@@ -0,0 +1,11 @@
+c.StartList("foreheadtictactoe")
+L["apparently not"]={{path="chatsounds/autoadd/foreheadtictactoe/apparently not.ogg",length=1.179}}
+L["do you see this shit"]={{path="chatsounds/autoadd/foreheadtictactoe/do you see this shit.ogg",length=1.842}}
+L["do you see what theyre doing to me"]={{path="chatsounds/autoadd/foreheadtictactoe/do you see what theyre doing to me.ogg",length=2.087}}
+L["on my fucking forehead"]={{path="chatsounds/autoadd/foreheadtictactoe/on my fucking forehead.ogg",length=2.394}}
+L["theres so many"]={{path="chatsounds/autoadd/foreheadtictactoe/theres so many.ogg",length=1.854}}
+L["theyre playing tic tac toe on my fucking forehead"]={{path="chatsounds/autoadd/foreheadtictactoe/theyre playing tic tac toe on my fucking forehead.ogg",length=4.592}}
+L["this is so fucked up"]={{path="chatsounds/autoadd/foreheadtictactoe/this is so fucked up.ogg",length=2.591}}
+L["what did i do to deserve this"]={{path="chatsounds/autoadd/foreheadtictactoe/what did i do to deserve this.ogg",length=2.443}}
+L["why"]={{path="chatsounds/autoadd/foreheadtictactoe/why/1.ogg",length=0.933},{path="chatsounds/autoadd/foreheadtictactoe/why/2.ogg",length=1.436}}
+c.EndList()
