227d0242fd7ff5ed25768c4af3520131b841f9d3
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-03-11 10:32:58 +0000

    chatsounds preprocessor (d88728cea5b742aa55e1ccf51b549f3e5270af53)
    [ci skip]

--- lua/chatsounds/lists_nosend/billyswift.lua
+++ lua/chatsounds/lists_nosend/billyswift.lua
@@ -1,11 +1,38 @@
 c.StartList("billyswift")
 L["all you need to do is just spray a nice e"]={{path="chatsounds/autoadd/billyswift/all you need to do is just spray a nice e.ogg",length=2.840}}
+L["and created the worlds first chicken boat"]={{path="chatsounds/autoadd/billyswift/and created the worlds first chicken boat.ogg",length=3.683}}
+L["and when insects or rodents are getting into your ass just fucking kill those unwanted pests"]={{path="chatsounds/autoadd/billyswift/and when insects or rodents are getting into your ass just fucking kill those unwanted pests.ogg",length=5.214}}
 L["but flex tape tights on tight and bonds tight"]={{path="chatsounds/autoadd/billyswift/but flex tape tights on tight and bonds tight.ogg",length=2.869}}
+L["chickendeath"]={{path="chatsounds/autoadd/billyswift/chickendeath.ogg",length=2.270}}
+L["cum"]={{path="chatsounds/autoadd/billyswift/cum.ogg",length=0.532}}
+L["deez nuts"]={{path="chatsounds/autoadd/billyswift/deez nuts.ogg",length=0.661}}
+L["flex paste deeply penetrates every hole"]={{path="chatsounds/autoadd/billyswift/flex paste deeply penetrates every hole.ogg",length=3.022}}
+L["flex paste is jizz"]={{path="chatsounds/autoadd/billyswift/flex paste is jizz.ogg",length=2.464}}
+L["flex paste tastes super shit"]={{path="chatsounds/autoadd/billyswift/flex paste tastes super shit.ogg",length=2.069}}
+L["flexpastehit"]={{path="chatsounds/autoadd/billyswift/flexpastehit.ogg",length=0.772}}
+L["hahahaha cum"]={{path="chatsounds/autoadd/billyswift/hahahaha cum.ogg",length=1.414}}
+L["he died"]={{path="chatsounds/autoadd/billyswift/he died.ogg",length=0.856}}
+L["i killed this chicken"]={{path="chatsounds/autoadd/billyswift/i killed this chicken.ogg",length=1.148}}
 L["if you do it properly youre gonna die"]={{path="chatsounds/autoadd/billyswift/if you do it properly youre gonna die.ogg",length=2.693}}
+L["if you test it and theres still a little dick you may need to let it dry"]={{path="chatsounds/autoadd/billyswift/if you test it and theres still a little dick you may need to let it dry.ogg",length=4.183}}
+L["if you test it and theres still a little water you can suck on my nuts"]={{path="chatsounds/autoadd/billyswift/if you test it and theres still a little water you can suck on my nuts.ogg",length=3.800}}
+L["in the chicken hole"]={{path="chatsounds/autoadd/billyswift/in the chicken hole.ogg",length=1.109}}
+L["it cums to the surface"]={{path="chatsounds/autoadd/billyswift/it cums to the surface.ogg",length=1.621}}
+L["its still not working"]={{path="chatsounds/autoadd/billyswift/its still not working.ogg",length=1.381}}
 L["leaky pipes can cause stinky damage"]={{path="chatsounds/autoadd/billyswift/leaky pipes can cause stinky damage.ogg",length=2.986}}
+L["my life is completely fucked"]={{path="chatsounds/autoadd/billyswift/my life is completely fucked.ogg",length=2.270}}
+L["ne"]={{path="chatsounds/autoadd/billyswift/ne.ogg",length=0.674}}
 L["now you can smoke crack"]={{path="chatsounds/autoadd/billyswift/now you can smoke crack.ogg",length=1.230}}
 L["ooooo"]={{path="chatsounds/autoadd/billyswift/ooooo.ogg",length=0.370}}
+L["philswiftglitch"]={{path="chatsounds/autoadd/billyswift/philswiftglitch.ogg",length=0.331}}
+L["philswiftpain"]={{path="chatsounds/autoadd/billyswift/philswiftpain.ogg",length=1.342}}
 L["thats a tight butt but flex tape cuts super wide"]={{path="chatsounds/autoadd/billyswift/thats a tight butt but flex tape cuts super wide.ogg",length=3.206}}
 L["three toot one"]={{path="chatsounds/autoadd/billyswift/three toot one.ogg",length=2.518}}
+L["to show you the power of flex paste"]={{path="chatsounds/autoadd/billyswift/to show you the power of flex paste.ogg",length=2.360}}
+L["what a sissy"]={{path="chatsounds/autoadd/billyswift/what a sissy.ogg",length=1.172}}
+L["woow"]={{path="chatsounds/autoadd/billyswift/woow.ogg",length=0.681}}
 L["yeay"]={{path="chatsounds/autoadd/billyswift/yeay.ogg",length=0.644}}
+L["you can push hard dicks deep into wet pussies"]={{path="chatsounds/autoadd/billyswift/you can push hard dicks deep into wet pussies.ogg",length=3.229}}
+L["you fucking bitch"]={{path="chatsounds/autoadd/billyswift/you fucking bitch.ogg",length=1.109}}
+L["you people disgust me"]={{path="chatsounds/autoadd/billyswift/you people disgust me.ogg",length=1.706}}
 c.EndList()
