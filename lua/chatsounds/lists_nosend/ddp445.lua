0cf97caef903601081e8a680f00d40a91e686648
Mike <mgamkrel@gmail.com>
2020-03-14 23:53:45 +0000

    FTFY
    apostrophe man bad

--- lua/chatsounds/lists_nosend/ddp445.lua
+++ lua/chatsounds/lists_nosend/ddp445.lua
@@ -1,4 +1,4 @@
 c.StartList("ddp445")
 L["do you see me fucking laughing my n"]={{path="chatsounds/autoadd/ddp445/do you see me fucking laughing my n.ogg",length=2.244}}
-L["i ain't fucking laughing"]={{path="chatsounds/autoadd/ddp445/i ain't fucking laughing.ogg",length=1.320}}
+L["i aint fucking laughing"]={{path="chatsounds/autoadd/ddp445/i aint fucking laughing.ogg",length=1.320}}
 c.EndList()
