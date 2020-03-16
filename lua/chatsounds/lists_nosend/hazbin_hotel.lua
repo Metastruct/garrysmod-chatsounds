3a1572d133ba9d519b4087288e5043e6b6664203
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-03-16 22:09:38 +0000

    chatsounds preprocessor (aa6ab470c0fd95d2e49089316ad3bc9d39cd2e31)
    [ci skip]

--- lua/chatsounds/lists_nosend/hazbin_hotel.lua
+++ lua/chatsounds/lists_nosend/hazbin_hotel.lua
@@ -1,5 +1,5 @@
 c.StartList("hazbin_hotel")
-L["and what can you do my effimate fellow"]={{path="chatsounds/autoadd/hazbin_hotel/and what can you do my effimate fellow.ogg",length=2.687}}
+L["and what can you do my effeminate fellow"]={{path="chatsounds/autoadd/hazbin_hotel/and what can you do my effeminate fellow.ogg",length=2.687}}
 L["i can suck your dick"]={{path="chatsounds/autoadd/hazbin_hotel/i can suck your dick.ogg",length=1.489}}
 L["oh harder daddy"]={{path="chatsounds/autoadd/hazbin_hotel/oh harder daddy.ogg",length=2.403}}
 L["your loss"]={{path="chatsounds/autoadd/hazbin_hotel/your loss.ogg",length=1.001}}
