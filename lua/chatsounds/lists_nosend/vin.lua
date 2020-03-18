64bd726df434ffa9d6d09565b00ec73d2e81d614
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-03-18 01:54:24 +0000

    chatsounds preprocessor (e79fff698a2598906d6569f6294bb30be326c600)
    [ci skip]

--- lua/chatsounds/lists_nosend/vin.lua
+++ lua/chatsounds/lists_nosend/vin.lua
@@ -1,5 +1,6 @@
 c.StartList("vin")
 L["and his name was inescapable death"]={{path="chatsounds/autoadd/vin/and his name was inescapable death.ogg",length=4.787}}
+L["bup"]={{path="chatsounds/autoadd/vin/bup.ogg",length=0.123}}
 L["fleelf"]={{path="chatsounds/autoadd/vin/fleelf.ogg",length=0.358}}
 L["flextapeglitch"]={{path="chatsounds/autoadd/vin/flextapeglitch.ogg",length=1.606}}
 L["hiss"]={{path="chatsounds/autoadd/vin/hiss.ogg",length=0.899}}
@@ -18,6 +19,7 @@ L["shut up"]={{path="chatsounds/autoadd/vin/shut up.ogg",length=0.643}}
 L["snans"]={{path="chatsounds/autoadd/vin/snans.ogg",length=0.696}}
 L["soup time"]={{path="chatsounds/autoadd/vin/soup time.ogg",length=6.010}}
 L["steamed hams"]={{path="chatsounds/autoadd/vin/steamed hams.ogg",length=1.202}}
+L["toadscream"]={{path="chatsounds/autoadd/vin/toadscream.ogg",length=1.779}}
 L["u wot"]={{path="chatsounds/autoadd/vin/u wot.ogg",length=0.844}}
 L["wait wait what"]={{path="chatsounds/autoadd/vin/wait wait what.ogg",length=1.242}}
 L["what was that"]={{path="chatsounds/autoadd/vin/what was that.ogg",length=0.568}}
