772ea00b871e638543c8e59d4cc6f9bf69e2d5aa
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-03-19 20:01:07 +0000

    chatsounds preprocessor (be8d3da8cf7af6425003484886bf7e672847d4a8)
    [ci skip]

--- lua/chatsounds/lists_nosend/gman.lua
+++ lua/chatsounds/lists_nosend/gman.lua
@@ -8,27 +8,16 @@ L["continually"]={{path="chatsounds/autoadd/gman/continually.ogg",length=0.971}}
 L["get back where you belong and forget about all this until"]={{path="chatsounds/autoadd/gman/get back where you belong and forget about all this until.ogg",length=5.608}}
 L["gmangasm"]={{path="chatsounds/autoadd/gman/gmangasm.ogg",length=1.621}}
 L["gmansino"]={{path="chatsounds/autoadd/gman/gmansino.ogg",length=4.113}}
-L["gordon freeman in the flesh or rather in the hazard suit i took the liberty of relieving you of your weapons"]={{path="chatsounds/autoadd/gman/gordon freeman in the flesh or rather in the hazard suit i took the liberty of relieving you of your weapons.ogg",length=16.507}}
 L["i admit i have a fascination with those who adapt and survive against all odds"]={{path="chatsounds/autoadd/gman/i admit i have a fascination with those who adapt and survive against all odds.ogg",length=6.111}}
 L["i do hope you understand"]={{path="chatsounds/autoadd/gman/i do hope you understand.ogg",length=2.256}}
 L["im sure you can imagine there are worse alternatives"]={{path="chatsounds/autoadd/gman/im sure you can imagine there are worse alternatives.ogg",length=4.010}}
-L["its time to choose"]={{path="chatsounds/autoadd/gman/its time to choose.ogg",length=1.590}}
-L["no regrets mr freeman"]={{path="chatsounds/autoadd/gman/no regrets mr freeman.ogg",length=2.674}}
-L["otherwise well i can offer you a battle you have no chance of winning"]={{path="chatsounds/autoadd/gman/otherwise well i can offer you a battle you have no chance of winning.ogg",length=13.214}}
 L["please dont think"]={{path="chatsounds/autoadd/gman/please dont think.ogg",length=1.416}}
 L["please dont think that"]={{path="chatsounds/autoadd/gman/please dont think that.ogg",length=1.717}}
 L["quite so"]={{path="chatsounds/autoadd/gman/quite so.ogg",length=1.045}}
 L["subject you"]={{path="chatsounds/autoadd/gman/subject you.ogg",length=0.622}}
 L["telling all"]={{path="chatsounds/autoadd/gman/telling all.ogg",length=0.941}}
-L["thats why im here mr freeman i have recommended your services to my employers"]={{path="chatsounds/autoadd/gman/thats why im here mr freeman i have recommended your services to my employers.ogg",length=20.117}}
 L["the biggest embarrassment has been"]={{path="chatsounds/autoadd/gman/the biggest embarrassment has been.ogg",length=2.541}}
-L["the borderworld then is in our control for the time being"]={{path="chatsounds/autoadd/gman/the borderworld then is in our control for the time being.ogg",length=12.698}}
-L["time to choose"]={{path="chatsounds/autoadd/gman/time to choose.ogg",length=1.802}}
 L["we have decided to convey you"]={{path="chatsounds/autoadd/gman/we have decided to convey you.ogg",length=2.825}}
 L["we meet"]={{path="chatsounds/autoadd/gman/we meet.ogg",length=1.039}}
 L["we meet at last"]={{path="chatsounds/autoadd/gman/we meet at last.ogg",length=1.938}}
-L["well it looks like we wont be working together"]={{path="chatsounds/autoadd/gman/well it looks like we wont be working together.ogg",length=3.354}}
-L["well it looks like we wont be working together no regrets"]={{path="chatsounds/autoadd/gman/well it looks like we wont be working together no regrets.ogg",length=20.072}}
-L["wisely done mr freeman i will see you up ahead"]={{path="chatsounds/autoadd/gman/wisely done mr freeman i will see you up ahead.ogg",length=5.106}}
-L["youve proved yourself a decisive man so i dont expect youll have any trouble deciding what to do"]={{path="chatsounds/autoadd/gman/youve proved yourself a decisive man so i dont expect youll have any trouble deciding what to do.ogg",length=13.499}}
 c.EndList()
