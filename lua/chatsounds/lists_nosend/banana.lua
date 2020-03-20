8445002266e3e04902fa87e743923b85589c4b0a
Mikeai <mgamkrel@gmail.com>
2020-03-20 19:38:13 +0000

    Banana rotat e
    Rotate faster banana

---
+++ lua/chatsounds/lists_nosend/banana.lua
@@ -0,0 +1,13 @@
+c.StartList("banana")
+L["banan a"]={{path="chatsounds/autoadd/banana/banan a.ogg",length=1.839}}
+L["bananas"]={{path="chatsounds/autoadd/banana/bananas.ogg",length=1.777}}
+L["bananas rotat e"]={{path="chatsounds/autoadd/banana/bananas rotat e.ogg",length=3.956}}
+L["can you feel it banana"]={{path="chatsounds/autoadd/banana/can you feel it banana.ogg",length=3.776}}
+L["g o"]={{path="chatsounds/autoadd/banana/g o.ogg",length=1.708}}
+L["goo"]={{path="chatsounds/autoadd/banana/goo.ogg",length=1.666}}
+L["rotat e"]={{path="chatsounds/autoadd/banana/rotat e.ogg",length=1.985}}
+L["rotate faster banana"]={{path="chatsounds/autoadd/banana/rotate faster banana.ogg",length=3.089}}
+L["we have reached maximum velocipy"]={{path="chatsounds/autoadd/banana/we have reached maximum velocipy.ogg",length=3.130}}
+L["who are you to accuse me"]={{path="chatsounds/autoadd/banana/who are you to accuse me.ogg",length=1.357}}
+L["yes feel the sped"]={{path="chatsounds/autoadd/banana/yes feel the sped.ogg",length=2.520}}
+c.EndList()
