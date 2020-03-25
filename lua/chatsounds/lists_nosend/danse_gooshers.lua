32fa10d16237f51c02f7b6ce1c4d8267f8a527f0
Mikeai <mgamkrel@gmail.com>
2020-03-25 03:59:47 +0000

    Removal of unused sounds in different languages
    Self explanatory by title, these removals are due to their near zero practical use in the chatsounds environment, with some being intrusive at times. Also chatsounds in a different language that have any meaning whatsoever, will never be used. You know it's true.

--- lua/chatsounds/lists_nosend/danse_gooshers.lua
+++
@@ -1,8 +0,0 @@
-c.StartList("danse_gooshers")
-L["kids crowd"]={{path="chatsounds/autoadd/danse_gooshers/kids crowd.ogg",length=3.524}}
-L["ok ja jetzt bin ich aber kaputt von all dem spass"]={{path="chatsounds/autoadd/danse_gooshers/ok ja jetzt bin ich aber kaputt von all dem spass.ogg",length=5.118}}
-L["schoen toll"]={{path="chatsounds/autoadd/danse_gooshers/schoen toll.ogg",length=1.097}}
-L["super toll"]={{path="chatsounds/autoadd/danse_gooshers/super toll/super toll1.ogg",length=1.046},{path="chatsounds/autoadd/danse_gooshers/super toll/super toll2.ogg",length=1.364}}
-L["toll toll"]={{path="chatsounds/autoadd/danse_gooshers/toll toll.ogg",length=1.259}}
-L["wie heisst du schoen"]={{path="chatsounds/autoadd/danse_gooshers/wie heisst du schoen.ogg",length=1.070}}
-c.EndList()
