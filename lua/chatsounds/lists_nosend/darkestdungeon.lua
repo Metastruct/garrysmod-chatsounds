46a6c957b3800cedf4cc203548b0583f9d29cd19
Mikeai <mgamkrel@gmail.com>
2020-03-26 05:14:25 +0000

    Some lines from the Darkest Dungeon narrator
    Remind yourself that overconfidence is a slow and insidious killer

---
+++ lua/chatsounds/lists_nosend/darkestdungeon.lua
@@ -0,0 +1,21 @@
+c.StartList("darkestdungeon")
+L["a singular strike"]={{path="chatsounds/autoadd/darkestdungeon/a singular strike.ogg",length=2.122}}
+L["a trifling victory but a victory nonetheless"]={{path="chatsounds/autoadd/darkestdungeon/a trifling victory but a victory nonetheless.ogg",length=5.301}}
+L["and now the true test"]={{path="chatsounds/autoadd/darkestdungeon/and now the true test.ogg",length=1.895}}
+L["another soul battered and broken"]={{path="chatsounds/autoadd/darkestdungeon/another soul battered and broken.ogg",length=2.842}}
+L["begone fiend"]={{path="chatsounds/autoadd/darkestdungeon/begone fiend.ogg",length=1.411}}
+L["cast aside like a spent torch"]={{path="chatsounds/autoadd/darkestdungeon/cast aside like a spent torch.ogg",length=2.543}}
+L["continue the onslaught"]={{path="chatsounds/autoadd/darkestdungeon/continue the onslaught.ogg",length=1.978}}
+L["death waits for the slightest lapse in concentration"]={{path="chatsounds/autoadd/darkestdungeon/death waits for the slightest lapse in concentration.ogg",length=4.673}}
+L["destroy them all"]={{path="chatsounds/autoadd/darkestdungeon/destroy them all.ogg",length=2.889}}
+L["gently"]={{path="chatsounds/autoadd/darkestdungeon/gently.ogg",length=0.888}}
+L["hold fast or expire"]={{path="chatsounds/autoadd/darkestdungeon/hold fast or expire.ogg",length=2.673}}
+L["how quickly the tide turns"]={{path="chatsounds/autoadd/darkestdungeon/how quickly the tide turns.ogg",length=2.344}}
+L["madness our old friend"]={{path="chatsounds/autoadd/darkestdungeon/madness our old friend.ogg",length=2.887}}
+L["no hope at all"]={{path="chatsounds/autoadd/darkestdungeon/no hope at all.ogg",length=1.329}}
+L["remind yourself that overconfidence is a slow and insidious killer"]={{path="chatsounds/autoadd/darkestdungeon/remind yourself that overconfidence is a slow and insidious killer.ogg",length=4.658}}
+L["slowly"]={{path="chatsounds/autoadd/darkestdungeon/slowly.ogg",length=0.798}}
+L["there can be no hope in this hell"]={{path="chatsounds/autoadd/darkestdungeon/there can be no hope in this hell.ogg",length=2.187}}
+L["these nightmarish creatures can be felled they can be beaten"]={{path="chatsounds/autoadd/darkestdungeon/these nightmarish creatures can be felled they can be beaten.ogg",length=4.226}}
+L["this is how a life is taken"]={{path="chatsounds/autoadd/darkestdungeon/this is how a life is taken.ogg",length=2.143}}
+c.EndList()
