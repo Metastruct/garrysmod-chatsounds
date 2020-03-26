5d37974f4b9b3abe1257019712c200ed0a488396
Mikeai <mgamkrel@gmail.com>
2020-03-26 22:36:30 +0000

    Return of Cracklife (Part 1/2 probably)
    Still need to check the other cracklife mod

--- lua/chatsounds/lists_nosend/cracklife.lua
+++ lua/chatsounds/lists_nosend/cracklife.lua
@@ -1,4 +1,7 @@
 c.StartList("cracklife")
+L["a place in the middle of fucking nowhere"]={{path="chatsounds/autoadd/cracklife/a place in the middle of fucking nowhere.ogg",length=2.299}}
+L["and nobody in here is a normal person"]={{path="chatsounds/autoadd/cracklife/and nobody in here is a normal person.ogg",length=2.374}}
+L["and welcome to black mesa"]={{path="chatsounds/autoadd/cracklife/and welcome to black mesa.ogg",length=1.767}}
 L["autismtown"]={{path="chatsounds/autoadd/cracklife/autismtown.ogg",length=35.886}}
 L["banditradio"]={{path="chatsounds/autoadd/cracklife/banditradio.ogg",length=113.476}}
 L["crack need human body parts"]={{path="chatsounds/autoadd/cracklife/crack need human body parts.ogg",length=3.965}}
@@ -8,11 +11,25 @@ L["cracklife door close"]={{path="chatsounds/autoadd/cracklife/cracklife door cl
 L["cracklife huge door"]={{path="chatsounds/autoadd/cracklife/cracklife huge door.ogg",length=14.680}}
 L["cracksong"]={{path="chatsounds/autoadd/cracklife/cracksong.ogg",length=28.096}}
 L["dance of the manwhore"]={{path="chatsounds/autoadd/cracklife/dance of the manwhore.ogg",length=28.096}}
+L["except me"]={{path="chatsounds/autoadd/cracklife/except me.ogg",length=0.987}}
 L["fock"]={{path="chatsounds/autoadd/cracklife/fock/geiger1.ogg",length=0.290},{path="chatsounds/autoadd/cracklife/fock/geiger2.ogg",length=0.284},{path="chatsounds/autoadd/cracklife/fock/geiger3.ogg",length=0.290},{path="chatsounds/autoadd/cracklife/fock/geiger4.ogg",length=0.180},{path="chatsounds/autoadd/cracklife/fock/geiger5.ogg",length=0.197},{path="chatsounds/autoadd/cracklife/fock/geiger6.ogg",length=0.342}}
+L["good morning faggot"]={{path="chatsounds/autoadd/cracklife/good morning faggot.ogg",length=1.187}}
+L["here at the black mesa we make sure all employees get the same treatment as others"]={{path="chatsounds/autoadd/cracklife/here at the black mesa we make sure all employees get the same treatment as others.ogg",length=4.848}}
 L["i appear to have lost any possible life signals carry on"]={{path="chatsounds/autoadd/cracklife/i appear to have lost any possible life signals carry on.ogg",length=4.719}}
+L["i cant believe you want to work here"]={{path="chatsounds/autoadd/cracklife/i cant believe you want to work here.ogg",length=2.205}}
 L["i will now proceed to pounce at you"]={{path="chatsounds/autoadd/cracklife/i will now proceed to pounce at you.ogg",length=2.368}}
+L["it appears that you completely failed"]={{path="chatsounds/autoadd/cracklife/it appears that you completely failed.ogg",length=2.276}}
 L["march01"]={{path="chatsounds/autoadd/cracklife/march01.ogg",length=17.816}}
+L["my dick is bigger than yours"]={{path="chatsounds/autoadd/cracklife/my dick is bigger than yours.ogg",length=1.584}}
+L["next stop faggot land"]={{path="chatsounds/autoadd/cracklife/next stop faggot land.ogg",length=7.007}}
 L["no fakku offu gordon"]={{path="chatsounds/autoadd/cracklife/no fakku offu gordon.ogg",length=2.061}}
 L["oi blyad"]={{path="chatsounds/autoadd/cracklife/oi blyad.ogg",length=1.097}}
 L["riders on the storm"]={{path="chatsounds/autoadd/cracklife/riders on the storm.ogg",length=18.274}}
+L["seriously everybody is a fucking retard and a freak"]={{path="chatsounds/autoadd/cracklife/seriously everybody is a fucking retard and a freak.ogg",length=3.729}}
+L["take cover guys this faggot is spamming shit"]={{path="chatsounds/autoadd/cracklife/take cover guys this faggot is spamming shit.ogg",length=3.054}}
+L["the c stands for cancer"]={{path="chatsounds/autoadd/cracklife/the c stands for cancer.ogg",length=1.676}}
+L["which means they can go fuck themselves"]={{path="chatsounds/autoadd/cracklife/which means they can go fuck themselves.ogg",length=2.405}}
+L["you really are a suicidal faggot arent you"]={{path="chatsounds/autoadd/cracklife/you really are a suicidal faggot arent you.ogg",length=2.820}}
+L["your mother you cocksucking son of a bitch"]={{path="chatsounds/autoadd/cracklife/your mother you cocksucking son of a bitch.ogg",length=2.647}}
+L["youre mom you cuntfuck"]={{path="chatsounds/autoadd/cracklife/youre mom you cuntfuck.ogg",length=1.238}}
 c.EndList()
