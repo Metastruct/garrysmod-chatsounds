9906b6a9abe16f354f1f6c8deb758e9e8a001703
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-03-29 00:24:15 +0000

    chatsounds preprocessor (0b95f4b14e5629d1c50973818edb7739c84df753)
    [ci skip]

--- lua/chatsounds/lists_nosend/douchebagchocolat.lua
+++ lua/chatsounds/lists_nosend/douchebagchocolat.lua
@@ -4,6 +4,5 @@ L["do it you pussy"]={{path="chatsounds/autoadd/douchebagchocolat/do it you puss
 L["gibberish"]={{path="chatsounds/autoadd/douchebagchocolat/gibberish.ogg",length=3.491}}
 L["is your fuckin fuel line on"]={{path="chatsounds/autoadd/douchebagchocolat/is your fuckin fuel line on.ogg",length=2.156}}
 L["is your fuel line on"]={{path="chatsounds/autoadd/douchebagchocolat/is your fuel line on.ogg",length=1.557}}
-L["no nigga"]={{path="chatsounds/autoadd/douchebagchocolat/no nigga.ogg",length=1.540}}
 L["tpuid ass"]={{path="chatsounds/autoadd/douchebagchocolat/tpuid ass.ogg",length=1.831}}
 c.EndList()
