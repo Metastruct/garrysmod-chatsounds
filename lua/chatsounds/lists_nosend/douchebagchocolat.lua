a2fd1bee8c3373744685707f6bfc8e22ef0b9ba7
Carl Roberts <48777329+carlroberts99@users.noreply.github.com>
2020-03-29 00:15:38 +0000

    douchebagchocolat/demolition d sounds
    sounds added by request of Waifu Barisita/Willa Doo

---
+++ lua/chatsounds/lists_nosend/douchebagchocolat.lua
@@ -0,0 +1,9 @@
+c.StartList("douchebagchocolat")
+L["about to start a dirt bike im a big boy not a tike yous a fuckin kike"]={{path="chatsounds/autoadd/douchebagchocolat/about to start a dirt bike im a big boy not a tike yous a fuckin kike.ogg",length=5.306}}
+L["do it you pussy"]={{path="chatsounds/autoadd/douchebagchocolat/do it you pussy.ogg",length=1.624}}
+L["gibberish"]={{path="chatsounds/autoadd/douchebagchocolat/gibberish.ogg",length=3.491}}
+L["is your fuckin fuel line on"]={{path="chatsounds/autoadd/douchebagchocolat/is your fuckin fuel line on.ogg",length=2.156}}
+L["is your fuel line on"]={{path="chatsounds/autoadd/douchebagchocolat/is your fuel line on.ogg",length=1.557}}
+L["no nigga"]={{path="chatsounds/autoadd/douchebagchocolat/no nigga.ogg",length=1.540}}
+L["tpuid ass"]={{path="chatsounds/autoadd/douchebagchocolat/tpuid ass.ogg",length=1.831}}
+c.EndList()
