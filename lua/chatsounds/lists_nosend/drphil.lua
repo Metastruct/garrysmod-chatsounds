48f9ea29efc53d6128cc87ed472f0e2075aef029
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-03-30 10:00:37 +0000

    chatsounds preprocessor (6e06d5a55a2b31ded5a4a8eb986b742b4770cc2c)
    [ci skip]

---
+++ lua/chatsounds/lists_nosend/drphil.lua
@@ -0,0 +1,10 @@
+c.StartList("drphil")
+L["go kill yourself go sit in the middle of the road and let a car run over you"]={{path="chatsounds/autoadd/drphil/go kill yourself go sit in the middle of the road and let a car run over you.ogg",length=4.277}}
+L["i want the money"]={{path="chatsounds/autoadd/drphil/i want the money.ogg",length=0.920}}
+L["nomoreiloveyous"]={{path="chatsounds/autoadd/drphil/nomoreiloveyous.ogg",length=23.429}}
+L["shut the hell up bitch"]={{path="chatsounds/autoadd/drphil/shut the hell up bitch.ogg",length=1.992}}
+L["uh ill raise enough hell til you give in and give it to me"]={{path="chatsounds/autoadd/drphil/uh ill raise enough hell til you give in and give it to me.ogg",length=3.377}}
+L["whats funny about that"]={{path="chatsounds/autoadd/drphil/whats funny about that.ogg",length=1.078}}
+L["youre an alcoholic"]={{path="chatsounds/autoadd/drphil/youre an alcoholic.ogg",length=1.163}}
+L["youre ugly youre disgusting im gonna kill you"]={{path="chatsounds/autoadd/drphil/youre ugly youre disgusting im gonna kill you.ogg",length=2.876}}
+c.EndList()
