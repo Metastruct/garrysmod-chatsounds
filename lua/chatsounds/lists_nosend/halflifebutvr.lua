f7043dd8e8ccc6a06ff5dd484c5dd3a4169e9402
Mikeai <mgamkrel@gmail.com>
2020-03-30 20:56:00 +0000

    Look gordon, ropes! (Part 1/?)
    Next batch will come around when the next episode does (probably)

---
+++ lua/chatsounds/lists_nosend/halflifebutvr.lua
@@ -0,0 +1,49 @@
+c.StartList("halflifebutvr")
+L["another clone defeated"]={{path="chatsounds/autoadd/halflifebutvr/another clone defeated.ogg",length=2.103}}
+L["can i see your passport"]={{path="chatsounds/autoadd/halflifebutvr/can i see your passport.ogg",length=1.233}}
+L["can you believe this guy doesnt have his fuckin passport"]={{path="chatsounds/autoadd/halflifebutvr/can you believe this guy doesnt have his fuckin passport.ogg",length=2.938}}
+L["feed me my coins mr freeman"]={{path="chatsounds/autoadd/halflifebutvr/feed me my coins mr freeman.ogg",length=5.906}}
+L["goddamnit im sorry for saying that but what are you doing"]={{path="chatsounds/autoadd/halflifebutvr/goddamnit im sorry for saying that but what are you doing.ogg",length=3.042}}
+L["good thing i have the rope"]={{path="chatsounds/autoadd/halflifebutvr/good thing i have the rope.ogg",length=1.564}}
+L["gordon be careful there are explosives here"]={{path="chatsounds/autoadd/halflifebutvr/gordon be careful there are explosives here.ogg",length=2.502}}
+L["he doesnt have his passport"]={{path="chatsounds/autoadd/halflifebutvr/he doesnt have his passport.ogg",length=1.358}}
+L["he owed me five"]={{path="chatsounds/autoadd/halflifebutvr/he owed me five.ogg",length=1.357}}
+L["hello gordon"]={{path="chatsounds/autoadd/halflifebutvr/hello gordon.ogg",length=0.961}}
+L["hes in pain"]={{path="chatsounds/autoadd/halflifebutvr/hes in pain.ogg",length=0.957}}
+L["hey youre not supposed to be in here"]={{path="chatsounds/autoadd/halflifebutvr/hey youre not supposed to be in here.ogg",length=1.278}}
+L["how did a pigeon get down here"]={{path="chatsounds/autoadd/halflifebutvr/how did a pigeon get down here.ogg",length=1.315}}
+L["how the fu"]={{path="chatsounds/autoadd/halflifebutvr/how the fu.ogg",length=1.351}}
+L["i believe this hole is about five hundred feet deep"]={{path="chatsounds/autoadd/halflifebutvr/i believe this hole is about five hundred feet deep.ogg",length=4.080}}
+L["i dont have my passport"]={{path="chatsounds/autoadd/halflifebutvr/i dont have my passport.ogg",length=1.244}}
+L["i dont think you can live with that"]={{path="chatsounds/autoadd/halflifebutvr/i dont think you can live with that.ogg",length=1.542}}
+L["i feel normal"]={{path="chatsounds/autoadd/halflifebutvr/i feel normal.ogg",length=1.450}}
+L["i like it there shut up"]={{path="chatsounds/autoadd/halflifebutvr/i like it there shut up.ogg",length=1.309}}
+L["i think you fucked it up"]={{path="chatsounds/autoadd/halflifebutvr/i think you fucked it up.ogg",length=1.647}}
+L["if we use our ropes we can easily traverse this area"]={{path="chatsounds/autoadd/halflifebutvr/if we use our ropes we can easily traverse this area.ogg",length=2.914}}
+L["im going to go fire a rocket"]={{path="chatsounds/autoadd/halflifebutvr/im going to go fire a rocket.ogg",length=1.763}}
+L["im the alpha male now"]={{path="chatsounds/autoadd/halflifebutvr/im the alpha male now.ogg",length=2.103}}
+L["its just brain cancer you can live with that"]={{path="chatsounds/autoadd/halflifebutvr/its just brain cancer you can live with that.ogg",length=2.289}}
+L["look gordon"]={{path="chatsounds/autoadd/halflifebutvr/look gordon.ogg",length=0.829}}
+L["look gordon beasts"]={{path="chatsounds/autoadd/halflifebutvr/look gordon beasts.ogg",length=1.564}}
+L["look gordon ropes"]={{path="chatsounds/autoadd/halflifebutvr/look gordon ropes.ogg",length=1.657}}
+L["my clone"]={{path="chatsounds/autoadd/halflifebutvr/my clone.ogg",length=1.039}}
+L["my god gordon you just killed a man"]={{path="chatsounds/autoadd/halflifebutvr/my god gordon you just killed a man.ogg",length=2.091}}
+L["my power grows gordon"]={{path="chatsounds/autoadd/halflifebutvr/my power grows gordon.ogg",length=1.677}}
+L["now gordon you may have killed three innocent lives but its important to stay calm"]={{path="chatsounds/autoadd/halflifebutvr/now gordon you may have killed three innocent lives but its important to stay calm.ogg",length=4.127}}
+L["ofcourse i have my passport"]={{path="chatsounds/autoadd/halflifebutvr/ofcourse i have my passport.ogg",length=2.064}}
+L["oh im fine now"]={{path="chatsounds/autoadd/halflifebutvr/oh im fine now.ogg",length=1.535}}
+L["okay thats not your name"]={{path="chatsounds/autoadd/halflifebutvr/okay thats not your name.ogg",length=1.210}}
+L["perfectly justified"]={{path="chatsounds/autoadd/halflifebutvr/perfectly justified.ogg",length=1.110}}
+L["ropes can be used to traverse pits gordon"]={{path="chatsounds/autoadd/halflifebutvr/ropes can be used to traverse pits gordon.ogg",length=2.582}}
+L["see what gordon"]={{path="chatsounds/autoadd/halflifebutvr/see what gordon.ogg",length=1.238}}
+L["self defense gordon"]={{path="chatsounds/autoadd/halflifebutvr/self defense gordon.ogg",length=1.185}}
+L["they didnt have their passport"]={{path="chatsounds/autoadd/halflifebutvr/they didnt have their passport.ogg",length=1.909}}
+L["this is my job dude"]={{path="chatsounds/autoadd/halflifebutvr/this is my job dude.ogg",length=1.751}}
+L["we can use these to help with pits"]={{path="chatsounds/autoadd/halflifebutvr/we can use these to help with pits.ogg",length=1.730}}
+L["were trying to dig to the center of the earth"]={{path="chatsounds/autoadd/halflifebutvr/were trying to dig to the center of the earth.ogg",length=2.611}}
+L["why are you yelling at me"]={{path="chatsounds/autoadd/halflifebutvr/why are you yelling at me.ogg",length=1.334}}
+L["yeah im sorry were kinda fucked"]={{path="chatsounds/autoadd/halflifebutvr/yeah im sorry were kinda fucked.ogg",length=1.144}}
+L["you could have helped him gordon"]={{path="chatsounds/autoadd/halflifebutvr/you could have helped him gordon.ogg",length=1.303}}
+L["you die if you go in here"]={{path="chatsounds/autoadd/halflifebutvr/you die if you go in here.ogg",length=1.425}}
+L["you owe me three dollars"]={{path="chatsounds/autoadd/halflifebutvr/you owe me three dollars.ogg",length=2.076}}
+c.EndList()
