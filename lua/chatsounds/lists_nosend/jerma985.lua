540c64b442b5f4bda392bdbb09406d9b4b06203b
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-03-30 05:55:36 +0000

    chatsounds preprocessor (f057bc9754214fb2fe0ff648520f3b7a4a06d9f8)
    [ci skip]

---
+++ lua/chatsounds/lists_nosend/jerma985.lua
@@ -0,0 +1,7 @@
+c.StartList("jerma985")
+L["im the giant rat that makes all of the rules"]={{path="chatsounds/autoadd/jerma985/im the giant rat that makes all of the rules.ogg",length=4.640}}
+L["lets see what kind of trouble we can get ourselves into"]={{path="chatsounds/autoadd/jerma985/lets see what kind of trouble we can get ourselves into.ogg",length=4.551}}
+L["rats were rats"]={{path="chatsounds/autoadd/jerma985/rats were rats.ogg",length=2.239}}
+L["we prey at night we stalk at night"]={{path="chatsounds/autoadd/jerma985/we prey at night we stalk at night.ogg",length=2.609}}
+L["were the rats"]={{path="chatsounds/autoadd/jerma985/were the rats/1.ogg",length=2.232},{path="chatsounds/autoadd/jerma985/were the rats/2.ogg",length=1.802}}
+c.EndList()
