2789c4e3ee7fb6c4ca583bdcb86a8750b4f597bb
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-03-31 01:21:38 +0000

    chatsounds preprocessor (7a3528d7cd921290efa52c0060844ca1e5720610)
    [ci skip]

--- lua/chatsounds/lists_nosend/drphil.lua
+++ lua/chatsounds/lists_nosend/drphil.lua
@@ -1,10 +1,72 @@
 c.StartList("drphil")
+L["7 suicide attempts"]={{path="chatsounds/autoadd/drphil/7 suicide attempts.ogg",length=2.029}}
+L["after she spills spaghettios"]={{path="chatsounds/autoadd/drphil/after she spills spaghettios.ogg",length=1.782}}
+L["and whatdya know"]={{path="chatsounds/autoadd/drphil/and whatdya know.ogg",length=1.384}}
+L["are you a traditional mom"]={{path="chatsounds/autoadd/drphil/are you a traditional mom.ogg",length=1.503}}
+L["arent you just kinda sittin around sayin"]={{path="chatsounds/autoadd/drphil/arent you just kinda sittin around sayin.ogg",length=2.256}}
+L["at that point"]={{path="chatsounds/autoadd/drphil/at that point.ogg",length=1.172}}
+L["because its ridiculous"]={{path="chatsounds/autoadd/drphil/because its ridiculous.ogg",length=1.313}}
+L["bet they do now"]={{path="chatsounds/autoadd/drphil/bet they do now.ogg",length=1.054}}
+L["bitches"]={{path="chatsounds/autoadd/drphil/bitches.ogg",length=0.551}}
+L["come on"]={{path="chatsounds/autoadd/drphil/come on.ogg",length=1.000}}
+L["diaper rash"]={{path="chatsounds/autoadd/drphil/diaper rash.ogg",length=0.859}}
+L["did you give your wife two stds"]={{path="chatsounds/autoadd/drphil/did you give your wife two stds.ogg",length=1.854}}
+L["do not insult my intelligence"]={{path="chatsounds/autoadd/drphil/do not insult my intelligence.ogg",length=1.860}}
+L["do not talk"]={{path="chatsounds/autoadd/drphil/do not talk.ogg",length=1.091}}
+L["do you see how this works"]={{path="chatsounds/autoadd/drphil/do you see how this works.ogg",length=2.086}}
+L["dont give me that self righteous crap"]={{path="chatsounds/autoadd/drphil/dont give me that self righteous crap.ogg",length=2.587}}
+L["every minute of every day of my life"]={{path="chatsounds/autoadd/drphil/every minute of every day of my life.ogg",length=2.705}}
+L["gee golly what"]={{path="chatsounds/autoadd/drphil/gee golly what.ogg",length=1.130}}
 L["go kill yourself go sit in the middle of the road and let a car run over you"]={{path="chatsounds/autoadd/drphil/go kill yourself go sit in the middle of the road and let a car run over you.ogg",length=4.277}}
+L["hemorrhoids"]={{path="chatsounds/autoadd/drphil/hemorrhoids.ogg",length=0.870}}
+L["honest to god"]={{path="chatsounds/autoadd/drphil/honest to god.ogg",length=1.092}}
+L["how bout this"]={{path="chatsounds/autoadd/drphil/how bout this.ogg",length=0.723}}
+L["how did i find two such gullible women"]={{path="chatsounds/autoadd/drphil/how did i find two such gullible women.ogg",length=2.800}}
+L["how much do you know about alternative explanations"]={{path="chatsounds/autoadd/drphil/how much do you know about alternative explanations.ogg",length=4.957}}
+L["i am not a fool"]={{path="chatsounds/autoadd/drphil/i am not a fool.ogg",length=4.003}}
+L["i cant begin to tell you how counterproductive this is"]={{path="chatsounds/autoadd/drphil/i cant begin to tell you how counterproductive this is.ogg",length=5.086}}
+L["i think im being punked"]={{path="chatsounds/autoadd/drphil/i think im being punked.ogg",length=1.447}}
 L["i want the money"]={{path="chatsounds/autoadd/drphil/i want the money.ogg",length=0.920}}
+L["ill give you a few minutes to point out what a worthless bitch she is in a minute okay"]={{path="chatsounds/autoadd/drphil/ill give you a few minutes to point out what a worthless bitch she is in a minute okay.ogg",length=3.643}}
+L["implosion of the rectum from the inside out"]={{path="chatsounds/autoadd/drphil/implosion of the rectum from the inside out.ogg",length=3.294}}
+L["it looks like bullying because it is"]={{path="chatsounds/autoadd/drphil/it looks like bullying because it is.ogg",length=1.784}}
+L["merry christmas"]={{path="chatsounds/autoadd/drphil/merry christmas.ogg",length=0.841}}
+L["my daughters are a couple of spoiled bitches"]={{path="chatsounds/autoadd/drphil/my daughters are a couple of spoiled bitches.ogg",length=2.790}}
+L["my girls are bitches"]={{path="chatsounds/autoadd/drphil/my girls are bitches.ogg",length=1.732}}
 L["nomoreiloveyous"]={{path="chatsounds/autoadd/drphil/nomoreiloveyous.ogg",length=23.429}}
+L["oh my god youre gonna start dismantling my body here"]={{path="chatsounds/autoadd/drphil/oh my god youre gonna start dismantling my body here.ogg",length=3.073}}
+L["oh no forgot"]={{path="chatsounds/autoadd/drphil/oh no forgot.ogg",length=1.490}}
+L["okay"]={{path="chatsounds/autoadd/drphil/okay.ogg",length=0.498}}
+L["one bald guy to another"]={{path="chatsounds/autoadd/drphil/one bald guy to another.ogg",length=1.548}}
+L["right"]={{path="chatsounds/autoadd/drphil/right.ogg",length=0.385}}
+L["seriously"]={{path="chatsounds/autoadd/drphil/seriously/1.ogg",length=1.571},{path="chatsounds/autoadd/drphil/seriously/2.ogg",length=1.494}}
+L["sexual abuse"]={{path="chatsounds/autoadd/drphil/sexual abuse.ogg",length=0.972}}
 L["shut the hell up bitch"]={{path="chatsounds/autoadd/drphil/shut the hell up bitch.ogg",length=1.992}}
+L["so who do you get along with"]={{path="chatsounds/autoadd/drphil/so who do you get along with.ogg",length=2.178}}
+L["spoiled bitches"]={{path="chatsounds/autoadd/drphil/spoiled bitches.ogg",length=1.328}}
+L["subsequent encounter"]={{path="chatsounds/autoadd/drphil/subsequent encounter.ogg",length=1.359}}
+L["that should be a huuuge wake up call"]={{path="chatsounds/autoadd/drphil/that should be a huuuge wake up call.ogg",length=2.520}}
+L["thats how it is in the code"]={{path="chatsounds/autoadd/drphil/thats how it is in the code.ogg",length=1.762}}
+L["theyre laughing"]={{path="chatsounds/autoadd/drphil/theyre laughing.ogg",length=1.183}}
 L["uh ill raise enough hell til you give in and give it to me"]={{path="chatsounds/autoadd/drphil/uh ill raise enough hell til you give in and give it to me.ogg",length=3.377}}
+L["vaginal violation"]={{path="chatsounds/autoadd/drphil/vaginal violation.ogg",length=1.562}}
+L["vaginitis"]={{path="chatsounds/autoadd/drphil/vaginitis.ogg",length=1.034}}
+L["well i googled ya house"]={{path="chatsounds/autoadd/drphil/well i googled ya house.ogg",length=1.273}}
+L["what do you think about that"]={{path="chatsounds/autoadd/drphil/what do you think about that.ogg",length=1.971}}
+L["what the hell are you people thinking"]={{path="chatsounds/autoadd/drphil/what the hell are you people thinking.ogg",length=1.839}}
+L["what would be your definition of negligence"]={{path="chatsounds/autoadd/drphil/what would be your definition of negligence.ogg",length=2.381}}
 L["whats funny about that"]={{path="chatsounds/autoadd/drphil/whats funny about that.ogg",length=1.078}}
+L["yeah bulletin"]={{path="chatsounds/autoadd/drphil/yeah bulletin.ogg",length=1.310}}
+L["yeah can get a little frustrating"]={{path="chatsounds/autoadd/drphil/yeah can get a little frustrating.ogg",length=1.141}}
+L["yeah really"]={{path="chatsounds/autoadd/drphil/yeah really.ogg",length=1.132}}
+L["yes you are actually"]={{path="chatsounds/autoadd/drphil/yes you are actually.ogg",length=1.158}}
+L["you do think an exocism is the answer here"]={{path="chatsounds/autoadd/drphil/you do think an exocism is the answer here.ogg",length=3.143}}
+L["you dont seem to understand"]={{path="chatsounds/autoadd/drphil/you dont seem to understand.ogg",length=2.050}}
+L["you got your cake and eat it too"]={{path="chatsounds/autoadd/drphil/you got your cake and eat it too.ogg",length=1.631}}
+L["you have part of your intestines removed"]={{path="chatsounds/autoadd/drphil/you have part of your intestines removed.ogg",length=2.558}}
 L["youre an alcoholic"]={{path="chatsounds/autoadd/drphil/youre an alcoholic.ogg",length=1.163}}
+L["youre bullying that boy"]={{path="chatsounds/autoadd/drphil/youre bullying that boy.ogg",length=1.338}}
+L["youre gonna now start dismantling me"]={{path="chatsounds/autoadd/drphil/youre gonna now start dismantling me.ogg",length=2.082}}
+L["youre not picking up the kids youre drunk"]={{path="chatsounds/autoadd/drphil/youre not picking up the kids youre drunk.ogg",length=1.833}}
 L["youre ugly youre disgusting im gonna kill you"]={{path="chatsounds/autoadd/drphil/youre ugly youre disgusting im gonna kill you.ogg",length=2.876}}
 c.EndList()
