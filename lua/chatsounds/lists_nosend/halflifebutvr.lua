29a874be2aedd280281e7a8dac7be907625dd2fa
Mikeai <mgamkrel@gmail.com>
2020-04-03 12:50:37 +0000

    Look gordon, ropes! (Part 2/?)
    Yeah i've got no idea when im gonna do the next bit

--- lua/chatsounds/lists_nosend/halflifebutvr.lua
+++ lua/chatsounds/lists_nosend/halflifebutvr.lua
@@ -2,10 +2,18 @@ c.StartList("halflifebutvr")
 L["another clone defeated"]={{path="chatsounds/autoadd/halflifebutvr/another clone defeated.ogg",length=2.103}}
 L["can i see your passport"]={{path="chatsounds/autoadd/halflifebutvr/can i see your passport.ogg",length=1.233}}
 L["can you believe this guy doesnt have his fuckin passport"]={{path="chatsounds/autoadd/halflifebutvr/can you believe this guy doesnt have his fuckin passport.ogg",length=2.938}}
+L["correct"]={{path="chatsounds/autoadd/halflifebutvr/correct.ogg",length=0.786}}
+L["did someone say ropes"]={{path="chatsounds/autoadd/halflifebutvr/did someone say ropes.ogg",length=1.269}}
+L["dont let my bloody form frighten you gordon"]={{path="chatsounds/autoadd/halflifebutvr/dont let my bloody form frighten you gordon.ogg",length=2.752}}
+L["dont worry gordon i lost the ability to feel pain three years ago"]={{path="chatsounds/autoadd/halflifebutvr/dont worry gordon i lost the ability to feel pain three years ago.ogg",length=4.692}}
+L["everywhere gordon"]={{path="chatsounds/autoadd/halflifebutvr/everywhere gordon.ogg",length=2.062}}
 L["feed me my coins mr freeman"]={{path="chatsounds/autoadd/halflifebutvr/feed me my coins mr freeman.ogg",length=5.906}}
 L["goddamnit im sorry for saying that but what are you doing"]={{path="chatsounds/autoadd/halflifebutvr/goddamnit im sorry for saying that but what are you doing.ogg",length=3.042}}
 L["good thing i have the rope"]={{path="chatsounds/autoadd/halflifebutvr/good thing i have the rope.ogg",length=1.564}}
 L["gordon be careful there are explosives here"]={{path="chatsounds/autoadd/halflifebutvr/gordon be careful there are explosives here.ogg",length=2.502}}
+L["gordon be careful theres a big hole"]={{path="chatsounds/autoadd/halflifebutvr/gordon be careful theres a big hole.ogg",length=2.191}}
+L["gordon every cell in my body is in excruciating pain"]={{path="chatsounds/autoadd/halflifebutvr/gordon every cell in my body is in excruciating pain.ogg",length=4.104}}
+L["gordon for one playcoin i can answer your question"]={{path="chatsounds/autoadd/halflifebutvr/gordon for one playcoin i can answer your question.ogg",length=3.159}}
 L["he doesnt have his passport"]={{path="chatsounds/autoadd/halflifebutvr/he doesnt have his passport.ogg",length=1.358}}
 L["he owed me five"]={{path="chatsounds/autoadd/halflifebutvr/he owed me five.ogg",length=1.357}}
 L["hello gordon"]={{path="chatsounds/autoadd/halflifebutvr/hello gordon.ogg",length=0.961}}
@@ -17,33 +25,59 @@ L["i believe this hole is about five hundred feet deep"]={{path="chatsounds/auto
 L["i dont have my passport"]={{path="chatsounds/autoadd/halflifebutvr/i dont have my passport.ogg",length=1.244}}
 L["i dont think you can live with that"]={{path="chatsounds/autoadd/halflifebutvr/i dont think you can live with that.ogg",length=1.542}}
 L["i feel normal"]={{path="chatsounds/autoadd/halflifebutvr/i feel normal.ogg",length=1.450}}
+L["i hate trains"]={{path="chatsounds/autoadd/halflifebutvr/i hate trains.ogg",length=1.607}}
 L["i like it there shut up"]={{path="chatsounds/autoadd/halflifebutvr/i like it there shut up.ogg",length=1.309}}
+L["i read it on wikipedia the free online encyclopedia that anyone can edit"]={{path="chatsounds/autoadd/halflifebutvr/i read it on wikipedia the free online encyclopedia that anyone can edit.ogg",length=5.150}}
+L["i think im going to die gordon"]={{path="chatsounds/autoadd/halflifebutvr/i think im going to die gordon.ogg",length=2.084}}
 L["i think you fucked it up"]={{path="chatsounds/autoadd/halflifebutvr/i think you fucked it up.ogg",length=1.647}}
+L["i told you"]={{path="chatsounds/autoadd/halflifebutvr/i told you.ogg",length=1.161}}
+L["id love to hear it gordon"]={{path="chatsounds/autoadd/halflifebutvr/id love to hear it gordon.ogg",length=1.607}}
 L["if we use our ropes we can easily traverse this area"]={{path="chatsounds/autoadd/halflifebutvr/if we use our ropes we can easily traverse this area.ogg",length=2.914}}
 L["im going to go fire a rocket"]={{path="chatsounds/autoadd/halflifebutvr/im going to go fire a rocket.ogg",length=1.763}}
 L["im the alpha male now"]={{path="chatsounds/autoadd/halflifebutvr/im the alpha male now.ogg",length=2.103}}
+L["im very intelligent thank you"]={{path="chatsounds/autoadd/halflifebutvr/im very intelligent thank you.ogg",length=2.273}}
+L["it was a little bit suspicious now that i think about it gordon"]={{path="chatsounds/autoadd/halflifebutvr/it was a little bit suspicious now that i think about it gordon.ogg",length=2.912}}
 L["its just brain cancer you can live with that"]={{path="chatsounds/autoadd/halflifebutvr/its just brain cancer you can live with that.ogg",length=2.289}}
+L["its me your friend dr coomer"]={{path="chatsounds/autoadd/halflifebutvr/its me your friend dr coomer.ogg",length=2.555}}
 L["look gordon"]={{path="chatsounds/autoadd/halflifebutvr/look gordon.ogg",length=0.829}}
 L["look gordon beasts"]={{path="chatsounds/autoadd/halflifebutvr/look gordon beasts.ogg",length=1.564}}
 L["look gordon ropes"]={{path="chatsounds/autoadd/halflifebutvr/look gordon ropes.ogg",length=1.657}}
+L["look gordon we made it to the surface distorted"]={{path="chatsounds/autoadd/halflifebutvr/look gordon we made it to the surface distorted.ogg",length=2.520}}
+L["look out gordon hotted boobs up ahead tits big ones"]={{path="chatsounds/autoadd/halflifebutvr/look out gordon hotted boobs up ahead tits big ones.ogg",length=3.529}}
 L["my clone"]={{path="chatsounds/autoadd/halflifebutvr/my clone.ogg",length=1.039}}
 L["my god gordon you just killed a man"]={{path="chatsounds/autoadd/halflifebutvr/my god gordon you just killed a man.ogg",length=2.091}}
 L["my power grows gordon"]={{path="chatsounds/autoadd/halflifebutvr/my power grows gordon.ogg",length=1.677}}
+L["never fear gordon it would be excruciatingly painful and long"]={{path="chatsounds/autoadd/halflifebutvr/never fear gordon it would be excruciatingly painful and long.ogg",length=4.289}}
+L["now gordon its only safe to warn you i felt everything they felt"]={{path="chatsounds/autoadd/halflifebutvr/now gordon its only safe to warn you i felt everything they felt.ogg",length=3.716}}
 L["now gordon you may have killed three innocent lives but its important to stay calm"]={{path="chatsounds/autoadd/halflifebutvr/now gordon you may have killed three innocent lives but its important to stay calm.ogg",length=4.127}}
 L["ofcourse i have my passport"]={{path="chatsounds/autoadd/halflifebutvr/ofcourse i have my passport.ogg",length=2.064}}
 L["oh im fine now"]={{path="chatsounds/autoadd/halflifebutvr/oh im fine now.ogg",length=1.535}}
+L["oh im quite aware"]={{path="chatsounds/autoadd/halflifebutvr/oh im quite aware.ogg",length=1.487}}
+L["oh my god i think his brain overloaded"]={{path="chatsounds/autoadd/halflifebutvr/oh my god i think his brain overloaded.ogg",length=1.806}}
 L["okay thats not your name"]={{path="chatsounds/autoadd/halflifebutvr/okay thats not your name.ogg",length=1.210}}
 L["perfectly justified"]={{path="chatsounds/autoadd/halflifebutvr/perfectly justified.ogg",length=1.110}}
 L["ropes can be used to traverse pits gordon"]={{path="chatsounds/autoadd/halflifebutvr/ropes can be used to traverse pits gordon.ogg",length=2.582}}
 L["see what gordon"]={{path="chatsounds/autoadd/halflifebutvr/see what gordon.ogg",length=1.238}}
 L["self defense gordon"]={{path="chatsounds/autoadd/halflifebutvr/self defense gordon.ogg",length=1.185}}
+L["surprise attack gordon"]={{path="chatsounds/autoadd/halflifebutvr/surprise attack gordon.ogg",length=2.039}}
+L["take that you damnable bitch"]={{path="chatsounds/autoadd/halflifebutvr/take that you damnable bitch.ogg",length=2.056}}
+L["that was pretty cool i will say"]={{path="chatsounds/autoadd/halflifebutvr/that was pretty cool i will say.ogg",length=1.850}}
+L["the hazardous environment suit was designed to never come off"]={{path="chatsounds/autoadd/halflifebutvr/the hazardous environment suit was designed to never come off.ogg",length=3.569}}
+L["then we kill everyone and take it for ourselves"]={{path="chatsounds/autoadd/halflifebutvr/then we kill everyone and take it for ourselves.ogg",length=1.959}}
+L["theres nothing there"]={{path="chatsounds/autoadd/halflifebutvr/theres nothing there.ogg",length=1.843}}
+L["they cant do that killing people is illegal"]={{path="chatsounds/autoadd/halflifebutvr/they cant do that killing people is illegal.ogg",length=2.557}}
 L["they didnt have their passport"]={{path="chatsounds/autoadd/halflifebutvr/they didnt have their passport.ogg",length=1.909}}
 L["this is my job dude"]={{path="chatsounds/autoadd/halflifebutvr/this is my job dude.ogg",length=1.751}}
 L["we can use these to help with pits"]={{path="chatsounds/autoadd/halflifebutvr/we can use these to help with pits.ogg",length=1.730}}
 L["were trying to dig to the center of the earth"]={{path="chatsounds/autoadd/halflifebutvr/were trying to dig to the center of the earth.ogg",length=2.611}}
+L["who the fuck knocked out the lights"]={{path="chatsounds/autoadd/halflifebutvr/who the fuck knocked out the lights.ogg",length=1.639}}
 L["why are you yelling at me"]={{path="chatsounds/autoadd/halflifebutvr/why are you yelling at me.ogg",length=1.334}}
 L["yeah im sorry were kinda fucked"]={{path="chatsounds/autoadd/halflifebutvr/yeah im sorry were kinda fucked.ogg",length=1.144}}
+L["yeah they had their passport what was i supposed to do"]={{path="chatsounds/autoadd/halflifebutvr/yeah they had their passport what was i supposed to do.ogg",length=2.316}}
+L["you are a fucking anomaly"]={{path="chatsounds/autoadd/halflifebutvr/you are a fucking anomaly.ogg",length=2.668}}
 L["you could have helped him gordon"]={{path="chatsounds/autoadd/halflifebutvr/you could have helped him gordon.ogg",length=1.303}}
 L["you die if you go in here"]={{path="chatsounds/autoadd/halflifebutvr/you die if you go in here.ogg",length=1.425}}
 L["you owe me three dollars"]={{path="chatsounds/autoadd/halflifebutvr/you owe me three dollars.ogg",length=2.076}}
+L["you sound like advertisements"]={{path="chatsounds/autoadd/halflifebutvr/you sound like advertisements.ogg",length=1.565}}
+L["you thought wrong my good bitch"]={{path="chatsounds/autoadd/halflifebutvr/you thought wrong my good bitch.ogg",length=1.695}}
 c.EndList()
