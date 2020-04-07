75504ed1e74697c67035ddfc719d4bfac2dc7fed
Mikeai <mgamkrel@gmail.com>
2020-04-07 17:59:31 +0000

    You're going to SUCK
    Return of an old classic

---
+++ lua/chatsounds/lists_nosend/goingtosuck.lua
@@ -0,0 +1,16 @@
+c.StartList("goingtosuck")
+L["breugh"]={{path="chatsounds/autoadd/goingtosuck/breugh.ogg",length=0.930}}
+L["clean"]={{path="chatsounds/autoadd/goingtosuck/clean.ogg",length=1.116}}
+L["cock"]={{path="chatsounds/autoadd/goingtosuck/cock.ogg",length=0.748}}
+L["right now"]={{path="chatsounds/autoadd/goingtosuck/right now.ogg",length=1.201}}
+L["stop looking suck my cock"]={{path="chatsounds/autoadd/goingtosuck/stop looking suck my cock.ogg",length=2.436}}
+L["suck"]={{path="chatsounds/autoadd/goingtosuck/suck.ogg",length=0.701}}
+L["this"]={{path="chatsounds/autoadd/goingtosuck/this.ogg",length=0.662}}
+L["this cock"]={{path="chatsounds/autoadd/goingtosuck/this cock.ogg",length=1.216}}
+L["yeees"]={{path="chatsounds/autoadd/goingtosuck/yeees.ogg",length=1.197}}
+L["yees"]={{path="chatsounds/autoadd/goingtosuck/yees.ogg",length=1.259}}
+L["yes"]={{path="chatsounds/autoadd/goingtosuck/yes.ogg",length=0.825}}
+L["you gonna suck this cock good"]={{path="chatsounds/autoadd/goingtosuck/you gonna suck this cock good.ogg",length=2.169}}
+L["youre gonna suck"]={{path="chatsounds/autoadd/goingtosuck/youre gonna suck.ogg",length=1.166}}
+L["youre gonna suck this cock clean"]={{path="chatsounds/autoadd/goingtosuck/youre gonna suck this cock clean.ogg",length=3.672}}
+c.EndList()
