dadbe4ad766ec8750a6638c40f3ca261c7141d17
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-04-07 01:32:42 +0000

    chatsounds preprocessor (3e46541f6c9de496e7675863bd36f8f2e8a6680e)
    [ci skip]

---
+++ lua/chatsounds/lists_nosend/greatest_freak_out.lua
@@ -0,0 +1,44 @@
+c.StartList("greatest_freak_out")
+L["dude freaking wow"]={{path="chatsounds/autoadd/greatest_freak_out/dude freaking wow.ogg",length=3.313}}
+L["dude okay"]={{path="chatsounds/autoadd/greatest_freak_out/dude okay.ogg",length=1.687}}
+L["every day"]={{path="chatsounds/autoadd/greatest_freak_out/every day.ogg",length=1.424}}
+L["every time you kill me it says betrayed you retard"]={{path="chatsounds/autoadd/greatest_freak_out/every time you kill me it says betrayed you retard.ogg",length=3.384}}
+L["freakin stooop"]={{path="chatsounds/autoadd/greatest_freak_out/freakin stooop.ogg",length=1.899}}
+L["freaking whore"]={{path="chatsounds/autoadd/greatest_freak_out/freaking whore.ogg",length=1.074}}
+L["get off now"]={{path="chatsounds/autoadd/greatest_freak_out/get off now.ogg",length=1.439}}
+L["go away"]={{path="chatsounds/autoadd/greatest_freak_out/go away.ogg",length=0.857}}
+L["he keeps betraying me"]={{path="chatsounds/autoadd/greatest_freak_out/he keeps betraying me.ogg",length=4.343}}
+L["he keeps betraying me he keeps betraying me"]={{path="chatsounds/autoadd/greatest_freak_out/he keeps betraying me he keeps betraying me.ogg",length=3.793}}
+L["how come youre not laughing now"]={{path="chatsounds/autoadd/greatest_freak_out/how come youre not laughing now.ogg",length=1.788}}
+L["i am sick of listening to this"]={{path="chatsounds/autoadd/greatest_freak_out/i am sick of listening to this.ogg",length=1.656}}
+L["i dont care if he finds out"]={{path="chatsounds/autoadd/greatest_freak_out/i dont care if he finds out.ogg",length=1.550}}
+L["i dont care what you think"]={{path="chatsounds/autoadd/greatest_freak_out/i dont care what you think.ogg",length=2.018}}
+L["i will find you"]={{path="chatsounds/autoadd/greatest_freak_out/i will find you.ogg",length=1.697}}
+L["i will freaking find you and i will kill you"]={{path="chatsounds/autoadd/greatest_freak_out/i will freaking find you and i will kill you.ogg",length=3.292}}
+L["i will kill you"]={{path="chatsounds/autoadd/greatest_freak_out/i will kill you.ogg",length=1.333}}
+L["i will kill you and eat your first born child"]={{path="chatsounds/autoadd/greatest_freak_out/i will kill you and eat your first born child.ogg",length=5.707}}
+L["im gonna kill you i will kill you"]={{path="chatsounds/autoadd/greatest_freak_out/im gonna kill you i will kill you.ogg",length=5.724}}
+L["im not afraid of him"]={{path="chatsounds/autoadd/greatest_freak_out/im not afraid of him.ogg",length=1.893}}
+L["it takes off points for the freakin team"]={{path="chatsounds/autoadd/greatest_freak_out/it takes off points for the freakin team.ogg",length=2.364}}
+L["my god you have no freaking life"]={{path="chatsounds/autoadd/greatest_freak_out/my god you have no freaking life.ogg",length=3.309}}
+L["noo i dont wanna get off"]={{path="chatsounds/autoadd/greatest_freak_out/noo i dont wanna get off.ogg",length=1.980}}
+L["nooooh"]={{path="chatsounds/autoadd/greatest_freak_out/nooooh/1.ogg",length=1.465},{path="chatsounds/autoadd/greatest_freak_out/nooooh/2.ogg",length=1.151}}
+L["oh my freaking god"]={{path="chatsounds/autoadd/greatest_freak_out/oh my freaking god.ogg",length=1.957}}
+L["oh my god"]={{path="chatsounds/autoadd/greatest_freak_out/oh my god/1.ogg",length=1.960},{path="chatsounds/autoadd/greatest_freak_out/oh my god/2.ogg",length=1.384},{path="chatsounds/autoadd/greatest_freak_out/oh my god/3.ogg",length=1.343},{path="chatsounds/autoadd/greatest_freak_out/oh my god/4.ogg",length=1.667}}
+L["shut uuup im better than you freaking whore"]={{path="chatsounds/autoadd/greatest_freak_out/shut uuup im better than you freaking whore.ogg",length=3.520}}
+L["shuuut uuup"]={{path="chatsounds/autoadd/greatest_freak_out/shuuut uuup.ogg",length=2.570}}
+L["stephen that is enough"]={{path="chatsounds/autoadd/greatest_freak_out/stephen that is enough.ogg",length=1.939}}
+L["stephenguitarloop"]={{path="chatsounds/autoadd/greatest_freak_out/stephenguitarloop.ogg",length=2.701}}
+L["stooop"]={{path="chatsounds/autoadd/greatest_freak_out/stooop/1.ogg",length=1.283},{path="chatsounds/autoadd/greatest_freak_out/stooop/2.ogg",length=1.566},{path="chatsounds/autoadd/greatest_freak_out/stooop/3.ogg",length=1.343},{path="chatsounds/autoadd/greatest_freak_out/stooop/4.ogg",length=1.576}}
+L["stop betraying me"]={{path="chatsounds/autoadd/greatest_freak_out/stop betraying me.ogg",length=2.414}}
+L["stop freaking betraying me"]={{path="chatsounds/autoadd/greatest_freak_out/stop freaking betraying me.ogg",length=2.667}}
+L["stop freaking betraying me im on your team"]={{path="chatsounds/autoadd/greatest_freak_out/stop freaking betraying me im on your team.ogg",length=3.818}}
+L["what"]={{path="chatsounds/autoadd/greatest_freak_out/what.ogg",length=1.030}}
+L["why dont you yell at him"]={{path="chatsounds/autoadd/greatest_freak_out/why dont you yell at him/1.ogg",length=1.535},{path="chatsounds/autoadd/greatest_freak_out/why dont you yell at him/2.ogg",length=1.444}}
+L["yeah i killed you dead now"]={{path="chatsounds/autoadd/greatest_freak_out/yeah i killed you dead now.ogg",length=1.889}}
+L["yeah why dont you yell at him"]={{path="chatsounds/autoadd/greatest_freak_out/yeah why dont you yell at him.ogg",length=1.495}}
+L["yell at him"]={{path="chatsounds/autoadd/greatest_freak_out/yell at him.ogg",length=1.424}}
+L["you freaking like that yeah"]={{path="chatsounds/autoadd/greatest_freak_out/you freaking like that yeah.ogg",length=1.515}}
+L["you suck at guitar"]={{path="chatsounds/autoadd/greatest_freak_out/you suck at guitar.ogg",length=2.123}}
+L["youre freaking retarded"]={{path="chatsounds/autoadd/greatest_freak_out/youre freaking retarded.ogg",length=1.869}}
+c.EndList()
