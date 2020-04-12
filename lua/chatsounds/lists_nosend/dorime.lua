397b0211a08dd8b2edb2687db139c6da69f2c67a
Mikeai <mgamkrel@gmail.com>
2020-04-12 14:49:16 +0000

    Dorime

---
+++ lua/chatsounds/lists_nosend/dorime.lua
@@ -0,0 +1,9 @@
+c.StartList("dorime")
+L["ameno"]={{path="chatsounds/autoadd/dorime/ameno.ogg",length=2.468}}
+L["ameno ameno"]={{path="chatsounds/autoadd/dorime/ameno ameno.ogg",length=2.318}}
+L["dorime"]={{path="chatsounds/autoadd/dorime/dorime.ogg",length=1.551}}
+L["interimo adapare dorime"]={{path="chatsounds/autoadd/dorime/interimo adapare dorime.ogg",length=4.018}}
+L["latire"]={{path="chatsounds/autoadd/dorime/latire.ogg",length=1.784}}
+L["latiremo"]={{path="chatsounds/autoadd/dorime/latiremo.ogg",length=2.368}}
+L["omanare imperavi ameno"]={{path="chatsounds/autoadd/dorime/omanare imperavi ameno.ogg",length=4.435}}
+c.EndList()
