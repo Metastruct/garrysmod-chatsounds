1eafa8d0c1bcaad2693aaebd2fd96bb75f952710
Mikeai <mgamkrel@gmail.com>
2020-04-30 18:38:46 +0100

    Cock and ball torture, Doctor Freeman

---
+++ lua/chatsounds/lists_nosend/gmancbt.lua
@@ -0,0 +1,6 @@
+c.StartList("gmancbt")
+L["cock and ball torture doctor freeman"]={{path="chatsounds/autoadd/gmancbt/cock and ball torture doctor freeman.ogg",length=3.386}}
+L["cock and ball torture or cbt"]={{path="chatsounds/autoadd/gmancbt/cock and ball torture or cbt.ogg",length=3.005}}
+L["or constriction to the penis or testicles"]={{path="chatsounds/autoadd/gmancbt/or constriction to the penis or testicles.ogg",length=5.766}}
+L["penis torture or dick torture is a sexual activity involving application of pain"]={{path="chatsounds/autoadd/gmancbt/penis torture or dick torture is a sexual activity involving application of pain.ogg",length=6.582}}
+c.EndList()
