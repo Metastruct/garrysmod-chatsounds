e7e469c80c53bca0c1177e6204ad67908078f95e
Elias Hogstvedt <eliashogstvedt@gmail.com>
2020-05-10 18:21:04 +0200

    Revert "Removal of unused sounds in different languages"
    This reverts commit 32fa10d16237f51c02f7b6ce1c4d8267f8a527f0.

---
+++ lua/chatsounds/lists_nosend/massagraf.lua
@@ -0,0 +1,92 @@
+c.StartList("massagraf")
+L["aaah burgemee aa"]={{path="chatsounds/autoadd/massagraf/aaah burgemee aa.ogg",length=2.231}}
+L["ah neen he"]={{path="chatsounds/autoadd/massagraf/ah neen he.ogg",length=1.406}}
+L["alehop"]={{path="chatsounds/autoadd/massagraf/alehop.ogg",length=0.492}}
+L["als dat maar goed afloopt"]={{path="chatsounds/autoadd/massagraf/als dat maar goed afloopt.ogg",length=1.410}}
+L["bij aankoop van een bloemkool krijg je gratis snoep"]={{path="chatsounds/autoadd/massagraf/bij aankoop van een bloemkool krijg je gratis snoep.ogg",length=3.457}}
+L["bosdwergsqueak"]={{path="chatsounds/autoadd/massagraf/bosdwergsqueak.ogg",length=2.593}}
+L["burgemeesterheaddesk"]={{path="chatsounds/autoadd/massagraf/burgemeesterheaddesk.ogg",length=1.015}}
+L["burgemeesterskate"]={{path="chatsounds/autoadd/massagraf/burgemeesterskate.ogg",length=1.975}}
+L["daar is adolf hitler met heineken bier"]={{path="chatsounds/autoadd/massagraf/daar is adolf hitler met heineken bier.ogg",length=2.909}}
+L["dag lieve ploppertjes allemaal welkom op youtube"]={{path="chatsounds/autoadd/massagraf/dag lieve ploppertjes allemaal welkom op youtube.ogg",length=4.518}}
+L["dag poepertjes allemaal"]={{path="chatsounds/autoadd/massagraf/dag poepertjes allemaal.ogg",length=1.879}}
+L["dat heeft klusje weer eens mooi geregeld zie"]={{path="chatsounds/autoadd/massagraf/dat heeft klusje weer eens mooi geregeld zie.ogg",length=1.992}}
+L["dat was weer spanned he kuthond"]={{path="chatsounds/autoadd/massagraf/dat was weer spanned he kuthond.ogg",length=1.471}}
+L["draai maar he dan wordt je mischien wat minder dik"]={{path="chatsounds/autoadd/massagraf/draai maar he dan wordt je mischien wat minder dik.ogg",length=2.665}}
+L["dramadramadrama"]={{path="chatsounds/autoadd/massagraf/dramadramadrama.ogg",length=8.161}}
+L["eh het is een ramp"]={{path="chatsounds/autoadd/massagraf/eh het is een ramp.ogg",length=1.950}}
+L["eh ja goed dan"]={{path="chatsounds/autoadd/massagraf/eh ja goed dan.ogg",length=1.589}}
+L["ehge zeg jij het maar van leemhuyzen"]={{path="chatsounds/autoadd/massagraf/ehge zeg jij het maar van leemhuyzen.ogg",length=1.850}}
+L["en jij"]={{path="chatsounds/autoadd/massagraf/en jij.ogg",length=0.391}}
+L["en jullie weten het je moeder is een plopkoek"]={{path="chatsounds/autoadd/massagraf/en jullie weten het je moeder is een plopkoek.ogg",length=3.043}}
+L["er klopt iemand op de deur"]={{path="chatsounds/autoadd/massagraf/er klopt iemand op de deur.ogg",length=1.009}}
+L["evil klus"]={{path="chatsounds/autoadd/massagraf/evil klus.ogg",length=1.150}}
+L["ga jij naar van leemhuyzen en zeg maar dat ik eraan kom"]={{path="chatsounds/autoadd/massagraf/ga jij naar van leemhuyzen en zeg maar dat ik eraan kom.ogg",length=1.951}}
+L["gertje gertje"]={{path="chatsounds/autoadd/massagraf/gertje gertje.ogg",length=1.950}}
+L["gertje gertje gertje gertje"]={{path="chatsounds/autoadd/massagraf/gertje gertje gertje gertje.ogg",length=1.526}}
+L["gertje gertje nee niet doen"]={{path="chatsounds/autoadd/massagraf/gertje gertje nee niet doen.ogg",length=1.701}}
+L["gertje kom mij toch helpen"]={{path="chatsounds/autoadd/massagraf/gertje kom mij toch helpen.ogg",length=2.073}}
+L["gertje zit nog altijd te"]={{path="chatsounds/autoadd/massagraf/gertje zit nog altijd te.ogg",length=1.816}}
+L["gertjee"]={{path="chatsounds/autoadd/massagraf/gertjee.ogg",length=0.975}}
+L["gertjeeee"]={{path="chatsounds/autoadd/massagraf/gertjeeee.ogg",length=0.958}}
+L["gertson ik ga braken"]={{path="chatsounds/autoadd/massagraf/gertson ik ga braken.ogg",length=1.967}}
+L["gertson1"]={{path="chatsounds/autoadd/massagraf/gertson1.ogg",length=0.958}}
+L["goed zo"]={{path="chatsounds/autoadd/massagraf/goed zo.ogg",length=0.429}}
+L["gratis snoep"]={{path="chatsounds/autoadd/massagraf/gratis snoep.ogg",length=1.129}}
+L["he wat was dat"]={{path="chatsounds/autoadd/massagraf/he wat was dat.ogg",length=0.873}}
+L["hebben jullie zin in een dansje"]={{path="chatsounds/autoadd/massagraf/hebben jullie zin in een dansje.ogg",length=1.312}}
+L["het is een grote lama"]={{path="chatsounds/autoadd/massagraf/het is een grote lama.ogg",length=2.428}}
+L["het is een ramp"]={{path="chatsounds/autoadd/massagraf/het is een ramp.ogg",length=2.358}}
+L["het lijden van de vlaamse bosdwerg theme"]={{path="chatsounds/autoadd/massagraf/het lijden van de vlaamse bosdwerg theme.ogg",length=14.013}}
+L["hey gertson"]={{path="chatsounds/autoadd/massagraf/hey gertson.ogg",length=1.303}}
+L["hij is worst"]={{path="chatsounds/autoadd/massagraf/hij is worst.ogg",length=1.312}}
+L["hou jij maar op met je flauwe kut"]={{path="chatsounds/autoadd/massagraf/hou jij maar op met je flauwe kut.ogg",length=1.129}}
+L["ik begin dat zo beu te worden"]={{path="chatsounds/autoadd/massagraf/ik begin dat zo beu te worden.ogg",length=1.550}}
+L["ik ben een dief"]={{path="chatsounds/autoadd/massagraf/ik ben een dief.ogg",length=9.500}}
+L["ik ben een robot"]={{path="chatsounds/autoadd/massagraf/ik ben een robot.ogg",length=1.541}}
+L["ik ben hier voor hem"]={{path="chatsounds/autoadd/massagraf/ik ben hier voor hem.ogg",length=1.226}}
+L["ik ben sexy"]={{path="chatsounds/autoadd/massagraf/ik ben sexy.ogg",length=0.782}}
+L["ik ben woest"]={{path="chatsounds/autoadd/massagraf/ik ben woest.ogg",length=0.701}}
+L["ik geef het up he piep"]={{path="chatsounds/autoadd/massagraf/ik geef het up he piep.ogg",length=1.999}}
+L["ik heb buikpijn"]={{path="chatsounds/autoadd/massagraf/ik heb buikpijn.ogg",length=1.077}}
+L["ik merkte niet dat klui en lus mijn melkherberg binnenkwamen"]={{path="chatsounds/autoadd/massagraf/ik merkte niet dat klui en lus mijn melkherberg binnenkwamen.ogg",length=4.324}}
+L["ik wil ook graag rijk worden"]={{path="chatsounds/autoadd/massagraf/ik wil ook graag rijk worden.ogg",length=1.866}}
+L["is er iets"]={{path="chatsounds/autoadd/massagraf/is er iets.ogg",length=0.643}}
+L["ja als u zo hard"]={{path="chatsounds/autoadd/massagraf/ja als u zo hard.ogg",length=0.923}}
+L["ja het is waar"]={{path="chatsounds/autoadd/massagraf/ja het is waar.ogg",length=1.286}}
+L["ja ja ja"]={{path="chatsounds/autoadd/massagraf/ja ja ja.ogg",length=0.974}}
+L["ja ja zeg dat was me wat"]={{path="chatsounds/autoadd/massagraf/ja ja zeg dat was me wat.ogg",length=1.652}}
+L["ja zeg als jij niet draait dan krijg jij geen 5 taarten he"]={{path="chatsounds/autoadd/massagraf/ja zeg als jij niet draait dan krijg jij geen 5 taarten he.ogg",length=3.565}}
+L["je moeder is een plopkoek"]={{path="chatsounds/autoadd/massagraf/je moeder is een plopkoek.ogg",length=1.438}}
+L["jij zo dom gertje"]={{path="chatsounds/autoadd/massagraf/jij zo dom gertje.ogg",length=1.219}}
+L["juist ja"]={{path="chatsounds/autoadd/massagraf/juist ja.ogg",length=0.509}}
+L["kabouter p in da house theme"]={{path="chatsounds/autoadd/massagraf/kabouter p in da house theme.ogg",length=26.765}}
+L["komaan naar de kelder"]={{path="chatsounds/autoadd/massagraf/komaan naar de kelder.ogg",length=0.850}}
+L["maar ik zij het nog tegen hem wat een slecht idee om"]={{path="chatsounds/autoadd/massagraf/maar ik zij het nog tegen hem wat een slecht idee om.ogg",length=2.298}}
+L["maar natuurlijk"]={{path="chatsounds/autoadd/massagraf/maar natuurlijk.ogg",length=0.738}}
+L["maar nee gertje ik wil niet naar de kelder"]={{path="chatsounds/autoadd/massagraf/maar nee gertje ik wil niet naar de kelder.ogg",length=3.835}}
+L["maar nu zullen jullie wel denken waarom ben ik mislukt"]={{path="chatsounds/autoadd/massagraf/maar nu zullen jullie wel denken waarom ben ik mislukt.ogg",length=4.169}}
+L["mag ik mee doen"]={{path="chatsounds/autoadd/massagraf/mag ik mee doen.ogg",length=1.530}}
+L["mah ik zal het zeggen meneer van veelhuyzen"]={{path="chatsounds/autoadd/massagraf/mah ik zal het zeggen meneer van veelhuyzen.ogg",length=3.191}}
+L["mijn geld"]={{path="chatsounds/autoadd/massagraf/mijn geld.ogg",length=0.685}}
+L["moet je nu wat horen"]={{path="chatsounds/autoadd/massagraf/moet je nu wat horen.ogg",length=1.070}}
+L["moh gertje"]={{path="chatsounds/autoadd/massagraf/moh gertje.ogg",length=0.827}}
+L["moh gertje die zit daar zo een beetje heel veel te"]={{path="chatsounds/autoadd/massagraf/moh gertje die zit daar zo een beetje heel veel te.ogg",length=3.258}}
+L["nee samson"]={{path="chatsounds/autoadd/massagraf/nee samson.ogg",length=0.551}}
+L["neen wat zeg ik"]={{path="chatsounds/autoadd/massagraf/neen wat zeg ik.ogg",length=0.913}}
+L["oooh"]={{path="chatsounds/autoadd/massagraf/oooh.ogg",length=0.790}}
+L["oooh ik kom meteen naar het gemeentehuis"]={{path="chatsounds/autoadd/massagraf/oooh ik kom meteen naar het gemeentehuis.ogg",length=2.783}}
+L["piep"]={{path="chatsounds/autoadd/massagraf/piep.ogg",length=0.266}}
+L["plop lahal"]={{path="chatsounds/autoadd/massagraf/plop lahal.ogg",length=0.927}}
+L["plop scream"]={{path="chatsounds/autoadd/massagraf/plop scream.ogg",length=2.071}}
+L["samson flikker op"]={{path="chatsounds/autoadd/massagraf/samson flikker op.ogg",length=1.252}}
+L["smal ja die die was dood"]={{path="chatsounds/autoadd/massagraf/smal ja die die was dood.ogg",length=2.086}}
+L["van leemhuyzen nu maak je het nog erger dan het al was"]={{path="chatsounds/autoadd/massagraf/van leemhuyzen nu maak je het nog erger dan het al was.ogg",length=2.283}}
+L["wat i wuuuuw"]={{path="chatsounds/autoadd/massagraf/wat i wuuuuw.ogg",length=0.832}}
+L["wat is dat eigenlijk zo"]={{path="chatsounds/autoadd/massagraf/wat is dat eigenlijk zo.ogg",length=1.698}}
+L["wat is er"]={{path="chatsounds/autoadd/massagraf/wat is er.ogg",length=0.472}}
+L["wie kan dat zijn"]={{path="chatsounds/autoadd/massagraf/wie kan dat zijn.ogg",length=5.915}}
+L["wuuw"]={{path="chatsounds/autoadd/massagraf/wuuw.ogg",length=0.417}}
+L["zeg dik ik ben niet dik"]={{path="chatsounds/autoadd/massagraf/zeg dik ik ben niet dik.ogg",length=2.606}}
+L["zeg ik heb bijna geen geld meer"]={{path="chatsounds/autoadd/massagraf/zeg ik heb bijna geen geld meer.ogg",length=2.207}}
+c.EndList()
---
+++ lua/chatsounds/lists_nosend/swiss_dubbing.lua
@@ -0,0 +1,24 @@
+c.StartList("swiss_dubbing")
+L["blas mir a blas ma blas ma jo"]={{path="chatsounds/autoadd/swiss_dubbing/blas mir a blas ma blas ma jo.ogg",length=2.237}}
+L["geil"]={{path="chatsounds/autoadd/swiss_dubbing/geil.ogg",length=0.542}}
+L["hah"]={{path="chatsounds/autoadd/swiss_dubbing/hah/hah1.ogg",length=0.702},{path="chatsounds/autoadd/swiss_dubbing/hah/hah2.ogg",length=0.740},{path="chatsounds/autoadd/swiss_dubbing/hah/hah3.ogg",length=0.737},{path="chatsounds/autoadd/swiss_dubbing/hah/hah4.ogg",length=1.841}}
+L["hah hah hah hah hah hah"]={{path="chatsounds/autoadd/swiss_dubbing/hah hah hah hah hah hah.ogg",length=5.388}}
+L["hah jo"]={{path="chatsounds/autoadd/swiss_dubbing/hah jo.ogg",length=0.764}}
+L["hmmm"]={{path="chatsounds/autoadd/swiss_dubbing/hmmm.ogg",length=0.965}}
+L["ho ja"]={{path="chatsounds/autoadd/swiss_dubbing/ho ja.ogg",length=1.013}}
+L["hoou"]={{path="chatsounds/autoadd/swiss_dubbing/hoou.ogg",length=0.938}}
+L["ja"]={{path="chatsounds/autoadd/swiss_dubbing/ja/ja1.ogg",length=0.471},{path="chatsounds/autoadd/swiss_dubbing/ja/ja2.ogg",length=0.614},{path="chatsounds/autoadd/swiss_dubbing/ja/ja3.ogg",length=0.546}}
+L["ja hah hah hah ja hah hah hah"]={{path="chatsounds/autoadd/swiss_dubbing/ja hah hah hah ja hah hah hah.ogg",length=6.465}}
+L["ja riech mal"]={{path="chatsounds/autoadd/swiss_dubbing/ja riech mal.ogg",length=1.238}}
+L["ja so so ist fein ja"]={{path="chatsounds/autoadd/swiss_dubbing/ja so so ist fein ja.ogg",length=2.827}}
+L["mach weiter"]={{path="chatsounds/autoadd/swiss_dubbing/mach weiter.ogg",length=1.077}}
+L["schleck mal ja"]={{path="chatsounds/autoadd/swiss_dubbing/schleck mal ja.ogg",length=1.245}}
+L["so geil"]={{path="chatsounds/autoadd/swiss_dubbing/so geil.ogg",length=1.156}}
+L["so ist laessig so ist gut"]={{path="chatsounds/autoadd/swiss_dubbing/so ist laessig so ist gut.ogg",length=2.820}}
+L["so ja ist gut"]={{path="chatsounds/autoadd/swiss_dubbing/so ja ist gut.ogg",length=1.350}}
+L["so laessig ja jo"]={{path="chatsounds/autoadd/swiss_dubbing/so laessig ja jo.ogg",length=2.223}}
+L["spritz"]={{path="chatsounds/autoadd/swiss_dubbing/spritz.ogg",length=0.648}}
+L["spritz auf den arsch"]={{path="chatsounds/autoadd/swiss_dubbing/spritz auf den arsch.ogg",length=1.204}}
+L["spritz auf muschi"]={{path="chatsounds/autoadd/swiss_dubbing/spritz auf muschi.ogg",length=1.364}}
+L["spritz jetzt auf titte"]={{path="chatsounds/autoadd/swiss_dubbing/spritz jetzt auf titte.ogg",length=1.575}}
+c.EndList()
