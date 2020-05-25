78d0281cb4fdf6b02fd777824d631744b29bd23f
Niccep <43934118+0x098@users.noreply.github.com>
2020-05-25 12:14:18 +0300

    brim with no yankee

---
+++ lua/chatsounds/lists_nosend/nuts_for_pudding.lua
@@ -0,0 +1,6 @@
+c.StartList("nuts_for_pudding")
+L["back off sister"]={{path="chatsounds/autoadd/nuts_for_pudding/back off sister.ogg",length=0.920}}
+L["breakfast"]={{path="chatsounds/autoadd/nuts_for_pudding/breakfast.ogg",length=0.637}}
+L["what is going on here"]={{path="chatsounds/autoadd/nuts_for_pudding/what is going on here.ogg",length=3.011}}
+L["you cant eat this"]={{path="chatsounds/autoadd/nuts_for_pudding/you cant eat this.ogg",length=2.300}}
+c.EndList()
--- lua/chatsounds/lists_nosend/vine.lua
+++ lua/chatsounds/lists_nosend/vine.lua
@@ -1,6 +1,16 @@
 c.StartList("vine")
+L["ayyo"]={{path="chatsounds/autoadd/vine/ayyo.ogg",length=0.567}}
+L["brim"]={{path="chatsounds/autoadd/vine/brim.ogg",length=0.658}}
 L["hahahahahahahahahahahahahahahahahahahahaha"]={{path="chatsounds/autoadd/vine/hahahahahahahahahahahahahahahahahahahahaha.ogg",length=5.335}}
+L["it is what it is"]={{path="chatsounds/autoadd/vine/it is what it is/1.ogg",length=1.425},{path="chatsounds/autoadd/vine/it is what it is/2.ogg",length=2.561}}
 L["not a krump stop"]={{path="chatsounds/autoadd/vine/not a krump stop.ogg",length=1.059}}
 L["officer i got one question for you"]={{path="chatsounds/autoadd/vine/officer i got one question for you.ogg",length=1.702}}
+L["pussy"]={{path="chatsounds/autoadd/vine/pussy.ogg",length=0.701}}
+L["suck my wiggly dick"]={{path="chatsounds/autoadd/vine/suck my wiggly dick.ogg",length=1.630}}
 L["what are thooooooose"]={{path="chatsounds/autoadd/vine/what are thooooooose.ogg",length=2.557}}
+L["with no"]={{path="chatsounds/autoadd/vine/with no.ogg",length=0.697}}
+L["woah"]={{path="chatsounds/autoadd/vine/woah.ogg",length=1.388}}
+L["yankee"]={{path="chatsounds/autoadd/vine/yankee.ogg",length=0.592}}
+L["yankee with no brim"]={{path="chatsounds/autoadd/vine/yankee with no brim.ogg",length=1.946}}
+L["yankeewithnobrimfull"]={{path="chatsounds/autoadd/vine/yankeewithnobrimfull.ogg",length=9.407}}
 c.EndList()
