4531ff3b866f160004cef02238e0ca72f53c3158
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-05-31 21:25:20 +0000

    chatsounds preprocessor (a79f33d1d8c09f29482d9411b81fca08251822fc)
    [ci skip]

---
+++ lua/chatsounds/lists_nosend/cockpit_warnings.lua
@@ -0,0 +1,10 @@
+c.StartList("cockpit_warnings")
+L["autopilot disconnect"]={{path="chatsounds/autoadd/cockpit_warnings/autopilot disconnect.ogg",length=1.082}}
+L["pull up"]={{path="chatsounds/autoadd/cockpit_warnings/pull up.ogg",length=0.777}}
+L["pull up alarm"]={{path="chatsounds/autoadd/cockpit_warnings/pull up alarm.ogg",length=0.754}}
+L["sink rate"]={{path="chatsounds/autoadd/cockpit_warnings/sink rate.ogg",length=0.560}}
+L["stall alarm"]={{path="chatsounds/autoadd/cockpit_warnings/stall alarm.ogg",length=1.054}}
+L["stall stall"]={{path="chatsounds/autoadd/cockpit_warnings/stall stall.ogg",length=1.202}}
+L["windshear"]={{path="chatsounds/autoadd/cockpit_warnings/windshear.ogg",length=0.616}}
+L["windshear alarm"]={{path="chatsounds/autoadd/cockpit_warnings/windshear alarm.ogg",length=0.572}}
+c.EndList()
