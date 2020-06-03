7ebf353ea6d450c3e06596b51991d883c4df3c60
mitterdoo <connorashcroft@live.com>
2020-06-02 21:59:09 -0700

    reality can be whatever i want

--- lua/chatsounds/lists_nosend/infinitywar_endgame.lua
+++ lua/chatsounds/lists_nosend/infinitywar_endgame.lua
@@ -5,6 +5,7 @@ L["i am inevitable"]={{path="chatsounds/autoadd/infinitywar_endgame/i am inevita
 L["i used the stones to destroy the stones"]={{path="chatsounds/autoadd/infinitywar_endgame/i used the stones to destroy the stones.ogg",length=2.876}}
 L["im sorry little one"]={{path="chatsounds/autoadd/infinitywar_endgame/im sorry little one.ogg",length=1.787}}
 L["perfectly balanced as all things should be"]={{path="chatsounds/autoadd/infinitywar_endgame/perfectly balanced as all things should be.ogg",length=3.292}}
+L["reality can be whatever i want"]={{path="chatsounds/autoadd/infinitywar_endgame/reality can be whatever i want.ogg",length=2.426}}
 L["reality is often disappointing"]={{path="chatsounds/autoadd/infinitywar_endgame/reality is often disappointing.ogg",length=2.252}}
 L["thanossnap"]={{path="chatsounds/autoadd/infinitywar_endgame/thanossnap/1.ogg",length=4.435},{path="chatsounds/autoadd/infinitywar_endgame/thanossnap/2.ogg",length=1.043}}
 L["what did it cost"]={{path="chatsounds/autoadd/infinitywar_endgame/what did it cost.ogg",length=1.021}}
