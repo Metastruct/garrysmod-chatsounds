23a227e436ce9d265db495e123b667efccfef1f6
MissVoxxie <odysseylove222@gmail.com>
2020-06-15 22:37:52 -0400

    Folder Sorting
    yeah

--- lua/chatsounds/lists_nosend/funny.lua
+++ lua/chatsounds/lists_nosend/funny.lua
@@ -1,5 +1,5 @@
 c.StartList("funny")
 L["gmod"]={{path="chatsounds/autoadd/funny/gmod/undo.ogg",length=0.313}}
-L["voxxie"]={{path="chatsounds/autoadd/funny/voxxie.ogg",length=0.708}}
-L["yankee with no brim"]={{path="chatsounds/autoadd/funny/yankee with no brim.ogg",length=3.170}}
+L["memes"]={{path="chatsounds/autoadd/funny/memes/yankee with no brim.ogg",length=3.170}}
+L["player_names"]={{path="chatsounds/autoadd/funny/player_names/voxxie.ogg",length=0.708}}
 c.EndList()
