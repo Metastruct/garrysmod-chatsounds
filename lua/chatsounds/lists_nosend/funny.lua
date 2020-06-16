85fa259a99e3e7ab8dca767b38170238f08418f9
MissVoxxie <odysseylove222@gmail.com>
2020-06-15 20:51:44 -0400

    fix

---
+++ lua/chatsounds/lists_nosend/funny.lua
@@ -0,0 +1,5 @@
+c.StartList("funny")
+L["gmod"]={{path="chatsounds/autoadd/funny/gmod/undo.ogg",length=0.313}}
+L["voxxie"]={{path="chatsounds/autoadd/funny/voxxie.ogg",length=0.708}}
+L["yankee with no brim"]={{path="chatsounds/autoadd/funny/yankee with no brim.ogg",length=3.170}}
+c.EndList()
--- lua/chatsounds/lists_nosend/voxxie.lua
+++
@@ -1,5 +0,0 @@
-c.StartList("voxxie")
-L["undo"]={{path="chatsounds/autoadd/voxxie/undo.ogg",length=0.313}}
-L["voxxie"]={{path="chatsounds/autoadd/voxxie/voxxie.ogg",length=0.708}}
-L["yankee with no brim"]={{path="chatsounds/autoadd/voxxie/yankee with no brim.ogg",length=3.170}}
-c.EndList()
