4b73a0b8df582a45fdb63b1fc8fc18a26ba4aea9
Niccep <43934118+0x098@users.noreply.github.com>
2020-06-26 02:22:01 +0300

    introducing - vsauce
    +vsauce
    +68 to vsauce
    +8 to vine
    +58 to eggx3
    +7 to memes

---
+++ lua/chatsounds/lists_nosend/vsauce.lua
@@ -0,0 +1,70 @@
+c.StartList("vsauce")
+L["a bearded nun flies through the sky i wrote this song when i was high"]={{path="chatsounds/autoadd/vsauce/a bearded nun flies through the sky i wrote this song when i was high.ogg",length=4.963}}
+L["a cat is made out of air water and catfood"]={{path="chatsounds/autoadd/vsauce/a cat is made out of air water and catfood.ogg",length=4.459}}
+L["and all of our cameras are hidden so she has no idea she is being watched"]={{path="chatsounds/autoadd/vsauce/and all of our cameras are hidden so she has no idea she is being watched.ogg",length=4.401}}
+L["and as always no thanks for watching couldnt care less"]={{path="chatsounds/autoadd/vsauce/and as always no thanks for watching couldnt care less.ogg",length=3.137}}
+L["as it turns out no"]={{path="chatsounds/autoadd/vsauce/as it turns out no.ogg",length=2.824}}
+L["bananas are fantastic"]={{path="chatsounds/autoadd/vsauce/bananas are fantastic.ogg",length=2.082}}
+L["bring in the cats"]={{path="chatsounds/autoadd/vsauce/bring in the cats.ogg",length=1.116}}
+L["build a nuclear reactor"]={{path="chatsounds/autoadd/vsauce/build a nuclear reactor.ogg",length=1.990}}
+L["but right now were talking about balls"]={{path="chatsounds/autoadd/vsauce/but right now were talking about balls.ogg",length=2.001}}
+L["but their children will be removed"]={{path="chatsounds/autoadd/vsauce/but their children will be removed.ogg",length=3.557}}
+L["comedy truly is a matter of taste"]={{path="chatsounds/autoadd/vsauce/comedy truly is a matter of taste.ogg",length=2.682}}
+L["could you come lick this"]={{path="chatsounds/autoadd/vsauce/could you come lick this.ogg",length=1.078}}
+L["digestion begins in the mouth"]={{path="chatsounds/autoadd/vsauce/digestion begins in the mouth.ogg",length=1.773}}
+L["ding dong"]={{path="chatsounds/autoadd/vsauce/ding dong.ogg",length=0.783}}
+L["do you want my head delivered to your door in a box"]={{path="chatsounds/autoadd/vsauce/do you want my head delivered to your door in a box.ogg",length=2.597}}
+L["dont you feel like a fool heh"]={{path="chatsounds/autoadd/vsauce/dont you feel like a fool heh.ogg",length=1.760}}
+L["donut uh wheres the nut"]={{path="chatsounds/autoadd/vsauce/donut uh wheres the nut.ogg",length=3.131}}
+L["everyone is scared of something"]={{path="chatsounds/autoadd/vsauce/everyone is scared of something.ogg",length=2.598}}
+L["heres something you can try at home"]={{path="chatsounds/autoadd/vsauce/heres something you can try at home.ogg",length=2.166}}
+L["hey honey do you want to make a little baby"]={{path="chatsounds/autoadd/vsauce/hey honey do you want to make a little baby.ogg",length=2.831}}
+L["hey vsauce michael here but now im not"]={{path="chatsounds/autoadd/vsauce/hey vsauce michael here but now im not.ogg",length=4.871}}
+L["hopefully i dont die"]={{path="chatsounds/autoadd/vsauce/hopefully i dont die.ogg",length=2.384}}
+L["human extinction will be sooner rather than later"]={{path="chatsounds/autoadd/vsauce/human extinction will be sooner rather than later.ogg",length=5.089}}
+L["hundreds of them were once peed out of albert einstein"]={{path="chatsounds/autoadd/vsauce/hundreds of them were once peed out of albert einstein.ogg",length=3.526}}
+L["i decided to learn how to make someone confess to a crime they didnt commit"]={{path="chatsounds/autoadd/vsauce/i decided to learn how to make someone confess to a crime they didnt commit.ogg",length=5.429}}
+L["i know when and how you will die"]={{path="chatsounds/autoadd/vsauce/i know when and how you will die.ogg",length=5.880}}
+L["i love you all"]={{path="chatsounds/autoadd/vsauce/i love you all.ogg",length=0.968}}
+L["i still havent drinked water this is coffee ive never had water in my life"]={{path="chatsounds/autoadd/vsauce/i still havent drinked water this is coffee ive never had water in my life.ogg",length=3.236}}
+L["i watch rick and morty"]={{path="chatsounds/autoadd/vsauce/i watch rick and morty.ogg",length=1.674}}
+L["im a ninja"]={{path="chatsounds/autoadd/vsauce/im a ninja.ogg",length=0.858}}
+L["im alone"]={{path="chatsounds/autoadd/vsauce/im alone.ogg",length=0.837}}
+L["im in my parents basement"]={{path="chatsounds/autoadd/vsauce/im in my parents basement.ogg",length=1.416}}
+L["im not your child though am i"]={{path="chatsounds/autoadd/vsauce/im not your child though am i.ogg",length=2.024}}
+L["inevitable death"]={{path="chatsounds/autoadd/vsauce/inevitable death.ogg",length=1.303}}
+L["just like mom used to make"]={{path="chatsounds/autoadd/vsauce/just like mom used to make.ogg",length=1.297}}
+L["ladies and gentlemen we got them"]={{path="chatsounds/autoadd/vsauce/ladies and gentlemen we got them.ogg",length=1.815}}
+L["let me ask you this where did u get that body"]={{path="chatsounds/autoadd/vsauce/let me ask you this where did u get that body.ogg",length=2.768}}
+L["look at those muscles"]={{path="chatsounds/autoadd/vsauce/look at those muscles.ogg",length=1.137}}
+L["no hole will be left unfilled"]={{path="chatsounds/autoadd/vsauce/no hole will be left unfilled.ogg",length=2.568}}
+L["no way that came on before i pushed the button i didnt do it"]={{path="chatsounds/autoadd/vsauce/no way that came on before i pushed the button i didnt do it.ogg",length=3.514}}
+L["now its time for some soft flully pancakes"]={{path="chatsounds/autoadd/vsauce/now its time for some soft flully pancakes.ogg",length=3.970}}
+L["now its time to be tested"]={{path="chatsounds/autoadd/vsauce/now its time to be tested.ogg",length=1.672}}
+L["now parents can have children that are not you"]={{path="chatsounds/autoadd/vsauce/now parents can have children that are not you.ogg",length=2.977}}
+L["okay what the heck is this thing"]={{path="chatsounds/autoadd/vsauce/okay what the heck is this thing.ogg",length=2.532}}
+L["or am i"]={{path="chatsounds/autoadd/vsauce/or am i.ogg",length=1.073}}
+L["prepare to get blown"]={{path="chatsounds/autoadd/vsauce/prepare to get blown.ogg",length=2.242}}
+L["remember this friend earlier that we made well were gonna dissect him"]={{path="chatsounds/autoadd/vsauce/remember this friend earlier that we made well were gonna dissect him.ogg",length=3.459}}
+L["sticks and stones may break my bones but words are merely the smallest element of language"]={{path="chatsounds/autoadd/vsauce/sticks and stones may break my bones but words are merely the smallest element of language.ogg",length=12.811}}
+L["sucking is not attractive"]={{path="chatsounds/autoadd/vsauce/sucking is not attractive.ogg",length=2.495}}
+L["that one youre in right now"]={{path="chatsounds/autoadd/vsauce/that one youre in right now.ogg",length=2.253}}
+L["that wont stop me because i am here for you"]={{path="chatsounds/autoadd/vsauce/that wont stop me because i am here for you.ogg",length=3.897}}
+L["this is unfortunate"]={{path="chatsounds/autoadd/vsauce/this is unfortunate.ogg",length=1.824}}
+L["use kids to commit your crimes"]={{path="chatsounds/autoadd/vsauce/use kids to commit your crimes.ogg",length=2.704}}
+L["vsauce michael autism"]={{path="chatsounds/autoadd/vsauce/vsauce michael autism.ogg",length=0.966}}
+L["vsauce michael grunting"]={{path="chatsounds/autoadd/vsauce/vsauce michael grunting.ogg",length=3.133}}
+L["vsauce ncis intro"]={{path="chatsounds/autoadd/vsauce/vsauce ncis intro.ogg",length=5.493}}
+L["we forced all of these participants to receive botox injections"]={{path="chatsounds/autoadd/vsauce/we forced all of these participants to receive botox injections.ogg",length=4.830}}
+L["where are your fingers"]={{path="chatsounds/autoadd/vsauce/where are your fingers.ogg",length=1.760}}
+L["wheres the nut"]={{path="chatsounds/autoadd/vsauce/wheres the nut.ogg",length=0.882}}
+L["whos this hot body with great cleavage"]={{path="chatsounds/autoadd/vsauce/whos this hot body with great cleavage.ogg",length=2.420}}
+L["would you like to play with my toys"]={{path="chatsounds/autoadd/vsauce/would you like to play with my toys.ogg",length=1.824}}
+L["yolo fellow kids"]={{path="chatsounds/autoadd/vsauce/yolo fellow kids.ogg",length=1.352}}
+L["you are mister gay"]={{path="chatsounds/autoadd/vsauce/you are mister gay.ogg",length=2.343}}
+L["you can live the entire rest of your life without breathing"]={{path="chatsounds/autoadd/vsauce/you can live the entire rest of your life without breathing.ogg",length=3.691}}
+L["you can not beat it"]={{path="chatsounds/autoadd/vsauce/you can not beat it.ogg",length=1.155}}
+L["you guessed it the bearded nun"]={{path="chatsounds/autoadd/vsauce/you guessed it the bearded nun.ogg",length=2.420}}
+L["you really scientifically are the center of the universe"]={{path="chatsounds/autoadd/vsauce/you really scientifically are the center of the universe.ogg",length=5.287}}
+L["your butt is magnificent"]={{path="chatsounds/autoadd/vsauce/your butt is magnificent.ogg",length=1.609}}
+c.EndList()
