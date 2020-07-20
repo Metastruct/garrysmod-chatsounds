dbf811464093893d9de56abd0473f3711caa53fe
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-07-20 22:56:00 +0000

    chatsounds preprocessor (cb32151890c75c3eb6fc4075f1ada38e7990b441)
    [ci skip]

---
+++ lua/chatsounds/lists_nosend/the_salami_lid.lua
@@ -0,0 +1,6 @@
+c.StartList("the_salami_lid")
+L["salami lid aint gonna fit like that"]={{path="chatsounds/autoadd/the_salami_lid/salami lid aint gonna fit like that.ogg",length=2.350}}
+L["so the salami lid"]={{path="chatsounds/autoadd/the_salami_lid/so the salami lid.ogg",length=3.724}}
+L["so the salami lid wont fit the salami lid"]={{path="chatsounds/autoadd/the_salami_lid/so the salami lid wont fit the salami lid.ogg",length=3.843}}
+L["the salami lid aint gonna fit"]={{path="chatsounds/autoadd/the_salami_lid/the salami lid aint gonna fit.ogg",length=3.710}}
+c.EndList()
