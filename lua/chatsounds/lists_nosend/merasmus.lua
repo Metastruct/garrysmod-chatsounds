66c6b9e9ba50181116b666fe83b3c4ffd9b6d462
Carl Roberts <48777329+carlroberts99@users.noreply.github.com>
2020-08-01 18:28:32 +0100

    a few more sounds from merasmus' questionable kingdom
    Sounds courtesy of Joel/Sensuka

--- lua/chatsounds/lists_nosend/merasmus.lua
+++ lua/chatsounds/lists_nosend/merasmus.lua
@@ -1,15 +1,19 @@
 c.StartList("merasmus")
 L["ahhhh crap"]={{path="chatsounds/autoadd/merasmus/ahhhh crap.ogg",length=1.586}}
+L["and censored up last people alive"]={{path="chatsounds/autoadd/merasmus/and censored up last people alive.ogg",length=2.720}}
 L["are now on sale mortals"]={{path="chatsounds/autoadd/merasmus/are now on sale mortals.ogg",length=2.813}}
 L["behold censored time"]={{path="chatsounds/autoadd/merasmus/behold censored time.ogg",length=2.367}}
 L["bonus fucks"]={{path="chatsounds/autoadd/merasmus/bonus fucks.ogg",length=1.098}}
+L["but i wanted to rape them"]={{path="chatsounds/autoadd/merasmus/but i wanted to rape them.ogg",length=2.563}}
 L["do you feel it tightening around your cock"]={{path="chatsounds/autoadd/merasmus/do you feel it tightening around your cock.ogg",length=2.319}}
+L["fap intensifies daaaah"]={{path="chatsounds/autoadd/merasmus/fap intensifies daaaah.ogg",length=2.326}}
 L["heblelb"]={{path="chatsounds/autoadd/merasmus/heblelb.ogg",length=0.854}}
 L["hes going to fuck that cock"]={{path="chatsounds/autoadd/merasmus/hes going to fuck that cock.ogg",length=2.733}}
 L["his cock got crushed"]={{path="chatsounds/autoadd/merasmus/his cock got crushed.ogg",length=2.343}}
 L["it took a while to get sexual again"]={{path="chatsounds/autoadd/merasmus/it took a while to get sexual again.ogg",length=2.374}}
 L["just to be clear though hes bleeding dry"]={{path="chatsounds/autoadd/merasmus/just to be clear though hes bleeding dry.ogg",length=3.344}}
 L["merasmus is pregnant"]={{path="chatsounds/autoadd/merasmus/merasmus is pregnant.ogg",length=2.831}}
+L["oh this is taking too long"]={{path="chatsounds/autoadd/merasmus/oh this is taking too long.ogg",length=2.125}}
 L["on the streets"]={{path="chatsounds/autoadd/merasmus/on the streets.ogg",length=1.269}}
 L["remember when i got bonah"]={{path="chatsounds/autoadd/merasmus/remember when i got bonah.ogg",length=1.837}}
 L["sorry i was just working out"]={{path="chatsounds/autoadd/merasmus/sorry i was just working out.ogg",length=1.782}}
