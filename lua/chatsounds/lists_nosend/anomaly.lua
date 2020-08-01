86dd17711c4885c29a3e1df9dd29f0f5f52de29b
Niccep <43934118+0x098@users.noreply.github.com>
2020-08-01 22:11:23 +0300

    anomaly and bread bank

---
+++ lua/chatsounds/lists_nosend/anomaly.lua
@@ -0,0 +1,7 @@
+c.StartList("anomaly")
+L["and you build auschwitz"]={{path="chatsounds/autoadd/anomaly/and you build auschwitz.ogg",length=1.884}}
+L["is this fukin auschwitz"]={{path="chatsounds/autoadd/anomaly/is this fukin auschwitz.ogg",length=1.662}}
+L["you built auschwitz on our minecraft server"]={{path="chatsounds/autoadd/anomaly/you built auschwitz on our minecraft server.ogg",length=4.304}}
+L["you built minecraft on our auschwitz server"]={{path="chatsounds/autoadd/anomaly/you built minecraft on our auschwitz server.ogg",length=4.304}}
+L["you literally made incinerator"]={{path="chatsounds/autoadd/anomaly/you literally made incinerator.ogg",length=1.930}}
+c.EndList()
---
+++ lua/chatsounds/lists_nosend/bread_bank.lua
@@ -0,0 +1,59 @@
+c.StartList("bread_bank")
+L["all im sayin is fuck yalls bread"]={{path="chatsounds/autoadd/bread_bank/all im sayin is fuck yalls bread.ogg",length=1.991}}
+L["and a brioche"]={{path="chatsounds/autoadd/bread_bank/and a brioche.ogg",length=1.153}}
+L["and frick them crackers"]={{path="chatsounds/autoadd/bread_bank/and frick them crackers.ogg",length=2.013}}
+L["at the bread store"]={{path="chatsounds/autoadd/bread_bank/at the bread store.ogg",length=1.415}}
+L["bread on the floor"]={{path="chatsounds/autoadd/bread_bank/bread on the floor.ogg",length=0.976}}
+L["bro shut the frick up"]={{path="chatsounds/autoadd/bread_bank/bro shut the frick up.ogg",length=1.566}}
+L["but the crackers dont have gluten"]={{path="chatsounds/autoadd/bread_bank/but the crackers dont have gluten.ogg",length=2.013}}
+L["do you want"]={{path="chatsounds/autoadd/bread_bank/do you want.ogg",length=1.130}}
+L["florida"]={{path="chatsounds/autoadd/bread_bank/florida.ogg",length=0.752}}
+L["frick the gluten"]={{path="chatsounds/autoadd/bread_bank/frick the gluten.ogg",length=1.416}}
+L["fuck all that what booass country are yall from"]={{path="chatsounds/autoadd/bread_bank/fuck all that what booass country are yall from.ogg",length=3.739}}
+L["fuck crackers"]={{path="chatsounds/autoadd/bread_bank/fuck crackers.ogg",length=1.593}}
+L["hell no you better take the gluten out the damn shit"]={{path="chatsounds/autoadd/bread_bank/hell no you better take the gluten out the damn shit.ogg",length=3.065}}
+L["honestly frick yall"]={{path="chatsounds/autoadd/bread_bank/honestly frick yall.ogg",length=1.460}}
+L["i aint never seen nobody act like this over no bread"]={{path="chatsounds/autoadd/bread_bank/i aint never seen nobody act like this over no bread.ogg",length=2.887}}
+L["i dont care if its free"]={{path="chatsounds/autoadd/bread_bank/i dont care if its free.ogg",length=1.770}}
+L["i got my dollar store camera"]={{path="chatsounds/autoadd/bread_bank/i got my dollar store camera.ogg",length=1.508}}
+L["i got my dollar store camera on"]={{path="chatsounds/autoadd/bread_bank/i got my dollar store camera on.ogg",length=2.055}}
+L["i knew it"]={{path="chatsounds/autoadd/bread_bank/i knew it.ogg",length=0.664}}
+L["i know youre smoking that pack"]={{path="chatsounds/autoadd/bread_bank/i know youre smoking that pack.ogg",length=2.002}}
+L["if you want to fight we going to fight"]={{path="chatsounds/autoadd/bread_bank/if you want to fight we going to fight.ogg",length=1.728}}
+L["ill take those"]={{path="chatsounds/autoadd/bread_bank/ill take those.ogg",length=0.907}}
+L["im going to need you to shut that bullshit up chief"]={{path="chatsounds/autoadd/bread_bank/im going to need you to shut that bullshit up chief.ogg",length=2.611}}
+L["im going weast"]={{path="chatsounds/autoadd/bread_bank/im going weast.ogg",length=1.206}}
+L["im the fricking manager"]={{path="chatsounds/autoadd/bread_bank/im the fricking manager.ogg",length=2.564}}
+L["is gluten free"]={{path="chatsounds/autoadd/bread_bank/is gluten free.ogg",length=0.941}}
+L["its gluten free you want the gluten or nah"]={{path="chatsounds/autoadd/bread_bank/its gluten free you want the gluten or nah.ogg",length=2.379}}
+L["listen i just need a baguette"]={{path="chatsounds/autoadd/bread_bank/listen i just need a baguette.ogg",length=1.681}}
+L["look we got whole wheat gluten free"]={{path="chatsounds/autoadd/bread_bank/look we got whole wheat gluten free.ogg",length=2.976}}
+L["look you can either take this yeast"]={{path="chatsounds/autoadd/bread_bank/look you can either take this yeast.ogg",length=2.080}}
+L["nah dont call the police i got a warrant"]={{path="chatsounds/autoadd/bread_bank/nah dont call the police i got a warrant.ogg",length=2.058}}
+L["nah frick that i aint payin"]={{path="chatsounds/autoadd/bread_bank/nah frick that i aint payin.ogg",length=1.682}}
+L["ok thats gonna be five"]={{path="chatsounds/autoadd/bread_bank/ok thats gonna be five.ogg",length=1.095}}
+L["or im calling the police"]={{path="chatsounds/autoadd/bread_bank/or im calling the police.ogg",length=1.914}}
+L["or potato bread"]={{path="chatsounds/autoadd/bread_bank/or potato bread.ogg",length=0.810}}
+L["swear on your fricking yeezys"]={{path="chatsounds/autoadd/bread_bank/swear on your fricking yeezys.ogg",length=2.221}}
+L["take that shit out"]={{path="chatsounds/autoadd/bread_bank/take that shit out.ogg",length=1.030}}
+L["tell him to take the motherfricking gluten out the bread"]={{path="chatsounds/autoadd/bread_bank/tell him to take the motherfricking gluten out the bread.ogg",length=4.724}}
+L["texas toast gluten free"]={{path="chatsounds/autoadd/bread_bank/texas toast gluten free.ogg",length=2.157}}
+L["toasted"]={{path="chatsounds/autoadd/bread_bank/toasted.ogg",length=0.775}}
+L["tortilla"]={{path="chatsounds/autoadd/bread_bank/tortilla.ogg",length=1.007}}
+L["u trying to be on worldstar"]={{path="chatsounds/autoadd/bread_bank/u trying to be on worldstar.ogg",length=1.650}}
+L["we cant take shit out the bread"]={{path="chatsounds/autoadd/bread_bank/we cant take shit out the bread.ogg",length=2.111}}
+L["we dont have either of those"]={{path="chatsounds/autoadd/bread_bank/we dont have either of those.ogg",length=1.130}}
+L["we got bread on deck"]={{path="chatsounds/autoadd/bread_bank/we got bread on deck.ogg",length=1.153}}
+L["we got crackers no gluten"]={{path="chatsounds/autoadd/bread_bank/we got crackers no gluten.ogg",length=2.312}}
+L["we sell bread"]={{path="chatsounds/autoadd/bread_bank/we sell bread.ogg",length=0.949}}
+L["we sell loafs"]={{path="chatsounds/autoadd/bread_bank/we sell loafs.ogg",length=1.342}}
+L["welcome to the breadbank full"]={{path="chatsounds/autoadd/bread_bank/welcome to the breadbank full.ogg",length=108.277}}
+L["welcome to the breakbank"]={{path="chatsounds/autoadd/bread_bank/welcome to the breakbank.ogg",length=1.315}}
+L["what the frick are you saying"]={{path="chatsounds/autoadd/bread_bank/what the frick are you saying.ogg",length=2.002}}
+L["what the frick is gluten"]={{path="chatsounds/autoadd/bread_bank/what the frick is gluten.ogg",length=0.972}}
+L["what you going to record it"]={{path="chatsounds/autoadd/bread_bank/what you going to record it.ogg",length=1.134}}
+L["whats the fricking situation what the frick"]={{path="chatsounds/autoadd/bread_bank/whats the fricking situation what the frick.ogg",length=2.584}}
+L["where they got this bullshit at"]={{path="chatsounds/autoadd/bread_bank/where they got this bullshit at.ogg",length=1.615}}
+L["why put it in the firstplace"]={{path="chatsounds/autoadd/bread_bank/why put it in the firstplace.ogg",length=2.301}}
+L["you can get the gluten free white bread"]={{path="chatsounds/autoadd/bread_bank/you can get the gluten free white bread.ogg",length=1.573}}
+c.EndList()
