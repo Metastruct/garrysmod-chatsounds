06824f8c361efb73d84e090bdc18998bae5a0b29
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-08-03 05:13:55 +0000

    chatsounds preprocessor (bff021eae151a3a5ed36e4425ecbeadd20d9bf19)
    [ci skip]

---
+++ lua/chatsounds/lists_nosend/typical_stan.lua
@@ -0,0 +1,3 @@
+c.StartList("typical_stan")
+L["i love refrigerators"]={{path="chatsounds/autoadd/typical_stan/i love refrigerators.ogg",length=1.813}}
+c.EndList()
---
+++ lua/chatsounds/lists_nosend/yoshinori_ono.lua
@@ -0,0 +1,3 @@
+c.StartList("yoshinori_ono")
+L["kage"]={{path="chatsounds/autoadd/yoshinori_ono/kage.ogg",length=3.171}}
+c.EndList()
