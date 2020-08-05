38b86c6d6bf9450a8b19938cb50e907a7826ab8e
Carl Roberts <carldavidroberts1996@gmail.com>
2020-08-05 22:40:11 +0100

    music and sounds from pootis engage
    credit to joel/sensuka

--- lua/chatsounds/lists_nosend/pootis_engage.lua
+++ lua/chatsounds/lists_nosend/pootis_engage.lua
@@ -8,6 +8,9 @@ L["im coming for the thighs"]={{path="chatsounds/autoadd/pootis_engage/im coming
 L["it is time to go"]={{path="chatsounds/autoadd/pootis_engage/it is time to go.ogg",length=2.013}}
 L["my body tell me yes"]={{path="chatsounds/autoadd/pootis_engage/my body tell me yes.ogg",length=3.507}}
 L["my mind tell me no"]={{path="chatsounds/autoadd/pootis_engage/my mind tell me no.ogg",length=2.554}}
+L["ot"]={{path="chatsounds/autoadd/pootis_engage/ot.ogg",length=0.693}}
+L["smeshariki song"]={{path="chatsounds/autoadd/pootis_engage/smeshariki song.ogg",length=199.419}}
 L["the booty will not wait forever"]={{path="chatsounds/autoadd/pootis_engage/the booty will not wait forever.ogg",length=2.186}}
+L["vinta"]={{path="chatsounds/autoadd/pootis_engage/vinta.ogg",length=1.588}}
 L["yes distorted"]={{path="chatsounds/autoadd/pootis_engage/yes distorted.ogg",length=1.457}}
 c.EndList()
