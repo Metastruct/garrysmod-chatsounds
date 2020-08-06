5975d4fad38937a14d6491869e901ff4cacfd85c
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-08-06 01:29:04 +0000

    chatsounds preprocessor (b2a698b27cc13d645dff465775ad64a84f38d394)
    [ci skip]

---
+++ lua/chatsounds/lists_nosend/russian_kingpin.lua
@@ -0,0 +1,79 @@
+c.StartList("russian_kingpin")
+L["ah kaki mi nejnie"]={{path="chatsounds/autoadd/russian_kingpin/ah kaki mi nejnie.ogg",length=3.181}}
+L["che za bazar"]={{path="chatsounds/autoadd/russian_kingpin/che za bazar.ogg",length=2.322}}
+L["chego tebe"]={{path="chatsounds/autoadd/russian_kingpin/chego tebe.ogg",length=0.906}}
+L["cho nada suka"]={{path="chatsounds/autoadd/russian_kingpin/cho nada suka.ogg",length=1.416}}
+L["cho pisdish"]={{path="chatsounds/autoadd/russian_kingpin/cho pisdish.ogg",length=1.579}}
+L["cho pisdish gandon"]={{path="chatsounds/autoadd/russian_kingpin/cho pisdish gandon.ogg",length=1.858}}
+L["da"]={{path="chatsounds/autoadd/russian_kingpin/da.ogg",length=0.673}}
+L["da idi ti nahuy"]={{path="chatsounds/autoadd/russian_kingpin/da idi ti nahuy.ogg",length=1.300}}
+L["da kto ti takoi"]={{path="chatsounds/autoadd/russian_kingpin/da kto ti takoi.ogg",length=2.438}}
+L["da poshel ti"]={{path="chatsounds/autoadd/russian_kingpin/da poshel ti.ogg",length=1.091}}
+L["derma kusok"]={{path="chatsounds/autoadd/russian_kingpin/derma kusok.ogg",length=1.184}}
+L["ebat tebya vo vse dirki"]={{path="chatsounds/autoadd/russian_kingpin/ebat tebya vo vse dirki.ogg",length=2.833}}
+L["ebi drugomu mosg"]={{path="chatsounds/autoadd/russian_kingpin/ebi drugomu mosg.ogg",length=3.413}}
+L["eta suka ohuela"]={{path="chatsounds/autoadd/russian_kingpin/eta suka ohuela.ogg",length=4.874}}
+L["eto tebe mosgi ne daut"]={{path="chatsounds/autoadd/russian_kingpin/eto tebe mosgi ne daut.ogg",length=2.647}}
+L["eto ti mne"]={{path="chatsounds/autoadd/russian_kingpin/eto ti mne.ogg",length=1.370}}
+L["eto ti so mnoi"]={{path="chatsounds/autoadd/russian_kingpin/eto ti so mnoi.ogg",length=2.763}}
+L["eto vse chto ya mogu skazat"]={{path="chatsounds/autoadd/russian_kingpin/eto vse chto ya mogu skazat.ogg",length=1.834}}
+L["gopota zaebala"]={{path="chatsounds/autoadd/russian_kingpin/gopota zaebala.ogg",length=1.347}}
+L["hochesh problem"]={{path="chatsounds/autoadd/russian_kingpin/hochesh problem.ogg",length=1.115}}
+L["hule te nado"]={{path="chatsounds/autoadd/russian_kingpin/hule te nado.ogg",length=1.161}}
+L["idi nahuy"]={{path="chatsounds/autoadd/russian_kingpin/idi nahuy/1.ogg",length=0.975},{path="chatsounds/autoadd/russian_kingpin/idi nahuy/2.ogg",length=1.254}}
+L["idi nahuy derma kusok"]={{path="chatsounds/autoadd/russian_kingpin/idi nahuy derma kusok.ogg",length=2.159}}
+L["idi sosat"]={{path="chatsounds/autoadd/russian_kingpin/idi sosat/1.ogg",length=1.045},{path="chatsounds/autoadd/russian_kingpin/idi sosat/2.ogg",length=1.115}}
+L["jopa kak dela"]={{path="chatsounds/autoadd/russian_kingpin/jopa kak dela.ogg",length=2.415}}
+L["jopa umeet razgovar"]={{path="chatsounds/autoadd/russian_kingpin/jopa umeet razgovar.ogg",length=2.763}}
+L["kurnut bi"]={{path="chatsounds/autoadd/russian_kingpin/kurnut bi.ogg",length=1.858}}
+L["menya eta huyany zaebala"]={{path="chatsounds/autoadd/russian_kingpin/menya eta huyany zaebala.ogg",length=2.531}}
+L["menya pret"]={{path="chatsounds/autoadd/russian_kingpin/menya pret.ogg",length=1.881}}
+L["nada poimat i viebat"]={{path="chatsounds/autoadd/russian_kingpin/nada poimat i viebat.ogg",length=3.367}}
+L["ne ebi mosgi"]={{path="chatsounds/autoadd/russian_kingpin/ne ebi mosgi.ogg",length=1.695}}
+L["ne zaebivate menya"]={{path="chatsounds/autoadd/russian_kingpin/ne zaebivate menya.ogg",length=2.485}}
+L["nea"]={{path="chatsounds/autoadd/russian_kingpin/nea.ogg",length=0.627}}
+L["neudachnik ti"]={{path="chatsounds/autoadd/russian_kingpin/neudachnik ti.ogg",length=2.299}}
+L["nu i vonyaet ot tebya"]={{path="chatsounds/autoadd/russian_kingpin/nu i vonyaet ot tebya.ogg",length=2.949}}
+L["otebis"]={{path="chatsounds/autoadd/russian_kingpin/otebis.ogg",length=1.370}}
+L["otpisdit paru pidorov"]={{path="chatsounds/autoadd/russian_kingpin/otpisdit paru pidorov.ogg",length=1.904}}
+L["otsasi"]={{path="chatsounds/autoadd/russian_kingpin/otsasi.ogg",length=0.929}}
+L["otsasi pidrila"]={{path="chatsounds/autoadd/russian_kingpin/otsasi pidrila.ogg",length=2.090}}
+L["pidor gnoinuy"]={{path="chatsounds/autoadd/russian_kingpin/pidor gnoinuy.ogg",length=1.161}}
+L["pisde slovo na davali"]={{path="chatsounds/autoadd/russian_kingpin/pisde slovo na davali.ogg",length=2.601}}
+L["pisdui nahuy"]={{path="chatsounds/autoadd/russian_kingpin/pisdui nahuy.ogg",length=2.020}}
+L["pisduy nahuy"]={{path="chatsounds/autoadd/russian_kingpin/pisduy nahuy.ogg",length=1.579}}
+L["poebatsya zahotelos"]={{path="chatsounds/autoadd/russian_kingpin/poebatsya zahotelos.ogg",length=2.810}}
+L["poshel nahuy"]={{path="chatsounds/autoadd/russian_kingpin/poshel nahuy/1.ogg",length=1.161},{path="chatsounds/autoadd/russian_kingpin/poshel nahuy/2.ogg",length=1.347}}
+L["poshel nahuy chenok"]={{path="chatsounds/autoadd/russian_kingpin/poshel nahuy chenok.ogg",length=1.649}}
+L["poshel ti nahuy"]={{path="chatsounds/autoadd/russian_kingpin/poshel ti nahuy.ogg",length=2.810}}
+L["problemi"]={{path="chatsounds/autoadd/russian_kingpin/problemi.ogg",length=1.231}}
+L["suka obnagleli"]={{path="chatsounds/autoadd/russian_kingpin/suka obnagleli.ogg",length=2.090}}
+L["tak mijno pisdi polu4it"]={{path="chatsounds/autoadd/russian_kingpin/tak mijno pisdi polu4it.ogg",length=1.602}}
+L["takih kak ti ubivaut"]={{path="chatsounds/autoadd/russian_kingpin/takih kak ti ubivaut.ogg",length=2.786}}
+L["ti bazar filtruy"]={{path="chatsounds/autoadd/russian_kingpin/ti bazar filtruy.ogg",length=1.486}}
+L["ti gandon"]={{path="chatsounds/autoadd/russian_kingpin/ti gandon.ogg",length=2.624}}
+L["ti gandon pidor"]={{path="chatsounds/autoadd/russian_kingpin/ti gandon pidor.ogg",length=3.646}}
+L["ti kto takoi"]={{path="chatsounds/autoadd/russian_kingpin/ti kto takoi.ogg",length=1.231}}
+L["ti kusok"]={{path="chatsounds/autoadd/russian_kingpin/ti kusok.ogg",length=2.183}}
+L["ti menya zaebal"]={{path="chatsounds/autoadd/russian_kingpin/ti menya zaebal.ogg",length=2.252}}
+L["ti nadoel"]={{path="chatsounds/autoadd/russian_kingpin/ti nadoel.ogg",length=1.254}}
+L["ti ohuel"]={{path="chatsounds/autoadd/russian_kingpin/ti ohuel.ogg",length=0.882}}
+L["ti so mno govorish"]={{path="chatsounds/autoadd/russian_kingpin/ti so mno govorish.ogg",length=1.834}}
+L["ti tipa kto takoi"]={{path="chatsounds/autoadd/russian_kingpin/ti tipa kto takoi.ogg",length=1.370}}
+L["ti tuhluy gandon"]={{path="chatsounds/autoadd/russian_kingpin/ti tuhluy gandon.ogg",length=4.063}}
+L["usralsya"]={{path="chatsounds/autoadd/russian_kingpin/usralsya.ogg",length=1.741}}
+L["vi vse menya zaebali"]={{path="chatsounds/autoadd/russian_kingpin/vi vse menya zaebali.ogg",length=2.322}}
+L["vi vse pridurki"]={{path="chatsounds/autoadd/russian_kingpin/vi vse pridurki.ogg",length=1.254}}
+L["welcome"]={{path="chatsounds/autoadd/russian_kingpin/welcome/1.ogg",length=2.113},{path="chatsounds/autoadd/russian_kingpin/welcome/2.ogg",length=5.526}}
+L["ya nahuy da eto ti nahuy"]={{path="chatsounds/autoadd/russian_kingpin/ya nahuy da eto ti nahuy.ogg",length=5.387}}
+L["ya nahyu da idi ti"]={{path="chatsounds/autoadd/russian_kingpin/ya nahyu da idi ti.ogg",length=4.528}}
+L["ya tebya viebu"]={{path="chatsounds/autoadd/russian_kingpin/ya tebya viebu.ogg",length=2.136}}
+L["ya tebya viebu chto mama"]={{path="chatsounds/autoadd/russian_kingpin/ya tebya viebu chto mama.ogg",length=2.461}}
+L["ya tebya viebu v"]={{path="chatsounds/autoadd/russian_kingpin/ya tebya viebu v.ogg",length=2.647}}
+L["ya vas imel"]={{path="chatsounds/autoadd/russian_kingpin/ya vas imel.ogg",length=1.672}}
+L["za bazar otvechai"]={{path="chatsounds/autoadd/russian_kingpin/za bazar otvechai.ogg",length=1.974}}
+L["za bazarom sledi"]={{path="chatsounds/autoadd/russian_kingpin/za bazarom sledi.ogg",length=1.974}}
+L["zaebal ti menya"]={{path="chatsounds/autoadd/russian_kingpin/zaebal ti menya.ogg",length=1.718}}
+L["zasin ruku v jopu"]={{path="chatsounds/autoadd/russian_kingpin/zasin ruku v jopu.ogg",length=4.017}}
+L["zdorovya to hvatit"]={{path="chatsounds/autoadd/russian_kingpin/zdorovya to hvatit.ogg",length=1.718}}
+c.EndList()
