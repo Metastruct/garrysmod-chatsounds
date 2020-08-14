b1f59a33508b7e4da390d690ae54d4a742330b86
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-08-14 23:23:51 +0000

    chatsounds preprocessor (b3de28b0d5cf579a3d353f593f936009827e9120)
    [ci skip]

---
+++ lua/chatsounds/lists_nosend/3kliksphillip.lua
@@ -0,0 +1,6 @@
+c.StartList("3kliksphillip")
+L["achieved with sorse"]={{path="chatsounds/autoadd/3kliksphillip/achieved with sorse.ogg",length=4.743}}
+L["achieved with sorse 2"]={{path="chatsounds/autoadd/3kliksphillip/achieved with sorse 2.ogg",length=4.651}}
+L["sorse"]={{path="chatsounds/autoadd/3kliksphillip/sorse.ogg",length=0.907}}
+L["sorse 2"]={{path="chatsounds/autoadd/3kliksphillip/sorse 2.ogg",length=1.250}}
+c.EndList()
