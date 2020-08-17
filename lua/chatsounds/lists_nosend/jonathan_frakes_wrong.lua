9b86caa96cf0c08e20d0fa078aeb941c520072b7
Mikeai <mgamkrel@gmail.com>
2020-08-17 01:32:05 +0100

    It happened.
    Jonathan Frakes tells you you're right and more wrong

---
+++ lua/chatsounds/lists_nosend/jonathan_frakes_wrong.lua
@@ -0,0 +1,18 @@
+c.StartList("jonathan_frakes_wrong")
+L["it never happened"]={{path="chatsounds/autoadd/jonathan_frakes_wrong/it never happened/1.ogg",length=1.212},{path="chatsounds/autoadd/jonathan_frakes_wrong/it never happened/2.ogg",length=1.070},{path="chatsounds/autoadd/jonathan_frakes_wrong/it never happened/3.ogg",length=1.097},{path="chatsounds/autoadd/jonathan_frakes_wrong/it never happened/4.ogg",length=0.880}}
+L["its a made up tale"]={{path="chatsounds/autoadd/jonathan_frakes_wrong/its a made up tale.ogg",length=1.137}}
+L["its a total fabrication"]={{path="chatsounds/autoadd/jonathan_frakes_wrong/its a total fabrication.ogg",length=1.225}}
+L["its an urban legend that never happened"]={{path="chatsounds/autoadd/jonathan_frakes_wrong/its an urban legend that never happened.ogg",length=2.356}}
+L["its false"]={{path="chatsounds/autoadd/jonathan_frakes_wrong/its false/1.ogg",length=0.690},{path="chatsounds/autoadd/jonathan_frakes_wrong/its false/2.ogg",length=0.961}}
+L["its fiction"]={{path="chatsounds/autoadd/jonathan_frakes_wrong/its fiction.ogg",length=0.731}}
+L["its totally made up"]={{path="chatsounds/autoadd/jonathan_frakes_wrong/its totally made up.ogg",length=0.961}}
+L["no way"]={{path="chatsounds/autoadd/jonathan_frakes_wrong/no way/1.ogg",length=0.569},{path="chatsounds/autoadd/jonathan_frakes_wrong/no way/2.ogg",length=0.487}}
+L["not a chance"]={{path="chatsounds/autoadd/jonathan_frakes_wrong/not a chance.ogg",length=0.806}}
+L["not this time"]={{path="chatsounds/autoadd/jonathan_frakes_wrong/not this time/1.ogg",length=0.839},{path="chatsounds/autoadd/jonathan_frakes_wrong/not this time/2.ogg",length=0.812},{path="chatsounds/autoadd/jonathan_frakes_wrong/not this time/3.ogg",length=0.731},{path="chatsounds/autoadd/jonathan_frakes_wrong/not this time/4.ogg",length=0.785},{path="chatsounds/autoadd/jonathan_frakes_wrong/not this time/5.ogg",length=0.704},{path="chatsounds/autoadd/jonathan_frakes_wrong/not this time/6.ogg",length=0.718},{path="chatsounds/autoadd/jonathan_frakes_wrong/not this time/7.ogg",length=0.555}}
+L["pure fiction"]={{path="chatsounds/autoadd/jonathan_frakes_wrong/pure fiction.ogg",length=0.718}}
+L["we gotcha"]={{path="chatsounds/autoadd/jonathan_frakes_wrong/we gotcha.ogg",length=0.630}}
+L["we made it up"]={{path="chatsounds/autoadd/jonathan_frakes_wrong/we made it up.ogg",length=0.860}}
+L["we made this one up"]={{path="chatsounds/autoadd/jonathan_frakes_wrong/we made this one up.ogg",length=0.982}}
+L["wrong"]={{path="chatsounds/autoadd/jonathan_frakes_wrong/wrong.ogg",length=0.545}}
+L["youre wrong"]={{path="chatsounds/autoadd/jonathan_frakes_wrong/youre wrong.ogg",length=0.623}}
+c.EndList()
