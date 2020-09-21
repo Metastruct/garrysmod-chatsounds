10e8618b3521e2fc668884dd6bbacfddc7f0295b
Mikeai <mgamkrel@gmail.com>
2020-09-22 00:58:06 +0100

    fnaf danger
    please help im being held at gunpoint to add this i dont have much ti

--- lua/chatsounds/lists_nosend/five_nights_at_freddys.lua
+++ lua/chatsounds/lists_nosend/five_nights_at_freddys.lua
@@ -1,6 +1,7 @@
 c.StartList("five_nights_at_freddys")
 L["fnaf breath"]={{path="chatsounds/autoadd/five_nights_at_freddys/fnaf breath/1.ogg",length=2.898},{path="chatsounds/autoadd/five_nights_at_freddys/fnaf breath/2.ogg",length=5.566},{path="chatsounds/autoadd/five_nights_at_freddys/fnaf breath/3.ogg",length=2.506},{path="chatsounds/autoadd/five_nights_at_freddys/fnaf breath/4.ogg",length=3.571}}
 L["fnaf chime"]={{path="chatsounds/autoadd/five_nights_at_freddys/fnaf chime.ogg",length=12.997}}
+L["fnaf danger"]={{path="chatsounds/autoadd/five_nights_at_freddys/fnaf danger.ogg",length=114.834}}
 L["fnaf doorerror"]={{path="chatsounds/autoadd/five_nights_at_freddys/fnaf doorerror.ogg",length=0.200}}
 L["fnaf dooroperate"]={{path="chatsounds/autoadd/five_nights_at_freddys/fnaf dooroperate.ogg",length=1.359}}
 L["fnaf freddylaugh"]={{path="chatsounds/autoadd/five_nights_at_freddys/fnaf freddylaugh/1.ogg",length=5.199},{path="chatsounds/autoadd/five_nights_at_freddys/fnaf freddylaugh/2.ogg",length=7.022},{path="chatsounds/autoadd/five_nights_at_freddys/fnaf freddylaugh/3.ogg",length=5.369}}
