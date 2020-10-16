bad442230fe2a575d5a06fcd3b49998aeaa556d2
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-10-16 15:57:23 +0000

    chatsounds preprocessor (286b2e9c7d688dc1c8d72b1ab3db97b9d7fc6697)
    [ci skip]

--- lua/chatsounds/lists_nosend/demo_mesa.lua
+++ lua/chatsounds/lists_nosend/demo_mesa.lua
@@ -5,4 +5,5 @@ L["it was open all along"]={{path="chatsounds/autoadd/demo_mesa/it was open all 
 L["oh thats gay"]={{path="chatsounds/autoadd/demo_mesa/oh thats gay.ogg",length=1.590}}
 L["shut the fuck up bitch"]={{path="chatsounds/autoadd/demo_mesa/shut the fuck up bitch.ogg",length=2.217}}
 L["that rock is full of magic"]={{path="chatsounds/autoadd/demo_mesa/that rock is full of magic.ogg",length=2.437}}
+L["ye bitch"]={{path="chatsounds/autoadd/demo_mesa/ye bitch.ogg",length=1.019}}
 c.EndList()
