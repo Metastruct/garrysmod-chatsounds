293c8c065aa2185160ce45136ec732708d8236fe
Niccep <43934118+0x098@users.noreply.github.com>
2020-10-26 18:38:38 +0200

    unnoticed fix
    :eyes:

--- lua/chatsounds/lists_nosend/anomaly.lua
+++ lua/chatsounds/lists_nosend/anomaly.lua
@@ -1,7 +1,7 @@
 c.StartList("anomaly")
-L["and you build auschwitz"]={{path="chatsounds/autoadd/anomaly/and you build auschwitz.ogg",length=1.884}}
-L["is this fukin auschwitz"]={{path="chatsounds/autoadd/anomaly/is this fukin auschwitz.ogg",length=1.662}}
-L["you built auschwitz on our minecraft server"]={{path="chatsounds/autoadd/anomaly/you built auschwitz on our minecraft server.ogg",length=4.304}}
-L["you built minecraft on our auschwitz server"]={{path="chatsounds/autoadd/anomaly/you built minecraft on our auschwitz server.ogg",length=4.304}}
+L["and you build abusement park"]={{path="chatsounds/autoadd/anomaly/and you build abusement park.ogg",length=1.884}}
+L["is this fukin abusement park"]={{path="chatsounds/autoadd/anomaly/is this fukin abusement park.ogg",length=1.662}}
+L["you built abusement park on our minecraft server"]={{path="chatsounds/autoadd/anomaly/you built abusement park on our minecraft server.ogg",length=4.304}}
+L["you built minecraft on our abusement park server"]={{path="chatsounds/autoadd/anomaly/you built minecraft on our abusement park server.ogg",length=4.304}}
 L["you literally made incinerator"]={{path="chatsounds/autoadd/anomaly/you literally made incinerator.ogg",length=1.930}}
 c.EndList()
