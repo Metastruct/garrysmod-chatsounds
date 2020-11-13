8d96716ca772c6bd42656069fb35d89b6342da2c
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-11-13 23:18:20 +0000

    chatsounds preprocessor (7f8468630be78392dbe5dc8b1809262e6480c95e)
    [ci skip]

---
+++ lua/chatsounds/lists_nosend/classicdoom.lua
@@ -0,0 +1,6 @@
+c.StartList("classicdoom")
+L["damage"]={{path="chatsounds/autoadd/classicdoom/damage.ogg",length=1.719}}
+L["death"]={{path="chatsounds/autoadd/classicdoom/death.ogg",length=1.719}}
+L["grunt"]={{path="chatsounds/autoadd/classicdoom/grunt.ogg",length=1.719}}
+L["teleport"]={{path="chatsounds/autoadd/classicdoom/teleport.ogg",length=1.719}}
+c.EndList()
