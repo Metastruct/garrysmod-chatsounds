eebdd8782bfca6475c06f581de9461476b5f95b9
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-12-19 13:45:02 +0000

    chatsounds preprocessor (ed9df7a9cbd69e07cec0d2ef5c2b9937cc8d4fdb)
    [ci skip]

---
+++ lua/chatsounds/lists_nosend/among_us.lua
@@ -0,0 +1,9 @@
+c.StartList("among_us")
+L["crewmate victory"]={{path="chatsounds/autoadd/among_us/crewmate victory.ogg",length=7.365}}
+L["emergency alarm"]={{path="chatsounds/autoadd/among_us/emergency alarm.ogg",length=7.231}}
+L["emergency meeting"]={{path="chatsounds/autoadd/among_us/emergency meeting.ogg",length=2.143}}
+L["imposter kill"]={{path="chatsounds/autoadd/among_us/imposter kill.ogg",length=1.071}}
+L["imposter victory"]={{path="chatsounds/autoadd/among_us/imposter victory.ogg",length=7.722}}
+L["vent enter"]={{path="chatsounds/autoadd/among_us/vent enter.ogg",length=0.893}}
+L["vent exit"]={{path="chatsounds/autoadd/among_us/vent exit.ogg",length=1.116}}
+c.EndList()
