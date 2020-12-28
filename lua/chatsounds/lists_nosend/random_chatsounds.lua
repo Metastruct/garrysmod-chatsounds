ab3a1f5e903886adbfff4c6cb72bc3f587c24e11
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-12-28 03:15:47 +0000

    chatsounds preprocessor (cab08bf13b6d4837d0e9c0ca5aba010993de792f)
    [ci skip]

--- lua/chatsounds/lists_nosend/random_chatsounds.lua
+++ lua/chatsounds/lists_nosend/random_chatsounds.lua
@@ -10,10 +10,12 @@ L["get over it"]={{path="chatsounds/autoadd/random_chatsounds/get over it.ogg",l
 L["ggiimmpp"]={{path="chatsounds/autoadd/random_chatsounds/ggiimmpp.ogg",length=3.077}}
 L["gimp"]={{path="chatsounds/autoadd/random_chatsounds/gimp.ogg",length=0.923}}
 L["hey shitass wanna see me speedbridge"]={{path="chatsounds/autoadd/random_chatsounds/hey shitass wanna see me speedbridge.ogg",length=2.870}}
+L["i am now the annihilator bitch"]={{path="chatsounds/autoadd/random_chatsounds/i am now the annihilator bitch.ogg",length=2.047}}
 L["i come to you right now"]={{path="chatsounds/autoadd/random_chatsounds/i come to you right now.ogg",length=1.544}}
 L["i have crippling depression"]={{path="chatsounds/autoadd/random_chatsounds/i have crippling depression.ogg",length=2.565}}
 L["i kill you right now"]={{path="chatsounds/autoadd/random_chatsounds/i kill you right now.ogg",length=1.950}}
 L["i rate the experience a six point five out of ten"]={{path="chatsounds/autoadd/random_chatsounds/i rate the experience a six point five out of ten.ogg",length=4.458}}
+L["i will stone cold study your vagina ass right here in the pastry aisle"]={{path="chatsounds/autoadd/random_chatsounds/i will stone cold study your vagina ass right here in the pastry aisle.ogg",length=3.429}}
 L["im an artist"]={{path="chatsounds/autoadd/random_chatsounds/im an artist.ogg",length=1.207}}
 L["jesus christ god"]={{path="chatsounds/autoadd/random_chatsounds/jesus christ god.ogg",length=2.624}}
 L["kill this arrogant moth er fu cker"]={{path="chatsounds/autoadd/random_chatsounds/kill this arrogant moth er fu cker.ogg",length=1.858}}
@@ -24,5 +26,7 @@ L["uagh"]={{path="chatsounds/autoadd/random_chatsounds/uagh.ogg",length=0.723}}
 L["wat"]={{path="chatsounds/autoadd/random_chatsounds/wat.ogg",length=0.147}}
 L["we do this you know one on one"]={{path="chatsounds/autoadd/random_chatsounds/we do this you know one on one.ogg",length=1.962}}
 L["what about the negro noodle"]={{path="chatsounds/autoadd/random_chatsounds/what about the negro noodle.ogg",length=1.718}}
+L["what the fuck did you just say you goddamn slut"]={{path="chatsounds/autoadd/random_chatsounds/what the fuck did you just say you goddamn slut.ogg",length=2.749}}
 L["why the hell so expensive"]={{path="chatsounds/autoadd/random_chatsounds/why the hell so expensive.ogg",length=1.591}}
+L["you prefer the powdered donuts to the holy chocolate donuts"]={{path="chatsounds/autoadd/random_chatsounds/you prefer the powdered donuts to the holy chocolate donuts.ogg",length=3.331}}
 c.EndList()
