8c19280faf14b36ffcb1ab724032e192f8a2ac2f
Meta Automator <MetaAutomator@users.noreply.github.com>
2020-12-28 03:24:41 +0000

    chatsounds preprocessor (e51c7834b21a2b468dde9f9fdb0790892277ce8c)
    [ci skip]

--- lua/chatsounds/lists_nosend/random_chatsounds.lua
+++ lua/chatsounds/lists_nosend/random_chatsounds.lua
@@ -10,12 +10,12 @@ L["get over it"]={{path="chatsounds/autoadd/random_chatsounds/get over it.ogg",l
 L["ggiimmpp"]={{path="chatsounds/autoadd/random_chatsounds/ggiimmpp.ogg",length=3.077}}
 L["gimp"]={{path="chatsounds/autoadd/random_chatsounds/gimp.ogg",length=0.923}}
 L["hey shitass wanna see me speedbridge"]={{path="chatsounds/autoadd/random_chatsounds/hey shitass wanna see me speedbridge.ogg",length=2.870}}
-L["i am now the annihilator bitch"]={{path="chatsounds/autoadd/random_chatsounds/i am now the annihilator bitch.ogg",length=2.047}}
+L["i am alan the annihilator bitch"]={{path="chatsounds/autoadd/random_chatsounds/i am alan the annihilator bitch.ogg",length=2.047}}
 L["i come to you right now"]={{path="chatsounds/autoadd/random_chatsounds/i come to you right now.ogg",length=1.544}}
 L["i have crippling depression"]={{path="chatsounds/autoadd/random_chatsounds/i have crippling depression.ogg",length=2.565}}
 L["i kill you right now"]={{path="chatsounds/autoadd/random_chatsounds/i kill you right now.ogg",length=1.950}}
 L["i rate the experience a six point five out of ten"]={{path="chatsounds/autoadd/random_chatsounds/i rate the experience a six point five out of ten.ogg",length=4.458}}
-L["i will stone cold study your vagina ass right here in the pastry aisle"]={{path="chatsounds/autoadd/random_chatsounds/i will stone cold study your vagina ass right here in the pastry aisle.ogg",length=3.429}}
+L["i will stone cold stunner your vagina ass right here in the pastry aisle"]={{path="chatsounds/autoadd/random_chatsounds/i will stone cold stunner your vagina ass right here in the pastry aisle.ogg",length=3.429}}
 L["im an artist"]={{path="chatsounds/autoadd/random_chatsounds/im an artist.ogg",length=1.207}}
 L["jesus christ god"]={{path="chatsounds/autoadd/random_chatsounds/jesus christ god.ogg",length=2.624}}
 L["kill this arrogant moth er fu cker"]={{path="chatsounds/autoadd/random_chatsounds/kill this arrogant moth er fu cker.ogg",length=1.858}}
