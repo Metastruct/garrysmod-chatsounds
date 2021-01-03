92984c28c37c9a6d30d0e9bd29a4d660edb5f29b
Meta Automator <MetaAutomator@users.noreply.github.com>
2021-01-03 22:09:06 +0000

    chatsounds preprocessor (83506a20a4c609bb3279005164fcd12a484b8d7f)
    [ci skip]

---
+++ lua/chatsounds/lists_nosend/newports.lua
@@ -0,0 +1,5 @@
+c.StartList("newports")
+L["my favorite number is 7 7 11 bitch"]={{path="chatsounds/autoadd/newports/my favorite number is 7 7 11 bitch.ogg",length=3.096}}
+L["newports"]={{path="chatsounds/autoadd/newports/newports.ogg",length=1.016}}
+L["oh my god"]={{path="chatsounds/autoadd/newports/oh my god.ogg",length=1.148}}
+c.EndList()
