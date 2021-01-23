394009ff7fe60a467ca4a9a50fe30c0353d612f8
Carl Roberts <carldavidroberts1996@gmail.com>
2021-01-23 02:10:09 +0000

    added more payday 2 sounds
    added by request

--- lua/chatsounds/lists_nosend/payday_2.lua
+++ lua/chatsounds/lists_nosend/payday_2.lua
@@ -7,5 +7,14 @@ L["cloakerscream"]={{path="chatsounds/autoadd/payday_2/cloakerscream.ogg",length
 L["donacdum"]={{path="chatsounds/autoadd/payday_2/donacdum/donacdum1.ogg",length=0.890},{path="chatsounds/autoadd/payday_2/donacdum/donacdum2.ogg",length=0.920}}
 L["guys the thermal drill go get it"]={{path="chatsounds/autoadd/payday_2/guys the thermal drill go get it.ogg",length=2.380}}
 L["i need a medicbag"]={{path="chatsounds/autoadd/payday_2/i need a medicbag.ogg",length=2.088}}
+L["imma drop bomb these shitheads like a fucking piano"]={{path="chatsounds/autoadd/payday_2/imma drop bomb these shitheads like a fucking piano.ogg",length=3.214}}
+L["just call me lightning bolt"]={{path="chatsounds/autoadd/payday_2/just call me lightning bolt/1.ogg",length=1.863},{path="chatsounds/autoadd/payday_2/just call me lightning bolt/2.ogg",length=1.883}}
+L["lightning bolt lightning bolt"]={{path="chatsounds/autoadd/payday_2/lightning bolt lightning bolt/1.ogg",length=1.783},{path="chatsounds/autoadd/payday_2/lightning bolt lightning bolt/2.ogg",length=1.755}}
+L["lightning bolt lightning bolt lightning bolt"]={{path="chatsounds/autoadd/payday_2/lightning bolt lightning bolt lightning bolt/1.ogg",length=2.955},{path="chatsounds/autoadd/payday_2/lightning bolt lightning bolt lightning bolt/2.ogg",length=2.448}}
+L["say goodnight mc murphy"]={{path="chatsounds/autoadd/payday_2/say goodnight mc murphy.ogg",length=1.639}}
+L["shit just got real"]={{path="chatsounds/autoadd/payday_2/shit just got real.ogg",length=1.778}}
 L["somebody drop a medicbag"]={{path="chatsounds/autoadd/payday_2/somebody drop a medicbag.ogg",length=1.890}}
+L["tased"]={{path="chatsounds/autoadd/payday_2/tased.ogg",length=1.482}}
+L["taser charging"]={{path="chatsounds/autoadd/payday_2/taser charging.ogg",length=1.462}}
+L["youre up against the wall and i am the fucking wall"]={{path="chatsounds/autoadd/payday_2/youre up against the wall and i am the fucking wall.ogg",length=3.488}}
 c.EndList()
