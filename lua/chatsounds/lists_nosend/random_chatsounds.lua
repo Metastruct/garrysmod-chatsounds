8cf62310f44a00f6583076da3fcfa607b059cec4
Meta Automator <MetaAutomator@users.noreply.github.com>
2021-02-14 15:28:25 +0000

    chatsounds preprocessor (ad310185a6aeefc77569aa753adf26ce7f73d3c9)
    [ci skip]

--- lua/chatsounds/lists_nosend/random_chatsounds.lua
+++ lua/chatsounds/lists_nosend/random_chatsounds.lua
@@ -1,7 +1,9 @@
 c.StartList("random_chatsounds")
+L["and push to stop"]={{path="chatsounds/autoadd/random_chatsounds/and push to stop.ogg",length=0.847}}
 L["are you stupid"]={{path="chatsounds/autoadd/random_chatsounds/are you stupid.ogg",length=1.033}}
 L["big cock"]={{path="chatsounds/autoadd/random_chatsounds/big cock.ogg",length=1.255}}
 L["but no cum"]={{path="chatsounds/autoadd/random_chatsounds/but no cum.ogg",length=1.197}}
+L["but thats not how it works"]={{path="chatsounds/autoadd/random_chatsounds/but thats not how it works.ogg",length=1.099}}
 L["can not stop will not stop"]={{path="chatsounds/autoadd/random_chatsounds/can not stop will not stop.ogg",length=1.927}}
 L["deep dark fantasies"]={{path="chatsounds/autoadd/random_chatsounds/deep dark fantasies.ogg",length=2.438}}
 L["duck has stopped working"]={{path="chatsounds/autoadd/random_chatsounds/duck has stopped working.ogg",length=87.575}}
@@ -10,21 +12,27 @@ L["fucking stop"]={{path="chatsounds/autoadd/random_chatsounds/fucking stop.ogg"
 L["get over it"]={{path="chatsounds/autoadd/random_chatsounds/get over it.ogg",length=1.260}}
 L["ggiimmpp"]={{path="chatsounds/autoadd/random_chatsounds/ggiimmpp.ogg",length=3.077}}
 L["gimp"]={{path="chatsounds/autoadd/random_chatsounds/gimp.ogg",length=0.923}}
+L["has too many sides"]={{path="chatsounds/autoadd/random_chatsounds/has too many sides.ogg",length=1.742}}
 L["hey shitass wanna see me speedbridge"]={{path="chatsounds/autoadd/random_chatsounds/hey shitass wanna see me speedbridge.ogg",length=2.870}}
 L["i am alan the annihilator bitch"]={{path="chatsounds/autoadd/random_chatsounds/i am alan the annihilator bitch.ogg",length=2.047}}
 L["i come to you right now"]={{path="chatsounds/autoadd/random_chatsounds/i come to you right now.ogg",length=1.544}}
 L["i have crippling depression"]={{path="chatsounds/autoadd/random_chatsounds/i have crippling depression.ogg",length=2.565}}
+L["i just created mark"]={{path="chatsounds/autoadd/random_chatsounds/i just created mark.ogg",length=0.969}}
 L["i kill you right now"]={{path="chatsounds/autoadd/random_chatsounds/i kill you right now.ogg",length=1.950}}
 L["i rate the experience a six point five out of ten"]={{path="chatsounds/autoadd/random_chatsounds/i rate the experience a six point five out of ten.ogg",length=4.458}}
 L["i will stone cold stunner your vagina ass right here in the pastry aisle"]={{path="chatsounds/autoadd/random_chatsounds/i will stone cold stunner your vagina ass right here in the pastry aisle.ogg",length=3.429}}
 L["im an artist"]={{path="chatsounds/autoadd/random_chatsounds/im an artist.ogg",length=1.207}}
 L["jesus christ god"]={{path="chatsounds/autoadd/random_chatsounds/jesus christ god.ogg",length=2.624}}
 L["kill this arrogant moth er fu cker"]={{path="chatsounds/autoadd/random_chatsounds/kill this arrogant moth er fu cker.ogg",length=1.858}}
+L["mark 1"]={{path="chatsounds/autoadd/random_chatsounds/mark 1.ogg",length=0.977}}
+L["mark 2"]={{path="chatsounds/autoadd/random_chatsounds/mark 2.ogg",length=0.904}}
+L["mark 3"]={{path="chatsounds/autoadd/random_chatsounds/mark 3.ogg",length=1.075}}
 L["no i want to pick up the food i put in my butt"]={{path="chatsounds/autoadd/random_chatsounds/no i want to pick up the food i put in my butt.ogg",length=2.949}}
 L["russian scream"]={{path="chatsounds/autoadd/random_chatsounds/russian scream.ogg",length=1.995}}
 L["stop"]={{path="chatsounds/autoadd/random_chatsounds/stop.ogg",length=1.005}}
 L["stop fucking stop"]={{path="chatsounds/autoadd/random_chatsounds/stop fucking stop.ogg",length=1.802}}
 L["stupid bitch"]={{path="chatsounds/autoadd/random_chatsounds/stupid bitch.ogg",length=1.054}}
+L["too many sides"]={{path="chatsounds/autoadd/random_chatsounds/too many sides/1.ogg",length=0.985},{path="chatsounds/autoadd/random_chatsounds/too many sides/2.ogg",length=1.335}}
 L["typical politician"]={{path="chatsounds/autoadd/random_chatsounds/typical politician.ogg",length=1.460}}
 L["uagh"]={{path="chatsounds/autoadd/random_chatsounds/uagh.ogg",length=0.723}}
 L["wat"]={{path="chatsounds/autoadd/random_chatsounds/wat.ogg",length=0.147}}
