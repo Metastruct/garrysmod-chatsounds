b9a0ed0b6f42beac49a364da31000609ec492099
Meta Automator <MetaAutomator@users.noreply.github.com>
2021-05-14 22:57:06 +0000

    chatsounds preprocessor (8b3108449ec1f83084d5212a27ef78c19bf4919b)
    [ci skip]

---
+++ lua/chatsounds/lists_nosend/true_crime.lua
@@ -0,0 +1,4 @@
+c.StartList("true_crime")
+L["the fuck"]={{path="chatsounds/autoadd/true_crime/the fuck.ogg",length=0.443}}
+L["what the fuck"]={{path="chatsounds/autoadd/true_crime/what the fuck.ogg",length=0.732}}
+c.EndList()
