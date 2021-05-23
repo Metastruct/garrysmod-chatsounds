e50bea88df5680a965b4d21473461692d10cc3d0
bjørn tønnesen <bjoern90@hotmail.com>
2021-05-23 10:19:22 +0200

    eugh this time im 100% sure it should be okay

---
+++ lua/chatsounds/lists_nosend/jeff_dunham.lua
@@ -0,0 +1,29 @@
+c.StartList("jeff_dunham")
+L["2 jews walking in a bar"]={{path="chatsounds/autoadd/jeff_dunham/2 jews walking in a bar.ogg",length=2.032}}
+L["are you my virgins"]={{path="chatsounds/autoadd/jeff_dunham/are you my virgins.ogg",length=1.068}}
+L["chinese waaa"]={{path="chatsounds/autoadd/jeff_dunham/chinese waaa.ogg",length=1.010}}
+L["dude you need a tictac"]={{path="chatsounds/autoadd/jeff_dunham/dude you need a tictac.ogg",length=1.520}}
+L["gay guy naaa"]={{path="chatsounds/autoadd/jeff_dunham/gay guy naaa.ogg",length=1.091}}
+L["german jawohl"]={{path="chatsounds/autoadd/jeff_dunham/german jawohl.ogg",length=0.998}}
+L["ha ha haaa"]={{path="chatsounds/autoadd/jeff_dunham/ha ha haaa.ogg",length=1.027}}
+L["heloooooo"]={{path="chatsounds/autoadd/jeff_dunham/heloooooo.ogg",length=1.399}}
+L["holy crap"]={{path="chatsounds/autoadd/jeff_dunham/holy crap.ogg",length=0.616}}
+L["holy crap youre quick"]={{path="chatsounds/autoadd/jeff_dunham/holy crap youre quick.ogg",length=1.382}}
+L["i hope not"]={{path="chatsounds/autoadd/jeff_dunham/i hope not.ogg",length=0.911}}
+L["i kill you"]={{path="chatsounds/autoadd/jeff_dunham/i kill you.ogg",length=1.691}}
+L["i would toss a penny between them and watch them fight to the death"]={{path="chatsounds/autoadd/jeff_dunham/i would toss a penny between them and watch them fight to the death.ogg",length=3.355}}
+L["im a suicide bomber"]={{path="chatsounds/autoadd/jeff_dunham/im a suicide bomber.ogg",length=1.788}}
+L["its a flesh wound"]={{path="chatsounds/autoadd/jeff_dunham/its a flesh wound.ogg",length=0.923}}
+L["knock knock whos there me i kill you"]={{path="chatsounds/autoadd/jeff_dunham/knock knock whos there me i kill you.ogg",length=2.415}}
+L["made in china"]={{path="chatsounds/autoadd/jeff_dunham/made in china.ogg",length=0.836}}
+L["oh im sorry"]={{path="chatsounds/autoadd/jeff_dunham/oh im sorry.ogg",length=1.858}}
+L["ole"]={{path="chatsounds/autoadd/jeff_dunham/ole.ogg",length=0.987}}
+L["silence i kill you"]={{path="chatsounds/autoadd/jeff_dunham/silence i kill you.ogg",length=3.251}}
+L["suicide hotline"]={{path="chatsounds/autoadd/jeff_dunham/suicide hotline.ogg",length=1.355}}
+L["theyre laughing like hell"]={{path="chatsounds/autoadd/jeff_dunham/theyre laughing like hell.ogg",length=1.811}}
+L["too late hahaha so jose are you legal are you legal legal legal legaloow"]={{path="chatsounds/autoadd/jeff_dunham/too late hahaha so jose are you legal are you legal legal legal legaloow.ogg",length=6.177}}
+L["wait if i am dead gasp that means i can get my 72 virgins"]={{path="chatsounds/autoadd/jeff_dunham/wait if i am dead gasp that means i can get my 72 virgins.ogg",length=4.333}}
+L["what you dont lets jews in your bar"]={{path="chatsounds/autoadd/jeff_dunham/what you dont lets jews in your bar.ogg",length=1.727}}
+L["you idiot you dont have an ass"]={{path="chatsounds/autoadd/jeff_dunham/you idiot you dont have an ass.ogg",length=2.020}}
+L["you really are an idiot"]={{path="chatsounds/autoadd/jeff_dunham/you really are an idiot.ogg",length=1.753}}
+c.EndList()
---
+++ lua/chatsounds/lists_nosend/jim_carrey.lua
@@ -0,0 +1,44 @@
+c.StartList("jim_carrey")
+L["a tommy gun"]={{path="chatsounds/autoadd/jim_carrey/a tommy gun.ogg",length=1.544}}
+L["aiaiaiaiai"]={{path="chatsounds/autoadd/jim_carrey/aiaiaiaiai.ogg",length=1.239}}
+L["cant make a scene if you have the green"]={{path="chatsounds/autoadd/jim_carrey/cant make a scene if you have the green.ogg",length=1.962}}
+L["did you miss me"]={{path="chatsounds/autoadd/jim_carrey/did you miss me.ogg",length=1.242}}
+L["ding ding ding what do we have for her johnny"]={{path="chatsounds/autoadd/jim_carrey/ding ding ding what do we have for her johnny.ogg",length=2.868}}
+L["here she comes to wreck the day"]={{path="chatsounds/autoadd/jim_carrey/here she comes to wreck the day.ogg",length=2.612}}
+L["hold on sugar daddys got a sweet tooth tonight"]={{path="chatsounds/autoadd/jim_carrey/hold on sugar daddys got a sweet tooth tonight.ogg",length=4.222}}
+L["how do you sleep at night"]={{path="chatsounds/autoadd/jim_carrey/how do you sleep at night.ogg",length=1.049}}
+L["i better make a little stop"]={{path="chatsounds/autoadd/jim_carrey/i better make a little stop.ogg",length=1.264}}
+L["i cant lie"]={{path="chatsounds/autoadd/jim_carrey/i cant lie.ogg",length=2.276}}
+L["i did it you hear and im glad glad i tell you"]={{path="chatsounds/autoadd/jim_carrey/i did it you hear and im glad glad i tell you.ogg",length=2.403}}
+L["i guess not"]={{path="chatsounds/autoadd/jim_carrey/i guess not.ogg",length=1.068}}
+L["i just proposed a settlement to dick with them gasps"]={{path="chatsounds/autoadd/jim_carrey/i just proposed a settlement to dick with them gasps.ogg",length=2.601}}
+L["i love itt"]={{path="chatsounds/autoadd/jim_carrey/i love itt.ogg",length=1.753}}
+L["i mean i wanna squeeze em gasps"]={{path="chatsounds/autoadd/jim_carrey/i mean i wanna squeeze em gasps.ogg",length=2.624}}
+L["i mean your boobs are huge"]={{path="chatsounds/autoadd/jim_carrey/i mean your boobs are huge.ogg",length=1.834}}
+L["if i was a boxer i would bounce those things like sugar ray leonard"]={{path="chatsounds/autoadd/jim_carrey/if i was a boxer i would bounce those things like sugar ray leonard.ogg",length=4.133}}
+L["im a little upset about a bad sexual episode last night"]={{path="chatsounds/autoadd/jim_carrey/im a little upset about a bad sexual episode last night.ogg",length=3.135}}
+L["im kicking my ass do you mind"]={{path="chatsounds/autoadd/jim_carrey/im kicking my ass do you mind.ogg",length=4.063}}
+L["it was me"]={{path="chatsounds/autoadd/jim_carrey/it was me.ogg",length=1.533}}
+L["its party time p a r t y why because i gotta"]={{path="chatsounds/autoadd/jim_carrey/its party time p a r t y why because i gotta.ogg",length=6.153}}
+L["jerk off"]={{path="chatsounds/autoadd/jim_carrey/jerk off.ogg",length=0.929}}
+L["jim carry fast speak"]={{path="chatsounds/autoadd/jim_carrey/jim carry fast speak.ogg",length=6.536}}
+L["jim judging"]={{path="chatsounds/autoadd/jim_carrey/jim judging.ogg",length=6.536}}
+L["look ma im roadkill"]={{path="chatsounds/autoadd/jim_carrey/look ma im roadkill.ogg",length=2.334}}
+L["mask horn"]={{path="chatsounds/autoadd/jim_carrey/mask horn.ogg",length=4.203}}
+L["no no no no this cant happen"]={{path="chatsounds/autoadd/jim_carrey/no no no no this cant happen.ogg",length=2.984}}
+L["now you have to ask yourself one question do i feel luck well do you punks"]={{path="chatsounds/autoadd/jim_carrey/now you have to ask yourself one question do i feel luck well do you punks.ogg",length=9.067}}
+L["p a r t y"]={{path="chatsounds/autoadd/jim_carrey/p a r t y.ogg",length=1.184}}
+L["prenuptial agreements"]={{path="chatsounds/autoadd/jim_carrey/prenuptial agreements.ogg",length=3.437}}
+L["shes so coy"]={{path="chatsounds/autoadd/jim_carrey/shes so coy.ogg",length=1.207}}
+L["sorry fellas waste not want not"]={{path="chatsounds/autoadd/jim_carrey/sorry fellas waste not want not.ogg",length=2.055}}
+L["sputtering nonsense syllables"]={{path="chatsounds/autoadd/jim_carrey/sputtering nonsense syllables.ogg",length=5.503}}
+L["sssomebody stop me"]={{path="chatsounds/autoadd/jim_carrey/sssomebody stop me.ogg",length=2.264}}
+L["sssssmoking"]={{path="chatsounds/autoadd/jim_carrey/sssssmoking.ogg",length=2.461}}
+L["stop breakin the law asshole"]={{path="chatsounds/autoadd/jim_carrey/stop breakin the law asshole.ogg",length=2.856}}
+L["thats a spicy meatball"]={{path="chatsounds/autoadd/jim_carrey/thats a spicy meatball.ogg",length=2.777}}
+L["the pen is blue the pen is blue the goddamn pen is blue"]={{path="chatsounds/autoadd/jim_carrey/the pen is blue the pen is blue the goddamn pen is blue.ogg",length=6.502}}
+L["there you go son now get out of here you bother me"]={{path="chatsounds/autoadd/jim_carrey/there you go son now get out of here you bother me.ogg",length=1.869}}
+L["theres noooooo such thing as a weaker sex"]={{path="chatsounds/autoadd/jim_carrey/theres noooooo such thing as a weaker sex.ogg",length=5.457}}
+L["this guys incorrigible"]={{path="chatsounds/autoadd/jim_carrey/this guys incorrigible.ogg",length=1.904}}
+L["well thats because you have big jugs"]={{path="chatsounds/autoadd/jim_carrey/well thats because you have big jugs.ogg",length=2.485}}
+c.EndList()
