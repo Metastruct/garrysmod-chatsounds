f10b153c15b6a08b093370ae92e1bd326b3c70d5
MaxSpec <maxspec@bk.ru>
2021-06-09 20:42:43 +0500

    stupid travis

--- lua/chatsounds/lists_nosend/random_chatsounds.lua
+++ lua/chatsounds/lists_nosend/random_chatsounds.lua
@@ -1,5 +1,6 @@
 c.StartList("random_chatsounds")
 L["are you stupid"]={{path="chatsounds/autoadd/random_chatsounds/are you stupid.ogg",length=1.033}}
+L["bitch oeh"]={{path="chatsounds/autoadd/random_chatsounds/bitch oeh.ogg",length=1.315}}
 L["can not stop will not stop"]={{path="chatsounds/autoadd/random_chatsounds/can not stop will not stop.ogg",length=1.927}}
 L["deep dark fantasies"]={{path="chatsounds/autoadd/random_chatsounds/deep dark fantasies.ogg",length=2.438}}
 L["duck has stopped working"]={{path="chatsounds/autoadd/random_chatsounds/duck has stopped working.ogg",length=87.575}}
@@ -7,13 +8,18 @@ L["flbfllflfllahflbflblfflflfl"]={{path="chatsounds/autoadd/random_chatsounds/fl
 L["get over it"]={{path="chatsounds/autoadd/random_chatsounds/get over it.ogg",length=1.260}}
 L["ggiimmpp"]={{path="chatsounds/autoadd/random_chatsounds/ggiimmpp.ogg",length=3.077}}
 L["gimp"]={{path="chatsounds/autoadd/random_chatsounds/gimp.ogg",length=0.923}}
+L["hey shitass wanna see me speedbridge"]={{path="chatsounds/autoadd/random_chatsounds/hey shitass wanna see me speedbridge.ogg",length=2.870}}
 L["i come to you right now"]={{path="chatsounds/autoadd/random_chatsounds/i come to you right now.ogg",length=1.544}}
 L["i have crippling depression"]={{path="chatsounds/autoadd/random_chatsounds/i have crippling depression.ogg",length=2.565}}
 L["i kill you right now"]={{path="chatsounds/autoadd/random_chatsounds/i kill you right now.ogg",length=1.950}}
 L["i rate the experience a six point five out of ten"]={{path="chatsounds/autoadd/random_chatsounds/i rate the experience a six point five out of ten.ogg",length=4.458}}
 L["im an artist"]={{path="chatsounds/autoadd/random_chatsounds/im an artist.ogg",length=1.207}}
 L["jesus christ god"]={{path="chatsounds/autoadd/random_chatsounds/jesus christ god.ogg",length=2.624}}
+L["kill this arrogant moth er fu cker"]={{path="chatsounds/autoadd/random_chatsounds/kill this arrogant moth er fu cker.ogg",length=1.858}}
 L["no i want to pick up the food i put in my butt"]={{path="chatsounds/autoadd/random_chatsounds/no i want to pick up the food i put in my butt.ogg",length=2.949}}
+L["overused thud2"]={{path="chatsounds/autoadd/random_chatsounds/overused thud2.ogg",length=2.834}}
+L["russian scream"]={{path="chatsounds/autoadd/random_chatsounds/russian scream.ogg",length=1.995}}
+L["stupid bitch"]={{path="chatsounds/autoadd/random_chatsounds/stupid bitch.ogg",length=1.054}}
 L["uagh"]={{path="chatsounds/autoadd/random_chatsounds/uagh.ogg",length=0.723}}
 L["we do this you know one on one"]={{path="chatsounds/autoadd/random_chatsounds/we do this you know one on one.ogg",length=1.962}}
 L["what about the negro noodle"]={{path="chatsounds/autoadd/random_chatsounds/what about the negro noodle.ogg",length=1.718}}
