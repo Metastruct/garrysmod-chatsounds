c8837546ce8bd3fb78b2a95be9895b35a95c084e
Niccep <43934118+0x098@users.noreply.github.com>
2021-06-21 21:58:07 +0300

    tiky, bogos pinted, stop posting about among us, marine

---
+++ lua/chatsounds/lists_nosend/bogos_pinted.lua
@@ -0,0 +1,7 @@
+c.StartList("bogos_pinted")
+L["bogos pinted"]={{path="chatsounds/autoadd/bogos_pinted/bogos pinted.ogg",length=0.472}}
+L["cavesound"]={{path="chatsounds/autoadd/bogos_pinted/cavesound.ogg",length=0.588}}
+L["hey just wondering if you got your photos printed"]={{path="chatsounds/autoadd/bogos_pinted/hey just wondering if you got your photos printed.ogg",length=1.798}}
+L["hey just wondering if you got your photos printed bogos pinted what cavesound"]={{path="chatsounds/autoadd/bogos_pinted/hey just wondering if you got your photos printed bogos pinted what cavesound.ogg",length=4.378}}
+L["what"]={{path="chatsounds/autoadd/bogos_pinted/what.ogg",length=0.365}}
+c.EndList()
