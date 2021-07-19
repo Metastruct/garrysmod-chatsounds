1022d6acd5f1ee46f0e219cb720883986ebea914
MaxSpec <maxspec@bk.ru>
2021-07-19 16:28:24 +0500

    stupid dog you make me look bad
    stupid dog you make me look bad2
    stupid dog you make me look bad

---
+++ lua/chatsounds/lists_nosend/courage_the_cowardly_dog.lua
@@ -0,0 +1,3 @@
+c.StartList("courage_the_cowardly_dog")
+L["stupid dog you make me look bad"]={{path="chatsounds/autoadd/courage_the_cowardly_dog/stupid dog you make me look bad.ogg",length=2.301}}
+c.EndList()
