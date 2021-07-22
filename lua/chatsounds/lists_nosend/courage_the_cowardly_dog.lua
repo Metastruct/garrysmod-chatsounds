336050cab39bbd018e94a7dde81f57a88176ee29
Mikeai <mgamkrel@gmail.com>
2021-07-22 09:08:48 +0100

    Stupid MaxSpec, you make me look bad
    you had one job

--- lua/chatsounds/lists_nosend/courage_the_cowardly_dog.lua
+++ lua/chatsounds/lists_nosend/courage_the_cowardly_dog.lua
@@ -1,3 +1,3 @@
 c.StartList("courage_the_cowardly_dog")
-L["stupid dog you make look bad"]={{path="chatsounds/autoadd/courage_the_cowardly_dog/stupid dog you make look bad.ogg",length=2.301}}
+L["stupid dog you make me look bad"]={{path="chatsounds/autoadd/courage_the_cowardly_dog/stupid dog you make me look bad.ogg",length=2.301}}
 c.EndList()
