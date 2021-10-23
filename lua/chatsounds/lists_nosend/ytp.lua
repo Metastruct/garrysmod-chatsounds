7735cd6509d9177927d28e998d61235f0df54d4d
Meta Automator <MetaAutomator@users.noreply.github.com>
2021-10-23 12:53:13 +0000

    Commit from GitHub Actions (check sounds and update lists)

--- lua/chatsounds/lists_nosend/ytp.lua
+++ lua/chatsounds/lists_nosend/ytp.lua
@@ -4,11 +4,15 @@ L["23 17 23 46 17 1234673147224146231327 1 1 1 1"]={{path="chatsounds/autoadd/yt
 L["all star poot"]={{path="chatsounds/autoadd/ytp/all star poot.ogg",length=3.789}}
 L["alright dickhead your time is up"]={{path="chatsounds/autoadd/ytp/alright dickhead your time is up.ogg",length=3.828}}
 L["back in texas we call ice cream frozen cock sex"]={{path="chatsounds/autoadd/ytp/back in texas we call ice cream frozen cock sex.ogg",length=3.183}}
+L["cock ass"]={{path="chatsounds/autoadd/ytp/cock ass.ogg",length=1.084}}
 L["colorado is in italy"]={{path="chatsounds/autoadd/ytp/colorado is in italy.ogg",length=2.539}}
 L["die"]={{path="chatsounds/autoadd/ytp/die.ogg",length=0.576}}
 L["dont wanna be an american"]={{path="chatsounds/autoadd/ytp/dont wanna be an american.ogg",length=5.649}}
 L["dr boob"]={{path="chatsounds/autoadd/ytp/dr boob.ogg",length=0.977}}
-L["fuck you"]={{path="chatsounds/autoadd/ytp/fuck you.ogg",length=0.847}}
+L["fuck you"]={{path="chatsounds/autoadd/ytp/fuck you.ogg",length=0.962}}
+L["fuckin"]={{path="chatsounds/autoadd/ytp/fuckin.ogg",length=0.645}}
+L["fucking cock ass"]={{path="chatsounds/autoadd/ytp/fucking cock ass.ogg",length=1.911}}
+L["fucking fuck yeah"]={{path="chatsounds/autoadd/ytp/fucking fuck yeah.ogg",length=1.668}}
 L["get the fuck out"]={{path="chatsounds/autoadd/ytp/get the fuck out.ogg",length=1.043}}
 L["gunshot"]={{path="chatsounds/autoadd/ytp/gunshot.ogg",length=1.340}}
 L["i dont give a flying fuck"]={{path="chatsounds/autoadd/ytp/i dont give a flying fuck.ogg",length=1.821}}
@@ -22,6 +26,7 @@ L["no"]={{path="chatsounds/autoadd/ytp/no.ogg",length=0.736}}
 L["now prisoner 24602060451"]={{path="chatsounds/autoadd/ytp/now prisoner 24602060451.ogg",length=3.487}}
 L["open your ass big and wide"]={{path="chatsounds/autoadd/ytp/open your ass big and wide.ogg",length=2.318}}
 L["poot poot poot poot poot poot"]={{path="chatsounds/autoadd/ytp/poot poot poot poot poot poot.ogg",length=1.366}}
+L["put two of them up your ass"]={{path="chatsounds/autoadd/ytp/put two of them up your ass.ogg",length=3.215}}
 L["s t f u"]={{path="chatsounds/autoadd/ytp/s t f u.ogg",length=2.073}}
 L["sees sees"]={{path="chatsounds/autoadd/ytp/sees sees.ogg",length=0.837}}
 L["shit"]={{path="chatsounds/autoadd/ytp/shit.ogg",length=0.603}}
@@ -40,8 +45,12 @@ L["wwwww oo o o"]={{path="chatsounds/autoadd/ytp/wwwww oo o o.ogg",length=1.163}
 L["you are a cunnnnnt"]={{path="chatsounds/autoadd/ytp/you are a cunnnnnt.ogg",length=3.775}}
 L["you are a n"]={{path="chatsounds/autoadd/ytp/you are a n.ogg",length=2.420}}
 L["you are going to want to tell your friends racial slurs"]={{path="chatsounds/autoadd/ytp/you are going to want to tell your friends racial slurs.ogg",length=4.392}}
+L["you fucking bitch"]={{path="chatsounds/autoadd/ytp/you fucking bitch.ogg",length=1.388}}
+L["you fucking dick"]={{path="chatsounds/autoadd/ytp/you fucking dick.ogg",length=1.071}}
+L["you fucking shit"]={{path="chatsounds/autoadd/ytp/you fucking shit.ogg",length=1.169}}
 L["you have to fuck me in the ass a a a a a a"]={{path="chatsounds/autoadd/ytp/you have to fuck me in the ass a a a a a a.ogg",length=3.271}}
 L["you have to put your dick in the cd distorted"]={{path="chatsounds/autoadd/ytp/you have to put your dick in the cd distorted.ogg",length=2.561}}
+L["you piece of shit"]={{path="chatsounds/autoadd/ytp/you piece of shit.ogg",length=1.023}}
 L["youtube poop is funny"]={{path="chatsounds/autoadd/ytp/youtube poop is funny.ogg",length=1.409}}
 L["yuuy yuuy"]={{path="chatsounds/autoadd/ytp/yuuy yuuy.ogg",length=0.877}}
 c.EndList()
