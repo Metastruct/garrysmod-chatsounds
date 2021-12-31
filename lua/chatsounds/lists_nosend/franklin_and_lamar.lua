a4b2d8f289602d496ee8969eb1a50ca11d343b6b
Meta Automator <MetaAutomator@users.noreply.github.com>
2021-12-31 11:55:21 +0000

    Commit from GitHub Actions (Push Action)

---
+++ lua/chatsounds/lists_nosend/franklin_and_lamar.lua
@@ -0,0 +1,14 @@
+c.StartList("franklin_and_lamar")
+L["ah n dont hate me cause im beautiful"]={{path="chatsounds/autoadd/franklin_and_lamar/ah n dont hate me cause im beautiful.ogg",length=1.903}}
+L["augh mf"]={{path="chatsounds/autoadd/franklin_and_lamar/augh mf.ogg",length=2.222}}
+L["barbershopsavailable"]={{path="chatsounds/autoadd/franklin_and_lamar/barbershopsavailable.ogg",length=0.372}}
+L["franklinwhat"]={{path="chatsounds/autoadd/franklin_and_lamar/franklinwhat.ogg",length=0.493}}
+L["if she ever stop fing with that brain surgeon or lawyer she fing with"]={{path="chatsounds/autoadd/franklin_and_lamar/if she ever stop fing with that brain surgeon or lawyer she fing with.ogg",length=3.145}}
+L["lamarn"]={{path="chatsounds/autoadd/franklin_and_lamar/lamarn.ogg",length=1.368}}
+L["man f you ill see you at work"]={{path="chatsounds/autoadd/franklin_and_lamar/man f you ill see you at work.ogg",length=1.981}}
+L["maybe if you got rid of that old yee yee as haircut you got"]={{path="chatsounds/autoadd/franklin_and_lamar/maybe if you got rid of that old yee yee as haircut you got.ogg",length=2.652}}
+L["maybe tanisha would call your dog a"]={{path="chatsounds/autoadd/franklin_and_lamar/maybe tanisha would call your dog a.ogg",length=1.990}}
+L["oh better yet"]={{path="chatsounds/autoadd/franklin_and_lamar/oh better yet.ogg",length=1.012}}
+L["wassup can a loc come up in yo crib"]={{path="chatsounds/autoadd/franklin_and_lamar/wassup can a loc come up in yo crib.ogg",length=1.918}}
+L["youd get some b on your dick"]={{path="chatsounds/autoadd/franklin_and_lamar/youd get some b on your dick.ogg",length=1.457}}
+c.EndList()
