4267ca4ea4f376d20a5459c7a672a7d063b771ef
Meta Automator <MetaAutomator@users.noreply.github.com>
2022-03-29 01:50:13 +0000

    Commit from GitHub Actions (Push Action)

---
+++ lua/chatsounds/lists_nosend/sterbuck.lua
@@ -0,0 +1,15 @@
+c.StartList("sterbuck")
+L["en in plaats van dat ze me een rietje geven voor me drinken"]={{path="chatsounds/autoadd/sterbuck/en in plaats van dat ze me een rietje geven voor me drinken.ogg",length=2.695}}
+L["geven ze me een"]={{path="chatsounds/autoadd/sterbuck/geven ze me een.ogg",length=0.869}}
+L["hoe de kanker moet ik met deze"]={{path="chatsounds/autoadd/sterbuck/hoe de kanker moet ik met deze.ogg",length=1.573}}
+L["ik heb toch betaald voor het kankerrietje"]={{path="chatsounds/autoadd/sterbuck/ik heb toch betaald voor het kankerrietje.ogg",length=2.309}}
+L["ik was net bij de sterbuck"]={{path="chatsounds/autoadd/sterbuck/ik was net bij de sterbuck.ogg",length=1.687}}
+L["jongens"]={{path="chatsounds/autoadd/sterbuck/jongens.ogg",length=0.483}}
+L["kanker drinken drinken"]={{path="chatsounds/autoadd/sterbuck/kanker drinken drinken.ogg",length=1.751}}
+L["kanker vork"]={{path="chatsounds/autoadd/sterbuck/kanker vork/kanker vork 1.ogg",length=1.287},{path="chatsounds/autoadd/sterbuck/kanker vork/kanker vork 2.ogg",length=1.028}}
+L["me"]={{path="chatsounds/autoadd/sterbuck/me.ogg",length=0.340}}
+L["me kankerrietje"]={{path="chatsounds/autoadd/sterbuck/me kankerrietje.ogg",length=1.929}}
+L["oh kanker"]={{path="chatsounds/autoadd/sterbuck/oh kanker.ogg",length=1.187}}
+L["vuile starbuck"]={{path="chatsounds/autoadd/sterbuck/vuile starbuck.ogg",length=2.564}}
+L["ze geven me geen"]={{path="chatsounds/autoadd/sterbuck/ze geven me geen.ogg",length=2.007}}
+c.EndList()
