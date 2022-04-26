811b5d2e9cb93a5156ab18462a115143d7557e3c
Ayane <chinodesuuu@gmail.com>
2022-04-27 00:40:56 +0800

    Fix directory structure for halo announcer lines (#359)
    i swear to god

---
+++ lua/chatsounds/lists_nosend/halo_announcer_reach.lua
@@ -0,0 +1,19 @@
+c.StartList("halo_announcer_reach")
+L["double kill"]={{path="chatsounds/autoadd/halo_announcer_reach/double kill.ogg",length=1.399}}
+L["extermination"]={{path="chatsounds/autoadd/halo_announcer_reach/extermination.ogg",length=2.391}}
+L["first strike"]={{path="chatsounds/autoadd/halo_announcer_reach/first strike.ogg",length=1.660}}
+L["killamanjaro"]={{path="chatsounds/autoadd/halo_announcer_reach/killamanjaro.ogg",length=1.843}}
+L["killing frenzy"]={{path="chatsounds/autoadd/halo_announcer_reach/killing frenzy.ogg",length=1.399}}
+L["killing spree"]={{path="chatsounds/autoadd/halo_announcer_reach/killing spree.ogg",length=1.399}}
+L["killionaire"]={{path="chatsounds/autoadd/halo_announcer_reach/killionaire.ogg",length=1.346}}
+L["killjoy"]={{path="chatsounds/autoadd/halo_announcer_reach/killjoy.ogg",length=1.477}}
+L["killpocalypse"]={{path="chatsounds/autoadd/halo_announcer_reach/killpocalypse.ogg",length=1.555}}
+L["killtacular"]={{path="chatsounds/autoadd/halo_announcer_reach/killtacular.ogg",length=1.843}}
+L["killtastrophe"]={{path="chatsounds/autoadd/halo_announcer_reach/killtastrophe.ogg",length=1.947}}
+L["killtrocity"]={{path="chatsounds/autoadd/halo_announcer_reach/killtrocity.ogg",length=1.608}}
+L["overkill"]={{path="chatsounds/autoadd/halo_announcer_reach/overkill.ogg",length=1.346}}
+L["rampage"]={{path="chatsounds/autoadd/halo_announcer_reach/rampage.ogg",length=1.738}}
+L["running riot"]={{path="chatsounds/autoadd/halo_announcer_reach/running riot.ogg",length=1.346}}
+L["triple kill"]={{path="chatsounds/autoadd/halo_announcer_reach/triple kill.ogg",length=1.242}}
+L["yoink"]={{path="chatsounds/autoadd/halo_announcer_reach/yoink.ogg",length=1.399}}
+c.EndList()
