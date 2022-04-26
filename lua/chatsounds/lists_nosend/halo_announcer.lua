78389a915ef1f7fc08a5a56ef020a91ce642a401
Meta Automator <MetaAutomator@users.noreply.github.com>
2022-04-26 07:47:07 +0000

    Commit from GitHub Actions (Push Action)

--- lua/chatsounds/lists_nosend/halo_announcer.lua
+++ lua/chatsounds/lists_nosend/halo_announcer.lua
@@ -1,21 +1,25 @@
 c.StartList("halo_announcer")
-L["double kill"]={{path="chatsounds/autoadd/halo_announcer/double kill.ogg",length=1.125}}
-L["killamanjaro"]={{path="chatsounds/autoadd/halo_announcer/killamanjaro.ogg",length=1.637}}
-L["killing frenzy"]={{path="chatsounds/autoadd/halo_announcer/killing frenzy.ogg",length=2.112}}
-L["killing spree"]={{path="chatsounds/autoadd/halo_announcer/killing spree.ogg",length=1.629}}
-L["killionaire"]={{path="chatsounds/autoadd/halo_announcer/killionaire.ogg",length=2.235}}
-L["killpocalypse"]={{path="chatsounds/autoadd/halo_announcer/killpocalypse.ogg",length=1.540}}
-L["killtacular"]={{path="chatsounds/autoadd/halo_announcer/killtacular.ogg",length=1.526}}
-L["killtastrophe"]={{path="chatsounds/autoadd/halo_announcer/killtastrophe.ogg",length=1.661}}
-L["killtrocity"]={{path="chatsounds/autoadd/halo_announcer/killtrocity.ogg",length=1.731}}
+L["double kill"]={{path="chatsounds/autoadd/halo_announcer/double kill.ogg",length=1.399}}
+L["extermination"]={{path="chatsounds/autoadd/halo_announcer/extermination.ogg",length=2.391}}
+L["first strike"]={{path="chatsounds/autoadd/halo_announcer/first strike.ogg",length=1.660}}
+L["killamanjaro"]={{path="chatsounds/autoadd/halo_announcer/killamanjaro.ogg",length=1.843}}
+L["killing frenzy"]={{path="chatsounds/autoadd/halo_announcer/killing frenzy.ogg",length=1.399}}
+L["killing spree"]={{path="chatsounds/autoadd/halo_announcer/killing spree.ogg",length=1.399}}
+L["killionaire"]={{path="chatsounds/autoadd/halo_announcer/killionaire.ogg",length=1.346}}
+L["killjoy"]={{path="chatsounds/autoadd/halo_announcer/killjoy.ogg",length=1.477}}
+L["killpocalypse"]={{path="chatsounds/autoadd/halo_announcer/killpocalypse.ogg",length=1.555}}
+L["killtacular"]={{path="chatsounds/autoadd/halo_announcer/killtacular.ogg",length=1.843}}
+L["killtastrophe"]={{path="chatsounds/autoadd/halo_announcer/killtastrophe.ogg",length=1.555}}
+L["killtrocity"]={{path="chatsounds/autoadd/halo_announcer/killtrocity.ogg",length=1.608}}
 L["mommy milkers"]={{path="chatsounds/autoadd/halo_announcer/mommy milkers.ogg",length=2.127}}
 L["no maidens"]={{path="chatsounds/autoadd/halo_announcer/no maidens.ogg",length=2.813}}
-L["overkill"]={{path="chatsounds/autoadd/halo_announcer/overkill.ogg",length=1.375}}
-L["rampage"]={{path="chatsounds/autoadd/halo_announcer/rampage.ogg",length=1.657}}
+L["overkill"]={{path="chatsounds/autoadd/halo_announcer/overkill.ogg",length=1.346}}
+L["rampage"]={{path="chatsounds/autoadd/halo_announcer/rampage.ogg",length=1.738}}
 L["redditor"]={{path="chatsounds/autoadd/halo_announcer/redditor.ogg",length=1.309}}
-L["running riot"]={{path="chatsounds/autoadd/halo_announcer/running riot.ogg",length=1.705}}
+L["running riot"]={{path="chatsounds/autoadd/halo_announcer/running riot.ogg",length=1.346}}
 L["skill issue"]={{path="chatsounds/autoadd/halo_announcer/skill issue.ogg",length=2.449}}
 L["social credit deducted"]={{path="chatsounds/autoadd/halo_announcer/social credit deducted.ogg",length=3.445}}
 L["that was sus"]={{path="chatsounds/autoadd/halo_announcer/that was sus.ogg",length=2.575}}
-L["triple kill"]={{path="chatsounds/autoadd/halo_announcer/triple kill.ogg",length=1.250}}
+L["triple kill"]={{path="chatsounds/autoadd/halo_announcer/triple kill.ogg",length=1.242}}
+L["yoink"]={{path="chatsounds/autoadd/halo_announcer/yoink.ogg",length=1.399}}
 c.EndList()
