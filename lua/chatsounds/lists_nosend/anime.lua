599eebe5dc981d837f1e91af03e99383fe8c9304
Meta Automator <MetaAutomator@users.noreply.github.com>
2022-07-18 14:44:40 +0000

    Commit from GitHub Actions (Push Action)

--- lua/chatsounds/lists_nosend/anime.lua
+++ lua/chatsounds/lists_nosend/anime.lua
@@ -1,6 +1,8 @@
 c.StartList("anime")
+L["aaaaahhh"]={{path="chatsounds/autoadd/anime/aaaaahhh.ogg",length=2.123}}
 L["awebo"]={{path="chatsounds/autoadd/anime/awebo.ogg",length=0.576}}
 L["burunyaa"]={{path="chatsounds/autoadd/anime/burunyaa.ogg",length=1.190}}
+L["eurobeat yuri"]={{path="chatsounds/autoadd/anime/eurobeat yuri.ogg",length=26.348}}
 L["gwah"]={{path="chatsounds/autoadd/anime/gwah.ogg",length=0.294}}
 L["hello mister"]={{path="chatsounds/autoadd/anime/hello mister.ogg",length=1.092}}
 L["hm"]={{path="chatsounds/autoadd/anime/hm.ogg",length=0.351}}
@@ -11,5 +13,10 @@ L["mota"]={{path="chatsounds/autoadd/anime/mota.ogg",length=0.570}}
 L["nya song"]={{path="chatsounds/autoadd/anime/nya song.ogg",length=7.411}}
 L["nyanya"]={{path="chatsounds/autoadd/anime/nyanya.ogg",length=0.967}}
 L["oui"]={{path="chatsounds/autoadd/anime/oui/oui 1.ogg",length=0.202},{path="chatsounds/autoadd/anime/oui/oui 2.ogg",length=0.250}}
+L["papa"]={{path="chatsounds/autoadd/anime/papa.ogg",length=0.496}}
+L["show it to me"]={{path="chatsounds/autoadd/anime/show it to me.ogg",length=0.756}}
+L["take it out"]={{path="chatsounds/autoadd/anime/take it out.ogg",length=0.512}}
 L["whyyy"]={{path="chatsounds/autoadd/anime/whyyy.ogg",length=0.930}}
+L["yeah yeah take it out take it out take it out"]={{path="chatsounds/autoadd/anime/yeah yeah take it out take it out take it out.ogg",length=2.027}}
+L["yeah yeah take it out take it out yeah show it to me show it to me papa aaaaahhh"]={{path="chatsounds/autoadd/anime/yeah yeah take it out take it out yeah show it to me show it to me papa aaaaahhh.ogg",length=5.734}}
 c.EndList()
