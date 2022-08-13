f78556eb991c917d77c214f862a7a2c145d10143
Meta Automator <MetaAutomator@users.noreply.github.com>
2022-08-13 00:28:15 +0000

    Commit from GitHub Actions (Push Action)

---
+++ lua/chatsounds/lists_nosend/macos_tts_susan.lua
@@ -0,0 +1,6 @@
+c.StartList("macos_tts_susan")
+L["balls"]={{path="chatsounds/autoadd/macos_tts_susan/balls.ogg",length=0.700}}
+L["capital_x"]={{path="chatsounds/autoadd/macos_tts_susan/capital_x.ogg",length=0.977}}
+L["later"]={{path="chatsounds/autoadd/macos_tts_susan/later.ogg",length=0.609}}
+L["uh_car_e"]={{path="chatsounds/autoadd/macos_tts_susan/uh_car_e.ogg",length=0.853}}
+c.EndList()
