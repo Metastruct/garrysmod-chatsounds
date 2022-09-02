82928981152677767952f19d0ec385044647f056
Meta Automator <MetaAutomator@users.noreply.github.com>
2022-09-02 14:53:06 +0000

    Commit from GitHub Actions (Push Action)

---
+++ lua/chatsounds/lists_nosend/yo_mama.lua
@@ -0,0 +1,7 @@
+c.StartList("yo_mama")
+L["superbowl"]={{path="chatsounds/autoadd/yo_mama/superbowl.ogg",length=1.447}}
+L["the superbowl"]={{path="chatsounds/autoadd/yo_mama/the superbowl.ogg",length=1.833}}
+L["the superbowl yeeeeeeaaah"]={{path="chatsounds/autoadd/yo_mama/the superbowl yeeeeeeaaah.ogg",length=4.320}}
+L["xbox live"]={{path="chatsounds/autoadd/yo_mama/xbox live.ogg",length=3.365}}
+L["yeeeeeeaaah"]={{path="chatsounds/autoadd/yo_mama/yeeeeeeaaah.ogg",length=2.219}}
+c.EndList()
