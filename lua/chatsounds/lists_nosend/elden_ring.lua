e44e7efcc9afc4a90e3cadeea797d9dd7d898721
Meta Automator <MetaAutomator@users.noreply.github.com>
2022-09-04 19:53:16 +0000

    Commit from GitHub Actions (Push Action)

--- lua/chatsounds/lists_nosend/elden_ring.lua
+++ lua/chatsounds/lists_nosend/elden_ring.lua
@@ -1,6 +1,6 @@
 c.StartList("elden_ring")
 L["a bit player fully divorced from the strength of runes"]={{path="chatsounds/autoadd/elden_ring/a bit player fully divorced from the strength of runes.ogg",length=6.213}}
 L["feel free to go off and die in a ditch somewhere"]={{path="chatsounds/autoadd/elden_ring/feel free to go off and die in a ditch somewhere.ogg",length=5.379}}
-L["unfortunately_for_you_however_you_are_maidenless"]={{path="chatsounds/autoadd/elden_ring/unfortunately_for_you_however_you_are_maidenless.ogg",length=5.054}}
+L["unfortunately for you however you are maidenless"]={{path="chatsounds/autoadd/elden_ring/unfortunately for you however you are maidenless.ogg",length=5.054}}
 L["you are maidenless"]={{path="chatsounds/autoadd/elden_ring/you are maidenless.ogg",length=1.947}}
 c.EndList()
