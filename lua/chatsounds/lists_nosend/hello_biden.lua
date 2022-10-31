e1839e61d13933531547185ce4a5fe8b13325f62
Meta Automator <MetaAutomator@users.noreply.github.com>
2022-10-31 04:25:01 +0000

    Commit from GitHub Actions (Push Action)

--- lua/chatsounds/lists_nosend/hello_biden.lua
+++ lua/chatsounds/lists_nosend/hello_biden.lua
@@ -1,6 +1,6 @@
 c.StartList("hello_biden")
 L["donetsk children"]={{path="chatsounds/autoadd/hello_biden/donetsk children.ogg",length=1.301}}
-L["hello biden"]={{path="chatsounds/autoadd/hello_biden/hello biden.ogg",length=1.268}}
+L["hello biden"]={{path="chatsounds/autoadd/hello_biden/hello biden.ogg",length=0.934}}
 L["its zelensky"]={{path="chatsounds/autoadd/hello_biden/its zelensky.ogg",length=1.168}}
 L["slava ukraini"]={{path="chatsounds/autoadd/hello_biden/slava ukraini.ogg",length=1.568}}
 L["to bomb"]={{path="chatsounds/autoadd/hello_biden/to bomb.ogg",length=0.701}}
