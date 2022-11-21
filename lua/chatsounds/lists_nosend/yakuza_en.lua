49a38c9000c2b15efb08ddfdd34008bad4a7a6e5
Meta Automator <MetaAutomator@users.noreply.github.com>
2022-11-21 18:44:44 +0000

    Commit from GitHub Actions (Push Action)

---
+++ lua/chatsounds/lists_nosend/yakuza_en.lua
@@ -0,0 +1,12 @@
+c.StartList("yakuza_en")
+L["10 years in the joint made you a fuckin pussy"]={{path="chatsounds/autoadd/yakuza_en/10 years in the joint made you a fuckin pussy.ogg",length=2.968}}
+L["bring that shit kazuma"]={{path="chatsounds/autoadd/yakuza_en/bring that shit kazuma.ogg",length=1.609}}
+L["fuck you"]={{path="chatsounds/autoadd/yakuza_en/fuck you.ogg",length=0.737}}
+L["get off of me"]={{path="chatsounds/autoadd/yakuza_en/get off of me.ogg",length=1.283}}
+L["get outta here"]={{path="chatsounds/autoadd/yakuza_en/get outta here.ogg",length=0.841}}
+L["how do you like this"]={{path="chatsounds/autoadd/yakuza_en/how do you like this.ogg",length=1.144}}
+L["nishiki"]={{path="chatsounds/autoadd/yakuza_en/nishiki.ogg",length=0.659}}
+L["piss off"]={{path="chatsounds/autoadd/yakuza_en/piss off.ogg",length=0.716}}
+L["were you someones prison bitch"]={{path="chatsounds/autoadd/yakuza_en/were you someones prison bitch.ogg",length=1.836}}
+L["what you think youre better than me"]={{path="chatsounds/autoadd/yakuza_en/what you think youre better than me.ogg",length=1.305}}
+c.EndList()
