c102bca790a8abe7a6f0cb8f7083e23fecc9442b
Meta Automator <MetaAutomator@users.noreply.github.com>
2022-12-05 18:16:36 +0000

    Commit from GitHub Actions (Push Action)

---
+++ lua/chatsounds/lists_nosend/ultrakill_minos.lua
@@ -0,0 +1,9 @@
+c.StartList("ultrakill_minos")
+L["crush"]={{path="chatsounds/autoadd/ultrakill_minos/crush.ogg",length=1.498}}
+L["die"]={{path="chatsounds/autoadd/ultrakill_minos/die/1.ogg",length=1.293},{path="chatsounds/autoadd/ultrakill_minos/die/2.ogg",length=1.021}}
+L["judgement"]={{path="chatsounds/autoadd/ultrakill_minos/judgement/1.ogg",length=1.581},{path="chatsounds/autoadd/ultrakill_minos/judgement/2.ogg",length=1.710}}
+L["prepare thyself"]={{path="chatsounds/autoadd/ultrakill_minos/prepare thyself/1.ogg",length=1.985},{path="chatsounds/autoadd/ultrakill_minos/prepare thyself/2.ogg",length=1.871}}
+L["thy end is now"]={{path="chatsounds/autoadd/ultrakill_minos/thy end is now/1.ogg",length=2.209},{path="chatsounds/autoadd/ultrakill_minos/thy end is now/2.ogg",length=2.202}}
+L["useless"]={{path="chatsounds/autoadd/ultrakill_minos/useless.ogg",length=1.672}}
+L["weak"]={{path="chatsounds/autoadd/ultrakill_minos/weak.ogg",length=1.491}}
+c.EndList()
