a7b67ed44f7f8a2153aa785f2c611d16282eae33
Meta Automator <MetaAutomator@users.noreply.github.com>
2023-01-29 12:10:46 +0000

    Commit from GitHub Actions (Push Action)

---
+++ lua/chatsounds/lists_nosend/sonic_racist.lua
@@ -0,0 +1,13 @@
+c.StartList("sonic_racist")
+L["behold the power of my ultimate masturbation"]={{path="chatsounds/autoadd/sonic_racist/behold the power of my ultimate masturbation.ogg",length=3.524}}
+L["eggmans forcing sonic to suck penis"]={{path="chatsounds/autoadd/sonic_racist/eggmans forcing sonic to suck penis.ogg",length=2.785}}
+L["fuck you"]={{path="chatsounds/autoadd/sonic_racist/fuck you.ogg",length=0.429}}
+L["good news is youre gay"]={{path="chatsounds/autoadd/sonic_racist/good news is youre gay.ogg",length=1.457}}
+L["im an optimist but im also a racist"]={{path="chatsounds/autoadd/sonic_racist/im an optimist but im also a racist.ogg",length=2.316}}
+L["its not gonna suck itself"]={{path="chatsounds/autoadd/sonic_racist/its not gonna suck itself.ogg",length=1.907}}
+L["kkkaos"]={{path="chatsounds/autoadd/sonic_racist/kkkaos.ogg",length=1.597}}
+L["okay lets fuck eggman"]={{path="chatsounds/autoadd/sonic_racist/okay lets fuck eggman.ogg",length=2.226}}
+L["okay lets get gay"]={{path="chatsounds/autoadd/sonic_racist/okay lets get gay.ogg",length=1.118}}
+L["shut the fuck up"]={{path="chatsounds/autoadd/sonic_racist/shut the fuck up.ogg",length=0.799}}
+L["this guy is gayer than sonic"]={{path="chatsounds/autoadd/sonic_racist/this guy is gayer than sonic.ogg",length=1.857}}
+c.EndList()
---
+++ lua/chatsounds/lists_nosend/toystoryytp_hellionhero.lua
@@ -0,0 +1,20 @@
+c.StartList("toystoryytp_hellionhero")
+L["ages 3 and up its on my cock"]={{path="chatsounds/autoadd/toystoryytp_hellionhero/ages 3 and up its on my cock.ogg",length=1.635}}
+L["and give whatever it is up there a nice big fuck"]={{path="chatsounds/autoadd/toystoryytp_hellionhero/and give whatever it is up there a nice big fuck.ogg",length=3.757}}
+L["are you saying you wanna fuck a guy"]={{path="chatsounds/autoadd/toystoryytp_hellionhero/are you saying you wanna fuck a guy.ogg",length=1.842}}
+L["buzz will you get up here and give me a handjob"]={{path="chatsounds/autoadd/toystoryytp_hellionhero/buzz will you get up here and give me a handjob.ogg",length=2.074}}
+L["get fucked"]={{path="chatsounds/autoadd/toystoryytp_hellionhero/get fucked.ogg",length=0.640}}
+L["i wanted to thank you woody for fucking my ass"]={{path="chatsounds/autoadd/toystoryytp_hellionhero/i wanted to thank you woody for fucking my ass.ogg",length=2.620}}
+L["its me and you bitch yeay sees"]={{path="chatsounds/autoadd/toystoryytp_hellionhero/its me and you bitch yeay sees.ogg",length=4.107}}
+L["noone is getting sexed"]={{path="chatsounds/autoadd/toystoryytp_hellionhero/noone is getting sexed.ogg",length=1.355}}
+L["pull my cock im gay"]={{path="chatsounds/autoadd/toystoryytp_hellionhero/pull my cock im gay.ogg",length=1.651}}
+L["suck my balls"]={{path="chatsounds/autoadd/toystoryytp_hellionhero/suck my balls.ogg",length=1.566}}
+L["suck my fucking balls"]={{path="chatsounds/autoadd/toystoryytp_hellionhero/suck my fucking balls.ogg",length=1.349}}
+L["this is a quality cock"]={{path="chatsounds/autoadd/toystoryytp_hellionhero/this is a quality cock.ogg",length=1.810}}
+L["we are special"]={{path="chatsounds/autoadd/toystoryytp_hellionhero/we are special.ogg",length=1.005}}
+L["were you scared tell me honestly"]={{path="chatsounds/autoadd/toystoryytp_hellionhero/were you scared tell me honestly.ogg",length=1.953}}
+L["you fg"]={{path="chatsounds/autoadd/toystoryytp_hellionhero/you fg.ogg",length=1.085}}
+L["you fg boy"]={{path="chatsounds/autoadd/toystoryytp_hellionhero/you fg boy.ogg",length=1.609}}
+L["you got a dick in my ass"]={{path="chatsounds/autoadd/toystoryytp_hellionhero/you got a dick in my ass.ogg",length=2.239}}
+L["you gotta suck my motherfuckin dick"]={{path="chatsounds/autoadd/toystoryytp_hellionhero/you gotta suck my motherfuckin dick.ogg",length=5.223}}
+c.EndList()
