33032914f99f9edf5f51f00969af1722e8fb98bd
Meta Automator <MetaAutomator@users.noreply.github.com>
2023-03-08 00:56:56 +0000

    Commit from GitHub Actions (Push Action)

--- lua/chatsounds/lists_nosend/family_guy.lua
+++ lua/chatsounds/lists_nosend/family_guy.lua
@@ -1,6 +1,8 @@
 c.StartList("family_guy")
 L["aaah brian"]={{path="chatsounds/autoadd/family_guy/aaah brian.ogg",length=1.119}}
 L["brian look out"]={{path="chatsounds/autoadd/family_guy/brian look out.ogg",length=0.981}}
+L["crack"]={{path="chatsounds/autoadd/family_guy/crack.ogg",length=0.608}}
+L["hey atleast im not drinking brian"]={{path="chatsounds/autoadd/family_guy/hey atleast im not drinking brian.ogg",length=1.483}}
 L["hey joe"]={{path="chatsounds/autoadd/family_guy/hey joe.ogg",length=0.465}}
 L["hey peter"]={{path="chatsounds/autoadd/family_guy/hey peter.ogg",length=0.739}}
 L["holy fuck"]={{path="chatsounds/autoadd/family_guy/holy fuck.ogg",length=1.538}}
@@ -8,6 +10,9 @@ L["im cumming"]={{path="chatsounds/autoadd/family_guy/im cumming.ogg",length=1.7
 L["lois"]={{path="chatsounds/autoadd/family_guy/lois.ogg",length=1.112}}
 L["oh yeah"]={{path="chatsounds/autoadd/family_guy/oh yeah.ogg",length=0.617}}
 L["peter"]={{path="chatsounds/autoadd/family_guy/peter.ogg",length=0.412}}
+L["peter what are you doing"]={{path="chatsounds/autoadd/family_guy/peter what are you doing.ogg",length=1.264}}
 L["petermoans"]={{path="chatsounds/autoadd/family_guy/petermoans/1.ogg",length=0.840},{path="chatsounds/autoadd/family_guy/petermoans/2.ogg",length=2.044}}
 L["the horse is here"]={{path="chatsounds/autoadd/family_guy/the horse is here.ogg",length=0.908}}
+L["what the fu"]={{path="chatsounds/autoadd/family_guy/what the fu.ogg",length=1.069}}
+L["what the fuck"]={{path="chatsounds/autoadd/family_guy/what the fuck.ogg",length=1.283}}
 c.EndList()
