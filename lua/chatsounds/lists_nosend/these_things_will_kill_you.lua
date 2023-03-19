35186ba8a8afd65d77eb3a4b6f0cab6258578eb6
Meta Automator <MetaAutomator@users.noreply.github.com>
2023-03-19 19:23:26 +0000

    Commit from GitHub Actions (Push Action)

--- lua/chatsounds/lists_nosend/these_things_will_kill_you.lua
+++ lua/chatsounds/lists_nosend/these_things_will_kill_you.lua
@@ -3,32 +3,45 @@ L["man urinates"]={{path="chatsounds/autoadd/these_things_will_kill_you/man urin
 L["number 1 harambe"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 1 harambe.ogg",length=2.876}}
 L["number 1 monkey brains"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 1 monkey brains.ogg",length=2.079}}
 L["number 1 tampon in steak"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 1 tampon in steak.ogg",length=2.802}}
+L["number 10 glass in baby food"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 10 glass in baby food.ogg",length=2.442}}
 L["number 10 mcafrica"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 10 mcafrica.ogg",length=1.968}}
 L["number 11 archdeluxe"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 11 archdeluxe.ogg",length=2.256}}
+L["number 11 blood inside blt"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 11 blood inside blt.ogg",length=3.111}}
 L["number 12 a monkey attacks 100 people in 10 days"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 12 a monkey attacks 100 people in 10 days.ogg",length=3.260}}
 L["number 12 cock"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 12 cock.ogg",length=1.093}}
+L["number 12 cockroach"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 12 cockroach.ogg",length=2.331}}
 L["number 13 mcpizza and mcspaghetti"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 13 mcpizza and mcspaghetti.ogg",length=4.087}}
+L["number 13 pizza hut band aid"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 13 pizza hut band aid.ogg",length=2.855}}
+L["number 14 human skin"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 14 human skin.ogg",length=2.462}}
+L["number 15 snake head"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 15 snake head.ogg",length=1.767}}
+L["number 16 human tooth"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 16 human tooth.ogg",length=2.286}}
 L["number 17 razor"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 17 razor.ogg",length=2.092}}
+L["number 18 black widow spider"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 18 black widow spider.ogg",length=2.522}}
 L["number 19 mouse in baked beans"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 19 mouse in baked beans.ogg",length=3.066}}
 L["number 2 clamchowder condom"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 2 clamchowder condom.ogg",length=2.591}}
 L["number 20 frog in a frozen vegetable bag"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 20 frog in a frozen vegetable bag.ogg",length=2.961}}
 L["number 3 brown lady"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 3 brown lady.ogg",length=2.465}}
 L["number 3 eminem"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 3 eminem.ogg",length=2.414}}
 L["number 3 herobrine"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 3 herobrine.ogg",length=1.968}}
+L["number 3 ice cream surprise"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 3 ice cream surprise.ogg",length=2.235}}
 L["number 3 man takes selfie with plane hijacker"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 3 man takes selfie with plane hijacker.ogg",length=4.498}}
+L["number 4 bullets in hotdog"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 4 bullets in hotdog.ogg",length=2.346}}
 L["number 4 justing bieber"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 4 justing bieber.ogg",length=2.450}}
 L["number 4 monkeys steal covid19 samples"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 4 monkeys steal covid19 samples.ogg",length=3.313}}
 L["number 5 syringe needle sandvich"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 5 syringe needle sandvich.ogg",length=2.790}}
-L["number 5 the french"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 5 the french.ogg",length=2.790}}
+L["number 5 the french"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 5 the french.ogg",length=1.880}}
 L["number 5 the french fries"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 5 the french fries.ogg",length=2.790}}
 L["number 6 man urinates on fellow passenger for not being allowed to smoke"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 6 man urinates on fellow passenger for not being allowed to smoke.ogg",length=6.768}}
 L["number 6 nail found in burger"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 6 nail found in burger.ogg",length=3.119}}
 L["number 6 racism"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 6 racism.ogg",length=2.206}}
+L["number 6 serrated knife"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 6 serrated knife.ogg",length=2.356}}
 L["number 7 condoms found in food"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 7 condoms found in food.ogg",length=4.130}}
 L["number 7 dead frog inside pepsi can"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 7 dead frog inside pepsi can.ogg",length=4.106}}
 L["number 7 mcrib mania"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 7 mcrib mania.ogg",length=3.287}}
 L["number 7 student watches porn and gets naked"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 7 student watches porn and gets naked.ogg",length=4.736}}
 L["number 8 abraham lincoln"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 8 abraham lincoln.ogg",length=2.941}}
 L["number 8 maggots inside big mac"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 8 maggots inside big mac.ogg",length=2.661}}
+L["number 8 severed finger"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 8 severed finger.ogg",length=1.923}}
 L["number 9 breastfeeding"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 9 breastfeeding.ogg",length=1.886}}
+L["number 9 chicken head"]={{path="chatsounds/autoadd/these_things_will_kill_you/number 9 chicken head.ogg",length=2.225}}
 c.EndList()
