86ff6e6c62920a960c614d3334d9f0c38010f56c
Meta Automator <MetaAutomator@users.noreply.github.com>
2024-03-16 23:17:14 +0000

    Commit from GitHub Actions (Push Action)

--- lua/chatsounds/lists_nosend/pizza_tower_sage.lua
+++ lua/chatsounds/lists_nosend/pizza_tower_sage.lua
@@ -3,6 +3,7 @@ L["collect giant pizza"]={{path="chatsounds/autoadd/pizza_tower_sage/collect gia
 L["collect pizza"]={{path="chatsounds/autoadd/pizza_tower_sage/collect pizza.ogg",length=2.400}}
 L["collect toppin"]={{path="chatsounds/autoadd/pizza_tower_sage/collect toppin.ogg",length=2.400}}
 L["dungeon depth"]={{path="chatsounds/autoadd/pizza_tower_sage/dungeon depth.ogg",length=51.480}}
+L["noise scream"]={{path="chatsounds/autoadd/pizza_tower_sage/noise scream.ogg",length=1.468}}
 L["noise woah"]={{path="chatsounds/autoadd/pizza_tower_sage/woah.ogg",length=0.607}}
 L["pizza tower rank a"]={{path="chatsounds/autoadd/pizza_tower_sage/pizza tower rank a.ogg",length=15.576}}
 L["pizza tower rank b"]={{path="chatsounds/autoadd/pizza_tower_sage/pizza tower rank b.ogg",length=15.637}}
