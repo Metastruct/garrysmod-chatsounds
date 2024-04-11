c6783085c28d00b6cc41a26a2f902d995feaf80d
Meta Automator <MetaAutomator@users.noreply.github.com>
2024-04-11 19:44:19 +0000

    Commit from GitHub Actions (Push Action)

--- lua/chatsounds/lists_nosend/tense.lua
+++ lua/chatsounds/lists_nosend/tense.lua
@@ -1,8 +1,44 @@
 c.StartList("tense")
+L["and you cry"]={{path="chatsounds/autoadd/tense/and you cry.ogg",length=1.078}}
+L["and you have and it matter because"]={{path="chatsounds/autoadd/tense/and you have and it matter because.ogg",length=2.503}}
+L["and you you have a problem"]={{path="chatsounds/autoadd/tense/and you you have a problem.ogg",length=2.210}}
+L["because in your country are au gays"]={{path="chatsounds/autoadd/tense/because in your country are au gays.ogg",length=3.836}}
 L["fakin bastard"]={{path="chatsounds/autoadd/tense/fakin bastard.ogg",length=4.650}}
+L["fakin kid"]={{path="chatsounds/autoadd/tense/fakin kid/1.ogg",length=0.941},{path="chatsounds/autoadd/tense/fakin kid/2.ogg",length=0.709}}
 L["fakin noob"]={{path="chatsounds/autoadd/tense/fakin noob.ogg",length=4.180}}
+L["gaah"]={{path="chatsounds/autoadd/tense/gaah.ogg",length=0.462}}
+L["gargamel is your fakin father"]={{path="chatsounds/autoadd/tense/gargamel is your fakin father.ogg",length=2.289}}
 L["green"]={{path="chatsounds/autoadd/tense/green/green1.ogg",length=6.150},{path="chatsounds/autoadd/tense/green/green2.ogg",length=4.426},{path="chatsounds/autoadd/tense/green/green3.ogg",length=1.629}}
+L["gremlin noises"]={{path="chatsounds/autoadd/tense/gremlin noises.ogg",length=2.049}}
+L["hey man sorry"]={{path="chatsounds/autoadd/tense/hey man sorry.ogg",length=1.749}}
+L["huh puto ruso asqueroso me esta haciendo ensuciar to el monitor"]={{path="chatsounds/autoadd/tense/huh puto ruso asqueroso me esta haciendo ensuciar to el monitor.ogg",length=4.471}}
+L["if your father and your father to fuck fuck in the night"]={{path="chatsounds/autoadd/tense/if your father and your father to fuck fuck in the night.ogg",length=3.568}}
+L["is fucking gg"]={{path="chatsounds/autoadd/tense/is fucking gg.ogg",length=1.290}}
+L["la prostata"]={{path="chatsounds/autoadd/tense/la prostata.ogg",length=0.889}}
+L["maanmaanmaan"]={{path="chatsounds/autoadd/tense/maanmaanmaan.ogg",length=1.593}}
+L["maanmaanmaanmaanmaanmaanmaanmaan"]={{path="chatsounds/autoadd/tense/maanmaanmaanmaanmaanmaanmaanmaan.ogg",length=13.278}}
+L["man man"]={{path="chatsounds/autoadd/tense/man man.ogg",length=0.969}}
+L["maybe silens silens or me afk"]={{path="chatsounds/autoadd/tense/maybe silens silens or me afk.ogg",length=3.373}}
+L["no listen anyone is fucking gg"]={{path="chatsounds/autoadd/tense/no listen anyone is fucking gg.ogg",length=2.487}}
+L["no my fathers"]={{path="chatsounds/autoadd/tense/no my fathers.ogg",length=1.170}}
+L["no no no man is gg is gg"]={{path="chatsounds/autoadd/tense/no no no man is gg is gg.ogg",length=2.776}}
+L["no your father and your father"]={{path="chatsounds/autoadd/tense/no your father and your father.ogg",length=2.127}}
+L["okay thank you thank you"]={{path="chatsounds/autoadd/tense/okay thank you thank you.ogg",length=2.220}}
+L["one teammate rush and no see one and you plays all time what is your fucking problem"]={{path="chatsounds/autoadd/tense/one teammate rush and no see one and you plays all time what is your fucking problem.ogg",length=6.328}}
+L["or me afk"]={{path="chatsounds/autoadd/tense/or me afk.ogg",length=1.136}}
+L["purple"]={{path="chatsounds/autoadd/tense/purple.ogg",length=0.707}}
+L["que prejalli prajalli per jallii no ai na prajaji no ai na el jaji ese no vale na"]={{path="chatsounds/autoadd/tense/que prejalli prajalli per jallii no ai na prajaji no ai na el jaji ese no vale na.ogg",length=7.175}}
 L["report"]={{path="chatsounds/autoadd/tense/report/report1.ogg",length=4.944},{path="chatsounds/autoadd/tense/report/report2.ogg",length=5.251}}
 L["retar"]={{path="chatsounds/autoadd/tense/retar.ogg",length=6.757}}
-L["you have a problem in your brain"]={{path="chatsounds/autoadd/tense/you have a problem in your brain.ogg",length=2.482}}
+L["silens"]={{path="chatsounds/autoadd/tense/silens.ogg",length=0.887}}
+L["sorry"]={{path="chatsounds/autoadd/tense/sorry.ogg",length=0.617}}
+L["thank you for speak thank you"]={{path="chatsounds/autoadd/tense/thank you for speak thank you.ogg",length=1.474}}
+L["what is your problem"]={{path="chatsounds/autoadd/tense/what is your problem.ogg",length=1.296}}
+L["you are fucking baiting losers"]={{path="chatsounds/autoadd/tense/you are fucking baiting losers.ogg",length=1.896}}
+L["you decide silens or me afk"]={{path="chatsounds/autoadd/tense/you decide silens or me afk.ogg",length=2.646}}
+L["you have a problem in yuum brain"]={{path="chatsounds/autoadd/tense/you have a problem in yuum brain.ogg",length=2.461}}
+L["your fakin father is a fakin gargamel"]={{path="chatsounds/autoadd/tense/your fakin father is a fakin gargamel.ogg",length=2.582}}
+L["your father and your father in the night fuck fuck fuck"]={{path="chatsounds/autoadd/tense/your father and your father in the night fuck fuck fuck.ogg",length=3.918}}
+L["your father is fakin gargamel"]={{path="chatsounds/autoadd/tense/your father is fakin gargamel.ogg",length=2.339}}
+L["your father is fakin gargamel maybe"]={{path="chatsounds/autoadd/tense/your father is fakin gargamel maybe.ogg",length=2.666}}
 c.EndList()
