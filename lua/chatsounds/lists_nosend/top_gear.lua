a0d5ab497450e4011d95f9382c41998f024ee386
Meta Automator <MetaAutomator@users.noreply.github.com>
2024-05-14 14:31:11 +0000

    Commit from GitHub Actions (Push Action)

---
+++ lua/chatsounds/lists_nosend/top_gear.lua
@@ -0,0 +1,5 @@
+c.StartList("top_gear")
+L["cock"]={{path="chatsounds/autoadd/top_gear/cock/1.ogg",length=0.544},{path="chatsounds/autoadd/top_gear/cock/2.ogg",length=0.590},{path="chatsounds/autoadd/top_gear/cock/3.ogg",length=0.844},{path="chatsounds/autoadd/top_gear/cock/4.ogg",length=0.706}}
+L["oh cock"]={{path="chatsounds/autoadd/top_gear/oh cock/1.ogg",length=1.016},{path="chatsounds/autoadd/top_gear/oh cock/2.ogg",length=1.184}}
+L["oh no anyway"]={{path="chatsounds/autoadd/top_gear/oh no anyway.ogg",length=1.640}}
+c.EndList()
