d19a26058a8bed3252a28add1a2f8856418a458f
Meta Automator <MetaAutomator@users.noreply.github.com>
2024-07-04 19:20:13 +0000

    Commit from GitHub Actions (Push Action)

--- lua/chatsounds/lists_nosend/top_gear.lua
+++ lua/chatsounds/lists_nosend/top_gear.lua
@@ -1,5 +1,8 @@
 c.StartList("top_gear")
 L["cock"]={{path="chatsounds/autoadd/top_gear/cock/1.ogg",length=0.544},{path="chatsounds/autoadd/top_gear/cock/2.ogg",length=0.590},{path="chatsounds/autoadd/top_gear/cock/3.ogg",length=0.844},{path="chatsounds/autoadd/top_gear/cock/4.ogg",length=0.706}}
 L["oh cock"]={{path="chatsounds/autoadd/top_gear/oh cock/1.ogg",length=1.016},{path="chatsounds/autoadd/top_gear/oh cock/2.ogg",length=1.184}}
+L["oh my god"]={{path="chatsounds/autoadd/top_gear/oh my god.ogg",length=1.590}}
 L["oh no anyway"]={{path="chatsounds/autoadd/top_gear/oh no anyway.ogg",length=1.640}}
+L["sometimes my genius is its almost frightening"]={{path="chatsounds/autoadd/top_gear/sometimes my genius is its almost frightening.ogg",length=2.813}}
+L["stupidest idea in history"]={{path="chatsounds/autoadd/top_gear/stupidest idea in history.ogg",length=2.467}}
 c.EndList()
