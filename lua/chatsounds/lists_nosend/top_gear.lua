8bceb088c45f95afe3155f3717f23173af204ba8
Meta Automator <MetaAutomator@users.noreply.github.com>
2024-07-04 21:54:56 +0000

    Commit from GitHub Actions (Push Action)

--- lua/chatsounds/lists_nosend/top_gear.lua
+++ lua/chatsounds/lists_nosend/top_gear.lua
@@ -1,8 +1,45 @@
 c.StartList("top_gear")
-L["cock"]={{path="chatsounds/autoadd/top_gear/cock/1.ogg",length=0.544},{path="chatsounds/autoadd/top_gear/cock/2.ogg",length=0.590},{path="chatsounds/autoadd/top_gear/cock/3.ogg",length=0.844},{path="chatsounds/autoadd/top_gear/cock/4.ogg",length=0.706}}
-L["oh cock"]={{path="chatsounds/autoadd/top_gear/oh cock/1.ogg",length=1.016},{path="chatsounds/autoadd/top_gear/oh cock/2.ogg",length=1.184}}
+L["ah"]={{path="chatsounds/autoadd/top_gear/ah.ogg",length=0.392}}
+L["ahh"]={{path="chatsounds/autoadd/top_gear/ahh.ogg",length=0.681}}
+L["and ill tell you something else"]={{path="chatsounds/autoadd/top_gear/and ill tell you something else.ogg",length=1.140}}
+L["because its stylish and contemporary"]={{path="chatsounds/autoadd/top_gear/because its stylish and contemporary.ogg",length=2.253}}
+L["clarkson lmao"]={{path="chatsounds/autoadd/top_gear/clarkson lmao/1.ogg",length=2.073},{path="chatsounds/autoadd/top_gear/clarkson lmao/2.ogg",length=4.068},{path="chatsounds/autoadd/top_gear/clarkson lmao/3.ogg",length=1.360},{path="chatsounds/autoadd/top_gear/clarkson lmao/4.ogg",length=2.191},{path="chatsounds/autoadd/top_gear/clarkson lmao/5.ogg",length=2.387}}
+L["cock"]={{path="chatsounds/autoadd/top_gear/cock/1.ogg",length=0.544},{path="chatsounds/autoadd/top_gear/cock/2.ogg",length=0.590},{path="chatsounds/autoadd/top_gear/cock/3.ogg",length=0.844},{path="chatsounds/autoadd/top_gear/cock/4.ogg",length=0.706},{path="chatsounds/autoadd/top_gear/cock/5.ogg",length=0.595},{path="chatsounds/autoadd/top_gear/cock/6.ogg",length=0.823},{path="chatsounds/autoadd/top_gear/cock/7.ogg",length=0.797}}
+L["did you notice i was first"]={{path="chatsounds/autoadd/top_gear/did you notice i was first.ogg",length=1.215}}
+L["dont touch it"]={{path="chatsounds/autoadd/top_gear/dont touch it/1.ogg",length=1.092},{path="chatsounds/autoadd/top_gear/dont touch it/2.ogg",length=0.764}}
+L["eggs come from the milkman"]={{path="chatsounds/autoadd/top_gear/eggs come from the milkman.ogg",length=1.480}}
+L["everytime i see you those are the words that pop into my head"]={{path="chatsounds/autoadd/top_gear/everytime i see you those are the words that pop into my head.ogg",length=3.402}}
+L["group lmao"]={{path="chatsounds/autoadd/top_gear/group lmao/1.ogg",length=2.605},{path="chatsounds/autoadd/top_gear/group lmao/2.ogg",length=3.795},{path="chatsounds/autoadd/top_gear/group lmao/3.ogg",length=2.686},{path="chatsounds/autoadd/top_gear/group lmao/4.ogg",length=3.305},{path="chatsounds/autoadd/top_gear/group lmao/5.ogg",length=1.376},{path="chatsounds/autoadd/top_gear/group lmao/6.ogg",length=2.856},{path="chatsounds/autoadd/top_gear/group lmao/7.ogg",length=4.058},{path="chatsounds/autoadd/top_gear/group lmao/8.ogg",length=2.453},{path="chatsounds/autoadd/top_gear/group lmao/9.ogg",length=1.644}}
+L["haha"]={{path="chatsounds/autoadd/top_gear/haha/1.ogg",length=0.760},{path="chatsounds/autoadd/top_gear/haha/2.ogg",length=0.832},{path="chatsounds/autoadd/top_gear/haha/3.ogg",length=1.470}}
+L["hammond"]={{path="chatsounds/autoadd/top_gear/hammond/1.ogg",length=0.624},{path="chatsounds/autoadd/top_gear/hammond/2.ogg",length=0.832},{path="chatsounds/autoadd/top_gear/hammond/3.ogg",length=0.795},{path="chatsounds/autoadd/top_gear/hammond/4.ogg",length=0.669},{path="chatsounds/autoadd/top_gear/hammond/5.ogg",length=0.527},{path="chatsounds/autoadd/top_gear/hammond/6.ogg",length=0.435}}
+L["he stands like a teapot"]={{path="chatsounds/autoadd/top_gear/he stands like a teapot.ogg",length=1.709}}
+L["help"]={{path="chatsounds/autoadd/top_gear/help/1.ogg",length=0.708},{path="chatsounds/autoadd/top_gear/help/2.ogg",length=0.484}}
+L["hes formed an emotional attachment"]={{path="chatsounds/autoadd/top_gear/hes formed an emotional attachment.ogg",length=1.717}}
+L["hoo"]={{path="chatsounds/autoadd/top_gear/hoo.ogg",length=1.079}}
+L["how am i gonna explain this to my wife"]={{path="chatsounds/autoadd/top_gear/how am i gonna explain this to my wife.ogg",length=1.914}}
+L["how hard can it be"]={{path="chatsounds/autoadd/top_gear/how hard can it be/1.ogg",length=1.536},{path="chatsounds/autoadd/top_gear/how hard can it be/10.ogg",length=1.614},{path="chatsounds/autoadd/top_gear/how hard can it be/2.ogg",length=1.542},{path="chatsounds/autoadd/top_gear/how hard can it be/3.ogg",length=1.059},{path="chatsounds/autoadd/top_gear/how hard can it be/4.ogg",length=1.470},{path="chatsounds/autoadd/top_gear/how hard can it be/5.ogg",length=1.138},{path="chatsounds/autoadd/top_gear/how hard can it be/6.ogg",length=1.383},{path="chatsounds/autoadd/top_gear/how hard can it be/7.ogg",length=0.947},{path="chatsounds/autoadd/top_gear/how hard can it be/8.ogg",length=1.477},{path="chatsounds/autoadd/top_gear/how hard can it be/9.ogg",length=1.708}}
+L["its not the holy program its the truth"]={{path="chatsounds/autoadd/top_gear/its not the holy program its the truth.ogg",length=1.810}}
+L["its so quick it can destroy your entire face"]={{path="chatsounds/autoadd/top_gear/its so quick it can destroy your entire face.ogg",length=3.746}}
+L["james"]={{path="chatsounds/autoadd/top_gear/james/1.ogg",length=0.873},{path="chatsounds/autoadd/top_gear/james/2.ogg",length=0.570}}
+L["manual semi automatic oh these are the weapons"]={{path="chatsounds/autoadd/top_gear/manual semi automatic oh these are the weapons.ogg",length=2.904}}
+L["now hang on soap is soap"]={{path="chatsounds/autoadd/top_gear/now hang on soap is soap.ogg",length=1.380}}
+L["now rich would you like some pussy"]={{path="chatsounds/autoadd/top_gear/now rich would you like some pussy.ogg",length=2.573}}
+L["oh bloody hell"]={{path="chatsounds/autoadd/top_gear/oh bloody hell.ogg",length=1.568}}
+L["oh cock"]={{path="chatsounds/autoadd/top_gear/oh cock/1.ogg",length=1.016},{path="chatsounds/autoadd/top_gear/oh cock/2.ogg",length=1.184},{path="chatsounds/autoadd/top_gear/oh cock/3.ogg",length=1.331}}
 L["oh my god"]={{path="chatsounds/autoadd/top_gear/oh my god.ogg",length=1.590}}
 L["oh no anyway"]={{path="chatsounds/autoadd/top_gear/oh no anyway.ogg",length=1.640}}
+L["ornament shatter"]={{path="chatsounds/autoadd/top_gear/ornament shatter.ogg",length=0.487}}
+L["ouh"]={{path="chatsounds/autoadd/top_gear/ouh.ogg",length=0.695}}
+L["pussy energy drink"]={{path="chatsounds/autoadd/top_gear/pussy energy drink.ogg",length=1.406}}
+L["so weve really wasted the viewers time"]={{path="chatsounds/autoadd/top_gear/so weve really wasted the viewers time.ogg",length=2.969}}
+L["some say"]={{path="chatsounds/autoadd/top_gear/some say/1.ogg",length=8.355},{path="chatsounds/autoadd/top_gear/some say/10.ogg",length=10.101},{path="chatsounds/autoadd/top_gear/some say/2.ogg",length=11.215},{path="chatsounds/autoadd/top_gear/some say/3.ogg",length=16.443},{path="chatsounds/autoadd/top_gear/some say/4.ogg",length=7.117},{path="chatsounds/autoadd/top_gear/some say/5.ogg",length=8.753},{path="chatsounds/autoadd/top_gear/some say/6.ogg",length=9.173},{path="chatsounds/autoadd/top_gear/some say/7.ogg",length=9.350},{path="chatsounds/autoadd/top_gear/some say/8.ogg",length=8.046},{path="chatsounds/autoadd/top_gear/some say/9.ogg",length=10.433}}
 L["sometimes my genius is its almost frightening"]={{path="chatsounds/autoadd/top_gear/sometimes my genius is its almost frightening.ogg",length=2.813}}
+L["sorry this is getting worse"]={{path="chatsounds/autoadd/top_gear/sorry this is getting worse.ogg",length=1.453}}
 L["stupidest idea in history"]={{path="chatsounds/autoadd/top_gear/stupidest idea in history.ogg",length=2.467}}
+L["thats just childish and doesnt solve the argument"]={{path="chatsounds/autoadd/top_gear/thats just childish and doesnt solve the argument.ogg",length=2.640}}
+L["thats not gone well"]={{path="chatsounds/autoadd/top_gear/thats not gone well.ogg",length=1.431}}
+L["they may say its extra cool but trust me on this it isnt"]={{path="chatsounds/autoadd/top_gear/they may say its extra cool but trust me on this it isnt.ogg",length=4.588}}
+L["this is pathetic"]={{path="chatsounds/autoadd/top_gear/this is pathetic.ogg",length=1.088}}
+L["yeahh"]={{path="chatsounds/autoadd/top_gear/yeahh.ogg",length=1.208}}
+L["your job is to shut up"]={{path="chatsounds/autoadd/top_gear/your job is to shut up.ogg",length=1.570}}
 c.EndList()
