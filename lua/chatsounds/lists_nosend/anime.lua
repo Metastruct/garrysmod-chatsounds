0b83bed798397caad30f91727921138bdd5155ef
Meta Automator <MetaAutomator@users.noreply.github.com>
2024-07-09 11:09:13 +0000

    Commit from GitHub Actions (Push Action)

--- lua/chatsounds/lists_nosend/anime.lua
+++ lua/chatsounds/lists_nosend/anime.lua
@@ -1,9 +1,9 @@
 c.StartList("anime")
 L["aaaaahhh"]={{path="chatsounds/autoadd/anime/aaaaahhh.ogg",length=2.123}}
 L["ah eto bleh"]={{path="chatsounds/autoadd/anime/ah eto bleh.ogg",length=2.749}}
+L["ah i wish i were a birdu"]={{path="chatsounds/autoadd/anime/ah i wish i were a birdu.ogg",length=3.620}}
 L["america ya"]={{path="chatsounds/autoadd/anime/america ya.ogg",length=1.387}}
 L["awebo"]={{path="chatsounds/autoadd/anime/awebo.ogg",length=0.576}}
-L["azumanga daioh"]={{path="chatsounds/autoadd/anime/azumanga daioh/ah i wish i were a birdu.ogg",length=3.620},{path="chatsounds/autoadd/anime/azumanga daioh/hello everynyan.ogg",length=2.328},{path="chatsounds/autoadd/anime/azumanga daioh/how are you why thank you.ogg",length=2.579},{path="chatsounds/autoadd/anime/azumanga daioh/oh my gah.ogg",length=1.361}}
 L["burunyaa"]={{path="chatsounds/autoadd/anime/burunyaa.ogg",length=1.190}}
 L["cawfee"]={{path="chatsounds/autoadd/anime/cawfee/1.ogg",length=0.650},{path="chatsounds/autoadd/anime/cawfee/2.ogg",length=0.869}}
 L["comment"]={{path="chatsounds/autoadd/anime/comment.ogg",length=0.604}}
@@ -16,8 +16,10 @@ L["good night"]={{path="chatsounds/autoadd/anime/good night.ogg",length=0.740}}
 L["gwah"]={{path="chatsounds/autoadd/anime/gwah.ogg",length=0.294}}
 L["gween tea"]={{path="chatsounds/autoadd/anime/gween tea/1.ogg",length=0.762},{path="chatsounds/autoadd/anime/gween tea/2.ogg",length=0.846}}
 L["hallo"]={{path="chatsounds/autoadd/anime/hallo/1.ogg",length=0.956},{path="chatsounds/autoadd/anime/hallo/2.ogg",length=0.918},{path="chatsounds/autoadd/anime/hallo/3.ogg",length=0.956},{path="chatsounds/autoadd/anime/hallo/4.ogg",length=0.956},{path="chatsounds/autoadd/anime/hallo/5.ogg",length=0.768}}
+L["hello everynyan"]={{path="chatsounds/autoadd/anime/hello everynyan.ogg",length=2.328}}
 L["hello mister"]={{path="chatsounds/autoadd/anime/hello mister.ogg",length=1.092}}
 L["hm"]={{path="chatsounds/autoadd/anime/hm.ogg",length=0.351}}
+L["how are you why thank you"]={{path="chatsounds/autoadd/anime/how are you why thank you.ogg",length=2.579}}
 L["ieehey"]={{path="chatsounds/autoadd/anime/ieehey.ogg",length=0.998}}
 L["kani"]={{path="chatsounds/autoadd/anime/kani.ogg",length=2.153}}
 L["kero"]={{path="chatsounds/autoadd/anime/kero.ogg",length=0.561}}
@@ -32,6 +34,7 @@ L["not tomorrow"]={{path="chatsounds/autoadd/anime/not tomorrow.ogg",length=1.23
 L["nya song"]={{path="chatsounds/autoadd/anime/nya song.ogg",length=7.411}}
 L["nyan ja jaaaan"]={{path="chatsounds/autoadd/anime/nyan ja jaaaan.ogg",length=1.717}}
 L["nyanya"]={{path="chatsounds/autoadd/anime/nyanya.ogg",length=0.967}}
+L["oh my gah"]={{path="chatsounds/autoadd/anime/oh my gah.ogg",length=1.361}}
 L["oui"]={{path="chatsounds/autoadd/anime/oui/oui 1.ogg",length=0.202},{path="chatsounds/autoadd/anime/oui/oui 2.ogg",length=0.250}}
 L["papa"]={{path="chatsounds/autoadd/anime/papa.ogg",length=0.496}}
 L["show it to me"]={{path="chatsounds/autoadd/anime/show it to me.ogg",length=0.756}}
