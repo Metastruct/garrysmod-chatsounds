f6137da1782cd13f78daa978e1f94f46918e2fbd
Meta Automator <MetaAutomator@users.noreply.github.com>
2024-07-20 19:52:57 +0000

    Commit from GitHub Actions (Push Action)

---
+++ lua/chatsounds/lists_nosend/hardstop_lucas.lua
@@ -0,0 +1,8 @@
+c.StartList("hardstop_lucas")
+L["ay whats your name"]={{path="chatsounds/autoadd/hardstop_lucas/ay whats your name.ogg",length=0.956}}
+L["i am talking to you"]={{path="chatsounds/autoadd/hardstop_lucas/i am talking to you.ogg",length=1.182}}
+L["im not playing these games"]={{path="chatsounds/autoadd/hardstop_lucas/im not playing these games.ogg",length=1.341}}
+L["its almost like hes talking to me"]={{path="chatsounds/autoadd/hardstop_lucas/its almost like hes talking to me.ogg",length=1.406}}
+L["what did you just say"]={{path="chatsounds/autoadd/hardstop_lucas/what did you just say.ogg",length=1.027}}
+L["what did you say"]={{path="chatsounds/autoadd/hardstop_lucas/what did you say.ogg",length=1.052}}
+c.EndList()
