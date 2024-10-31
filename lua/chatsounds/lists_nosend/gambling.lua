7d5f9fb73768bab53e4fb9809327fd8592e08dc6
Meta Automator <MetaAutomator@users.noreply.github.com>
2024-10-31 21:07:51 +0000

    Commit from GitHub Actions (Push Action)

---
+++ lua/chatsounds/lists_nosend/gambling.lua
@@ -0,0 +1,5 @@
+c.StartList("gambling")
+L["aw dangit"]={{path="chatsounds/autoadd/gambling/aw dangit.ogg",length=0.923}}
+L["buzz"]={{path="chatsounds/autoadd/gambling/buzz.ogg",length=0.716}}
+L["lets go gambling"]={{path="chatsounds/autoadd/gambling/lets go gambling.ogg",length=1.466}}
+c.EndList()
