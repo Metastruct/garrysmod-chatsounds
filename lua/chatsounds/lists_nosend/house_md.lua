bafb40deb1ea636b125d497c9f0651c5501e43b5
Meta Automator <MetaAutomator@users.noreply.github.com>
2025-03-10 18:30:52 +0000

    Commit from GitHub Actions (Push Action)

--- lua/chatsounds/lists_nosend/house_md.lua
+++ lua/chatsounds/lists_nosend/house_md.lua
@@ -1,7 +1,10 @@
 c.StartList("house_md")
+L["are you being intentionally dense"]={{path="chatsounds/autoadd/house_md/are you being intentionally dense.ogg",length=2.251}}
+L["do you have hair in your special place"]={{path="chatsounds/autoadd/house_md/do you have hair in your special place.ogg",length=2.170}}
 L["doubt it"]={{path="chatsounds/autoadd/house_md/doubt it.ogg",length=0.604}}
 L["enzymes indicate"]={{path="chatsounds/autoadd/house_md/enzymes indicate.ogg",length=1.463}}
 L["houselaugh"]={{path="chatsounds/autoadd/house_md/houselaugh.ogg",length=1.939}}
+L["huh"]={{path="chatsounds/autoadd/house_md/huh.ogg",length=0.374}}
 L["were going to cure death"]={{path="chatsounds/autoadd/house_md/were going to cure death.ogg",length=1.219}}
 L["were going to cure her"]={{path="chatsounds/autoadd/house_md/were going to cure her.ogg",length=1.231}}
 L["yeah so what is it"]={{path="chatsounds/autoadd/house_md/yeah so what is it.ogg",length=1.718}}
