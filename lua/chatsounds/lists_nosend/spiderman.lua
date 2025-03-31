87f82937cacd98d18c61f0a21f10d2ba6b95fe36
Meta Automator <MetaAutomator@users.noreply.github.com>
2025-03-31 19:17:58 +0000

    Commit from GitHub Actions (Push Action)

--- lua/chatsounds/lists_nosend/spiderman.lua
+++ lua/chatsounds/lists_nosend/spiderman.lua
@@ -1,4 +1,5 @@
 c.StartList("spiderman")
+L["prowler sound"]={{path="chatsounds/autoadd/spiderman/prowler sound.ogg",length=4.584}}
 L["wha you cant do this to me"]={{path="chatsounds/autoadd/spiderman/wha you cant do this to me.ogg",length=1.840}}
 L["you know how much i sacrificed"]={{path="chatsounds/autoadd/spiderman/you know how much i sacrificed.ogg",length=2.077}}
 c.EndList()
