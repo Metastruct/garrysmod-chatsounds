44978105158d67350c0994344bfc6404f3aa56f6
Meta Automator <MetaAutomator@users.noreply.github.com>
2025-05-03 22:09:47 +0000

    Commit from GitHub Actions (Push Action)

---
+++ lua/chatsounds/lists_nosend/fuwamoco.lua
@@ -0,0 +1,8 @@
+c.StartList("fuwamoco")
+L["baubau"]={{path="chatsounds/autoadd/fuwamoco/baubau/1.ogg",length=2.485},{path="chatsounds/autoadd/fuwamoco/baubau/2.ogg",length=3.709}}
+L["bro when i activate lovense"]={{path="chatsounds/autoadd/fuwamoco/bro when i activate lovense.ogg",length=3.204}}
+L["fuwawa bau"]={{path="chatsounds/autoadd/fuwamoco/fuwawa bau.ogg",length=0.805}}
+L["lets baubau together everyday"]={{path="chatsounds/autoadd/fuwamoco/lets baubau together everyday.ogg",length=3.709}}
+L["lets go baubaubaubaubau"]={{path="chatsounds/autoadd/fuwamoco/lets go baubaubaubaubau.ogg",length=3.840}}
+L["mococo baubau"]={{path="chatsounds/autoadd/fuwamoco/mococo baubau.ogg",length=1.429}}
+c.EndList()
---
+++ lua/chatsounds/lists_nosend/pipkin_pippa.lua
@@ -0,0 +1,8 @@
+c.StartList("pipkin_pippa")
+L["hah based people are dead"]={{path="chatsounds/autoadd/pipkin_pippa/hah based people are dead.ogg",length=2.096}}
+L["i need drugs chat where are my drugs"]={{path="chatsounds/autoadd/pipkin_pippa/i need drugs chat where are my drugs.ogg",length=3.176}}
+L["oh you bitch oh i hate you"]={{path="chatsounds/autoadd/pipkin_pippa/oh you bitch oh i hate you.ogg",length=2.327}}
+L["pipipipi"]={{path="chatsounds/autoadd/pipkin_pippa/pipipipi.ogg",length=2.053}}
+L["seeeexu"]={{path="chatsounds/autoadd/pipkin_pippa/seeeexu.ogg",length=3.029}}
+L["what the fck did you mean by this"]={{path="chatsounds/autoadd/pipkin_pippa/what the fck did you mean by this.ogg",length=2.559}}
+c.EndList()
