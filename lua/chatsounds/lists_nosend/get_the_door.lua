4f37230795a0396cb80c0f270dabd226cf954ecd
Meta Automator <MetaAutomator@users.noreply.github.com>
2026-01-01 02:25:36 +0000

    Commit from GitHub Actions (Push Action)

---
+++ lua/chatsounds/lists_nosend/get_the_door.lua
@@ -0,0 +1,6 @@
+c.StartList("get_the_door")
+L["doorbell"]={{path="chatsounds/autoadd/get_the_door/doorbell.ogg",length=1.609}}
+L["shit hold on"]={{path="chatsounds/autoadd/get_the_door/shit hold on.ogg",length=1.462}}
+L["yo is somebody gonna get the door"]={{path="chatsounds/autoadd/get_the_door/yo is somebody gonna get the door.ogg",length=1.688}}
+L["yo somebody get the door"]={{path="chatsounds/autoadd/get_the_door/yo somebody get the door.ogg",length=1.755}}
+c.EndList()
