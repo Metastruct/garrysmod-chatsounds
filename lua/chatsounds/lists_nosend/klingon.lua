ed1d247a66d57c2ddae24d84212bf6b42f49c483
Meta Automator <MetaAutomator@users.noreply.github.com>
2026-02-17 00:03:45 +0000

    Commit from GitHub Actions (Push Action)

---
+++ lua/chatsounds/lists_nosend/klingon.lua
@@ -0,0 +1,117 @@
+c.StartList("klingon")
+L["bah"]={{path="chatsounds/autoadd/klingon/bah.ogg",length=3.266}}
+L["betleh"]={{path="chatsounds/autoadd/klingon/betleh.ogg",length=3.333}}
+L["betleh bey"]={{path="chatsounds/autoadd/klingon/betleh bey.ogg",length=3.866}}
+L["bijeghbe chugh vaj bihegh"]={{path="chatsounds/autoadd/klingon/bijeghbe chugh vaj bihegh.ogg",length=7.133}}
+L["biq bal"]={{path="chatsounds/autoadd/klingon/biq bal.ogg",length=2.666}}
+L["bireqtagh"]={{path="chatsounds/autoadd/klingon/bireqtagh.ogg",length=4.600}}
+L["bireqtal"]={{path="chatsounds/autoadd/klingon/bireqtal.ogg",length=2.133}}
+L["biyaj a"]={{path="chatsounds/autoadd/klingon/biyaj a.ogg",length=5.400}}
+L["biyem a"]={{path="chatsounds/autoadd/klingon/biyem a.ogg",length=4.133}}
+L["botjan yichu"]={{path="chatsounds/autoadd/klingon/botjan yichu.ogg",length=3.066}}
+L["cha"]={{path="chatsounds/autoadd/klingon/cha.ogg",length=0.557}}
+L["cha mah"]={{path="chatsounds/autoadd/klingon/cha mah.ogg",length=1.196}}
+L["cha nob"]={{path="chatsounds/autoadd/klingon/cha nob.ogg",length=3.866}}
+L["charghbej vit"]={{path="chatsounds/autoadd/klingon/charghbej vit.ogg",length=4.866}}
+L["chonnaq"]={{path="chatsounds/autoadd/klingon/chonnaq.ogg",length=2.266}}
+L["chontay"]={{path="chatsounds/autoadd/klingon/chontay.ogg",length=5.600}}
+L["chorgh"]={{path="chatsounds/autoadd/klingon/chorgh.ogg",length=1.265}}
+L["chuydah yilaq"]={{path="chatsounds/autoadd/klingon/chuydah yilaq.ogg",length=4.266}}
+L["dah"]={{path="chatsounds/autoadd/klingon/dah.ogg",length=2.266}}
+L["daqtagh"]={{path="chatsounds/autoadd/klingon/daqtagh.ogg",length=2.666}}
+L["dargh"]={{path="chatsounds/autoadd/klingon/dargh.ogg",length=3.000}}
+L["denib qatlh"]={{path="chatsounds/autoadd/klingon/denib qatlh.ogg",length=4.866}}
+L["duran lung dir"]={{path="chatsounds/autoadd/klingon/duran lung dir.ogg",length=2.666}}
+L["etlh"]={{path="chatsounds/autoadd/klingon/etlh.ogg",length=2.600}}
+L["gha tlhiq"]={{path="chatsounds/autoadd/klingon/gha tlhiq.ogg",length=4.333}}
+L["ghintaq"]={{path="chatsounds/autoadd/klingon/ghintaq.ogg",length=2.600}}
+L["ghobchuq lodni pu"]={{path="chatsounds/autoadd/klingon/ghobchuq lodni pu.ogg",length=4.400}}
+L["ghobe"]={{path="chatsounds/autoadd/klingon/ghobe.ogg",length=3.333}}
+L["ghochwi"]={{path="chatsounds/autoadd/klingon/ghochwi.ogg",length=3.600}}
+L["ghojmeh taj"]={{path="chatsounds/autoadd/klingon/ghojmeh taj.ogg",length=5.133}}
+L["ha dibah"]={{path="chatsounds/autoadd/klingon/ha dibah.ogg",length=2.800}}
+L["heghtay"]={{path="chatsounds/autoadd/klingon/heghtay.ogg",length=1.933}}
+L["highos"]={{path="chatsounds/autoadd/klingon/highos.ogg",length=4.866}}
+L["hija"]={{path="chatsounds/autoadd/klingon/hija.ogg",length=3.533}}
+L["hiq hivje mey"]={{path="chatsounds/autoadd/klingon/hiq hivje mey.ogg",length=3.533}}
+L["hod"]={{path="chatsounds/autoadd/klingon/hod.ogg",length=3.800}}
+L["hohwi"]={{path="chatsounds/autoadd/klingon/hohwi.ogg",length=3.200}}
+L["huh"]={{path="chatsounds/autoadd/klingon/huh.ogg",length=4.466}}
+L["hut"]={{path="chatsounds/autoadd/klingon/hut.ogg",length=0.848}}
+L["iw  ip"]={{path="chatsounds/autoadd/klingon/iw  ip.ogg",length=2.866}}
+L["iw  ip ghomey"]={{path="chatsounds/autoadd/klingon/iw  ip ghomey.ogg",length=3.733}}
+L["jav"]={{path="chatsounds/autoadd/klingon/jav.ogg",length=0.894}}
+L["jiyaj"]={{path="chatsounds/autoadd/klingon/jiyaj.ogg",length=3.266}}
+L["jol yichu"]={{path="chatsounds/autoadd/klingon/jol yichu.ogg",length=3.400}}
+L["lighon duqwi  pogh"]={{path="chatsounds/autoadd/klingon/lighon duqwi  pogh.ogg",length=5.466}}
+L["lopno"]={{path="chatsounds/autoadd/klingon/lopno.ogg",length=4.666}}
+L["los"]={{path="chatsounds/autoadd/klingon/los.ogg",length=0.929}}
+L["losmah"]={{path="chatsounds/autoadd/klingon/losmah.ogg",length=1.312}}
+L["luq ratlh"]={{path="chatsounds/autoadd/klingon/luq ratlh.ogg",length=4.733}}
+L["nahlet"]={{path="chatsounds/autoadd/klingon/nahlet.ogg",length=2.466}}
+L["nalqad"]={{path="chatsounds/autoadd/klingon/nalqad.ogg",length=3.533}}
+L["naqjej"]={{path="chatsounds/autoadd/klingon/naqjej.ogg",length=2.866}}
+L["nentay"]={{path="chatsounds/autoadd/klingon/nentay.ogg",length=5.200}}
+L["niswi  beh"]={{path="chatsounds/autoadd/klingon/niswi  beh.ogg",length=3.266}}
+L["niswi mey"]={{path="chatsounds/autoadd/klingon/niswi mey.ogg",length=3.466}}
+L["no  dir"]={{path="chatsounds/autoadd/klingon/no  dir.ogg",length=2.800}}
+L["nuchpu"]={{path="chatsounds/autoadd/klingon/nuchpu.ogg",length=3.800}}
+L["nuh"]={{path="chatsounds/autoadd/klingon/nuh.ogg",length=2.466}}
+L["nuh bey"]={{path="chatsounds/autoadd/klingon/nuh bey.ogg",length=2.933}}
+L["nuqneh"]={{path="chatsounds/autoadd/klingon/nuqneh.ogg",length=3.000}}
+L["oy naq"]={{path="chatsounds/autoadd/klingon/oy naq.ogg",length=4.066}}
+L["pagh"]={{path="chatsounds/autoadd/klingon/pagh.ogg",length=1.200}}
+L["petad"]={{path="chatsounds/autoadd/klingon/petad.ogg",length=3.133}}
+L["petaq"]={{path="chatsounds/autoadd/klingon/petaq.ogg",length=2.066}}
+L["pevang"]={{path="chatsounds/autoadd/klingon/pevang.ogg",length=4.400}}
+L["pitlh"]={{path="chatsounds/autoadd/klingon/pitlh.ogg",length=2.666}}
+L["pivghor"]={{path="chatsounds/autoadd/klingon/pivghor.ogg",length=3.200}}
+L["pu"]={{path="chatsounds/autoadd/klingon/pu.ogg",length=3.333}}
+L["qa hom"]={{path="chatsounds/autoadd/klingon/qa hom.ogg",length=2.800}}
+L["qagh"]={{path="chatsounds/autoadd/klingon/qagh.ogg",length=5.266}}
+L["qapla"]={{path="chatsounds/autoadd/klingon/qapla.ogg",length=2.733}}
+L["qeylis betleh"]={{path="chatsounds/autoadd/klingon/qeylis betleh.ogg",length=3.733}}
+L["qeylis mindu"]={{path="chatsounds/autoadd/klingon/qeylis mindu.ogg",length=4.733}}
+L["qi tu"]={{path="chatsounds/autoadd/klingon/qi tu.ogg",length=4.266}}
+L["qi yah"]={{path="chatsounds/autoadd/klingon/qi yah.ogg",length=2.933}}
+L["qoh"]={{path="chatsounds/autoadd/klingon/qoh.ogg",length=4.866}}
+L["qoy qeylis puqlod"]={{path="chatsounds/autoadd/klingon/qoy qeylis puqlod.ogg",length=35.248}}
+L["qul tuq"]={{path="chatsounds/autoadd/klingon/qul tuq.ogg",length=2.666}}
+L["qurgh"]={{path="chatsounds/autoadd/klingon/qurgh.ogg",length=2.933}}
+L["quv bey "]={{path="chatsounds/autoadd/klingon/quv bey /1.ogg",length=3.333},{path="chatsounds/autoadd/klingon/quv bey /2.ogg",length=4.400}}
+L["rahta"]={{path="chatsounds/autoadd/klingon/rahta.ogg",length=3.000}}
+L["reh nuhmey tu lu"]={{path="chatsounds/autoadd/klingon/reh nuhmey tu lu.ogg",length=4.400}}
+L["rep"]={{path="chatsounds/autoadd/klingon/rep.ogg",length=1.000}}
+L["ro qegh iwchab"]={{path="chatsounds/autoadd/klingon/ro qegh iwchab.ogg",length=3.666}}
+L["so wi  yichu"]={{path="chatsounds/autoadd/klingon/so wi  yichu.ogg",length=3.400}}
+L["soch"]={{path="chatsounds/autoadd/klingon/soch.ogg",length=1.033}}
+L["sos taj"]={{path="chatsounds/autoadd/klingon/sos taj.ogg",length=2.600}}
+L["targh tiq"]={{path="chatsounds/autoadd/klingon/targh tiq.ogg",length=4.600}}
+L["tiqnagh lemdu"]={{path="chatsounds/autoadd/klingon/tiqnagh lemdu.ogg",length=3.200}}
+L["tlhingan"]={{path="chatsounds/autoadd/klingon/tlhingan.ogg",length=3.400}}
+L["tlhingan hol dajatlh a"]={{path="chatsounds/autoadd/klingon/tlhingan hol dajatlh a.ogg",length=5.733}}
+L["tu homi rah"]={{path="chatsounds/autoadd/klingon/tu homi rah.ogg",length=3.200}}
+L["tujqu choh quq"]={{path="chatsounds/autoadd/klingon/tujqu choh quq.ogg",length=4.800}}
+L["vagh"]={{path="chatsounds/autoadd/klingon/vagh.ogg",length=1.161}}
+L["vaghmah"]={{path="chatsounds/autoadd/klingon/vaghmah.ogg",length=1.405}}
+L["vatlh"]={{path="chatsounds/autoadd/klingon/vatlh.ogg",length=0.882}}
+L["veq"]={{path="chatsounds/autoadd/klingon/veq.ogg",length=2.533}}
+L["veqlargh"]={{path="chatsounds/autoadd/klingon/veqlargh.ogg",length=4.000}}
+L["verengan ha dibah"]={{path="chatsounds/autoadd/klingon/verengan ha dibah.ogg",length=3.866}}
+L["vino va  qurgh"]={{path="chatsounds/autoadd/klingon/vino va  qurgh.ogg",length=3.333}}
+L["vithay"]={{path="chatsounds/autoadd/klingon/vithay.ogg",length=2.333}}
+L["wa"]={{path="chatsounds/autoadd/klingon/wa.ogg",length=0.639}}
+L["wa mah"]={{path="chatsounds/autoadd/klingon/wa mah.ogg",length=1.254}}
+L["wej"]={{path="chatsounds/autoadd/klingon/wej.ogg",length=1.022}}
+L["wejmah"]={{path="chatsounds/autoadd/klingon/wejmah.ogg",length=1.184}}
+L["wiy yicha"]={{path="chatsounds/autoadd/klingon/wiy yicha.ogg",length=3.733}}
+L["wornagh"]={{path="chatsounds/autoadd/klingon/wornagh.ogg",length=3.866}}
+L["yibej"]={{path="chatsounds/autoadd/klingon/yibej.ogg",length=4.200}}
+L["yimev"]={{path="chatsounds/autoadd/klingon/yimev.ogg",length=3.666}}
+L["yintagh"]={{path="chatsounds/autoadd/klingon/yintagh.ogg",length=3.400}}
+L["yiqim"]={{path="chatsounds/autoadd/klingon/yiqim.ogg",length=2.933}}
+L["yiratlh"]={{path="chatsounds/autoadd/klingon/yiratlh.ogg",length=2.333}}
+L["yitamchoh"]={{path="chatsounds/autoadd/klingon/yitamchoh.ogg",length=3.266}}
+L["yitlhutlhchu"]={{path="chatsounds/autoadd/klingon/yitlhutlhchu.ogg",length=2.933}}
+L["yuch"]={{path="chatsounds/autoadd/klingon/yuch.ogg",length=3.866}}
+c.EndList()
