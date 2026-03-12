b626487028852e283a9501403d8d3d3eb8dd7815
Meta Automator <MetaAutomator@users.noreply.github.com>
2026-03-12 17:56:55 +0000

    Commit from GitHub Actions (Push Action)

--- lua/chatsounds/lists_nosend/memes.lua
+++ lua/chatsounds/lists_nosend/memes.lua
@@ -115,6 +115,7 @@ L["hitting the griddy"]={{path="chatsounds/autoadd/memes/hitting the griddy.ogg"
 L["hmm creamy italian"]={{path="chatsounds/autoadd/memes/hmm creamy italian.ogg",length=2.798}}
 L["honey you mean hunkules"]={{path="chatsounds/autoadd/memes/honey you mean hunkules.ogg",length=2.145}}
 L["honey you mean hunkules distorted"]={{path="chatsounds/autoadd/memes/honey you mean hunkules distorted.ogg",length=3.121}}
+L["hood cricket"]={{path="chatsounds/autoadd/memes/hood cricket.ogg",length=1.100}}
 L["how does anyone find that funny"]={{path="chatsounds/autoadd/memes/how does anyone find that funny.ogg",length=1.687}}
 L["hrrngh"]={{path="chatsounds/autoadd/memes/hrrngh.ogg",length=0.891}}
 L["huh"]={{path="chatsounds/autoadd/memes/huh.ogg",length=0.383}}
@@ -170,6 +171,7 @@ L["line up at the start"]={{path="chatsounds/autoadd/memes/line up at the start.
 L["look again"]={{path="chatsounds/autoadd/memes/look again.ogg",length=0.873}}
 L["look down"]={{path="chatsounds/autoadd/memes/look down.ogg",length=0.685}}
 L["look down nathan"]={{path="chatsounds/autoadd/memes/look down nathan.ogg",length=1.055}}
+L["low honor"]={{path="chatsounds/autoadd/memes/low honor.ogg",length=5.217}}
 L["lumiscream"]={{path="chatsounds/autoadd/memes/lumiscream.ogg",length=0.824}}
 L["man i could use a monster right now i could go for a monster need a monster right now"]={{path="chatsounds/autoadd/memes/man i could use a monster right now i could go for a monster need a monster right now.ogg",length=7.245}}
 L["mega gay zone"]={{path="chatsounds/autoadd/memes/mega gay zone.ogg",length=1.608}}
