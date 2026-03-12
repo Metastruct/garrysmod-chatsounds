06bca4da50b7b57573eb6e50603ac12965eaaeb2
Meta Automator <MetaAutomator@users.noreply.github.com>
2026-03-12 17:50:54 +0000

    Commit from GitHub Actions (Push Action)

--- lua/chatsounds/lists_nosend/baldis_basics.lua
+++ lua/chatsounds/lists_nosend/baldis_basics.lua
@@ -1,12 +1,14 @@
 c.StartList("baldis_basics")
-L["apple cronch"]={{path="chatsounds/autoadd/baldis_basics/apple cronch.ogg",length=1.231}}
-L["apple crunch"]={{path="chatsounds/autoadd/baldis_basics/apple crunch.ogg",length=1.231}}
+L["aha you got it"]={{path="chatsounds/autoadd/baldis_basics/aha you got it/1.ogg",length=2.183},{path="chatsounds/autoadd/baldis_basics/aha you got it/2.ogg",length=2.183}}
+L["an apple for me thanks"]={{path="chatsounds/autoadd/baldis_basics/an apple for me thanks.ogg",length=3.855}}
+L["apple crunch"]={{path="chatsounds/autoadd/baldis_basics/apple crunch/1.ogg",length=1.231},{path="chatsounds/autoadd/baldis_basics/apple crunch/2.ogg",length=1.231}}
 L["oh"]={{path="chatsounds/autoadd/baldis_basics/oh.ogg",length=0.438}}
 L["oh hi welcome to my schoolhouse"]={{path="chatsounds/autoadd/baldis_basics/oh hi welcome to my schoolhouse/1.ogg",length=5.129},{path="chatsounds/autoadd/baldis_basics/oh hi welcome to my schoolhouse/2.ogg",length=5.596}}
 L["ohhhh"]={{path="chatsounds/autoadd/baldis_basics/ohhhh.ogg",length=2.229}}
+L["reverb wow"]={{path="chatsounds/autoadd/baldis_basics/reverb wow.ogg",length=3.019}}
 L["ruler slap"]={{path="chatsounds/autoadd/baldis_basics/ruler slap.ogg",length=1.138}}
 L["ruler snap"]={{path="chatsounds/autoadd/baldis_basics/ruler snap.ogg",length=1.858}}
-L["shrink machine sound"]={{path="chatsounds/autoadd/baldis_basics/shrink machine sound.ogg",length=14.343}}
-L["woahooohhh aaaaahhhhhhh"]={{path="chatsounds/autoadd/baldis_basics/woahooohhh aaaaahhhhhhh.ogg",length=14.343}}
+L["shrink machine sound"]={{path="chatsounds/autoadd/baldis_basics/shrink machine sound.ogg",length=5.691}}
+L["woahooohhh aaaaahhhhhhh"]={{path="chatsounds/autoadd/baldis_basics/woahooohhh aaaaahhhhhhh.ogg",length=6.784}}
 L["yum"]={{path="chatsounds/autoadd/baldis_basics/yum.ogg",length=1.486}}
 c.EndList()
