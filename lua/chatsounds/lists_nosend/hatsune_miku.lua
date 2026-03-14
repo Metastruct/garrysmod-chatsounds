bcf6e7d8ff1ef0cd80295988d1e8b0654381a6e1
Meta Automator <MetaAutomator@users.noreply.github.com>
2026-03-14 13:24:45 +0000

    Commit from GitHub Actions (Push Action)

---
+++ lua/chatsounds/lists_nosend/hatsune_miku.lua
@@ -0,0 +1,6 @@
+c.StartList("hatsune_miku")
+L["1 2 3"]={{path="chatsounds/autoadd/hatsune_miku/1 2 3.ogg",length=3.589}}
+L["and now its time for the moment youve been waiting for"]={{path="chatsounds/autoadd/hatsune_miku/and now its time for the moment youve been waiting for.ogg",length=4.706}}
+L["miku miku beam"]={{path="chatsounds/autoadd/hatsune_miku/miku miku beam.ogg",length=9.550}}
+L["ready"]={{path="chatsounds/autoadd/hatsune_miku/ready.ogg",length=0.816}}
+c.EndList()
