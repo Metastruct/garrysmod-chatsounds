3880e5f72b506b4a5f007233abdb4571d5fb4dc9
Meta Automator <MetaAutomator@users.noreply.github.com>
2026-05-12 15:30:33 +0000

    Commit from GitHub Actions (Push Action)

---
+++ lua/chatsounds/lists_nosend/trevor_and_michael.lua
@@ -0,0 +1,18 @@
+c.StartList("trevor_and_michael")
+L["and he kinda got a little angry so i admit i kinda got a little angry"]={{path="chatsounds/autoadd/trevor_and_michael/and he kinda got a little angry so i admit i kinda got a little angry.ogg",length=5.004}}
+L["bastard you know i really dont know why you mess around with people like that mike i mean really"]={{path="chatsounds/autoadd/trevor_and_michael/bastard you know i really dont know why you mess around with people like that mike i mean really.ogg",length=4.795}}
+L["but i did kidnap his wife"]={{path="chatsounds/autoadd/trevor_and_michael/but i did kidnap his wife.ogg",length=1.764}}
+L["did you kill him"]={{path="chatsounds/autoadd/trevor_and_michael/did you kill him.ogg",length=1.372}}
+L["i asked for a fair days pay after fair days work"]={{path="chatsounds/autoadd/trevor_and_michael/i asked for a fair days pay after fair days work.ogg",length=4.742}}
+L["no wonder people are stabbin em in the back"]={{path="chatsounds/autoadd/trevor_and_michael/no wonder people are stabbin em in the back.ogg",length=2.626}}
+L["oh fuck"]={{path="chatsounds/autoadd/trevor_and_michael/oh fuck.ogg",length=1.268}}
+L["oh shit"]={{path="chatsounds/autoadd/trevor_and_michael/oh shit.ogg",length=1.147}}
+L["ohh no"]={{path="chatsounds/autoadd/trevor_and_michael/ohh no.ogg",length=1.346}}
+L["piece of turd eh"]={{path="chatsounds/autoadd/trevor_and_michael/piece of turd eh.ogg",length=2.130}}
+L["the fuck did you do"]={{path="chatsounds/autoadd/trevor_and_michael/the fuck did you do.ogg",length=1.346}}
+L["the fuck has happened"]={{path="chatsounds/autoadd/trevor_and_michael/the fuck has happened.ogg",length=1.973}}
+L["trevor answer the fucking question"]={{path="chatsounds/autoadd/trevor_and_michael/trevor answer the fucking question.ogg",length=2.548}}
+L["what happened"]={{path="chatsounds/autoadd/trevor_and_michael/what happened.ogg",length=1.242}}
+L["what kind of fucking animal do you take me for no i didnt kill him"]={{path="chatsounds/autoadd/trevor_and_michael/what kind of fucking animal do you take me for no i didnt kill him.ogg",length=3.697}}
+L["why do you have his car"]={{path="chatsounds/autoadd/trevor_and_michael/why do you have his car.ogg",length=1.451}}
+c.EndList()
