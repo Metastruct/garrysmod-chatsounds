813cab6fc59e4759816a98446ce04ec2c2df68de
Meta Automator <MetaAutomator@users.noreply.github.com>
2026-06-23 23:12:43 +0000

    Commit from GitHub Actions (Push Action)

---
+++ lua/chatsounds/lists_nosend/pvz.lua
@@ -0,0 +1,4 @@
+c.StartList("pvz")
+L["the zombies are coming"]={{path="chatsounds/autoadd/pvz/the zombies are coming.ogg",length=3.871}}
+L["winmusic"]={{path="chatsounds/autoadd/pvz/winmusic.ogg",length=3.946}}
+c.EndList()
