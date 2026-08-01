d82df7a6e9ce4608b35f7ecacf016c2133f5ea8b
Meta Automator <30638685+MetaAutomator@users.noreply.github.com>
2026-08-01 18:10:24 +0000

    Commit from GitHub Actions (Push Action)

--- lua/chatsounds/lists_nosend/let_it_ride.lua
+++ lua/chatsounds/lists_nosend/let_it_ride.lua
@@ -1,7 +1,3 @@
 c.StartList("let_it_ride")
-L["1"]={{path="chatsounds/autoadd/let_it_ride/1.ogg",length=0.645}}
-L["2"]={{path="chatsounds/autoadd/let_it_ride/2.ogg",length=0.629}}
-L["3"]={{path="chatsounds/autoadd/let_it_ride/3.ogg",length=0.834}}
-L["4"]={{path="chatsounds/autoadd/let_it_ride/4.ogg",length=0.501}}
 L["fuck vegas"]={{path="chatsounds/autoadd/let_it_ride/fuck vegas.ogg",length=1.111}}
 c.EndList()
