7d2af238714d5cf8978517a3b4e72eafc67f736f
Meta Automator <30638685+MetaAutomator@users.noreply.github.com>
2026-09-24 16:14:00 +0000

    Commit from GitHub Actions (Push Action)

--- lua/chatsounds/lists_nosend/markiplier.lua
+++ lua/chatsounds/lists_nosend/markiplier.lua
@@ -1,6 +1,11 @@
 c.StartList("markiplier")
+L["come where i am"]={{path="chatsounds/autoadd/markiplier/come where i am.ogg",length=0.910}}
 L["hello everybody my name is markiplier and im officially back yes yes yes"]={{path="chatsounds/autoadd/markiplier/hello everybody my name is markiplier and im officially back yes yes yes.ogg",length=7.559}}
 L["hello everybody my name is markiplier and welcome to five nights at freddies"]={{path="chatsounds/autoadd/markiplier/hello everybody my name is markiplier and welcome to five nights at freddies.ogg",length=4.319}}
+L["huh he he who is coming"]={{path="chatsounds/autoadd/markiplier/huh he he who is coming.ogg",length=2.865}}
+L["if youre coming right now let me see it let me see it show me"]={{path="chatsounds/autoadd/markiplier/if youre coming right now let me see it let me see it show me.ogg",length=4.545}}
 L["was that the bite of 87"]={{path="chatsounds/autoadd/markiplier/was that the bite of 87.ogg",length=2.601}}
 L["what the hell is that why the hell is that what the hells going on"]={{path="chatsounds/autoadd/markiplier/what the hell is that why the hell is that what the hells going on.ogg",length=6.233}}
+L["whos coming huh"]={{path="chatsounds/autoadd/markiplier/whos coming huh.ogg",length=1.780}}
+L["you better get out here you better come out here"]={{path="chatsounds/autoadd/markiplier/you better get out here you better come out here.ogg",length=2.835}}
 c.EndList()
